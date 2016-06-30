779c96792e9e005d6b6986f576d85a22e27e5d87
Liang Li <liang.z.li@intel.com>
2016-06-30 15:31:43 +0800

    balloon: Fix failure of updating guest memory status
    After live migration, 'guest-stats' can't get the expected memory
    status in the guest. This issue is caused by commit 4eae2a657d.
    The value of 's->stats_vq_elem' will be NULL after live migration,
    and the check in the function 'balloon_stats_poll_cb()' will
    prevent the 'virtio_notify()' from executing. So guest will not
    update the memory status.
    
    Signed-off-by: Liang Li <liang.z.li@intel.com>
    Cc: Michael S. Tsirkin <mst@redhat.com>
    Cc: Ladi Prosek <lprosek@redhat.com>
    Cc: Paolo Bonzini <pbonzini@redhat.com>
    Message-id: 1467271903-23812-1-git-send-email-liang.z.li@intel.com

--- hw/virtio/virtio-balloon.c
+++ hw/virtio/virtio-balloon.c
@@ -98,13 +98,19 @@ static void balloon_stats_poll_cb(void *opaque)
 {
     VirtIOBalloon *s = opaque;
     VirtIODevice *vdev = VIRTIO_DEVICE(s);
+    VirtQueueElement elem = {0};
 
-    if (s->stats_vq_elem == NULL || !balloon_stats_supported(s)) {
+    if (!balloon_stats_supported(s)) {
         /* re-schedule */
         balloon_stats_change_timer(s, s->stats_poll_interval);
         return;
     }
 
+    if (s->stats_vq_elem == NULL) {
+        virtqueue_push(s->svq, &elem, 0);
+        virtio_notify(vdev, s->svq);
+        return;
+    }
     virtqueue_push(s->svq, s->stats_vq_elem, s->stats_vq_offset);
     virtio_notify(vdev, s->svq);
     g_free(s->stats_vq_elem);
