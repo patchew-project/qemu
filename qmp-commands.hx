e18d7ccd23f23b76193af8b3aad1350b025a9748
Alberto Garcia <berto@igalia.com>
2016-07-01 18:52:07 +0300

    commit: Add 'job-id' parameter to 'block-commit'
    This patch adds a new optional 'job-id' parameter to 'block-commit',
    allowing the user to specify the ID of the block job to be created.
    
    Signed-off-by: Alberto Garcia <berto@igalia.com>
    Reviewed-by: Max Reitz <mreitz@redhat.com>
    Message-id: 48bdf3ad3ff8ee6b68ecb87ba76147aa0cbfeeea.1467386530.git.berto@igalia.com
    Reviewed-by: Max Reitz <mreitz@redhat.com>

--- qmp-commands.hx
+++ qmp-commands.hx
@@ -1150,7 +1150,7 @@ EQMP
 
     {
         .name       = "block-commit",
-        .args_type  = "device:B,base:s?,top:s?,backing-file:s?,speed:o?",
+        .args_type  = "job-id:s?,device:B,base:s?,top:s?,backing-file:s?,speed:o?",
         .mhandler.cmd_new = qmp_marshal_block_commit,
     },
 
@@ -1163,6 +1163,7 @@ data between 'top' and 'base' into 'base'.
 
 Arguments:
 
+- "job-id": identifier for the newly-created block job (json-string, optional)
 - "device": The device's ID, must be unique (json-string)
 - "base": The file name of the backing image to write data into.
           If not specified, this is the deepest backing image
