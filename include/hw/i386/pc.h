90de19fe4327f35b21d4faf1cc0b7fe96bb48d57
Dr. David Alan Gilbert <dgilbert@redhat.com>
2016-07-04 20:16:06 +0100

    x86: fill high bits of mtrr mask
    Fill the bits between 51..number-of-physical-address-bits in the
    MTRR_PHYSMASKn variable range mtrr masks so that they're consistent
    in the migration stream irrespective of the physical address space
    of the source VM in a migration.
    
    Signed-off-by: Dr. David Alan Gilbert <dgilbert@redhat.com>
    Suggested-by: Paolo Bonzini <pbonzini@redhat.com>
    Message-id: 1467659769-15900-4-git-send-email-dgilbert@redhat.com
    Suggested-by: Paolo Bonzini <pbonzini@redhat.com>

--- include/hw/i386/pc.h
+++ include/hw/i386/pc.h
@@ -374,6 +374,11 @@ bool e820_get_entry(int, uint32_t, uint64_t *, uint64_t *);
         .driver   = TYPE_X86_CPU,\
         .property = "cpuid-0xb",\
         .value    = "off",\
+    },\
+    {\
+        .driver = TYPE_X86_CPU,\
+        .property = "fill-mtrr-mask",\
+        .value = "off",\
     },
 
 #define PC_COMPAT_2_5 \
