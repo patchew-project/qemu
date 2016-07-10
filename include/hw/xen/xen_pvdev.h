02523b70e42c73f5adf36effc12f30e42316bfe2
Emil Condrea <emilcondrea@gmail.com>
2016-07-10 14:47:40 +0300

    xen: Rename xen_be_evtchn_event
    Prepare xen_be_evtchn_event to be shared with frontends:
     * xen_be_evtchn_event -> xen_pv_evtchn_event
    
    Signed-off-by: Emil Condrea <emilcondrea@gmail.com>
    Message-id: 1468151270-12984-10-git-send-email-emilcondrea@gmail.com

--- include/hw/xen/xen_pvdev.h
+++ include/hw/xen/xen_pvdev.h
@@ -66,7 +66,7 @@ void xenstore_update(void *unused);
 
 const char *xenbus_strstate(enum xenbus_state state);
 
-void xen_be_evtchn_event(void *opaque);
+void xen_pv_evtchn_event(void *opaque);
 void xen_pv_insert_xendev(struct XenDevice *xendev);
 struct XenDevice *xen_be_del_xendev(int dom, int dev);
 struct XenDevice *xen_be_find_xendev(const char *type, int dom, int dev);
