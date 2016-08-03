0d00d3e7d3c8381b36fb338e4fd66bcf9cc486e4
Igor Mammedov <imammedo@redhat.com>
2016-08-03 10:04:57 +0200

    pc: pcihp: avoid adding ACPI_PCIHP_PROP_BSEL twice
    PCI hotplug for bridges was introduced only since 2.0 however
      acpi_set_bsel()->object_property_add_uint32_ptr(bus, ACPI_PCIHP_PROP_BSEL)
    didn't take in account that for legacy mode (1.7) when
    PCI hotplug for bridges is unavailable and ACPI_PCIHP_PROP_BSEL property
    the only bus "PCI.0' has been created earlier at acpi_pcihp_init() time.
    
    We managed to live with it only because of error rised by adding
    a duplicate property in acpi_set_bsel() has been ignored which
    resulted in useless leaking of just allocated (int)bus_bsel.
    
    Issue affects only 1.7 machine type as ACPI tables supported by
    QEMU were introduced at that time, but there wasn't PCI hotplug
    for bridges till the next release (2.0).
    
    Fix it by removing duplicate ACPI_PCIHP_PROP_BSEL intialization
    in acpi_pcihp_init() and doing it only in one place acpi_set_pci_info().
    
    PS:
    do not ignore error returned by object_property_add_uint32_ptr()
    and abort QEMU since it's programming error which should be fixed
    instead of being ignored.
    
    Signed-off-by: Igor Mammedov <imammedo@redhat.com>
    Reported-by: Marc-André Lureau <marcandre.lureau@redhat.com>
    Message-Id: <1470211497-116801-1-git-send-email-imammedo@redhat.com>
    Reported-by: Marc-André Lureau <marcandre.lureau@redhat.com>

--- hw/acpi/pcihp.c
+++ hw/acpi/pcihp.c
@@ -302,16 +302,6 @@ void acpi_pcihp_init(Object *owner, AcpiPciHpState *s, PCIBus *root_bus,
     s->root= root_bus;
     s->legacy_piix = !bridges_enabled;
 
-    if (s->legacy_piix) {
-        unsigned *bus_bsel = g_malloc(sizeof *bus_bsel);
-
-        s->io_len = ACPI_PCIHP_LEGACY_SIZE;
-
-        *bus_bsel = ACPI_PCIHP_BSEL_DEFAULT;
-        object_property_add_uint32_ptr(OBJECT(root_bus), ACPI_PCIHP_PROP_BSEL,
-                                       bus_bsel, NULL);
-    }
-
     memory_region_init_io(&s->io, owner, &acpi_pcihp_io_ops, s,
                           "acpi-pci-hotplug", s->io_len);
     memory_region_add_subregion(address_space_io, s->io_base, &s->io);
