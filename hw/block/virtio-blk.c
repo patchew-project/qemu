42ede2ea2085b98b2516e96924aed5d885f07b6a
Changlong Xie <xiecl.fnst@cn.fujitsu.com>
2016-08-03 16:49:07 +0800

    virtio-blk: rename virtio_device_info to virtio_blk_info
    The old one is confusing with @virtio_device_info in virtio.c,
    so make it more appropriate.
    
    Signed-off-by: Changlong Xie <xiecl.fnst@cn.fujitsu.com>
    Message-Id: <1470214147-32560-1-git-send-email-xiecl.fnst@cn.fujitsu.com>

--- hw/block/virtio-blk.c
+++ hw/block/virtio-blk.c
@@ -987,7 +987,7 @@ static void virtio_blk_class_init(ObjectClass *klass, void *data)
     vdc->load = virtio_blk_load_device;
 }
 
-static const TypeInfo virtio_device_info = {
+static const TypeInfo virtio_blk_info = {
     .name = TYPE_VIRTIO_BLK,
     .parent = TYPE_VIRTIO_DEVICE,
     .instance_size = sizeof(VirtIOBlock),
@@ -997,7 +997,7 @@ static const TypeInfo virtio_device_info = {
 
 static void virtio_register_types(void)
 {
-    type_register_static(&virtio_device_info);
+    type_register_static(&virtio_blk_info);
 }
 
 type_init(virtio_register_types)
