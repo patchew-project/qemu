9e0ec18b38bdb8d40f72f01ed66d1f5d95ee5238
Daniel P. Berrange <berrange@redhat.com>
2016-08-09 16:32:00 +0100

    trace: switch hw/nvram/ directory to modular trace.h file
    Switch files in the hw/nvram/ directory to include the
    hw/nvram/trace.h file instead of the global trace.h
    file.
    
    Signed-off-by: Daniel P. Berrange <berrange@redhat.com>
    Message-Id: <1470756748-18933-33-git-send-email-berrange@redhat.com>

--- hw/nvram/fw_cfg.c
+++ hw/nvram/fw_cfg.c
@@ -30,7 +30,7 @@
 #include "hw/nvram/fw_cfg.h"
 #include "hw/sysbus.h"
 #include "hw/boards.h"
-#include "trace.h"
+#include "hw/nvram/trace.h"
 #include "qemu/error-report.h"
 #include "qemu/config-file.h"
 #include "qemu/cutils.h"
