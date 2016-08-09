7f2c96a8a0240d26ddcca890313f67b19eb55999
Fam Zheng <famz@redhat.com>
2016-08-09 14:49:59 +0800

    util: Add UUID API
    A number of different places across the code base use CONFIG_UUID. Some
    of them are soft dependency, some are not built if libuuid is not
    available, some come with dummy fallback, some throws runtime error.
    
    It is hard to maintain, and hard to reason for users.
    
    Since UUID is a simple standard with only a small number of operations,
    it is cleaner to have a central support in libqemuutil. This patch adds
    qemu_uuid_* the functions so that all uuid users in the code base can
    rely on. Except for qemu_uuid_generate which is new code, all other
    functions are just copy from existing fallbacks from other files.
    
    Note that qemu_uuid_parse is moved without updating the function
    signature to use QemuUUID, to keep this patch simple.
    
    Signed-off-by: Fam Zheng <famz@redhat.com>
    Message-Id: <1470725407-5051-2-git-send-email-famz@redhat.com>

---
+++ include/qemu/uuid.h
@@ -0,0 +1,48 @@
+/*
+ *  QEMU UUID functions
+ *
+ *  Copyright 2016 Red Hat, Inc.,
+ *
+ *  Authors:
+ *   Fam Zheng <famz@redhat.com>
+ *
+ * This program is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU General Public License as published by the Free
+ * Software Foundation; either version 2 of the License, or (at your option)
+ * any later version.
+ *
+ */
+
+#ifndef QEMU_UUID_H
+#define QEMU_UUID_H
+
+#include "qemu-common.h"
+
+/* Version 4 UUID (pseudo random numbers), RFC4122 4.4. */
+
+typedef struct {
+    unsigned char data[16];
+} QemuUUID;
+
+#define UUID_FMT "%02hhx%02hhx%02hhx%02hhx-" \
+                 "%02hhx%02hhx-%02hhx%02hhx-" \
+                 "%02hhx%02hhx-" \
+                 "%02hhx%02hhx%02hhx%02hhx%02hhx%02hhx"
+
+#define UUID_FMT_LEN 36
+
+#define UUID_NONE "00000000-0000-0000-0000-000000000000"
+
+void qemu_uuid_generate(QemuUUID *out);
+
+int qemu_uuid_is_null(const QemuUUID *uu);
+
+void qemu_uuid_unparse(char *out, const QemuUUID *uuid);
+
+char *qemu_uuid_unparse_strdup(const QemuUUID *uuid);
+
+int qemu_uuid_parse(const char *str, uint8_t *uuid);
+
+void qemu_uuid_convert(QemuUUID *uuid);
+
+#endif
