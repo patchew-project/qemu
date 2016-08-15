f63ef93c78a79339085fbb0e59eb2562dde3e9a7
Daniel P. Berrange <berrange@redhat.com>
2016-08-15 15:22:19 +0100

    qapi: add a QmpInputVisitor that does string conversion
    Currently the QmpInputVisitor assumes that all scalar
    values are directly represented as their final types.
    ie it assumes an 'int' is using QInt, and a 'bool' is
    using QBool.
    
    This adds an alternative constructor for QmpInputVisitor
    that will set it up such that it expects a QString for
    all scalar types instead.
    
    This makes it possible to use QmpInputVisitor with a
    QDict produced from QemuOpts, where everything is in
    string format.
    
    Reviewed-by: Marc-André Lureau <marcandre.lureau@redhat.com>
    Signed-off-by: Daniel P. Berrange <berrange@redhat.com>
    Message-Id: <1471270945-19975-6-git-send-email-berrange@redhat.com>

--- include/qapi/qobject-input-visitor.h
+++ include/qapi/qobject-input-visitor.h
@@ -19,12 +19,45 @@
 
 typedef struct QObjectInputVisitor QObjectInputVisitor;
 
-/*
- * Return a new input visitor that converts a QObject to a QAPI object.
+/**
+ * qobject_input_visitor_new:
+ * @obj: the input object to visit
+ * @strict: whether to require that all input keys are consumed
  *
- * Set @strict to reject a parse that doesn't consume all keys of a
- * dictionary; otherwise excess input is ignored.
+ * Create a new input visitor that converts a QObject to a QAPI object.
+ *
+ * Any scalar values in the @obj input data structure should be in the
+ * required type already. i.e. if visiting a bool, the value should
+ * already be a QBool instance.
+ *
+ * If @strict is set to true, then an error will be reported if any
+ * dict keys are not consumed during visitation.
+ *
+ * The returned input visitor should be released by calling
+ * visit_free() when no longer required.
+ *
+ * Returns: a new input visitor
  */
 Visitor *qobject_input_visitor_new(QObject *obj, bool strict);
+
+/**
+ * qobject_string_input_visitor_new:
+ * @obj: the input object to visit
+ *
+ * Create a new input visitor that converts a QObject to a QAPI object.
+ *
+ * Any scalar values in the @obj input data structure should always be
+ * represented as strings. i.e. if visiting a boolean, the value should
+ * be a QString whose contents represent a valid boolean.
+ *
+ * The visitor always operates in strict mode, requiring all dict keys
+ * to be consumed during visitation.
+ *
+ * The returned input visitor should be released by calling
+ * visit_free() when no longer required.
+ *
+ * Returns: a new input visitor
+ */
+Visitor *qobject_string_input_visitor_new(QObject *obj);
 
 #endif
