ad32f284e3468f6b626b26c4562b77f9080b4f0e
Prasad J Pandit <pjp@fedoraproject.org>
2016-08-31 01:43:51 +0530

    scsi: check page count while initialising descriptor rings
    Vmware Paravirtual SCSI emulation uses command descriptors to
    process SCSI commands. These descriptors come with their ring
    buffers. A guest could set the page count for these rings to
    be zero, leading to an infinite loop. Add check to avoid it.
    
    Reported-by: Tom Victor <vv474172261@gmail.com>
    Signed-off-by: Prasad J Pandit <pjp@fedoraproject.org>
    Message-Id: <1472588031-6179-1-git-send-email-ppandit@redhat.com>

--- hw/scsi/vmw_pvscsi.c
+++ hw/scsi/vmw_pvscsi.c
@@ -160,8 +160,10 @@ pvscsi_ring_init_data(PVSCSIRingInfo *m, PVSCSICmdDescSetupRings *ri)
     uint32_t req_ring_size, cmp_ring_size;
     m->rs_pa = ri->ringsStatePPN << VMW_PAGE_SHIFT;
 
-    if ((ri->reqRingNumPages > PVSCSI_SETUP_RINGS_MAX_NUM_PAGES)
-        || (ri->cmpRingNumPages > PVSCSI_SETUP_RINGS_MAX_NUM_PAGES)) {
+    if (!ri->reqRingNumPages
+        || ri->reqRingNumPages > PVSCSI_SETUP_RINGS_MAX_NUM_PAGES
+        || !ri->cmpRingNumPages
+        || ri->cmpRingNumPages > PVSCSI_SETUP_RINGS_MAX_NUM_PAGES) {
         return -1;
     }
     req_ring_size = ri->reqRingNumPages * PVSCSI_MAX_NUM_REQ_ENTRIES_PER_PAGE;
