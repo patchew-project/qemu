be5d2eb2d4f8e2d9c020ec2a8d323713d8d934e7
Richard Henderson <rth@twiddle.net>
2016-09-03 13:39:35 -0700

    HACK: Always enable parallel_cpus
    This is really just a placeholder for an actual
    command-line switch for mttcg.
    Message-Id: <1472935202-3342-8-git-send-email-rth@twiddle.net>

--- translate-all.c
+++ translate-all.c
@@ -119,7 +119,7 @@ static void *l1_map[V_L1_SIZE];
 
 /* code generation context */
 TCGContext tcg_ctx;
-bool parallel_cpus;
+bool parallel_cpus = 1;
 
 /* translation block context */
 #ifdef CONFIG_USER_ONLY
