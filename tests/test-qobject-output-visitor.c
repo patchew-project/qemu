3fdda4e9a8a2cb1fcfb7c17b157cd5c505ddb251
Daniel P. Berrange <berrange@redhat.com>
2016-09-05 16:16:38 +0100

    qapi: rename QmpOutputVisitor to QObjectOutputVisitor
    The QmpOutputVisitor has no direct dependancy on QMP. It is
    valid to use it anywhere that one wants a QObject. Rename it
    to better reflect its functionality as a generic QAPI
    to QObject convertor.
    
    Signed-off-by: Daniel P. Berrange <berrange@redhat.com>
    Message-Id: <1473088600-17930-5-git-send-email-berrange@redhat.com>

--- tests/test-qobject-output-visitor.c
+++ tests/test-qobject-output-visitor.c
@@ -14,7 +14,7 @@
 
 #include "qemu-common.h"
 #include "qapi/error.h"
-#include "qapi/qmp-output-visitor.h"
+#include "qapi/qobject-output-visitor.h"
 #include "test-qapi-types.h"
 #include "test-qapi-visit.h"
 #include "qapi/qmp/types.h"
@@ -28,7 +28,7 @@ typedef struct TestOutputVisitorData {
 static void visitor_output_setup(TestOutputVisitorData *data,
                                  const void *unused)
 {
-    data->ov = qmp_output_visitor_new(&data->obj);
+    data->ov = qobject_output_visitor_new(&data->obj);
     g_assert(data->ov);
 }
 
