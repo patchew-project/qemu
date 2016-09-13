a829a31d723818cc89ad066febd3000de5b482c0
Gonglei <arei.gonglei@huawei.com>
2016-09-13 11:52:07 +0800

    crypto: introduce cryptodev backend and crypto legacy hardware
    cryptodev backend is used to realize the active work for
    virtual crypto device. CryptoLegacyHW device is a cryptographic
    hardware device seen by the virtual machine.
    The relationship between cryptodev backend and legacy hadware
    as follow:
     crypto_legacy_hw device (1)--->(n) cryptodev client backend
    
    Signed-off-by: Gonglei <arei.gonglei@huawei.com>
    Message-Id: <1473738741-220600-2-git-send-email-arei.gonglei@huawei.com>

--- include/qemu/typedefs.h
+++ include/qemu/typedefs.h
@@ -94,5 +94,6 @@ typedef struct SSIBus SSIBus;
 typedef struct uWireSlave uWireSlave;
 typedef struct VirtIODevice VirtIODevice;
 typedef struct Visitor Visitor;
+typedef struct CryptoClientState CryptoClientState;
 
 #endif /* QEMU_TYPEDEFS_H */
