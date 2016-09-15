e3f2312196c79032e17ee49c72d9ebf7cb79029c
Daniel P. Berrange <berrange@redhat.com>
2016-09-15 13:45:23 +0100

    qom: support arbitrary non-scalar properties with -object
    The current -object command line syntax only allows for
    creation of objects with scalar properties, or a list
    with a fixed scalar element type. Objects which have
    properties that are represented as structs in the QAPI
    schema cannot be created using -object.
    
    This is a design limitation of the way the OptsVisitor
    is written. It simply iterates over the QemuOpts values
    as a flat list. The support for lists is enabled by
    allowing the same key to be repeated in the opts string.
    
    It is not practical to extend the OptsVisitor to support
    more complex data structures while also maintaining
    the existing list handling behaviour that is relied upon
    by other areas of QEMU.
    
    Fortunately there is no existing object that implements
    the UserCreatable interface that relies on the list
    handling behaviour, so it is possible to swap out the
    OptsVisitor for a different visitor implementation, so
    -object supports non-scalar properties, thus leaving
    other users of OptsVisitor unaffected.
    
    The previously added qdict_crumple() method is able to
    take a qdict containing a flat set of properties and
    turn that into a arbitrarily nested set of dicts and
    lists. By combining qemu_opts_to_qdict and qdict_crumple()
    together, we can turn the opt string into a data structure
    that is practically identical to that passed over QMP
    when defining an object. The only difference is that all
    the scalar values are represented as strings, rather than
    strings, ints and bools. This is sufficient to let us
    replace the OptsVisitor with the QObjectInputVisitor for
    use with -object.
    
    Thus -object can now support non-scalar properties,
    for example the QMP object
    
      {
        "execute": "object-add",
        "arguments": {
          "qom-type": "demo",
          "id": "demo0",
          "parameters": {
            "foo": [
              { "bar": "one", "wizz": "1" },
              { "bar": "two", "wizz": "2" }
            ]
          }
        }
      }
    
    Would be creatable via the CLI now using
    
        $QEMU \
          -object demo,id=demo0,\
                  foo.0.bar=one,foo.0.wizz=1,\
                  foo.1.bar=two,foo.1.wizz=2
    
    Notice that this syntax is intentionally compatible
    with that currently used by block drivers.
    
    This is also wired up to work for the 'object_add' command
    in the HMP monitor with the same syntax.
    
      (hmp) object_add demo,id=demo0,\
                       foo.0.bar=one,foo.0.wizz=1,\
                       foo.1.bar=two,foo.1.wizz=2
    
    NB indentation should not be used with HMP commands, this
    is just for convenient formatting in this commit message.
    
    Signed-off-by: Daniel P. Berrange <berrange@redhat.com>
    Message-Id: <1473943523-20270-7-git-send-email-berrange@redhat.com>

--- hmp.c
+++ hmp.c
@@ -25,7 +25,7 @@
 #include "qemu/sockets.h"
 #include "monitor/monitor.h"
 #include "monitor/qdev.h"
-#include "qapi/opts-visitor.h"
+#include "qapi/qobject-input-visitor.h"
 #include "qapi/qmp/qerror.h"
 #include "qapi/string-output-visitor.h"
 #include "qapi/util.h"
@@ -1695,20 +1695,14 @@ void hmp_netdev_del(Monitor *mon, const QDict *qdict)
 void hmp_object_add(Monitor *mon, const QDict *qdict)
 {
     Error *err = NULL;
-    QemuOpts *opts;
     Visitor *v;
     Object *obj = NULL;
+    QDict *pdict = qdict_clone_shallow(qdict);
 
-    opts = qemu_opts_from_qdict(qemu_find_opts("object"), qdict, &err);
-    if (err) {
-        hmp_handle_error(mon, &err);
-        return;
-    }
-
-    v = opts_visitor_new(opts);
-    obj = user_creatable_add(qdict, v, &err);
+    v = qobject_string_input_visitor_new(QOBJECT(pdict));
+    obj = user_creatable_add(pdict, v, &err);
     visit_free(v);
-    qemu_opts_del(opts);
+    QDECREF(pdict);
 
     if (err) {
         hmp_handle_error(mon, &err);
