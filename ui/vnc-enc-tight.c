75e96a4b82715fed5cad304d94e66988a94744f8
Alex Bennée <alex.bennee@linaro.org>
2016-09-19 16:51:32 +0100

    ui/vnc-enc-tight: add abort() for unexpected default
    When enabling the sanitizer build it will complain about control
    reaching a non-void function. Normally the compiler should detect that
    there is only one possible exit given a static VNC_SERVER_FB_BYTES.
    
    As we should never get here I added an abort() rather than a default
    return value.
    
    Signed-off-by: Alex Bennée <alex.bennee@linaro.org>
    Message-Id: <20160919155139.28371-2-alex.bennee@linaro.org>

--- ui/vnc-enc-tight.c
+++ ui/vnc-enc-tight.c
@@ -710,6 +710,8 @@ static bool check_solid_tile(VncState *vs, int x, int y, int w, int h,
     switch (VNC_SERVER_FB_BYTES) {
     case 4:
         return check_solid_tile32(vs, x, y, w, h, color, samecolor);
+    default:
+        abort();
     }
 }
 
