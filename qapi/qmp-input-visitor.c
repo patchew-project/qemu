341b659370d9772c3c795fe066694046e3b4e596
Marc-André Lureau <marcandre.lureau@redhat.com>
2016-09-22 00:10:15 +0400

    qapi: add assert about root value
    qiv->root should not be null, make that clearer with some assert.
    
    Signed-off-by: Marc-André Lureau <marcandre.lureau@redhat.com>
    Message-Id: <20160921201018.20957-2-marcandre.lureau@redhat.com>
    Reviewed-by: Eric Blake <eblake@redhat.com>

--- qapi/qmp-input-visitor.c
+++ qapi/qmp-input-visitor.c
@@ -64,6 +64,7 @@ static QObject *qmp_input_get_object(QmpInputVisitor *qiv,
 
     if (QSLIST_EMPTY(&qiv->stack)) {
         /* Starting at root, name is ignored. */
+        assert(qiv->root);
         return qiv->root;
     }
 
@@ -384,6 +385,7 @@ Visitor *qmp_input_visitor_new(QObject *obj, bool strict)
 {
     QmpInputVisitor *v;
 
+    assert(obj);
     v = g_malloc0(sizeof(*v));
 
     v->visitor.type = VISITOR_INPUT;
