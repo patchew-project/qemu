eee04714d0ddac99f073827f1f54a44186553c5d
Eduardo Habkost <ehabkost@redhat.com>
2016-09-22 11:53:48 -0300

    target-i386: Automatically set level/xlevel/xlevel2 when needed
    Instead of requiring users and management software to be aware of
    required CPUID level/xlevel/xlevel2 values for each feature,
    automatically increase those values when features need them.
    
    This was already done for CPUID[7].EBX, and is now made generic
    for all CPUID feature flags. Unit test included, to make sure we
    don't break ABI on older machine-types and don't mess with the
    CPUID level values if they are explicitly set by the user.
    
    Signed-off-by: Eduardo Habkost <ehabkost@redhat.com>
    Message-Id: <1474556029-24992-6-git-send-email-ehabkost@redhat.com>
    Reviewed-by: Richard Henderson <rth@twiddle.net>

--- include/hw/i386/pc.h
+++ include/hw/i386/pc.h
@@ -376,7 +376,12 @@ bool e820_get_entry(int, uint32_t, uint64_t *, uint64_t *);
 
 #define PC_COMPAT_2_7 \
     PC_COMPAT_2_8 \
-    HW_COMPAT_2_7
+    HW_COMPAT_2_7 \
+    {\
+        .driver   = TYPE_X86_CPU,\
+        .property = "full-cpuid-auto-level",\
+        .value    = "off",\
+    },
 
 #define PC_COMPAT_2_6 \
     HW_COMPAT_2_6 \
