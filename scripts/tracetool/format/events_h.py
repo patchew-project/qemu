d7f293ea3ce4b1f7851929820549a846197ca257
Daniel P. Berrange <berrange@redhat.com>
2016-09-22 09:40:43 +0100

    trace: remove the TraceEventID and TraceEventVCPUID enums
    The TraceEventID and TraceEventVCPUID enums constants are
    no longer actually used for anything critical.
    
    The TRACE_EVENT_COUNT limit is used to determine the size
    of the TraceEvents array, and can be removed if we just
    NULL terminate the array instead.
    
    The TRACE_VCPU_EVENT_COUNT limit is used as a magic value
    for marking non-vCPU events, and also for declaring the
    size of the trace dstate mask in the CPUState struct.
    The former usage can be replaced by a dedicated constant
    TRACE_EVENT_VCPU_NONE, defined as (uint32_t)-1. For the
    latter usage, we can simply define a constant for the
    number of VCPUs, avoiding the need for the full enum.
    
    The only other usages of the enum values can be replaced
    by accesing the id/vcpu_id fields via the named TraceEvent
    structs.
    
    Signed-off-by: Daniel P. Berrange <berrange@redhat.com>
    Message-Id: <1474533652-31170-9-git-send-email-berrange@redhat.com>
    Reviewed-by: Stefan Hajnoczi <stefanha@redhat.com>

--- scripts/tracetool/format/events_h.py
+++ scripts/tracetool/format/events_h.py
@@ -29,27 +29,15 @@ def generate(events, backend):
         out('extern TraceEvent %(event)s;',
             event = e.api(e.QEMU_EVENT))
 
-    # event identifiers
-    out('typedef enum {')
-
-    for e in events:
-        out('    TRACE_%s,' % e.name.upper())
-
-    out('    TRACE_EVENT_COUNT',
-        '} TraceEventID;')
-
     for e in events:
         out('extern uint16_t %s;' % e.api(e.QEMU_DSTATE))
 
-    # per-vCPU event identifiers
-    out('typedef enum {')
-
+    numvcpu = 0
     for e in events:
         if "vcpu" in e.properties:
-            out('    TRACE_VCPU_%s,' % e.name.upper())
+            numvcpu += 1
 
-    out('    TRACE_VCPU_EVENT_COUNT',
-        '} TraceEventVCPUID;')
+    out("#define TRACE_VCPU_EVENT_COUNT %d" % numvcpu)
 
     # static state
     for e in events:
