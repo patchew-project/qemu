926619c8fb17cfd7447d911aca31bf4565edf534
Gerd Hoffmann <kraxel@redhat.com>
2016-09-27 10:32:47 +0200

    xhci: drop unused comp_xfer field
    Signed-off-by: Gerd Hoffmann <kraxel@redhat.com>
    Message-Id: <1474965172-30321-4-git-send-email-kraxel@redhat.com>

--- hw/usb/hcd-xhci.c
+++ hw/usb/hcd-xhci.c
@@ -384,7 +384,6 @@ struct XHCIEPContext {
 
     XHCIRing ring;
     unsigned int next_xfer;
-    unsigned int comp_xfer;
     XHCITransfer transfers[TD_QUEUE];
     XHCITransfer *retry;
     EPType type;
