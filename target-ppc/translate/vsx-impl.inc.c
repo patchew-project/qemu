bb26b2a2e42333632390826ee9da5dd6a6ac97df
Nikunj A Dadhania <nikunj@linux.vnet.ibm.com>
2016-09-28 11:01:25 +0530

    target-ppc: add stxvh8x instruction
    stxvh8x:  Store VSX Vector Halfword*8
    
    Vector:
    +-------+-------+-------+-------+-------+-------+-------+-------+
    | 00 01 | 10 11 | 20 21 | 30 31 | 40 41 | 50 51 | 60 61 | 70 71 |
    +-------+-------+-------+-------+-------+-------+-------+-------+
    
    Store results in following:
    
    Big-Endian Storage
    +-------+-------+-------+-------+-------+-------+-------+-------+
    | 00 01 | 10 11 | 20 21 | 30 31 | 40 41 | 50 51 | 60 61 | 70 71 |
    +-------+-------+-------+-------+-------+-------+-------+-------+
    
    Little-Endian Storage
    +-------+-------+-------+-------+-------+-------+-------+-------+
    | 01 00 | 11 10 | 21 20 | 31 30 | 41 40 | 51 50 | 61 60 | 71 70 |
    +-------+-------+-------+-------+-------+-------+-------+-------+
    
    Signed-off-by: Nikunj A Dadhania <nikunj@linux.vnet.ibm.com>
    Message-Id: <1475040687-27523-8-git-send-email-nikunj@linux.vnet.ibm.com>

--- target-ppc/translate/vsx-impl.inc.c
+++ target-ppc/translate/vsx-impl.inc.c
@@ -206,6 +206,33 @@ static void gen_stxvw4x(DisasContext *ctx)
     tcg_temp_free(EA);
 }
 
+static void gen_stxvh8x(DisasContext *ctx)
+{
+    TCGv_i64 xsh = cpu_vsrh(xS(ctx->opcode));
+    TCGv_i64 xsl = cpu_vsrl(xS(ctx->opcode));
+    TCGv EA;
+
+    if (unlikely(!ctx->vsx_enabled)) {
+        gen_exception(ctx, POWERPC_EXCP_VSXU);
+        return;
+    }
+    gen_set_access_type(ctx, ACCESS_INT);
+    EA = tcg_temp_new();
+    gen_addr_reg_index(ctx, EA);
+    if (ctx->le_mode) {
+        gen_helper_bswap16x4(xsh, xsh);
+        tcg_gen_qemu_st_i64(xsh, EA, ctx->mem_idx, MO_BEQ);
+        tcg_gen_addi_tl(EA, EA, 8);
+        gen_helper_bswap16x4(xsl, xsl);
+        tcg_gen_qemu_st_i64(xsl, EA, ctx->mem_idx, MO_BEQ);
+    } else {
+        tcg_gen_qemu_st_i64(xsh, EA, ctx->mem_idx, MO_BEQ);
+        tcg_gen_addi_tl(EA, EA, 8);
+        tcg_gen_qemu_st_i64(xsl, EA, ctx->mem_idx, MO_BEQ);
+    }
+    tcg_temp_free(EA);
+}
+
 #define MV_VSRW(name, tcgop1, tcgop2, target, source)           \
 static void gen_##name(DisasContext *ctx)                       \
 {                                                               \
