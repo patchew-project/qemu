976fa60bfcc47ab1579b00d86b80af1d26bc454d
Laurent Vivier <lvivier@redhat.com>
2016-09-28 20:51:33 +0200

    tests: enable ohci/uhci/xhci tests on PPC64
    Signed-off-by: Laurent Vivier <lvivier@redhat.com>
    Message-Id: <1475088693-29091-7-git-send-email-lvivier@redhat.com>

--- tests/libqos/usb.c
+++ tests/libqos/usb.c
@@ -28,7 +28,7 @@ void qusb_pci_init_one(QPCIBus *pcibus, struct qhc *hc, uint32_t devfn, int bar)
 void uhci_port_test(struct qhc *hc, int port, uint16_t expect)
 {
     void *addr = hc->base + 0x10 + 2 * port;
-    uint16_t value = qpci_io_readw(hc->dev, addr);
+    uint16_t value = target_le16_to_cpu(qpci_io_readw(hc->dev, addr));
     uint16_t mask = ~(UHCI_PORT_WRITE_CLEAR | UHCI_PORT_RSVD1);
 
     g_assert((value & mask) == (expect & mask));
