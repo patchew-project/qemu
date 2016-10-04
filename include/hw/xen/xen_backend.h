0fadd6ddea4342b5fd2f79706d15c77a8c2aedeb
Emil Condrea <emilcondrea@gmail.com>
2016-10-04 09:43:36 +0300

    xen: Prepare xendev qtail to be shared with frontends
     * move xendevs qtail to xen_pvdev.c
     * change xen_be_get_xendev to use a new function: xen_pv_insert_xendev
    
    Signed-off-by: Emil Condrea <emilcondrea@gmail.com>
    Message-Id: <1475563424-6604-8-git-send-email-emilcondrea@gmail.com>

--- include/hw/xen/xen_backend.h
+++ include/hw/xen/xen_backend.h
@@ -22,7 +22,6 @@ int xenstore_read_be_int(struct XenDevice *xendev, const char *node, int *ival);
 void xenstore_update_be(char *watch, char *type, int dom,
                                struct XenDevOps *ops);
 
-struct XenDevice *xen_be_find_xendev(const char *type, int dom, int dev);
 void xen_be_check_state(struct XenDevice *xendev);
 
 /* xen backend driver bits */
