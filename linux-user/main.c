bdb6c3f8ae10f8b276b0c36b035ca76610627557
Thomas Huth <thuth@redhat.com>
2016-10-05 11:54:44 +0200

    Put the copyright information on a separate line
    The output string QEMU with "--version" is very long, it does
    not fit into a normal line of a terminal window anymore. By
    putting the copyright information on a separate line instead,
    the output looks much nicer.
    
    Signed-off-by: Thomas Huth <thuth@redhat.com>
    Message-Id: <1475661284-30153-1-git-send-email-thuth@redhat.com>
    Acked-by: Kevin Wolf <kwolf@redhat.com>
    Reviewed-by: Eric Blake <eblake@redhat.com>

--- linux-user/main.c
+++ linux-user/main.c
@@ -3936,7 +3936,7 @@ static void handle_arg_strace(const char *arg)
 static void handle_arg_version(const char *arg)
 {
     printf("qemu-" TARGET_NAME " version " QEMU_VERSION QEMU_PKGVERSION
-           ", " QEMU_COPYRIGHT "\n");
+           "\n" QEMU_COPYRIGHT "\n");
     exit(EXIT_SUCCESS);
 }
 
