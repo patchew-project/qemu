cb8d053b3075a4fd7413d70b89a6f112a3c683eb
Michael Roth <mdroth@linux.vnet.ibm.com>
2016-10-12 18:13:51 -0500

    spapr: add option vector handling in CAS-generated resets
    In some cases, ibm,client-architecture-support calls can fail. This
    could happen in the current code for situations where the modified
    device tree segment exceeds the buffer size provided by the guest
    via the call parameters. In these cases, QEMU will reset, allowing
    an opportunity to regenerate the device tree from scratch via
    boot-time handling. There are potentially other scenarios as well,
    not currently reachable in the current code, but possible in theory,
    such as cases where device-tree properties or nodes need to be removed.
    
    We currently don't handle either of these properly for option vector
    capabilities however. Instead of carrying the negotiated capability
    beyond the reset and creating the boot-time device tree accordingly,
    we start from scratch, generating the same boot-time device tree as we
    did prior to the CAS-generated and the same device tree updates as we
    did before. This could (in theory) cause us to get stuck in a reset
    loop. This hasn't been observed, but depending on the extensiveness
    of CAS-induced device tree updates in the future, could eventually
    become an issue.
    
    Address this by pulling capability-related device tree
    updates resulting from CAS calls into a common routine,
    spapr_populate_cas_updates(), and adding an sPAPROptionVector*
    parameter that allows us to test for newly-negotiated capabilities.
    We invoke it as follows:
    
    1) When ibm,client-architecture-support gets called, we
       call spapr_populate_cas_updates() with the set of capabilities
       added since the previous call to ibm,client-architecture-support.
       For the initial boot, or a system reset generated by something
       other than the CAS call itself, this set will consist of *all*
       options supported both the platform and the guest. For calls
       to ibm,client-architecture-support immediately after a CAS-induced
       reset, we call spapr_populate_cas_updates() with only the set
       of capabilities added since the previous call, since the other
       capabilities will have already been addressed by the boot-time
       device-tree this time around. In the unlikely event that
       capabilities are *removed* since the previous CAS, we will
       generate a CAS-induced reset. In the unlikely event that we
       cannot fit the device-tree updates into the buffer provided
       by the guest, well generate a CAS-induced reset.
    
    2) When a CAS update results in the need to reset the machine and
       include the updates in the boot-time device tree, we call the
       spapr_populate_cas_updates() using the full set of negotiated
       capabilities as part of the reset path. At initial boot, or after
       a reset generated by something other than the CAS call itself,
       this set will be empty, resulting in what should be the same
       boot-time device-tree as we generated prior to this patch. For
       CAS-induced reset, this routine will be called with the full set of
       capabilities negotiated by the platform/guest in the previous
       CAS call, which should result in CAS updates from previous call
       being accounted for in the initial boot-time device tree.
    
    Signed-off-by: Michael Roth <mdroth@linux.vnet.ibm.com>
    Message-Id: <1476314039-9520-4-git-send-email-mdroth@linux.vnet.ibm.com>
    Reviewed-by: David Gibson <david@gibson.dropbear.id.au>

--- include/hw/ppc/spapr.h
+++ include/hw/ppc/spapr.h
@@ -69,6 +69,7 @@ struct sPAPRMachineState {
     bool has_graphics;
     sPAPROptionVector *ov5;
     sPAPROptionVector *ov5_cas;
+    bool cas_reboot;
 
     uint32_t check_exception_irq;
     Notifier epow_notifier;
@@ -580,7 +581,8 @@ void spapr_events_init(sPAPRMachineState *sm);
 void spapr_events_fdt_skel(void *fdt, uint32_t epow_irq);
 int spapr_h_cas_compose_response(sPAPRMachineState *sm,
                                  target_ulong addr, target_ulong size,
-                                 bool cpu_update);
+                                 bool cpu_update,
+                                 sPAPROptionVector *ov5_updates);
 sPAPRTCETable *spapr_tce_new_table(DeviceState *owner, uint32_t liobn);
 void spapr_tce_table_enable(sPAPRTCETable *tcet,
                             uint32_t page_shift, uint64_t bus_offset,
