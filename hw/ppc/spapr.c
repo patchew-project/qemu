9e5d6496c0c0d10239a1110feb0059400ce88d6f
Michael Roth <mdroth@linux.vnet.ibm.com>
2016-10-12 18:13:53 -0500

    spapr: fix inheritance chain for default machine options
    Rather than machine instances having backward-compatible option
    defaults that need to be repeatedly re-enabled for every new machine
    type we introduce, we set the defaults appropriate for newer machine
    types, then add code to explicitly disable instance options as needed
    to maintain compatibility with older machine types.
    
    Currently pseries-2.5 does not inherit from pseries-2.6 in this
    fashion, which is okay at the moment since we do not have any
    instance compatibility options for pseries-2.6+ currently.
    
    We will make use of this in future patches though, so fix it here.
    
    Signed-off-by: Michael Roth <mdroth@linux.vnet.ibm.com>
    Message-Id: <1476314039-9520-6-git-send-email-mdroth@linux.vnet.ibm.com>

--- hw/ppc/spapr.c
+++ hw/ppc/spapr.c
@@ -2544,6 +2544,7 @@ DEFINE_SPAPR_MACHINE(2_7, "2.7", false);
 
 static void spapr_machine_2_6_instance_options(MachineState *machine)
 {
+    spapr_machine_2_7_instance_options(machine);
 }
 
 static void spapr_machine_2_6_class_options(MachineClass *mc)
@@ -2568,6 +2569,7 @@ DEFINE_SPAPR_MACHINE(2_6, "2.6", false);
 
 static void spapr_machine_2_5_instance_options(MachineState *machine)
 {
+    spapr_machine_2_6_instance_options(machine);
 }
 
 static void spapr_machine_2_5_class_options(MachineClass *mc)
