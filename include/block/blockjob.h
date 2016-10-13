fbf4636141b7c76481b9ed1d24824a120a2343b8
John Snow <jsnow@redhat.com>
2016-10-13 18:56:56 -0400

    blockjobs: hide internal jobs from management API
    If jobs are not created directly by the user, do not allow them to be
    seen by the user/management utility. At the moment, 'internal' jobs are
    those that do not have an ID. As of this patch it is impossible to
    create such jobs.
    
    Signed-off-by: John Snow <jsnow@redhat.com>
    Message-Id: <1476399422-8028-2-git-send-email-jsnow@redhat.com>

--- include/block/blockjob.h
+++ include/block/blockjob.h
@@ -107,7 +107,7 @@ struct BlockJob {
     BlockBackend *blk;
 
     /**
-     * The ID of the block job.
+     * The ID of the block job. May be NULL for internal jobs.
      */
     char *id;
 
@@ -333,7 +333,7 @@ bool block_job_is_cancelled(BlockJob *job);
  *
  * Return information about a job.
  */
-BlockJobInfo *block_job_query(BlockJob *job);
+BlockJobInfo *block_job_query(BlockJob *job, Error **errp);
 
 /**
  * block_job_pause_point:
@@ -503,5 +503,13 @@ void block_job_txn_unref(BlockJobTxn *txn);
  * to release the reference that is automatically grabbed here.
  */
 void block_job_txn_add_job(BlockJobTxn *txn, BlockJob *job);
+
+/**
+ * block_job_is_internal:
+ * @job: The job to determine if it is user-visible or not.
+ *
+ * Returns true if the job should not be visible to the management layer.
+ */
+bool block_job_is_internal(BlockJob *job);
 
 #endif
