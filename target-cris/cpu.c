ee0fc734a4ae7251b83ed3fce8e80a3cb2ee1120
Laurent Vivier <lvivier@redhat.com>
2016-10-13 18:24:49 +0200

    target-cris: move cpu_exec_realize() to realize function
    I've removed the cannot_destroy_with_object_finalize_yet field as
    unsafe references have been moved to cpu_exec_realize().
    (tested with QOM command provided by commit 4c315c27 with
     "crisv17-cris-cpu")
    
    CC: Edgar E. Iglesias <edgar.iglesias@gmail.com>
    Signed-off-by: Laurent Vivier <lvivier@redhat.com>
    Message-Id: <1476375902-11715-8-git-send-email-lvivier@redhat.com>

--- target-cris/cpu.c
+++ target-cris/cpu.c
@@ -142,6 +142,13 @@ static void cris_cpu_realizefn(DeviceState *dev, Error **errp)
 {
     CPUState *cs = CPU(dev);
     CRISCPUClass *ccc = CRIS_CPU_GET_CLASS(dev);
+    Error *local_err = NULL;
+
+    cpu_exec_realize(cs, &local_err);
+    if (local_err != NULL) {
+        error_propagate(errp, local_err);
+        return;
+    }
 
     cpu_reset(cs);
     qemu_init_vcpu(cs);
@@ -188,7 +195,6 @@ static void cris_cpu_initfn(Object *obj)
 
     cs->env_ptr = env;
     cpu_exec_init(cs);
-    cpu_exec_realize(cs, &error_abort);
 
     env->pregs[PR_VR] = ccc->vr;
 
@@ -327,13 +333,6 @@ static void cris_cpu_class_init(ObjectClass *oc, void *data)
     cc->gdb_stop_before_watchpoint = true;
 
     cc->disas_set_info = cris_disas_set_info;
-
-    /*
-     * Reason: cris_cpu_initfn() calls cpu_exec_init(), which saves
-     * the object in cpus -> dangling pointer after final
-     * object_unref().
-     */
-    dc->cannot_destroy_with_object_finalize_yet = true;
 }
 
 static const TypeInfo cris_cpu_type_info = {
