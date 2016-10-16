68134041d1f1115cf76ff787e1988a935854f943
Veronia Bahaa <veroniabahaa@gmail.com>
2016-10-16 23:03:47 +0200

    include/qemu: Add documentation to functions in include/qemu/id.h
    Add documentation to the functions id_generate and id_wellformed in include/qemu/id.h
    
    Signed-off-by: Veronia Bahaa <veroniabahaa@gmail.com>
    Message-Id: <20161016210347.GA4484@veronia-Lenovo-IdeaPad-S400>

--- include/qemu/id.h
+++ include/qemu/id.h
@@ -7,7 +7,30 @@ typedef enum IdSubSystems {
     ID_MAX      /* last element, used as array size */
 } IdSubSystems;
 
+/**
+ * id_generate: Generates an ID of the form PREFIX SUBSYSTEM NUMBER
+ *  where:
+ *
+ *  - PREFIX is the reserved character '#'
+ *  - SUBSYSTEM identifies the subsystem creating the ID
+ *  - NUMBER is a decimal number unique within SUBSYSTEM.
+ *
+ *    Example: "#block146"
+ *
+ * Returns the generated id string for the subsystem
+ *
+ * @id: the subsystem to generate an id for
+ */
 char *id_generate(IdSubSystems id);
+
+/**
+ * id_wellformed: checks that an id starts with a letter
+ *  followed by numbers, digits, '-','.', or '_'
+ *
+ * Returns %true if the id is well-formed
+ *
+ * @id: the id to be checked
+ */
 bool id_wellformed(const char *id);
 
 #endif
