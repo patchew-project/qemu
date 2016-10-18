5608c06695b83aaa389efd8771b78d0efab38e02
Peter Maydell <peter.maydell@linaro.org>
2016-10-18 16:21:39 +0300

    linux-user: Don't use alloca() for epoll_wait's epoll event array
    The epoll event array which epoll_wait() allocates has a size
    determined by the guest which could potentially be quite large.
    Use g_try_new() rather than alloca() so that we can fail more
    cleanly if the guest hands us an oversize value. (ENOMEM is
    not a documented return value for epoll_wait() but in practice
    some kernel configurations can return it -- see for instance
    sys_oabi_epoll_wait() on ARM.)
    
    This rearrangement includes fixing a bug where we were
    incorrectly passing a negative length to unlock_user() in
    the error-exit codepath.
    
    Signed-off-by: Peter Maydell <peter.maydell@linaro.org>
    Signed-off-by: Riku Voipio <riku.voipio@linaro.org>
    Message-Id: <b60ca9337f6f32a9b96bf3933d994448ff967ab3.1476796525.git.riku.voipio@linaro.org>

--- linux-user/syscall.c
+++ linux-user/syscall.c
@@ -11796,7 +11796,12 @@ abi_long do_syscall(void *cpu_env, int num, abi_long arg1,
             goto efault;
         }
 
-        ep = alloca(maxevents * sizeof(struct epoll_event));
+        ep = g_try_new(struct epoll_event, maxevents);
+        if (!ep) {
+            unlock_user(target_ep, arg2, 0);
+            ret = -TARGET_ENOMEM;
+            break;
+        }
 
         switch (num) {
 #if defined(TARGET_NR_epoll_pwait)
@@ -11814,8 +11819,8 @@ abi_long do_syscall(void *cpu_env, int num, abi_long arg1,
                 target_set = lock_user(VERIFY_READ, arg5,
                                        sizeof(target_sigset_t), 1);
                 if (!target_set) {
-                    unlock_user(target_ep, arg2, 0);
-                    goto efault;
+                    ret = -TARGET_EFAULT;
+                    break;
                 }
                 target_to_host_sigset(set, target_set);
                 unlock_user(target_set, arg5, 0);
@@ -11843,8 +11848,12 @@ abi_long do_syscall(void *cpu_env, int num, abi_long arg1,
                 target_ep[i].events = tswap32(ep[i].events);
                 target_ep[i].data.u64 = tswap64(ep[i].data.u64);
             }
+            unlock_user(target_ep, arg2,
+                        ret * sizeof(struct target_epoll_event));
+        } else {
+            unlock_user(target_ep, arg2, 0);
         }
-        unlock_user(target_ep, arg2, ret * sizeof(struct target_epoll_event));
+        g_free(ep);
         break;
     }
 #endif
