3c99838056c7ff3ee01ec890dcbb82e0734b48d6
Cao jin <caoj.fnst@cn.fujitsu.com>
2016-10-19 14:47:38 +0800

    pci: Convert msix_init() to Error and fix callers to check it
    msix_init() reports errors with error_report(), which is wrong when
    it's used in realize().  The same issue was fixed for msi_init() in
    commit 1108b2f.
    
    For some devices(like e1000e, vmxnet3) who won't fail because of
    msix_init's failure, suppress the error report by passing NULL error object.
    
    Bonus: add comment for msix_init.
    
    CC: Jiri Pirko <jiri@resnulli.us>
    CC: Gerd Hoffmann <kraxel@redhat.com>
    CC: Dmitry Fleytman <dmitry@daynix.com>
    CC: Jason Wang <jasowang@redhat.com>
    CC: Michael S. Tsirkin <mst@redhat.com>
    CC: Hannes Reinecke <hare@suse.de>
    CC: Paolo Bonzini <pbonzini@redhat.com>
    CC: Alex Williamson <alex.williamson@redhat.com>
    CC: Markus Armbruster <armbru@redhat.com>
    CC: Marcel Apfelbaum <marcel@redhat.com>
    Signed-off-by: Cao jin <caoj.fnst@cn.fujitsu.com>
    Message-Id: <1476859665-30133-4-git-send-email-caoj.fnst@cn.fujitsu.com>

--- hw/vfio/pci.c
+++ hw/vfio/pci.c
@@ -1365,6 +1365,7 @@ static void vfio_msix_early_setup(VFIOPCIDevice *vdev, Error **errp)
 static int vfio_msix_setup(VFIOPCIDevice *vdev, int pos, Error **errp)
 {
     int ret;
+    Error *err = NULL;
 
     vdev->msix->pending = g_malloc0(BITS_TO_LONGS(vdev->msix->entries) *
                                     sizeof(unsigned long));
@@ -1372,7 +1373,8 @@ static int vfio_msix_setup(VFIOPCIDevice *vdev, int pos, Error **errp)
                     vdev->bars[vdev->msix->table_bar].region.mem,
                     vdev->msix->table_bar, vdev->msix->table_offset,
                     vdev->bars[vdev->msix->pba_bar].region.mem,
-                    vdev->msix->pba_bar, vdev->msix->pba_offset, pos);
+                    vdev->msix->pba_bar, vdev->msix->pba_offset, pos,
+                    &err);
     if (ret < 0) {
         if (ret == -ENOTSUP) {
             return 0;
