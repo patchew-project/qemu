0841a9fab3911ea037fa2dc776e10da1a4fea227
Gonglei <arei.gonglei@huawei.com>
2016-10-20 19:45:52 +0800

    virtio-crypto: set capacity of algorithms supported
    Expose the capacity of algorithms supported by
    virtio crypto device to the frontend driver using
    pci configuration space.
    
    Signed-off-by: Gonglei <arei.gonglei@huawei.com>
    Message-Id: <1476963957-176296-8-git-send-email-arei.gonglei@huawei.com>

--- include/hw/virtio/virtio-crypto.h
+++ include/hw/virtio/virtio-crypto.h
@@ -39,6 +39,17 @@ do { \
 
 typedef struct VirtIOCryptoConf {
     CryptoDevBackend *cryptodev;
+
+    /* Supported service mask */
+    uint32_t crypto_services;
+
+    /* Detailed algorithms mask */
+    uint32_t cipher_algo_l;
+    uint32_t cipher_algo_h;
+    uint32_t hash_algo;
+    uint32_t mac_algo_l;
+    uint32_t mac_algo_h;
+    uint32_t aead_algo;
 } VirtIOCryptoConf;
 
 struct VirtIOCrypto;
