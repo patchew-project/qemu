21209dbd409a25877542807b474494eb6434735f
Cao jin <caoj.fnst@cn.fujitsu.com>
2016-10-26 15:18:54 +0800

    util/mmap-alloc: check parameter before using
    Also refactor some code hunk for readability
    
    Signed-off-by: Cao jin <caoj.fnst@cn.fujitsu.com>
    Message-Id: <1477466334-4078-1-git-send-email-caoj.fnst@cn.fujitsu.com>

--- util/mmap-alloc.c
+++ util/mmap-alloc.c
@@ -41,6 +41,11 @@ size_t qemu_fd_getpagesize(int fd)
 
 void *qemu_ram_mmap(int fd, size_t size, size_t align, bool shared)
 {
+    /* Make sure align is a power of 2 */
+    assert(!(align & (align - 1)));
+    /* Always align to host page size */
+    assert(align >= getpagesize());
+
     /*
      * Note: this always allocates at least one extra page of virtual address
      * space, even if size is already aligned.
@@ -68,11 +73,6 @@ void *qemu_ram_mmap(int fd, size_t size, size_t align, bool shared)
         return MAP_FAILED;
     }
 
-    /* Make sure align is a power of 2 */
-    assert(!(align & (align - 1)));
-    /* Always align to host page size */
-    assert(align >= getpagesize());
-
     ptr1 = mmap(ptr + offset, size, PROT_READ | PROT_WRITE,
                 MAP_FIXED |
                 (fd == -1 ? MAP_ANONYMOUS : 0) |
@@ -83,22 +83,20 @@ void *qemu_ram_mmap(int fd, size_t size, size_t align, bool shared)
         return MAP_FAILED;
     }
 
-    ptr += offset;
-    total -= offset;
-
     if (offset > 0) {
-        munmap(ptr - offset, offset);
+        munmap(ptr, offset);
     }
 
     /*
      * Leave a single PROT_NONE page allocated after the RAM block, to serve as
      * a guard page guarding against potential buffer overflows.
      */
+    total -= offset;
     if (total > size + getpagesize()) {
-        munmap(ptr + size + getpagesize(), total - size - getpagesize());
+        munmap(ptr1 + size + getpagesize(), total - size - getpagesize());
     }
 
-    return ptr;
+    return ptr1;
 }
 
 void qemu_ram_munmap(void *ptr, size_t size)
