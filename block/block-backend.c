92d807188fdede5eaaa84f1b087b231b5f1cd584
John Snow <jsnow@redhat.com>
2016-10-28 17:23:58 -0400

    block-backend: Always notify on blk_eject
    blk_eject is only used by scsi-disk and atapi, and in both cases we
    only attempt to invoke blk_eject if we have a bona-fide change in
    tray state.
    
    The "issue" here is that the tray state does not generate a QMP event
    unless there is a medium/BDS attached to the device, so if libvirt et al
    are waiting for a tray event to occur from an empty-but-closed drive,
    software opening that drive will not emit an event and libvirt will
    wait forever.
    
    Change this by modifying blk_eject to always emit an event, instead of
    conditionally on a "real" backend eject.
    
    Fixes: https://bugzilla.redhat.com/show_bug.cgi?id=1373264
    
    Reported-by: Peter Krempa <pkrempa@redhat.com>
    Signed-off-by: John Snow <jsnow@redhat.com>
    Message-Id: <1477689838-27053-1-git-send-email-jsnow@redhat.com>

--- block/block-backend.c
+++ block/block-backend.c
@@ -1381,15 +1381,16 @@ void blk_eject(BlockBackend *blk, bool eject_flag)
     /* blk_eject is only called by qdevified devices */
     assert(!blk->legacy_dev);
 
+    id = blk_get_attached_dev_id(blk);
     if (bs) {
         bdrv_eject(bs, eject_flag);
-
-        id = blk_get_attached_dev_id(blk);
-        qapi_event_send_device_tray_moved(blk_name(blk), id,
-                                          eject_flag, &error_abort);
-        g_free(id);
-
     }
+
+    /* Whether or not we ejected on the backend,
+     * the frontend experienced a tray event. */
+    qapi_event_send_device_tray_moved(blk_name(blk), id,
+                                      eject_flag, &error_abort);
+    g_free(id);
 }
 
 int blk_get_flags(BlockBackend *blk)
