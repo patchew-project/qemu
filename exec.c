aef35829c10fca0c388e0623b31998810092fb0f
Haozhong Zhang <haozhong.zhang@intel.com>
2016-11-01 17:29:20 +0100

    exec.c: check memory backend file size with 'size' option
    If the memory backend file is not large enough to hold the required 'size',
    Qemu will report error and exit.
    
    Signed-off-by: Haozhong Zhang <haozhong.zhang@intel.com>
    Message-Id: <20161027042300.5929-3-haozhong.zhang@intel.com>
    Signed-off-by: Paolo Bonzini <pbonzini@redhat.com>
    Message-Id: <1478017783-7703-8-git-send-email-pbonzini@redhat.com>

--- exec.c
+++ exec.c
@@ -1321,6 +1321,13 @@ static void *file_ram_alloc(RAMBlock *block,
         goto error;
     }
 
+    if (file_size > 0 && file_size < memory) {
+        error_setg(errp, "backing store %s size %"PRId64
+                   " does not match 'size' option %"PRIu64,
+                   path, file_size, memory);
+        goto error;
+    }
+
     memory = ROUND_UP(memory, block->page_size);
 
     /*
