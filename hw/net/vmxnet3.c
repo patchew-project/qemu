5b5857930bfeb6060d23bf18094ab11319189429
Cao jin <caoj.fnst@cn.fujitsu.com>
2016-11-03 12:06:35 +0800

    vmxnet3: fix reference leak issue
    On migration target, msix_vector_use() will be called in vmxnet3_post_load()
    in second time, without a matching second call to msi_vector_unuse(),
    which results in vector reference leak.
    
    CC: Dmitry Fleytman <dmitry@daynix.com>
    CC: Jason Wang <jasowang@redhat.com>
    CC: Markus Armbruster <armbru@redhat.com>
    CC: Michael S. Tsirkin <mst@redhat.com>
    
    Reviewed-by: Markus Armbruster <armbru@redhat.com>
    Reviewed-by: Dmitry Fleytman <dmitry@daynix.com>
    Signed-off-by: Cao jin <caoj.fnst@cn.fujitsu.com>
    Message-Id: <1478145997-28865-9-git-send-email-caoj.fnst@cn.fujitsu.com>

--- hw/net/vmxnet3.c
+++ hw/net/vmxnet3.c
@@ -2552,20 +2552,10 @@ static void vmxnet3_put_rxq_descr(QEMUFile *f, void *pv, size_t size)
 static int vmxnet3_post_load(void *opaque, int version_id)
 {
     VMXNET3State *s = opaque;
-    PCIDevice *d = PCI_DEVICE(s);
 
     net_tx_pkt_init(&s->tx_pkt, PCI_DEVICE(s),
                     s->max_tx_frags, s->peer_has_vhdr);
     net_rx_pkt_init(&s->rx_pkt, s->peer_has_vhdr);
-
-    if (s->msix_used) {
-        if  (!vmxnet3_use_msix_vectors(s, VMXNET3_MAX_INTRS)) {
-            VMW_WRPRN("Failed to re-use MSI-X vectors");
-            msix_uninit(d, &s->msix_bar, &s->msix_bar);
-            s->msix_used = false;
-            return -1;
-        }
-    }
 
     vmxnet3_validate_queues(s);
     vmxnet3_validate_interrupts(s);
