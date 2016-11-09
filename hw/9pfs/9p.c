df71ef941253ed5f2cec1682a44787fb0198f65d
Pradeep Jagadeesh <pradeepkiruvale@gmail.com>
2016-11-09 04:50:40 -0500

    fsdev: add IO throttle support to fsdev devices
    Uses throttling APIs to limit I/O bandwidth and number of operations on the
    devices which use 9p-local driver.
    
    Signed-off-by: Pradeep Jagadeesh <pradeep.jagadeesh@huawei.com>
    Message-Id: <1478685040-2035-1-git-send-email-pradeep.jagadeesh@huawei.com>
    Reviewed-by: Alberto Garcia <berto@igalia.com>

--- hw/9pfs/9p.c
+++ hw/9pfs/9p.c
@@ -3516,6 +3516,10 @@ int v9fs_device_realize_common(V9fsState *s, Error **errp)
         error_setg(errp, "share path %s is not a directory", fse->path);
         goto out;
     }
+
+    s->ctx.fst = &fse->fst;
+    fsdev_throttle_init(s->ctx.fst);
+
     v9fs_path_free(&path);
 
     rc = 0;
@@ -3530,6 +3534,7 @@ out:
 
 void v9fs_device_unrealize_common(V9fsState *s, Error **errp)
 {
+    fsdev_throttle_cleanup(s->ctx.fst);
     g_free(s->ctx.fs_root);
     g_free(s->tag);
 }
