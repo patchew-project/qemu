c24c35c0e2b36d3b228cf631c23df3b240d982ff
Vladimir Sementsov-Ogievskiy <vsementsov@virtuozzo.com>
2016-11-09 21:17:47 +0300

    block/dirty-bitmap: add bdrv_dirty_bitmap_next()
    Signed-off-by: Vladimir Sementsov-Ogievskiy <vsementsov@virtuozzo.com>
    Reviewed-by: Max Reitz <mreitz@redhat.com>
    Message-Id: <1478715476-132280-13-git-send-email-vsementsov@virtuozzo.com>

--- block/dirty-bitmap.c
+++ block/dirty-bitmap.c
@@ -582,3 +582,10 @@ bool bdrv_has_persistent_bitmaps(BlockDriverState *bs)
 
     return false;
 }
+
+BdrvDirtyBitmap *bdrv_dirty_bitmap_next(BlockDriverState *bs,
+                                        BdrvDirtyBitmap *bitmap)
+{
+    return bitmap == NULL ? QLIST_FIRST(&bs->dirty_bitmaps) :
+                            QLIST_NEXT(bitmap, list);
+}
