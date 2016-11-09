c7e69fe9035ab5e9eab4572736abfdf07c89d1f5
Stefan Hajnoczi <stefanha@redhat.com>
2016-11-09 17:13:21 +0000

    virtio: poll virtqueues for new buffers
    Add an AioContext poll handler to detect new virtqueue buffers without
    waiting for a guest->host notification.
    
    Signed-off-by: Stefan Hajnoczi <stefanha@redhat.com>
    Message-Id: <1478711602-12620-3-git-send-email-stefanha@redhat.com>

--- hw/virtio/virtio.c
+++ hw/virtio/virtio.c
@@ -2026,15 +2026,34 @@ static void virtio_queue_host_notifier_aio_read(EventNotifier *n)
     }
 }
 
+static bool virtio_queue_aio_poll(void *opaque)
+{
+    VirtQueue *vq = opaque;
+
+    return !virtio_queue_empty(vq);
+}
+
+static void virtio_queue_aio_poll_handler(void *opaque)
+{
+    VirtQueue *vq = opaque;
+
+    virtio_queue_notify_aio_vq(vq);
+}
+
 void virtio_queue_aio_set_host_notifier_handler(VirtQueue *vq, AioContext *ctx,
                                                 VirtIOHandleOutput handle_output)
 {
     if (handle_output) {
         vq->handle_aio_output = handle_output;
+        aio_set_poll_handler(ctx,
+                             virtio_queue_aio_poll,
+                             virtio_queue_aio_poll_handler,
+                             vq);
         aio_set_event_notifier(ctx, &vq->host_notifier, true,
                                virtio_queue_host_notifier_aio_read);
     } else {
         aio_set_event_notifier(ctx, &vq->host_notifier, true, NULL);
+        aio_set_poll_handler(ctx, virtio_queue_aio_poll, NULL, vq);
         /* Test and clear notifier before after disabling event,
          * in case poll callback didn't have time to run. */
         virtio_queue_host_notifier_aio_read(&vq->host_notifier);
