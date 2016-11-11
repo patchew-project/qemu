edf12f7e4c721aa148242ec0b7c51afb14fe8cd8
Vincent Palatin <vpalatin@chromium.org>
2016-11-11 12:28:32 +0100

    target-i386: Add Intel HAX files
    That's a forward port of the core HAX interface code mostly unmodified from
    emu-2.2-release branch in the external/qemu-android repository as used by
    the Android emulator.
    
    The original commit was "target-i386: Add Intel HAX to android emulator"
    saying:
    """
      Backport of 2b3098ff27bab079caab9b46b58546b5036f5c0c
      from studio-1.4-dev into emu-master-dev
    
        Intel HAX (harware acceleration) will enhance android emulator performance
        in Windows and Mac OS X in the systems powered by Intel processors with
        "Intel Hardware Accelerated Execution Manager" package installed when
        user runs android emulator with Intel target.
    
        Signed-off-by: David Chou <david.j.chou@intel.com>
    """
    
    It has minor modifications to build and run along with the current
    code base.
    The formatting has been fixed to go through scripts/checkpatch.pl.
    Update the DPRINTF macros to get the instanciations checked by the
    compiler.
    Does not include the Darwin support.
    
    Signed-off-by: Vincent Palatin <vpalatin@chromium.org>
    Message-Id: <08625798334d3ea3ccead1b40d1068982b40f1d1.1478863621.git.vpalatin@chromium.org>

---
+++ hax-stub.c
@@ -0,0 +1,74 @@
+/*
+ * QEMU HAXM support
+ *
+ * Copyright (c) 2015, Intel Corporation
+ *
+ * Copyright 2016 Google, Inc.
+ *
+ * This software is licensed under the terms of the GNU General Public
+ * License version 2, as published by the Free Software Foundation, and
+ * may be copied, distributed, and modified under those terms.
+ *
+ * See the COPYING file in the top-level directory.
+ *
+ */
+
+#include "qemu/osdep.h"
+#include "qemu-common.h"
+#include "cpu.h"
+#include "sysemu/hax.h"
+
+int hax_sync_vcpus(void)
+{
+    return 0;
+}
+
+void hax_disable(int disable)
+{
+   return;
+}
+
+int hax_pre_init(uint64_t ram_size)
+{
+   return 0;
+}
+
+int hax_get_max_ram(uint64_t *max_ram)
+{
+    return 0;
+}
+
+int hax_populate_ram(uint64_t va, uint32_t size)
+{
+    return -ENOSYS;
+}
+
+int hax_init_vcpu(CPUState *cpu)
+{
+    return -ENOSYS;
+}
+
+int hax_smp_cpu_exec(CPUState *cpu)
+{
+    return -ENOSYS;
+}
+
+int hax_vcpu_exec(CPUState *cpu)
+{
+    return -ENOSYS;
+}
+
+int hax_vcpu_emulation_mode(CPUState *cpu)
+{
+    return 0;
+}
+
+int hax_stop_emulation(CPUState *cpu)
+{
+    return 0;
+}
+
+int hax_stop_translate(CPUState *cpu)
+{
+    return 0;
+}
