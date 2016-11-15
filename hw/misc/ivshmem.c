04745bd6f00e200d7bb8385c71397e928e8915b0
ZhuangYanying <ann.zhuangyanying@huawei.com>
2016-11-15 18:45:19 +0800

    ivshmem: set not_legacy_32bit to 1 for ivshmem_doorbell and ivshmem-plain
    Signed-off-by: Zhuang Yanying <ann.zhuangyanying@huawei.com>
    Message-Id: <1479206719-14780-3-git-send-email-ann.zhuangyanying@huawei.com>
    Reviewed-by: Gonglei <arei.gonglei@huawei.com>
    Reviewed-by: Marc-André Lureau <marcandre.lureau@redhat.com>

--- hw/misc/ivshmem.c
+++ hw/misc/ivshmem.c
@@ -1045,6 +1045,7 @@ static void ivshmem_plain_init(Object *obj)
                              ivshmem_check_memdev_is_busy,
                              OBJ_PROP_LINK_UNREF_ON_RELEASE,
                              &error_abort);
+    s->not_legacy_32bit = 1;
 }
 
 static void ivshmem_plain_realize(PCIDevice *dev, Error **errp)
@@ -1116,6 +1117,7 @@ static void ivshmem_doorbell_init(Object *obj)
 
     s->features |= (1 << IVSHMEM_MSI);
     s->legacy_size = SIZE_MAX;  /* whatever the server sends */
+    s->not_legacy_32bit = 1;
 }
 
 static void ivshmem_doorbell_realize(PCIDevice *dev, Error **errp)
