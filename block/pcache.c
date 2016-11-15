8d1e3b6e96a267f27bb81a561087f5c12205e7c2
Pavel Butsykin <pbutsykin@virtuozzo.com>
2016-11-15 09:37:05 +0300

    block/pcache: updating statistics for overlapping requests
    When updating the statistics sometimes i/O requests can overlap each other,
    in this case the requests are not stored in the statistics. It's not very good,
    especially when the requests have a small range of intersection.
    
    We can cut the requests in the intersection and add the pieces of requests in
    the statistics. Maybe not significantly, but it can make the statistics more
    accurate. Here, update_req_stats() adds the ability to save overlapping
    requests.
    
    Signed-off-by: Pavel Butsykin <pbutsykin@virtuozzo.com>
    Message-Id: <20161115063715.12561-9-pbutsykin@virtuozzo.com>

--- block/pcache.c
+++ block/pcache.c
@@ -64,6 +64,40 @@ static void pcache_aio_cb(void *opaque, int ret)
     qemu_coroutine_enter(acb->co);
 }
 
+static void update_req_stats(RBCache *rbcache, uint64_t offset, uint64_t bytes)
+{
+    do {
+        RBCacheNode *node = rbcache_search_and_insert(rbcache, offset, bytes);
+        /* The node was successfully added or already exists */
+        if (node->offset <= offset &&
+            node->offset + node->bytes >= offset + bytes)
+        {
+            break;
+        }
+
+        /* Request covers the whole node */
+        if (offset <= node->offset &&
+            offset + bytes >= node->offset + node->bytes)
+        {
+            rbcache_remove(rbcache, node);
+            continue;
+        }
+
+        if (offset < node->offset) {
+            RBCacheNode *new_node =
+                rbcache_node_alloc(rbcache, offset, node->offset - offset);
+            if (new_node != rbcache_insert(rbcache, new_node)) {
+                /* The presence of the node in this range is impossible */
+                abort();
+            }
+            break;
+        }
+
+        bytes = (offset + bytes) - (node->offset + node->bytes);
+        offset = node->offset + node->bytes;
+    } while (true);
+}
+
 static coroutine_fn int pcache_co_preadv(BlockDriverState *bs, uint64_t offset,
                                          uint64_t bytes, QEMUIOVector *qiov,
                                          int flags)
@@ -74,7 +108,7 @@ static coroutine_fn int pcache_co_preadv(BlockDriverState *bs, uint64_t offset,
     };
 
     if (s->max_aio_size >= bytes) {
-        rbcache_search_and_insert(s->req_stats, offset, bytes);
+        update_req_stats(s->req_stats, offset, bytes);
     }
 
     bdrv_aio_preadv(bs->file, offset, qiov, bytes, pcache_aio_cb, &acb);
