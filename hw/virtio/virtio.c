bb36b63621f2c3480de854ccd801b0b322606d55
Stefan Hajnoczi <stefanha@redhat.com>
2016-11-22 16:31:37 +0000

    virtio: add missing vdev->broken check
    virtio_queue_notify_vq() checks vdev->broken before invoking the
    handler, virtio_queue_notify_aio_vq() should too.
    
    Signed-off-by: Stefan Hajnoczi <stefanha@redhat.com>
    Message-Id: <1479832306-26440-2-git-send-email-stefanha@redhat.com>

--- hw/virtio/virtio.c
+++ hw/virtio/virtio.c
@@ -1239,6 +1239,10 @@ static void virtio_queue_notify_aio_vq(VirtQueue *vq)
     if (vq->vring.desc && vq->handle_aio_output) {
         VirtIODevice *vdev = vq->vdev;
 
+        if (unlikely(vq->vdev->broken)) {
+            return;
+        }
+
         trace_virtio_queue_notify(vdev, vq - vdev->vq, vq);
         vq->handle_aio_output(vdev, vq);
     }
