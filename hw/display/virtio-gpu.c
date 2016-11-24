77e1f32a14c8c2e26b6c0caef7e08abf8771b3ac
Li Qiang <liq3ea@gmail.com>
2016-11-24 07:30:09 -0500

    virtio-gpu: call cleanup mapping function in resource destroy
    If the guest destroy the resource before detach banking, the 'iov'
    and 'addrs' field in resource is not freed thus leading memory
    leak issue. This patch avoid this.
    
    Signed-off-by: Li Qiang <liq3ea@gmail.com>
    Message-Id: <1479990609-28243-3-git-send-email-liq3ea@gmail.com>
    Reviewed-by: Marc-André Lureau <marcandre.lureau@redhat.com>

--- hw/display/virtio-gpu.c
+++ hw/display/virtio-gpu.c
@@ -360,6 +360,7 @@ static void virtio_gpu_resource_destroy(VirtIOGPU *g,
                                         struct virtio_gpu_simple_resource *res)
 {
     pixman_image_unref(res->image);
+    virtio_gpu_cleanup_mapping(res);
     QTAILQ_REMOVE(&g->reslist, res, next);
     g_free(res);
 }
