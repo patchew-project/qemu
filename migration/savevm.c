3bc12b5f9f9b05fea8d7e0288ef79fa4bfc39201
Zhang Chen <zhangchen.fnst@cn.fujitsu.com>
2016-12-01 14:06:37 +0800

    Migration: Don't load vmdesc when xen is enabled
    Xen need't this.
    
    Signed-off-by: Zhang Chen <zhangchen.fnst@cn.fujitsu.com>
    Signed-off-by: Wen Congyang <wency@cn.fujitsu.com>
    Message-Id: <1480572399-10455-2-git-send-email-zhangchen.fnst@cn.fujitsu.com>

--- migration/savevm.c
+++ migration/savevm.c
@@ -1973,6 +1973,10 @@ int qemu_loadvm_state(QEMUFile *f)
         ret = qemu_file_get_error(f);
     }
 
+    if (xen_enabled()) {
+        return ret;
+    }
+
     /*
      * Try to read in the VMDESC section as well, so that dumping tools that
      * intercept our migration stream have the chance to see it.
