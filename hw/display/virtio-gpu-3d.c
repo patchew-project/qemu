929140ddab2f5213fb648530e2156b06ba38ecae
Prasad J Pandit <pjp@fedoraproject.org>
2016-12-13 12:44:39 +0530

    display: virtio-gpu-3d: check virgl capabilities max_size
    Virtio GPU device while processing 'VIRTIO_GPU_CMD_GET_CAPSET'
    command, retrieves the maximum capabilities size to fill in the
    response object. It continues to fill in capabilities even if
    retrieved 'max_size' is zero(0), thus resulting in OOB access.
    Add check to avoid it.
    
    Reported-by: Zhenhao Hong <zhenhaohong@gmail.com>
    Signed-off-by: Prasad J Pandit <pjp@fedoraproject.org>
    Message-Id: <20161213071439.32322-1-ppandit@redhat.com>

--- hw/display/virtio-gpu-3d.c
+++ hw/display/virtio-gpu-3d.c
@@ -372,11 +372,12 @@ static void virgl_cmd_get_capset(VirtIOGPU *g,
     virgl_renderer_get_cap_set(gc.capset_id, &max_ver,
                                &max_size);
     resp = g_malloc(sizeof(*resp) + max_size);
-
-    resp->hdr.type = VIRTIO_GPU_RESP_OK_CAPSET;
-    virgl_renderer_fill_caps(gc.capset_id,
-                             gc.capset_version,
-                             (void *)resp->capset_data);
+    if (max_size) {
+        resp->hdr.type = VIRTIO_GPU_RESP_OK_CAPSET;
+        virgl_renderer_fill_caps(gc.capset_id,
+                                 gc.capset_version,
+                                 (void *)resp->capset_data);
+    }
     virtio_gpu_ctrl_response(g, cmd, &resp->hdr, sizeof(*resp) + max_size);
     g_free(resp);
 }
