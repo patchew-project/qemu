8b569995a28c46b3afd3a218cb0a75030243b96f
Wei Wang <wei.w.wang@intel.com>
2016-12-19 13:58:49 +0800

    vhost-pci-slave/msg: VHOST_USER_SET_OWNER
    Do nothing currently.
    
    Signed-off-by: Wei Wang <wei.w.wang@intel.com>
    Message-Id: <1482127152-84732-15-git-send-email-wei.w.wang@intel.com>

--- hw/virtio/vhost-pci-slave.c
+++ hw/virtio/vhost-pci-slave.c
@@ -169,6 +169,8 @@ static void vp_slave_read(void *opaque, const uint8_t *buf, int size)
             goto err_handling;
         }
         break;
+    case VHOST_USER_SET_OWNER:
+        break;
     default:
         error_report("vhost-pci-slave does not support msg request = %d",
                      msg.request);
