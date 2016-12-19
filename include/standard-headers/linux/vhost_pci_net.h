183ce9e2942b6d34c9128506ee73454df93fe819
Wei Wang <wei.w.wang@intel.com>
2016-12-19 13:58:36 +0800

    vhost-pci-net: the fundamental vhost-pci-net device emulation
    This patch introduces the fundamental parts of the device emulation.
    
    Signed-off-by: Wei Wang <wei.w.wang@intel.com>
    Message-Id: <1482127152-84732-2-git-send-email-wei.w.wang@intel.com>

--- include/standard-headers/linux/vhost_pci_net.h
+++ include/standard-headers/linux/vhost_pci_net.h
@@ -1,9 +1,7 @@
-#ifndef _LINUX_VIRTIO_IDS_H
-#define _LINUX_VIRTIO_IDS_H
-/*
- * Virtio IDs
- *
- * This header is BSD licensed so anyone can use the definitions to implement
+#ifndef _LINUX_VHOST_PCI_NET_H
+#define _LINUX_VHOST_PCI_NET_H
+
+/* This header is BSD licensed so anyone can use the definitions to implement
  * compatible drivers/servers.
  *
  * Redistribution and use in source and binary forms, with or without
@@ -14,13 +12,13 @@
  * 2. Redistributions in binary form must reproduce the above copyright
  *    notice, this list of conditions and the following disclaimer in the
  *    documentation and/or other materials provided with the distribution.
- * 3. Neither the name of IBM nor the names of its contributors
+ * 3. Neither the name of Intel nor the names of its contributors
  *    may be used to endorse or promote products derived from this software
  *    without specific prior written permission.
  * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS ``AS IS'' AND
  * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
  * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
- * ARE DISCLAIMED.  IN NO EVENT SHALL IBM OR CONTRIBUTORS BE LIABLE
+ * ARE DISCLAIMED.  IN NO EVENT SHALL Intel OR CONTRIBUTORS BE LIABLE
  * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
  * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS
  * OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)
@@ -29,19 +27,17 @@
  * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
  * SUCH DAMAGE. */
 
-#define VIRTIO_ID_NET		 1 /* virtio net */
-#define VIRTIO_ID_BLOCK		 2 /* virtio block */
-#define VIRTIO_ID_CONSOLE	 3 /* virtio console */
-#define VIRTIO_ID_RNG		 4 /* virtio rng */
-#define VIRTIO_ID_BALLOON	 5 /* virtio balloon */
-#define VIRTIO_ID_RPMSG		 7 /* virtio remote processor messaging */
-#define VIRTIO_ID_SCSI		 8 /* virtio scsi */
-#define VIRTIO_ID_9P		 9 /* 9p virtio console */
-#define VIRTIO_ID_RPROC_SERIAL  11 /* virtio remoteproc serial link */
-#define VIRTIO_ID_CAIF	        12 /* Virtio caif */
-#define VIRTIO_ID_GPU           16 /* virtio GPU */
-#define VIRTIO_ID_INPUT         18 /* virtio input */
-#define VIRTIO_ID_VSOCK         19 /* virtio vsock transport */
-#define VIRTIO_ID_CRYPTO        20 /* virtio crypto */
-#define VIRTIO_ID_VHOST_PCI_NET 21 /* vhost-pci-net */
-#endif /* _LINUX_VIRTIO_IDS_H */
+#include "standard-headers/linux/virtio_ids.h"
+
+#define VPNET_S_LINK_UP	1	/* Link is up */
+
+struct vhost_pci_net_config {
+	/*
+	 * Legal values are between 1 and 0x8000
+	 */
+	uint16_t peer_vq_num;
+	/* See VPNET_S_* above */
+	uint16_t status;
+} QEMU_PACKED;
+
+#endif
