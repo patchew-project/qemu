3e14cac8d8242ab4dbe76e7ca6611e64dcd091c5
Wei Wang <wei.w.wang@intel.com>
2016-12-19 13:59:07 +0800

    vhost-user: add asynchronous read for the vhost-user master
    Enable the vhost-user master to asynchronously receive messages
    from the slave.
    
    Signed-off-by: Wei Wang <wei.w.wang@intel.com>
    Message-Id: <1482127152-84732-33-git-send-email-wei.w.wang@intel.com>

--- include/net/vhost-user.h
+++ include/net/vhost-user.h
@@ -11,8 +11,12 @@
 #ifndef NET_VHOST_USER_H
 #define NET_VHOST_USER_H
 
+#include "sysemu/char.h"
+
 struct vhost_net;
 struct vhost_net *vhost_user_get_vhost_net(NetClientState *nc);
 uint64_t vhost_user_get_acked_features(NetClientState *nc);
+
+CharBackend *net_name_to_chr_be(const char *name);
 
 #endif /* VHOST_USER_H */
