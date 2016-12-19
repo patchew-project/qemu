dbe50298514de170d9ba058e344f2a06e2ea28b9
Ben Warren <ben@skyportsystems.com>
2016-12-19 14:46:31 -0800

    ACPI: Add a function for building named qword entries
    Signed-off-by: Ben Warren <ben@skyportsystems.com>
    Message-Id: <8f8971ae6feb51f445e50f65ad70d276b1ac231e.1482187052.git.ben@skyportsystems.com>

--- include/hw/acpi/aml-build.h
+++ include/hw/acpi/aml-build.h
@@ -385,6 +385,10 @@ int
 build_append_named_dword(GArray *array, const char *name_format, ...)
 GCC_FMT_ATTR(2, 3);
 
+int
+build_append_named_qword(GArray *array, const char *name_format, ...)
+GCC_FMT_ATTR(2, 3);
+
 void build_srat_memory(AcpiSratMemoryAffinity *numamem, uint64_t base,
                        uint64_t len, int node, MemoryAffinityFlags flags);
 
