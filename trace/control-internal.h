31ef87fc130880f56bbc7317ee79fcc369798bea
Lluís Vilanova <vilanova@ac.upc.edu>
2016-12-28 15:07:53 +0100

    trace: Make trace_get_vcpu_event_count() inlinable
    Later patches will make use of it.
    
    Signed-off-by: Lluís Vilanova <vilanova@ac.upc.edu>
    Message-Id: <148293407313.28901.16350296471570196043.stgit@fimbulvetr.bsc.es>

--- trace/control-internal.h
+++ trace/control-internal.h
@@ -16,6 +16,7 @@
 
 
 extern int trace_events_enabled_count;
+extern uint32_t trace_next_vcpu_id;
 
 
 static inline bool trace_event_is_pattern(const char *str)
@@ -82,6 +83,10 @@ static inline bool trace_event_get_vcpu_state_dynamic(CPUState *vcpu,
     return trace_event_get_vcpu_state_dynamic_by_vcpu_id(vcpu, vcpu_id);
 }
 
+static inline uint32_t trace_get_vcpu_event_count(void)
+{
+    return trace_next_vcpu_id;
+}
 
 void trace_event_register_group(TraceEvent **events);
 
