3cf503f8ed0c81eb28e72bb0fef523db162bc67e
Longpeng(Mike) <longpeng2@huawei.com>
2017-01-05 08:49:37 +0800

    crypto: add AEAD algorithms framework
    This patch introduce AEAD algorithms framework.
    
    We currently plan to support six basic AEAD algorithms,
    ccm(aes128/192/256) and gcm(aes128/192/256), so we need
    to add ccm/gcm mode in qapi/crypto.json simultaneously.
    
    Signed-off-by: Longpeng(Mike) <longpeng2@huawei.com>
    Message-Id: <1483577381-38088-3-git-send-email-longpeng2@huawei.com>

---
+++ crypto/aead.c
@@ -0,0 +1,84 @@
+/*
+ * QEMU Crypto aead algorithms
+ *
+ * Copyright (c) 2016 HUAWEI TECHNOLOGIES CO., LTD.
+ *
+ * Authors:
+ *    Longpeng(Mike) <longpeng2@huawei.com>
+ *
+ * This work is licensed under the terms of the GNU GPL, version 2 or
+ * (at your option) any later version.  See the COPYING file in the
+ * top-level directory.
+ *
+ */
+
+#include "qemu/osdep.h"
+#include "qapi/error.h"
+#include "crypto/aead.h"
+
+bool qcrypto_aead_supports(QCryptoCipherAlgorithm alg,
+                           QCryptoCipherMode mode)
+{
+    return false;
+}
+
+size_t qcrypto_aead_get_key_len(QCryptoCipherAlgorithm alg)
+{
+    return -1;
+}
+
+#if !defined(CONFIG_NETTLE_AEAD) && !defined(CONFIG_GCRYPT_AEAD)
+
+QCryptoAead *qcrypto_aead_new(QCryptoCipherAlgorithm alg,
+                              QCryptoCipherMode mode,
+                              const uint8_t *key, size_t nkey,
+                              Error **errp)
+{
+    return NULL;
+}
+
+void qcrypto_aead_free(QCryptoAead *aead)
+{
+    return;
+}
+
+int qcrypto_aead_set_nonce(QCryptoAead *aead,
+                           const uint8_t *nonce, size_t nonce_len,
+                           size_t aad_len, size_t in_len,
+                           size_t tag_len,
+                           Error **errp)
+{
+    return -1;
+}
+
+int qcrypto_aead_authenticate(QCryptoAead *aead,
+                              const uint8_t *aad, size_t aad_len,
+                              Error **errp)
+{
+    return -1;
+}
+
+int qcrypto_aead_encrypt(QCryptoAead *aead,
+                         const uint8_t *in, size_t in_len,
+                         uint8_t *out, size_t out_len,
+                         Error **errp)
+{
+    return -1;
+}
+
+int qcrypto_aead_decrypt(QCryptoAead *aead,
+                         const uint8_t *in, size_t in_len,
+                         uint8_t *out, size_t out_len,
+                         Error **errp)
+{
+    return -1;
+}
+
+int qcrypto_aead_get_tag(QCryptoAead *aead,
+                         uint8_t *tag, size_t tag_len,
+                         Error **errp)
+{
+    return -1;
+}
+
+#endif
