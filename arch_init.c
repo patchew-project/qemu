50f45565ced616c7f453d86351ba3bbe7da9b21e
Eduardo Habkost <ehabkost@redhat.com>
2017-01-17 17:30:31 -0200

    stubs: Add smbios_entry_add() stub
    Instead of using an #ifdef in arch_init.c, add a
    smbios_entry_add() stub. The stub will never be called because
    the -smbios option is defined as arch-specific in
    qemu-options.hx.
    
    Signed-off-by: Eduardo Habkost <ehabkost@redhat.com>
    Message-Id: <20170117193033.23178-2-ehabkost@redhat.com>

--- arch_init.c
+++ arch_init.c
@@ -248,13 +248,6 @@ void do_acpitable_option(const QemuOpts *opts)
 #endif
 }
 
-void do_smbios_option(QemuOpts *opts)
-{
-#ifdef TARGET_I386
-    smbios_entry_add(opts);
-#endif
-}
-
 int kvm_available(void)
 {
 #ifdef CONFIG_KVM
---
+++ stubs/smbios.c
@@ -0,0 +1,7 @@
+#include "qemu/osdep.h"
+#include "hw/smbios/smbios.h"
+
+void smbios_entry_add(QemuOpts *opts)
+{
+    abort(); /* must never be called */
+}
