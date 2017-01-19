b3b1d1832efc188f552c2846d081a27b1d7afd0e
Alex Bennée <alex.bennee@linaro.org>
2017-01-19 17:05:01 +0000

    target-arm: helpers which may affect global state need the BQL
    As the arm_call_el_change_hook may affect global state (for example with
    updating the global GIC state) we need to assert/take the BQL.
    
    Signed-off-by: Alex Bennée <alex.bennee@linaro.org>
    Reviewed-by: Richard Henderson <rth@twiddle.net>
    Message-Id: <20170119170507.16185-22-alex.bennee@linaro.org>

--- target/arm/op_helper.c
+++ target/arm/op_helper.c
@@ -488,7 +488,9 @@ void HELPER(cpsr_write_eret)(CPUARMState *env, uint32_t val)
      */
     env->regs[15] &= (env->thumb ? ~1 : ~3);
 
+    qemu_mutex_lock_iothread();
     arm_call_el_change_hook(arm_env_get_cpu(env));
+    qemu_mutex_unlock_iothread();
 }
 
 /* Access to user mode registers from privileged modes.  */
@@ -1020,7 +1022,9 @@ void HELPER(exception_return)(CPUARMState *env)
                       cur_el, new_el, env->pc);
     }
 
+    qemu_mutex_lock_iothread();
     arm_call_el_change_hook(arm_env_get_cpu(env));
+    qemu_mutex_unlock_iothread();
 
     return;
 
