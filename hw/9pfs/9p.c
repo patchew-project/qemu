c7e03e8db13f016c3410c61b0443da86c15eb01e
Greg Kurz <groug@kaod.org>
2017-01-21 15:27:57 +0100

    9pfs: fix offset error in v9fs_xattr_read()
    The current code tries to copy `read_count' bytes starting at offset
    `offset' from a `read_count`-sized iovec. This causes v9fs_pack() to
    fail with ENOBUFS.
    
    Since the PDU iovec is already partially filled with `offset' bytes,
    let's skip them when creating `qiov_full' and have v9fs_pack() to
    copy the whole of it. Moreover, this is consistent with the other
    places where v9fs_init_qiov_from_pdu() is called.
    
    This fixes commit "bcb8998fac16 9pfs: call v9fs_init_qiov_from_pdu
    before v9fs_pack".
    
    Signed-off-by: Greg Kurz <groug@kaod.org>
    Message-Id: <148500887769.19567.3784247823786701992.stgit@bahia.lan>

--- hw/9pfs/9p.c
+++ hw/9pfs/9p.c
@@ -1685,8 +1685,8 @@ static int v9fs_xattr_read(V9fsState *s, V9fsPDU *pdu, V9fsFidState *fidp,
     }
     offset += err;
 
-    v9fs_init_qiov_from_pdu(&qiov_full, pdu, 0, read_count, false);
-    err = v9fs_pack(qiov_full.iov, qiov_full.niov, offset,
+    v9fs_init_qiov_from_pdu(&qiov_full, pdu, offset, read_count, false);
+    err = v9fs_pack(qiov_full.iov, qiov_full.niov, 0,
                     ((char *)fidp->fs.xattr.value) + off,
                     read_count);
     qemu_iovec_destroy(&qiov_full);
