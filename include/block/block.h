c2bc546ae6e994b480bad89245e91ad234d2cf0e
Fam Zheng <famz@redhat.com>
2017-01-23 20:30:42 +0800

    block: Define BDRV_O_SHARE_RW
    Signed-off-by: Fam Zheng <famz@redhat.com>
    Message-Id: <20170123123056.30383-3-famz@redhat.com>
    Reviewed-by: Max Reitz <mreitz@redhat.com>

--- include/block/block.h
+++ include/block/block.h
@@ -97,6 +97,8 @@ typedef struct HDGeometry {
                                       select an appropriate protocol driver,
                                       ignoring the format layer */
 #define BDRV_O_NO_IO       0x10000 /* don't initialize for I/O */
+#define BDRV_O_SHARE_RW    0x20000 /* accept shared read-write from other users
+                                      of the image */
 
 #define BDRV_O_CACHE_MASK  (BDRV_O_NOCACHE | BDRV_O_NO_FLUSH)
 
