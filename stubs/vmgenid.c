956cb8a4627bda3f48332a62ff4db513d924f4b7
Igor Mammedov <imammedo@redhat.com>
2017-01-22 23:32:51 -0800

    qmp/hmp: add set-vm-generation-id commands
    Add set-vm-generation-id command to set Virtual Machine
    Generation ID counter.
    
    QMP command example:
        { "execute": "set-vm-generation-id",
              "arguments": {
                  "guid": "324e6eaf-d1d1-4bf6-bf41-b9bb6c91fb87"
              }
        }
    
    HMP command example:
        set-vm-generation-id guid=324e6eaf-d1d1-4bf6-bf41-b9bb6c91fb87
    
    Signed-off-by: Igor Mammedov <imammedo@redhat.com>
    Reviewed-by: Eric Blake <eblake@redhat.com>
    Signed-off-by: Ben Warren <ben@skyportsystems.com>
    Message-Id: <d696071bf823396b55c43418cc38c160bf47385d.1485155611.git.ben@skyportsystems.com>

--- stubs/vmgenid.c
+++ stubs/vmgenid.c
@@ -6,3 +6,9 @@ GuidInfo *qmp_query_vm_generation_id(Error **errp)
     error_setg(errp, "this command is not currently supported");
     return NULL;
 }
+
+void qmp_set_vm_generation_id(const char *guid, Error **errp)
+{
+    error_setg(errp, "this command is not currently supported");
+    return;
+}
