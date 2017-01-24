080333c9b56994676869100338fe6908346b2eb1
Li Qiang <liqiang6-s@360.cn>
2017-01-24 01:34:07 -0800

    cirrus: fix oob access issue
    When doing bitblt copy in backward mode, minus the blt width first
    to avoid an oob access issue.
    
    Signed-off-by: Li Qiang <liqiang6-s@360.cn>
    Message-Id: <58871f9b.d635240a.4cda6.5322@mx.google.com>

--- hw/display/cirrus_vga.c
+++ hw/display/cirrus_vga.c
@@ -277,7 +277,8 @@ static bool blit_region_is_unsafe(struct CirrusVGAState *s,
     }
     if (pitch < 0) {
         int64_t min = addr
-            + ((int64_t)s->cirrus_blt_height-1) * pitch;
+            + ((int64_t)s->cirrus_blt_height-1) * pitch
+            - s->cirrus_blt_width;
         int32_t max = addr
             + s->cirrus_blt_width;
         if (min < 0 || max > s->vga.vram_size) {
