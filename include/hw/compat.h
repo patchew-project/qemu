c0c230d0098e293c9e300a984066cb8463e80969
Marcel Apfelbaum <marcel@redhat.com>
2017-01-25 10:44:46 +0200

    hw/pci: disable pci-bridge's shpc by default
    The shpc component is optional while  ACPI hotplug is used
    for hot-plugging PCI devices into a PCI-PCI bridge.
    Disabling the shpc by default will make slot 0 usable at boot time
    and not only for hot-plug, without loosing any functionality.
    Older machines will have shpc enabled for compatibility reasons.
    
    Signed-off-by: Marcel Apfelbaum <marcel@redhat.com>
    Message-Id: <1485333886-6111-1-git-send-email-marcel@redhat.com>

--- include/hw/compat.h
+++ include/hw/compat.h
@@ -10,6 +10,10 @@
         .driver   = "fw_cfg_io",\
         .property = "x-file-slots",\
         .value    = stringify(0x10),\
+    },{\
+        .driver   = "pci-bridge",\
+        .property = "shpc",\
+        .value    = "on",\
     },
 
 #define HW_COMPAT_2_7 \
