5ca29725b812b66d6da9d028f721c5cd117e9de4
Thomas Huth <thuth@redhat.com>
2017-01-27 15:51:28 +0100

    hw/core/or-irq: Mark the device with cannot_instantiate_with_device_add_yet
    The "or-irq" device is just used internally. It's strange to
    see this device showing up in the "-device ?" help text. Let's mark it
    with cannot_instantiate_with_device_add_yet to hide it from the users.
    
    Signed-off-by: Thomas Huth <thuth@redhat.com>
    Message-Id: <1485528688-29914-1-git-send-email-thuth@redhat.com>

--- hw/core/or-irq.c
+++ hw/core/or-irq.c
@@ -89,6 +89,9 @@ static void or_irq_class_init(ObjectClass *klass, void *data)
     dc->props = or_irq_properties;
     dc->realize = or_irq_realize;
     dc->vmsd = &vmstate_or_irq;
+
+    /* It's just used internally, and should not be exposed to users */
+    dc->cannot_instantiate_with_device_add_yet = true;
 }
 
 static const TypeInfo or_irq_type_info = {
