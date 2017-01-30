ed1f7791f2a500258004b7f46bf946e652967672
Greg Kurz <groug@kaod.org>
2017-01-30 13:10:44 +0100

    9pfs: local: post link operation for mapped-file security
    The link operation is really the same for the passthrough and mapped
    security models. This patch simply moves the mapped-file bits to a
    separate function. This will make future modifications easier.
    
    This doesn't fix any bug, it is just preparatory cleanup.
    
    Signed-off-by: Greg Kurz <groug@kaod.org>
    Message-Id: <148577824407.10533.8056765551994230024.stgit@bahia.lan>

--- hw/9pfs/9p-local.c
+++ hw/9pfs/9p-local.c
@@ -1108,6 +1108,35 @@ static int local_symlink(FsContext *fs_ctx, const char *oldpath,
     g_assert_not_reached();
 }
 
+static int local_post_link_mapped_file(FsContext *ctx, V9fsPath *oldpath,
+                                       V9fsPath *dirpath, const char *name)
+{
+    int ret;
+    V9fsString newpath;
+    char *buffer, *buffer1;
+
+    v9fs_string_init(&newpath);
+    v9fs_string_sprintf(&newpath, "%s/%s", dirpath->data, name);
+
+    /* Link the .virtfs_metadata files. Create the metada directory */
+    ret = local_create_mapped_attr_dir(ctx, newpath.data);
+    if (ret < 0) {
+        goto out;
+    }
+    buffer = local_mapped_attr_path(ctx, oldpath->data);
+    buffer1 = local_mapped_attr_path(ctx, newpath.data);
+    ret = link(buffer, buffer1);
+    g_free(buffer);
+    g_free(buffer1);
+    if (ret < 0 && errno == ENOENT) {
+        ret = 0;
+    }
+
+out:
+    v9fs_string_free(&newpath);
+    return ret;
+}
+
 static int local_link(FsContext *ctx, V9fsPath *oldpath,
                       V9fsPath *dirpath, const char *name)
 {
@@ -1129,19 +1158,8 @@ static int local_link(FsContext *ctx, V9fsPath *oldpath,
 
     /* now link the virtfs_metadata files */
     if (ctx->export_flags & V9FS_SM_MAPPED_FILE) {
-        char *vbuffer, *vbuffer1;
-
-        /* Link the .virtfs_metadata files. Create the metada directory */
-        ret = local_create_mapped_attr_dir(ctx, newpath.data);
+        ret = local_post_link_mapped_file(ctx, oldpath, dirpath, name);
         if (ret < 0) {
-            goto err_out;
-        }
-        vbuffer = local_mapped_attr_path(ctx, oldpath->data);
-        vbuffer1 = local_mapped_attr_path(ctx, newpath.data);
-        ret = link(vbuffer, vbuffer1);
-        g_free(vbuffer);
-        g_free(vbuffer1);
-        if (ret < 0 && errno != ENOENT) {
             goto err_out;
         }
     }
