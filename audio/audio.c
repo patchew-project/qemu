9c7ac53f961098491e69d5c15438bfc502615309
Pavel Dovgalyuk <pavel.dovgaluk@ispras.ru>
2017-01-31 14:59:13 +0300

    audio: make audio poll timer deterministic
    This patch changes resetting strategy of the audio polling timer.
    It does not change expiration time if the timer is already set.
    
    Signed-off-by: Pavel Dovgalyuk <pavel.dovgaluk@ispras.ru>
    Message-Id: <20170131115913.6828.12266.stgit@PASHA-ISP>

--- audio/audio.c
+++ audio/audio.c
@@ -1112,8 +1112,10 @@ static int audio_is_timer_needed (void)
 static void audio_reset_timer (AudioState *s)
 {
     if (audio_is_timer_needed ()) {
-        timer_mod (s->ts,
-            qemu_clock_get_ns(QEMU_CLOCK_VIRTUAL) + conf.period.ticks);
+        if (!timer_pending(s->ts)) {
+            timer_mod (s->ts,
+                qemu_clock_get_ns(QEMU_CLOCK_VIRTUAL) + conf.period.ticks);
+        }
     }
     else {
         timer_del (s->ts);
