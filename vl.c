12bf42bc3fe1f22c0fc7bcfcfd05cf8f83a76cf0
Dou Liyang <douly.fnst@cn.fujitsu.com>
2017-02-01 10:29:38 +0800

    vl: Move the cpu_synchronize_all_post_init() after generic devices initialization
    At the Qemu initialization, we call the cpu_synchronize_all_post_init()
    to synchronize All CPU states to KVM in the ./vl.c::main().
    
    Currently, it is called before we initialize the CPUs, which is created
    by "-device" command and parsed by generic devices initialization, So,
    these CPUs may be ignored to synchronize.
    
    The patch moves the cpu_synchronize_all_post_init func after generic
    devices initialization to make sure that all the CPUs can be included.
    
    Signed-off-by: Dou Liyang <douly.fnst@cn.fujitsu.com>
    Message-Id: <1485916178-17838-1-git-send-email-douly.fnst@cn.fujitsu.com>
    Reviewed-by: Eduardo Habkost <ehabkost@redhat.com>
    Acked-by: Eduardo Habkost <ehabkost@redhat.com>

--- vl.c
+++ vl.c
@@ -4490,8 +4490,6 @@ int main(int argc, char **argv, char **envp)
 
     audio_init();
 
-    cpu_synchronize_all_post_init();
-
     if (hax_enabled()) {
         hax_sync_vcpus();
     }
@@ -4516,6 +4514,8 @@ int main(int argc, char **argv, char **envp)
                           device_init_func, NULL, NULL)) {
         exit(1);
     }
+
+    cpu_synchronize_all_post_init();
 
     numa_post_machine_init();
 
