ea67fd4d195f7872509f48711c8e568978996718
Ziyue Yang <yzylivezh@hotmail.com>
2017-02-17 16:27:04 +0800

    target/i386/monitor.c: check return value of mon_get_cpu before using it
    This patch eliminates the segfault caused by accessing CPU that doesn't
    exist in hmp command "info lapic", which can be reproduced by
    
    $ qemu-system-x86_64 -nographic -M none -serial none -monitor stdio
    
    and then type "info lapic" into qemu monitor.
    
    Signed-off-by: Ziyue Yang <skiver.cloud.yzy@gmail.com>
    Message-Id: <20170217082704.23270-3-skiver.cloud.yzy@gmail.com>
    Reviewed-by: Philippe Mathieu-Daudé <f4bug@amsat.org>

--- target/i386/monitor.c
+++ target/i386/monitor.c
@@ -624,8 +624,11 @@ const MonitorDef *target_monitor_defs(void)
 
 void hmp_info_local_apic(Monitor *mon, const QDict *qdict)
 {
-    x86_cpu_dump_local_apic_state(mon_get_cpu(), (FILE *)mon, monitor_fprintf,
-                                  CPU_DUMP_FPU);
+    CPUState *cs = mon_get_cpu();
+    if (cs) {
+        x86_cpu_dump_local_apic_state(cs, (FILE *)mon, monitor_fprintf,
+                                      CPU_DUMP_FPU);
+    }
 }
 
 void hmp_info_io_apic(Monitor *mon, const QDict *qdict)
