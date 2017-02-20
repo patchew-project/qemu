458ff4967c03e399b457ebf2283cd980bace285b
Nikunj A Dadhania <nikunj@linux.vnet.ibm.com>
2017-02-20 15:41:56 +0530

    target/ppc: update overflow flags for add/sub
    * SO and OV reflects overflow of the 64-bit result in 64-bit mode and
      overflow of the low-order 32-bit result in 32-bit mode
    
    * OV32 reflects overflow of the low-order 32-bit independent of the mode
    
    Signed-off-by: Nikunj A Dadhania <nikunj@linux.vnet.ibm.com>
    Message-Id: <1487585521-19445-6-git-send-email-nikunj@linux.vnet.ibm.com>

--- target/ppc/translate.c
+++ target/ppc/translate.c
@@ -809,10 +809,11 @@ static inline void gen_op_arith_compute_ov(DisasContext *ctx, TCGv arg0,
         tcg_gen_andc_tl(cpu_ov, cpu_ov, t0);
     }
     tcg_temp_free(t0);
+    tcg_gen_extract_tl(cpu_ov32, cpu_ov, 31, 1);
+    tcg_gen_extract_tl(cpu_ov, cpu_ov, 63, 1);
     if (NARROW_MODE(ctx)) {
-        tcg_gen_ext32s_tl(cpu_ov, cpu_ov);
+        tcg_gen_mov_tl(cpu_ov, cpu_ov32);
     }
-    tcg_gen_shri_tl(cpu_ov, cpu_ov, TARGET_LONG_BITS - 1);
     tcg_gen_or_tl(cpu_so, cpu_so, cpu_ov);
 }
 
