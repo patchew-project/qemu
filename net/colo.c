f1d099b2bb18729dd5e9b171cae748e6a7000a38
zhanghailiang <zhang.zhanghailiang@huawei.com>
2017-02-20 16:01:59 +0800

    net/colo: fix memory double free error
    The 'primary_list' and 'secondary_list' members of struct Connection
    is not allocated through dynamically g_queue_new(), but we free it by using
    g_queue_free(), which will lead to a double-free bug.
    
    Signed-off-by: zhanghailiang <zhang.zhanghailiang@huawei.com>
    Message-Id: <1487577721-31084-2-git-send-email-zhang.zhanghailiang@huawei.com>

--- net/colo.c
+++ net/colo.c
@@ -147,9 +147,7 @@ void connection_destroy(void *opaque)
     Connection *conn = opaque;
 
     g_queue_foreach(&conn->primary_list, packet_destroy, NULL);
-    g_queue_free(&conn->primary_list);
     g_queue_foreach(&conn->secondary_list, packet_destroy, NULL);
-    g_queue_free(&conn->secondary_list);
     g_slice_free(Connection, conn);
 }
 
