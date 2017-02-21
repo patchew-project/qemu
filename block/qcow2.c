3acf6b7ffc6c9a106c8f3c545ff01fda0d602224
Kevin Wolf <kwolf@redhat.com>
2017-02-21 15:58:22 +0100

    block: Add BDRV_O_RESIZE for blk_new_open()
    blk_new_open() is a convenience function that processes flags rather
    than QDict options as a simple way to just open an image file.
    
    In order to keep it convenient in the future, it must automatically
    request the necessary permissions. This can easily be inferred from the
    flags for read and write, but we need another flag that tells us whether
    to get the resize permission.
    
    We can't just always request it because that means that no block jobs
    can run on the resulting BlockBackend (which is something that e.g.
    qemu-img commit wants to do), but we also can't request it never because
    most of the .bdrv_create() implementations call blk_truncate().
    
    The solution is to introduce another flag that is passed by all users
    that want to resize the image.
    
    Signed-off-by: Kevin Wolf <kwolf@redhat.com>
    Message-Id: <1487689130-30373-27-git-send-email-kwolf@redhat.com>

--- block/qcow2.c
+++ block/qcow2.c
@@ -2266,7 +2266,8 @@ static int qcow2_create2(const char *filename, int64_t total_size,
     options = qdict_new();
     qdict_put(options, "driver", qstring_from_str("qcow2"));
     blk = blk_new_open(filename, NULL, options,
-                       BDRV_O_RDWR | BDRV_O_NO_FLUSH, &local_err);
+                       BDRV_O_RDWR | BDRV_O_RESIZE | BDRV_O_NO_FLUSH,
+                       &local_err);
     if (blk == NULL) {
         error_propagate(errp, local_err);
         ret = -EIO;
