856800535f5675659b6604bdcccd5273fe4065f2
Eric Auger <eric.auger@redhat.com>
2017-02-21 22:10:57 +0100

    vfio/platform: make the vfio-platform device non abstract
    Up to now the vfio-platform device has been abstract and could not be
    instantiated. The integration of a new vfio platform device required
    to create a dummy derived device which only set the compatibility
    string.
    
    Following the few vfio-platform device integration we have seen
    the actual requested adaptation happens on device tree node creation
    (sysbus-fdt).
    
    So this patch removes the abstract setting and defines 2 new options,
    manufacturer and model that are used to build a compatibility string.
    
    This latter will be used to match the device tree node creation
    function
    
    sysbus-fdt does not support the instantiation of the vfio-platform
    device yet.
    
    Signed-off-by: Eric Auger <eric.auger@redhat.com>
    Message-Id: <1487711458-18198-2-git-send-email-eric.auger@redhat.com>

--- hw/vfio/platform.c
+++ hw/vfio/platform.c
@@ -637,7 +637,20 @@ static void vfio_platform_realize(DeviceState *dev, Error **errp)
     VFIOPlatformDevice *vdev = VFIO_PLATFORM_DEVICE(dev);
     SysBusDevice *sbdev = SYS_BUS_DEVICE(dev);
     VFIODevice *vbasedev = &vdev->vbasedev;
-    int i, ret;
+    int i, ret = -EINVAL;
+
+    if (!vdev->compat) {
+        if (!vdev->model) {
+            error_setg(errp, "no usable compatible string");
+            goto out;
+        }
+        if (!vdev->manufacturer) {
+            vdev->compat = g_strdup(vdev->model);
+        } else {
+            vdev->compat = g_strjoin(",", vdev->manufacturer,
+                                     vdev->model, NULL);
+        }
+    }
 
     vbasedev->type = VFIO_DEVICE_TYPE_PLATFORM;
     vbasedev->ops = &vfio_platform_ops;
@@ -678,6 +691,8 @@ static const VMStateDescription vfio_platform_vmstate = {
 static Property vfio_platform_dev_properties[] = {
     DEFINE_PROP_STRING("host", VFIOPlatformDevice, vbasedev.name),
     DEFINE_PROP_STRING("sysfsdev", VFIOPlatformDevice, vbasedev.sysfsdev),
+    DEFINE_PROP_STRING("manufacturer", VFIOPlatformDevice, manufacturer),
+    DEFINE_PROP_STRING("model", VFIOPlatformDevice, model),
     DEFINE_PROP_BOOL("x-no-mmap", VFIOPlatformDevice, vbasedev.no_mmap, false),
     DEFINE_PROP_UINT32("mmap-timeout-ms", VFIOPlatformDevice,
                        mmap_timeout, 1100),
@@ -704,7 +719,6 @@ static const TypeInfo vfio_platform_dev_info = {
     .instance_size = sizeof(VFIOPlatformDevice),
     .class_init = vfio_platform_class_init,
     .class_size = sizeof(VFIOPlatformDeviceClass),
-    .abstract   = true,
 };
 
 static void register_vfio_platform_dev_type(void)
