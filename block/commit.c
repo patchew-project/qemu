96796ce0378d7e624ce4f2cb6674b99671da0b13
Kevin Wolf <kwolf@redhat.com>
2017-02-21 15:58:41 +0100

    commit: Add filter-node-name to block-commit
    Management tools need to be able to know about every node in the graph
    and need a way to address them. This new option to block-commit allows
    the client to set a node-name for the automatically inserted filter
    driver, and at the same time serves as a witness that this version of
    qemu does automatically insert a filter driver.
    
    Signed-off-by: Kevin Wolf <kwolf@redhat.com>
    Message-Id: <1487689130-30373-46-git-send-email-kwolf@redhat.com>
    Reviewed-by: Max Reitz <mreitz@redhat.com>

--- block/commit.c
+++ block/commit.c
@@ -254,7 +254,7 @@ static BlockDriver bdrv_commit_top = {
 void commit_start(const char *job_id, BlockDriverState *bs,
                   BlockDriverState *base, BlockDriverState *top, int64_t speed,
                   BlockdevOnError on_error, const char *backing_file_str,
-                  Error **errp)
+                  const char *filter_node_name, Error **errp)
 {
     CommitBlockJob *s;
     BlockReopenQueue *reopen_queue = NULL;
@@ -307,8 +307,8 @@ void commit_start(const char *job_id, BlockDriverState *bs,
 
     /* Insert commit_top block node above top, so we can block consistent read
      * on the backing chain below it */
-    commit_top_bs = bdrv_new_open_driver(&bdrv_commit_top, NULL, BDRV_O_RDWR,
-                                         errp);
+    commit_top_bs = bdrv_new_open_driver(&bdrv_commit_top, filter_node_name,
+                                         BDRV_O_RDWR, errp);
     if (commit_top_bs == NULL) {
         goto fail;
     }
