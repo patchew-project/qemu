f1c5e4449c46faa88b9657894371a25a27e4e0f6
Greg Kurz <groug@kaod.org>
2017-02-26 23:42:26 +0100

    9pfs: local: lgetxattr: don't follow symlinks
    The local_lgetxattr() callback is vulnerable to symlink attacks because
    it calls lgetxattr() which follows symbolic links in all path elements but
    the rightmost one.
    
    This patch introduces a helper to emulate the non-existing fgetxattrat()
    function: it is implemented with /proc/self/fd which provides a trusted
    path that can be safely passed to lgetxattr().
    
    local_lgetxattr() is converted to use this helper and opendir_nofollow().
    
    This partly fixes CVE-2016-9602.
    
    Signed-off-by: Greg Kurz <groug@kaod.org>
    Message-Id: <148814894608.28146.6510271749788030194.stgit@bahia>
    Reviewed-by: Stefan Hajnoczi <stefanha@redhat.com>

--- hw/9pfs/9p-util.c
+++ hw/9pfs/9p-util.c
@@ -11,6 +11,7 @@
  */
 
 #include "qemu/osdep.h"
+#include "qemu/xattr.h"
 #include "9p-util.h"
 
 int openat_nofollow(int dirfd, const char *path, int flags, mode_t mode)
@@ -50,4 +51,15 @@ int openat_nofollow(int dirfd, const char *path, int flags, mode_t mode)
     }
 
     return fd;
+}
+
+ssize_t fgetxattrat_nofollow(int dirfd, const char *filename, const char *name,
+                             void *value, size_t size)
+{
+    char *proc_path = g_strdup_printf("/proc/self/fd/%d/%s", dirfd, filename);
+    int ret;
+
+    ret = lgetxattr(proc_path, name, value, size);
+    g_free(proc_path);
+    return ret;
 }
