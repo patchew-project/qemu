9a015de3d7b6d4b811608e5022bc06f092c77b66
Kevin Wolf <kwolf@redhat.com>
2017-02-27 21:09:31 +0100

    block: Allow backing file links in change_parent_backing_link()
    Now that the backing file child role implements .attach/.detach
    callbacks, nothing prevents us from modifying the graph even if that
    involves changing backing file links.
    
    Signed-off-by: Kevin Wolf <kwolf@redhat.com>
    Reviewed-by: Max Reitz <mreitz@redhat.com>
    Message-Id: <1488226184-9044-31-git-send-email-kwolf@redhat.com>

--- block.c
+++ block.c
@@ -2872,9 +2872,9 @@ static void change_parent_backing_link(BlockDriverState *from,
             continue;
         }
         if (c->role == &child_backing) {
-            /* @from is generally not allowed to be a backing file, except for
-             * when @to is the overlay. In that case, @from may not be replaced
-             * by @to as @to's backing node. */
+            /* If @from is a backing file of @to, ignore the child to avoid
+             * creating a loop. We only want to change the pointer of other
+             * parents. */
             QLIST_FOREACH(to_c, &to->children, next) {
                 if (to_c == c) {
                     break;
@@ -2885,7 +2885,6 @@ static void change_parent_backing_link(BlockDriverState *from,
             }
         }
 
-        assert(c->role != &child_backing);
         bdrv_ref(to);
         bdrv_replace_child(c, to);
         bdrv_unref(from);
