e91d707f8cc3218c0aa60df440378b9115fe4e39
Kevin Wolf <kwolf@redhat.com>
2017-02-28 13:54:19 +0100

    mirror: Add filter-node-name to blockdev-mirror
    Management tools need to be able to know about every node in the graph
    and need a way to address them. Changing the graph structure was okay
    because libvirt doesn't really manage the node level yet, but future
    libvirt versions need to deal with both new and old version of qemu.
    
    This new option to blockdev-mirror allows the client to set a node-name
    for the automatically inserted filter driver, and at the same time
    serves as a witness for a future libvirt that this version of qemu does
    automatically insert a filter driver.
    
    Signed-off-by: Kevin Wolf <kwolf@redhat.com>
    Reviewed-by: Max Reitz <mreitz@redhat.com>
    Message-Id: <1488286469-9381-35-git-send-email-kwolf@redhat.com>
    Acked-by: Fam Zheng <famz@redhat.com>

--- blockdev.c
+++ blockdev.c
@@ -3365,6 +3365,8 @@ static void blockdev_mirror_common(const char *job_id, BlockDriverState *bs,
                                    bool has_on_target_error,
                                    BlockdevOnError on_target_error,
                                    bool has_unmap, bool unmap,
+                                   bool has_filter_node_name,
+                                   const char *filter_node_name,
                                    Error **errp)
 {
 
@@ -3385,6 +3387,9 @@ static void blockdev_mirror_common(const char *job_id, BlockDriverState *bs,
     }
     if (!has_unmap) {
         unmap = true;
+    }
+    if (!has_filter_node_name) {
+        filter_node_name = NULL;
     }
 
     if (granularity != 0 && (granularity < 512 || granularity > 1048576 * 64)) {
@@ -3415,7 +3420,8 @@ static void blockdev_mirror_common(const char *job_id, BlockDriverState *bs,
     mirror_start(job_id, bs, target,
                  has_replaces ? replaces : NULL,
                  speed, granularity, buf_size, sync, backing_mode,
-                 on_source_error, on_target_error, unmap, errp);
+                 on_source_error, on_target_error, unmap, filter_node_name,
+                 errp);
 }
 
 void qmp_drive_mirror(DriveMirror *arg, Error **errp)
@@ -3553,6 +3559,7 @@ void qmp_drive_mirror(DriveMirror *arg, Error **errp)
                            arg->has_on_source_error, arg->on_source_error,
                            arg->has_on_target_error, arg->on_target_error,
                            arg->has_unmap, arg->unmap,
+                           false, NULL,
                            &local_err);
     bdrv_unref(target_bs);
     error_propagate(errp, local_err);
@@ -3571,6 +3578,8 @@ void qmp_blockdev_mirror(bool has_job_id, const char *job_id,
                          BlockdevOnError on_source_error,
                          bool has_on_target_error,
                          BlockdevOnError on_target_error,
+                         bool has_filter_node_name,
+                         const char *filter_node_name,
                          Error **errp)
 {
     BlockDriverState *bs;
@@ -3602,6 +3611,7 @@ void qmp_blockdev_mirror(bool has_job_id, const char *job_id,
                            has_on_source_error, on_source_error,
                            has_on_target_error, on_target_error,
                            true, true,
+                           has_filter_node_name, filter_node_name,
                            &local_err);
     error_propagate(errp, local_err);
 
