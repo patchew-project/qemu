a0f9aeee968287ab0d0c77d8d67cc85c3e6b6984
Fam Zheng <famz@redhat.com>
2017-03-21 11:16:32 +0800

    nbd: Allow BLK_PERM_AIO_CONTEXT_CHANGE on BB
    This is safe because of the aio context notifier we'll register on this
    node. So allow it.
    
    Signed-off-by: Fam Zheng <famz@redhat.com>
    Message-Id: <20170321031635.22123-14-famz@redhat.com>

--- nbd/server.c
+++ nbd/server.c
@@ -900,8 +900,10 @@ NBDExport *nbd_export_new(BlockDriverState *bs, off_t dev_offset, off_t size,
     if ((nbdflags & NBD_FLAG_READ_ONLY) == 0) {
         perm |= BLK_PERM_WRITE;
     }
-    blk = blk_new(perm, BLK_PERM_CONSISTENT_READ | BLK_PERM_WRITE_UNCHANGED |
-                        BLK_PERM_WRITE | BLK_PERM_GRAPH_MOD);
+    blk = blk_new(perm,
+                  BLK_PERM_CONSISTENT_READ | BLK_PERM_WRITE_UNCHANGED |
+                  BLK_PERM_WRITE | BLK_PERM_GRAPH_MOD |
+                  BLK_PERM_AIO_CONTEXT_CHANGE);
     ret = blk_insert_bs(blk, bs, errp);
     if (ret < 0) {
         goto fail;
