a94f81201dc852db07d111fd71b354fc0d888d5b
Paolo Bonzini <pbonzini@redhat.com>
2017-03-23 18:39:21 +0100

    blockjob: introduce block_job_fail
    Outside blockjob.c, block_job_unref is only used when a block job fails
    to start, and block_job_ref is not used at all.  The reference counting
    thus is pretty well hidden.  Introduce a separate function to be used
    by block jobs; because block_job_ref and block_job_unref now become
    static, move them earlier in blockjob.c.
    
    Later on, block_job_fail will also have different locking than
    block_job_unref.
    
    Signed-off-by: Paolo Bonzini <pbonzini@redhat.com>
    Message-Id: <20170323173928.14439-4-pbonzini@redhat.com>

--- block/mirror.c
+++ block/mirror.c
@@ -1251,7 +1251,7 @@ fail:
 
         g_free(s->replaces);
         blk_unref(s->target);
-        block_job_unref(&s->common);
+        block_job_fail(&s->common);
     }
 
     bdrv_child_try_set_perm(mirror_top_bs->backing, 0, BLK_PERM_ALL,
