d4abcd29cb3454e8c1a70a02832ced0f51520c94
Eduardo Habkost <ehabkost@redhat.com>
2017-03-31 21:46:14 -0300

    kvmvapic: Remove user_creatable flag
    The kvmvapic device needs to be created by apic_common_realize()
    when apic-common.vapic=on is set, not using -device. Remove the
    user_creatable flag from the device class.
    
    Cc: Igor Mammedov <imammedo@redhat.com>
    Cc: Paolo Bonzini <pbonzini@redhat.com>
    Cc: Richard Henderson <rth@twiddle.net>
    Cc: Eduardo Habkost <ehabkost@redhat.com>
    Cc: "Michael S. Tsirkin" <mst@redhat.com>
    Signed-off-by: Eduardo Habkost <ehabkost@redhat.com>
    Message-Id: <20170401004624.30886-10-ehabkost@redhat.com>

--- hw/i386/kvmvapic.c
+++ hw/i386/kvmvapic.c
@@ -856,11 +856,6 @@ static void vapic_class_init(ObjectClass *klass, void *data)
     dc->reset   = vapic_reset;
     dc->vmsd    = &vmstate_vapic;
     dc->realize = vapic_realize;
-    /*
-     * FIXME: Set only for compatibility on q35 machine-type.
-     * Probably never meant to be user-creatable
-     */
-    dc->user_creatable = true;
 }
 
 static const TypeInfo vapic_type = {
