fe8027b82ca9cd78ef25da47c374aeb717bfb1a6
Denis V. Lunev <den@openvz.org>
2017-04-05 18:12:06 +0300

    parallels: relax check for auto switch of prealloc_mode
    PRL_PREALLOC_MODE_TRUNCATE can be set only through command line. Remove
    the check that bdrv_truncate() is working on, this is almost always the
    case, while the check could lead to serious consequences during migration
    etc when we should not even try to API which technically could change the
    image (asserts are possible).
    
    Let us keep this simple.
    
    Signed-off-by: Denis V. Lunev <den@openvz.org>
    CC: Stefan Hajnoczi <stefanha@redhat.com>
    Message-Id: <1491405126-30591-3-git-send-email-den@openvz.org>
    Reviewed-by: Stefan Hajnoczi <stefanha@redhat.com>

--- block/parallels.c
+++ block/parallels.c
@@ -695,8 +695,7 @@ static int parallels_open(BlockDriverState *bs, QDict *options, int flags,
         goto fail_options;
     }
 
-    if (!(flags & BDRV_O_RESIZE) || !bdrv_has_zero_init(bs->file->bs) ||
-            bdrv_truncate(bs->file, bdrv_getlength(bs->file->bs)) != 0) {
+    if (!bdrv_has_zero_init(bs->file->bs) != 0) {
         s->prealloc_mode = PRL_PREALLOC_MODE_FALLOCATE;
     }
 
