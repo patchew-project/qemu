8e2afbac5107f3309ba9fd640c7cd933dae1dc9f
Eric Blake <eblake@redhat.com>
2017-04-10 20:17:12 -0500

    qcow2: Discard/zero clusters by byte count
    Passing a byte offset, but sector count, when we ultimately
    want to operate on cluster granularity, is madness.  Clean up
    the external interfaces to take both offset and count as bytes,
    while still keeping the assertion added previously that the
    caller must align the values to a cluster.  Then rename things
    to make sure backports don't get confused by changed units:
    instead of qcow2_discard_clusters() and qcow2_zero_clusters(),
    we now have qcow2_cluster_discard() and qcow2_cluster_zeroize().
    
    The internal functions still operate on clusters at a time, and
    return an int for number of cleared clusters; but on an image
    with 2M clusters, a single L2 table holds 256k entries that each
    represent a 2M cluster, totalling well over INT_MAX bytes if we
    ever had a request for that many bytes at once.  All our callers
    currently limit themselves to 32-bit bytes (and therefore fewer
    clusters), but by making this function 64-bit clean, we have one
    less place to clean up if we later improve the block layer to
    support 64-bit bytes through all operations (with the block layer
    auto-fragmenting on behalf of more-limited drivers), rather than
    the current state where some interfaces are artificially limited
    to INT_MAX at a time.
    
    Signed-off-by: Eric Blake <eblake@redhat.com>
    
    Message-Id: <20170411011718.9152-8-eblake@redhat.com>

--- block/qcow2-cluster.c
+++ block/qcow2-cluster.c
@@ -1509,15 +1509,15 @@ static int discard_single_l2(BlockDriverState *bs, uint64_t offset,
     return nb_clusters;
 }
 
-int qcow2_discard_clusters(BlockDriverState *bs, uint64_t offset,
-    int nb_sectors, enum qcow2_discard_type type, bool full_discard)
+int qcow2_cluster_discard(BlockDriverState *bs, uint64_t offset,
+                          uint64_t bytes, enum qcow2_discard_type type,
+                          bool full_discard)
 {
     BDRVQcow2State *s = bs->opaque;
-    uint64_t end_offset;
+    uint64_t end_offset = offset + bytes;
     uint64_t nb_clusters;
+    int64_t cleared;
     int ret;
-
-    end_offset = offset + (nb_sectors << BDRV_SECTOR_BITS);
 
     /* Caller must pass aligned values, except at image end */
     assert(QEMU_IS_ALIGNED(offset, s->cluster_size));
@@ -1530,13 +1530,15 @@ int qcow2_discard_clusters(BlockDriverState *bs, uint64_t offset,
 
     /* Each L2 table is handled by its own loop iteration */
     while (nb_clusters > 0) {
-        ret = discard_single_l2(bs, offset, nb_clusters, type, full_discard);
-        if (ret < 0) {
+        cleared = discard_single_l2(bs, offset, nb_clusters, type,
+                                    full_discard);
+        if (cleared < 0) {
+            ret = cleared;
             goto fail;
         }
 
-        nb_clusters -= ret;
-        offset += (ret * s->cluster_size);
+        nb_clusters -= cleared;
+        offset += (cleared * s->cluster_size);
     }
 
     ret = 0;
@@ -1591,20 +1593,22 @@ static int zero_single_l2(BlockDriverState *bs, uint64_t offset,
     return nb_clusters;
 }
 
-int qcow2_zero_clusters(BlockDriverState *bs, uint64_t offset, int nb_sectors,
-                        int flags)
+int qcow2_cluster_zeroize(BlockDriverState *bs, uint64_t offset,
+                          uint64_t bytes, int flags)
 {
     BDRVQcow2State *s = bs->opaque;
     uint64_t end_offset;
     uint64_t nb_clusters;
+    int64_t cleared;
     int ret;
 
-    end_offset = offset + (nb_sectors << BDRV_SECTOR_BITS);
+    end_offset = offset + bytes;
 
     /* Caller must pass aligned values, except at image end */
     assert(QEMU_IS_ALIGNED(offset, s->cluster_size));
     assert(QEMU_IS_ALIGNED(end_offset, s->cluster_size) ||
            end_offset == bs->total_sectors << BDRV_SECTOR_BITS);
+    assert(QEMU_IS_ALIGNED(bytes, s->cluster_size));
 
     /* The zero flag is only supported by version 3 and newer; we
      * require the use of that flag if there is a backing file or if
@@ -1615,18 +1619,19 @@ int qcow2_zero_clusters(BlockDriverState *bs, uint64_t offset, int nb_sectors,
     }
 
     /* Each L2 table is handled by its own loop iteration */
-    nb_clusters = size_to_clusters(s, nb_sectors << BDRV_SECTOR_BITS);
+    nb_clusters = size_to_clusters(s, bytes);
 
     s->cache_discards = true;
 
     while (nb_clusters > 0) {
-        ret = zero_single_l2(bs, offset, nb_clusters, flags);
-        if (ret < 0) {
+        cleared = zero_single_l2(bs, offset, nb_clusters, flags);
+        if (cleared < 0) {
+            ret = cleared;
             goto fail;
         }
 
-        nb_clusters -= ret;
-        offset += (ret * s->cluster_size);
+        nb_clusters -= cleared;
+        offset += (cleared * s->cluster_size);
     }
 
     ret = 0;
