6252fc9f17aadd50f1e31d0031b2ebb77247604a
Daniel P. Berrange <berrange@redhat.com>
2017-04-25 16:38:47 +0100

    block: deprecate "encryption=on" in favour of "encrypt.format=aes"
    Historically the qcow & qcow2 image formats supported a property
    "encryption=on" to enable their built-in AES encryption. We'll
    soon be supporting LUKS for qcow2, so need a more general purpose
    way to enable encryption, with a choice of formats.
    
    This introduces an "encrypt.format" option, which will later be
    joined by a number of other "encrypt.XXX" options. The use of
    a "encrypt." prefix instead of "encrypt-" is done to facilitate
    mapping to a nested QAPI schema at later date.
    
    e.g. the preferred syntax is now
    
      qemu-img create -f qcow2 -o encrypt.format=aes demo.qcow2
    
    Signed-off-by: Daniel P. Berrange <berrange@redhat.com>
    Message-Id: <20170425153858.25660-8-berrange@redhat.com>
    Reviewed-by: Eric Blake <eblake@redhat.com>

--- qemu-img.c
+++ qemu-img.c
@@ -2229,6 +2229,8 @@ static int img_convert(int argc, char **argv)
     if (compress) {
         bool encryption =
             qemu_opt_get_bool(opts, BLOCK_OPT_ENCRYPT, false);
+        const char *encryptfmt =
+            qemu_opt_get(opts, BLOCK_OPT_ENCRYPT_FORMAT);
         const char *preallocation =
             qemu_opt_get(opts, BLOCK_OPT_PREALLOC);
 
@@ -2238,7 +2240,7 @@ static int img_convert(int argc, char **argv)
             goto out;
         }
 
-        if (encryption) {
+        if (encryption || encryptfmt) {
             error_report("Compression and encryption not supported at "
                          "the same time");
             ret = -1;
