e63ec9785b942badcf4d3367b63657fdab293d35
Yulei Zhang <yulei.zhang@intel.com>
2017-05-09 15:58:46 +0800

    vfio: Add struct vfio_vmstate_info to introduce put/get callback funtion for vfio device status save/restore
    Introduce vfio_device_put/vfio_device_get funtion for vfio device state
    save/restore usage.
    
    For VFIO pci device status migrate, on the source side with
    funtion vfio_device_put to save the following states
    1. pci configuration space addr0~addr5
    2. pci configuration space msi_addr msi_data
    3. pci device status fetch from device driver
    
    And on the target side with funtion vfio_device_get to restore
    the same states
    1. re-setup the pci bar configuration
    2. re-setup the pci device msi configuration
    3. restore the pci device status
    
    Signed-off-by: Yulei Zhang <yulei.zhang@intel.com>
    Message-Id: <1494316727-15518-4-git-send-email-yulei.zhang@intel.com>

--- linux-headers/linux/vfio.h
+++ linux-headers/linux/vfio.h
@@ -304,6 +304,8 @@ struct vfio_region_info_cap_type {
 #define VFIO_DEVICE_START	0
 #define VFIO_DEVICE_STOP	1
 
+#define VFIO_DEVICE_STATE_OFFSET       1
+
 /**
  * VFIO_DEVICE_GET_IRQ_INFO - _IOWR(VFIO_TYPE, VFIO_BASE + 9,
  *				    struct vfio_irq_info)
