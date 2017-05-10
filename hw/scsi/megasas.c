dfeb4e48a2bf3a272b39bd9f33d0ea2f62a99246
Prasad J Pandit <pjp@fedoraproject.org>
2017-05-10 16:43:50 +0530

    scsi: megasas: check request object in MegasasCmd
    When processing completed commands, if SCSIRequest object
    'cmd->req' was null, it could lead to null pointer dereference
    in megasas_complete_command. Add check to avoid it.
    
    Reported-by: Zhangyanyu <zyy4013@stu.ouc.edu.cn>
    Signed-off-by: Prasad J Pandit <pjp@fedoraproject.org>
    Message-Id: <20170510111350.18422-1-ppandit@redhat.com>

--- hw/scsi/megasas.c
+++ hw/scsi/megasas.c
@@ -580,9 +580,11 @@ static void megasas_complete_command(MegasasCmd *cmd)
     cmd->iov_size = 0;
     cmd->iov_offset = 0;
 
-    cmd->req->hba_private = NULL;
-    scsi_req_unref(cmd->req);
-    cmd->req = NULL;
+    if (cmd->req) {
+        cmd->req->hba_private = NULL;
+        scsi_req_unref(cmd->req);
+        cmd->req = NULL;
+    }
 
     megasas_unmap_frame(cmd->state, cmd);
     megasas_complete_frame(cmd->state, cmd->context);
