03ec6e520ae38f3417d556355174615045e72fbd
Peter Xu <peterx@redhat.com>
2017-05-10 16:01:45 +0800

    memory: tune last param of iommu_ops.translate()
    This patch converts the old "is_write" bool into IOMMUAccessFlags. The
    difference is that "is_write" can only express either read/write, but
    sometimes what we really want is "none" here (neither read nor write).
    Replay is an good example - during replay, we should not check any RW
    permission bits since thats not an actual IO at all.
    
    CC: Paolo Bonzini <pbonzini@redhat.com>
    CC: David Gibson <david@gibson.dropbear.id.au>
    Reviewed-by: David Gibson <david@gibson.dropbear.id.au>
    Acked-by: David Gibson <david@gibson.dropbear.id.au>
    Acked-by: Paolo Bonzini <pbonzini@redhat.com>
    Signed-off-by: Peter Xu <peterx@redhat.com>
    Message-Id: <1494403315-12760-3-git-send-email-peterx@redhat.com>

--- exec.c
+++ exec.c
@@ -481,7 +481,8 @@ IOMMUTLBEntry address_space_get_iotlb_entry(AddressSpace *as, hwaddr addr,
             break;
         }
 
-        iotlb = mr->iommu_ops->translate(mr, addr, is_write);
+        iotlb = mr->iommu_ops->translate(mr, addr, is_write ?
+                                         IOMMU_WO : IOMMU_RO);
         if (!(iotlb.perm & (1 << is_write))) {
             iotlb.target_as = NULL;
             break;
@@ -513,7 +514,8 @@ MemoryRegion *address_space_translate(AddressSpace *as, hwaddr addr,
             break;
         }
 
-        iotlb = mr->iommu_ops->translate(mr, addr, is_write);
+        iotlb = mr->iommu_ops->translate(mr, addr, is_write ?
+                                         IOMMU_WO : IOMMU_RO);
         addr = ((iotlb.translated_addr & ~iotlb.addr_mask)
                 | (addr & iotlb.addr_mask));
         *plen = MIN(*plen, (addr | iotlb.addr_mask) - addr + 1);
