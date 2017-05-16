21c3f41f15af402ea91b88fa1990a241433fd855
Juan Quintela <quintela@redhat.com>
2017-05-16 13:11:21 +0200

    migration: Remove use of old MigrationParams
    We have change in the previous patch to use migration capabilities for
    it.  Notice that we continue using the old command line flags from
    migrate command from the time being.  Remove the set_params method as
    now it is empty.
    
    Signed-off-by: Juan Quintela <quintela@redhat.com>
    
    Message-Id: <20170516111123.17692-4-quintela@redhat.com>

--- migration/migration.c
+++ migration/migration.c
@@ -1248,9 +1248,6 @@ void qmp_migrate(const char *uri, bool has_blk, bool blk,
     bool block_enabled = false;
     const char *p;
 
-    params.blk = has_blk && blk;
-    params.shared = has_inc && inc;
-
     if (migration_is_setup_or_active(s->state) ||
         s->state == MIGRATION_STATUS_CANCELLING ||
         s->state == MIGRATION_STATUS_COLO) {
