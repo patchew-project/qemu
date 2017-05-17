115b84f67aaf231516fd842c93d4b00ba7c06d46
Bharata B Rao <bharata@linux.vnet.ibm.com>
2017-05-17 09:19:17 +0530

    migration: Fix unregister_savevm()
    In unregister_savevm(), free se->compat only if it was allocated earlier.
    
    Signed-off-by: Bharata B Rao <bharata@linux.vnet.ibm.com>
    Message-Id: <1494992962-6929-2-git-send-email-bharata@linux.vnet.ibm.com>

--- migration/savevm.c
+++ migration/savevm.c
@@ -648,7 +648,9 @@ void unregister_savevm(DeviceState *dev, const char *idstr, void *opaque)
     QTAILQ_FOREACH_SAFE(se, &savevm_state.handlers, entry, new_se) {
         if (strcmp(se->idstr, id) == 0 && se->opaque == opaque) {
             QTAILQ_REMOVE(&savevm_state.handlers, se, entry);
-            g_free(se->compat);
+            if (dev) {
+                g_free(se->compat);
+            }
             g_free(se->ops);
             g_free(se);
         }
