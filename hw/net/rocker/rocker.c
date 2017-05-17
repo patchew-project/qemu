84a7725b186307a7e4473ae0adbf07a56f4d5c2d
Mao Zhongyi <maozy.fnst@cn.fujitsu.com>
2017-05-17 19:12:24 +0800

    net/rocker: Plug memory leak in pci_rocker_init()
    pci_rocker_init() leaks a World when the name more than 9 chars,
    then return a negative value directly, doesn't make a correct
    cleanup. So add a new goto label to fix it.
    
    Cc: jasowang@redhat.com
    Cc: jiri@resnulli.us
    Cc: f4bug@amsat.org
    Cc: armbru@redhat.com
    Signed-off-by: Mao Zhongyi <maozy.fnst@cn.fujitsu.com>
    Message-Id: <c1f2bc8f51ab57ad99e103af6b36320ba8980fe2.1495018956.git.maozy.fnst@cn.fujitsu.com>

--- hw/net/rocker/rocker.c
+++ hw/net/rocker/rocker.c
@@ -1371,7 +1371,8 @@ static int pci_rocker_init(PCIDevice *dev)
         fprintf(stderr,
                 "rocker: name too long; please shorten to at most %d chars\n",
                 MAX_ROCKER_NAME_LEN);
-        return -EINVAL;
+        err = -EINVAL;
+        goto err_name_too_long;
     }
 
     if (memcmp(&r->fp_start_macaddr, &zero, sizeof(zero)) == 0) {
@@ -1430,6 +1431,7 @@ static int pci_rocker_init(PCIDevice *dev)
 
     return 0;
 
+err_name_too_long:
 err_duplicate:
     rocker_msix_uninit(r);
 err_msix_init:
