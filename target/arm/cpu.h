b946845ee8478ab7080131737f274ca090df39df
Igor Mammedov <imammedo@redhat.com>
2017-05-30 18:24:00 +0200

    numa: move numa_node from CPUState into target specific classes
    Move vcpu's assocciated numa_node field out of generic CPUState
    into inherited classes that actually care about cpu<->numa mapping,
    i.e: ARMCPU, PowerPCCPU, X86CPU.
    
    Signed-off-by: Igor Mammedov <imammedo@redhat.com>
    Message-Id: <1496161442-96665-6-git-send-email-imammedo@redhat.com>

--- target/arm/cpu.h
+++ target/arm/cpu.h
@@ -703,6 +703,8 @@ struct ARMCPU {
 
     ARMELChangeHook *el_change_hook;
     void *el_change_hook_opaque;
+
+    int32_t node_id; /* NUMA node this CPU is belonging to */
 };
 
 static inline ARMCPU *arm_env_get_cpu(CPUARMState *env)
