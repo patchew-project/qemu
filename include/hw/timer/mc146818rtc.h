5001e27558812dcd7eef0caef38aa73a84a92116
Xiao Guangrong <xiaoguangrong@tencent.com>
2017-06-01 14:41:43 +0200

    qtest: add rtc periodic timer test
    It tests the accuracy of rtc periodic timer which is recently
    improved & fixed by commit 7ffcb539a3 ("mc146818rtc: precisely count
    the clock for periodic timer", 2017-05-19).
    
    Signed-off-by: Xiao Guangrong <xiaoguangrong@tencent.com>
    Message-Id: <20170527025301.23499-1-xiaoguangrong@tencent.com>
    Signed-off-by: Paolo Bonzini <pbonzini@redhat.com>
    Message-Id: <1496320911-51305-26-git-send-email-pbonzini@redhat.com>

--- include/hw/timer/mc146818rtc.h
+++ include/hw/timer/mc146818rtc.h
@@ -10,4 +10,23 @@ ISADevice *rtc_init(ISABus *bus, int base_year, qemu_irq intercept_irq);
 void rtc_set_memory(ISADevice *dev, int addr, int val);
 int rtc_get_memory(ISADevice *dev, int addr);
 
+static inline uint32_t periodic_period_to_clock(int period_code)
+{
+    if (!period_code) {
+        return 0;
+   }
+
+    if (period_code <= 2) {
+        period_code += 7;
+    }
+    /* period in 32 Khz cycles */
+   return 1 << (period_code - 1);
+}
+
+#define RTC_CLOCK_RATE            32768
+
+static inline int64_t periodic_clock_to_ns(int64_t clocks)
+{
+    return muldiv64(clocks, NANOSECONDS_PER_SECOND, RTC_CLOCK_RATE);
+}
 #endif /* MC146818RTC_H */
