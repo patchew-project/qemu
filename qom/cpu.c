7f8219871a8c867122982bb1e8dc3858c6d51a19
Emilio G. Cota <cota@braap.org>
2017-06-08 22:25:17 -0400

    cpu: allocate cpu->trace_dstate in place
    There's little point in dynamically allocating the bitmap if we
    know at compile-time the max number of events we want to support.
    Thus, make room in the struct for the bitmap, which will make things
    easier later: this paves the way for upcoming changes, in which
    we'll use a u32 to fully capture cpu->trace_dstate.
    
    This change also increases performance by saving a dereference and
    improving locality--note that this is important since upcoming work
    makes reading this bitmap fairly common.
    
    Signed-off-by: Emilio G. Cota <cota@braap.org>
    Message-Id: <1496975122-16999-3-git-send-email-cota@braap.org>
    Reviewed-by: Lluís Vilanova <vilanova@ac.upc.edu>

--- qom/cpu.c
+++ qom/cpu.c
@@ -382,7 +382,6 @@ static void cpu_common_unrealizefn(DeviceState *dev, Error **errp)
 
 static void cpu_common_initfn(Object *obj)
 {
-    uint32_t count;
     CPUState *cpu = CPU(obj);
     CPUClass *cc = CPU_GET_CLASS(obj);
 
@@ -397,18 +396,11 @@ static void cpu_common_initfn(Object *obj)
     QTAILQ_INIT(&cpu->breakpoints);
     QTAILQ_INIT(&cpu->watchpoints);
 
-    count = trace_get_vcpu_event_count();
-    if (count) {
-        cpu->trace_dstate = bitmap_new(count);
-    }
-
     cpu_exec_initfn(cpu);
 }
 
 static void cpu_common_finalize(Object *obj)
 {
-    CPUState *cpu = CPU(obj);
-    g_free(cpu->trace_dstate);
 }
 
 static int64_t cpu_common_get_arch_id(CPUState *cpu)
