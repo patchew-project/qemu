b943619de54a61ce1bbd56a525210de4c648c910
Alex Bennée <alex.bennee@linaro.org>
2017-06-14 15:02:08 +0100

    tcg-runtime: light re-factor of lookup_tb_ptr
    Just a little precursor re-factoring before I was going to add a trace
    point:
    
      - single return point, defaulting to tcg_ctx.code_gen_epilogue
      - move cs_base, pc and flags inside the jump cache hit scope
      - calculate the tb_jmp_cache hash once
    
    Signed-off-by: Alex Bennée <alex.bennee@linaro.org>
    Message-Id: <20170614140209.29847-3-alex.bennee@linaro.org>
    Tested-by: Philippe Mathieu-Daudé <f4bug@amsat.org>

--- tcg-runtime.c
+++ tcg-runtime.c
@@ -147,30 +147,33 @@ uint64_t HELPER(ctpop_i64)(uint64_t arg)
 void *HELPER(lookup_tb_ptr)(CPUArchState *env, target_ulong addr)
 {
     CPUState *cpu = ENV_GET_CPU(env);
+    unsigned int addr_hash = tb_jmp_cache_hash_func(addr);
+    void *code_ptr = NULL;
     TranslationBlock *tb;
-    target_ulong cs_base, pc;
-    uint32_t flags;
 
-    tb = atomic_rcu_read(&cpu->tb_jmp_cache[tb_jmp_cache_hash_func(addr)]);
+    tb = atomic_rcu_read(&cpu->tb_jmp_cache[addr_hash]);
     if (likely(tb)) {
+        target_ulong cs_base, pc;
+        uint32_t flags;
+
         cpu_get_tb_cpu_state(env, &pc, &cs_base, &flags);
+
         if (likely(tb->pc == addr && tb->cs_base == cs_base &&
                    tb->flags == flags)) {
-            goto found;
-        }
-        tb = tb_htable_lookup(cpu, addr, cs_base, flags);
-        if (likely(tb)) {
-            atomic_set(&cpu->tb_jmp_cache[tb_jmp_cache_hash_func(addr)], tb);
-            goto found;
+            code_ptr = tb->tc_ptr;
+        } else {
+            /* If we didn't find it in the jmp_cache we still might
+             * find it in the global tb_htable
+             */
+            tb = tb_htable_lookup(cpu, addr, cs_base, flags);
+            if (likely(tb)) {
+                atomic_set(&cpu->tb_jmp_cache[addr_hash], tb);
+                code_ptr = tb->tc_ptr;
+            }
         }
     }
-    return tcg_ctx.code_gen_epilogue;
- found:
-    qemu_log_mask_and_addr(CPU_LOG_EXEC, addr,
-                           "Chain %p [%d: " TARGET_FMT_lx "] %s\n",
-                           tb->tc_ptr, cpu->cpu_index, addr,
-                           lookup_symbol(addr));
-    return tb->tc_ptr;
+
+    return code_ptr ? code_ptr : tcg_ctx.code_gen_epilogue;
 }
 
 void HELPER(exit_atomic)(CPUArchState *env)
