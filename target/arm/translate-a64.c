a4aec38af5459b018e86b1204a0c026f47283ac8
Richard Henderson <rth@twiddle.net>
2017-06-13 22:23:10 -0700

    Revert "target/aarch64: optimize indirect branches"
    This reverts commit e75449a346bf558296966a44277bfd93412c6da6.
    
    This patch appears to induce lockups, or maybe livelocks, while
    booting a Linux kernel.  The assumption is that there is a bug
    elsewhere within QEMU, but reverting the patch allows normal work
    to continue in the meantime.
    
    Signed-off-by: Richard Henderson <rth@twiddle.net>
    Message-Id: <20170614052311.13785-10-rth@twiddle.net>

--- target/arm/translate-a64.c
+++ target/arm/translate-a64.c
@@ -11367,7 +11367,8 @@ void gen_intermediate_code_a64(ARMCPU *cpu, TranslationBlock *tb)
             gen_a64_set_pc_im(dc->pc);
             /* fall through */
         case DISAS_JUMP:
-            tcg_gen_lookup_and_goto_ptr(cpu_pc);
+            /* indicate that the hash table must be used to find the next TB */
+            tcg_gen_exit_tb(0);
             break;
         case DISAS_TB_JUMP:
         case DISAS_EXC:
