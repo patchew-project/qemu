13de78575822c2643f662a58f71344fdeac2019e
Emilio G. Cota <cota@braap.org>
2017-06-14 16:27:33 -0400

    tcg-runtime: increase hit rate of lookup_tb_ptr
    On Wed, Jun 14, 2017 at 12:48:17 -0700, Richard Henderson wrote:
    > We can call tb_htable_lookup even when the tb_jmp_cache
    > is completely empty.  Therefore, un-nest most of the code
    > dependent on tb != NULL from the read from the cache.
    >
    > Signed-off-by: Richard Henderson <rth@twiddle.net>
    
    I just wrote this alternative patch, which does the same thing
    as yours. I also measured what the effect of this change
    has on the hit rate of lookup_tb_ptr. Feel free to reuse parts
    of the patch and/or the commit message!
    
    Thanks,
    
    		E.
    
    Message-Id: <20170614202733.GA8420@flamenco>
    Suggested-by: Alex Bennée <alex.bennee@linaro.org>
    Suggested-by: Richard Henderson <rth@twiddle.net>

--- tcg-runtime.c
+++ tcg-runtime.c
@@ -149,23 +149,19 @@ void *HELPER(lookup_tb_ptr)(CPUArchState *env, target_ulong addr)
     CPUState *cpu = ENV_GET_CPU(env);
     TranslationBlock *tb;
     target_ulong cs_base, pc;
+    unsigned int hash = tb_jmp_cache_hash_func(addr);
     uint32_t flags;
 
-    tb = atomic_rcu_read(&cpu->tb_jmp_cache[tb_jmp_cache_hash_func(addr)]);
-    if (likely(tb)) {
-        cpu_get_tb_cpu_state(env, &pc, &cs_base, &flags);
-        if (likely(tb->pc == addr && tb->cs_base == cs_base &&
-                   tb->flags == flags)) {
-            goto found;
-        }
+    tb = atomic_rcu_read(&cpu->tb_jmp_cache[hash]);
+    cpu_get_tb_cpu_state(env, &pc, &cs_base, &flags);
+    if (unlikely(tb == NULL || tb->pc != addr || tb->cs_base != cs_base ||
+               tb->flags != flags)) {
         tb = tb_htable_lookup(cpu, addr, cs_base, flags);
-        if (likely(tb)) {
-            atomic_set(&cpu->tb_jmp_cache[tb_jmp_cache_hash_func(addr)], tb);
-            goto found;
+        if (unlikely(tb == NULL)) {
+            return tcg_ctx.code_gen_epilogue;
         }
+        atomic_set(&cpu->tb_jmp_cache[hash], tb);
     }
-    return tcg_ctx.code_gen_epilogue;
- found:
     qemu_log_mask_and_addr(CPU_LOG_EXEC, addr,
                            "Chain %p [%d: " TARGET_FMT_lx "] %s\n",
                            tb->tc_ptr, cpu->cpu_index, addr,
