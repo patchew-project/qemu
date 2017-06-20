2122acc6e970f22ab245d96d0c61ea1adcc362f0
David Hildenbrand <david@redhat.com>
2017-06-20 14:35:53 +0200

    target/s390x: Implement idte instruction
    Let's keep it very simple for now and flush the complete tlb,
    we currently can't find the right entries in our tlb, we would have
    to store the used tables for each element.
    
    Signed-off-by: David Hildenbrand <david@redhat.com>
    Message-Id: <20170620123553.16186-4-david@redhat.com>

--- target/s390x/translate.c
+++ target/s390x/translate.c
@@ -2407,6 +2407,17 @@ static ExitStatus op_ipm(DisasContext *s, DisasOps *o)
 }
 
 #ifndef CONFIG_USER_ONLY
+static ExitStatus op_idte(DisasContext *s, DisasOps *o)
+{
+    TCGv_i32 m4;
+
+    check_privileged(s);
+    m4 = tcg_const_i32(get_field(s->fields, m4));
+    gen_helper_idte(cpu_env, o->in1, o->in2, m4);
+    tcg_temp_free_i32(m4);
+    return NO_EXIT;
+}
+
 static ExitStatus op_ipte(DisasContext *s, DisasOps *o)
 {
     TCGv_i32 m4;
