324225bd82f4f01f71be694f3225e03e9d51ef56
David Hildenbrand <david@redhat.com>
2017-06-22 11:41:49 +0200

    target/s390x: Indicate and check for local tlb clearing
    Let's allow to enable it for the qemu cpu model and correctly emulate
    it.
    
    Signed-off-by: David Hildenbrand <david@redhat.com>
    Message-Id: <20170622094151.28633-2-david@redhat.com>
    Reviewed-by: Richard Henderson <rth@twiddle.net>

--- target/s390x/cpu_models.c
+++ target/s390x/cpu_models.c
@@ -685,6 +685,7 @@ static void add_qemu_cpu_model_features(S390FeatBitmap fbm)
         S390_FEAT_GENERAL_INSTRUCTIONS_EXT,
         S390_FEAT_EXECUTE_EXT,
         S390_FEAT_STFLE_45,
+        S390_FEAT_LOCAL_TLB_CLEARING,
     };
     int i;
 
