4e9b1ce4c9c42ca138822593eb0bb6c72d5ec06b
Lluís Vilanova <vilanova@ac.upc.edu>
2017-06-28 17:09:39 +0300

    target/arm: [tcg, a64] Port to disas_log
    Incrementally paves the way towards using the generic instruction translation
    loop.
    
    Signed-off-by: Lluís Vilanova <vilanova@ac.upc.edu>
    Message-Id: <149865897948.17063.7243731047242702451.stgit@frigg.lan>
    Reviewed-by: Richard Henderson <rth@twiddle.net>

--- target/arm/translate-a64.c
+++ target/arm/translate-a64.c
@@ -11379,6 +11379,16 @@ static void aarch64_trblock_tb_stop(DisasContextBase *dcbase, CPUState *cpu)
     }
 }
 
+static void aarch64_trblock_disas_log(const DisasContextBase *dcbase,
+                                      CPUState *cpu)
+{
+    DisasContext *dc = container_of(dcbase, DisasContext, base);
+
+    qemu_log("IN: %s\n", lookup_symbol(dc->base.pc_first));
+    log_target_disas(cpu, dc->base.pc_first, dc->pc - dc->base.pc_first,
+                     4 | (bswap_code(dc->sctlr_b) ? 2 : 0));
+}
+
 void gen_intermediate_code_a64(DisasContextBase *dcbase, CPUState *cs,
                                TranslationBlock *tb)
 {
@@ -11475,9 +11485,7 @@ done_generating:
         qemu_log_in_addr_range(dc->base.pc_first)) {
         qemu_log_lock();
         qemu_log("----------------\n");
-        qemu_log("IN: %s\n", lookup_symbol(dc->base.pc_first));
-        log_target_disas(cs, dc->base.pc_first, dc->pc - dc->base.pc_first,
-                         4 | (bswap_code(dc->sctlr_b) ? 2 : 0));
+        aarch64_trblock_disas_log(&dc->base, cs);
         qemu_log("\n");
         qemu_log_unlock();
     }
