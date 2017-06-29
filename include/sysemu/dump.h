652c371ded599f26719a9b37b78f05a7442e48b2
Marc-André Lureau <marcandre.lureau@redhat.com>
2017-06-29 15:23:07 +0200

    dump: add vmcoreinfo ELF note
    Read the vmcoreinfo ELF PT_NOTE from guest memory when vmcoreinfo
    device provides the location, and write it as an ELF note in the dump.
    
    There are now 2 possible sources of phys_base information.
    
    (1) arch guessed value from arch_dump_info_get()
    (2) vmcoreinfo ELF note NUMBER(phys_base)= field
    
        NUMBER(phys_base) in vmcoreinfo has only been recently introduced
        in Linux 4.10 (401721ecd1dc "kexec: export the value of phys_base
        instead of symbol address").
    
    Since (2) has better chances to be accurate, the guessed value is
    replaced by the value from the vmcoreinfo ELF note.
    
    The phys_base value is stored in the same dump field locations as
    before, and may duplicate the information available in the vmcoreinfo
    ELF PT_NOTE. Crash tools should be prepared to handle this case.
    
    Signed-off-by: Marc-André Lureau <marcandre.lureau@redhat.com>
    Message-Id: <20170629132310.18865-5-marcandre.lureau@redhat.com>

--- include/sysemu/dump.h
+++ include/sysemu/dump.h
@@ -192,6 +192,8 @@ typedef struct DumpState {
                                   * this could be used to calculate
                                   * how much work we have
                                   * finished. */
+    uint8_t *vmcoreinfo;         /* ELF note content */
+    size_t vmcoreinfo_size;
 } DumpState;
 
 uint16_t cpu_to_dump16(DumpState *s, uint16_t val);
