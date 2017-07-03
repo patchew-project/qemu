0f55df802acca6c7c75a9b43e1d1029751304a22
Yang Zhong <yang.zhong@intel.com>
2017-07-03 18:12:17 +0800

    tcg: add the tcg-stub.c file into accel/stubs/
    If tcg is disabled, the functions in tcg-stub.c file will be called.
    This file is target-independent file, do not include any platform
    related stub functions into this file.
    
    Signed-off-by: Yang Zhong <yang.zhong@intel.com>
    Message-Id: <1499076743-15477-10-git-send-email-yang.zhong@intel.com>

---
+++ accel/stubs/tcg-stub.c
@@ -0,0 +1,80 @@
+/*
+ * QEMU TCG accelerator stub
+ *
+ * Copyright Red Hat, Inc. 2013
+ *
+ * Author: Paolo Bonzini     <pbonzini@redhat.com>
+ *
+ * This work is licensed under the terms of the GNU GPL, version 2 or later.
+ * See the COPYING file in the top-level directory.
+ *
+ */
+
+#include "qemu/osdep.h"
+#include "qemu-common.h"
+#include "cpu.h"
+#include "tcg/tcg.h"
+#include "exec/cpu-common.h"
+#include "exec/exec-all.h"
+#include "translate-all.h"
+#include "exec/cpu-all.h"
+
+
+void tb_flush(CPUState *cpu)
+{
+}
+
+void tb_invalidate_phys_page_fast(tb_page_addr_t start, int len)
+{
+}
+
+void tb_invalidate_phys_range(tb_page_addr_t start, tb_page_addr_t end)
+{
+}
+
+void tb_check_watchpoint(CPUState *cpu)
+{
+}
+
+TranslationBlock *tb_gen_code(CPUState *cpu,
+                              target_ulong pc, target_ulong cs_base,
+                              uint32_t flags, int cflags)
+{
+    return NULL;
+}
+
+void tlb_reset_dirty(CPUState *cpu, ram_addr_t start1, ram_addr_t length)
+{
+}
+
+void tlb_set_dirty(CPUState *cpu, target_ulong vaddr)
+{
+}
+
+bool cpu_restore_state(CPUState *cpu, uintptr_t searched_pc)
+{
+    return false;
+}
+
+void dump_exec_info(FILE *f, fprintf_function cpu_fprintf)
+{
+}
+
+void dump_opcount_info(FILE *f, fprintf_function cpu_fprintf)
+{
+}
+
+void cpu_loop_exit(CPUState *cpu)
+{
+    abort();
+}
+
+void cpu_loop_exit_noexc(CPUState *cpu)
+{
+    abort();
+}
+
+int cpu_exec(CPUState *cpu)
+{
+    abort();
+}
