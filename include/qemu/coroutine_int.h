b0b006b73820d78fd812093bc9c5cd38f133e9b5
Marc-André Lureau <marcandre.lureau@redhat.com>
2017-07-05 00:03:16 +0200

    coroutine: remove coroutine_fn from qemu_co_queue_run_restart()
    The function can be invoked from non-coroutine context.
    
    Signed-off-by: Marc-André Lureau <marcandre.lureau@redhat.com>
    Message-Id: <20170704220346.29244-6-marcandre.lureau@redhat.com>

--- include/qemu/coroutine_int.h
+++ include/qemu/coroutine_int.h
@@ -61,6 +61,6 @@ Coroutine *qemu_coroutine_new(void);
 void qemu_coroutine_delete(Coroutine *co);
 CoroutineAction qemu_coroutine_switch(Coroutine *from, Coroutine *to,
                                       CoroutineAction action);
-void coroutine_fn qemu_co_queue_run_restart(Coroutine *co);
+void qemu_co_queue_run_restart(Coroutine *co);
 
 #endif
