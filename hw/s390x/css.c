49ddd5c58d638f4fec2b40e371b5975eb7aa34f1
Fam Zheng <famz@redhat.com>
2017-07-04 14:43:32 +0800

    qdev: Add const qualifier to PropertyInfo definitions
    The remaining non-const ones are in e1000e which modifies description at
    runtime. They can be addressed separatedly.
    
    Signed-off-by: Fam Zheng <famz@redhat.com>
    Message-Id: <20170704064347.7022-6-famz@redhat.com>

--- hw/s390x/css.c
+++ hw/s390x/css.c
@@ -2029,7 +2029,7 @@ out:
     g_free(str);
 }
 
-PropertyInfo css_devid_propinfo = {
+const PropertyInfo css_devid_propinfo = {
     .name = "str",
     .description = "Identifier of an I/O device in the channel "
                    "subsystem, example: fe.1.23ab",
@@ -2037,7 +2037,7 @@ PropertyInfo css_devid_propinfo = {
     .set = set_css_devid,
 };
 
-PropertyInfo css_devid_ro_propinfo = {
+const PropertyInfo css_devid_ro_propinfo = {
     .name = "str",
     .description = "Read-only identifier of an I/O device in the channel "
                    "subsystem, example: fe.1.23ab",
