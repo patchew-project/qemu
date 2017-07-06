e600b26721280fbf2b19a7dd35157c25a3253c82
Paolo Bonzini <pbonzini@redhat.com>
2017-07-06 18:38:19 +0200

    block: make write-threshold thread-safe
    For simplicity, use bdrv_drained_begin/end to avoid concurrent
    writes to the write threshold, or reading it while it is being set.
    qmp_block_set_write_threshold is protected by the BQL.
    
    Signed-off-by: Paolo Bonzini <pbonzini@redhat.com>
    Message-Id: <20170706163828.24082-3-pbonzini@redhat.com>
    Reviewed-by: Eric Blake <eblake@redhat.com>

--- block/write-threshold.c
+++ block/write-threshold.c
@@ -112,7 +112,6 @@ void qmp_block_set_write_threshold(const char *node_name,
                                    Error **errp)
 {
     BlockDriverState *bs;
-    AioContext *aio_context;
 
     bs = bdrv_find_node(node_name);
     if (!bs) {
@@ -120,10 +119,8 @@ void qmp_block_set_write_threshold(const char *node_name,
         return;
     }
 
-    aio_context = bdrv_get_aio_context(bs);
-    aio_context_acquire(aio_context);
-
+    /* Avoid a concurrent write_threshold_disable.  */
+    bdrv_drained_begin(bs);
     bdrv_write_threshold_set(bs, threshold_bytes);
-
-    aio_context_release(aio_context);
+    bdrv_drained_end(bs);
 }
