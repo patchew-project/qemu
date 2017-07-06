75c3c7dba52a264ca1f2d3a952f70cbf3cc04daa
Paolo Bonzini <pbonzini@redhat.com>
2017-07-06 18:38:24 +0200

    block: do not acquire AioContext in check_to_replace_node
    The only caller does it already.
    
    Signed-off-by: Paolo Bonzini <pbonzini@redhat.com>
    Message-Id: <20170706163828.24082-8-pbonzini@redhat.com>

--- block.c
+++ block.c
@@ -4701,23 +4701,19 @@ bool bdrv_is_first_non_filter(BlockDriverState *candidate)
     return false;
 }
 
+/* Called with AioContext lock held.  */
 BlockDriverState *check_to_replace_node(BlockDriverState *parent_bs,
                                         const char *node_name, Error **errp)
 {
     BlockDriverState *to_replace_bs = bdrv_find_node(node_name);
-    AioContext *aio_context;
 
     if (!to_replace_bs) {
         error_setg(errp, "Node name '%s' not found", node_name);
         return NULL;
     }
 
-    aio_context = bdrv_get_aio_context(to_replace_bs);
-    aio_context_acquire(aio_context);
-
     if (bdrv_op_is_blocked(to_replace_bs, BLOCK_OP_TYPE_REPLACE, errp)) {
-        to_replace_bs = NULL;
-        goto out;
+        return NULL;
     }
 
     /* We don't want arbitrary node of the BDS chain to be replaced only the top
@@ -4727,12 +4723,9 @@ BlockDriverState *check_to_replace_node(BlockDriverState *parent_bs,
      */
     if (!bdrv_recurse_is_first_non_filter(parent_bs, to_replace_bs)) {
         error_setg(errp, "Only top most non filter can be replaced");
-        to_replace_bs = NULL;
-        goto out;
+        return NULL;
     }
 
-out:
-    aio_context_release(aio_context);
     return to_replace_bs;
 }
 
