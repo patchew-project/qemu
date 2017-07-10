5b80edd2ce1809487caf50552f8a2a1f588edc62
Thomas Huth <thuth@redhat.com>
2017-07-10 17:32:39 +0200

    pc-bios/s390-ccw: Add virtio-net driver code
    The driver provides the recv() and send() functions which will
    be required by SLOF's libnet code for receiving and sending
    packets.
    
    Signed-off-by: Thomas Huth <thuth@redhat.com>
    Message-Id: <1499700760-4777-10-git-send-email-thuth@redhat.com>

--- pc-bios/s390-ccw/virtio.c
+++ pc-bios/s390-ccw/virtio.c
@@ -150,7 +150,7 @@ static void vring_init(VRing *vr, VqInfo *info)
     debug_print_addr("init vr", vr);
 }
 
-static bool vring_notify(VRing *vr)
+bool vring_notify(VRing *vr)
 {
     vr->cookie = virtio_notify(vr->schid, vr->id, vr->cookie);
     return vr->cookie >= 0;
@@ -189,7 +189,7 @@ ulong get_second(void)
     return (get_clock() >> 12) / 1000000;
 }
 
-static int vr_poll(VRing *vr)
+int vr_poll(VRing *vr)
 {
     if (vr->used->idx == vr->used_idx) {
         vring_notify(vr);
@@ -266,6 +266,11 @@ void virtio_setup_ccw(VDev *vdev, uint32_t guestfeats[2])
     run_ccw(vdev, CCW_CMD_VDEV_RESET, NULL, 0);
 
     switch (vdev->senseid.cu_model) {
+    case VIRTIO_ID_NET:
+        vdev->nr_vqs = 2;
+        vdev->cmd_vr_idx = 0;
+        cfg_size = sizeof(vdev->config.net);
+        break;
     case VIRTIO_ID_BLOCK:
         vdev->nr_vqs = 1;
         vdev->cmd_vr_idx = 0;
