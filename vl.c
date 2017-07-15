63cfcba852a15153b236f9e4c20c4ff2dd677ce0
Gonglei <arei.gonglei@huawei.com>
2017-07-15 18:21:08 +0800

    cryptodev: add vhost-user as a new cryptodev backend
    Usage:
     -chardev socket,id=charcrypto0,path=/path/to/your/socket
     -object cryptodev-vhost-user,id=cryptodev0,chardev=charcrypto0
     -device virtio-crypto-pci,id=crypto0,cryptodev=cryptodev0
    
    Signed-off-by: Gonglei <arei.gonglei@huawei.com>
    Signed-off-by: Longpeng(Mike) <longpeng2@huawei.com>
    Signed-off-by: Zhoujian <jianjay.zhou@huawei.com>
    Message-Id: <1500114070-181004-2-git-send-email-arei.gonglei@huawei.com>

--- vl.c
+++ vl.c
@@ -2811,6 +2811,10 @@ static bool object_create_initial(const char *type)
         return false;
     }
 
+    if (g_str_equal(type, "cryptodev-vhost-user")) {
+        return false;
+    }
+
     /*
      * return false for concrete netfilters since
      * they depend on netdevs already existing
