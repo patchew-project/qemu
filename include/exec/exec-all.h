362fc54c4e959ddf71f06322c6189793435c9178
Richard Henderson <rth@twiddle.net>
2017-07-14 23:42:11 -1000

    tcg: Add generic DISAS_NORETURN
    This will allow some amount of cleanup to happen before
    switching the backends over to enum DisasJumpType.
    
    Signed-off-by: Richard Henderson <rth@twiddle.net>
    Message-Id: <20170715094243.28371-3-rth@twiddle.net>

--- include/exec/exec-all.h
+++ include/exec/exec-all.h
@@ -40,6 +40,7 @@ typedef ram_addr_t tb_page_addr_t;
 #define DISAS_JUMP    1 /* only pc was modified dynamically */
 #define DISAS_UPDATE  2 /* cpu state was modified dynamically */
 #define DISAS_TB_JUMP 3 /* only pc was modified statically */
+#define DISAS_NORETURN 4 /* the tb has already been exited */
 
 #include "qemu/log.h"
 
