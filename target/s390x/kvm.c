5f26ca121831ff97468783af560d41536bdfa93a
Cornelia Huck <cohuck@redhat.com>
2017-07-18 16:24:54 +0200

    s390x/kvm: msi route fixup for non-pci
    If we don't provide pci, we cannot have a pci device for which we
    have to translate to adapter routes: just return -ENODEV.
    
    Signed-off-by: Cornelia Huck <cohuck@redhat.com>
    Message-Id: <20170718142455.32676-9-cohuck@redhat.com>

--- target/s390x/kvm.c
+++ target/s390x/kvm.c
@@ -2424,22 +2424,27 @@ int kvm_arch_fixup_msi_route(struct kvm_irq_routing_entry *route,
     uint32_t idx = data >> ZPCI_MSI_VEC_BITS;
     uint32_t vec = data & ZPCI_MSI_VEC_MASK;
 
-    pbdev = s390_pci_find_dev_by_idx(s390_get_phb(), idx);
-    if (!pbdev) {
-        DPRINTF("add_msi_route no dev\n");
+    if (s390_has_feat(S390_FEAT_ZPCI)) {
+        pbdev = s390_pci_find_dev_by_idx(s390_get_phb(), idx);
+        if (!pbdev) {
+            DPRINTF("add_msi_route no dev\n");
+            return -ENODEV;
+        }
+
+        pbdev->routes.adapter.ind_offset = vec;
+
+        route->type = KVM_IRQ_ROUTING_S390_ADAPTER;
+        route->flags = 0;
+        route->u.adapter.summary_addr = pbdev->routes.adapter.summary_addr;
+        route->u.adapter.ind_addr = pbdev->routes.adapter.ind_addr;
+        route->u.adapter.summary_offset = pbdev->routes.adapter.summary_offset;
+        route->u.adapter.ind_offset = pbdev->routes.adapter.ind_offset;
+        route->u.adapter.adapter_id = pbdev->routes.adapter.adapter_id;
+        return 0;
+    } else {
+        DPRINTF("fixup_msi_route on non-pci machine?!\n");
         return -ENODEV;
     }
-
-    pbdev->routes.adapter.ind_offset = vec;
-
-    route->type = KVM_IRQ_ROUTING_S390_ADAPTER;
-    route->flags = 0;
-    route->u.adapter.summary_addr = pbdev->routes.adapter.summary_addr;
-    route->u.adapter.ind_addr = pbdev->routes.adapter.ind_addr;
-    route->u.adapter.summary_offset = pbdev->routes.adapter.summary_offset;
-    route->u.adapter.ind_offset = pbdev->routes.adapter.ind_offset;
-    route->u.adapter.adapter_id = pbdev->routes.adapter.adapter_id;
-    return 0;
 }
 
 int kvm_arch_add_msi_route_post(struct kvm_irq_routing_entry *route,
