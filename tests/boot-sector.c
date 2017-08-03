a509c93771d264469e61e5e1758e02b692202496
Thomas Huth <thuth@redhat.com>
2017-08-03 15:30:19 +0200

    tests/pxe: Check virtio-net-ccw on s390x
    Now that we've got a firmware that can do TFTP booting on s390x (i.e.
    the pc-bios/s390-netboot.img), we can enable the PXE tester for this
    architecture, too.
    
    Signed-off-by: Thomas Huth <thuth@redhat.com>
    Message-Id: <1501767019-4989-1-git-send-email-thuth@redhat.com>

--- tests/boot-sector.c
+++ tests/boot-sector.c
@@ -21,6 +21,7 @@
 #define SIGNATURE 0xdead
 #define SIGNATURE_OFFSET 0x10
 #define BOOT_SECTOR_ADDRESS 0x7c00
+#define SIGNATURE_ADDR (BOOT_SECTOR_ADDRESS + SIGNATURE_OFFSET)
 
 /* Boot sector code: write SIGNATURE into memory,
  * then halt.
@@ -73,6 +74,7 @@ int boot_sector_init(char *fname)
 {
     int fd, ret;
     size_t len = sizeof boot_sector;
+    const char *arch = qtest_get_arch();
 
     fd = mkstemp(fname);
     if (fd < 0) {
@@ -81,10 +83,27 @@ int boot_sector_init(char *fname)
     }
 
     /* For Open Firmware based system, we can use a Forth script instead */
-    if (strcmp(qtest_get_arch(), "ppc64") == 0) {
+    if (g_str_equal(arch, "ppc64")) {
         len = sprintf((char *)boot_sector, "\\ Bootscript\n%x %x c! %x %x c!\n",
-                LOW(SIGNATURE), BOOT_SECTOR_ADDRESS + SIGNATURE_OFFSET,
-                HIGH(SIGNATURE), BOOT_SECTOR_ADDRESS + SIGNATURE_OFFSET + 1);
+                LOW(SIGNATURE), SIGNATURE_ADDR,
+                HIGH(SIGNATURE), SIGNATURE_ADDR + 1);
+    } else if (g_str_equal(arch, "s390x")) {
+        /* For s390x, fake a kernel signature */
+        const uint8_t psw[] = {
+            0x00, 0x08, 0x00, 0x00, 0x80, 0x01, 0x00, 0x00
+        };
+        const uint8_t code[] = {
+            0xa7, 0xf4, 0x00, 0x0a,     /* j 0x10010 */
+            0x00, 0x00, 0x00, 0x00,
+            'S', '3', '9', '0',
+            'E', 'P', 0x00, 0x01,
+            0xa7, 0x38, HIGH(SIGNATURE_ADDR), LOW(SIGNATURE_ADDR), /* lhi r3 */
+            0xa7, 0x48, LOW(SIGNATURE), HIGH(SIGNATURE),    /* lhi r4,0xadde */
+            0x40, 0x40, 0x30, 0x00,     /* sth r4,0(r3) */
+            0xa7, 0xf4, 0xff, 0xfa      /* j 0x10010 */
+        };
+        memcpy(boot_sector, psw, 8);
+        memcpy(&boot_sector[0x10000], code, sizeof(code));
     }
 
     ret = write(fd, boot_sector, len);
