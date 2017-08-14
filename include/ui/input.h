ecf4c226f6fdb964b3ee3f9858d43bce1ca291b1
Daniel P. Berrange <berrange@redhat.com>
2017-08-14 13:49:24 +0100

    ui: convert cocoa frontend to keycodemapdb
    Replace the mac_to_qkeycode_map table with automatically
    generated table.
    
    Signed-off-by: Daniel P. Berrange <berrange@redhat.com>
    Message-Id: <20170814124927.30134-10-berrange@redhat.com>

--- include/ui/input.h
+++ include/ui/input.h
@@ -71,6 +71,9 @@ void qemu_remove_mouse_mode_change_notifier(Notifier *notify);
 extern const guint qemu_input_map_linux_to_qcode_len;
 extern const guint16 qemu_input_map_linux_to_qcode[];
 
+extern const guint qemu_input_map_osx_to_qcode_len;
+extern const guint16 qemu_input_map_osx_to_qcode[];
+
 extern const guint qemu_input_map_qcode_to_adb_len;
 extern const guint16 qemu_input_map_qcode_to_adb[];
 
