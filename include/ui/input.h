b299ef6c1808cb0362932e3fc9d4598ee0fddcba
Daniel P. Berrange <berrange@redhat.com>
2017-08-15 10:36:13 +0100

    ui: convert the SDL2 frontend to keycodemapdb
    The SDL2 scancodes are conveniently identical to the USB
    scancodes. Replace the sdl2_scancode_to_qcode table with
    an automatically generated table.
    
    Missing entries in sdl2_scancode_to_qcode now fixed:
    
      - 0x32 -> Q_KEY_CODE_BACKSLASH
      - 0x66 -> Q_KEY_CODE_POWER
      - 0x67 -> Q_KEY_CODE_KP_EQUALS
      - 0x74 -> Q_KEY_CODE_OPEN
      - 0x77 -> Q_KEY_CODE_FRONT
      - 0x7f -> Q_KEY_CODE_AUDIOMUTE
      - 0x80 -> Q_KEY_CODE_VOLUMEUP
      - 0x81 -> Q_KEY_CODE_VOLUMEDOWN
      - 0x85 -> Q_KEY_CODE_KP_COMMA
      - 0x87 -> Q_KEY_CODE_RO
      - 0x89 -> Q_KEY_CODE_YEN
      - 0x8a -> Q_KEY_CODE_HENKAN
      - 0x93 -> Q_KEY_CODE_HIRAGANA
      - 0xe8 -> Q_KEY_CODE_AUDIOPLAY
      - 0xe9 -> Q_KEY_CODE_AUDIOSTOP
      - 0xea -> Q_KEY_CODE_AUDIOPREV
      - 0xeb -> Q_KEY_CODE_AUDIONEXT
      - 0xed -> Q_KEY_CODE_VOLUMEUP
      - 0xee -> Q_KEY_CODE_VOLUMEDOWN
      - 0xef -> Q_KEY_CODE_AUDIOMUTE
      - 0xf1 -> Q_KEY_CODE_AC_BACK
      - 0xf2 -> Q_KEY_CODE_AC_FORWARD
      - 0xf3 -> Q_KEY_CODE_STOP
      - 0xf4 -> Q_KEY_CODE_FIND
      - 0xf8 -> Q_KEY_CODE_SLEEP
      - 0xfa -> Q_KEY_CODE_AC_REFRESH
      - 0xfb -> Q_KEY_CODE_CALCULATOR
    
    And some mistakes corrected:
    
      - 0x65 -> Q_KEY_CODE_COMPOSE, not duplicating Q_KEY_CODE_MENU
    
    Signed-off-by: Daniel P. Berrange <berrange@redhat.com>
    Message-Id: <20170815093615.16453-11-berrange@redhat.com>

--- include/ui/input.h
+++ include/ui/input.h
@@ -98,4 +98,7 @@ extern const guint16 qemu_input_map_qcode_to_sun[];
 extern const guint qemu_input_map_qnum_to_qcode_len;
 extern const guint16 qemu_input_map_qnum_to_qcode[];
 
+extern const guint qemu_input_map_usb_to_qcode_len;
+extern const guint16 qemu_input_map_usb_to_qcode[];
+
 #endif /* INPUT_H */
