95078b7bd094a4db947dc7a35227c27fdd5e1e42
Halil Pasic <pasic@linux.vnet.ibm.com>
2017-08-30 18:36:03 +0200

    s390x/css: be more consistent if broken beyond repair
    If we detect that the internally manged state of the subchannel
    is broken beyond repair while in do_subchannel_work in case of
    virtual we just abort the operation and pretend all went well,
    while in case of pass-through we honor the situation with -ENODEV
    which results in cc 3 for the instruction whose handler triggered
    the call.
    
    Let's be consistent on this and do the -ENODEV also for virtual
    subchannels.
    
    Signed-off-by: Halil Pasic <pasic@linux.vnet.ibm.com>
    Acked-by: Pierre Morel<pmorel@linux.vnet.ibm.com>
    Message-Id: <20170830163609.50260-4-pasic@linux.vnet.ibm.com>

--- hw/s390x/css.c
+++ hw/s390x/css.c
@@ -1078,7 +1078,7 @@ int do_subchannel_work_virtual(SubchDev *sch)
         sch_handle_start_func_virtual(sch);
     } else {
         /* Cannot happen. */
-        return 0;
+        return -ENODEV;
     }
     css_inject_io_interrupt(sch);
     return 0;
