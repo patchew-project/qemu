0a7aa7f606011769d210ce55204cdea13eaa0523
David Hildenbrand <david@redhat.com>
2017-08-30 19:05:59 +0200

    target/s390x: tcg_s390_program_interrupt() will never return
    The assert should hold in both scenarios.
    
    Signed-off-by: David Hildenbrand <david@redhat.com>
    Message-Id: <20170830170601.15855-10-david@redhat.com>

--- target/s390x/interrupt.c
+++ target/s390x/interrupt.c
@@ -32,9 +32,8 @@ static void tcg_s390_program_interrupt(CPUS390XState *env, uint32_t code,
 #ifdef CONFIG_TCG
     trigger_pgm_exception(env, code, ilen);
     cpu_loop_exit(CPU(s390_env_get_cpu(env)));
-#else
-    g_assert_not_reached();
 #endif
+    g_assert_not_reached();
 }
 
 void program_interrupt(CPUS390XState *env, uint32_t code, int ilen)
