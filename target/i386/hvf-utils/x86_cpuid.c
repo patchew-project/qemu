7b89baf0e89949afc15ef5a4734d8a9825ee16a6
Sergio Andres Gomez Del Real <sergio.g.delreal@gmail.com>
2017-09-04 22:54:46 -0500

    hvf: fix licensing issues; isolate task handling code (GPL v2-only)
    This patch replaces the license header for those files that were either
    GPL v2-or-v3, or GPL v2-only; the replacing license is GPL v2-or-later.
    The code for task switching/handling, which is derived from KVM and
    hence is GPL v2-only, is isolated in the new files (with this license)
    x86_task.c/.h
    
    Signed-off-by: Sergio Andres Gomez Del Real <Sergio.G.DelReal@gmail.com>
    Message-Id: <20170905035457.3753-4-Sergio.G.DelReal@gmail.com>

--- target/i386/hvf-utils/x86_cpuid.c
+++ target/i386/hvf-utils/x86_cpuid.c
@@ -4,18 +4,18 @@
  *  Copyright (c) 2003 Fabrice Bellard
  *  Copyright (c) 2017 Google Inc.
  *
- * This library is free software; you can redistribute it and/or
+ * This program is free software; you can redistribute it and/or
  * modify it under the terms of the GNU Lesser General Public
  * License as published by the Free Software Foundation; either
  * version 2 of the License, or (at your option) any later version.
  *
- * This library is distributed in the hope that it will be useful,
+ * This program is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
  * Lesser General Public License for more details.
  *
  * You should have received a copy of the GNU Lesser General Public
- * License along with this library; if not, see <http://www.gnu.org/licenses/>.
+ * License along with this program; if not, see <http://www.gnu.org/licenses/>.
  *
  * cpuid
  */
