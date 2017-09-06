7bc90b8b3e395fc489bee6970cd06f5c8df6b6a8
Richard W.M. Jones <rjones@redhat.com>
2017-09-06 09:50:06 +0100

    qemu-options: Document the -drive locking parameter.
    Commit 16b48d5d66d2 ("file-posix: Add 'locking' option") added this
    option, but as it was not documented in the -help output it was not
    easily possible to tell if a particular qemu binary supports it.
    
    Signed-off-by: Richard W.M. Jones <rjones@redhat.com>
    Message-Id: <20170906085006.26983-1-rjones@redhat.com>
    Reviewed-by: Daniel P. Berrange <berrange@redhat.com>

--- qemu-options.hx
+++ qemu-options.hx
@@ -796,7 +796,7 @@ DEF("drive", HAS_ARG, QEMU_OPTION_drive,
     "       [,cache=writethrough|writeback|none|directsync|unsafe][,format=f]\n"
     "       [,serial=s][,addr=A][,rerror=ignore|stop|report]\n"
     "       [,werror=ignore|stop|report|enospc][,id=name][,aio=threads|native]\n"
-    "       [,readonly=on|off][,copy-on-read=on|off]\n"
+    "       [,readonly=on|off][,copy-on-read=on|off][,locking=off|auto|on]\n"
     "       [,discard=ignore|unmap][,detect-zeroes=on|off|unmap]\n"
     "       [[,bps=b]|[[,bps_rd=r][,bps_wr=w]]]\n"
     "       [[,iops=i]|[[,iops_rd=r][,iops_wr=w]]]\n"
