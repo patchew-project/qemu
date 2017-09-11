8c2b86db222bad60fd3f7b5b5e701ad825a681cb
Laurent Vivier <lvivier@redhat.com>
2017-09-11 13:00:37 +0200

    hmp: fix "dump-quest-memory" segfault (ppc)
    Commit fd5d23babf (hmp: fix "dump-quest-memory" segfault)
    fixes the problem for i386, do the same for ppc.
    
    Running QEMU with
        qemu-system-ppc64 -M none -nographic -m 256
    and executing
        dump-guest-memory /dev/null 0 8192
    results in segfault
    
    Fix by checking if we have CPU.
    
    Signed-off-by: Laurent Vivier <lvivier@redhat.com>
    Message-Id: <20170911110037.6567-1-lvivier@redhat.com>
    Reviewed-by: Greg Kurz <groug@kaod.org>
    Reviewed-by: Thomas Huth <thuth@redhat.com>

--- target/ppc/arch_dump.c
+++ target/ppc/arch_dump.c
@@ -224,17 +224,22 @@ typedef struct NoteFuncDescStruct NoteFuncDesc;
 int cpu_get_dump_info(ArchDumpInfo *info,
                       const struct GuestPhysBlockList *guest_phys_blocks)
 {
-    PowerPCCPU *cpu = POWERPC_CPU(first_cpu);
-    PowerPCCPUClass *pcc = POWERPC_CPU_GET_CLASS(cpu);
-
     info->d_machine = PPC_ELF_MACHINE;
     info->d_class = ELFCLASS;
 
-    if ((*pcc->interrupts_big_endian)(cpu)) {
-        info->d_endian = ELFDATA2MSB;
+    if (first_cpu) {
+        PowerPCCPU *cpu = POWERPC_CPU(first_cpu);
+        PowerPCCPUClass *pcc = POWERPC_CPU_GET_CLASS(cpu);
+
+        if ((*pcc->interrupts_big_endian)(cpu)) {
+            info->d_endian = ELFDATA2MSB;
+        } else {
+            info->d_endian = ELFDATA2LSB;
+        }
     } else {
-        info->d_endian = ELFDATA2LSB;
+        info->d_endian = ELFDATA2MSB;
     }
+
     /* 64KB is the max page size for pseries kernel */
     if (strncmp(object_get_typename(qdev_get_machine()),
                 "pseries-", 8) == 0) {
