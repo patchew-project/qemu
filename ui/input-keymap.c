c8d95241f3f69d62b58c2d98d020ddcc2276a7a7
Daniel P. Berrange <berrange@redhat.com>
2017-09-12 13:37:43 +0100

    ui: convert GTK and SDL1 frontends to keycodemapdb
    The x_keycode_to_pc_keycode and evdev_keycode_to_pc_keycode
    tables are replaced with automatically generated tables.
    In addition the X11 heuristics are improved to detect running
    on XQuartz and XWin X11 servers, to activate the correct OS-X
    and Win32 keycode maps.
    
    Signed-off-by: Daniel P. Berrange <berrange@redhat.com>
    Message-Id: <20170912123744.14730-12-berrange@redhat.com>

--- ui/input-keymap.c
+++ ui/input-keymap.c
@@ -5,6 +5,7 @@
 
 #include "standard-headers/linux/input.h"
 
+#include "ui/input-keymap-atset1-to-qcode.c"
 #include "ui/input-keymap-linux-to-qcode.c"
 #include "ui/input-keymap-osx-to-qcode.c"
 #include "ui/input-keymap-qcode-to-adb.c"
@@ -16,6 +17,12 @@
 #include "ui/input-keymap-qcode-to-sun.c"
 #include "ui/input-keymap-qnum-to-qcode.c"
 #include "ui/input-keymap-usb-to-qcode.c"
+#include "ui/input-keymap-win32-to-qcode.c"
+#include "ui/input-keymap-x11-to-qcode.c"
+#include "ui/input-keymap-xorgevdev-to-qcode.c"
+#include "ui/input-keymap-xorgkbd-to-qcode.c"
+#include "ui/input-keymap-xorgxquartz-to-qcode.c"
+#include "ui/input-keymap-xorgxwin-to-qcode.c"
 
 int qemu_input_linux_to_qcode(unsigned int lnx)
 {
