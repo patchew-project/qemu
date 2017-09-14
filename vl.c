54a6420e6f6ca2a6c3302ff540470178114d51c3
Peter Xu <peterx@redhat.com>
2017-09-14 15:50:28 +0800

    monitor: unify global init
    There are many places for monitor init its globals, at least:
    
    - monitor_init_qmp_commands() at the very beginning
    - single function to init monitor_lock
    - in the first entry of monitor_init() using "is_first_init"
    
    Unify them a bit.
    
    Signed-off-by: Peter Xu <peterx@redhat.com>
    Message-Id: <1505375436-28439-8-git-send-email-peterx@redhat.com>

--- vl.c
+++ vl.c
@@ -3129,7 +3129,6 @@ int main(int argc, char **argv, char **envp)
     qemu_init_exec_dir(argv[0]);
 
     module_call_init(MODULE_INIT_QOM);
-    monitor_init_qmp_commands();
 
     qemu_add_opts(&qemu_drive_opts);
     qemu_add_drive_opts(&qemu_legacy_drive_opts);
@@ -4666,6 +4665,8 @@ int main(int argc, char **argv, char **envp)
     default_drive(default_sdcard, snapshot, IF_SD, 0, SD_OPTS);
 
     parse_numa_opts(current_machine);
+
+    monitor_init_globals();
 
     if (qemu_opts_foreach(qemu_find_opts("mon"),
                           mon_init_func, NULL, NULL)) {
