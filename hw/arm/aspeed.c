5236c524644a26403a08ef71b061253be1d52a09
Cédric Le Goater <clg@kaod.org>
2017-09-19 13:26:45 +0200

    aspeed: add the pc9552 chips to the witherspoon machine
    The pca9552 LED blinkers on the Witherspoon machine are used for leds
    but also as GPIOs to control fans and GPUs.
    
    Signed-off-by: Cédric Le Goater <clg@kaod.org>
    Message-Id: <20170919112645.8574-6-clg@kaod.org>

--- hw/arm/aspeed.c
+++ hw/arm/aspeed.c
@@ -386,6 +386,8 @@ static void witherspoon_bmc_i2c_init(AspeedBoardState *bmc)
 {
     AspeedSoCState *soc = &bmc->soc;
 
+    i2c_create_slave(aspeed_i2c_get_bus(DEVICE(&soc->i2c), 3), "pca9552", 0x60);
+
     i2c_create_slave(aspeed_i2c_get_bus(DEVICE(&soc->i2c), 4), "tmp423", 0x4c);
     i2c_create_slave(aspeed_i2c_get_bus(DEVICE(&soc->i2c), 5), "tmp423", 0x4c);
 
@@ -397,6 +399,8 @@ static void witherspoon_bmc_i2c_init(AspeedBoardState *bmc)
 
     aspeed_i2c_add_eeprom(aspeed_i2c_get_bus(DEVICE(&soc->i2c), 11), 0x51,
                           8 * 1024);
+    i2c_create_slave(aspeed_i2c_get_bus(DEVICE(&soc->i2c), 11), "pca9552",
+                     0x60);
 }
 
 static void witherspoon_bmc_init(MachineState *machine)
