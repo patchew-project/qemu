225d46b5cd370201d95d54cd4604ab8ddc6d27be
David Hildenbrand <david@redhat.com>
2017-09-26 20:33:15 +0200

    s390x/tcg: make lura(g) use the new _real mmu.
    Looks like, lurag was not loading 64bit but only 32bit.
    
    As we properly handle the return address now, we can drop
    potential_page_fault().
    
    Signed-off-by: David Hildenbrand <david@redhat.com>
    Message-Id: <20170926183318.12995-4-david@redhat.com>
    Reviewed-by: Thomas Huth <thuth@redhat.com>
    Reviewed-by: Richard Henderson <richard.henderson@linaro.org>

--- target/s390x/translate.c
+++ target/s390x/translate.c
@@ -2915,7 +2915,6 @@ static ExitStatus op_lpq(DisasContext *s, DisasOps *o)
 static ExitStatus op_lura(DisasContext *s, DisasOps *o)
 {
     check_privileged(s);
-    potential_page_fault(s);
     gen_helper_lura(o->out, cpu_env, o->in2);
     return NO_EXIT;
 }
@@ -2923,7 +2922,6 @@ static ExitStatus op_lura(DisasContext *s, DisasOps *o)
 static ExitStatus op_lurag(DisasContext *s, DisasOps *o)
 {
     check_privileged(s);
-    potential_page_fault(s);
     gen_helper_lurag(o->out, cpu_env, o->in2);
     return NO_EXIT;
 }
