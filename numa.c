a4135362d930055ce3f50147b7e9f9bf2a47efd1
Philippe Mathieu-Daudé <f4bug@amsat.org>
2017-10-06 20:50:15 -0300

    numa: use g_new() family of functions
    Signed-off-by: Philippe Mathieu-Daudé <f4bug@amsat.org>
    Message-Id: <20171006235023.11952-81-f4bug@amsat.org>

--- numa.c
+++ numa.c
@@ -66,7 +66,7 @@ void numa_set_mem_node_id(ram_addr_t addr, uint64_t size, uint32_t node)
         return;
     }
 
-    range = g_malloc0(sizeof(*range));
+    range = g_new0(struct numa_addr_range, 1);
     range->mem_start = addr;
     range->mem_end = addr + size - 1;
     QLIST_INSERT_HEAD(&numa_info[node].addr, range, entry);
