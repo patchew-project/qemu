262c35522cc21c82098885beb0dc1071a488105e
Marc-André Lureau <marcandre.lureau@redhat.com>
2017-10-06 20:49:49 -0300

    KVM: use g_new() family of functions
    Signed-off-by: Marc-André Lureau <marcandre.lureau@redhat.com>
    Signed-off-by: Philippe Mathieu-Daudé <f4bug@amsat.org>
    [PMD: more changes]
    Message-Id: <20171006235023.11952-55-f4bug@amsat.org>
    Reviewed-by: Gabriel Somlo <gsomlo@gmail.com>

--- accel/kvm/kvm-all.c
+++ accel/kvm/kvm-all.c
@@ -278,7 +278,7 @@ int kvm_destroy_vcpu(CPUState *cpu)
         goto err;
     }
 
-    vcpu = g_malloc0(sizeof(*vcpu));
+    vcpu = g_new0(struct KVMParkedVcpu, 1);
     vcpu->vcpu_id = kvm_arch_vcpu_id(cpu);
     vcpu->kvm_fd = cpu->kvm_fd;
     QLIST_INSERT_HEAD(&kvm_state->kvm_parked_vcpus, vcpu, node);
@@ -865,7 +865,7 @@ void kvm_memory_listener_register(KVMState *s, KVMMemoryListener *kml,
 {
     int i;
 
-    kml->slots = g_malloc0(s->nr_slots * sizeof(KVMSlot));
+    kml->slots = g_new0(KVMSlot, s->nr_slots);
     kml->as_id = as_id;
 
     for (i = 0; i < s->nr_slots; i++) {
@@ -1129,7 +1129,7 @@ int kvm_irqchip_send_msi(KVMState *s, MSIMessage msg)
             return virq;
         }
 
-        route = g_malloc0(sizeof(KVMMSIRoute));
+        route = g_new0(KVMMSIRoute, 1);
         route->kroute.gsi = virq;
         route->kroute.type = KVM_IRQ_ROUTING_MSI;
         route->kroute.flags = 0;
@@ -2243,7 +2243,7 @@ int kvm_insert_breakpoint(CPUState *cpu, target_ulong addr,
             return 0;
         }
 
-        bp = g_malloc(sizeof(struct kvm_sw_breakpoint));
+        bp = g_new(struct kvm_sw_breakpoint, 1);
         bp->pc = addr;
         bp->use_count = 1;
         err = kvm_arch_insert_sw_breakpoint(cpu, bp);
