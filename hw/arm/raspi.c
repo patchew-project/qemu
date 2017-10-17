42fd12caacb18ca1a20cd95077bac47eb1b3f872
Alistair Francis <alistair.francis@xilinx.com>
2017-10-17 15:31:16 -0700

    raspi: Specify the valid CPUs
    List all possible valid CPU options.
    
    Signed-off-by: Alistair Francis <alistair.francis@xilinx.com>
    Reviewed-by: Philippe Mathieu-Daudé <f4bug@amsat.org>
    Message-Id: <041506a5827f2764886b45c6d74a35666ca72af6.1508279421.git.alistair.francis@xilinx.com>

--- hw/arm/raspi.c
+++ hw/arm/raspi.c
@@ -158,6 +158,11 @@ static void raspi2_init(MachineState *machine)
     setup_boot(machine, 2, machine->ram_size - vcram_size);
 }
 
+static const char *raspi2_valid_cpus[] = {
+                                            ARM_CPU_TYPE_NAME("cortex-a7"),
+                                            NULL
+                                         };
+
 static void raspi2_machine_init(MachineClass *mc)
 {
     mc->desc = "Raspberry Pi 2";
@@ -169,5 +174,7 @@ static void raspi2_machine_init(MachineClass *mc)
     mc->max_cpus = BCM2836_NCPUS;
     mc->default_ram_size = 1024 * 1024 * 1024;
     mc->ignore_memory_transaction_failures = true;
+    mc->default_cpu_type = ARM_CPU_TYPE_NAME("cortex-a7");
+    mc->valid_cpu_types = raspi2_valid_cpus;
 };
 DEFINE_MACHINE("raspi2", raspi2_machine_init)
