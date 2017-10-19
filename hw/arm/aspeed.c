4210fb218c4e5458bebdc20fb6d8816850f352cf
Cédric Le Goater <clg@kaod.org>
2017-10-19 17:12:43 +0200

    aspeed: remove ignore_memory_transaction_failures all boards
    Now that we have a dummy ROM device catching the invalid writes, we
    can remove this flag and work on fixing the current firwmares still
    accessing unimplemented addresses.
    
    Signed-off-by: Cédric Le Goater <clg@kaod.org>
    Message-Id: <20171019151249.13663-3-clg@kaod.org>
    Reviewed-by: Peter Maydell <peter.maydell@linaro.org>

--- hw/arm/aspeed.c
+++ hw/arm/aspeed.c
@@ -288,7 +288,6 @@ static void palmetto_bmc_class_init(ObjectClass *oc, void *data)
     mc->no_floppy = 1;
     mc->no_cdrom = 1;
     mc->no_parallel = 1;
-    mc->ignore_memory_transaction_failures = true;
 }
 
 static const TypeInfo palmetto_bmc_type = {
@@ -321,7 +320,6 @@ static void ast2500_evb_class_init(ObjectClass *oc, void *data)
     mc->no_floppy = 1;
     mc->no_cdrom = 1;
     mc->no_parallel = 1;
-    mc->ignore_memory_transaction_failures = true;
 }
 
 static const TypeInfo ast2500_evb_type = {
@@ -346,7 +344,6 @@ static void romulus_bmc_class_init(ObjectClass *oc, void *data)
     mc->no_floppy = 1;
     mc->no_cdrom = 1;
     mc->no_parallel = 1;
-    mc->ignore_memory_transaction_failures = true;
 }
 
 static const TypeInfo romulus_bmc_type = {
