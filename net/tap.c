072514d34b93616bba79679a31e8e877f19d1714
Daniel P. Berrange <berrange@redhat.com>
2017-10-27 09:55:48 +0100

    net: detect errors from probing vnet hdr flag for TAP devices
    When QEMU sets up a tap based network device backend, it mostly ignores errors
    reported from various ioctl() calls it makes, assuming the TAP file descriptor
    is valid. This assumption can easily be violated when the user is passing in a
    pre-opened file descriptor. At best, the ioctls may fail with a -EBADF, but if
    the user passes in a bogus FD number that happens to clash with a FD number that
    QEMU has opened internally for another reason, a wide variety of errnos may
    result, as the TUNGETIFF ioctl number may map to a completely different command
    on a different type of file.
    
    By ignoring all these errors, QEMU sets up a zombie network backend that will
    never pass any data. Even worse, when QEMU shuts down, or that network backend
    is hot-removed, it will close this bogus file descriptor, which could belong to
    another QEMU device backend.
    
    There's no obvious guaranteed reliable way to detect that a FD genuinely is a
    TAP device, as opposed to a UNIX socket, or pipe, or something else. Checking
    the errno from probing vnet hdr flag though, does catch the big common cases.
    ie calling TUNGETIFF will return EBADF for an invalid FD, and ENOTTY when FD is
    a UNIX socket, or pipe which catches accidental collisions with FDs used for
    stdio, or monitor socket.
    
    Previously the example below where bogus fd 9 collides with the FD used for the
    chardev saw:
    
    $ ./x86_64-softmmu/qemu-system-x86_64 -netdev tap,id=hostnet0,fd=9 \
      -chardev socket,id=charchannel0,path=/tmp/qga,server,nowait \
      -monitor stdio -vnc :0
    qemu-system-x86_64: -netdev tap,id=hostnet0,fd=9: TUNGETIFF ioctl() failed: Inappropriate ioctl for device
    TUNSETOFFLOAD ioctl() failed: Bad address
    QEMU 2.9.1 monitor - type 'help' for more information
    (qemu) Warning: netdev hostnet0 has no peer
    
    which gives a running QEMU with a zombie network backend.
    
    With this change applied we get an error message and QEMU immediately exits
    before carrying on and making a bigger disaster:
    
    $ ./x86_64-softmmu/qemu-system-x86_64 -netdev tap,id=hostnet0,fd=9 \
      -chardev socket,id=charchannel0,path=/tmp/qga,server,nowait \
      -monitor stdio -vnc :0
    qemu-system-x86_64: -netdev tap,id=hostnet0,vhost=on,fd=9: Unable to query TUNGETIFF on FD 9: Inappropriate ioctl for device
    
    Reported-by: Dr. David Alan Gilbert <dgilbert@redhat.com>
    Signed-off-by: Daniel P. Berrange <berrange@redhat.com>
    Message-Id: <20171027085548.3472-1-berrange@redhat.com>
    Tested-by: Dr. David Alan Gilbert <dgilbert@redhat.com>

--- net/tap.c
+++ net/tap.c
@@ -592,7 +592,11 @@ int net_init_bridge(const Netdev *netdev, const char *name,
     }
 
     fcntl(fd, F_SETFL, O_NONBLOCK);
-    vnet_hdr = tap_probe_vnet_hdr(fd);
+    vnet_hdr = tap_probe_vnet_hdr(fd, errp);
+    if (vnet_hdr < 0) {
+        close(fd);
+        return -1;
+    }
     s = net_tap_fd_init(peer, "bridge", name, fd, vnet_hdr);
 
     snprintf(s->nc.info_str, sizeof(s->nc.info_str), "helper=%s,br=%s", helper,
@@ -779,7 +783,11 @@ int net_init_tap(const Netdev *netdev, const char *name,
 
         fcntl(fd, F_SETFL, O_NONBLOCK);
 
-        vnet_hdr = tap_probe_vnet_hdr(fd);
+        vnet_hdr = tap_probe_vnet_hdr(fd, errp);
+        if (vnet_hdr < 0) {
+            close(fd);
+            return -1;
+        }
 
         net_init_tap_one(tap, peer, "tap", name, NULL,
                          script, downscript,
@@ -825,8 +833,11 @@ int net_init_tap(const Netdev *netdev, const char *name,
             fcntl(fd, F_SETFL, O_NONBLOCK);
 
             if (i == 0) {
-                vnet_hdr = tap_probe_vnet_hdr(fd);
-            } else if (vnet_hdr != tap_probe_vnet_hdr(fd)) {
+                vnet_hdr = tap_probe_vnet_hdr(fd, errp);
+                if (vnet_hdr < 0) {
+                    goto free_fail;
+                }
+            } else if (vnet_hdr != tap_probe_vnet_hdr(fd, NULL)) {
                 error_setg(errp,
                            "vnet_hdr not consistent across given tap fds");
                 goto free_fail;
@@ -870,7 +881,11 @@ free_fail:
         }
 
         fcntl(fd, F_SETFL, O_NONBLOCK);
-        vnet_hdr = tap_probe_vnet_hdr(fd);
+        vnet_hdr = tap_probe_vnet_hdr(fd, errp);
+        if (vnet_hdr < 0) {
+            close(fd);
+            return -1;
+        }
 
         net_init_tap_one(tap, peer, "bridge", name, ifname,
                          script, downscript, vhostfdname,
