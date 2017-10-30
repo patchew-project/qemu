428c629d3b6609e5f38f99c9a2d130c867469496
Philippe Mathieu-Daudé <f4bug@amsat.org>
2017-10-30 00:05:50 -0300

    target/i386: tcg stubs
    missing since 55c3ceef61:
    
        LINK    x86_64-softmmu/qemu-system-x86_64
      target/i386/cpu.o: In function `x86_cpu_common_class_init':
      target/i386/cpu.c:4215: undefined reference to `tcg_x86_init'
    
    Suggested-by: Eduardo Habkost <ehabkost@redhat.com>
    Signed-off-by: Philippe Mathieu-Daudé <f4bug@amsat.org>
    Message-Id: <20171030030553.21259-3-f4bug@amsat.org>

---
+++ target/i386/tcg-stub.c
@@ -0,0 +1,13 @@
+/*
+ * QEMU TCG x86 specific function stubs
+ *
+ * This work is licensed under the terms of the GNU GPL, version 2 or later.
+ * See the COPYING file in the top-level directory.
+ *
+ */
+#include "qemu/osdep.h"
+#include "cpu.h"
+
+void tcg_x86_init(void)
+{
+}
