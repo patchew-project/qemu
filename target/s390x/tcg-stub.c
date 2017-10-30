fd780c3ef532c742c5c3cb47665d46bcd39cbca0
Philippe Mathieu-Daudé <f4bug@amsat.org>
2017-10-30 00:05:51 -0300

    target/s390x: tcg stubs
    missing since 55c3ceef61:
    
        LINK    s390x-softmmu/qemu-system-s390x
      target/s390x/cpu.o: In function `s390_cpu_class_init':
      target/s390x/cpu.c:500: undefined reference to `s390x_translate_init'
    
    Suggested-by: Eduardo Habkost <ehabkost@redhat.com>
    Signed-off-by: Philippe Mathieu-Daudé <f4bug@amsat.org>
    Message-Id: <20171030030553.21259-4-f4bug@amsat.org>

---
+++ target/s390x/tcg-stub.c
@@ -0,0 +1,13 @@
+/*
+ * QEMU TCG support -- s390x specific function stubs.
+ *
+ * This work is licensed under the terms of the GNU GPL, version 2 or later.
+ * See the COPYING file in the top-level directory.
+ */
+
+#include "qemu/osdep.h"
+#include "internal.h"
+
+void s390x_translate_init(void)
+{
+}
