603c114bd49023b7e6578ede2cf8f1ddc877cce5
ZhiPeng Lu <lu.zhipeng@zte.com.cn>
2017-11-03 22:54:20 +0800

    qga: replace GetIfEntry
    The data obtained by GetIfEntry is 32 bits, and it may overflow. Thus using GetIfEntry2 instead of GetIfEntry.
    
    Signed-off-by: ZhiPeng Lu <lu.zhipeng@zte.com.cn>
    Message-Id: <1509720860-10065-1-git-send-email-lu.zhipeng@zte.com.cn>

--- qga/commands-win32.c
+++ qga/commands-win32.c
@@ -1173,20 +1173,25 @@ static int guest_get_network_stats(const char *name,
                        GuestNetworkInterfaceStat *stats)
 {
     DWORD if_index = 0;
-    MIB_IFROW a_mid_ifrow;
-    memset(&a_mid_ifrow, 0, sizeof(a_mid_ifrow));
+    OSVERSIONINFO OSver;
     if_index = get_interface_index(name);
-    a_mid_ifrow.dwIndex = if_index;
-    if (NO_ERROR == GetIfEntry(&a_mid_ifrow)) {
-        stats->rx_bytes = a_mid_ifrow.dwInOctets;
-        stats->rx_packets = a_mid_ifrow.dwInUcastPkts;
-        stats->rx_errs = a_mid_ifrow.dwInErrors;
-        stats->rx_dropped = a_mid_ifrow.dwInDiscards;
-        stats->tx_bytes = a_mid_ifrow.dwOutOctets;
-        stats->tx_packets = a_mid_ifrow.dwOutUcastPkts;
-        stats->tx_errs = a_mid_ifrow.dwOutErrors;
-        stats->tx_dropped = a_mid_ifrow.dwOutDiscards;
-        return 0;
+    OSver.dwOSVersionInfoSize = sizeof(OSVERSIONINFO);
+    GetVersionEx(&OSver);
+    if (OSver.dwMajorVersion >= 6) {
+        MIB_IF_ROW2 a_mid_ifrow;
+        memset(&a_mid_ifrow, 0, sizeof(a_mid_ifrow));
+        a_mid_ifrow.dwIndex = if_index;
+        if (NO_ERROR == GetIfEntry2(&a_mid_ifrow)) {
+            stats->rx_bytes = a_mid_ifrow.dwInOctets;
+            stats->rx_packets = a_mid_ifrow.dwInUcastPkts;
+            stats->rx_errs = a_mid_ifrow.dwInErrors;
+            stats->rx_dropped = a_mid_ifrow.dwInDiscards;
+            stats->tx_bytes = a_mid_ifrow.dwOutOctets;
+            stats->tx_packets = a_mid_ifrow.dwOutUcastPkts;
+            stats->tx_errs = a_mid_ifrow.dwOutErrors;
+            stats->tx_dropped = a_mid_ifrow.dwOutDiscards;
+            return 0;
+        }
     }
     return -1;
 }
