9ca642467f681b51682790613a0a27bfb1824293
Marc-André Lureau <marcandre.lureau@redhat.com>
2017-11-06 19:39:04 +0100

    tpm-be: ask model to the TPM interface
    No need to store the mode in the backend, or to let the frontend set
    it itself.
    
    Signed-off-by: Marc-André Lureau <marcandre.lureau@redhat.com>
    Reviewed-by: Stefan Berger <stefanb@linux.vnet.ibm.com>
    Message-Id: <20171106183925.16747-8-marcandre.lureau@redhat.com>

--- hw/tpm/tpm_tis.c
+++ hw/tpm/tpm_tis.c
@@ -1068,8 +1068,6 @@ static void tpm_tis_realizefn(DeviceState *dev, Error **errp)
         return;
     }
 
-    s->be_driver->fe_model = TPM_MODEL_TPM_TIS;
-
     if (tpm_backend_init(s->be_driver, TPM_IF(s), errp)) {
         return;
     }
@@ -1104,6 +1102,7 @@ static void tpm_tis_class_init(ObjectClass *klass, void *data)
     dc->props = tpm_tis_properties;
     dc->reset = tpm_tis_reset;
     dc->vmsd  = &vmstate_tpm_tis;
+    tc->model = TPM_MODEL_TPM_TIS;
     tc->request_completed = tpm_tis_request_completed;
 }
 
