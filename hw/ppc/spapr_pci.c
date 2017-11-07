004012fdef82d59bd2fed0c2c45b98582aa4c23a
Seeteena Thoufeek <s1seetee@linux.vnet.ibm.com>
2017-11-07 18:48:58 +0530

    spapr_pci: fix the path while fetching loc-code from host DT
    The function spapr_phb_vfio_get_loc_code uses wrong path for
    fetching loc-code from host DT
    
    this is the call that needs to be fixed:
    /* Construct and read from host device tree the loc-code */
    path = g_strdup_printf("/proc/device-tree%s/ibm,loc-code", buf);
    
    Signed-off-by: Seeteena Thoufeek <s1seetee@linux.vnet.ibm.com>
    Message-Id: <1510060738-25975-1-git-send-email-s1seetee@linux.vnet.ibm.com>

--- hw/ppc/spapr_pci.c
+++ hw/ppc/spapr_pci.c
@@ -762,7 +762,7 @@ static char *spapr_phb_vfio_get_loc_code(sPAPRPHBState *sphb,  PCIDevice *pdev)
     g_free(path);
 
     /* Construct and read from host device tree the loc-code */
-    path = g_strdup_printf("/proc/device-tree%s/ibm,loc-code", buf);
+    path = g_strdup_printf("/proc/device-tree/%s/ibm,loc-code", buf);
     g_free(buf);
     if (!g_file_get_contents(path, &buf, NULL, NULL)) {
         goto err_out;
