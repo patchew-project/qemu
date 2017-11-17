2c82950f1c21f05c5ac6d3b01db6b21e38407d68
Gerd Hoffmann <kraxel@redhat.com>
2017-11-17 11:30:30 +0100

    egl-headless: use DisplayOptions
    Switch egl-headless ui to use qapi DisplayOptions for configuration.
    
    Signed-off-by: Gerd Hoffmann <kraxel@redhat.com>
    Message-Id: <20171117103046.15943-9-kraxel@redhat.com>

--- vl.c
+++ vl.c
@@ -2214,6 +2214,7 @@ static LegacyDisplayType select_display(const char *p)
         request_opengl = 1;
         display_opengl = 1;
         display = DT_EGL;
+        dpy.type = DISPLAY_TYPE_EGL_HEADLESS;
 #else
         fprintf(stderr, "egl support is disabled\n");
         exit(1);
@@ -4844,7 +4845,7 @@ int main(int argc, char **argv, char **envp)
 
 #ifdef CONFIG_OPENGL_DMABUF
     if (display_type == DT_EGL) {
-        egl_headless_init();
+        egl_headless_init(&dpy);
     }
 #endif
 
