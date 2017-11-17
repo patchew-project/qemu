d4e140bc0d1ca076ac044ac975d8ceda9c9f3c80
Gerd Hoffmann <kraxel@redhat.com>
2017-11-17 11:30:37 +0100

    sdl: hook up to display registry
    Signed-off-by: Gerd Hoffmann <kraxel@redhat.com>
    Message-Id: <20171117103046.15943-16-kraxel@redhat.com>

--- vl.c
+++ vl.c
@@ -2141,7 +2141,6 @@ static void parse_display(const char *p)
     const char *opts;
 
     if (strstart(p, "sdl", &opts)) {
-#ifdef CONFIG_SDL
         dpy.type = DISPLAY_TYPE_SDL;
         while (*opts) {
             const char *nextopt;
@@ -2182,10 +2181,6 @@ static void parse_display(const char *p)
             }
             opts = nextopt;
         }
-#else
-        error_report("SDL support is disabled");
-        exit(1);
-#endif
     } else if (strstart(p, "vnc", &opts)) {
         if (*opts == '=') {
             vnc_parse(opts + 1, &error_fatal);
@@ -4444,12 +4439,7 @@ int main(int argc, char **argv, char **envp)
                      "ignoring option");
     }
 
-    if (dpy.type == DISPLAY_TYPE_SDL) {
-        sdl_display_early_init(&dpy);
-    } else {
-        qemu_display_early_init(&dpy);
-    }
-
+    qemu_display_early_init(&dpy);
     qemu_console_early_init();
 
     if (dpy.has_gl && dpy.gl && display_opengl == 0) {
@@ -4781,9 +4771,6 @@ int main(int argc, char **argv, char **envp)
     switch (dpy.type) {
     case DISPLAY_TYPE_CURSES:
         curses_display_init(ds, &dpy);
-        break;
-    case DISPLAY_TYPE_SDL:
-        sdl_display_init(ds, &dpy);
         break;
     case DISPLAY_TYPE_COCOA:
         cocoa_display_init(ds, &dpy);
