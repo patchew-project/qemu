7657b24e5afeeb6004acbbad351ea3d7cf5753a0
Yoni Bettan <ybettan@redhat.com>
2017-11-21 08:41:06 +0200

    vl.c && linux-user/main.c : removed **envp from main() arguments
            * it was added on 2008 902b3d5c392bb6f48ef340ad8ecc3311705d2800
              when introduced cache-utils.[ch]
            * since then cache-utils.[ch] were removed but **envp was left
              behind
    
    Signed-off-by: Yoni Bettan <ybettan@redhat.com>
    Message-Id: <20171121064106.13721-1-ybettan@redhat.com>

--- vl.c
+++ vl.c
@@ -35,10 +35,10 @@
 #ifdef CONFIG_SDL
 #if defined(__APPLE__) || defined(main)
 #include <SDL.h>
-int qemu_main(int argc, char **argv, char **envp);
+int qemu_main(int argc, char **argv);
 int main(int argc, char **argv)
 {
-    return qemu_main(argc, argv, NULL);
+    return qemu_main(argc, argv);
 }
 #undef main
 #define main qemu_main
@@ -3088,7 +3088,7 @@ static void register_global_properties(MachineState *ms)
     user_register_global_props();
 }
 
-int main(int argc, char **argv, char **envp)
+int main(int argc, char **argv)
 {
     int i;
     int snapshot, linux_boot;
