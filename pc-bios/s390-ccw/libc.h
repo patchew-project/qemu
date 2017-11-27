b2803ba95bf12f917dcfda353fbcf9c0a97fb780
Collin L. Walling <walling@linux.vnet.ibm.com>
2017-11-27 15:55:32 -0500

    s390-ccw: update libc.h
    Moved:
      memcmp from bootmap.h to libc.h (renamed from _memcmp)
      strlen from sclp.c to libc.h (renamed from _strlen)
    
    Added C standard functions:
      isdigit
      atoi
    
    Added non-C standard function:
      itostr
    
    Signed-off-by: Collin L. Walling <walling@linux.vnet.ibm.com>
    Acked-by: Christian Borntraeger <borntraeger@de.ibm.com>
    Reviewed-by: frankja@linux.vnet.ibm.com
    Message-Id: <1511816136-30068-2-git-send-email-walling@linux.vnet.ibm.com>

--- pc-bios/s390-ccw/libc.h
+++ pc-bios/s390-ccw/libc.h
@@ -42,4 +42,98 @@ static inline void *memcpy(void *s1, const void *s2, size_t n)
     return s1;
 }
 
+static inline int memcmp(const void *s1, const void *s2, size_t n)
+{
+    int i;
+    const uint8_t *p1 = s1, *p2 = s2;
+
+    for (i = 0; i < n; i++) {
+        if (p1[i] != p2[i]) {
+            return p1[i] > p2[i] ? 1 : -1;
+        }
+    }
+
+    return 0;
+}
+
+static inline size_t strlen(const char *str)
+{
+    size_t i;
+    for (i = 0; *str; i++) {
+        str++;
+    }
+    return i;
+}
+
+static inline int isdigit(int c)
+{
+    return (c >= '0') && (c <= '9');
+}
+
+/* atoi
+ *
+ * Given a string, convert it to an integer. Any
+ * non-numerical value will end the conversion.
+ *
+ * @str - the string to be converted
+ *
+ * @return - an integer converted from the string str
+ */
+static inline int atoi(const char *str)
+{
+    int i;
+    int val = 0;
+
+    for (i = 0; str[i]; i++) {
+        char c = str[i];
+        if (!isdigit(c)) {
+            break;
+        }
+        val *= 10;
+        val += c - '0';
+    }
+
+    return val;
+}
+
+/* itostr
+ *
+ * Given an integer, convert it to a string. The string must be allocated
+ * beforehand. The resulting string will be null terminated and returned.
+ *
+ * @str - the integer to be converted
+ * @num - a pointer to a string to store the conversion
+ *
+ * @return - the string of the converted integer
+ */
+static inline char *itostr(int num, char *str)
+{
+    int i;
+    int len = 0;
+    long tens = 1;
+
+    /* Handle 0 */
+    if (num == 0) {
+        str[0] = '0';
+        str[1] = '\0';
+        return str;
+    }
+
+    /* Count number of digits */
+    while (num / tens != 0) {
+        tens *= 10;
+        len++;
+    }
+
+    /* Convert int -> string */
+    for (i = 0; i < len; i++) {
+        tens /= 10;
+        str[i] = num / tens % 10 + '0';
+    }
+
+    str[i] = '\0';
+
+    return str;
+}
+
 #endif
