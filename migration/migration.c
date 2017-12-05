00b5121a0c0ee41d986ba80be5def7d0a097ecc0
Peter Xu <peterx@redhat.com>
2017-12-05 14:52:59 +0800

    migration: synchronize dirty bitmap for resume
    This patch implements the first part of core RAM resume logic for
    postcopy. ram_resume_prepare() is provided for the work.
    
    When the migration is interrupted by network failure, the dirty bitmap
    on the source side will be meaningless, because even the dirty bit is
    cleared, it is still possible that the sent page was lost along the way
    to destination. Here instead of continue the migration with the old
    dirty bitmap on source, we ask the destination side to send back its
    received bitmap, then invert it to be our initial dirty bitmap.
    
    The source side send thread will issue the MIG_CMD_RECV_BITMAP requests,
    once per ramblock, to ask for the received bitmap. On destination side,
    MIG_RP_MSG_RECV_BITMAP will be issued, along with the requested bitmap.
    Data will be received on the return-path thread of source, and the main
    migration thread will be notified when all the ramblock bitmaps are
    synchronized.
    
    Reviewed-by: Dr. David Alan Gilbert <dgilbert@redhat.com>
    Signed-off-by: Peter Xu <peterx@redhat.com>
    Message-Id: <20171205065307.21853-21-peterx@redhat.com>
    Based-on: <20171205055200.16305-1-peterx@redhat.com>

--- migration/migration.c
+++ migration/migration.c
@@ -2839,6 +2839,7 @@ static void migration_instance_finalize(Object *obj)
     qemu_sem_destroy(&ms->pause_sem);
     qemu_sem_destroy(&ms->postcopy_pause_sem);
     qemu_sem_destroy(&ms->postcopy_pause_rp_sem);
+    qemu_sem_destroy(&ms->rp_state.rp_sem);
 }
 
 static void migration_instance_init(Object *obj)
@@ -2870,6 +2871,7 @@ static void migration_instance_init(Object *obj)
 
     qemu_sem_init(&ms->postcopy_pause_sem, 0);
     qemu_sem_init(&ms->postcopy_pause_rp_sem, 0);
+    qemu_sem_init(&ms->rp_state.rp_sem, 0);
 }
 
 /*
