16181fc4d01ec27773fa64e3c3568414f97b8b70
Mark Cave-Ayland <mark.cave-ayland@ilande.co.uk>
2017-12-05 08:17:43 +0000

    eepro100: switch e100_compute_mcast_idx() over to use net_crc32()
    Signed-off-by: Mark Cave-Ayland <mark.cave-ayland@ilande.co.uk>
    Message-Id: <20171205081744.6563-5-mark.cave-ayland@ilande.co.uk>
    Reviewed-by: Stefan Weil <sw@weilnetz.de>
    Reviewed-by: Philippe Mathieu-Daudé <f4bug@amsat.org>

--- hw/net/eepro100.c
+++ hw/net/eepro100.c
@@ -327,26 +327,9 @@ static const uint16_t eepro100_mdi_mask[] = {
 
 static E100PCIDeviceInfo *eepro100_get_class(EEPRO100State *s);
 
-/* From FreeBSD (locally modified). */
 static unsigned e100_compute_mcast_idx(const uint8_t *ep)
 {
-    uint32_t crc;
-    int carry, i, j;
-    uint8_t b;
-
-    crc = 0xffffffff;
-    for (i = 0; i < 6; i++) {
-        b = *ep++;
-        for (j = 0; j < 8; j++) {
-            carry = ((crc & 0x80000000L) ? 1 : 0) ^ (b & 0x01);
-            crc <<= 1;
-            b >>= 1;
-            if (carry) {
-                crc = ((crc ^ POLYNOMIAL) | carry);
-            }
-        }
-    }
-    return (crc & BITS(7, 2)) >> 2;
+    return (net_crc32(ep, 6) & BITS(7, 2)) >> 2;
 }
 
 /* Read a 16 bit control/status (CSR) register. */
