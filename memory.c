445edee2ff3ea4753491ec9fe608181530c35e59
Marcel Apfelbaum <marcel@redhat.com>
2018-01-04 18:36:49 +0200

    mem: add share parameter to memory-backend-ram
    Currently only file backed memory backend can
    be created with a "share" flag in order to allow
    sharing guest RAM with other processes in the host.
    
    Add the "share" flag also to RAM Memory Backend
    in order to allow remapping parts of the guest RAM
    to different host virtual addresses. This is needed
    by the RDMA devices in order to remap non-contiguous
    QEMU virtual addresses to a contiguous virtual address range.
    
    Moved the "share" flag to the Host Memory base class,
    modified phys_mem_alloc to include the new parameter
    and a new interface memory_region_init_ram_shared_nomigrate.
    
    There are no functional changes if the new flag is not used.
    
    Signed-off-by: Marcel Apfelbaum <marcel@redhat.com>
    Message-Id: <20180104163652.65450-3-marcel@redhat.com>

--- memory.c
+++ memory.c
@@ -1538,11 +1538,21 @@ void memory_region_init_ram_nomigrate(MemoryRegion *mr,
                                       uint64_t size,
                                       Error **errp)
 {
+    memory_region_init_ram_shared_nomigrate(mr, owner, name, size, false, errp);
+}
+
+void memory_region_init_ram_shared_nomigrate(MemoryRegion *mr,
+                                             Object *owner,
+                                             const char *name,
+                                             uint64_t size,
+                                             bool share,
+                                             Error **errp)
+{
     memory_region_init(mr, owner, name, size);
     mr->ram = true;
     mr->terminates = true;
     mr->destructor = memory_region_destructor_ram;
-    mr->ram_block = qemu_ram_alloc(size, mr, errp);
+    mr->ram_block = qemu_ram_alloc(size, share, mr, errp);
     mr->dirty_log_mask = tcg_enabled() ? (1 << DIRTY_MEMORY_CODE) : 0;
 }
 
@@ -1651,7 +1661,7 @@ void memory_region_init_rom_nomigrate(MemoryRegion *mr,
     mr->readonly = true;
     mr->terminates = true;
     mr->destructor = memory_region_destructor_ram;
-    mr->ram_block = qemu_ram_alloc(size, mr, errp);
+    mr->ram_block = qemu_ram_alloc(size, false, mr, errp);
     mr->dirty_log_mask = tcg_enabled() ? (1 << DIRTY_MEMORY_CODE) : 0;
 }
 
@@ -1670,7 +1680,7 @@ void memory_region_init_rom_device_nomigrate(MemoryRegion *mr,
     mr->terminates = true;
     mr->rom_device = true;
     mr->destructor = memory_region_destructor_ram;
-    mr->ram_block = qemu_ram_alloc(size, mr, errp);
+    mr->ram_block = qemu_ram_alloc(size, false,  mr, errp);
 }
 
 void memory_region_init_iommu(void *_iommu_mr,
