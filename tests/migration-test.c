9ab8009506d70cd08ee2a82bc2fda1dd93b37b40
Juan Quintela <quintela@redhat.com>
2018-01-05 22:52:44 +0100

    tests: Create migrate-start-postcopy command
    This way, it is like the rest of commands
    
    Signed-off-by: Juan Quintela <quintela@redhat.com>
    Message-Id: <20180105215246.908-10-quintela@redhat.com>
    Reviewed-by: Dr. David Alan Gilbert <dgilbert@redhat.com>
    Based-on: 20180105205109.683-1-quintela@redhat.com
    Reviewed-by: Peter Xu <peterx@redhat.com>

--- tests/migration-test.c
+++ tests/migration-test.c
@@ -436,6 +436,15 @@ static void migrate(QTestState *who, const char *uri)
     QDECREF(rsp);
 }
 
+static void migrate_start_postcopy(QTestState *who)
+{
+    QDict *rsp;
+
+    rsp = wait_command(who, "{ 'execute': 'migrate-start-postcopy' }");
+    g_assert(qdict_haskey(rsp, "return"));
+    QDECREF(rsp);
+}
+
 static void test_migrate_start(QTestState **from, QTestState **to,
                                const char *uri)
 {
@@ -598,7 +607,6 @@ static void test_postcopy(void)
 {
     char *uri = g_strdup_printf("unix:%s/migsocket", tmpfs);
     QTestState *from, *to;
-    QDict *rsp;
 
     test_migrate_start(&from, &to, uri);
 
@@ -620,9 +628,7 @@ static void test_postcopy(void)
 
     wait_for_migration_pass(from);
 
-    rsp = wait_command(from, "{ 'execute': 'migrate-start-postcopy' }");
-    g_assert(qdict_haskey(rsp, "return"));
-    QDECREF(rsp);
+    migrate_start_postcopy(from);
 
     if (!got_stop) {
         qtest_qmp_eventwait(from, "STOP");
