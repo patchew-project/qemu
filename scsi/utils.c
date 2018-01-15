e1f4caefe11031a7eae9ed08e688edac2409d879
linzhecheng <linzhecheng@huawei.com>
2018-01-15 12:16:48 +0800

    scsi: handle the special parameters
    scsi_disk_emulate_command calls
    scsi_build_sense(NULL, 0, outbuf, r->buflen,
                                      (req->cmd.buf[1] & 1) == 0);
    But scsi_convert_sense doesn't handle the case when in_buf is NULL
    or in_len is 0, which will lead to segfault.
    
    Signed-off-by: linzhecheng <linzhecheng@huawei.com>
    Message-Id: <20180115041648.33740-1-linzhecheng@huawei.com>

--- scsi/utils.c
+++ scsi/utils.c
@@ -322,6 +322,10 @@ int scsi_convert_sense(uint8_t *in_buf, int in_len,
     SCSISense sense;
     bool fixed_in;
 
+    if (!in_buf || !in_len) {
+        return 0;
+    }
+
     fixed_in = (in_buf[0] & 2) == 0;
     if (in_len && fixed == fixed_in) {
         memcpy(buf, in_buf, MIN(len, in_len));
