c31e0becab749e98643add953f7128e6b4735dc1
Anton Nefedov <anton.nefedov@virtuozzo.com>
2018-01-16 16:04:27 +0300

    block: support BDRV_REQ_ALLOCATE in passthrough drivers
    Support the flag if the underlying BDS supports it
    
    Signed-off-by: Anton Nefedov <anton.nefedov@virtuozzo.com>
    Reviewed-by: Alberto Garcia <berto@igalia.com>
    Message-Id: <1516107870-8110-7-git-send-email-anton.nefedov@virtuozzo.com>

--- block/blkverify.c
+++ block/blkverify.c
@@ -145,7 +145,7 @@ static int blkverify_open(BlockDriverState *bs, QDict *options, int flags,
         s->test_file->bs->supported_write_flags;
 
     bs->supported_zero_flags =
-        (BDRV_REQ_FUA | BDRV_REQ_MAY_UNMAP) &
+        (BDRV_REQ_FUA | BDRV_REQ_MAY_UNMAP | BDRV_REQ_ALLOCATE) &
         bs->file->bs->supported_zero_flags &
         s->test_file->bs->supported_zero_flags;
 
