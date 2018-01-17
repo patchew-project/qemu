9ff55234c6d041a56d8cee0da85390c4f1abd6d5
Igor Mammedov <imammedo@redhat.com>
2018-01-17 16:43:25 +0100

    sh4: cpu: add TARGET_DEFAULT_CPU_TYPE macro
    it will be used for for 2 purposes, 1st is to
    provide to cpu name resolving class for machine 'none'
      cpu_parse_cpu_model(machine_class->default_cpu_type, cpu_model)
    and later to drop a bunch of ifdefs *-user/main.c that
    set default cpu_model.
    
    Use default value from linux-user for TARGET_DEFAULT_CPU_TYPE.
    
    Signed-off-by: Igor Mammedov <imammedo@redhat.com>
    Message-Id: <1516203816-19374-14-git-send-email-imammedo@redhat.com>

--- target/sh4/cpu.h
+++ target/sh4/cpu.h
@@ -278,6 +278,7 @@ void cpu_load_tlb(CPUSH4State * env);
 
 #define SUPERH_CPU_TYPE_SUFFIX "-" TYPE_SUPERH_CPU
 #define SUPERH_CPU_TYPE_NAME(model) model SUPERH_CPU_TYPE_SUFFIX
+#define TARGET_DEFAULT_CPU_TYPE SUPERH_CPU_TYPE_NAME("sh7785")
 
 #define cpu_signal_handler cpu_sh4_signal_handler
 #define cpu_list sh4_cpu_list
