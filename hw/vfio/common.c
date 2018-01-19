342de99547ca4cf1ecfa38ca71ec1e13c571baa3
Eric Auger <eric.auger@redhat.com>
2018-01-19 14:49:15 +0000

    hw/vfio/common: Do not print error when viommu translates into an mmio region
    On ARM, the MSI doorbell is translated by the virtual IOMMU.
    As such address_space_translate() returns the MSI controller
    MMIO region and we get an "iommu map to non memory area"
    message. Let's remove this latter.
    
    Signed-off-by: Eric Auger <eric.auger@redhat.com>
    Message-Id: <1516373355-305-23-git-send-email-eric.auger@redhat.com>

--- hw/vfio/common.c
+++ hw/vfio/common.c
@@ -326,8 +326,6 @@ static bool vfio_get_vaddr(IOMMUTLBEntry *iotlb, void **vaddr,
                                  iotlb->translated_addr,
                                  &xlat, &len, writable);
     if (!memory_region_is_ram(mr)) {
-        error_report("iommu map to non memory area %"HWADDR_PRIx"",
-                     xlat);
         return false;
     }
 
