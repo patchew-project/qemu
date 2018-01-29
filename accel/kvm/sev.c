80d6eacd927f47b9a92b93b1282a2ba58f6c1af6
Brijesh Singh <brijesh.singh@amd.com>
2018-01-29 11:41:20 -0600

    sev: register the guest memory range which may contain encrypted data
    When SEV is enabled, the hardware encryption engine uses a tweak such
    that the two identical plaintext at different location will have a
    different ciphertexts. So swapping or moving a ciphertexts of two guest
    pages will not result in plaintexts being swapped. Hence relocating
    a physical backing pages of the SEV guest will require some additional
    steps in KVM driver. The KVM_MEMORY_ENCRYPT_{UN,}REG_REGION ioctl can be
    used to register/unregister the guest memory region which may contain the
    encrypted data. KVM driver will internally handle the relocating physical
    backing pages of registered memory regions.
    
    Cc: Paolo Bonzini <pbonzini@redhat.com>
    Signed-off-by: Brijesh Singh <brijesh.singh@amd.com>
    Message-Id: <20180129174132.108925-12-brijesh.singh@amd.com>

--- accel/kvm/sev.c
+++ accel/kvm/sev.c
@@ -85,6 +85,45 @@ fw_error_to_str(int code)
 }
 
 static void
+sev_ram_block_added(RAMBlockNotifier *n, void *host, size_t size)
+{
+    int r;
+    struct kvm_enc_region range;
+
+    range.addr = (__u64)host;
+    range.size = size;
+
+    trace_kvm_memcrypt_register_region(host, size);
+    r = kvm_vm_ioctl(kvm_state, KVM_MEMORY_ENCRYPT_REG_REGION, &range);
+    if (r) {
+        error_report("%s: failed to register region (%p+%#lx)",
+                     __func__, host, size);
+    }
+}
+
+static void
+sev_ram_block_removed(RAMBlockNotifier *n, void *host, size_t size)
+{
+    int r;
+    struct kvm_enc_region range;
+
+    range.addr = (__u64)host;
+    range.size = size;
+
+    trace_kvm_memcrypt_unregister_region(host, size);
+    r = kvm_vm_ioctl(kvm_state, KVM_MEMORY_ENCRYPT_UNREG_REGION, &range);
+    if (r) {
+        error_report("%s: failed to unregister region (%p+%#lx)",
+                     __func__, host, size);
+    }
+}
+
+static struct RAMBlockNotifier sev_ram_notifier = {
+    .ram_block_added = sev_ram_block_added,
+    .ram_block_removed = sev_ram_block_removed,
+};
+
+static void
 qsev_guest_finalize(Object *obj)
 {
 }
@@ -287,6 +326,8 @@ sev_guest_init(const char *id)
                      __func__, ret, fw_error, fw_error_to_str(fw_error));
         goto err;
     }
+
+    ram_block_notifier_add(&sev_ram_notifier);
 
     return s;
 err:
