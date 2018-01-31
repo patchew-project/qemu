a56ae317b381f89bea13bc7c53f4ce082672b5d6
Gerd Hoffmann <kraxel@redhat.com>
2018-01-31 13:12:17 +0100

    vfio/display: adding region support
    Wire up region-based display.
    
    Signed-off-by: Gerd Hoffmann <kraxel@redhat.com>
    Message-Id: <20180131121217.13557-6-kraxel@redhat.com>
    Tested-by: Kirti Wankhede <kwankhede@nvidia.com>

--- hw/vfio/pci.h
+++ hw/vfio/pci.h
@@ -143,6 +143,7 @@ typedef struct VFIOPCIDevice {
     bool no_kvm_intx;
     bool no_kvm_msi;
     bool no_kvm_msix;
+    VFIODisplay *dpy;
 } VFIOPCIDevice;
 
 uint32_t vfio_pci_read_config(PCIDevice *pdev, uint32_t addr, int len);
