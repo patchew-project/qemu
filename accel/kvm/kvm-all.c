e1062ee8254eaa612dcd6f5dc08966846c30a84e
Prasad J Pandit <pjp@fedoraproject.org>
2018-02-05 11:48:49 +0530

    kvm: check dev parameter when updating msi route
    When updating message signalled interrupt(MSI) route in routine
    kvm_irqchip_update_msi_route, device parameter could be null.
    Add check to avoid null dereference.
    
    Reported-by: Guoxiang Niu <niuguoxiang@huawei.com>
    Signed-off-by: Prasad J Pandit <pjp@fedoraproject.org>
    Message-Id: <20180205061849.17696-1-ppandit@redhat.com>

--- accel/kvm/kvm-all.c
+++ accel/kvm/kvm-all.c
@@ -1167,7 +1167,7 @@ int kvm_irqchip_add_msi_route(KVMState *s, int vector, PCIDevice *dev)
     kroute.u.msi.address_lo = (uint32_t)msg.address;
     kroute.u.msi.address_hi = msg.address >> 32;
     kroute.u.msi.data = le32_to_cpu(msg.data);
-    if (pci_available && kvm_msi_devid_required()) {
+    if (pci_available && dev && kvm_msi_devid_required()) {
         kroute.flags = KVM_MSI_VALID_DEVID;
         kroute.u.msi.devid = pci_requester_id(dev);
     }
@@ -1205,7 +1205,7 @@ int kvm_irqchip_update_msi_route(KVMState *s, int virq, MSIMessage msg,
     kroute.u.msi.address_lo = (uint32_t)msg.address;
     kroute.u.msi.address_hi = msg.address >> 32;
     kroute.u.msi.data = le32_to_cpu(msg.data);
-    if (pci_available && kvm_msi_devid_required()) {
+    if (pci_available && dev && kvm_msi_devid_required()) {
         kroute.flags = KVM_MSI_VALID_DEVID;
         kroute.u.msi.devid = pci_requester_id(dev);
     }
