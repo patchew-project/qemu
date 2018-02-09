63d6999c40b6c507df19a37e6573a525ffc89fb3
Yasmin Beatriz <yasmins@linux.vnet.ibm.com>
2018-02-09 14:26:21 -0600

    dump: Show custom message for ENOSPC
    This patch intends to make a more specific message for when
    the system has not enough space to save guest memory.
    
    Reported-by: yilzhang@redhat.com
    Cc: Jose Ricardo Ziviani <joserz@linux.vnet.ibm.com>
    Signed-off-by: Yasmin Beatriz <yasmins@linux.vnet.ibm.com>
    Message-Id: <20180209202621.97852-1-yasmins@linux.vnet.ibm.com>
    Reviewed-by: Daniel Henrique Barboza <danielhb@linux.vnet.ibm.com>

--- dump.c
+++ dump.c
@@ -107,7 +107,7 @@ static int fd_write_vmcore(const void *buf, size_t size, void *opaque)
 
     written_size = qemu_write_full(s->fd, buf, size);
     if (written_size != size) {
-        return -1;
+        return -errno;
     }
 
     return 0;
@@ -365,7 +365,11 @@ static void write_data(DumpState *s, void *buf, int length, Error **errp)
 
     ret = fd_write_vmcore(buf, length, s);
     if (ret < 0) {
-        error_setg(errp, "dump: failed to save memory");
+        if (ret == -ENOSPC) {
+            error_setg(errp, "dump: not enough space to save memory");
+        } else {
+            error_setg(errp, "dump: failed to save memory");
+        }
     } else {
         s->written_size += length;
     }
