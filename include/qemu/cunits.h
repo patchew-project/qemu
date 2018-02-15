c1e8225a50c5472f03cf88ee0666797eb4e2a2d3
Philippe Mathieu-Daudé <f4bug@amsat.org>
2018-02-15 01:28:31 -0300

    util/cutils: extract byte-based definitions into a new header: "qemu/cunits.h"
    (added in 076b35b5a56)
    
    Signed-off-by: Philippe Mathieu-Daudé <f4bug@amsat.org>
    Message-Id: <20180215042900.16078-2-f4bug@amsat.org>
    Reviewed-by: Marc-André Lureau <marcandre.lureau@redhat.com>

---
+++ include/qemu/cunits.h
@@ -0,0 +1,11 @@
+#ifndef QEMU_CUNITS_H
+#define QEMU_CUNITS_H
+
+#define K_BYTE     (1ULL << 10)
+#define M_BYTE     (1ULL << 20)
+#define G_BYTE     (1ULL << 30)
+#define T_BYTE     (1ULL << 40)
+#define P_BYTE     (1ULL << 50)
+#define E_BYTE     (1ULL << 60)
+
+#endif
