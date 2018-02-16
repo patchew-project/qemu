f30693de6562e150170dc8e93d5944bd0c9535e8
Haozhong Zhang <haozhong.zhang@intel.com>
2018-02-16 16:46:11 +0800

    mem/nvdimm: ensure write persistence to PMEM in label emulation
    Guest writes to vNVDIMM labels are intercepted and performed on the
    backend by QEMU. When the backend is a real persistent memort, QEMU
    needs to take proper operations to ensure its write persistence on the
    persistent memory. Otherwise, a host power failure may result in the
    loss of guest label configurations.
    
    Signed-off-by: Haozhong Zhang <haozhong.zhang@intel.com>
    Message-Id: <20180216084615.26483-5-haozhong.zhang@intel.com>

---
+++ include/qemu/pmem.h
@@ -0,0 +1,23 @@
+/*
+ * QEMU header file for libpmem.
+ *
+ * Copyright (c) 2018 Intel Corporation.
+ *
+ * Author: Haozhong Zhang <haozhong.zhang@intel.com>
+ *
+ * This work is licensed under the terms of the GNU GPL, version 2 or later.
+ * See the COPYING file in the top-level directory.
+ */
+
+#ifndef QEMU_PMEM_H
+#define QEMU_PMEM_H
+
+#ifdef CONFIG_LIBPMEM
+#include <libpmem.h>
+#else  /* !CONFIG_LIBPMEM */
+
+void *pmem_memcpy_persist(void *pmemdest, const void *src, size_t len);
+
+#endif /* CONFIG_LIBPMEM */
+
+#endif /* !QEMU_PMEM_H */
