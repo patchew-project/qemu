aaa584d75432b226dfe778ad679cdc220d560b51
Luiz Capitulino <lcapitulino@redhat.com>
2018-02-16 13:04:49 +0100

    qmp: add query-cpus-fast
    The query-cpus command has an extremely serious side effect:
    it always interrupts all running vCPUs so that they can run
    ioctl calls. This can cause a huge performance degradation for
    some workloads. And most of the information retrieved by the
    ioctl calls are not even used by query-cpus.
    
    This commit introduces a replacement for query-cpus called
    query-cpus-fast, which has the following features:
    
     o Never interrupt vCPUs threads. query-cpus-fast only returns
       vCPU information maintained by QEMU itself, which should be
       sufficient for most management software needs
    
     o Drop "halted" field as it can not be retrieved in a fast
       way on most architectures
    
     o Drop irrelevant fields such as "current", "pc" and "arch"
    
     o Rename some fields for better clarification & proper naming
       standard
    
    Further, the HMP "info cpus" implementation is changed to
    use the new QMP interface to avoid the side effects caused
    by query-cpus. This means that only a reduced subset of cpu
    information will be reported, which is acceptable as
    the content of "info cpus" is not documented or guaranteed
    to be stable.
    
    Signed-off-by: Luiz Capitulino <lcapitulino@redhat.com>
    Signed-off-by: Viktor Mihajlovski <mihajlov@linux.vnet.ibm.com>
    Reviewed-by: Cornelia Huck <cohuck@redhat.com>
    Acked-by: Eric Blake <eblake@redhat.com>
    Message-Id: <1518782691-1232-3-git-send-email-mihajlov@linux.vnet.ibm.com>
    Acked-by.  But I do think this means we need a v5.

--- hmp.c
+++ hmp.c
@@ -360,50 +360,16 @@ void hmp_info_migrate_cache_size(Monitor *mon, const QDict *qdict)
 
 void hmp_info_cpus(Monitor *mon, const QDict *qdict)
 {
-    CpuInfoList *cpu_list, *cpu;
+    CpuInfoFastList *head, *cpu;
 
-    cpu_list = qmp_query_cpus(NULL);
+    head = qmp_query_cpus_fast(NULL);
 
-    for (cpu = cpu_list; cpu; cpu = cpu->next) {
-        int active = ' ';
-
-        if (cpu->value->CPU == monitor_get_cpu_index()) {
-            active = '*';
-        }
-
-        monitor_printf(mon, "%c CPU #%" PRId64 ":", active, cpu->value->CPU);
-
-        switch (cpu->value->arch) {
-        case CPU_INFO_ARCH_X86:
-            monitor_printf(mon, " pc=0x%016" PRIx64, cpu->value->u.x86.pc);
-            break;
-        case CPU_INFO_ARCH_PPC:
-            monitor_printf(mon, " nip=0x%016" PRIx64, cpu->value->u.ppc.nip);
-            break;
-        case CPU_INFO_ARCH_SPARC:
-            monitor_printf(mon, " pc=0x%016" PRIx64,
-                           cpu->value->u.q_sparc.pc);
-            monitor_printf(mon, " npc=0x%016" PRIx64,
-                           cpu->value->u.q_sparc.npc);
-            break;
-        case CPU_INFO_ARCH_MIPS:
-            monitor_printf(mon, " PC=0x%016" PRIx64, cpu->value->u.q_mips.PC);
-            break;
-        case CPU_INFO_ARCH_TRICORE:
-            monitor_printf(mon, " PC=0x%016" PRIx64, cpu->value->u.tricore.PC);
-            break;
-        default:
-            break;
-        }
-
-        if (cpu->value->halted) {
-            monitor_printf(mon, " (halted)");
-        }
-
-        monitor_printf(mon, " thread_id=%" PRId64 "\n", cpu->value->thread_id);
+    for (cpu = head; cpu; cpu = cpu->next) {
+        monitor_printf(mon, "  CPU #%" PRId64 ":", cpu->value->cpu_index);
+        monitor_printf(mon, " thread-id=%" PRId64 "\n", cpu->value->thread_id);
     }
 
-    qapi_free_CpuInfoList(cpu_list);
+    qapi_free_CpuInfoFastList(head);
 }
 
 static void print_block_info(Monitor *mon, BlockInfo *info,
