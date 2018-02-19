e384b62024d0fd5124465d535f50dcdcd5cecc89
Thomas Huth <thuth@redhat.com>
2018-02-19 21:23:40 +0100

    Remove the deprecated -tdf option
    It's been marked as deprecated since a very long time already, and
    the parameter is not doing anything useful anymore except for printing
    a warning, so it's now time to finally get rid of this option.
    
    Signed-off-by: Thomas Huth <thuth@redhat.com>
    Message-Id: <1519071820-4062-1-git-send-email-thuth@redhat.com>
    Reviewed-by: Kashyap Chamarthy <kchamart@redhat.com>
    Reviewed-by: Daniel P. Berrangé <berrange@redhat.com>

--- vl.c
+++ vl.c
@@ -3841,9 +3841,6 @@ int main(int argc, char **argv, char **envp)
                     exit(1);
                 }
                 break;
-            case QEMU_OPTION_tdf:
-                warn_report("ignoring deprecated option");
-                break;
             case QEMU_OPTION_name:
                 opts = qemu_opts_parse_noisily(qemu_find_opts("name"),
                                                optarg, true);
