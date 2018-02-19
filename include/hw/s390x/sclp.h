0a070f4284a27a12b39d981e81fedb6679a4742a
David Hildenbrand <david@redhat.com>
2018-02-19 15:33:41 +0100

    s390x/sclp: remove memory hotplug support
    From an architecture point of view, nothing can be mapped into the address
    space on s390x. All there is is memory. Therefore there is also not really
    an interface to communicate such information to the guest. All we can do is
    specify the maximum ram address and guests can probe in that range if
    memory is available and usable (TPROT).
    
    Also memory hotplug is strange. The guest can decide at some point in
    time to add / remove memory in some range. And nobody can really hinder it
    from doing so. So if we specify right now e.g.
        -m 2G,slots=2,maxmem=20G
    An ordinary fedora guest will happily online (hotplug) all memory,
    resulting in a guest consuming 20G. So it really behaves rather like
        -m 22G
    There is no way to hotplug memory from the outside like on other
    architectures. This is of course bad for upper management layers.
    
    As the guest can create/delete memory regions while it is running, of
    course migration support is not available and tricky to implement.
    
    With virtualization, it is different. We might want to map something
    into guest address space (e.g. fake DAX devices) and not detect it
    automatically as memory. So we really want to use the maxmem and slots
    parameter just like on all other architectures. Such devices will have
    to expose the applicable memory range themselves. To finally be able to
    provide memory hotplug to guests, we will need a new paravirtualized
    interface to do that (e.g. something into the direction of virtio-mem).
    
    This implies, that maxmem cannot be used for s390x memory hotplug
    anymore and has to go. This simplifies the code quite a bit.
    
    As migration support is not working, this change cannot really break
    migration. As the memory hotplug device was always created, we can
    simply continue faking support for SCLP_FC_ASSIGN_ATTACH_READ_STOR and
    expose the same information just as if no maxmem and slots parameter was
    given on the command line (to not break migration of ordinary guests).
    
    Signed-off-by: David Hildenbrand <david@redhat.com>
    Message-Id: <20180219143341.4290-1-david@redhat.com>

--- include/hw/s390x/sclp.h
+++ include/hw/s390x/sclp.h
@@ -215,23 +215,6 @@ typedef struct SCLPDeviceClass {
     void (*service_interrupt)(SCLPDevice *sclp, uint32_t sccb);
 } SCLPDeviceClass;
 
-typedef struct sclpMemoryHotplugDev sclpMemoryHotplugDev;
-
-#define TYPE_SCLP_MEMORY_HOTPLUG_DEV "sclp-memory-hotplug-dev"
-#define SCLP_MEMORY_HOTPLUG_DEV(obj) \
-  OBJECT_CHECK(sclpMemoryHotplugDev, (obj), TYPE_SCLP_MEMORY_HOTPLUG_DEV)
-
-struct sclpMemoryHotplugDev {
-    SysBusDevice parent;
-    ram_addr_t standby_mem_size;
-    ram_addr_t padded_ram_size;
-    ram_addr_t pad_size;
-    ram_addr_t standby_subregion_size;
-    ram_addr_t rzm;
-    int increment_size;
-    char *standby_state_map;
-};
-
 static inline int sccb_data_len(SCCB *sccb)
 {
     return be16_to_cpu(sccb->h.length) - sizeof(sccb->h);
@@ -239,8 +222,6 @@ static inline int sccb_data_len(SCCB *sccb)
 
 
 void s390_sclp_init(void);
-sclpMemoryHotplugDev *init_sclp_memory_hotplug_dev(void);
-sclpMemoryHotplugDev *get_sclp_memory_hotplug_dev(void);
 void sclp_service_interrupt(uint32_t sccb);
 void raise_irq_cpu_hotplug(void);
 int sclp_service_call(CPUS390XState *env, uint64_t sccb, uint32_t code);
