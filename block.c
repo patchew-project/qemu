902e5671d72c28074c3558b938c87d0e6841a3d8
Max Reitz <mreitz@redhat.com>
2018-02-24 16:40:33 +0100

    block: Deprecate "backing": ""
    We have a clear replacement, so let's deprecate it.
    
    Signed-off-by: Max Reitz <mreitz@redhat.com>
    Reviewed-by: Eric Blake <eblake@redhat.com>
    Reviewed-by: Alberto Garcia <berto@igalia.com>
    Message-Id: <20180224154033.29559-8-mreitz@redhat.com>

--- block.c
+++ block.c
@@ -2603,6 +2603,10 @@ static BlockDriverState *bdrv_open_inherit(const char *filename,
     if (qobject_to(qdict_get(options, "backing"), QNull) != NULL ||
         (backing && *backing == '\0'))
     {
+        if (backing) {
+            warn_report("Use of \"backing\": \"\" is deprecated; "
+                        "use \"backing\": null instead");
+        }
         flags |= BDRV_O_NO_BACKING;
         qdict_del(options, "backing");
     }
