c0d13777a0ff8b9f11ddc5652ddec76da290fbed
Or Idgar <oidgar@redhat.com>
2018-02-28 15:39:12 +0200

    vmgenid: allow VM Generation ID modification via QMP/HMP
    This patch allow changing the Virtual Machine Generation
    ID through QMP/HMP while the vm guest is running.
    As the definition block of VMGENID in ACPI includes the
    "Notify" method, we can use it to notify the guest about
    ID changes.
    
    QMP command example:
        { "execute": "set-vm-generation-id",
              "arguments": {
                  "guid": "324e6eaf-d1d1-4bf6-bf41-b9bb6c91fb87"
              }
        }
    
    HMP command example:
        set-vm-generation-id 324e6eaf-d1d1-4bf6-bf41-b9bb6c91fb87
    
    This patch is based off an earlier version by
    Igor Mammedov (imammedo@redhat.com)
    
    Signed-off-by: Or Idgar <oidgar@redhat.com>
    Reviewed-by: Gal Hammer <ghammer@redhat.com>
    Message-Id: <20180228133912.21496-1-idgar@virtualoco.com>

--- hmp.h
+++ hmp.h
@@ -146,5 +146,6 @@ void hmp_info_ramblock(Monitor *mon, const QDict *qdict);
 void hmp_hotpluggable_cpus(Monitor *mon, const QDict *qdict);
 void hmp_info_vm_generation_id(Monitor *mon, const QDict *qdict);
 void hmp_info_memory_size_summary(Monitor *mon, const QDict *qdict);
+void hmp_set_vm_generation_id(Monitor *mon, const QDict *qdict);
 
 #endif
