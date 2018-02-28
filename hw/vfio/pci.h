360b4c20ebad6f3a512b6a7329a479cb57944d84
Alex Williamson <alex.williamson@redhat.com>
2018-02-28 13:45:54 -0700

    vfio/quirks: ioeventfd quirk acceleration
    The NVIDIA BAR0 quirks virtualize the PCI config space mirrors found
    in device MMIO space.  Normally PCI config space is considered a slow
    path and further optimization is unnecessary, however NVIDIA uses a
    register here to enable the MSI interrupt to re-trigger.  Exiting to
    QEMU for this MSI-ACK handling can therefore rate limit our interrupt
    handling.  Fortunately the MSI-ACK write is easily detected since the
    quirk MemoryRegion otherwise has very few accesses, so simply looking
    for consecutive writes with the same data is sufficient, in this case
    10 consecutive writes with the same data and size is arbitrarily
    chosen.  We configure the KVM ioeventfd with data match, so there's
    no risk of triggering for the wrong data or size, but we do risk that
    pathological driver behavior might consume all of QEMU's file
    descriptors, so we cap ourselves to 10 ioeventfds for this purpose.
    
    In support of the above, generic ioeventfd infrastructure is added
    for vfio quirks.  This automatically initializes an ioeventfd list
    per quirk, disables and frees ioeventfds on exit, and allows
    ioeventfds marked as dynamic to be dropped on device reset.  The
    rationale for this latter feature is that useful ioeventfds may
    depend on specific driver behavior and since we necessarily place a
    cap on our use of ioeventfds, a machine reset is a reasonable point
    at which to assume a new driver and re-profile.
    
    Signed-off-by: Alex Williamson <alex.williamson@redhat.com>
    Message-Id: <20180228204554.26629.59183.stgit@gimli.home>
    Reviewed-by: Eric Auger <eric.auger@redhat.com>
    Reviewed-by: Peter Xu <peterx@redhat.com>

--- hw/vfio/pci.h
+++ hw/vfio/pci.h
@@ -24,9 +24,23 @@
 
 struct VFIOPCIDevice;
 
+typedef struct VFIOIOEventFD {
+    QLIST_ENTRY(VFIOIOEventFD) next;
+    MemoryRegion *mr;
+    hwaddr addr;
+    unsigned size;
+    uint64_t data;
+    EventNotifier e;
+    VFIORegion *region;
+    hwaddr region_addr;
+    bool match_data;
+    bool dynamic;
+} VFIOIOEventFD;
+
 typedef struct VFIOQuirk {
     QLIST_ENTRY(VFIOQuirk) next;
     void *data;
+    QLIST_HEAD(, VFIOIOEventFD) ioeventfds;
     int nr_mem;
     MemoryRegion *mem;
     void (*reset)(struct VFIOPCIDevice *vdev, struct VFIOQuirk *quirk);
