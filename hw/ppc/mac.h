aec6210402bd324cf0ca620901243d388cfac11d
Mark Cave-Ayland <mark.cave-ayland@ilande.co.uk>
2018-03-06 20:31:02 +0000

    mac_newworld: remove pics IRQ array and wire up macio to OpenPIC directly
    Introduce constants for the pre-defined New World IRQs to help keep things
    readable.
    
    Signed-off-by: Mark Cave-Ayland <mark.cave-ayland@ilande.co.uk>
    Message-Id: <20180306203103.25563-19-mark.cave-ayland@ilande.co.uk>
    Reviewed-by: David Gibson <david@gibson.dropbear.id.au>
    Reviewed-by: Philippe Mathieu-Daudé <f4bug@amsat.org>

--- hw/ppc/mac.h
+++ hw/ppc/mac.h
@@ -47,6 +47,14 @@
 
 #define ESCC_CLOCK 3686400
 
+/* New World IRQs */
+#define NEWWORLD_CUDA_IRQ      0x19
+#define NEWWORLD_ESCCB_IRQ     0x24
+#define NEWWORLD_ESCCA_IRQ     0x25
+#define NEWWORLD_IDE0_IRQ      0xd
+#define NEWWORLD_IDE0_DMA_IRQ  0x2
+#define NEWWORLD_IDE1_IRQ      0xe
+#define NEWWORLD_IDE1_DMA_IRQ  0x3
 
 /* MacIO */
 #define TYPE_MACIO_IDE "macio-ide"
