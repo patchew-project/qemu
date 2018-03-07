eb023ea8bc8dba34a0ecbe35378038560d72b68b
Juan Quintela <quintela@redhat.com>
2018-03-07 11:59:59 +0100

    migration: Introduce multifd_recv_new_channel()
    Signed-off-by: Juan Quintela <quintela@redhat.com>
    Message-Id: <20180307110010.2205-14-quintela@redhat.com>

--- migration/ram.c
+++ migration/ram.c
@@ -36,6 +36,7 @@
 #include "xbzrle.h"
 #include "ram.h"
 #include "migration.h"
+#include "socket.h"
 #include "migration/register.h"
 #include "migration/misc.h"
 #include "qemu-file.h"
@@ -622,6 +623,11 @@ int multifd_load_setup(void)
         atomic_inc(&multifd_recv_state->count);
     }
     return 0;
+}
+
+void multifd_recv_new_channel(QIOChannel *ioc)
+{
+    /* nothing to do yet */
 }
 
 /**
