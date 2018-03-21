ba4011e602d227c1b45fd374f6e7c4f767fc9017
Marc-André Lureau <marcandre.lureau@redhat.com>
2018-03-21 12:51:44 +0100

    qapi-event: add 'if' condition to implicit event enum
    Add condition to QAPIEvent enum members based on the event 'if'.
    
    Generated code is not changed by this patch, but with "qapi: add #if
    conditions to generated code" patch.
    
    There is no coverage of this change in qapi-schema-test.out since the
    event_names enum is an implicit type created by qapi-event.py.
    
    Signed-off-by: Marc-André Lureau <marcandre.lureau@redhat.com>
    Message-Id: <20180321115211.17937-23-marcandre.lureau@redhat.com>

--- scripts/qapi/events.py
+++ scripts/qapi/events.py
@@ -188,7 +188,7 @@ class QAPISchemaGenEventVisitor(QAPISchemaModularCVisitor):
         with ifcontext(ifcond, self._genh, self._genc):
             self._genh.add(gen_event_send_decl(name, arg_type, boxed))
             self._genc.add(gen_event_send(name, arg_type, boxed, self._enum_name))
-        self._event_names.append(QAPISchemaMember(name))
+        self._event_names.append(QAPISchemaMember(name, ifcond))
 
 
 def gen_events(schema, output_dir, prefix):
