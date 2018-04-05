693553867a6f64a36f7feccff9e4fa87c90b32bd
Igor Mammedov <imammedo@redhat.com>
2018-04-05 11:05:12 +0200

    qmp: add set-numa-node command
    Command is allowed to run only in preconfig stage and
    will allow to configure numa mapping for CPUs depending
    on possible CPUs layout (query-hotpluggable-cpus) for
    given machine instance.
    
    Signed-off-by: Igor Mammedov <imammedo@redhat.com>
    Message-Id: <1522919113-158775-11-git-send-email-imammedo@redhat.com>

--- numa.c
+++ numa.c
@@ -444,6 +444,11 @@ void parse_numa_opts(MachineState *ms)
     }
 }
 
+void qmp_set_numa_node(NumaOptions *cmd, Error **errp)
+{
+    set_numa_options(MACHINE(qdev_get_machine()), cmd, errp);
+}
+
 void numa_cpu_pre_plug(const CPUArchId *slot, DeviceState *dev, Error **errp)
 {
     int node_id = object_property_get_int(OBJECT(dev), "node-id", &error_abort);
