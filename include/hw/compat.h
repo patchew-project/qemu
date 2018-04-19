d6453c9686354b2b0d16ece9617e7bb1270867f8
Gerd Hoffmann <kraxel@redhat.com>
2018-04-19 15:10:48 +0200

    audio/hda: create millisecond timers that handle IO
    Currently, the HDA device tries to sync itself with the QEMU audio
    backend by waiting for the guest driver to handle buffer completion
    interrupts. This causes the backend to often read too much data from the
    device, as well as running out of data whenever the guest takes too long
    to handle the interrupt.
    
    According to the HDA specification, the guest is also not required to
    use interrupts, but can also sync itself by polling the LPIB registers.
    
    This patch will introduce high frequency (1000Hz) timers that interface
    with the device and allow for much smoother emulation of the LPIB
    registers. Since the timing is now provided by these timers, the need
    to wait for buffer completion interrupts also ceases.
    
    Signed-off-by: Martin Schrodt <martin@schrodt.org>
    Message-id: 20171015184033.2951-3-martin@schrodt.org
    
    [ kraxel: keep old code for compatibility with older qemu versions,
              add property to switch code paths at runtime ]
    
    Signed-off-by: Gerd Hoffmann <kraxel@redhat.com>
    Message-Id: <20180419131051.11249-2-kraxel@redhat.com>

--- include/hw/compat.h
+++ include/hw/compat.h
@@ -1,6 +1,13 @@
 #ifndef HW_COMPAT_H
 #define HW_COMPAT_H
 
+#define HW_COMPAT_2_12 \
+    {\
+        .driver   = "hda-audio",\
+        .property = "use-timer",\
+        .value    = "false",\
+    },
+
 #define HW_COMPAT_2_11 \
     {\
         .driver   = "hpet",\
