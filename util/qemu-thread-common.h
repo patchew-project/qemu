0a2ddef014131a056dd74d48c69f3bd65091ef58
Peter Xu <peterx@redhat.com>
2018-04-23 13:39:25 +0800

    qemu-thread: introduce qemu-thread-common.h
    Introduce some hooks for the shared part of qemu thread between POSIX
    and Windows implementations.  Note that in qemu_mutex_unlock_impl() we
    moved the call before unlock operation which should make more sense.
    And we don't need qemu_mutex_post_unlock() hook.
    
    Currently the hooks only calls the tracepoints.
    
    Put all these shared hooks into the header files.  It should be internal
    to qemu-thread but not for qemu-thread users, hence put into util/
    directory.
    
    Signed-off-by: Peter Xu <peterx@redhat.com>
    Message-Id: <20180423053927.13715-3-peterx@redhat.com>
    Reviewed-by: Emilio G. Cota <cota@braap.org>

---
+++ util/qemu-thread-common.h
@@ -0,0 +1,38 @@
+/*
+ * Common qemu-thread implementation header file.
+ *
+ * Copyright Red Hat, Inc. 2018
+ *
+ * Authors:
+ *  Peter Xu <peterx@redhat.com>,
+ *
+ * This work is licensed under the terms of the GNU GPL, version 2 or later.
+ * See the COPYING file in the top-level directory.
+ */
+
+#ifndef __QEMU_THREAD_COMMON_H__
+#define __QEMU_THREAD_COMMON_H__
+
+#include "qemu/typedefs.h"
+#include "qemu/thread.h"
+#include "trace.h"
+
+static inline void qemu_mutex_pre_lock(QemuMutex *mutex,
+                                       const char *file, int line)
+{
+    trace_qemu_mutex_lock(mutex, file, line);
+}
+
+static inline void qemu_mutex_post_lock(QemuMutex *mutex,
+                                        const char *file, int line)
+{
+    trace_qemu_mutex_locked(mutex, file, line);
+}
+
+static inline void qemu_mutex_pre_unlock(QemuMutex *mutex,
+                                         const char *file, int line)
+{
+    trace_qemu_mutex_unlock(mutex, file, line);
+}
+
+#endif
