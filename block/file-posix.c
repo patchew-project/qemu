d83eb070403bedf105230243e8cb7c0a25a3cf77
Anton Nefedov <anton.nefedov@virtuozzo.com>
2018-05-08 17:58:13 +0300

    file-posix: support BDRV_REQ_ALLOCATE
    Current write_zeroes implementation is good enough to satisfy this flag too
    
    Signed-off-by: Anton Nefedov <anton.nefedov@virtuozzo.com>
    Message-Id: <1525791496-125188-7-git-send-email-anton.nefedov@virtuozzo.com>
    Reviewed-by: Alberto Garcia <berto@igalia.com>

--- block/file-posix.c
+++ block/file-posix.c
@@ -570,7 +570,10 @@ static int raw_open_common(BlockDriverState *bs, QDict *options,
     }
     if (S_ISREG(st.st_mode)) {
         s->discard_zeroes = true;
+#ifdef CONFIG_FALLOCATE
         s->has_fallocate = true;
+        bs->supported_zero_flags |= BDRV_REQ_ALLOCATE;
+#endif
     }
     if (S_ISBLK(st.st_mode)) {
 #ifdef BLKDISCARDZEROES
@@ -605,10 +608,11 @@ static int raw_open_common(BlockDriverState *bs, QDict *options,
 #ifdef CONFIG_XFS
     if (platform_test_xfs_fd(s->fd)) {
         s->is_xfs = true;
+        bs->supported_zero_flags |= BDRV_REQ_ALLOCATE;
     }
 #endif
 
-    bs->supported_zero_flags = s->discard_zeroes ? BDRV_REQ_MAY_UNMAP : 0;
+    bs->supported_zero_flags |= s->discard_zeroes ? BDRV_REQ_MAY_UNMAP : 0;
     ret = 0;
 fail:
     if (filename && (bdrv_flags & BDRV_O_TEMPORARY)) {
@@ -1440,6 +1444,10 @@ static ssize_t handle_aiocb_write_zeroes(RawPosixAIOData *aiocb)
             return ret;
         }
         s->has_fallocate = false;
+    }
+
+    if (!s->has_fallocate) {
+        aiocb->bs->supported_zero_flags &= ~BDRV_REQ_ALLOCATE;
     }
 #endif
 
