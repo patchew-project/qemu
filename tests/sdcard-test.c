dea6d84037ac39daa1321d0f96755f775e46e6d5
Philippe Mathieu-Daudé <f4bug@amsat.org>
2018-05-09 00:46:50 -0300

    sdcard: Add test_sd_verify_cksum_frame136()
    Per the Physical Layer Simplified Spec. "3.6 Bus Protocol":
    
      There are two types of Data packet format for the SD card.
    
      (1) Usual data (8-bit width): The usual data (8-bit width) are sent in
          LSB (Least Significant Byte) first, MSB (Most Significant Byte) last
          sequence. But in the individual byte, it is MSB (Most Significant Bit)
          first, LSB (Least Significant Bit) last.
    
      (2) Wide width data (SD Memory Register): The wide width data is shifted
          from the MSB bit.
    
    Since the SD frames use different byte order, we use the lm32 target
    for big-endian qtesting, and aarch64 for the little-endian qtesting.
    
    Signed-off-by: Philippe Mathieu-Daudé <f4bug@amsat.org>
    Message-Id: <20180509034658.26455-7-f4bug@amsat.org>

---
+++ tests/sdcard-test.c
@@ -0,0 +1,35 @@
+/*
+ * QTest testcase for SD protocol and cards
+ *
+ * Examples taken from:
+ *
+ * - Physical Layer Simplified Specification (chap. 4.5: Cyclic Redundancy Code)
+ * - http://wiki.seabright.co.nz/wiki/SdCardProtocol.html
+ *
+ * Tests written by Philippe Mathieu-Daudé <f4bug@amsat.org>
+ *
+ * SPDX-License-Identifier: GPL-2.0-or-later
+ */
+#include "qemu/osdep.h"
+#include "libqtest.h"
+#include "hw/sd/sd.h"
+
+static void test_sd_response_frame136_crc7(void)
+{
+    uint8_t buf[16];
+
+    /* response to CMD2 ALL_SEND_CID */
+    memcpy(&buf, "\x1d\x41\x44\x53\x44\x20\x20\x20\x10\xa0\x40\x0b\xc1\x00\x88",
+           sizeof(buf));
+    buf[15] = sd_frame136_calc_checksum(buf);
+    g_assert_cmphex(buf[15], ==, 0xad);
+}
+
+int main(int argc, char *argv[])
+{
+    g_test_init(&argc, &argv, NULL);
+
+    qtest_add_func("sd/prepare_resp136_crc7", test_sd_response_frame136_crc7);
+
+    return g_test_run();
+}
