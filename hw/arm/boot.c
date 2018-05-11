fd5ff266e4aa5f1da44a28e30874a95b86b7f96d
Su Hang <suhang16@mails.ucas.ac.cn>
2018-05-11 13:53:13 +0800

    Implement .hex file loader
    This patch adds Intel Hexadecimal Object File format support to
    the loader.  The file format specification is available here:
    http://www.piclist.com/techref/fileext/hex/intel.htm
    
    The file format is mainly intended for embedded systems
    and microcontrollers, such as Micro:bit Arduino, ARM, STM32, etc.
    
    Suggested-by: Stefan Hajnoczi <stefanha@redhat.com>
    Signed-off-by: Su Hang <suhang16@mails.ucas.ac.cn>
    Message-Id: <1526017994-22246-2-git-send-email-suhang16@mails.ucas.ac.cn>

--- hw/arm/boot.c
+++ hw/arm/boot.c
@@ -1066,12 +1066,17 @@ static void arm_load_kernel_notify(Notifier *notifier, void *data)
         kernel_size = load_uimage_as(info->kernel_filename, &entry, NULL,
                                      &is_linux, NULL, NULL, as);
     }
+    if (kernel_size < 0) {
+        /* 32-bit ARM Intel HEX file */
+        entry = 0;
+        kernel_size = load_targphys_hex_as(info->kernel_filename, &entry, as);
+    }
     if (arm_feature(&cpu->env, ARM_FEATURE_AARCH64) && kernel_size < 0) {
         kernel_size = load_aarch64_image(info->kernel_filename,
                                          info->loader_start, &entry, as);
         is_linux = 1;
     } else if (kernel_size < 0) {
-        /* 32-bit ARM */
+        /* 32-bit ARM binary file */
         entry = info->loader_start + KERNEL_LOAD_ADDR;
         kernel_size = load_image_targphys_as(info->kernel_filename, entry,
                                              info->ram_size - KERNEL_LOAD_ADDR,
