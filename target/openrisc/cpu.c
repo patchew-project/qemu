6f3285191bcbb7f66e7f19a1a4223e496e9d8845
Richard Henderson <richard.henderson@linaro.org>
2018-05-27 09:13:23 -0500

    target/openrisc: Add print_insn_or1k
    Rather than emit disassembly while translating, reuse the
    generated decoder to build a separate disassembler.
    
    Signed-off-by: Richard Henderson <richard.henderson@linaro.org>
    Message-Id: <20180527141324.11937-20-richard.henderson@linaro.org>

--- target/openrisc/cpu.c
+++ target/openrisc/cpu.c
@@ -36,6 +36,11 @@ static bool openrisc_cpu_has_work(CPUState *cs)
                                     CPU_INTERRUPT_TIMER);
 }
 
+static void openrisc_disas_set_info(CPUState *cpu, disassemble_info *info)
+{
+    info->print_insn = print_insn_or1k;
+}
+
 /* CPUClass::reset() */
 static void openrisc_cpu_reset(CPUState *s)
 {
@@ -151,6 +156,7 @@ static void openrisc_cpu_class_init(ObjectClass *oc, void *data)
 #endif
     cc->gdb_num_core_regs = 32 + 3;
     cc->tcg_initialize = openrisc_translate_init;
+    cc->disas_set_info = openrisc_disas_set_info;
 }
 
 /* Sort alphabetically by type name, except for "any". */
