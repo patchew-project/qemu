19d9d1ff89d324820b57846a0f2604dc2ba5bb44
Philippe Mathieu-Daudé <f4bug@amsat.org>
2018-05-28 03:11:54 -0300

    hw/misc/unimp: Add create_unimplemented_subregion_device()
    The create_unimplemented_device() function is very useful to
    register unimplemented devices to the SysBus 'absolute' address.
    
    Some devices are modelled as container of memory subregions,
    the subregions being mmio-mapped relatively to the container
    base address.
    
    With these container devices, the current create_unimplemented_device()
    does not work.
    Add a function to remove the current limitation, and allow
    containerized devices to use the helpful UnimplementedDevice.
    
    Signed-off-by: Philippe Mathieu-Daudé <f4bug@amsat.org>
    Message-Id: <20180528061154.25367-1-f4bug@amsat.org>

--- include/hw/misc/unimp.h
+++ include/hw/misc/unimp.h
@@ -23,7 +23,43 @@ typedef struct {
 } UnimplementedDeviceState;
 
 /**
- * create_unimplemented_device: create and map a dummy device
+ * create_unimplemented_subregion_device: create and map a dummy device
+ *
+ * @mr: the #MemoryRegion to contain the new device.
+ * @name: name of the device for debug logging
+ * @addr: base address of the device's MMIO region, or
+ *        offset relative to @mr where the device is added.
+ * @size: size of the device's MMIO region
+ *
+ * This utility function creates and maps an instance of unimplemented-device,
+ * which is a dummy device which simply logs all guest accesses to
+ * it via the qemu_log LOG_UNIMP debug log.
+ * The device is mapped at priority -1000, which means that you can
+ * use it to cover a large region and then map other devices on top of it
+ * if necessary.
+ */
+static inline void create_unimplemented_subregion_device(MemoryRegion *mr,
+                                                         const char *name,
+                                                         hwaddr addr,
+                                                         hwaddr size)
+{
+    DeviceState *dev = qdev_create(NULL, TYPE_UNIMPLEMENTED_DEVICE);
+
+    qdev_prop_set_string(dev, "name", name);
+    qdev_prop_set_uint64(dev, "size", size);
+    qdev_init_nofail(dev);
+
+    if (mr) {
+        MemoryRegion *submr = sysbus_mmio_get_region(SYS_BUS_DEVICE(dev), 0);
+        memory_region_add_subregion_overlap(mr, addr, submr, -1000);
+    } else {
+        sysbus_mmio_map_overlap(SYS_BUS_DEVICE(dev), 0, addr, -1000);
+    }
+}
+
+/**
+ * create_unimplemented_device: create and map a dummy SysBus device
+ *
  * @name: name of the device for debug logging
  * @base: base address of the device's MMIO region
  * @size: size of the device's MMIO region
@@ -39,13 +75,7 @@ static inline void create_unimplemented_device(const char *name,
                                                hwaddr base,
                                                hwaddr size)
 {
-    DeviceState *dev = qdev_create(NULL, TYPE_UNIMPLEMENTED_DEVICE);
-
-    qdev_prop_set_string(dev, "name", name);
-    qdev_prop_set_uint64(dev, "size", size);
-    qdev_init_nofail(dev);
-
-    sysbus_mmio_map_overlap(SYS_BUS_DEVICE(dev), 0, base, -1000);
+    create_unimplemented_subregion_device(NULL, name, base, size);
 }
 
 #endif
