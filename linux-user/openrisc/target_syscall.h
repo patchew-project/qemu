e444217868da010f04bb08f5184957edb85a46bb
Richard Henderson <richard.henderson@linaro.org>
2018-05-30 21:18:21 -0700

    linux-user: Implement signals for openrisc
    All of the existing code was boilerplate from elsewhere,
    and would crash the guest upon the first signal.
    
    Signed-off-by: Richard Henderson <richard.henderson@linaro.org>
    Message-Id: <20180531041822.5761-2-richard.henderson@linaro.org>

--- linux-user/openrisc/target_syscall.h
+++ linux-user/openrisc/target_syscall.h
@@ -2,26 +2,9 @@
 #define OPENRISC_TARGET_SYSCALL_H
 
 struct target_pt_regs {
-    union {
-        struct {
-            /* Named registers */
-            uint32_t sr;       /* Stored in place of r0 */
-            target_ulong sp;   /* r1 */
-        };
-        struct {
-            /* Old style */
-            target_ulong offset[2];
-            target_ulong gprs[30];
-        };
-        struct {
-            /* New style */
-            target_ulong gpr[32];
-        };
-    };
-    target_ulong pc;
-    target_ulong orig_gpr11;   /* For restarting system calls */
-    uint32_t syscallno;        /* Syscall number (used by strace) */
-    target_ulong dummy;     /* Cheap alignment fix */
+    abi_ulong gpr[32];
+    abi_ulong pc;
+    abi_ulong sr;
 };
 
 #define UNAME_MACHINE "openrisc"
