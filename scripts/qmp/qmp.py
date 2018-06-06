f32a25dd5f7613f1ae96698eecc8ad20d967f92f
Philippe Mathieu-Daudé <f4bug@amsat.org>
2018-06-06 16:27:31 -0300

    qmp.py: Fix exception parsing partial JSON
    The readline() call returns partial data.
    Keep appending until the JSON buffer is complete.
    
    This fixes:
    
        $ scripts/qmp/qmp-shell -v -p /tmp/qmp.sock
        Traceback (most recent call last):
          File "scripts/qmp/qmp-shell", line 456, in <module>
            main()
          File "scripts/qmp/qmp-shell", line 441, in main
            qemu.connect(negotiate)
          File "scripts/qmp/qmp-shell", line 284, in connect
            self._greeting = super(QMPShell, self).connect(negotiate)
          File "scripts/qmp/qmp.py", line 143, in connect
            return self.__negotiate_capabilities()
          File "scripts/qmp/qmp.py", line 71, in __negotiate_capabilities
            greeting = self.__json_read()
          File "scripts/qmp/qmp.py", line 85, in __json_read
            resp = json.loads(data)
          File "/usr/lib/python2.7/json/__init__.py", line 339, in loads
            return _default_decoder.decode(s)
          File "/usr/lib/python2.7/json/decoder.py", line 364, in decode
            obj, end = self.raw_decode(s, idx=_w(s, 0).end())
          File "/usr/lib/python2.7/json/decoder.py", line 380, in raw_decode
            obj, end = self.scan_once(s, idx)
        ValueError: Expecting object: line 1 column 3 (char 2)
    
    Signed-off-by: Philippe Mathieu-Daudé <f4bug@amsat.org>
    Message-Id: <20180606192731.6910-1-f4bug@amsat.org>

--- scripts/qmp/qmp.py
+++ scripts/qmp/qmp.py
@@ -78,11 +78,18 @@ class QEMUMonitorProtocol(object):
         raise QMPCapabilitiesError
 
     def __json_read(self, only_event=False):
+        data = ""
         while True:
-            data = self.__sockfile.readline()
-            if not data:
+            tmp = self.__sockfile.readline()
+            if not tmp:
                 return
-            resp = json.loads(data)
+            data += tmp
+            try:
+                resp = json.loads(data)
+            except ValueError:
+                # XXX: blindly loop, even if QEMU ever sends malformed data
+                continue
+            data = ""
             if 'event' in resp:
                 self.logger.debug("<<< %s", resp)
                 self.__events.append(resp)
