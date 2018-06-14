897e3178f53012297f470ad4fed3d3fca3f79d95
David Gibson <david@gibson.dropbear.id.au>
2018-06-14 11:04:46 +1000

    target/ppc, spapr: Move VPA information to machine_data
    CPUPPCState currently contains a number of fields containing the state of
    the VPA.  The VPA is a PAPR specific concept covering several guest/host
    shared memory areas used to communicate some information with the
    hypervisor.
    
    As a PAPR concept this is really machine specific information, although it
    is per-cpu, so it doesn't really belong in the core CPU state structure.
    So, move it to the PAPR specific 'machine_data' structure.
    
    Signed-off-by: David Gibson <david@gibson.dropbear.id.au>
    Reviewed-by: Greg Kurz <groug@kaod.org>
    Message-Id: <20180614010446.30854-8-david@gibson.dropbear.id.au>

--- include/hw/ppc/spapr_cpu_core.h
+++ include/hw/ppc/spapr_cpu_core.h
@@ -44,6 +44,9 @@ void spapr_cpu_set_entry_state(PowerPCCPU *cpu, target_ulong nip, target_ulong r
 typedef struct ICPState ICPState;
 typedef struct sPAPRCPUState {
     ICPState *icp;
+    uint64_t vpa_addr;
+    uint64_t slb_shadow_addr, slb_shadow_size;
+    uint64_t dtl_addr, dtl_size;
 } sPAPRCPUState;
 
 static inline sPAPRCPUState *spapr_cpu_state(PowerPCCPU *cpu)
