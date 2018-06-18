161a9914eed29d5a4d7e1c0c2fe9e9bfcfcc91ae
David Gibson <david@gibson.dropbear.id.au>
2018-06-18 16:36:03 +1000

    spapr: Use maximum page size capability to simplify memory backend checking
    The way we used to handle KVM allowable guest pagesizes for PAPR guests
    required some convoluted checking of memory attached to the guest.
    
    The allowable pagesizes advertised to the guest cpus depended on the memory
    which was attached at boot, but then we needed to ensure that any memory
    later hotplugged didn't change which pagesizes were allowed.
    
    Now that we have an explicit machine option to control the allowable
    maximum pagesize we can simplify this.  We just check all memory backends
    against that declared pagesize.  We check base and cold-plugged memory at
    reset time, and hotplugged memory at pre_plug() time.
    
    Signed-off-by: David Gibson <david@gibson.dropbear.id.au>
    Message-Id: <20180618063606.2513-7-david@gibson.dropbear.id.au>
    Reviewed-by: Greg Kurz <groug@kaod.org>
    Reviewed-by: Cédric Le Goater <clg@kaod.org>

--- hw/ppc/spapr_caps.c
+++ hw/ppc/spapr_caps.c
@@ -26,6 +26,7 @@
 #include "qapi/error.h"
 #include "qapi/visitor.h"
 #include "sysemu/hw_accel.h"
+#include "exec/ram_addr.h"
 #include "target/ppc/cpu.h"
 #include "target/ppc/mmu-hash64.h"
 #include "cpu-models.h"
@@ -304,6 +305,23 @@ static void cap_safe_indirect_branch_apply(sPAPRMachineState *spapr,
 
 #define VALUE_DESC_TRISTATE     " (broken, workaround, fixed)"
 
+void spapr_check_pagesize(sPAPRMachineState *spapr, hwaddr pagesize,
+                          Error **errp)
+{
+    hwaddr maxpagesize = (1ULL << spapr->eff.caps[SPAPR_CAP_HPT_MPS]);
+
+    if (!kvmppc_hpt_needs_host_contiguous_pages()) {
+        return;
+    }
+
+    if (maxpagesize > pagesize) {
+        error_setg(errp,
+                   "Can't support %"HWADDR_PRIu" kiB guest pages with %"
+                   HWADDR_PRIu" kiB host pages with this KVM implementation",
+                   maxpagesize >> 10, pagesize >> 10);
+    }
+}
+
 static void cap_hpt_maxpagesize_apply(sPAPRMachineState *spapr,
                                       uint8_t val, Error **errp)
 {
@@ -312,6 +330,8 @@ static void cap_hpt_maxpagesize_apply(sPAPRMachineState *spapr,
     } else if (val < 16) {
         warn_report("Many guests require at least 64kiB hpt-max-page-size");
     }
+
+    spapr_check_pagesize(spapr, qemu_getrampagesize(), errp);
 }
 
 sPAPRCapabilityInfo capability_table[SPAPR_CAP_NUM] = {
