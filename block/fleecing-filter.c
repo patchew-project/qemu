76930f060d7ba83e8289ce6323bab1eab141c381
Vladimir Sementsov-Ogievskiy <vsementsov@virtuozzo.com>
2018-06-21 18:48:50 +0300

    block/fleecing-filter: new filter driver for fleecing
    We need to synchronize backup job with reading from fleecing image
    like it was done in block/replication.c.
    
    Otherwise, the following situation is theoretically possible:
    
    1. client start reading
    2. client understand, that there is no corresponding cluster in
       fleecing image
    3. client is going to read from backing file (i.e. active image)
    4. guest writes to active image
    5. this write is stopped by backup(sync=none) and cluster is copied to
       fleecing image
    6. guest write continues...
    7. and client reads _new_ (or partly new) date from active image
    
    So, this fleecing-filter should be above fleecing image, the whole
    picture of fleecing looks like this:
    
        +-------+           +------------+
        |       |           |            |
        | guest |           | NBD client +<------+
        |       |           |            |       |
        ++-----++           +------------+       |only read
         |     ^                                 |
         | IO  |                                 |
         v     |                           +-----+------+
        ++-----+---------+                 |            |
        |                |                 |  internal  |
        |  active image  +----+            | NBD server |
        |                |    |            |            |
        +-+--------------+    |backup      +-+----------+
          ^                   |sync=none     ^
          |backing            |              |only read
          |                   |              |
        +-+--------------+    |       +------+----------+
        |                |    |       |                 |
        | fleecing image +<---+       | fleecing filter |
        |                |            |                 |
        +--------+-------+            +-----+-----------+
                 ^                          |
                 |                          |
                 +--------------------------+
                           file
    
    Signed-off-by: Vladimir Sementsov-Ogievskiy <vsementsov@virtuozzo.com>
    Message-Id: <20180621154850.23354-1-vsementsov@virtuozzo.com>

---
+++ block/fleecing-filter.c
@@ -0,0 +1,72 @@
+#include "qemu/osdep.h"
+#include "qemu-common.h"
+#include "block/blockjob.h"
+#include "block/block_int.h"
+#include "block/block_backup.h"
+
+static int64_t fleecing_getlength(BlockDriverState *bs)
+{
+    return bdrv_getlength(bs->file->bs);
+}
+
+static coroutine_fn int fleecing_co_preadv(BlockDriverState *bs,
+                                           uint64_t offset, uint64_t bytes,
+                                           QEMUIOVector *qiov, int flags)
+{
+    int ret;
+    BlockJob *job = bs->file->bs->backing->bs->job;
+    CowRequest req;
+
+    backup_wait_for_overlapping_requests(job, offset, bytes);
+    backup_cow_request_begin(&req, job, offset, bytes);
+
+    ret = bdrv_co_preadv(bs->file, offset, bytes, qiov, flags);
+
+    backup_cow_request_end(&req);
+
+    return ret;
+}
+
+static coroutine_fn int fleecing_co_pwritev(BlockDriverState *bs,
+                                            uint64_t offset, uint64_t bytes,
+                                            QEMUIOVector *qiov, int flags)
+{
+    return -EINVAL;
+}
+
+static bool fleecing_recurse_is_first_non_filter(BlockDriverState *bs,
+                                                 BlockDriverState *candidate)
+{
+    return bdrv_recurse_is_first_non_filter(bs->file->bs, candidate);
+}
+
+static int fleecing_open(BlockDriverState *bs, QDict *options,
+                         int flags, Error **errp)
+{
+    bs->file = bdrv_open_child(NULL, options, "file", bs, &child_file, false,
+                               errp);
+
+    return bs->file ? 0 : -EINVAL;
+}
+
+BlockDriver bdrv_fleecing_filter = {
+    .format_name = "fleecing-filter",
+    .protocol_name = "fleecing-filter",
+    .instance_size = 0,
+
+    .bdrv_open = fleecing_open,
+
+    .bdrv_getlength = fleecing_getlength,
+    .bdrv_co_preadv = fleecing_co_preadv,
+    .bdrv_co_pwritev = fleecing_co_pwritev,
+
+    .is_filter = true,
+    .bdrv_recurse_is_first_non_filter = fleecing_recurse_is_first_non_filter,
+};
+
+static void bdrv_fleecing_init(void)
+{
+    bdrv_register(&bdrv_fleecing_filter);
+}
+
+block_init(bdrv_fleecing_init);
