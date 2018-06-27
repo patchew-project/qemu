bf470e05b7dcfb644870ed2902e53cf020fcfd03
Pankaj Gupta <pagupta@redhat.com>
2018-06-27 13:52:48 +0530

    virtio: Set status early in VirtIODevice parent object
     To process pending requests for driver status 'VIRTIO_CONFIG_S_DRIVER_OK',
     virtio-rng 'set_status' calls 'is_guest_ready' function. This checks if
     virtqueue is ready and status is set to 'VIRTIO_CONFIG_S_DRIVER_OK'.
    
     This call is made before new status is updated in VirtIODevice parent object
     and calling function uses old status value.
    
    Signed-off-by: Pankaj Gupta <pagupta@redhat.com>
    Message-Id: <20180627082248.14921-3-pagupta@redhat.com>

--- hw/virtio/virtio.c
+++ hw/virtio/virtio.c
@@ -1154,10 +1154,11 @@ int virtio_set_status(VirtIODevice *vdev, uint8_t val)
             }
         }
     }
+    vdev->status = val;
+
     if (k->set_status) {
         k->set_status(vdev, val);
     }
-    vdev->status = val;
     return 0;
 }
 
