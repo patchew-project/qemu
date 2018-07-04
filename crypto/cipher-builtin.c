d079d51c782801a28c0cc6dc477f43da73f3a5c3
Philippe Mathieu-Daudé <f4bug@amsat.org>
2018-07-04 12:39:14 -0300

    crypto: Remove useless casts
    Patch created mechanically by rerunning:
    
      $  spatch --sp-file scripts/coccinelle/typecast.cocci \
                --macro-file scripts/cocci-macro-file.h \
                --dir . --in-place
    
    Signed-off-by: Philippe Mathieu-Daudé <f4bug@amsat.org>
    Message-Id: <20180704153919.12432-4-f4bug@amsat.org>
    Acked-by: Michael S. Tsirkin <mst@redhat.com>

--- crypto/cipher-builtin.c
+++ crypto/cipher-builtin.c
@@ -133,7 +133,7 @@ static void qcrypto_cipher_aes_xts_encrypt(const void *ctx,
 {
     const QCryptoCipherBuiltinAESContext *aesctx = ctx;
 
-    qcrypto_cipher_aes_ecb_encrypt((AES_KEY *)&aesctx->enc,
+    qcrypto_cipher_aes_ecb_encrypt(&aesctx->enc,
                                    src, dst, length);
 }
 
@@ -145,7 +145,7 @@ static void qcrypto_cipher_aes_xts_decrypt(const void *ctx,
 {
     const QCryptoCipherBuiltinAESContext *aesctx = ctx;
 
-    qcrypto_cipher_aes_ecb_decrypt((AES_KEY *)&aesctx->dec,
+    qcrypto_cipher_aes_ecb_decrypt(&aesctx->dec,
                                    src, dst, length);
 }
 
