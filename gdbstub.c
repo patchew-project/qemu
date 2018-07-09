76575e1043563b37a5e27f12f68f731d4fa58994
stephane duverger <stephane.duverger@gmail.com>
2018-07-09 14:54:59 +0200

    fix gdbserver_state pointer validation
    Hi,
    
    This is a small patch to gdbstub rather insignificant at first sight:
    fix null pointer dereference. It actually allows to take benefit of
    gdb features (breakpoints/sstep) internally (ie. special purpose
    board) without connecting a gdb client to the Qemu instance gdbserver
    stub.
    
    Regards,
    
    Signed-off-by: Stephane Duverger <stephane.duverger@gmail.com>
    Message-Id: <20180709125452.7utyomtsxr6xaxdi@vm.localdomain>

--- gdbstub.c
+++ gdbstub.c
@@ -1412,6 +1412,9 @@ static int gdb_handle_packet(GDBState *s, const char *line_buf)
 
 void gdb_set_stop_cpu(CPUState *cpu)
 {
+    if (!gdbserver_state) {
+        return;
+    }
     gdbserver_state->c_cpu = cpu;
     gdbserver_state->g_cpu = cpu;
 }
