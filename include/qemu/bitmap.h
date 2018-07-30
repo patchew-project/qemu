bf7ba9c2c9806325b199073ab17da12771717f80
Wei Wang <wei.w.wang@intel.com>
2018-07-30 15:13:56 +0800

    bitmap.h: add comments to BITMAP_LAST_WORD_MASK
    This macro was ported from Linux and we've reached an aggreement there
    that the corner case "nbits = 0" is not applicable to this macro, because
    when "nbits = 0", which means no bits to mask, this macro is expected to
    return 0, instead of 0xffffffff. This patch simply adds a comment above
    the macro as a note to users about the corner case.
    
    Signed-off-by: Wei Wang <wei.w.wang@intel.com>
    CC: Dr. David Alan Gilbert <dgilbert@redhat.com>
    CC: Juan Quintela <quintela@redhat.com>
    CC: Peter Xu <peterx@redhat.com>
    Message-Id: <1532934837-5966-2-git-send-email-wei.w.wang@intel.com>

--- include/qemu/bitmap.h
+++ include/qemu/bitmap.h
@@ -60,6 +60,7 @@
  */
 
 #define BITMAP_FIRST_WORD_MASK(start) (~0UL << ((start) & (BITS_PER_LONG - 1)))
+/* "nbits = 0" is not applicable to this macro. Callers should avoid that. */
 #define BITMAP_LAST_WORD_MASK(nbits) (~0UL >> (-(nbits) & (BITS_PER_LONG - 1)))
 
 #define DECLARE_BITMAP(name,bits)                  \
