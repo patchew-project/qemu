04a64ebd5f20b0a18561deab42b7d351015c532d
Stefan Markovic <smarkovic@wavecomp.com>
2018-08-02 16:16:26 +0200

    target/mips: Add emulation of DSP ASE for nanoMIPS - part 2
    Add emulation of DSP ASE instructions for nanoMIPS - part 2.
    
    Signed-off-by: Aleksandar Markovic <amarkovic@wavecomp.com>
    Signed-off-by: Stefan Markovic <smarkovic@wavecomp.com>
    Message-Id: <1533219424-7627-40-git-send-email-stefan.markovic@rt-rk.com>

--- target/mips/translate.c
+++ target/mips/translate.c
@@ -18792,6 +18792,16 @@ static int decode_nanomips_32_48_opc(CPUMIPSState *env, DisasContext *ctx)
                 case NM_BC1NEZC:
                     gen_compute_branch1_r6(ctx, OPC_BC1NEZ, rt, s, 0);
                     break;
+                case NM_BPOSGE32C:
+                    check_dsp(ctx);
+                    {
+                        int32_t imm = extract32(ctx->opcode, 1, 13) |
+                                      extract32(ctx->opcode, 0, 1) << 13;
+
+                        gen_compute_branch(ctx, OPC_BPOSGE32, 4, -1, -2,
+                                           imm, 4);
+                    }
+                    break;
                 default:
                     generate_exception_end(ctx, EXCP_RI);
                     break;
