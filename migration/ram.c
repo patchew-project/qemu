4bba0c6abd318b5349bef63d8c9ba02e3a75af4e
Lidong Chen <jemmy858585@gmail.com>
2018-08-03 17:13:42 +0800

    migration: implement bi-directional RDMA QIOChannel
    This patch implements bi-directional RDMA QIOChannel. Because different
    threads may access RDMAQIOChannel currently, this patch use RCU to protect it.
    
    Signed-off-by: Lidong Chen <lidongchen@tencent.com>
    Reviewed-by: Dr. David Alan Gilbert <dgilbert@redhat.com>
    Message-Id: <1533287630-4221-5-git-send-email-lidongchen@tencent.com>

--- migration/ram.c
+++ migration/ram.c
@@ -988,6 +988,7 @@ static void *multifd_send_thread(void *opaque)
     int ret;
 
     trace_multifd_send_thread_start(p->id);
+    rcu_register_thread();
 
     if (multifd_send_initial_packet(p, &local_err) < 0) {
         goto out;
@@ -1050,6 +1051,7 @@ out:
     p->running = false;
     qemu_mutex_unlock(&p->mutex);
 
+    rcu_unregister_thread();
     trace_multifd_send_thread_end(p->id, p->num_packets, p->num_pages);
 
     return NULL;
@@ -1219,6 +1221,7 @@ static void *multifd_recv_thread(void *opaque)
     int ret;
 
     trace_multifd_recv_thread_start(p->id);
+    rcu_register_thread();
 
     while (true) {
         uint32_t used;
@@ -1265,6 +1268,7 @@ static void *multifd_recv_thread(void *opaque)
     p->running = false;
     qemu_mutex_unlock(&p->mutex);
 
+    rcu_unregister_thread();
     trace_multifd_recv_thread_end(p->id, p->num_packets, p->num_pages);
 
     return NULL;
