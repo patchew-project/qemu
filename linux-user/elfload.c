a1964cf0dc51aafd742f8c4703c6a43142c8081d
Shivaprasad G Bhat <sbhat@linux.vnet.ibm.com>
2018-08-27 07:37:14 -0500

    linux-user: elf: mmap all the target-pages of hostpage for data segment
    If the hostpage size is greater than the TARGET_PAGESIZE, the
    target-pages of size TARGET_PAGESIZE are marked valid only till the
    length requested during the elfload. The glibc attempts to consume unused
    space in the last page of data segment(__libc_memalign() in
    elf/dl-minimal.c). The GLRO(dl_pagesize) is actually the host pagesize as
    set in the auxillary vectors. So, there is no explicit mmap request for
    the remaining target-pages on the last hostpage. The glibc assumes that
    particular space as available and subsequent attempts to use
    those addresses lead to crash as the target_mmap has not marked them valid
    for those target-pages.
    
    The issue is seen when trying to chroot to 16.04-x86_64 ubuntu on a PPC64
    host where the fork fails to access the thread_id as it is allocated on a
    page not marked valid. The recent glibc doesnt have checks for thread-id in
    fork, but the issue can manifest somewhere else, none the less.
    
    The fix here is to map all the target-pages of the hostpage during the
    ELF load for data segment to allow the glibc for proper consumption.
    
    Signed-off-by: Shivaprasad G Bhat <sbhat@linux.vnet.ibm.com>
    Message-Id: <153537327445.92657.5884855110375406189.stgit@lep8c.aus.stglabs.ibm.com>

--- linux-user/elfload.c
+++ linux-user/elfload.c
@@ -1438,9 +1438,17 @@ struct exec
 
 /* Necessary parameters */
 #define TARGET_ELF_EXEC_PAGESIZE TARGET_PAGE_SIZE
-#define TARGET_ELF_PAGESTART(_v) ((_v) & \
-                                 ~(abi_ulong)(TARGET_ELF_EXEC_PAGESIZE-1))
-#define TARGET_ELF_PAGEOFFSET(_v) ((_v) & (TARGET_ELF_EXEC_PAGESIZE-1))
+#define TARGET_ELF_PAGESTART(_v, _s) \
+        ((TARGET_ELF_EXEC_PAGESIZE > _s) ? \
+         (_v) & ~(abi_ulong)(TARGET_ELF_EXEC_PAGESIZE - 1) : \
+         (_v) & ~(abi_ulong)(_s - 1));
+#define TARGET_ELF_PAGEOFFSET(_v, _s) \
+        ((TARGET_ELF_EXEC_PAGESIZE > _s) ? \
+         (_v) & (TARGET_ELF_EXEC_PAGESIZE - 1) : \
+         (_v) & (_s - 1));
+#define TARGET_ELF_PAGELENGTH(_v, _s) \
+        ((TARGET_ELF_EXEC_PAGESIZE > _s) ? \
+         TARGET_PAGE_ALIGN(_v) : HOST_PAGE_ALIGN(_v));
 
 #define DLINFO_ITEMS 15
 
@@ -2279,7 +2287,7 @@ static void load_elf_image(const char *image_name, int image_fd,
     for (i = 0; i < ehdr->e_phnum; i++) {
         struct elf_phdr *eppnt = phdr + i;
         if (eppnt->p_type == PT_LOAD) {
-            abi_ulong vaddr, vaddr_po, vaddr_ps, vaddr_ef, vaddr_em;
+            abi_ulong vaddr, vaddr_po, vaddr_ps, vaddr_ef, vaddr_em, vaddr_len;
             int elf_prot = 0;
 
             if (eppnt->p_flags & PF_R) elf_prot =  PROT_READ;
@@ -2287,10 +2295,12 @@ static void load_elf_image(const char *image_name, int image_fd,
             if (eppnt->p_flags & PF_X) elf_prot |= PROT_EXEC;
 
             vaddr = load_bias + eppnt->p_vaddr;
-            vaddr_po = TARGET_ELF_PAGEOFFSET(vaddr);
-            vaddr_ps = TARGET_ELF_PAGESTART(vaddr);
+            vaddr_po = TARGET_ELF_PAGEOFFSET(vaddr, qemu_host_page_size);
+            vaddr_ps = TARGET_ELF_PAGESTART(vaddr, qemu_host_page_size);
+            vaddr_len = TARGET_ELF_PAGELENGTH(eppnt->p_filesz + vaddr_po,
+                                              qemu_host_page_size);
 
-            error = target_mmap(vaddr_ps, eppnt->p_filesz + vaddr_po,
+            error = target_mmap(vaddr_ps, vaddr_len,
                                 elf_prot, MAP_PRIVATE | MAP_FIXED,
                                 image_fd, eppnt->p_offset - vaddr_po);
             if (error == -1) {
