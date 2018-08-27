e031d671c51ddd464befa4ae8aa463fe4371c153
Marc-André Lureau <marcandre.lureau@redhat.com>
2018-08-28 00:23:15 +0200

    chardev: mark the calls that allow an implicit mux monitor
    This is mostly for readability of the code. Let's make it clear which
    callers can create an implicit monitor when the chardev is muxed.
    
    This will also enforce a safer behaviour, as we don't really support
    creating monitor anywhere/anytime at the moment. Add an assert() to
    make sure the programmer explicitely wanted that behaviour.
    
    There are documented cases, such as: -serial/-parallel/-virtioconsole
    and to less extent -debugcon.
    
    Less obvious and questionable ones are -gdb, SLIRP -guestfwd and Xen
    console. Add a FIXME note for those, but keep the support for now.
    
    Other qemu_chr_new() callers either have a fixed parameter/filename
    string or do not need it, such as -qtest:
    
    * qtest.c: qtest_init()
      Afaik, only used by tests/libqtest.c, without mux. I don't think we
      support it outside of qemu testing: drop support for implicit mux
      monitor (qemu_chr_new() call: no implicit mux now).
    
    * hw/
      All with literal @filename argument that doesn't enable mux monitor.
    
    * tests/
      All with @filename argument that doesn't enable mux monitor.
    
    On a related note, the list of monitor creation places:
    
    - the chardev creators listed above: all from command line (except
      perhaps Xen console?)
    
    - -gdb & hmp gdbserver will create a "GDB monitor command" chardev
      that is wired to an HMP monitor.
    
    - -mon command line option
    
    From this short study, I would like to think that a monitor may only
    be created in the main thread today, though I remain skeptical :)
    
    Signed-off-by: Marc-André Lureau <marcandre.lureau@redhat.com>
    Message-Id: <20180827222322.26009-3-marcandre.lureau@redhat.com>
    Reviewed-by: Markus Armbruster <armbru@redhat.com>

--- net/slirp.c
+++ net/slirp.c
@@ -836,7 +836,10 @@ static int slirp_guestfwd(SlirpState *s, const char *config_str,
         }
     } else {
         Error *err = NULL;
-        Chardev *chr = qemu_chr_new(buf, p);
+        /*
+         * FIXME: should it support implicit muxed monitors?
+         */
+        Chardev *chr = qemu_chr_new_mux_mon(buf, p);
 
         if (!chr) {
             error_setg(errp, "Could not open guest forwarding device '%s'",
