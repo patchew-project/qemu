4497c7f0482b9521258e03b35f9cd7e09ae0c23f
Fam Zheng <famz@redhat.com>
2018-09-06 13:43:40 +0800

    tests: vm: auto_install OpenBSD
    Upgrade OpenBSD to 6.3 using auto_install. Especially, drop SDL1,
    include SDL2.
    
    Signed-off-by: Fam Zheng <famz@redhat.com>
    Message-Id: <20180906054340.28988-5-famz@redhat.com>
    Tested-by: Gerd Hoffmann <kraxel@redhat.com>

--- tests/vm/basevm.py
+++ tests/vm/basevm.py
@@ -65,8 +65,6 @@ class BaseVM(object):
             self._stdout = self._devnull
         self._args = [ \
             "-nodefaults", "-m", "4G",
-            "-netdev", "user,id=vnet,hostfwd=:127.0.0.1:0-:22",
-            "-device", "virtio-net-pci,netdev=vnet",
             "-vnc", "127.0.0.1:0,to=20",
             "-serial", "file:%s" % os.path.join(self._tmpdir, "serial.out")]
         if vcpus:
@@ -145,8 +143,10 @@ class BaseVM(object):
                             "-device",
                             "virtio-blk,drive=%s,serial=%s,bootindex=1" % (name, name)]
 
-    def boot(self, img, extra_args=[]):
+    def boot(self, img, extra_args=[], extra_usernet_args=""):
         args = self._args + [
+            "-netdev", "user,id=vnet,hostfwd=:127.0.0.1:0-:22" + extra_usernet_args,
+            "-device", "virtio-net-pci,netdev=vnet",
             "-device", "VGA",
             "-drive", "file=%s,if=none,id=drive0,cache=writeback" % img,
             "-device", "virtio-blk,drive=drive0,bootindex=0"]
