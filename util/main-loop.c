477bd4545a26d82e46aa0ac829db5c66f73f5bc7
Fei Li <fli@suse.com>
2018-09-07 21:38:59 +0800

    Fix segmentation fault when qemu_signal_init fails
    Currently, when qemu_signal_init() fails it only returns a non-zero
    value but without propagating any Error. But its callers need a
    non-null err when runs error_report_err(err), or else 0->msg occurs.
    
    To avoid such segmentation fault, add a new Error parameter to make
    the call trace to propagate the err to the final caller.
    
    This patch also adds the omitted error handling when creating signalfd
    pipe fails in qemu_signalfd_compat().
    
    Signed-off-by: Fei Li <fli@suse.com>
    Message-Id: <20180907133902.28462-2-fli@suse.com>

--- util/main-loop.c
+++ util/main-loop.c
@@ -71,7 +71,7 @@ static void sigfd_handler(void *opaque)
     }
 }
 
-static int qemu_signal_init(void)
+static int qemu_signal_init(Error **errp)
 {
     int sigfd;
     sigset_t set;
@@ -94,10 +94,10 @@ static int qemu_signal_init(void)
     pthread_sigmask(SIG_BLOCK, &set, NULL);
 
     sigdelset(&set, SIG_IPI);
-    sigfd = qemu_signalfd(&set);
+    sigfd = qemu_signalfd(&set, errp);
     if (sigfd == -1) {
         fprintf(stderr, "failed to create signalfd\n");
-        return -errno;
+        return -1;
     }
 
     fcntl_setfl(sigfd, O_NONBLOCK);
@@ -109,7 +109,7 @@ static int qemu_signal_init(void)
 
 #else /* _WIN32 */
 
-static int qemu_signal_init(void)
+static int qemu_signal_init(Error **errp)
 {
     return 0;
 }
@@ -148,7 +148,7 @@ int qemu_init_main_loop(Error **errp)
 
     init_clocks(qemu_timer_notify_cb);
 
-    ret = qemu_signal_init();
+    ret = qemu_signal_init(errp);
     if (ret) {
         return ret;
     }
