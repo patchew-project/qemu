c7b75dbb128d173ed4a72812de4f22e4efbbc5bc
Marc-André Lureau <marcandre.lureau@redhat.com>
2018-09-07 11:59:42 +0400

    qom/object: register 'type' property as class property
    Let's save a few byte in each object instance.
    
    (Is this property really used anywhere? Only qom-get could read it)
    
    Signed-off-by: Marc-André Lureau <marcandre.lureau@redhat.com>
    Message-Id: <20180907075948.26917-7-marcandre.lureau@redhat.com>

--- qom/object.c
+++ qom/object.c
@@ -2423,9 +2423,10 @@ void object_class_property_set_description(ObjectClass *klass,
     op->description = g_strdup(description);
 }
 
-static void object_instance_init(Object *obj)
+static void object_class_init(ObjectClass *klass, void *data)
 {
-    object_property_add_str(obj, "type", qdev_get_type, NULL, NULL);
+    object_class_property_add_str(klass, "type", qdev_get_type,
+                                  NULL, &error_abort);
 }
 
 static void register_types(void)
@@ -2439,7 +2440,7 @@ static void register_types(void)
     static TypeInfo object_info = {
         .name = TYPE_OBJECT,
         .instance_size = sizeof(Object),
-        .instance_init = object_instance_init,
+        .class_init = object_class_init,
         .abstract = true,
     };
 
