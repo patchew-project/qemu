0a3475b89fe585fdc334f9697fad539df0cf1c52
Li Qiang <liq3ea@gmail.com>
2018-09-12 09:01:17 -0700

    hw: sun4c: add read memory region callback
    Signed-off-by: Li Qiang <liq3ea@gmail.com>
    Message-Id: <20180912160118.21158-8-liq3ea@163.com>

--- hw/sparc64/sun4u.c
+++ hw/sparc64/sun4u.c
@@ -213,6 +213,11 @@ typedef struct PowerDevice {
     MemoryRegion power_mmio;
 } PowerDevice;
 
+static uint64_t power_mem_read(void *opaque, hwaddr addr, unsigned size)
+{
+    return 0;
+}
+
 /* Power */
 static void power_mem_write(void *opaque, hwaddr addr,
                             uint64_t val, unsigned size)
@@ -224,6 +229,7 @@ static void power_mem_write(void *opaque, hwaddr addr,
 }
 
 static const MemoryRegionOps power_mem_ops = {
+    .read = power_mem_read,
     .write = power_mem_write,
     .endianness = DEVICE_NATIVE_ENDIAN,
     .valid = {
