039b3d13981cb82e76997534fdfbedc0da11e2a5
Paolo Bonzini <pbonzini@redhat.com>
2018-09-17 12:30:46 -0400

    target/mips: access cpu->interrupt_request with atomics
    Cc: Aurelien Jarno <aurelien@aurel32.net>
    Cc: Aleksandar Markovic <amarkovic@wavecomp.com>
    Cc: James Hogan <jhogan@kernel.org>
    Signed-off-by: Paolo Bonzini <pbonzini@redhat.com>
    Signed-off-by: Emilio G. Cota <cota@braap.org>
    Message-Id: <20180917163103.6113-19-cota@braap.org>
    Tested-by: Mark Cave-Ayland <mark.cave-ayland@ilande.co.uk>

--- target/mips/kvm.c
+++ target/mips/kvm.c
@@ -135,7 +135,7 @@ void kvm_arch_pre_run(CPUState *cs, struct kvm_run *run)
 
     qemu_mutex_lock_iothread();
 
-    if ((cs->interrupt_request & CPU_INTERRUPT_HARD) &&
+    if ((atomic_read(&cs->interrupt_request) & CPU_INTERRUPT_HARD) &&
             cpu_mips_io_interrupts_pending(cpu)) {
         intr.cpu = -1;
         intr.irq = 2;
