a687f574389d41aa5558f42c7a73b30084380576
Paolo Bonzini <pbonzini@redhat.com>
2018-09-17 12:30:48 -0400

    target/nios2: access cpu->interrupt_request with atomics
    Cc: Chris Wulff <crwulff@gmail.com>
    Cc: Marek Vasut <marex@denx.de>
    Signed-off-by: Paolo Bonzini <pbonzini@redhat.com>
    Signed-off-by: Emilio G. Cota <cota@braap.org>
    Message-Id: <20180917163103.6113-21-cota@braap.org>
    Tested-by: Mark Cave-Ayland <mark.cave-ayland@ilande.co.uk>

--- target/nios2/cpu.c
+++ target/nios2/cpu.c
@@ -36,7 +36,8 @@ static void nios2_cpu_set_pc(CPUState *cs, vaddr value)
 
 static bool nios2_cpu_has_work(CPUState *cs)
 {
-    return cs->interrupt_request & (CPU_INTERRUPT_HARD | CPU_INTERRUPT_NMI);
+    return atomic_read(&cs->interrupt_request) & (CPU_INTERRUPT_HARD |
+                                                  CPU_INTERRUPT_NMI);
 }
 
 /* CPUClass::reset() */
