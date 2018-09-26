a81d180f2bc68d704b91579ae8aa4883955082d4
Fei Li <fli@suse.com>
2018-09-26 18:02:44 +0800

    migration: fix the multifd code
    When multifd is used during migration, if there is an error before
    the destination receives all new channels, the destination does not
    exit but keeps waiting in our current code. However, a segmentaion
    fault will occur in the source when multifd_save_cleanup() is called
    again as the multifd_send_state has been freed earlier in the first
    error handling.  This can happen when migrate_fd_connect() fails and
    multifd_fd_cleanup() is called, and then multifd_new_send_channel_
    async() fails and multifd_save_cleanup() is called again.
    
    If the QIOChannel *c of multifd_recv_state->params[i] (p->c) is not
    initialized, there is no need to close the channel. Or else a
    segmentation fault will occur in multifd_recv_terminate_threads()
    when multifd_recv_initial_packet() fails.
    
    Signed-off-by: Fei Li <fli@suse.com>
    Message-Id: <20180926100246.14010-6-fli@suse.com>

--- migration/ram.c
+++ migration/ram.c
@@ -904,6 +904,11 @@ static void multifd_send_terminate_threads(Error *err)
         }
     }
 
+    /* in case multifd_send_state has been freed earlier */
+    if (!multifd_send_state) {
+        return;
+    }
+
     for (i = 0; i < migrate_multifd_channels(); i++) {
         MultiFDSendParams *p = &multifd_send_state->params[i];
 
@@ -919,7 +924,7 @@ int multifd_save_cleanup(Error **errp)
     int i;
     int ret = 0;
 
-    if (!migrate_use_multifd()) {
+    if (!migrate_use_multifd() || !multifd_send_state) {
         return 0;
     }
     multifd_send_terminate_threads(NULL);
@@ -1128,7 +1133,7 @@ struct {
     uint64_t packet_num;
 } *multifd_recv_state;
 
-static void multifd_recv_terminate_threads(Error *err)
+static void multifd_recv_terminate_threads(Error *err, bool channel)
 {
     int i;
 
@@ -1140,6 +1145,11 @@ static void multifd_recv_terminate_threads(Error *err)
             migrate_set_state(&s->state, s->state,
                               MIGRATION_STATUS_FAILED);
         }
+    }
+
+    /* in case p->c is not initialized */
+    if (!channel) {
+        return;
     }
 
     for (i = 0; i < migrate_multifd_channels(); i++) {
@@ -1163,7 +1173,7 @@ int multifd_load_cleanup(Error **errp)
     if (!migrate_use_multifd()) {
         return 0;
     }
-    multifd_recv_terminate_threads(NULL);
+    multifd_recv_terminate_threads(NULL, true);
     for (i = 0; i < migrate_multifd_channels(); i++) {
         MultiFDRecvParams *p = &multifd_recv_state->params[i];
 
@@ -1266,7 +1276,7 @@ static void *multifd_recv_thread(void *opaque)
     }
 
     if (local_err) {
-        multifd_recv_terminate_threads(local_err);
+        multifd_recv_terminate_threads(local_err, true);
     }
     qemu_mutex_lock(&p->mutex);
     p->running = false;
@@ -1328,7 +1338,7 @@ bool multifd_recv_new_channel(QIOChannel *ioc)
 
     id = multifd_recv_initial_packet(ioc, &local_err);
     if (id < 0) {
-        multifd_recv_terminate_threads(local_err);
+        multifd_recv_terminate_threads(local_err, false);
         return false;
     }
 
@@ -1336,7 +1346,7 @@ bool multifd_recv_new_channel(QIOChannel *ioc)
     if (p->c != NULL) {
         error_setg(&local_err, "multifd: received id '%d' already setup'",
                    id);
-        multifd_recv_terminate_threads(local_err);
+        multifd_recv_terminate_threads(local_err, true);
         return false;
     }
     p->c = ioc;
