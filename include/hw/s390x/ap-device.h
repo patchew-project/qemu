f120f4f086afad8a062c61e76334492f5435611b
Tony Krowiak <akrowiak@linux.ibm.com>
2018-10-09 13:52:24 -0400

    s390x/ap: base Adjunct Processor (AP) object model
    Introduces the base object model for virtualizing AP devices.
    
    Signed-off-by: Tony Krowiak <akrowiak@linux.ibm.com>
    Tested-by: Pierre Morel<pmorel@linux.ibm.com>
    Acked-by: David Hildenbrand <david@redhat.com>
    Message-Id: <20181009175226.22138-5-akrowiak@linux.vnet.ibm.com>
    Reviewed-by: Halil Pasic <pasic@linux.ibm.com>
    Reviewed-by: Thomas Huth <thuth@redhat.com>

---
+++ include/hw/s390x/ap-device.h
@@ -0,0 +1,23 @@
+/*
+ * Adjunct Processor (AP) matrix device interfaces
+ *
+ * Copyright 2018 IBM Corp.
+ * Author(s): Tony Krowiak <akrowiak@linux.vnet.ibm.com>
+ *
+ * This work is licensed under the terms of the GNU GPL, version 2 or (at
+ * your option) any later version. See the COPYING file in the top-level
+ * directory.
+ */
+#ifndef HW_S390X_AP_DEVICE_H
+#define HW_S390X_AP_DEVICE_H
+
+#define AP_DEVICE_TYPE       "ap-device"
+
+typedef struct APDevice {
+    DeviceState parent_obj;
+} APDevice;
+
+#define AP_DEVICE(obj) \
+    OBJECT_CHECK(APDevice, (obj), AP_DEVICE_TYPE)
+
+#endif /* HW_S390X_AP_DEVICE_H */
