2f635b39593f9dd1463f97aa10f6d4fae54bec29
Max Reitz <mreitz@redhat.com>
2018-10-15 19:28:16 +0200

    object: Make option help nicer to read
    Just like in qemu_opts_print_help(), print the object name as a caption
    instead of on every single line, indent all options, and replace the '='
    by ': '.
    
    Also, indent every object name in the list of available objects.
    
    Signed-off-by: Max Reitz <mreitz@redhat.com>
    Message-Id: <20181015172817.19796-5-mreitz@redhat.com>

--- vl.c
+++ vl.c
@@ -2707,7 +2707,7 @@ static bool object_create_initial(const char *type, QemuOpts *opts)
         list = object_class_get_list_sorted(TYPE_USER_CREATABLE, false);
         for (l = list; l != NULL; l = l->next) {
             ObjectClass *oc = OBJECT_CLASS(l->data);
-            printf("%s\n", object_class_get_name(oc));
+            printf("  %s\n", object_class_get_name(oc));
         }
         g_slist_free(list);
         exit(0);
@@ -2729,16 +2729,20 @@ static bool object_create_initial(const char *type, QemuOpts *opts)
             }
 
             str = g_string_new(NULL);
-            g_string_append_printf(str, "%s.%s=%s", type,
-                                   prop->name, prop->type);
+            g_string_append_printf(str, "%s: %s", prop->name, prop->type);
             if (prop->description) {
                 g_string_append_printf(str, " - %s", prop->description);
             }
             g_ptr_array_add(array, g_string_free(str, false));
         }
         g_ptr_array_sort(array, (GCompareFunc)qemu_pstrcmp0);
+        if (array->len > 0) {
+            printf("%s options:\n", type);
+        } else {
+            printf("There are no options for %s.\n", type);
+        }
         for (i = 0; i < array->len; i++) {
-            printf("%s\n", (char *)array->pdata[i]);
+            printf("  %s\n", (char *)array->pdata[i]);
         }
         g_ptr_array_set_free_func(array, g_free);
         g_ptr_array_free(array, true);
