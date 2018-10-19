1d19c83818187f5678473dd9649832b58cf133ab
Emilio G. Cota <cota@braap.org>
2018-10-18 21:05:44 -0400

    mips: convert to cpu_halted
    Cc: Aurelien Jarno <aurelien@aurel32.net>
    Cc: Aleksandar Markovic <amarkovic@wavecomp.com>
    Cc: James Hogan <jhogan@kernel.org>
    Signed-off-by: Emilio G. Cota <cota@braap.org>
    Message-Id: <20181019010625.25294-16-cota@braap.org>
    Reviewed-by: Richard Henderson <richard.henderson@linaro.org>

--- target/mips/translate.c
+++ target/mips/translate.c
@@ -26432,7 +26432,7 @@ void cpu_state_reset(CPUMIPSState *env)
             env->tcs[i].CP0_TCHalt = 1;
         }
         env->active_tc.CP0_TCHalt = 1;
-        cs->halted = 1;
+        cpu_halted_set(cs, 1);
 
         if (cs->cpu_index == 0) {
             /* VPE0 starts up enabled.  */
@@ -26440,7 +26440,7 @@ void cpu_state_reset(CPUMIPSState *env)
             env->CP0_VPEConf0 |= (1 << CP0VPEC0_MVP) | (1 << CP0VPEC0_VPA);
 
             /* TC0 starts up unhalted.  */
-            cs->halted = 0;
+            cpu_halted_set(cs, 0);
             env->active_tc.CP0_TCHalt = 0;
             env->tcs[0].CP0_TCHalt = 0;
             /* With thread 0 active.  */
