d1afc352fc16a502f36e7d01302c3e8fc25b85c0
Marc-André Lureau <marcandre.lureau@redhat.com>
2018-10-30 19:04:52 +0400

    hw/i386: add pc-i440fx-3.1 & pc-q35-3.1
    The following patch is going to add compatiblity parameters.
    
    Signed-off-by: Marc-André Lureau <marcandre.lureau@redhat.com>
    Reviewed-by: Michael S. Tsirkin <mst@redhat.com>
    Message-Id: <20181030150453.9344-10-marcandre.lureau@redhat.com>
    Reviewed-by: Eduardo Habkost <ehabkost@redhat.com>
    Reviewed-by: Igor Mammedov <imammedo@redhat.com>

--- include/hw/i386/pc.h
+++ include/hw/i386/pc.h
@@ -300,7 +300,7 @@ bool e820_get_entry(int, uint32_t, uint64_t *, uint64_t *);
         .driver   = TYPE_X86_CPU,\
         .property = "x-hv-synic-kvm-only",\
         .value    = "on",\
-    }
+    },
 
 #define PC_COMPAT_2_12 \
     HW_COMPAT_2_12 \
