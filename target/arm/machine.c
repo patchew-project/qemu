5a3876bb4d30dc2c0b4c3a5df599779e66f833bb
Aaron Lindsay <aaron@os.amperecomputing.com>
2018-11-05 18:51:53 +0000

    target/arm: Swap PMU values before/after migrations
    Because of the PMU's design, many register accesses have side effects
    which are inter-related, meaning that the normal method of saving CP
    registers can result in inconsistent state. These side-effects are
    largely handled in pmu_op_start/finish functions which can be called
    before and after the state is saved/restored. By doing this and adding
    raw read/write functions for the affected registers, we avoid
    migration-related inconsistencies.
    
    Signed-off-by: Aaron Lindsay <aclindsa@gmail.com>
    Message-Id: <20181105185046.2802-4-aaron@os.amperecomputing.com>

--- target/arm/machine.c
+++ target/arm/machine.c
@@ -604,6 +604,8 @@ static int cpu_pre_save(void *opaque)
 {
     ARMCPU *cpu = opaque;
 
+    pmu_op_start(&cpu->env);
+
     if (kvm_enabled()) {
         if (!write_kvmstate_to_list(cpu)) {
             /* This should never fail */
@@ -622,6 +624,20 @@ static int cpu_pre_save(void *opaque)
     memcpy(cpu->cpreg_vmstate_values, cpu->cpreg_values,
            cpu->cpreg_array_len * sizeof(uint64_t));
 
+    return 0;
+}
+
+static int cpu_post_save(void *opaque)
+{
+    ARMCPU *cpu = opaque;
+    pmu_op_finish(&cpu->env);
+    return 0;
+}
+
+static int cpu_pre_load(void *opaque)
+{
+    ARMCPU *cpu = opaque;
+    pmu_op_start(&cpu->env);
     return 0;
 }
 
@@ -672,6 +688,8 @@ static int cpu_post_load(void *opaque, int version_id)
     hw_breakpoint_update_all(cpu);
     hw_watchpoint_update_all(cpu);
 
+    pmu_op_finish(&cpu->env);
+
     return 0;
 }
 
@@ -680,6 +698,8 @@ const VMStateDescription vmstate_arm_cpu = {
     .version_id = 22,
     .minimum_version_id = 22,
     .pre_save = cpu_pre_save,
+    .post_save = cpu_post_save,
+    .pre_load = cpu_pre_load,
     .post_load = cpu_post_load,
     .fields = (VMStateField[]) {
         VMSTATE_UINT32_ARRAY(env.regs, ARMCPU, 16),
