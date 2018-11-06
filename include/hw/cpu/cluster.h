daeb66687129945b5783ca3b105613bd10298309
Luc Michel <luc.michel@greensocs.com>
2018-11-06 12:05:33 +0100

    hw/cpu: introduce CPU clusters
    This commit adds the cpu-cluster type. It aims at gathering CPUs from
    the same cluster in a machine.
    
    For now it only has a `cluster-id` property.
    
    Signed-off-by: Luc Michel <luc.michel@greensocs.com>
    Reviewed-by: Alistair Francis <alistair.francis@wdc.com>
    Message-Id: <20181106110548.4209-2-luc.michel@greensocs.com>
    Tested-by: Philippe Mathieu-Daudé <philmd@redhat.com>
    Reviewed-by: Philippe Mathieu-Daudé <philmd@redhat.com>

---
+++ include/hw/cpu/cluster.h
@@ -0,0 +1,38 @@
+/*
+ * QEMU CPU cluster
+ *
+ * Copyright (c) 2018 GreenSocs SAS
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, see
+ * <http://www.gnu.org/licenses/gpl-2.0.html>
+ */
+#ifndef HW_CPU_CLUSTER_H
+#define HW_CPU_CLUSTER_H
+
+#include "qemu/osdep.h"
+#include "hw/qdev.h"
+
+#define TYPE_CPU_CLUSTER "cpu-cluster"
+#define CPU_CLUSTER(obj) \
+    OBJECT_CHECK(CPUClusterState, (obj), TYPE_CPU_CLUSTER)
+
+typedef struct CPUClusterState {
+    /*< private >*/
+    DeviceState parent_obj;
+
+    /*< public >*/
+    uint64_t cluster_id;
+} CPUClusterState;
+
+#endif
