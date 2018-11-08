d24c181e25cfe9e5281e256bbe8ab745c24e9a17
Corey Minyard <cminyard@mvista.com>
2018-11-08 08:19:43 -0600

    qdev: Add a no default uuid property
    This is for IPMI, which will behave differently if the UUID is
    not set.
    
    Signed-off-by: Corey Minyard <cminyard@mvista.com>
    Cc: Fam Zheng <famz@redhat.com>
    Cc: Michael S. Tsirkin <mst@redhat.com>
    Cc: Marc-André Lureau <marcandre.lureau@redhat.com>
    Message-Id: <20181108141944.15769-2-minyard@acm.org>

--- include/hw/qdev-properties.h
+++ include/hw/qdev-properties.h
@@ -226,6 +226,13 @@ extern const PropertyInfo qdev_prop_off_auto_pcibar;
         .set_default = true,                                       \
         }
 
+#define DEFINE_PROP_UUID_NODEFAULT(_name, _state, _field) {        \
+        .name      = (_name),                                      \
+        .info      = &qdev_prop_uuid,                              \
+        .offset    = offsetof(_state, _field)                      \
+            + type_check(QemuUUID, typeof_field(_state, _field)),  \
+        }
+
 #define DEFINE_PROP_END_OF_LIST()               \
     {}
 
