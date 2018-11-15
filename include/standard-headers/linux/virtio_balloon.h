f36f77cf7f5a8c46082af67a6e4494410a89d364
Wei Wang <wei.w.wang@intel.com>
2018-11-15 18:08:04 +0800

    virtio-balloon: VIRTIO_BALLOON_F_FREE_PAGE_HINT
    The new feature enables the virtio-balloon device to receive hints of
    guest free pages from the free page vq.
    
    A notifier is registered to the migration precopy notifier chain. The
    notifier calls free_page_start after the migration thread syncs the dirty
    bitmap, so that the free page optimization starts to clear bits of free
    pages from the bitmap. It calls the free_page_stop before the migration
    thread syncs the bitmap, which is the end of the current round of ram
    save. The free_page_stop is also called to stop the optimization in the
    case when there is an error occurred in the process of ram saving.
    
    Note: balloon will report pages which were free at the time of this call.
    As the reporting happens asynchronously, dirty bit logging must be
    enabled before this free_page_start call is made. Guest reporting must be
    disabled before the migration dirty bitmap is synchronized.
    
    Signed-off-by: Wei Wang <wei.w.wang@intel.com>
    CC: Michael S. Tsirkin <mst@redhat.com>
    CC: Dr. David Alan Gilbert <dgilbert@redhat.com>
    CC: Juan Quintela <quintela@redhat.com>
    CC: Peter Xu <peterx@redhat.com>
    Message-Id: <1542276484-25508-9-git-send-email-wei.w.wang@intel.com>

--- include/standard-headers/linux/virtio_balloon.h
+++ include/standard-headers/linux/virtio_balloon.h
@@ -34,15 +34,20 @@
 #define VIRTIO_BALLOON_F_MUST_TELL_HOST	0 /* Tell before reclaiming pages */
 #define VIRTIO_BALLOON_F_STATS_VQ	1 /* Memory Stats virtqueue */
 #define VIRTIO_BALLOON_F_DEFLATE_ON_OOM	2 /* Deflate balloon on OOM */
+#define VIRTIO_BALLOON_F_FREE_PAGE_HINT 3 /* VQ to report free pages */
 
 /* Size of a PFN in the balloon interface. */
 #define VIRTIO_BALLOON_PFN_SHIFT 12
 
+#define VIRTIO_BALLOON_FREE_PAGE_REPORT_STOP_ID 0
+#define VIRTIO_BALLOON_FREE_PAGE_REPORT_DONE_ID 1
 struct virtio_balloon_config {
 	/* Number of pages host wants Guest to give up. */
 	uint32_t num_pages;
 	/* Number of pages we've actually got in balloon. */
 	uint32_t actual;
+	/* Free page report command id, readonly by guest */
+	uint32_t free_page_report_cmd_id;
 };
 
 #define VIRTIO_BALLOON_S_SWAP_IN  0   /* Amount of memory swapped in */
