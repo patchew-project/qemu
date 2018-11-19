b04ead8901f3c6630230c4635b1d8e14036f68da
Mao Zhongyi <maozhongyi@cmss.chinamobile.com>
2018-11-19 20:08:15 +0800

    timer/grlib_gptimer: Convert sysbus init function to realize function
    Use DeviceClass rather than SysBusDeviceClass in
    grlib_gptimer_class_init().
    
    Cc: chouteau@adacore.com
    
    Signed-off-by: Mao Zhongyi <maozhongyi@cmss.chinamobile.com>
    Signed-off-by: Zhang Shengju <zhangshengju@cmss.chinamobile.com>
    Message-Id: <20181119120820.29878-18-maozhongyi@cmss.chinamobile.com>

--- hw/timer/grlib_gptimer.c
+++ hw/timer/grlib_gptimer.c
@@ -347,7 +347,7 @@ static void grlib_gptimer_reset(DeviceState *d)
     }
 }
 
-static int grlib_gptimer_init(SysBusDevice *dev)
+static void grlib_gptimer_realize(DeviceState *dev, Error **errp)
 {
     GPTimerUnit  *unit = GRLIB_GPTIMER(dev);
     unsigned int  i;
@@ -366,7 +366,7 @@ static int grlib_gptimer_init(SysBusDevice *dev)
         timer->id     = i;
 
         /* One IRQ line for each timer */
-        sysbus_init_irq(dev, &timer->irq);
+        sysbus_init_irq(SYS_BUS_DEVICE(dev), &timer->irq);
 
         ptimer_set_freq(timer->ptimer, unit->freq_hz);
     }
@@ -375,8 +375,7 @@ static int grlib_gptimer_init(SysBusDevice *dev)
                           unit, "gptimer",
                           UNIT_REG_SIZE + GPTIMER_REG_SIZE * unit->nr_timers);
 
-    sysbus_init_mmio(dev, &unit->iomem);
-    return 0;
+    sysbus_init_mmio(SYS_BUS_DEVICE(dev), &unit->iomem);
 }
 
 static Property grlib_gptimer_properties[] = {
@@ -389,9 +388,8 @@ static Property grlib_gptimer_properties[] = {
 static void grlib_gptimer_class_init(ObjectClass *klass, void *data)
 {
     DeviceClass *dc = DEVICE_CLASS(klass);
-    SysBusDeviceClass *k = SYS_BUS_DEVICE_CLASS(klass);
 
-    k->init = grlib_gptimer_init;
+    dc->realize = grlib_gptimer_realize;
     dc->reset = grlib_gptimer_reset;
     dc->props = grlib_gptimer_properties;
 }
