f01aa3573fb2503951bb93af90f3f3ba1e01e4b4
Eric Auger <eric.auger@redhat.com>
2018-11-26 16:46:16 +0100

    hw/arm/virt-acpi-build: Fix SMMUv3 ACPI integration
    The AcpiIortSmmu3 misses 2 32b fields corresponding to the
    proximity domain and the device id mapping index.
    
    Also let's report IO-coherent access is supported for
    translation table walks, descriptor fetches and queues by
    setting the COHACC override flag. Without that, we observe
    wrong command opcodes. The DT description also advertises
    the dma coherency.
    
    Fixes a703b4f6c1ee ("hw/arm/virt-acpi-build: Add smmuv3 node in IORT table")
    
    Signed-off-by: Eric Auger <eric.auger@redhat.com>
    Reported-by: Shameerali Kolothum Thodi <shameerali.kolothum.thodi@huawei.com>
    Message-Id: <20181126154616.18173-1-eric.auger@redhat.com>

--- include/hw/acpi/acpi-defs.h
+++ include/hw/acpi/acpi-defs.h
@@ -628,6 +628,12 @@ struct AcpiIortItsGroup {
 } QEMU_PACKED;
 typedef struct AcpiIortItsGroup AcpiIortItsGroup;
 
+enum {
+    ACPI_IORT_SMMU_V3_COHACC_OVERRIDE = 1 << 0,
+    ACPI_IORT_SMMU_V3_HTTU_OVERRIDE   = 3 << 1,
+    ACPI_IORT_SMMU_V3_PXM_VALID       = 1 << 3
+};
+
 struct AcpiIortSmmu3 {
     ACPI_IORT_NODE_HEADER_DEF
     uint64_t base_address;
@@ -639,6 +645,8 @@ struct AcpiIortSmmu3 {
     uint32_t pri_gsiv;
     uint32_t gerr_gsiv;
     uint32_t sync_gsiv;
+    uint32_t pxm;
+    uint32_t id_mapping_index;
     AcpiIortIdMapping id_mapping_array[0];
 } QEMU_PACKED;
 typedef struct AcpiIortSmmu3 AcpiIortSmmu3;
