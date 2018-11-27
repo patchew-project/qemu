31f5bd6617a3bd1bd3b59f29357977ed4ce4d412
Marc-André Lureau <marcandre.lureau@redhat.com>
2018-11-27 13:27:59 +0400

    hw/i386: add pc-i440fx-3.2 & pc-q35-3.2
    The following patch is going to add compatiblity parameters for
    qemu <= 3.1.
    
    Signed-off-by: Marc-André Lureau <marcandre.lureau@redhat.com>
    Message-Id: <20181127092801.21777-27-marcandre.lureau@redhat.com>

--- hw/i386/pc_piix.c
+++ hw/i386/pc_piix.c
@@ -428,11 +428,22 @@ static void pc_i440fx_machine_options(MachineClass *m)
     machine_class_allow_dynamic_sysbus_dev(m, TYPE_RAMFB_DEVICE);
 }
 
-static void pc_i440fx_3_1_machine_options(MachineClass *m)
+static void pc_i440fx_3_2_machine_options(MachineClass *m)
 {
     pc_i440fx_machine_options(m);
     m->alias = "pc";
     m->is_default = 1;
+}
+
+DEFINE_I440FX_MACHINE(v3_2, "pc-i440fx-3.2", NULL,
+                      pc_i440fx_3_2_machine_options);
+
+static void pc_i440fx_3_1_machine_options(MachineClass *m)
+{
+    pc_i440fx_3_2_machine_options(m);
+    m->is_default = 0;
+    m->alias = NULL;
+    SET_COMPAT(m, PC_COMPAT_3_1);
 }
 
 DEFINE_I440FX_MACHINE(v3_1, "pc-i440fx-3.1", NULL,
@@ -441,8 +452,6 @@ DEFINE_I440FX_MACHINE(v3_1, "pc-i440fx-3.1", NULL,
 static void pc_i440fx_3_0_machine_options(MachineClass *m)
 {
     pc_i440fx_3_1_machine_options(m);
-    m->is_default = 0;
-    m->alias = NULL;
     SET_COMPAT(m, PC_COMPAT_3_0);
 }
 
