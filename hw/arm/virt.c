4cff2be62f3d941ae3de3284e74668039fa4bce8
Peng Hao <peng.hao2@zte.com.cn>
2018-11-28 20:12:50 +0800

    hw/misc/pvpanic: preparing for adding configure interface
    Prepare for pvpanic-mmio configure interface.
    
    Signed-off-by: Peng Hao <peng.hao2@zte.com.cn>
    Message-Id: <1543407172-16175-8-git-send-email-peng.hao2@zte.com.cn>

--- hw/arm/virt.c
+++ hw/arm/virt.c
@@ -59,6 +59,7 @@
 #include "qapi/visitor.h"
 #include "standard-headers/linux/input.h"
 #include "hw/arm/smmuv3.h"
+#include "hw/misc/pvpanic.h"
 
 #define DEFINE_VIRT_MACHINE_LATEST(major, minor, latest) \
     static void virt_##major##_##minor##_class_init(ObjectClass *oc, \
@@ -1784,6 +1785,7 @@ static void virt_machine_class_init(ObjectClass *oc, void *data)
     machine_class_allow_dynamic_sysbus_dev(mc, TYPE_VFIO_CALXEDA_XGMAC);
     machine_class_allow_dynamic_sysbus_dev(mc, TYPE_VFIO_AMD_XGBE);
     machine_class_allow_dynamic_sysbus_dev(mc, TYPE_RAMFB_DEVICE);
+    machine_class_allow_dynamic_sysbus_dev(mc, TYPE_PVPANIC_MMIO);
     machine_class_allow_dynamic_sysbus_dev(mc, TYPE_VFIO_PLATFORM);
     mc->block_default_type = IF_VIRTIO;
     mc->no_cdrom = 1;
