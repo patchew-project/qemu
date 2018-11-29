bfc9fb53638aa854631017e16e192f3cfc9e300c
Mikhail Abakumov <mikhail.abakumov@ispras.ru>
2018-11-29 17:28:03 +0300

    windbg: add chardev
    Add chardev for listening to windbg client. Target device is a parameter
    in the '-windbg' option.
    
    Signed-off-by: Mikhail Abakumov <mikhail.abakumov@ispras.ru>
    Signed-off-by: Pavel Dovgalyuk <dovgaluk@ispras.ru>
    Message-Id: <154350168373.8036.7695646527428454601.stgit@Misha-PC.lan02.inno>
    Tested-by: Ladi Prosek <lprosek@redhat.com>

--- windbgstub.c
+++ windbgstub.c
@@ -10,6 +10,10 @@
  */
 
 #include "qemu/osdep.h"
+#include "qapi/error.h"
+#include "chardev/char.h"
+#include "chardev/char-fe.h"
+#include "qemu/cutils.h"
 #include "exec/windbgstub.h"
 #include "exec/windbgstub-utils.h"
 
@@ -18,6 +22,8 @@ typedef struct WindbgState {
     bool catched_breakin_byte;
     uint32_t wait_packet_type;
     uint32_t curr_packet_id;
+
+    CharBackend chr;
 } WindbgState;
 
 static WindbgState *windbg_state;
@@ -30,6 +36,15 @@ static void windbg_state_clean(WindbgState *state)
     state->curr_packet_id = INITIAL_PACKET_ID | SYNC_PACKET_ID;
 }
 
+static int windbg_chr_can_receive(void *opaque)
+{
+    return PACKET_MAX_SIZE;
+}
+
+static void windbg_chr_receive(void *opaque, const uint8_t *buf, int size)
+{
+}
+
 static void windbg_exit(void)
 {
     g_free(windbg_state);
@@ -37,13 +52,29 @@ static void windbg_exit(void)
 
 int windbg_server_start(const char *device)
 {
+    Chardev *chr = NULL;
+
     if (windbg_state) {
         WINDBG_ERROR("Multiple instances of windbg are not supported.");
         exit(1);
     }
 
+    if (!strstart(device, "pipe:", NULL)) {
+        WINDBG_ERROR("Unsupported device. Supported only pipe.");
+        exit(1);
+    }
+
     windbg_state = g_new0(WindbgState, 1);
     windbg_state_clean(windbg_state);
+
+    chr = qemu_chr_new_noreplay("windbg", device, true);
+    if (!chr) {
+        return -1;
+    }
+
+    qemu_chr_fe_init(&windbg_state->chr, chr, &error_abort);
+    qemu_chr_fe_set_handlers(&windbg_state->chr, windbg_chr_can_receive,
+                             windbg_chr_receive, NULL, NULL, NULL, NULL, true);
 
     atexit(windbg_exit);
     return 0;
