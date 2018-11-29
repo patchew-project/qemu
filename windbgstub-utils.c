97b20af7ea261aaa63f6210d5d33ae5022aee6e5
Mikhail Abakumov <mikhail.abakumov@ispras.ru>
2018-11-29 17:31:05 +0300

    windbg: implement kd_api_query_memory
    Signed-off-by: Mikhail Abakumov <mikhail.abakumov@ispras.ru>
    Signed-off-by: Pavel Dovgalyuk <dovgaluk@ispras.ru>
    Message-Id: <154350186485.8036.2655284817381568392.stgit@Misha-PC.lan02.inno>
    Tested-by: Ladi Prosek <lprosek@redhat.com>

--- windbgstub-utils.c
+++ windbgstub-utils.c
@@ -476,6 +476,16 @@ void kd_api_fill_memory(CPUState *cs, PacketData *pd)
     g_free(mem);
 }
 
+void kd_api_query_memory(CPUState *cs, PacketData *pd)
+{
+    DBGKD_QUERY_MEMORY *mem = &pd->m64.u.QueryMemory;
+
+    /* TODO: Needs test memory. */
+    stl_p(&mem->AddressSpace, DBGKD_QUERY_MEMORY_PROCESS);
+    stl_p(&mem->Flags, DBGKD_QUERY_MEMORY_READ | DBGKD_QUERY_MEMORY_WRITE |
+                       DBGKD_QUERY_MEMORY_EXECUTE);
+}
+
 void kd_api_unsupported(CPUState *cs, PacketData *pd)
 {
     WINDBG_ERROR("Caught unimplemented api %s", kd_api_name(pd->m64.ApiNumber));
