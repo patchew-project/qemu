e444486a908d6e1091b81e0eadee3cc215ff16a7
Mikhail Abakumov <mikhail.abakumov@ispras.ru>
2018-12-05 15:54:01 +0300

    windbg: implement kd_api_read_virtual_memory and kd_api_write_virtual_memory
    Signed-off-by: Mikhail Abakumov <mikhail.abakumov@ispras.ru>
    Signed-off-by: Pavel Dovgalyuk <dovgaluk@ispras.ru>
    Message-Id: <154401444081.8440.12026768872502273785.stgit@Misha-PC.lan02.inno>
    Tested-by: Ladi Prosek <lprosek@redhat.com>

--- windbgstub-utils.c
+++ windbgstub-utils.c
@@ -200,6 +200,53 @@ InitedAddr windbg_search_vmaddr(CPUState *cs, target_ulong start,
     return ret;
 }
 
+void kd_api_read_virtual_memory(CPUState *cs, PacketData *pd)
+{
+    DBGKD_READ_MEMORY64 *mem = &pd->m64.u.ReadMemory;
+    uint32_t len;
+    target_ulong addr;
+    int err;
+
+    len = MIN(ldl_p(&mem->TransferCount),
+              PACKET_MAX_SIZE - sizeof(DBGKD_MANIPULATE_STATE64));
+    addr = ldtul_p(&mem->TargetBaseAddress);
+    err = cpu_memory_rw_debug(cs, addr, pd->extra, len, 0);
+
+    if (err) {
+        len = 0;
+        pd->m64.ReturnStatus = STATUS_UNSUCCESSFUL;
+
+        DPRINTF("read_virtual_memory: No physical page mapped: " FMT_ADDR "\n",
+                addr);
+    }
+
+    pd->extra_size = len;
+    stl_p(&mem->ActualBytesRead, len);
+}
+
+void kd_api_write_virtual_memory(CPUState *cs, PacketData *pd)
+{
+    DBGKD_WRITE_MEMORY64 *mem = &pd->m64.u.WriteMemory;
+    uint32_t len;
+    target_ulong addr;
+    int err;
+
+    len = MIN(ldl_p(&mem->TransferCount), pd->extra_size);
+    addr = ldtul_p(&mem->TargetBaseAddress);
+    err = cpu_memory_rw_debug(cs, addr, pd->extra, len, 1);
+
+    if (err) {
+        len = 0;
+        pd->m64.ReturnStatus = STATUS_UNSUCCESSFUL;
+
+        DPRINTF("read_write_memory: No physical page mapped: " FMT_ADDR "\n",
+                addr);
+    }
+
+    pd->extra_size = 0;
+    stl_p(&mem->ActualBytesWritten, len);
+}
+
 void kd_api_unsupported(CPUState *cs, PacketData *pd)
 {
     WINDBG_ERROR("Caught unimplemented api %s", kd_api_name(pd->m64.ApiNumber));
