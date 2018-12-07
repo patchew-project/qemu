4e77329932e00a3d95f9513cf7c58e8b92426786
Andrey Shinkevich <andrey.shinkevich@virtuozzo.com>
2018-12-07 13:00:49 +0300

    qemu-img info lists bitmap directory entries
    In the 'Format specific information' section of the 'qemu-img info'
    command output, the supplemental information about existing QCOW2
    bitmaps will be shown, such as a bitmap name, flags and granularity:
    
    image: /vz/vmprivate/VM1/harddisk.hdd
    file format: qcow2
    virtual size: 64G (68719476736 bytes)
    disk size: 3.0M
    cluster_size: 1048576
    Format specific information:
        compat: 1.1
        lazy refcounts: true
        bitmaps:
            [0]:
                flags:
                    [0]: in-use
                    [1]: auto
                name: back-up1
                unknown flags: 4
                granularity: 65536
            [1]:
                flags:
                    [0]: in-use
                    [1]: auto
                name: back-up2
                unknown flags: 8
                granularity: 65536
        refcount bits: 16
        corrupt: false
    
    Signed-off-by: Andrey Shinkevich <andrey.shinkevich@virtuozzo.com>
    Reviewed-by: Vladimir Sementsov-Ogievskiy <vsementsov@virtuozzo.com>
    Message-Id: <1544176849-899477-1-git-send-email-andrey.shinkevich@virtuozzo.com>

--- block/qcow2.c
+++ block/qcow2.c
@@ -4270,6 +4270,12 @@ static ImageInfoSpecific *qcow2_get_specific_info(BlockDriverState *bs)
             .refcount_bits      = s->refcount_bits,
         };
     } else if (s->qcow_version == 3) {
+        Qcow2BitmapInfoList *bitmaps;
+        Error *local_err = NULL;
+        bitmaps = qcow2_get_bitmap_info_list(bs, &local_err);
+        if (local_err != NULL) {
+            error_report_err(local_err);
+        }
         *spec_info->u.qcow2.data = (ImageInfoSpecificQCow2){
             .compat             = g_strdup("1.1"),
             .lazy_refcounts     = s->compatible_features &
@@ -4279,6 +4285,8 @@ static ImageInfoSpecific *qcow2_get_specific_info(BlockDriverState *bs)
                                   QCOW2_INCOMPAT_CORRUPT,
             .has_corrupt        = true,
             .refcount_bits      = s->refcount_bits,
+            .has_bitmaps        = !!bitmaps,
+            .bitmaps            = bitmaps,
         };
     } else {
         /* if this assertion fails, this probably means a new version was
