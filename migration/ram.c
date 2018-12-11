94a780a784437096c913afc3d9fcf297bbe2b9ec
Fei Li <fli@suse.com>
2018-12-11 17:50:53 +0800

    migration: fix the multifd code when receiving less channels
    In our current code, when multifd is used during migration, if there
    is an error before the destination receives all new channels, the
    source keeps running, however the destination does not exit but keeps
    waiting until the source is killed deliberately.
    
    Fix this by dumping the specific error and let users decide whether
    to quit from the destination side when failing to receive packet via
    some channel.
    
    Cc: Dr. David Alan Gilbert <dgilbert@redhat.com>
    Signed-off-by: Fei Li <fli@suse.com>
    Reviewed-by: Peter Xu <peterx@redhat.com>
    Message-Id: <20181211095057.14623-4-fli@suse.com>

--- migration/ram.c
+++ migration/ram.c
@@ -1323,7 +1323,7 @@ bool multifd_recv_all_channels_created(void)
 }
 
 /* Return true if multifd is ready for the migration, otherwise false */
-bool multifd_recv_new_channel(QIOChannel *ioc)
+bool multifd_recv_new_channel(QIOChannel *ioc, Error **errp)
 {
     MultiFDRecvParams *p;
     Error *local_err = NULL;
@@ -1331,6 +1331,10 @@ bool multifd_recv_new_channel(QIOChannel *ioc)
 
     id = multifd_recv_initial_packet(ioc, &local_err);
     if (id < 0) {
+        error_propagate_prepend(errp, local_err,
+                                "failed to receive packet"
+                                " via multifd channel %d: ",
+                                atomic_read(&multifd_recv_state->count));
         multifd_recv_terminate_threads(local_err);
         return false;
     }
@@ -1340,6 +1344,7 @@ bool multifd_recv_new_channel(QIOChannel *ioc)
         error_setg(&local_err, "multifd: received id '%d' already setup'",
                    id);
         multifd_recv_terminate_threads(local_err);
+        error_propagate(errp, local_err);
         return false;
     }
     p->c = ioc;
@@ -1351,7 +1356,8 @@ bool multifd_recv_new_channel(QIOChannel *ioc)
     qemu_thread_create(&p->thread, p->name, multifd_recv_thread, p,
                        QEMU_THREAD_JOINABLE);
     atomic_inc(&multifd_recv_state->count);
-    return multifd_recv_state->count == migrate_multifd_channels();
+    return atomic_read(&multifd_recv_state->count) ==
+           migrate_multifd_channels();
 }
 
 /**
