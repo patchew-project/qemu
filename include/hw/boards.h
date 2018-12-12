15278affa786a5328baef55444a3b617c1111ad4
Marc-André Lureau <marcandre.lureau@redhat.com>
2018-12-13 01:48:40 +0400

    compat: replace PC_COMPAT_2_2 & HW_COMPAT_2_2 macros
    Use static arrays instead.  I decided to rename the conflicting
    pc_compat_2_2() function with pc_compat_2_2_fn().
    
    Suggested-by: Eduardo Habkost <ehabkost@redhat.com>
    Signed-off-by: Marc-André Lureau <marcandre.lureau@redhat.com>
    Message-Id: <20181212214850.29953-19-marcandre.lureau@redhat.com>

--- include/hw/boards.h
+++ include/hw/boards.h
@@ -323,4 +323,7 @@ extern const size_t hw_compat_2_4_len;
 extern GlobalProperty hw_compat_2_3[];
 extern const size_t hw_compat_2_3_len;
 
+extern GlobalProperty hw_compat_2_2[];
+extern const size_t hw_compat_2_2_len;
+
 #endif
