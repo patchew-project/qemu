f5a08ba3c86e1c4a25a73b6b1e3a5a296f45dd20
Philippe Mathieu-Daudé <philmd@redhat.com>
2018-12-18 18:51:20 +0100

    hw/acpi: Use QEMU_NONSTRING for non NUL-terminated arrays
    GCC 8 added a -Wstringop-truncation warning:
    
      The -Wstringop-truncation warning added in GCC 8.0 via r254630 for
      bug 81117 is specifically intended to highlight likely unintended
      uses of the strncpy function that truncate the terminating NUL
      character from the source string.
    
    This new warning leads to compilation failures:
    
        CC      hw/acpi/core.o
      In function 'acpi_table_install', inlined from 'acpi_table_add' at qemu/hw/acpi/core.c:296:5:
      qemu/hw/acpi/core.c:184:9: error: 'strncpy' specified bound 4 equals destination size [-Werror=stringop-truncation]
               strncpy(ext_hdr->sig, hdrs->sig, sizeof ext_hdr->sig);
               ^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
      make: *** [qemu/rules.mak:69: hw/acpi/core.o] Error 1
    
    Use the QEMU_NONSTRING attribute, since ACPI tables don't require the
    strings to be NUL-terminated.
    
    Suggested-by: Michael S. Tsirkin <mst@redhat.com>
    Signed-off-by: Philippe Mathieu-Daudé <philmd@redhat.com>
    Message-Id: <20181218175122.3229-4-philmd@redhat.com>

--- include/hw/acpi/acpi-defs.h
+++ include/hw/acpi/acpi-defs.h
@@ -43,7 +43,7 @@ enum {
 struct AcpiRsdpDescriptor {        /* Root System Descriptor Pointer */
     uint64_t signature;              /* ACPI signature, contains "RSD PTR " */
     uint8_t  checksum;               /* To make sum of struct == 0 */
-    uint8_t  oem_id [6];             /* OEM identification */
+    uint8_t  oem_id [6] QEMU_NONSTRING; /* OEM identification */
     uint8_t  revision;               /* Must be 0 for 1.0, 2 for 2.0 */
     uint32_t rsdt_physical_address;  /* 32-bit physical address of RSDT */
     uint32_t length;                 /* XSDT Length in bytes including hdr */
@@ -62,10 +62,10 @@ typedef struct AcpiRsdpDescriptor AcpiRsdpDescriptor;
     uint32_t length;                 /* Length of table, in bytes, including header */ \
     uint8_t  revision;               /* ACPI Specification minor version # */ \
     uint8_t  checksum;               /* To make sum of entire table == 0 */ \
-    uint8_t  oem_id [6];             /* OEM identification */ \
-    uint8_t  oem_table_id [8];       /* OEM table identification */ \
+    uint8_t  oem_id [6] QEMU_NONSTRING; /* OEM identification */ \
+    uint8_t  oem_table_id [8] QEMU_NONSTRING; /* OEM table identification */ \
     uint32_t oem_revision;           /* OEM revision number */ \
-    uint8_t  asl_compiler_id [4];    /* ASL compiler vendor ID */ \
+    uint8_t  asl_compiler_id [4] QEMU_NONSTRING; /* ASL compiler vendor ID */ \
     uint32_t asl_compiler_revision;  /* ASL compiler revision number */
 
 
