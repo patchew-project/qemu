f0b1912e2ccb1af200d5d032dc58eeb0da910972
Marc-André Lureau <marcandre.lureau@redhat.com>
2018-12-21 13:03:48 +0400

    compat: replace PC_COMPAT_3_1 & HW_COMPAT_3_1 macros
    Use static arrays instead.
    
    Suggested-by: Eduardo Habkost <ehabkost@redhat.com>
    Signed-off-by: Marc-André Lureau <marcandre.lureau@redhat.com>
    Message-Id: <20181221090410.27250-7-marcandre.lureau@redhat.com>

--- hw/core/machine.c
+++ hw/core/machine.c
@@ -22,6 +22,9 @@
 #include "qemu/error-report.h"
 #include "sysemu/qtest.h"
 
+GlobalProperty hw_compat_3_1[] = {};
+const size_t hw_compat_3_1_len = G_N_ELEMENTS(hw_compat_3_1);
+
 static char *machine_get_accel(Object *obj, Error **errp)
 {
     MachineState *ms = MACHINE(obj);
