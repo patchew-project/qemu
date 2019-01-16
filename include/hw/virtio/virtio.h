6f1464ae9ad8d670d0b5ced8df40bbb7ce39e59e
Wei Xu <wexu@redhat.com>
2019-01-16 13:31:20 -0500

    virtio: enable packed ring via a new command line
    Signed-off-by: Wei Xu <wexu@redhat.com>
    Message-Id: <1547663480-547-16-git-send-email-wexu@redhat.com>

--- include/hw/virtio/virtio.h
+++ include/hw/virtio/virtio.h
@@ -264,7 +264,9 @@ typedef struct VirtIORNGConf VirtIORNGConf;
     DEFINE_PROP_BIT64("any_layout", _state, _field, \
                       VIRTIO_F_ANY_LAYOUT, true), \
     DEFINE_PROP_BIT64("iommu_platform", _state, _field, \
-                      VIRTIO_F_IOMMU_PLATFORM, false)
+                      VIRTIO_F_IOMMU_PLATFORM, false), \
+    DEFINE_PROP_BIT64("ring_packed", _state, _field, \
+                      VIRTIO_F_RING_PACKED, false)
 
 hwaddr virtio_queue_get_desc_addr(VirtIODevice *vdev, int n);
 hwaddr virtio_queue_get_avail_addr(VirtIODevice *vdev, int n);
