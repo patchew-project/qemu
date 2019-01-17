1995edd0d0094c051bfdf39f337d1aa01180de66
Cleber Rosa <crosa@redhat.com>
2019-01-17 13:56:23 -0500

    Boot Linux Console Test: add a test for mips64el + malta
    Similar to the x86_64 + pc test, it boots a Linux kernel on a Malta
    board and verify the serial is working.
    
    If mips64el is a target being built, "make check-acceptance" will
    automatically include this test by the use of the "arch:mips" tags.
    
    Alternatively, this test can be run using:
    
        $ avocado run -t arch:mips64el tests/acceptance
        $ avocado run -t machine:malta tests/acceptance
    
    Signed-off-by: Philippe Mathieu-Daudé <f4bug@amsat.org>
    Signed-off-by: Cleber Rosa <crosa@redhat.com>
    Message-Id: <20190117185628.21862-14-crosa@redhat.com>
    Reviewed-by: Philippe Mathieu-Daudé <philmd@redhat.com>
    Reviewed-by: Caio Carrara <ccarrara@redhat.com>

--- tests/acceptance/boot_linux_console.py
+++ tests/acceptance/boot_linux_console.py
@@ -9,8 +9,11 @@
 # later.  See the COPYING file in the top-level directory.
 
 import logging
+import os
 
 from avocado_qemu import Test
+from avocado.utils import process
+from avocado.utils import archive
 
 
 class BootLinuxConsole(Test):
@@ -71,6 +74,42 @@ class BootLinuxConsole(Test):
                       'vmlinux-3.2.0-4-4kc-malta')
         kernel_hash = '592e384a4edc16dade52a6cd5c785c637bcbc9ad'
         kernel_path = self.fetch_asset(kernel_url, asset_hash=kernel_hash)
+
+        self.vm.set_machine('malta')
+        self.vm.set_console()
+        kernel_command_line = 'console=ttyS0 printk.time=0'
+        self.vm.add_args('-kernel', kernel_path,
+                         '-append', kernel_command_line)
+        self.vm.launch()
+        console_pattern = 'Kernel command line: %s' % kernel_command_line
+        self.wait_for_console_pattern(console_pattern)
+
+    def test_mips64el_malta(self):
+        """
+        This test requires the ar tool to extract "data.tar.gz" from
+        the Debian package.
+
+        The kernel can be rebuilt using this Debian kernel source [1] and
+        following the instructions on [2].
+
+        [1] https://kernel-team.pages.debian.net/kernel-handbook/ch-common-tasks.html#s-common-official
+        [2] http://snapshot.debian.org/package/linux-2.6/2.6.32-48/#linux-source-2.6.32_2.6.32-48
+
+        :avocado: tags=arch:mips64el
+        :avocado: tags=machine:malta
+        """
+        deb_url = ('http://snapshot.debian.org/archive/debian/20130217T032700Z/'
+                   'pool/main/l/linux-2.6/'
+                   'linux-image-2.6.32-5-5kc-malta_2.6.32-48_mipsel.deb')
+        deb_hash = '1aaec92083bf22fda31e0d27fa8d9a388e5fc3d5'
+        deb_path = self.fetch_asset(deb_url, asset_hash=deb_hash)
+
+        cwd = os.getcwd()
+        os.chdir(self.workdir)
+        process.run("ar x %s data.tar.gz" % deb_path)
+        archive.extract("data.tar.gz", self.workdir)
+        os.chdir(cwd)
+        kernel_path = self.workdir + '/boot/vmlinux-2.6.32-5-5kc-malta'
 
         self.vm.set_machine('malta')
         self.vm.set_console()
