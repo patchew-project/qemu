4c657d12421cde04523091dbeb237617b99df3d5
Fabiano Rosas <farosas@linux.ibm.com>
2019-01-18 12:07:55 -0200

    kvm-all: Introduce kvm_set_singlestep
    This will allow architecture-specifc implementations of a fallback
    mechanism for single stepping in cases where KVM does not support it
    via the KVM_SET_GUEST_DEBUG ioctl.
    
    Signed-off-by: Fabiano Rosas <farosas@linux.ibm.com>
    Message-Id: <20190118140758.829-5-farosas@linux.ibm.com>

--- accel/kvm/kvm-all.c
+++ accel/kvm/kvm-all.c
@@ -2320,6 +2320,15 @@ int kvm_update_guest_debug(CPUState *cpu, unsigned long reinject_trap)
     return data.err;
 }
 
+void kvm_set_singlestep(CPUState *cs, int enabled)
+{
+    if (kvm_has_guestdbg_singlestep()) {
+        kvm_update_guest_debug(cs, 0);
+    } else {
+        kvm_arch_set_singlestep(cs, enabled);
+    }
+}
+
 int kvm_insert_breakpoint(CPUState *cpu, target_ulong addr,
                           target_ulong len, int type)
 {
