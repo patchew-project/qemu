12ce7872f15f79a7d8cbf66ea789fa04f58a5a91
Knut Omang <knut.omang@oracle.com>
2019-01-24 11:12:53 +0100

    gen_pcie_root_port: Add ACS (Access Control Services) capability
    Claim ACS support in the generic PCIe root port to allow
    passthrough of individual functions of a device to different
    guests (in a nested virt.setting) with VFIO.
    Without this patch, all functions of a device, such as all VFs of
    an SR/IOV device, will end up in the same IOMMU group.
    A similar situation occurs on Windows with Hyper-V.
    
    In the single function device case, it also has a small cosmetic
    benefit in that the root port itself is not grouped with
    the device. VFIO handles that situation in that binding rules
    only apply to endpoints, so it does not limit passthrough in
    those cases.
    
    Signed-off-by: Knut Omang <knut.omang@oracle.com>
    Message-Id: <b9ae6eabc2a87fb27e969c9d095263f4a097b740.1548323393.git-series.knut.omang@oracle.com>

--- hw/pci-bridge/gen_pcie_root_port.c
+++ hw/pci-bridge/gen_pcie_root_port.c
@@ -20,6 +20,7 @@
         OBJECT_CHECK(GenPCIERootPort, (obj), TYPE_GEN_PCIE_ROOT_PORT)
 
 #define GEN_PCIE_ROOT_PORT_AER_OFFSET           0x100
+#define GEN_PCIE_ROOT_PORT_ACS_OFFSET           0x148
 #define GEN_PCIE_ROOT_PORT_MSIX_NR_VECTOR       1
 
 typedef struct GenPCIERootPort {
@@ -149,6 +150,7 @@ static void gen_rp_dev_class_init(ObjectClass *klass, void *data)
     rpc->interrupts_init = gen_rp_interrupts_init;
     rpc->interrupts_uninit = gen_rp_interrupts_uninit;
     rpc->aer_offset = GEN_PCIE_ROOT_PORT_AER_OFFSET;
+    rpc->acs_offset = GEN_PCIE_ROOT_PORT_ACS_OFFSET;
 }
 
 static const TypeInfo gen_rp_dev_info = {
