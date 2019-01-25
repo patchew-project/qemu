c8aeebcffd5abfd82c9f60152102b4be892d87e8
Vitaly Kuznetsov <vkuznets@redhat.com>
2019-01-25 12:41:51 +0100

    i386/kvm: Implement 'hv-all' pass-through mode
    In many case we just want to give Windows guests all currently supported
    Hyper-V enlightenments and that's where this new mode may come handy. We
    pass through what was returned by KVM_GET_SUPPORTED_HV_CPUID.
    
    hv_cpuid_check_and_set() is modified to also set cpu->hyperv_* flags as
    we may want to check them later (and we actually do for hv_runtime,
    hv_synic,...).
    
    Signed-off-by: Vitaly Kuznetsov <vkuznets@redhat.com>
    Message-Id: <20190125114155.32062-5-vkuznets@redhat.com>

--- target/i386/cpu.h
+++ target/i386/cpu.h
@@ -1397,6 +1397,7 @@ struct X86CPU {
     bool hyperv_tlbflush;
     bool hyperv_evmcs;
     bool hyperv_ipi;
+    bool hyperv_all;
     bool check_cpuid;
     bool enforce_cpuid;
     bool expose_kvm;
