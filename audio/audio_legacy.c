eca548aefae27e10b39912900d71649186f48d35
Kővágó, Zoltán <dirty.ice.hu@gmail.com>
2019-01-28 23:43:32 +0100

    ossaudio: port to -audiodev config
    Signed-off-by: Kővágó, Zoltán <DirtY.iCE.hu@gmail.com>
    Message-Id: <e69d47e43fa8a5aba20306971f6af99e5cef255c.1548714862.git.DirtY.iCE.hu@gmail.com>

--- audio/audio_legacy.c
+++ audio/audio_legacy.c
@@ -221,6 +221,35 @@ static void handle_dsound(Audiodev *dev)
                        &dev->in->has_buffer_len, dev->in);
 }
 
+/* OSS */
+static void handle_oss_per_direction(
+    AudiodevPerDirectionOptions *pdo, AudiodevOssPerDirectionOptions **opdo,
+    bool *has_opdo, const char *try_poll_env, const char *dev_env)
+{
+    *opdo = g_malloc0(sizeof(AudiodevOssPerDirectionOptions));
+    *has_opdo = true;
+
+    get_bool(try_poll_env, &(*opdo)->try_poll, &(*opdo)->has_try_poll);
+    get_str(dev_env, &(*opdo)->dev, &(*opdo)->has_dev);
+
+    get_bytes_to_usecs("QEMU_OSS_FRAGSIZE",
+                       &pdo->buffer_len, &pdo->has_buffer_len, pdo);
+   get_int("QEMU_OSS_NFRAGS", &pdo->buffer_count, &pdo->has_buffer_count);
+}
+
+static void handle_oss(Audiodev *dev)
+{
+    AudiodevOssOptions *oopt = &dev->u.oss;
+    handle_oss_per_direction(dev->in, &oopt->oss_in, &oopt->has_oss_in,
+                             "QEMU_AUDIO_ADC_TRY_POLL", "QEMU_OSS_ADC_DEV");
+    handle_oss_per_direction(dev->out, &oopt->oss_out, &oopt->has_oss_out,
+                             "QEMU_AUDIO_DAC_TRY_POLL", "QEMU_OSS_DAC_DEV");
+
+    get_bool("QEMU_OSS_MMAP", &oopt->try_mmap, &oopt->has_try_mmap);
+    get_bool("QEMU_OSS_EXCLUSIVE", &oopt->exclusive, &oopt->has_exclusive);
+    get_int("QEMU_OSS_POLICY", &oopt->dsp_policy, &oopt->has_dsp_policy);
+}
+
 /* general */
 static void handle_per_direction(
     AudiodevPerDirectionOptions *pdo, const char *prefix)
@@ -274,6 +303,10 @@ static AudiodevListEntry *legacy_opt(const char *drvname)
 
     case AUDIODEV_DRIVER_DSOUND:
         handle_dsound(e->dev);
+        break;
+
+    case AUDIODEV_DRIVER_OSS:
+        handle_oss(e->dev);
         break;
 
     default:
