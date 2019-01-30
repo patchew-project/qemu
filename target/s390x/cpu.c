60c5e3126165f0e58eb6435a8b5949db63218f59
Emilio G. Cota <cota@braap.org>
2019-01-29 19:47:50 -0500

    s390x: convert to cpu_interrupt_request
    Cc: Cornelia Huck <cohuck@redhat.com>
    Cc: Christian Borntraeger <borntraeger@de.ibm.com>
    Cc: David Hildenbrand <david@redhat.com>
    Cc: qemu-s390x@nongnu.org
    Reviewed-by: Richard Henderson <richard.henderson@linaro.org>
    Reviewed-by: Alex Bennée <alex.bennee@linaro.org>
    Reviewed-by: Cornelia Huck <cohuck@redhat.com>
    Signed-off-by: Emilio G. Cota <cota@braap.org>
    Message-Id: <20190130004811.27372-53-cota@braap.org>

--- target/s390x/cpu.c
+++ target/s390x/cpu.c
@@ -65,7 +65,7 @@ static bool s390_cpu_has_work(CPUState *cs)
         return false;
     }
 
-    if (!(cs->interrupt_request & CPU_INTERRUPT_HARD)) {
+    if (!(cpu_interrupt_request(cs) & CPU_INTERRUPT_HARD)) {
         return false;
     }
 
