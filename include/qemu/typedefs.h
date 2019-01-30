7a0fe177a2f192ea4dd1a3bbcc5c8f56698a1d2b
Dr. David Alan Gilbert <dgilbert@redhat.com>
2019-01-30 10:32:29 +0000

    migration: Add announce parameters
    Add migration parameters that control RARP/GARP announcement timeouts.
    
    Based on earlier patches by myself and
      Vladislav Yasevich <vyasevic@redhat.com>
    
    Signed-off-by: Dr. David Alan Gilbert <dgilbert@redhat.com>
    Message-Id: <20190130103236.18302-3-dgilbert@redhat.com>

--- include/qemu/typedefs.h
+++ include/qemu/typedefs.h
@@ -8,6 +8,7 @@
 typedef struct AdapterInfo AdapterInfo;
 typedef struct AddressSpace AddressSpace;
 typedef struct AioContext AioContext;
+typedef struct AnnounceParameters AnnounceParameters;
 typedef struct AnnounceTimer AnnounceTimer;
 typedef struct BdrvDirtyBitmap BdrvDirtyBitmap;
 typedef struct BdrvDirtyBitmapIter BdrvDirtyBitmapIter;
