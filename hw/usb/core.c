9d62b197abdc5f5ed1e4b05eb257f947719c0dec
Liam Merwick <liam.merwick@oracle.com>
2019-01-30 14:37:01 +0000

    usb: rearrange usb_ep_get()
    There is no need to calculate the 'eps' variable in usb_ep_get()
    if 'ep' is the control endpoint.  Instead the calculation should
    be done after validating the input and the resulting pointer also
    validated before returning an entry indexed on the endpoint 'ep'.
    
    Signed-off-by: Liam Merwick <liam.merwick@oracle.com>
    Reviewed-by: Darren Kenny <Darren.Kenny@oracle.com>
    Reviewed-by: Mark Kanda <Mark.Kanda@oracle.com>
    Reviewed-by: Ameya More <ameya.more@oracle.com>
    Message-Id: <1548859022-3969-2-git-send-email-liam.merwick@oracle.com>

--- hw/usb/core.c
+++ hw/usb/core.c
@@ -720,12 +720,13 @@ struct USBEndpoint *usb_ep_get(USBDevice *dev, int pid, int ep)
     if (dev == NULL) {
         return NULL;
     }
-    eps = (pid == USB_TOKEN_IN) ? dev->ep_in : dev->ep_out;
     if (ep == 0) {
         return &dev->ep_ctl;
     }
     assert(pid == USB_TOKEN_IN || pid == USB_TOKEN_OUT);
     assert(ep > 0 && ep <= USB_MAX_ENDPOINTS);
+    eps = (pid == USB_TOKEN_IN) ? dev->ep_in : dev->ep_out;
+    assert(eps != NULL);
     return eps + ep - 1;
 }
 
