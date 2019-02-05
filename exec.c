06a7b881f6b2836e48d7f7db49cacf10fad8b69e
Pavel Dovgalyuk <Pavel.Dovgaluk@ispras.ru>
2019-02-05 11:21:19 +0300

    gdbstub: add reverse continue support in replay mode
    This patch adds support of the reverse continue operation for gdbstub.
    Reverse continue finds the last breakpoint that would happen in normal
    execution from the beginning to the current moment.
    Implementation of the reverse continue replays the execution twice:
    to find the breakpoints that were hit and to seek to the last breakpoint.
    Reverse continue loads the previous snapshot and tries to find the breakpoint
    since that moment. If there are no such breakpoints, it proceeds to
    the earlier snapshot, and so on. When no breakpoints or watchpoints were
    hit at all, execution stops at the beginning of the replay log.
    
    Signed-off-by: Pavel Dovgalyuk <Pavel.Dovgaluk@ispras.ru>
    Message-Id: <154935487896.7509.843303050095073079.stgit@pasha-VirtualBox>

--- exec.c
+++ exec.c
@@ -2747,6 +2747,7 @@ static void check_watchpoint(int offset, int len, MemTxAttrs attrs, int flags)
                  * Don't process the watchpoints when we are
                  * in a reverse debugging operation.
                  */
+                replay_breakpoint();
                 return;
             }
             if (flags == BP_MEM_READ) {
