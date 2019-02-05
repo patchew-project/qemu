41ff8625d59932203b5808863514b49255e4c319
Pavel Dovgalyuk <Pavel.Dovgaluk@ispras.ru>
2019-02-05 11:21:58 +0300

    replay: rename step-related variables and functions
    This patch renames replay_get_current_step() and related variables
    to make these names consistent with hmp/qmp commands.
    
    Signed-off-by: Pavel Dovgalyuk <Pavel.Dovgaluk@ispras.ru>
    Message-Id: <154935491876.7509.3362365927769261001.stgit@pasha-VirtualBox>

--- migration/savevm.c
+++ migration/savevm.c
@@ -2511,7 +2511,7 @@ int save_snapshot(const char *name, Error **errp)
     sn->date_nsec = tv.tv_usec * 1000;
     sn->vm_clock_nsec = qemu_clock_get_ns(QEMU_CLOCK_VIRTUAL);
     if (replay_mode != REPLAY_MODE_NONE) {
-        sn->icount = replay_get_current_step();
+        sn->icount = replay_get_current_icount();
     } else {
         sn->icount = -1ULL;
     }
