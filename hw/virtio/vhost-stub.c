8fa747cbc7bebb4be309c009ec27be15291fc399
Paolo Bonzini <pbonzini@redhat.com>
2019-02-07 18:57:34 +0100

    FIXME vhost: add more stubs
    Due to the dependency of net/ against hw/net/, it is not possible
    to compile out hw/net/vhost_net.c for now.  Add more stubs to
    allow it to compile when hw/net/vhost.c is missing.
    
    (To be re-evaluated after rebase on top of the revamped vhost_net configury).
    
    Signed-off-by: Paolo Bonzini <pbonzini@redhat.com>
    Message-Id: <1549562254-41157-52-git-send-email-pbonzini@redhat.com>
    Reviewed-by: Michael S. Tsirkin <mst@redhat.com>

--- hw/virtio/vhost-stub.c
+++ hw/virtio/vhost-stub.c
@@ -15,3 +15,61 @@ VhostUserState *vhost_user_init(void)
 void vhost_user_cleanup(VhostUserState *user)
 {
 }
+
+int vhost_dev_init(struct vhost_dev *hdev, void *opaque,
+                   VhostBackendType backend_type, uint32_t busyloop_timeout)
+{
+    return -ENODEV;
+}
+
+void vhost_dev_cleanup(struct vhost_dev *hdev)
+{
+}
+
+int vhost_dev_start(struct vhost_dev *hdev, VirtIODevice *vdev)
+{
+    abort();
+}
+
+void vhost_dev_stop(struct vhost_dev *hdev, VirtIODevice *vdev)
+{
+}
+
+int vhost_dev_enable_notifiers(struct vhost_dev *hdev, VirtIODevice *vdev)
+{
+    abort();
+}
+
+void vhost_dev_disable_notifiers(struct vhost_dev *hdev, VirtIODevice *vdev)
+{
+    abort();
+}
+
+int vhost_net_set_backend(struct vhost_dev *hdev,
+                          struct vhost_vring_file *file)
+{
+    abort();
+}
+
+uint64_t vhost_get_features(struct vhost_dev *hdev, const int *feature_bits,
+                            uint64_t features)
+{
+    abort();
+}
+
+void vhost_ack_features(struct vhost_dev *hdev, const int *feature_bits,
+                        uint64_t features)
+{
+    abort();
+}
+
+bool vhost_virtqueue_pending(struct vhost_dev *hdev, int n)
+{
+    abort();
+}
+
+void vhost_virtqueue_mask(struct vhost_dev *hdev, VirtIODevice *vdev, int n,
+                         bool mask)
+{
+    abort();
+}
