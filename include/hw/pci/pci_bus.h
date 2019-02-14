cf22a470aa54cb0a550f965d744c1a04901c9940
David Gibson <david@gibson.dropbear.id.au>
2019-02-14 16:08:07 +1100

    pci: Simplify pci_bus_is_root()
    pci_bus_is_root() currently relies on a method in the PCIBusClass.
    But it's always known if a PCI bus is a root bus when we create it, so
    using a dynamic method is overkill.
    
    This replaces it with an IS_ROOT bit in a new flags field, which is set on
    root buses and otherwise clear.  As a bonus this removes the special
    is_root logic from pci_expander_bridge, since it already creates its bus
    as a root bus.
    
    Signed-off-by: David Gibson <david@gibson.dropbear.id.au>
    Reviewed-by: Marcel Apfelbaum <marcel@redhat.com>
    Reviewed-by: Peter Xu <peterx@redhat.com>
    Message-Id: <20190214050808.16653-2-david@gibson.dropbear.id.au>

--- include/hw/pci/pci_bus.h
+++ include/hw/pci/pci_bus.h
@@ -20,8 +20,14 @@ typedef struct PCIBusClass {
     uint16_t (*numa_node)(PCIBus *bus);
 } PCIBusClass;
 
+enum PCIBusFlags {
+    /* This bus is the root of a PCI domain */
+    PCI_BUS_IS_ROOT                                         = 0x0001,
+};
+
 struct PCIBus {
     BusState qbus;
+    enum PCIBusFlags flags;
     PCIIOMMUFunc iommu_fn;
     void *iommu_opaque;
     uint8_t devfn_min;
@@ -45,5 +51,10 @@ struct PCIBus {
 
     Notifier machine_done;
 };
+
+static inline bool pci_bus_is_root(PCIBus *bus)
+{
+    return !!(bus->flags & PCI_BUS_IS_ROOT);
+}
 
 #endif /* QEMU_PCI_BUS_H */
