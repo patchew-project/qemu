961e787e9eb060e0259bd3423b3a2417a0cfdf6f
Wei Yang <richardw.yang@linux.intel.com>
2019-02-19 14:07:18 +0800

    pc-dimm: remove check on pc-dimm hotpluggable
    Function acpi_memory_plug_cb() is only invoked when dev is a PCDIMM,
    which is hotpluggable. This means it is not necessary to check this
    property again.
    
    This patch removes this check.
    
    Signed-off-by: Wei Yang <richardw.yang@linux.intel.com>
    Message-Id: <20190219060719.8211-2-richardw.yang@linux.intel.com>
    Reviewed-by: Philippe Mathieu-Daudé <philmd@redhat.com>
    Tested-by: Philippe Mathieu-Daudé <philmd@redhat.com>

--- hw/acpi/memory_hotplug.c
+++ hw/acpi/memory_hotplug.c
@@ -267,10 +267,6 @@ void acpi_memory_plug_cb(HotplugHandler *hotplug_dev, MemHotplugState *mem_st,
     MemStatus *mdev;
     DeviceClass *dc = DEVICE_GET_CLASS(dev);
 
-    if (!dc->hotpluggable) {
-        return;
-    }
-
     mdev = acpi_memory_slot_status(mem_st, dev, errp);
     if (!mdev) {
         return;
