cf659141cf6b979fbdb04c032403e95737ed007c
David Hildenbrand <david@redhat.com>
2019-02-27 22:15:25 +0100

    tests/tcg: target/s390x: Test VECTOR LOAD GR FROM VR ELEMENT
    Reuse the existing helpers.
    
    Signed-off-by: David Hildenbrand <david@redhat.com>
    Message-Id: <20190227211525.2470-3-david@redhat.com>

--- tests/tcg/s390x/helper.h
+++ tests/tcg/s390x/helper.h
@@ -11,6 +11,12 @@ typedef union S390Vector {
     uint8_t h[16];
 } S390Vector;
 
+#define ES_8    0
+#define ES_16   1
+#define ES_32   2
+#define ES_64   3
+#define ES_128  4
+
 static inline void check(const char *s, bool cond)
 {
     if (!cond) {
