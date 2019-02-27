f9040c50394010d421a2248d0239ccd2078eb22d
Yuval Shaia <yuval.shaia@oracle.com>
2019-02-27 16:06:28 +0200

    hw/pvrdma: Delete pvrdma_exit function
    This hook is not called and was implemented by mistake.
    
    Signed-off-by: Yuval Shaia <yuval.shaia@oracle.com>
    Reviewed-by: Marcel Apfelbaum <marcel.apfelbaum@gmail.com>
    Message-Id: <20190227140629.1569-9-yuval.shaia@oracle.com>

--- hw/rdma/vmw/pvrdma_main.c
+++ hw/rdma/vmw/pvrdma_main.c
@@ -701,18 +701,12 @@ out:
     }
 }
 
-static void pvrdma_exit(PCIDevice *pdev)
-{
-    pvrdma_fini(pdev);
-}
-
 static void pvrdma_class_init(ObjectClass *klass, void *data)
 {
     DeviceClass *dc = DEVICE_CLASS(klass);
     PCIDeviceClass *k = PCI_DEVICE_CLASS(klass);
 
     k->realize = pvrdma_realize;
-    k->exit = pvrdma_exit;
     k->vendor_id = PCI_VENDOR_ID_VMWARE;
     k->device_id = PCI_DEVICE_ID_VMWARE_PVRDMA;
     k->revision = 0x00;
