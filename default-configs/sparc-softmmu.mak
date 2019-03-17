patchew-project/qemu

commit 8c75eec06d4f44977178d5f228058c6b696afe1c

sparc-softmmu.mak: express dependencies with Kconfig
%-softmmu.mak only keep boards and optional device
definitions in Kconfig mode.

Signed-off-by: Paolo Bonzini <pbonzini@redhat.com>

default-configs/sparc-softmmu.mak
@@ -1,21 +1,11 @@
 # Default configuration for sparc-softmmu
 
-CONFIG_ISA_BUS=y
-CONFIG_ECC=y
-CONFIG_ESP=y
-CONFIG_ESCC=y
-CONFIG_M48T59=y
-CONFIG_FDC=y
-CONFIG_EMPTY_SLOT=y
-CONFIG_PCNET_COMMON=y
-CONFIG_LANCE=y
-CONFIG_TCX=y
-CONFIG_CG3=y
-CONFIG_SLAVIO=y
-CONFIG_CS4231=y
-CONFIG_GRLIB=y
-CONFIG_STP2000=y
-CONFIG_ECCMEMCTL=y
+# Uncomment the following lines to disable these optional devices:
+#
+#CONFIG_TCX=n
+#CONFIG_CG3=n
 
+# Boards:
+#
 CONFIG_SUN4M=y
 CONFIG_LEON3=y

hw/sparc/Kconfig
@@ -1,8 +1,21 @@
 config SUN4M
     bool
+    imply TCX
+    imply CG3
+    select CS4231
+    select ECCMEMCTL
+    select EMPTY_SLOT
+    select ESCC
+    select ESP
+    select FDC
+    select SLAVIO
+    select LANCE
+    select M48T59
+    select STP2000
 
 config LEON3
     bool
+    select GRLIB
 
 config GRLIB
     bool

hw/misc/Kconfig
@@ -71,6 +71,7 @@ config IVSHMEM_DEVICE
 
 config ECCMEMCTL
     bool
+    select ECC
 
 config IMX
     bool

hw/net/Kconfig
@@ -77,6 +77,7 @@ config STELLARIS_ENET
 
 config LANCE
     bool
+    select PCNET_COMMON
 
 config SUNHME
     bool

commit 66cf9e986117d34704a21be52250089b05c794b5

hw/sparc/Kconfig: SPARCstation machine requires the TCX display
This is the default display device used in sun4m_hw_init():

    /* If no display specified, default to TCX */

This fixes when configuring with --without-default-devices:

  $ sparc-softmmu/qemu-system-sparc
  qemu-system-sparc: Unknown device 'SUNW,tcx' for default sysbus
  Aborted (core dumped)

  (gdb) bt
  #0  0x00007fc78d19353f in __GI_raise (sig=sig@entry=6) at ../sysdeps/unix/sysv/linux/raise.c:50
  #1  0x00007fc78d17d895 in __GI_abort () at abort.c:79
  #2  0x0000560beaf637f3 in qdev_create (bus=bus@entry=0x0, name=name@entry=0x560beb1be36b "SUNW,tcx") at hw/core/qdev.c:131
  #3  0x0000560beaf1392d in tcx_init (vram_size=1048576, width=1024, height=768, depth=8, irq=0x560bed1a0230, addr=1342177280) at hw/sparc/sun4m.c:477
  #4  0x0000560beaf1392d in sun4m_hw_init (hwdef=0x560beb1be780 <sun4m_hwdefs>, machine=0x560becf65f00) at hw/sparc/sun4m.c:943
  #5  0x0000560beaf6b15b in machine_run_board_init (machine=0x560becf65f00) at hw/core/machine.c:1030
  #6  0x0000560beae86692 in main (argc=<optimized out>, argv=<optimized out>, envp=<optimized out>) at vl.c:4463

Fixes: 8c75eec06d4
Signed-off-by: Philippe Mathieu-Daudé <philmd@redhat.com>
Message-Id: <20190316200818.8265-12-philmd@redhat.com>

default-configs/sparc-softmmu.mak
@@ -2,7 +2,6 @@
 
 # Uncomment the following lines to disable these optional devices:
 #
-#CONFIG_TCX=n
 #CONFIG_CG3=n
 
 # Boards:

hw/sparc/Kconfig
@@ -1,6 +1,6 @@
 config SUN4M
     bool
-    imply TCX
+    select TCX
     imply CG3
     select CS4231
     select ECCMEMCTL