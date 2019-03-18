a660b4c8daef6cb443cb226a04f4d829a9f67b8f
Li Qiang <liq3ea@163.com>
2019-03-18 09:11:47 +0800

    backends: cryptodev: fix oob access issue
    The 'queue_index' of create/close_session function
    is from guest and can be exceed 'MAX_CRYPTO_QUEUE_NUM'.
    This leads oob access. This patch avoid this.
    
    Signed-off-by: Li Qiang <liq3ea@163.com>
    Message-Id: <20190318011147.15690-1-liq3ea@163.com>
    Reviewed-by: Gonglei <arei.gonglei@huawei.com>

--- backends/cryptodev-builtin.c
+++ backends/cryptodev-builtin.c
@@ -249,6 +249,8 @@ static int64_t cryptodev_builtin_sym_create_session(
            CryptoDevBackendSymSessionInfo *sess_info,
            uint32_t queue_index, Error **errp)
 {
+    assert(queue_index < MAX_CRYPTO_QUEUE_NUM);
+
     CryptoDevBackendBuiltin *builtin =
                       CRYPTODEV_BACKEND_BUILTIN(backend);
     int64_t session_id = -1;
@@ -280,6 +282,8 @@ static int cryptodev_builtin_sym_close_session(
            uint64_t session_id,
            uint32_t queue_index, Error **errp)
 {
+    assert(queue_index < MAX_CRYPTO_QUEUE_NUM);
+
     CryptoDevBackendBuiltin *builtin =
                       CRYPTODEV_BACKEND_BUILTIN(backend);
 
