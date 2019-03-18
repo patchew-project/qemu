45f7fd695ec0c670eb9234f92a6c6356f58df209
Roger Pau Monne <roger.pau@citrix.com>
2019-03-18 16:45:45 +0100

    xen-mapcache: use MAP_FIXED flag so the mmap address hint is always honored
    Or if it's not possible to honor the hinted address an error is returned
    instead. This makes it easier to spot the actual failure, instead of
    failing later on when the caller of xen_remap_bucket realizes the
    mapping has not been created at the requested address.
    
    Also note that at least on FreeBSD using MAP_FIXED will cause mmap to
    try harder to honor the passed address.
    
    Signed-off-by: Roger Pau Monné <roger.pau@citrix.com>
    Message-Id: <20190318154545.13974-1-roger.pau@citrix.com>
    Reviewed-by: Paul Durrant <paul.durrant@citrix.com>

--- hw/i386/xen/xen-mapcache.c
+++ hw/i386/xen/xen-mapcache.c
@@ -185,8 +185,13 @@ static void xen_remap_bucket(MapCacheEntry *entry,
     }
 
     if (!dummy) {
+        /*
+         * If the caller has requested the mapping at a specific address use
+         * MAP_FIXED to make sure it's honored.
+         */
         vaddr_base = xenforeignmemory_map2(xen_fmem, xen_domid, vaddr,
-                                           PROT_READ | PROT_WRITE, 0,
+                                           PROT_READ | PROT_WRITE,
+                                           vaddr ? MAP_FIXED : 0,
                                            nb_pfn, pfns, err);
         if (vaddr_base == NULL) {
             perror("xenforeignmemory_map2");
@@ -198,7 +203,8 @@ static void xen_remap_bucket(MapCacheEntry *entry,
          * mapping immediately due to certain circumstances (i.e. on resume now)
          */
         vaddr_base = mmap(vaddr, size, PROT_READ | PROT_WRITE,
-                          MAP_ANON | MAP_SHARED, -1, 0);
+                          MAP_ANON | MAP_SHARED | (vaddr ? MAP_FIXED : 0),
+                          -1, 0);
         if (vaddr_base == MAP_FAILED) {
             perror("mmap");
             exit(-1);
