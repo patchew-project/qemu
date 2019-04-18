fb5f14e92c0bb2dcda928cd4526a57f06c3d989d
Markus Armbruster <armbru@redhat.com>
2019-04-18 16:53:52 +0200

    gdbstub: Reject invalid RLE repeat counts
    "Debugging with GDB / Appendix E GDB Remote Serial Protocol /
    Overview" specifies "The printable characters '#' and '$' or with a
    numeric value greater than 126 must not be used."  gdb_read_byte()
    only rejects values < 32.  This is wrong.  Impact depends on the caller:
    
    * gdb_handlesig() passes a char.  Incorrectly accepts '#', '$' and
      '\127'.
    
    * gdb_chr_receive() passes an uint8_t.  Additionally accepts
      characters with the most-significant bit set.
    
    Correct the validity check to match the specification.
    
    Signed-off-by: Markus Armbruster <armbru@redhat.com>
    Message-Id: <20190418145355.21100-4-armbru@redhat.com>
    Reviewed-by: Philippe Mathieu-Daudé <philmd@redhat.com>

--- gdbstub.c
+++ gdbstub.c
@@ -2064,7 +2064,7 @@ static void gdb_read_byte(GDBState *s, int ch)
             }
             break;
         case RS_GETLINE_RLE:
-            if (ch < ' ') {
+            if (ch < ' ' || ch == '#' || ch == '$' || ch > 126) {
                 /* invalid RLE count encoding */
                 trace_gdbstub_err_invalid_repeat((uint8_t)ch);
                 s->state = RS_GETLINE;
