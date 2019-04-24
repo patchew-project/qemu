3ad8267fdd7ccbf79d3a772a3cda0dea5db18829
Thomas Huth <thuth@redhat.com>
2019-04-24 12:37:46 +0200

    tests: Run the iotests during "make check" again
    People often forget to run the iotests before submitting patches or
    pull requests - this is likely due to the fact that we do not run the
    tests during our mandatory "make check" tests yet. Now that we've got
    a new "ci" group of iotests that should be fine to run in every environ-
    ment, it should be OK to enable the iotests during "make check" again.
    Thus we now run the "ci" tests by default from the qemu-iotests-quick.sh
    script, and only use the former "quick" group (that contains some tests
    that are failing in some environments) when the user decided to run
    "make check SPEED=thorough" or something similar.
    
    Signed-off-by: Thomas Huth <thuth@redhat.com>
    Message-Id: <20190424103747.10173-6-thuth@redhat.com>
    Acked-by: Christian Borntraeger <borntraeger@de.ibm.com>

--- tests/qemu-iotests-quick.sh
+++ tests/qemu-iotests-quick.sh
@@ -1,8 +1,23 @@
 #!/bin/sh
 
+# Honor the SPEED environment variable, just like we do it for the qtests.
+# The default is to run all tests that still work fine in a CI environments,
+# but if the user set SPEED=slow or SPEED=thorough, we also run all other
+# tests that are still marked as "quick"
+if [ "$SPEED" = "slow" -o "$SPEED" = "thorough" ]; then
+    group=quick
+else
+    group=ci
+fi
+
+if [ -z "$(find . -name 'qemu-system-*' -print)" ]; then
+    echo "No qemu-system binary available. Skipped qemu-iotests."
+    exit 0
+fi
+
 cd tests/qemu-iotests
 
 ret=0
-TEST_DIR=${TEST_DIR:-/tmp/qemu-iotests-quick-$$} ./check -T -qcow2 -g quick || ret=1
+TEST_DIR=${TEST_DIR:-/tmp/qemu-iotests-quick-$$} ./check -T -qcow2 -g "$group" || ret=1
 
 exit $ret
