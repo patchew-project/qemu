8b78eaf88086e599350f6e2f8b5603ecb946459b
Richard Henderson <richard.henderson@linaro.org>
2019-05-08 23:02:25 -0700

    target/i386: Convert to CPUClass::tlb_fill
    We do not support probing, but we do not need it yet either.
    
    Cc: Paolo Bonzini <pbonzini@redhat.com>
    Cc: Eduardo Habkost <ehabkost@redhat.com>
    Reviewed-by: Peter Maydell <peter.maydell@linaro.org>
    Signed-off-by: Richard Henderson <richard.henderson@linaro.org>
    Message-Id: <20190509060246.4031-7-richard.henderson@linaro.org>

--- target/i386/cpu.c
+++ target/i386/cpu.c
@@ -5915,9 +5915,8 @@ static void x86_cpu_common_class_init(ObjectClass *oc, void *data)
     cc->gdb_write_register = x86_cpu_gdb_write_register;
     cc->get_arch_id = x86_cpu_get_arch_id;
     cc->get_paging_enabled = x86_cpu_get_paging_enabled;
-#ifdef CONFIG_USER_ONLY
-    cc->handle_mmu_fault = x86_cpu_handle_mmu_fault;
-#else
+    cc->tlb_fill = x86_cpu_tlb_fill;
+#ifndef CONFIG_USER_ONLY
     cc->asidx_from_attrs = x86_asidx_from_attrs;
     cc->get_memory_mapping = x86_cpu_get_memory_mapping;
     cc->get_phys_page_debug = x86_cpu_get_phys_page_debug;
