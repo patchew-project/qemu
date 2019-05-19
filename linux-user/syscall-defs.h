1c5cb4b54f1f517d8aabde61d38be4fa4d01daae
Richard Henderson <richard.henderson@linaro.org>
2019-05-19 13:36:14 -0700

    linux-user: Split out open, open_at
    For the moment, leave a forward declaration for is_proc_myself
    until the readlink syscalls are also moved to syscall-file.inc.c.
    
    Signed-off-by: Richard Henderson <richard.henderson@linaro.org>
    Message-Id: <20190519203726.20729-3-richard.henderson@linaro.org>
    Based-on: <20190519201953.20161-1-richard.henderson@linaro.org>

--- linux-user/syscall-defs.h
+++ linux-user/syscall-defs.h
@@ -15,3 +15,8 @@
  *  You should have received a copy of the GNU General Public License
  *  along with this program; if not, see <http://www.gnu.org/licenses/>.
  */
+
+#ifdef TARGET_NR_open
+SYSCALL_DEF(open, ARG_STR, ARG_OPENFLAG, ARG_MODEFLAG);
+#endif
+SYSCALL_DEF(openat, ARG_ATDIRFD, ARG_STR, ARG_OPENFLAG, ARG_MODEFLAG);
