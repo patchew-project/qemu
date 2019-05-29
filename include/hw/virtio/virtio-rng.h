2e122e2b046722b9abd56a2023fe43583ced4628
Markus Armbruster <armbru@redhat.com>
2019-05-29 16:31:05 +0200

    virtio-rng: Keep the default backend out of VirtIORNGConf
    The default backend is only used within virtio_rng_device_realize().
    Replace VirtIORNGConf member default_backend by a local variable.
    Adjust its type to reduce conversions.
    
    While there, pass &error_abort instead of NULL when failure would be a
    programming error.
    
    Signed-off-by: Markus Armbruster <armbru@redhat.com>
    Signed-off-by: Laurent Vivier <lvivier@redhat.com>
    Message-Id: <20190529143106.11789-4-lvivier@redhat.com>
    Reviewed-by: Michael S. Tsirkin <mst@redhat.com>

--- include/hw/virtio/virtio-rng.h
+++ include/hw/virtio/virtio-rng.h
@@ -13,7 +13,6 @@
 #define QEMU_VIRTIO_RNG_H
 
 #include "sysemu/rng.h"
-#include "sysemu/rng-random.h"
 #include "standard-headers/linux/virtio_rng.h"
 
 #define TYPE_VIRTIO_RNG "virtio-rng-device"
@@ -26,7 +25,6 @@ struct VirtIORNGConf {
     RngBackend *rng;
     uint64_t max_bytes;
     uint32_t period_ms;
-    RngRandom *default_backend;
 };
 
 typedef struct VirtIORNG {
