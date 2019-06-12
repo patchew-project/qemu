1e830eaf5f1a2e45f9e4d0e3cd50d32c43829697
Daniel P. Berrangé <berrange@redhat.com>
2019-06-12 13:41:25 +0200

    qapi: report the default CPU type for each machine
    When user doesn't request any explicit CPU model with libvirt or QEMU,
    a machine type specific CPU model is picked. Currently there is no way
    to determine what this QEMU built-in default is, so libvirt cannot
    report this back to the user in the XML config.
    
    This extends the "query-machines" QMP command so that it reports the
    default CPU model typename for each machine.
    
    Reviewed-by: Eric Blake <eblake@redhat.com>
    Signed-off-by: Daniel P. Berrangé <berrange@redhat.com>
    Message-Id: <20190612114125.22060-1-berrange@redhat.com>

--- vl.c
+++ vl.c
@@ -1428,6 +1428,10 @@ MachineInfoList *qmp_query_machines(Error **errp)
         info->name = g_strdup(mc->name);
         info->cpu_max = !mc->max_cpus ? 1 : mc->max_cpus;
         info->hotpluggable_cpus = mc->has_hotpluggable_cpus;
+        if (mc->default_cpu_type) {
+            info->default_cpu_type = g_strdup(mc->default_cpu_type);
+            info->has_default_cpu_type = true;
+        }
 
         entry = g_malloc0(sizeof(*entry));
         entry->value = info;
