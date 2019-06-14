ffb3762c7c6cf01a5669fa7e30748d862246b15f
Alex Bennée <alex.bennee@linaro.org>
2019-06-14 18:11:57 +0100

    accel/stubs: reduce headers from tcg-stub
    We don't need much for these. However I do wonder why these aren't
    just null inlines in exec-all.h
    
    Signed-off-by: Alex Bennée <alex.bennee@linaro.org>
    Message-Id: <20190614171200.21078-48-alex.bennee@linaro.org>

--- accel/stubs/tcg-stub.c
+++ accel/stubs/tcg-stub.c
@@ -11,10 +11,7 @@
  */
 
 #include "qemu/osdep.h"
-#include "qemu-common.h"
 #include "cpu.h"
-#include "tcg/tcg.h"
-#include "exec/cpu-common.h"
 #include "exec/exec-all.h"
 
 void tb_flush(CPUState *cpu)
