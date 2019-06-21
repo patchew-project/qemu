fbc96cff7a1c2d110c5b54677c51ace3bed9a51f
Tao Xu <tao3.xu@intel.com>
2019-06-21 14:51:34 +0800

    target/i386: Add support for save/load IA32_UMWAIT_CONTROL MSR
    UMWAIT and TPAUSE instructions use IA32_UMWAIT_CONTROL at MSR index
    E1H to determines the maximum time in TSC-quanta that the processor
    can reside in either C0.1 or C0.2.
    
    This patch is to Add support for save/load IA32_UMWAIT_CONTROL MSR in
    guest.
    
    Co-developed-by: Jingqi Liu <jingqi.liu@intel.com>
    Signed-off-by: Jingqi Liu <jingqi.liu@intel.com>
    Signed-off-by: Tao Xu <tao3.xu@intel.com>
    Message-Id: <20190621065134.7392-3-tao3.xu@intel.com>

--- target/i386/cpu.h
+++ target/i386/cpu.h
@@ -450,6 +450,7 @@ typedef enum X86Seg {
 
 #define MSR_IA32_BNDCFGS                0x00000d90
 #define MSR_IA32_XSS                    0x00000da0
+#define MSR_IA32_UMWAIT_CONTROL         0xe1
 
 #define XSTATE_FP_BIT                   0
 #define XSTATE_SSE_BIT                  1
@@ -1348,6 +1349,7 @@ typedef struct CPUX86State {
     uint16_t fpregs_format_vmstate;
 
     uint64_t xss;
+    uint64_t umwait;
 
     TPRAccess tpr_access_type;
 } CPUX86State;
