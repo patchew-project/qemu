97325650394d23e179360c817e40959edc6cfefb
Singh, Brijesh <brijesh.singh@amd.com>
2019-07-10 20:23:07 +0000

    target/i386: sev: add support to encrypt the outgoing page
    The sev_save_outgoing_page() provide the implementation to encrypt the
    guest private pages during the transit. The routines uses the SEND_START
    command to create the outgoing encryption context on the first call then
    uses the SEND_UPDATE_DATA command to encrypt the data before writing it
    to the socket. While encrypting the data SEND_UPDATE_DATA produces some
    metadata (e.g MAC, IV). The metadata is also sent to the target machine.
    After migration is completed, we issue the SEND_FINISH command to transition
    the SEV guest state from sending to unrunnable state.
    
    Signed-off-by: Brijesh Singh <brijesh.singh@amd.com>
    Message-Id: <20190710202219.25939-10-brijesh.singh@amd.com>

--- accel/kvm/kvm-all.c
+++ accel/kvm/kvm-all.c
@@ -1792,6 +1792,7 @@ static int kvm_init(MachineState *ms)
         }
 
         kvm_state->memcrypt_encrypt_data = sev_encrypt_data;
+        kvm_state->memcrypt_save_outgoing_page = sev_save_outgoing_page;
     }
 
     ret = kvm_arch_init(ms, s);
