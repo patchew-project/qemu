538478573879f26a8a4ecb4fa7201ef0ec480f11
Singh, Brijesh <brijesh.singh@amd.com>
2019-07-10 20:23:07 +0000

    target/i386: sev: add support to load incoming encrypted page
    The sev_load_incoming_page() provide the implementation to read the
    incoming guest private pages from the socket and load it into the guest
    memory. The routines uses the RECEIVE_START command to create the
    incoming encryption context on the first call then uses the
    RECEIEVE_UPDATE_DATA command to load the encrypted pages into the guest
    memory. After migration is completed, we issue the RECEIVE_FINISH command
    to transition the SEV guest to the runnable state so that it can be
    executed.
    
    Signed-off-by: Brijesh Singh <brijesh.singh@amd.com>
    Message-Id: <20190710202219.25939-11-brijesh.singh@amd.com>

--- accel/kvm/kvm-all.c
+++ accel/kvm/kvm-all.c
@@ -1793,6 +1793,7 @@ static int kvm_init(MachineState *ms)
 
         kvm_state->memcrypt_encrypt_data = sev_encrypt_data;
         kvm_state->memcrypt_save_outgoing_page = sev_save_outgoing_page;
+        kvm_state->memcrypt_load_incoming_page = sev_load_incoming_page;
     }
 
     ret = kvm_arch_init(ms, s);
