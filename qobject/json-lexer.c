fa5df15039e6a5518d5237e8fb83ef91c5383866
Marc-André Lureau <marcandre.lureau@redhat.com>
2019-07-15 23:09:43 +0400

    json-lexer: make it safe to call destroy multiple times
    We can easily avoid the burden of checking if the lexer was
    initialized prior to calling destroy by the caller, let's do it.
    
    This allows simplification in state tracking with the following patch,
    "qmp: add QmpSession" can call qmp_session_destroy() multiple times,
    which in turns calls json_lexer_destroy().
    
    Signed-off-by: Marc-André Lureau <marcandre.lureau@redhat.com>
    Message-Id: <20190715191001.1188-3-marcandre.lureau@redhat.com>

--- qobject/json-lexer.c
+++ qobject/json-lexer.c
@@ -361,5 +361,8 @@ void json_lexer_flush(JSONLexer *lexer)
 
 void json_lexer_destroy(JSONLexer *lexer)
 {
-    g_string_free(lexer->token, true);
+    if (lexer->token) {
+        g_string_free(lexer->token, true);
+        lexer->token = NULL;
+    }
 }
