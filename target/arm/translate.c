a4e2b534f0c8f9cdb3eb32f33b3d1c234e3bf88b
Richard Henderson <richard.henderson@linaro.org>
2019-07-26 10:49:56 -0700

    target/arm: Convert SVC
    Signed-off-by: Richard Henderson <richard.henderson@linaro.org>
    Message-Id: <20190726175032.6769-32-richard.henderson@linaro.org>

--- target/arm/translate.c
+++ target/arm/translate.c
@@ -9980,6 +9980,18 @@ static bool trans_BLX_i(DisasContext *s, arg_BLX_i *a)
 }
 
 /*
+ * Supervisor call
+ */
+
+static bool trans_SVC(DisasContext *s, arg_SVC *a)
+{
+    gen_set_pc_im(s, s->pc);
+    s->svc_imm = a->imm;
+    s->base.is_jmp = DISAS_SWI;
+    return true;
+}
+
+/*
  * Legacy decoder.
  */
 
@@ -10243,6 +10255,7 @@ static void disas_arm_insn(DisasContext *s, unsigned int insn)
         case 0x09:
         case 0xa:
         case 0xb:
+        case 0xf:
             /* All done in decodetree.  Reach here for illegal ops.  */
             goto illegal_op;
         case 0xc:
@@ -10257,12 +10270,6 @@ static void disas_arm_insn(DisasContext *s, unsigned int insn)
                 /* Coprocessor.  */
                 goto illegal_op;
             }
-            break;
-        case 0xf:
-            /* swi */
-            gen_set_pc_im(s, s->pc);
-            s->svc_imm = extract32(insn, 0, 24);
-            s->base.is_jmp = DISAS_SWI;
             break;
         default:
         illegal_op:
