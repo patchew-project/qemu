aea117a6cbba61ce2cf12841585f4653baa0a4ea
Damien Hedde <damien.hedde@greensocs.com>
2019-07-29 16:56:36 +0200

    hw/ide/piix.c: remove qdev_reset_all call
    Replace deprecated qdev_reset_all by device_reset_warm.
    
    This does not impact the behavior.
    
    Signed-off-by: Damien Hedde <damien.hedde@greensocs.com>
    Message-Id: <20190729145654.14644-16-damien.hedde@greensocs.com>

--- hw/ide/piix.c
+++ hw/ide/piix.c
@@ -196,7 +196,7 @@ int pci_piix3_xen_ide_unplug(DeviceState *dev, bool aux)
             blk_unref(blk);
         }
     }
-    qdev_reset_all(DEVICE(dev));
+    device_reset_warm(DEVICE(dev));
     return 0;
 }
 
