ea5158f2e153064a3ada444629365a25704b9e98
Richard Henderson <richard.henderson@linaro.org>
2019-08-03 14:08:00 -0700

    linux-user: Set PAGE_TARGET_1 for TARGET_PROT_BTI
    Transform the prot bit to a qemu internal page bit, and save
    it in the page tables.
    
    Signed-off-by: Richard Henderson <richard.henderson@linaro.org>
    Message-Id: <20190803210803.5701-4-richard.henderson@linaro.org>
    Based-on: 20190803184800.8221-1-richard.henderson@linaro.org

--- linux-user/mmap.c
+++ linux-user/mmap.c
@@ -83,6 +83,22 @@ static int validate_prot_to_pageflags(int *host_prot, int prot)
      */
     *host_prot = prot & (PROT_READ | PROT_WRITE | PROT_EXEC);
 
+#ifdef TARGET_AARCH64
+    /*
+     * The PROT_BTI bit is only accepted if the cpu supports the feature.
+     * Since this is the unusual case, don't bother checking unless
+     * the bit has been requested.  If set and valid, record the bit
+     * within QEMU's page_flags as PAGE_TARGET_1.
+     */
+    if (prot & TARGET_PROT_BTI) {
+        ARMCPU *cpu = ARM_CPU(thread_cpu);
+        if (cpu_isar_feature(aa64_bti, cpu)) {
+            valid |= TARGET_PROT_BTI;
+            page_flags |= PAGE_TARGET_1;
+        }
+    }
+#endif
+
     return prot & ~valid ? 0 : page_flags;
 }
 
