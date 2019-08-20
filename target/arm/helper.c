ee8c9d161bd5a58391a296c62787738e7ad235ac
Richard Henderson <richard.henderson@linaro.org>
2019-08-20 14:07:17 -0700

    target/arm: Hoist store to cs_base in cpu_get_tb_cpu_state
    By performing this store early, we avoid having to save and restore
    the register holding the address around any function calls.
    
    Signed-off-by: Richard Henderson <richard.henderson@linaro.org>
    Message-Id: <20190820210720.18976-15-richard.henderson@linaro.org>

--- target/arm/helper.c
+++ target/arm/helper.c
@@ -11187,6 +11187,7 @@ void cpu_get_tb_cpu_state(CPUARMState *env, target_ulong *pc,
 {
     uint32_t flags, pstate_for_ss;
 
+    *cs_base = 0;
     flags = rebuild_hflags_internal(env);
 
     if (is_a64(env)) {
@@ -11260,7 +11261,6 @@ void cpu_get_tb_cpu_state(CPUARMState *env, target_ulong *pc,
     }
 
     *pflags = flags;
-    *cs_base = 0;
 }
 
 #ifdef TARGET_AARCH64
