c7526d581e0edf71408082cbbcad52219773fffe
Tony Nguyen <tony.nguyen@bt.com>
2019-08-24 04:36:56 +1000

    cputlb: Byte swap memory transaction attribute
    Notice new attribute, byte swap, and force the transaction through the
    memory slow path.
    
    Required by architectures that can invert endianness of memory
    transaction, e.g. SPARC64 has the Invert Endian TTE bit.
    
    Suggested-by: Richard Henderson <richard.henderson@linaro.org>
    Signed-off-by: Tony Nguyen <tony.nguyen@bt.com>
    Reviewed-by: Richard Henderson <richard.henderson@linaro.org>
    Message-Id: <2a10a1f1c00a894af1212c8f68ef09c2966023c1.1566466906.git.tony.nguyen@bt.com>

--- accel/tcg/cputlb.c
+++ accel/tcg/cputlb.c
@@ -738,6 +738,9 @@ void tlb_set_page_with_attrs(CPUState *cpu, target_ulong vaddr,
          */
         address |= TLB_RECHECK;
     }
+    if (attrs.byte_swap) {
+        address |= TLB_FORCE_SLOW;
+    }
     if (!memory_region_is_ram(section->mr) &&
         !memory_region_is_romd(section->mr)) {
         /* IO memory case */
@@ -891,6 +894,10 @@ static uint64_t io_readx(CPUArchState *env, CPUIOTLBEntry *iotlbentry,
     bool locked = false;
     MemTxResult r;
 
+    if (iotlbentry->attrs.byte_swap) {
+        op ^= MO_BSWAP;
+    }
+
     section = iotlb_to_section(cpu, iotlbentry->addr, iotlbentry->attrs);
     mr = section->mr;
     mr_offset = (iotlbentry->addr & TARGET_PAGE_MASK) + addr;
@@ -932,6 +939,10 @@ static void io_writex(CPUArchState *env, CPUIOTLBEntry *iotlbentry,
     MemoryRegion *mr;
     bool locked = false;
     MemTxResult r;
+
+    if (iotlbentry->attrs.byte_swap) {
+        op ^= MO_BSWAP;
+    }
 
     section = iotlb_to_section(cpu, iotlbentry->addr, iotlbentry->attrs);
     mr = section->mr;
