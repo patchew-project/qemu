b7dbdb82df997cda8a6a67e619ac7738343e268d
Dmitry Fomichev <dmitry.fomichev@wdc.com>
2019-08-23 15:22:23 -0400

    block: Add zoned device model property
    This commit adds Zoned Device Model (as defined in T10 ZBC and
    T13 ZAC standards) as a block driver property, along with some
    useful access functions.
    
    A new backend driver permission, BLK_PERM_SUPPORT_ZONED, is also
    introduced. Only the drivers having this permission will be allowed
    to open zoned block devices.
    
    No code is added yet to initialize or check the value of this new
    property, therefore this commit doesn't change any functionality.
    
    Signed-off-by: Dmitry Fomichev <dmitry.fomichev@wdc.com>
    Message-Id: <20190823192226.21154-2-dmitry.fomichev@wdc.com>

--- include/block/block_int.h
+++ include/block/block_int.h
@@ -415,6 +415,7 @@ struct BlockDriver {
     bool (*bdrv_debug_is_suspended)(BlockDriverState *bs, const char *tag);
 
     void (*bdrv_refresh_limits)(BlockDriverState *bs, Error **errp);
+    void (*bdrv_get_zoned_info)(BlockDriverState *bs);
 
     /*
      * Returns 1 if newly created images are guaranteed to contain only
@@ -620,6 +621,9 @@ typedef struct BlockLimits {
 
     /* maximum number of iovec elements */
     int max_iov;
+
+    /* Zoned device model. Zero value indicates a regular block device */
+    uint8_t zoned_model;
 } BlockLimits;
 
 typedef struct BdrvOpBlocker BdrvOpBlocker;
