554b9e8f0310d880cd338c4e8aba5ea396eb3e55
Philippe Mathieu-Daudé <f4bug@amsat.org>
2019-09-15 23:19:36 +0200

    tests/acceptance: Test Open Firmware on the PReP/40p
    User case from:
    https://tyom.blogspot.com/2019/04/aixprep-under-qemu-how-to.html
    
    Signed-off-by: Philippe Mathieu-Daudé <f4bug@amsat.org>
    Message-Id: <20190915211940.30427-3-f4bug@amsat.org>
    Acked-by: David Gibson <david@gibson.dropbear.id.au>

--- tests/acceptance/ppc_prep_40p.py
+++ tests/acceptance/ppc_prep_40p.py
@@ -61,3 +61,24 @@ class IbmPrep40pMachine(Test):
         os_banner = 'NetBSD 4.0 (GENERIC) #0: Sun Dec 16 00:49:40 PST 2007'
         self.wait_for_console_pattern(os_banner)
         self.wait_for_console_pattern('Model: IBM PPS Model 6015')
+
+    def test_openfirmware(self):
+        """
+        :avocado: tags=arch:ppc
+        :avocado: tags=machine:40p
+        """
+        bios_url = ('https://github.com/artyom-tarasenko/openfirmware/'
+                    'releases/download/40p-20190413/q40pofw-serial.rom')
+        bios_hash = '880c80172ea5b2247c0ac2a8bf36bbe385192c72'
+        bios_path = self.fetch_asset(bios_url, asset_hash=bios_hash)
+
+        self.vm.set_machine('40p')
+        self.vm.set_console()
+        self.vm.add_args('-bios', bios_path)
+
+        self.vm.launch()
+        self.wait_for_console_pattern('QEMU PReP/40p')
+        fw_banner = 'Open Firmware, built  April 13, 2019 09:29:23'
+        self.wait_for_console_pattern(fw_banner)
+        prompt_msg = 'Type any key to interrupt automatic startup'
+        self.wait_for_console_pattern(prompt_msg)
