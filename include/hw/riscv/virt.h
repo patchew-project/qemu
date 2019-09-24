f8258db37f8e2abb0f60f15232a1cdbc33973159
Anup Patel <Anup.Patel@wdc.com>
2019-09-24 08:43:05 +0000

    riscv: virt: Use Goldfish RTC device
    We extend QEMU RISC-V virt machine by adding Goldfish RTC device
    to it. This will allow Guest Linux to sync it's local date/time
    with Host date/time via RTC device.
    
    Signed-off-by: Anup Patel <anup.patel@wdc.com>
    Message-Id: <20190924084201.107958-3-anup.patel@wdc.com>

--- include/hw/riscv/virt.h
+++ include/hw/riscv/virt.h
@@ -37,6 +37,7 @@ enum {
     VIRT_DEBUG,
     VIRT_MROM,
     VIRT_TEST,
+    VIRT_RTC,
     VIRT_CLINT,
     VIRT_PLIC,
     VIRT_UART0,
@@ -49,6 +50,7 @@ enum {
 
 enum {
     UART0_IRQ = 10,
+    RTC_IRQ = 11,
     VIRTIO_IRQ = 1, /* 1 to 8 */
     VIRTIO_COUNT = 8,
     PCIE_IRQ = 0x20, /* 32 to 35 */
