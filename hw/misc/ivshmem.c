50752cd2d8796ac9fed33bb473b9f05d7fcc18c9
Vladimir Sementsov-Ogievskiy <vsementsov@virtuozzo.com>
2019-10-01 18:53:19 +0300

    ivshmem: Fix error_append_hint/error_prepend usage
    If we want to add some info to errp (by error_prepend() or
    error_append_hint()), we must use the ERRP_AUTO_PROPAGATE macro.
    Otherwise, this info will not be added when errp == &fatal_err
    (the program will exit prior to the error_append_hint() or
    error_prepend() call).  Fix such cases.
    
    This commit (together with its neighbors) was generated by
    
    git grep -l 'error_\(append_hint\|prepend\)(errp' | while read f; do \
    spatch --sp-file scripts/coccinelle/fix-error-add-info.cocci \
    --in-place $f; done
    
    and then
    
    ./python/commit-per-subsystem.py MAINTAINERS "$(< auto-msg)"
    
    (auto-msg was a file with this commit message)
    
    and then by hand, for not maintained changed files:
    
    git commit -m "<SUB-SYSTEM>: $(< auto-msg)" <FILES>
    
    Still, for backporting it may be more comfortable to use only the first
    command and then do one huge commit.
    
    Reported-by: Greg Kurz <groug@kaod.org>
    Signed-off-by: Vladimir Sementsov-Ogievskiy <vsementsov@virtuozzo.com>
    Message-Id: <20191001155319.8066-32-vsementsov@virtuozzo.com>

--- hw/misc/ivshmem.c
+++ hw/misc/ivshmem.c
@@ -864,6 +864,7 @@ static void ivshmem_write_config(PCIDevice *pdev, uint32_t address,
 
 static void ivshmem_common_realize(PCIDevice *dev, Error **errp)
 {
+    ERRP_AUTO_PROPAGATE();
     IVShmemState *s = IVSHMEM_COMMON(dev);
     Error *err = NULL;
     uint8_t *pci_conf;
