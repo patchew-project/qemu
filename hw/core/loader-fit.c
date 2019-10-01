9a551996b1878a7a2d3e5e7b8d698304dbe0607f
Vladimir Sementsov-Ogievskiy <vsementsov@virtuozzo.com>
2019-10-01 18:52:50 +0300

    hw/core/loader-fit: fix freeing errp in fit_load_fdt
    fit_load_fdt forget to check that errp is not NULL and to zero it after
    freeing. Fix it.
    
    Signed-off-by: Vladimir Sementsov-Ogievskiy <vsementsov@virtuozzo.com>
    Message-Id: <20191001155319.8066-3-vsementsov@virtuozzo.com>

--- hw/core/loader-fit.c
+++ hw/core/loader-fit.c
@@ -200,7 +200,10 @@ static int fit_load_fdt(const struct fit_loader *ldr, const void *itb,
     err = fit_image_addr(itb, img_off, "load", &load_addr, errp);
     if (err == -ENOENT) {
         load_addr = ROUND_UP(kernel_end, 64 * KiB) + (10 * MiB);
-        error_free(*errp);
+        if (errp) {
+            error_free(*errp);
+            *errp = NULL;
+        }
     } else if (err) {
         error_prepend(errp, "unable to read FDT load address from FIT: ");
         ret = err;
