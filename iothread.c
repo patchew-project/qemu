bbf8c65eee265ff5cba499eecd1875e5fd0e087b
Vladimir Sementsov-Ogievskiy <vsementsov@virtuozzo.com>
2019-10-11 19:05:48 +0300

    iothread.c: introduce ERRP_AUTO_PROPAGATE
    If we want to add some info to errp (by error_prepend() or
    error_append_hint()), we must use the ERRP_AUTO_PROPAGATE macro.
    Otherwise, this info will not be added when errp == &fatal_err
    (the program will exit prior to the error_append_hint() or
    error_prepend() call).  Fix such cases.
    
    If we want to check error after errp-function call, we need to
    introduce local_err and than propagate it to errp. Instead, use
    ERRP_AUTO_PROPAGATE macro, benefits are:
    1. No need of explicit error_propagate call
    2. No need of explicit local_err variable: use errp directly
    3. ERRP_AUTO_PROPAGATE leaves errp as is if it's not NULL or
       &error_fatel, this means that we don't break error_abort
       (we'll abort on error_set, not on error_propagate)
    
    This commit (together with its neighbors) was generated by
    
    for f in $(git grep -l errp \*.[ch]); do \
        spatch --sp-file scripts/coccinelle/auto-propagated-errp.cocci \
        --macro-file scripts/cocci-macro-file.h --in-place --no-show-diff $f; \
    done;
    
    then fix a bit of compilation problems: coccinelle for some reason
    leaves several
    f() {
        ...
        goto out;
        ...
        out:
    }
    patterns, with "out:" at function end.
    
    then
    ./python/commit-per-subsystem.py MAINTAINERS "$(< auto-msg)"
    
    (auto-msg was a file with this commit message)
    
    Still, for backporting it may be more comfortable to use only the first
    command and then do one huge commit.
    
    Reported-by: Kevin Wolf <kwolf@redhat.com>
    Reported-by: Greg Kurz <groug@kaod.org>
    Signed-off-by: Vladimir Sementsov-Ogievskiy <vsementsov@virtuozzo.com>
    Message-Id: <20191011160552.22907-123-vsementsov@virtuozzo.com>

--- iothread.c
+++ iothread.c
@@ -163,15 +163,14 @@ static void iothread_init_gcontext(IOThread *iothread)
 
 static void iothread_complete(UserCreatable *obj, Error **errp)
 {
-    Error *local_error = NULL;
+    ERRP_AUTO_PROPAGATE();
     IOThread *iothread = IOTHREAD(obj);
     char *name, *thread_name;
 
     iothread->stopping = false;
     iothread->running = true;
-    iothread->ctx = aio_context_new(&local_error);
+    iothread->ctx = aio_context_new(errp);
     if (!iothread->ctx) {
-        error_propagate(errp, local_error);
         return;
     }
 
@@ -185,9 +184,8 @@ static void iothread_complete(UserCreatable *obj, Error **errp)
                                 iothread->poll_max_ns,
                                 iothread->poll_grow,
                                 iothread->poll_shrink,
-                                &local_error);
-    if (local_error) {
-        error_propagate(errp, local_error);
+                                errp);
+    if (*errp) {
         aio_context_unref(iothread->ctx);
         iothread->ctx = NULL;
         return;
@@ -237,21 +235,21 @@ static void iothread_get_poll_param(Object *obj, Visitor *v,
 static void iothread_set_poll_param(Object *obj, Visitor *v,
         const char *name, void *opaque, Error **errp)
 {
+    ERRP_AUTO_PROPAGATE();
     IOThread *iothread = IOTHREAD(obj);
     PollParamInfo *info = opaque;
     int64_t *field = (void *)iothread + info->offset;
-    Error *local_err = NULL;
     int64_t value;
 
-    visit_type_int64(v, name, &value, &local_err);
-    if (local_err) {
-        goto out;
+    visit_type_int64(v, name, &value, errp);
+    if (*errp) {
+        return;
     }
 
     if (value < 0) {
-        error_setg(&local_err, "%s value must be in range [0, %"PRId64"]",
+        error_setg(errp, "%s value must be in range [0, %"PRId64"]",
                    info->name, INT64_MAX);
-        goto out;
+        return;
     }
 
     *field = value;
@@ -261,11 +259,8 @@ static void iothread_set_poll_param(Object *obj, Visitor *v,
                                     iothread->poll_max_ns,
                                     iothread->poll_grow,
                                     iothread->poll_shrink,
-                                    &local_err);
+                                    errp);
     }
-
-out:
-    error_propagate(errp, local_err);
 }
 
 static void iothread_class_init(ObjectClass *klass, void *class_data)
