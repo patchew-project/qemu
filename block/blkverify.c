a54c2055f82c36c6db31b952039216ef7695afce
Vladimir Sementsov-Ogievskiy <vsementsov@virtuozzo.com>
2019-10-11 19:05:34 +0300

    blkverify: introduce ERRP_AUTO_PROPAGATE
    If we want to add some info to errp (by error_prepend() or
    error_append_hint()), we must use the ERRP_AUTO_PROPAGATE macro.
    Otherwise, this info will not be added when errp == &fatal_err
    (the program will exit prior to the error_append_hint() or
    error_prepend() call).  Fix such cases.
    
    If we want to check error after errp-function call, we need to
    introduce local_err and than propagate it to errp. Instead, use
    ERRP_AUTO_PROPAGATE macro, benefits are:
    1. No need of explicit error_propagate call
    2. No need of explicit local_err variable: use errp directly
    3. ERRP_AUTO_PROPAGATE leaves errp as is if it's not NULL or
       &error_fatel, this means that we don't break error_abort
       (we'll abort on error_set, not on error_propagate)
    
    This commit (together with its neighbors) was generated by
    
    for f in $(git grep -l errp \*.[ch]); do \
        spatch --sp-file scripts/coccinelle/auto-propagated-errp.cocci \
        --macro-file scripts/cocci-macro-file.h --in-place --no-show-diff $f; \
    done;
    
    then fix a bit of compilation problems: coccinelle for some reason
    leaves several
    f() {
        ...
        goto out;
        ...
        out:
    }
    patterns, with "out:" at function end.
    
    then
    ./python/commit-per-subsystem.py MAINTAINERS "$(< auto-msg)"
    
    (auto-msg was a file with this commit message)
    
    Still, for backporting it may be more comfortable to use only the first
    command and then do one huge commit.
    
    Reported-by: Kevin Wolf <kwolf@redhat.com>
    Reported-by: Greg Kurz <groug@kaod.org>
    Signed-off-by: Vladimir Sementsov-Ogievskiy <vsementsov@virtuozzo.com>
    Message-Id: <20191011160552.22907-109-vsementsov@virtuozzo.com>

--- block/blkverify.c
+++ block/blkverify.c
@@ -110,35 +110,32 @@ static QemuOptsList runtime_opts = {
 static int blkverify_open(BlockDriverState *bs, QDict *options, int flags,
                           Error **errp)
 {
+    ERRP_AUTO_PROPAGATE();
     BDRVBlkverifyState *s = bs->opaque;
     QemuOpts *opts;
-    Error *local_err = NULL;
     int ret;
 
     opts = qemu_opts_create(&runtime_opts, NULL, 0, &error_abort);
-    qemu_opts_absorb_qdict(opts, options, &local_err);
-    if (local_err) {
-        error_propagate(errp, local_err);
+    qemu_opts_absorb_qdict(opts, options, errp);
+    if (*errp) {
         ret = -EINVAL;
         goto fail;
     }
 
     /* Open the raw file */
     bs->file = bdrv_open_child(qemu_opt_get(opts, "x-raw"), options, "raw",
-                               bs, &child_file, false, &local_err);
-    if (local_err) {
+                               bs, &child_file, false, errp);
+    if (*errp) {
         ret = -EINVAL;
-        error_propagate(errp, local_err);
         goto fail;
     }
 
     /* Open the test file */
     s->test_file = bdrv_open_child(qemu_opt_get(opts, "x-image"), options,
                                    "test", bs, &child_format, false,
-                                   &local_err);
-    if (local_err) {
+                                   errp);
+    if (*errp) {
         ret = -EINVAL;
-        error_propagate(errp, local_err);
         goto fail;
     }
 
