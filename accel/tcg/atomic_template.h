e80533af42ba44b7cb95f3fa04cc83caf5429d8c
Alex Bennée <alex.bennee@linaro.org>
2019-10-14 11:48:55 +0100

    trace: add mmu_index to mem_info
    We are going to re-use mem_info later for plugins and will need to
    track the mmu_idx for softmmu code.
    
    [TODO: convert everything to use TCGMemOpIdx?]
    
    Signed-off-by: Alex Bennée <alex.bennee@linaro.org>
    
    Message-Id: <20191014104948.4291-3-alex.bennee@linaro.org>

--- accel/tcg/atomic_template.h
+++ accel/tcg/atomic_template.h
@@ -60,7 +60,7 @@
 #endif
 
 #define ATOMIC_TRACE_RMW do {                                           \
-        uint8_t info = glue(trace_mem_build_info_no_se, MEND)(SHIFT, false); \
+        uint16_t info = glue(trace_mem_build_info_no_se, MEND)(SHIFT, false, ATOMIC_MMU_IDX); \
                                                                         \
         trace_guest_mem_before_exec(env_cpu(env), addr, info);      \
         trace_guest_mem_before_exec(env_cpu(env), addr,             \
@@ -68,13 +68,13 @@
     } while (0)
 
 #define ATOMIC_TRACE_LD do {                                            \
-        uint8_t info = glue(trace_mem_build_info_no_se, MEND)(SHIFT, false); \
+        uint16_t info = glue(trace_mem_build_info_no_se, MEND)(SHIFT, false, ATOMIC_MMU_IDX); \
                                                                         \
         trace_guest_mem_before_exec(env_cpu(env), addr, info);      \
     } while (0)
 
 # define ATOMIC_TRACE_ST do {                                           \
-        uint8_t info = glue(trace_mem_build_info_no_se, MEND)(SHIFT, true); \
+        uint16_t info = glue(trace_mem_build_info_no_se, MEND)(SHIFT, true, ATOMIC_MMU_IDX); \
                                                                         \
         trace_guest_mem_before_exec(env_cpu(env), addr, info);      \
     } while (0)
