cb2eab7d3c6854a04b5e18523a02fe0f0e4be667
Klaus Jensen <its@irrelevant.dk>
2019-10-15 12:38:47 +0200

    nvme: refactor device realization
    This patch splits up nvme_realize into multiple individual functions,
    each initializing a different subset of the device.
    
    Signed-off-by: Klaus Jensen <klaus.jensen@cnexlabs.com>
    Message-Id: <20191015103900.313928-8-its@irrelevant.dk>
    Acked-by: Fam Zheng <fam@euphon.net>

--- hw/block/nvme.h
+++ hw/block/nvme.h
@@ -101,4 +101,26 @@ typedef struct NvmeCtrl {
     NvmeIdCtrl      id_ctrl;
 } NvmeCtrl;
 
+static inline NvmeLBAF nvme_ns_lbaf(NvmeNamespace *ns)
+{
+    NvmeIdNs *id_ns = &ns->id_ns;
+    return id_ns->lbaf[NVME_ID_NS_FLBAS_INDEX(id_ns->flbas)];
+}
+
+static inline uint8_t nvme_ns_lbads(NvmeNamespace *ns)
+{
+    return nvme_ns_lbaf(ns).ds;
+}
+
+static inline size_t nvme_ns_lbads_bytes(NvmeNamespace *ns)
+{
+    return 1 << nvme_ns_lbads(ns);
+}
+
+static inline uint64_t nvme_ns_nlbas(NvmeCtrl *n, NvmeNamespace *ns)
+{
+    return n->ns_size >> nvme_ns_lbads(ns);
+}
+
+
 #endif /* HW_NVME_H */
