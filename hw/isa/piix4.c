dc6974df28e43ba6b49b2fcb81256803e5d7366e
Hervé Poussineau <hpoussin@reactos.org>
2019-10-15 18:26:48 +0200

    piix4: add a i8254 pit controller as specified in datasheet
    Remove i8254 instanciated in malta board, to not have it twice.
    
    Acked-by: Michael S. Tsirkin <mst@redhat.com>
    Acked-by: Paolo Bonzini <pbonzini@redhat.com>
    Signed-off-by: Hervé Poussineau <hpoussin@reactos.org>
    Message-Id: <20171216090228.28505-10-hpoussin@reactos.org>
    Signed-off-by: Philippe Mathieu-Daudé <philmd@redhat.com>
    Message-Id: <20191015162705.28087-16-philmd@redhat.com>

--- hw/isa/piix4.c
+++ hw/isa/piix4.c
@@ -30,6 +30,7 @@
 #include "hw/isa/isa.h"
 #include "hw/sysbus.h"
 #include "hw/dma/i8257.h"
+#include "hw/timer/i8254.h"
 #include "migration/vmstate.h"
 #include "sysemu/reset.h"
 #include "sysemu/runstate.h"
@@ -164,6 +165,9 @@ static void piix4_realize(PCIDevice *pci_dev, Error **errp)
 
     /* initialize ISA irqs */
     isa_bus_irqs(isa_bus, s->isa);
+
+    /* initialize pit */
+    i8254_pit_init(isa_bus, 0x40, 0, NULL);
 
     /* DMA */
     i8257_dma_init(isa_bus, 0);
