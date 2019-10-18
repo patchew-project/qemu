03b0abc4f9309bd7c93557b6f383f32a26540cbf
Damien Hedde <damien.hedde@greensocs.com>
2019-10-18 17:06:18 +0200

    add device_legacy_reset function to prepare for reset api change
    Provide a temporary device_legacy_reset function doing what
    device_reset does to prepare for the transition with Resettable
    API.
    
    All occurrence of device_reset in the code tree are also replaced
    by device_legacy_reset.
    
    The new resettable API has different prototype and semantics
    (resetting child buses as well as the specified device). Subsequent
    commits will make the changeover for each call site individually; once
    that is complete device_legacy_reset() will be removed.
    
    Signed-off-by: Damien Hedde <damien.hedde@greensocs.com>
    Reviewed-by: Peter Maydell <peter.maydell@linaro.org>
    Acked-by: David Gibson <david@gibson.dropbear.id.au>
    Message-Id: <20191018150630.31099-2-damien.hedde@greensocs.com>

--- hw/i386/pc.c
+++ hw/i386/pc.c
@@ -2146,7 +2146,7 @@ static void pc_machine_reset(MachineState *machine)
         cpu = X86_CPU(cs);
 
         if (cpu->apic_state) {
-            device_reset(cpu->apic_state);
+            device_legacy_reset(cpu->apic_state);
         }
     }
 }
