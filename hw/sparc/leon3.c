994e8ce61c4eab768e1c7fe8af18ad33a9dfe3e4
Philippe Mathieu-Daudé <philmd@redhat.com>
2019-10-21 00:56:49 +0200

    hw/sparc: Let the machine be the owner of the system memory
    Signed-off-by: Philippe Mathieu-Daudé <philmd@redhat.com>
    Message-Id: <20191020225650.3671-21-philmd@redhat.com>
    Based-on: <20191008113318.7012-1-imammedo@redhat.com>

--- hw/sparc/leon3.c
+++ hw/sparc/leon3.c
@@ -244,7 +244,7 @@ static void leon3_generic_hw_init(MachineState *machine)
         exit(1);
     }
 
-    memory_region_allocate_system_memory(ram, NULL, "leon3.ram", ram_size);
+    memory_region_allocate_system_memory(ram, machine, "leon3.ram", ram_size);
     memory_region_add_subregion(address_space_mem, LEON3_RAM_OFFSET, ram);
 
     /* Allocate BIOS */
