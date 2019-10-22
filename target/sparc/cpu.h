83de904d2dd272238c8249ff909e96f9e4f7b468
Marc-André Lureau <marcandre.lureau@redhat.com>
2019-10-22 18:21:28 +0200

    leon3: use qemu_irq framework instead of callback as property
    "set_pin_in" property is used to define a callback mechanism where the
    device says "call the callback function, passing it an opaque cookie
    and a 32-bit value". We already have a generic mechanism for doing
    that, which is the qemu_irq. So we should just use that.
    
    Signed-off-by: Marc-André Lureau <marcandre.lureau@redhat.com>
    Message-Id: <20191022162137.27161-20-marcandre.lureau@redhat.com>

--- target/sparc/cpu.h
+++ target/sparc/cpu.h
@@ -508,6 +508,7 @@ struct CPUSPARCState {
 #endif
     sparc_def_t def;
 
+    qemu_irq pil_irq;
     void *irq_manager;
     void (*qemu_irq_ack)(CPUSPARCState *env, void *irq_manager, int intno);
 
