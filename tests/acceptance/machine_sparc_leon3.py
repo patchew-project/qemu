d629addb90d50ed261661ec4eb7cb1d2ef451afa
Philippe Mathieu-Daudé <f4bug@amsat.org>
2019-10-28 08:34:26 +0100

    tests/acceptance: Add test that boots Linux up to BusyBox on Leon3
    Gaisler provides convenient images:
    https://www.gaisler.com/index.php/downloads/linux
    
    HOWTO build:
    https://www.gaisler.com/index.php/products/operating-systems/linux
    
    Signed-off-by: Philippe Mathieu-Daudé <f4bug@amsat.org>
    Message-Id: <20191028073441.6448-12-philmd@redhat.com>

--- tests/acceptance/machine_sparc_leon3.py
+++ tests/acceptance/machine_sparc_leon3.py
@@ -10,6 +10,8 @@ import logging
 
 from avocado import skipIf
 from avocado_qemu import MachineTest
+from avocado_qemu import wait_for_console_pattern
+from avocado_qemu import exec_command_and_wait_for_pattern
 
 
 class Leon3Machine(MachineTest):
@@ -35,3 +37,29 @@ class Leon3Machine(MachineTest):
 
         wait_for_console_pattern(self,'Copyright (c) 2001-2014 HelenOS project')
         wait_for_console_pattern(self,'Booting the kernel ...')
+
+    def test_leon3_linux_kernel_4_9_busybox(self):
+        """
+        :avocado: tags=arch:sparc
+        :avocado: tags=machine:leon3
+        """
+        kernel_url = ('https://www.gaisler.com/anonftp/linux/linux-4/images/'
+                     'leon-linux-4.9/leon-linux-4.9-1.0/up/image.ram')
+        kernel_hash = '289bd1bcca10cda76d0ef2264a8657adc251f5f5'
+        kernel_path = self.fetch_asset(kernel_url, asset_hash=kernel_hash)
+
+        self.vm.set_machine('leon3_generic')
+        self.vm.set_console()
+        self.vm.add_args('-kernel', kernel_path)
+
+        self.vm.launch()
+
+        wait_for_console_pattern(self, 'TYPE: Leon3 System-on-a-Chip')
+
+        wait_for_console_pattern(self, 'Welcome to Buildroot')
+
+        wait_for_console_pattern(self, 'buildroot login:')
+        exec_command_and_wait_for_pattern(self, 'root', '#')
+        uname = 'Linux buildroot 4.9.54-00018-g62dab2c #2 ' \
+                'Wed Oct 18 09:45:51 CEST 2017 sparc GNU/Linux'
+        exec_command_and_wait_for_pattern(self, 'uname -a', uname)
