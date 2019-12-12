1b302d0bab8feac47871f88420a736d0c902b973
Dr. David Alan Gilbert <dgilbert@redhat.com>
2019-12-12 16:37:27 +0000

    virtiofsd: Format imported files to qemu style
    Mostly using a set like:
    
    indent -nut -i 4 -nlp -br -cs -ce --no-space-after-function-call-names file
    clang-format -style=file -i -- file
    clang-tidy -fix-errors -checks=readability-braces-around-statements file
    clang-format -style=file -i -- file
    
    With manual cleanups.
    
    The .clang-format used is below.
    
    Signed-off-by: Dr. David Alan Gilbert <dgilbert@redhat.com>
    
    Language:        Cpp
    AlignAfterOpenBracket: Align
    AlignConsecutiveAssignments: false # although we like it, it creates churn
    AlignConsecutiveDeclarations: false
    AlignEscapedNewlinesLeft: true
    AlignOperands:   true
    AlignTrailingComments: false # churn
    AllowAllParametersOfDeclarationOnNextLine: true
    AllowShortBlocksOnASingleLine: false
    AllowShortCaseLabelsOnASingleLine: false
    AllowShortFunctionsOnASingleLine: None
    AllowShortIfStatementsOnASingleLine: false
    AllowShortLoopsOnASingleLine: false
    AlwaysBreakAfterReturnType: None # AlwaysBreakAfterDefinitionReturnType is taken into account
    AlwaysBreakBeforeMultilineStrings: false
    BinPackArguments: true
    BinPackParameters: true
    BraceWrapping:
      AfterControlStatement: false
      AfterEnum:       false
      AfterFunction:   true
      AfterStruct:     false
      AfterUnion:      false
      BeforeElse:      false
      IndentBraces:    false
    BreakBeforeBinaryOperators: None
    BreakBeforeBraces: Custom
    BreakBeforeTernaryOperators: false
    BreakStringLiterals: true
    ColumnLimit:     80
    ContinuationIndentWidth: 4
    Cpp11BracedListStyle: false
    DerivePointerAlignment: false
    DisableFormat:   false
    ForEachMacros:   [
      'CPU_FOREACH',
      'CPU_FOREACH_REVERSE',
      'CPU_FOREACH_SAFE',
      'IOMMU_NOTIFIER_FOREACH',
      'QLIST_FOREACH',
      'QLIST_FOREACH_ENTRY',
      'QLIST_FOREACH_RCU',
      'QLIST_FOREACH_SAFE',
      'QLIST_FOREACH_SAFE_RCU',
      'QSIMPLEQ_FOREACH',
      'QSIMPLEQ_FOREACH_SAFE',
      'QSLIST_FOREACH',
      'QSLIST_FOREACH_SAFE',
      'QTAILQ_FOREACH',
      'QTAILQ_FOREACH_REVERSE',
      'QTAILQ_FOREACH_SAFE',
      'QTAILQ_RAW_FOREACH',
      'RAMBLOCK_FOREACH'
    ]
    IncludeCategories:
      - Regex:           '^"qemu/osdep.h'
        Priority:        -3
      - Regex:           '^"(block|chardev|crypto|disas|exec|fpu|hw|io|libdecnumber|migration|monitor|net|qapi|qemu|qom|standard-headers|sysemu|ui)/'
        Priority:        -2
      - Regex:           '^"(elf.h|qemu-common.h|glib-compat.h|qemu-io.h|trace-tcg.h)'
        Priority:        -1
      - Regex:           '.*'
        Priority:        1
    IncludeIsMainRegex: '$'
    IndentCaseLabels: false
    IndentWidth:     4
    IndentWrappedFunctionNames: false
    KeepEmptyLinesAtTheStartOfBlocks: false
    MacroBlockBegin: '.*_BEGIN$' # only PREC_BEGIN ?
    MacroBlockEnd:   '.*_END$'
    MaxEmptyLinesToKeep: 2
    PointerAlignment: Right
    ReflowComments:  true
    SortIncludes:    true
    SpaceAfterCStyleCast: false
    SpaceBeforeAssignmentOperators: true
    SpaceBeforeParens: ControlStatements
    SpaceInEmptyParentheses: false
    SpacesBeforeTrailingComments: 1
    SpacesInContainerLiterals: true
    SpacesInParentheses: false
    SpacesInSquareBrackets: false
    Standard:        Auto
    UseTab:          Never
    ...
    Message-Id: <20191212163904.159893-8-dgilbert@redhat.com>
    reviewed-bys given how much Ive rebased and tweeked it.

--- tools/virtiofsd/fuse_loop_mt.c
+++ tools/virtiofsd/fuse_loop_mt.c
@@ -1,54 +1,56 @@
 /*
-  FUSE: Filesystem in Userspace
-  Copyright (C) 2001-2007  Miklos Szeredi <miklos@szeredi.hu>
-
-  Implementation of the multi-threaded FUSE session loop.
-
-  This program can be distributed under the terms of the GNU LGPLv2.
-  See the file COPYING.LIB.
-*/
+ * FUSE: Filesystem in Userspace
+ * Copyright (C) 2001-2007  Miklos Szeredi <miklos@szeredi.hu>
+ *
+ * Implementation of the multi-threaded FUSE session loop.
+ *
+ * This program can be distributed under the terms of the GNU LGPLv2.
+ * See the file COPYING.LIB.
+ */
 
 #include "config.h"
+#include "fuse_i.h"
+#include "fuse_kernel.h"
 #include "fuse_lowlevel.h"
 #include "fuse_misc.h"
-#include "fuse_kernel.h"
-#include "fuse_i.h"
 
+#include <assert.h>
+#include <errno.h>
+#include <semaphore.h>
+#include <signal.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-#include <unistd.h>
-#include <signal.h>
-#include <semaphore.h>
-#include <errno.h>
-#include <sys/time.h>
 #include <sys/ioctl.h>
-#include <assert.h>
+#include <sys/time.h>
+#include <unistd.h>
 
 /* Environment var controlling the thread stack size */
 #define ENVNAME_THREAD_STACK "FUSE_THREAD_STACK"
 
 struct fuse_chan *fuse_chan_get(struct fuse_chan *ch)
 {
-	assert(ch->ctr > 0);
-	pthread_mutex_lock(&ch->lock);
-	ch->ctr++;
-	pthread_mutex_unlock(&ch->lock);
+    assert(ch->ctr > 0);
+    pthread_mutex_lock(&ch->lock);
+    ch->ctr++;
+    pthread_mutex_unlock(&ch->lock);
 
-	return ch;
+    return ch;
 }
 
 void fuse_chan_put(struct fuse_chan *ch)
 {
-	if (ch == NULL)
-		return;
-	pthread_mutex_lock(&ch->lock);
-	ch->ctr--;
-	if (!ch->ctr) {
-		pthread_mutex_unlock(&ch->lock);
-		close(ch->fd);
-		pthread_mutex_destroy(&ch->lock);
-		free(ch);
-	} else
-		pthread_mutex_unlock(&ch->lock);
+    if (ch == NULL) {
+        return;
+    }
+    pthread_mutex_lock(&ch->lock);
+    ch->ctr--;
+    if (!ch->ctr) {
+        pthread_mutex_unlock(&ch->lock);
+        close(ch->fd);
+        pthread_mutex_destroy(&ch->lock);
+        free(ch);
+    } else {
+        pthread_mutex_unlock(&ch->lock);
+    }
 }
