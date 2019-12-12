06a3dbdeb2f08d36f891117dedf9479439b3a566
Andrew Jones <drjones@redhat.com>
2019-12-12 18:33:16 +0100

    hw: add compat machines for 5.0
    Add 5.0 machine types for arm/i440fx/q35/s390x/spapr.
    
    Signed-off-by: Andrew Jones <drjones@redhat.com>
    
    Message-Id: <20191212173320.11610-2-drjones@redhat.com>

--- hw/arm/virt.c
+++ hw/arm/virt.c
@@ -2147,10 +2147,17 @@ static void machvirt_machine_init(void)
 }
 type_init(machvirt_machine_init);
 
-static void virt_machine_4_2_options(MachineClass *mc)
+static void virt_machine_5_0_options(MachineClass *mc)
 {
 }
-DEFINE_VIRT_MACHINE_AS_LATEST(4, 2)
+DEFINE_VIRT_MACHINE_AS_LATEST(5, 0)
+
+static void virt_machine_4_2_options(MachineClass *mc)
+{
+    virt_machine_5_0_options(mc);
+    compat_props_add(mc->compat_props, hw_compat_4_2, hw_compat_4_2_len);
+}
+DEFINE_VIRT_MACHINE(4, 2)
 
 static void virt_machine_4_1_options(MachineClass *mc)
 {
