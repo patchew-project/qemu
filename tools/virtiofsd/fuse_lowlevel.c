a20de7e30bc1dded8805736ed3eceb46aae56d4f
Dr. David Alan Gilbert <dgilbert@redhat.com>
2019-12-12 16:37:34 +0000

    virtiofsd: Add options for virtio
    Add options to specify parameters for virtio-fs paths, i.e.
    
       ./virtiofsd -o vhost_user_socket=/tmp/vhostqemu
    
    Signed-off-by: Dr. David Alan Gilbert <dgilbert@redhat.com>
    Message-Id: <20191212163904.159893-15-dgilbert@redhat.com>
    reviewed-bys given how much Ive rebased and tweeked it.

--- tools/virtiofsd/fuse_lowlevel.c
+++ tools/virtiofsd/fuse_lowlevel.c
@@ -2118,8 +2118,12 @@ reply_err:
     }
 
 static const struct fuse_opt fuse_ll_opts[] = {
-    LL_OPTION("debug", debug, 1), LL_OPTION("-d", debug, 1),
-    LL_OPTION("--debug", debug, 1), LL_OPTION("allow_root", deny_others, 1),
+    LL_OPTION("debug", debug, 1),
+    LL_OPTION("-d", debug, 1),
+    LL_OPTION("--debug", debug, 1),
+    LL_OPTION("allow_root", deny_others, 1),
+    LL_OPTION("--socket-path=%s", vu_socket_path, 0),
+    LL_OPTION("vhost_user_socket=%s", vu_socket_path, 0),
     FUSE_OPT_END
 };
 
@@ -2135,9 +2139,12 @@ void fuse_lowlevel_help(void)
      * These are not all options, but the ones that are
      * potentially of interest to an end-user
      */
-    printf("    -o allow_other         allow access by all users\n"
-           "    -o allow_root          allow access by root\n"
-           "    -o auto_unmount        auto unmount on process termination\n");
+    printf(
+        "    -o allow_other             allow access by all users\n"
+        "    -o allow_root              allow access by root\n"
+        "    --socket-path=PATH         path for the vhost-user socket\n"
+        "    -o vhost_user_socket=PATH  path for the vhost-user socket\n"
+        "    -o auto_unmount            auto unmount on process termination\n");
 }
 
 void fuse_session_destroy(struct fuse_session *se)
