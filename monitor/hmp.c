bcb54c52d612ecc7606ace2be91ee5c4c458e691
Philippe Mathieu-Daudé <philmd@redhat.com>
2019-12-17 17:38:04 +0100

    monitor/hmp: Explicit we ignore a QEMUChrEvent in IOEventHandler
    The Chardev events are listed in the QEMUChrEvent enum. To be
    able to use this enum in the IOEventHandler typedef, we need to
    explicit when frontends ignore some events, to silent GCC the
    following warnings:
    
        CC      monitor/hmp.o
      monitor/hmp.c: In function ‘monitor_event’:
      monitor/hmp.c:1330:5: error: enumeration value ‘CHR_EVENT_BREAK’ not handled in switch [-Werror=switch]
       1330 |     switch (event) {
            |     ^~~~~~
      cc1: all warnings being treated as errors
    
    Signed-off-by: Philippe Mathieu-Daudé <philmd@redhat.com>
    Message-Id: <20191217163808.20068-11-philmd@redhat.com>

--- monitor/hmp.c
+++ monitor/hmp.c
@@ -1371,6 +1371,10 @@ static void monitor_event(void *opaque, int event)
         mon_refcount--;
         monitor_fdsets_cleanup();
         break;
+
+    default:
+        /* Ignore */
+        break;
     }
 }
 
