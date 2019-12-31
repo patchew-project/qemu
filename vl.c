6773f71a006ff931a05e97ea50220aeda3752e58
Igor Mammedov <imammedo@redhat.com>
2019-12-31 14:04:05 +0100

    hostmem: fix strict bind policy
    When option -mem-prealloc is used with one or more memory-backend
    objects, created backends may not obey configured bind policy or
    creation may fail after kernel attempts to move pages according
    to bind policy.
    Reason is in file_ram_alloc(), which will pre-allocate
    any descriptor based RAM if global mem_prealloc != 0 and that
    happens way before bind policy is applied to memory range.
    
    One way to fix it would be to extend memory_region_foo() API
    and add more invariants that could broken later due implicit
    dependencies that's hard to track.
    
    Another approach is to drop adhoc main RAM allocation and
    consolidate it around memory-backend. That allows to have
    single place that allocates guest RAM (main and memdev)
    in the same way and then global mem_prealloc could be
    replaced by backend's property[s] that will affect created
    memory-backend objects but only in correct order this time.
    
    With main RAM now converted to hostmem backends, there is no
    point in keeping global mem_prealloc around, so alias
     -mem-prealloc to "memory-backend.prealloc=on"
    machine compat[*] property and make mem_prealloc a local
    variable to only stir registration of compat property.
    
    *) currently user accessible -global works only with DEVICE
       based objects and extra work is needed to make it work
       with hostmem backends. But that is convenience option
       and out of scope of this already huge refactoring.
       Hence machine compat properties were used.
    
    Signed-off-by: Igor Mammedov <imammedo@redhat.com>
    Message-Id: <1577797450-88458-82-git-send-email-imammedo@redhat.com>

--- vl.c
+++ vl.c
@@ -140,7 +140,6 @@ enum vga_retrace_method vga_retrace_method = VGA_RETRACE_DUMB;
 int display_opengl;
 const char* keyboard_layout = NULL;
 ram_addr_t ram_size;
-int mem_prealloc = 0; /* force preallocation of physical target memory */
 bool enable_mlock = false;
 bool enable_cpu_pm = false;
 int nb_nics;
@@ -2868,6 +2867,7 @@ int main(int argc, char **argv, char **envp)
     const char *mem_path = NULL;
     BlockdevOptionsQueue bdo_queue = QSIMPLEQ_HEAD_INITIALIZER(bdo_queue);
     QemuPluginList plugin_list = QTAILQ_HEAD_INITIALIZER(plugin_list);
+    int mem_prealloc = 0; /* force preallocation of physical target memory */
 
     os_set_line_buffering();
 
@@ -3968,6 +3968,7 @@ int main(int argc, char **argv, char **envp)
     if (mem_prealloc) {
         static GlobalProperty p[] = {
             {"memory-backend", "prealloc-threads", NULL},
+            {"memory-backend", "prealloc", "on"},
         };
 
         p[0].value = g_strdup_printf("%d", current_machine->smp.cpus);
