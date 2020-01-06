59ab30bdbf2d7ba9b393f2752e31ef66ae30f80a
Daniel Henrique Barboza <danielhb413@gmail.com>
2020-01-06 15:24:19 -0300

    net/net_tx_pkt.c: remove unneeded label in net_tx_pkt_get_gso_type()
    'func_exit' can be replaced by 'return' with the appropriate
    return value. 'rc' also becomes unnecessary after this change,
    thus let's remove it as well.
    
    CC: Dmitry Fleytman <dmitry.fleytman@gmail.com>
    Signed-off-by: Daniel Henrique Barboza <danielhb413@gmail.com>
    Message-Id: <20200106182425.20312-54-danielhb413@gmail.com>

--- hw/net/net_tx_pkt.c
+++ hw/net/net_tx_pkt.c
@@ -287,21 +287,18 @@ struct virtio_net_hdr *net_tx_pkt_get_vhdr(struct NetTxPkt *pkt)
 static uint8_t net_tx_pkt_get_gso_type(struct NetTxPkt *pkt,
                                           bool tso_enable)
 {
-    uint8_t rc = VIRTIO_NET_HDR_GSO_NONE;
     uint16_t l3_proto;
 
     l3_proto = eth_get_l3_proto(&pkt->vec[NET_TX_PKT_L2HDR_FRAG], 1,
         pkt->vec[NET_TX_PKT_L2HDR_FRAG].iov_len);
 
     if (!tso_enable) {
-        goto func_exit;
+        return VIRTIO_NET_HDR_GSO_NONE;
     }
 
-    rc = eth_get_gso_type(l3_proto, pkt->vec[NET_TX_PKT_L3HDR_FRAG].iov_base,
-                          pkt->l4proto);
-
-func_exit:
-    return rc;
+    return eth_get_gso_type(l3_proto,
+                            pkt->vec[NET_TX_PKT_L3HDR_FRAG].iov_base,
+                            pkt->l4proto);
 }
 
 void net_tx_pkt_build_vheader(struct NetTxPkt *pkt, bool tso_enable,
