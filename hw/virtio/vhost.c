1a8165c8b4a400f263fd11bb2420873e59a98294
Dr. David Alan Gilbert <dgilbert@redhat.com>
2020-01-08 13:53:52 +0000

    vhost: Don't pass ram device sections
    Don't pass RAM blocks that are marked as ram devices to vhost.
    There's normally something special about them and they're not
    normally just shared memory.
    
    Signed-off-by: Dr. David Alan Gilbert <dgilbert@redhat.com>
    Message-Id: <20200108135353.75471-2-dgilbert@redhat.com>

--- hw/virtio/vhost.c
+++ hw/virtio/vhost.c
@@ -402,6 +402,7 @@ static bool vhost_section(struct vhost_dev *dev, MemoryRegionSection *section)
     bool log_dirty = memory_region_get_dirty_log_mask(section->mr) &
                      ~(1 << DIRTY_MEMORY_MIGRATION);
     result = memory_region_is_ram(section->mr) &&
+        !memory_region_is_ram_device(section->mr) &&
         !memory_region_is_rom(section->mr);
 
     /* Vhost doesn't handle any block which is doing dirty-tracking other
