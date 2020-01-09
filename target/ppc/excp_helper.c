474b6f3f3d1d069cbc17f5e61cd1e37ab3e47966
Cédric Le Goater <clg@kaod.org>
2020-01-09 17:33:46 +0100

    target/ppc: add support for Hypervisor Facility Unavailable Exception
    The privileged message send and clear instructions (msgsndp & msgclrp)
    are privileged, but will generate a hypervisor facility unavailable
    exception if not enabled in the HFSCR and executed in privileged
    non-hypervisor state.
    
    Add checks when accessing the DPDES register and when using the
    msgsndp and msgclrp isntructions.
    
    Based on previous work from Suraj Jitindar Singh.
    
    Cc: Suraj Jitindar Singh <sjitindarsingh@gmail.com>
    Signed-off-by: Cédric Le Goater <clg@kaod.org>
    Message-Id: <20200109163346.23062-3-clg@kaod.org>

--- target/ppc/excp_helper.c
+++ target/ppc/excp_helper.c
@@ -471,6 +471,15 @@ static inline void powerpc_excp(PowerPCCPU *cpu, int excp_model, int excp)
         env->spr[SPR_FSCR] |= ((target_ulong)env->error_code << 56);
 #endif
         break;
+    case POWERPC_EXCP_HV_FU:     /* Hypervisor Facility Unavailable Exception */
+#ifdef TARGET_PPC64
+        env->spr[SPR_HFSCR] |= ((target_ulong)env->error_code << FSCR_IC_POS);
+        srr0 = SPR_HSRR0;
+        srr1 = SPR_HSRR1;
+        new_msr |= (target_ulong)MSR_HVB;
+        new_msr |= env->msr & ((target_ulong)1 << MSR_RI);
+#endif
+        break;
     case POWERPC_EXCP_PIT:       /* Programmable interval timer interrupt    */
         LOG_EXCP("PIT exception\n");
         break;
@@ -1285,6 +1294,8 @@ void helper_book3s_msgclrp(CPUPPCState *env, target_ulong rb)
 {
     int irq = book3s_dbell2irq(rb, false);
 
+    helper_hfscr_facility_check(env, HFSCR_MSGP, "msgclrp", HFSCR_IC_MSGP);
+
     if (irq < 0) {
         return;
     }
@@ -1300,6 +1311,8 @@ void helper_book3s_msgsndp(CPUPPCState *env, target_ulong rb)
 {
     int irq = book3s_dbell2irq(rb, false);
     int pir = env->spr_cb[SPR_PIR].default_value;
+
+    helper_hfscr_facility_check(env, HFSCR_MSGP, "msgsndp", HFSCR_IC_MSGP);
 
     if (irq < 0) {
         return;
