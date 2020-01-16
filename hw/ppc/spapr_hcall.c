36f5a125ed0f272136be2c0ea1d957702fe42f65
Greg Kurz <groug@kaod.org>
2020-01-16 16:05:56 +0100

    spapr: Fail CAS if option vector table cannot be parsed
    Most of the option vector helpers have assertions to check their
    arguments aren't null. The guest can provide an arbitrary address
    for the CAS structure that would result in such null arguments.
    Fail CAS with H_PARAMETER instead of aborting QEMU.
    
    Signed-off-by: Greg Kurz <groug@kaod.org>
    Message-Id: <157918715618.376249.7891210201270364781.stgit@bahia.lan>
    Reviewed-by: Philippe Mathieu-Daudé <philmd@redhat.com>

--- hw/ppc/spapr_hcall.c
+++ hw/ppc/spapr_hcall.c
@@ -1701,9 +1701,18 @@ static target_ulong h_client_architecture_support(PowerPCCPU *cpu,
 
     /* For the future use: here @ov_table points to the first option vector */
     ov_table = addr;
+    if (!ov_table) {
+        return H_PARAMETER;
+    }
 
     ov1_guest = spapr_ovec_parse_vector(ov_table, 1);
+    if (!ov1_guest) {
+        return H_PARAMETER;
+    }
     ov5_guest = spapr_ovec_parse_vector(ov_table, 5);
+    if (!ov5_guest) {
+        return H_PARAMETER;
+    }
     if (spapr_ovec_test(ov5_guest, OV5_MMU_BOTH)) {
         error_report("guest requested hash and radix MMU, which is invalid.");
         exit(EXIT_FAILURE);
