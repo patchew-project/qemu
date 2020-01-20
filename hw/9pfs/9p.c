79b5d634bc6eb109923a0e5c6696e6c7e121d7f5
Christian Schoenebeck <qemu_oss@crudebyte.com>
2020-01-21 00:50:33 +0100

    9pfs: validate count sent by client with T_readdir
    A good 9p client sends T_readdir with "count" parameter that's sufficiently
    smaller than client's initially negotiated msize (maximum message size).
    We perform a check for that though to avoid the server to be interrupted
    with a "Failed to encode VirtFS reply type 41" transport error message by
    bad clients. This count value constraint uses msize - 11, because 11 is the
    header size of R_readdir.
    
    Signed-off-by: Christian Schoenebeck <qemu_oss@crudebyte.com>
    Message-Id: <3990d3891e8ae2074709b56449e96ab4b4b93b7d.1579567020.git.qemu_oss@crudebyte.com>

--- hw/9pfs/9p.c
+++ hw/9pfs/9p.c
@@ -2434,6 +2434,7 @@ static void coroutine_fn v9fs_readdir(void *opaque)
     int32_t count;
     uint32_t max_count;
     V9fsPDU *pdu = opaque;
+    V9fsState *s = pdu->s;
 
     retval = pdu_unmarshal(pdu, offset, "dqd", &fid,
                            &initial_offset, &max_count);
@@ -2441,6 +2442,13 @@ static void coroutine_fn v9fs_readdir(void *opaque)
         goto out_nofid;
     }
     trace_v9fs_readdir(pdu->tag, pdu->id, fid, initial_offset, max_count);
+
+    if (max_count > s->msize - 11) {
+        max_count = s->msize - 11;
+        warn_report_once(
+            "9p: bad client: T_readdir with count > msize - 11"
+        );
+    }
 
     fidp = get_fid(pdu, fid);
     if (fidp == NULL) {
