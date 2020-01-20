791149b0d068ce0c574862f126f035693eb11941
Philippe Mathieu-Daudé <f4bug@amsat.org>
2020-01-21 00:51:59 +0100

    tests/boot_linux_console: Tag Emcraft Smartfusion2 as running 'u-boot'
    Avocado tags are handy to automatically select tests matching
    the tags. Since this test also runs U-Boot, tag it.
    
    We can run all the tests using U-Boot as once with:
    
      $ avocado --show=app run -t u-boot tests/acceptance/
      JOB LOG    : avocado/job-results/job-2020-01-21T00.16-ee9344e/job.log
       (1/3) tests/acceptance/boot_linux_console.py:BootLinuxConsole.test_arm_emcraft_sf2: PASS (16.59 s)
       (2/3) tests/acceptance/boot_linux_console.py:BootLinuxConsole.test_arm_raspi2_uboot: PASS (0.47 s)
       (3/3) tests/acceptance/boot_linux_console.py:BootLinuxConsole.test_aarch64_raspi3_uboot: PASS (2.43 s)
      RESULTS    : PASS 3 | ERROR 0 | FAIL 0 | SKIP 0 | WARN 0 | INTERRUPT 0 | CANCEL 0
      JOB TIME   : 19.78 s
    
    Signed-off-by: Philippe Mathieu-Daudé <f4bug@amsat.org>
    Message-Id: <20200120235159.18510-8-f4bug@amsat.org>
    Supersedes: <20191019234715.25750-1-f4bug@amsat.org>

--- tests/acceptance/boot_linux_console.py
+++ tests/acceptance/boot_linux_console.py
@@ -305,6 +305,7 @@ class BootLinuxConsole(Test):
         :avocado: tags=arch:arm
         :avocado: tags=machine:emcraft-sf2
         :avocado: tags=endian:little
+        :avocado: tags=u-boot
         """
         uboot_url = ('https://raw.githubusercontent.com/'
                      'Subbaraya-Sundeep/qemu-test-binaries/'
