ce11ec0bf4a131af9ca7eb60d8bc8130656ef08b
Aleksandar Markovic <amarkovic@wavecomp.com>
2020-01-24 16:47:18 +0100

    linux-user: Add support for FIFREEZE and FITHAW ioctls
    Both FIFREEZE and FITHAW ioctls accept an integer as their third
    argument.
    
    All ioctls in this group (FI* ioctl) are guarded with "#ifdef", so the
    guards are used in this implementation too for consistency (however,
    many of ioctls in FI* group became old enough that their #ifdef guards
    could be removed, bit this is out of the scope of this patch).
    
    Reviewed-by: Laurent Vivier <laurent@vivier.eu>
    Signed-off-by: Aleksandar Markovic <amarkovic@wavecomp.com>
    Message-Id: <1579880839-31466-4-git-send-email-aleksandar.markovic@rt-rk.com>

--- linux-user/syscall_defs.h
+++ linux-user/syscall_defs.h
@@ -950,7 +950,11 @@ struct target_rtc_pll_info {
 #define TARGET_FIBMAP     TARGET_IO(0x00,1)  /* bmap access */
 #define TARGET_FIGETBSZ   TARGET_IO(0x00,2)  /* get the block size used for bmap */
 
+#define TARGET_FIFREEZE   TARGET_IOWR('X', 119, int)    /* Freeze */
+#define TARGET_FITHAW     TARGET_IOWR('X', 120, int)    /* Thaw */
+#ifdef FITRIM
 #define TARGET_FITRIM     TARGET_IOWR('X', 121, struct fstrim_range)
+#endif
 #define TARGET_FICLONE    TARGET_IOW(0x94, 9, int)
 #define TARGET_FICLONERANGE TARGET_IOW(0x94, 13, struct file_clone_range)
 
