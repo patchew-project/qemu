d595cdeb3f1f42e1bafe1716320ab863cb0ba479
Juan Quintela <quintela@redhat.com>
2020-01-28 10:05:44 +0100

    migration: Add support for modules
    So we don't have to compile everything in, or have ifdefs
    
    Signed-off-by: Juan Quintela <quintela@redhat.com>
    Message-Id: <20200128090550.5112-2-quintela@redhat.com>
    Based-on: 20200127223321.2742-1-quintela@redhat.com

--- vl.c
+++ vl.c
@@ -2890,6 +2890,7 @@ int main(int argc, char **argv, char **envp)
     qemu_init_exec_dir(argv[0]);
 
     module_call_init(MODULE_INIT_QOM);
+    module_call_init(MODULE_INIT_MIGRATION);
 
     qemu_add_opts(&qemu_drive_opts);
     qemu_add_drive_opts(&qemu_legacy_drive_opts);
