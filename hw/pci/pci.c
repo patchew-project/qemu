a5f6a124b3f38bdea983b0719b189ad8c27be95d
Liu Yi L <yi.l.liu@intel.com>
2020-01-29 04:16:35 -0800

    hw/pci: introduce pci_device_iommu_context()
    This patch adds pci_device_iommu_context() to get an iommu_context
    for a given device. A new callback is added in PCIIOMMUOps. Users
    who wants to call into vIOMMU could leaverage it. And further vIOMMU
    operation could be done via IOMMUContext.
    
    Note:
    Previous version has got Reviewed by from David Gibson and Peter Xu.
    But this version has slight changes due to the patch rebase. so I
    didn't add reviewed by from you two in case of that you two want to
    have one more review. If you two have no objection, I would add your
    review by in next version. Thanks.
    
    Cc: Kevin Tian <kevin.tian@intel.com>
    Cc: Jacob Pan <jacob.jun.pan@linux.intel.com>
    Cc: Peter Xu <peterx@redhat.com>
    Cc: Eric Auger <eric.auger@redhat.com>
    Cc: Yi Sun <yi.y.sun@linux.intel.com>
    Cc: David Gibson <david@gibson.dropbear.id.au>
    Cc: Michael S. Tsirkin <mst@redhat.com>
    Signed-off-by: Liu Yi L <yi.l.liu@intel.com>
    Message-Id: <1580300216-86172-5-git-send-email-yi.l.liu@intel.com>

--- hw/pci/pci.c
+++ hw/pci/pci.c
@@ -2638,7 +2638,8 @@ static void pci_device_class_base_init(ObjectClass *klass, void *data)
     }
 }
 
-AddressSpace *pci_device_iommu_address_space(PCIDevice *dev)
+static void pci_device_get_iommu_bus_devfn(PCIDevice *dev,
+                              PCIBus **pbus, uint8_t *pdevfn)
 {
     PCIBus *bus = pci_get_bus(dev);
     PCIBus *iommu_bus = bus;
@@ -2683,12 +2684,36 @@ AddressSpace *pci_device_iommu_address_space(PCIDevice *dev)
 
         iommu_bus = parent_bus;
     }
-    if (iommu_bus && iommu_bus->iommu_ops &&
-                     iommu_bus->iommu_ops->get_address_space) {
-        return iommu_bus->iommu_ops->get_address_space(bus,
-                                 iommu_bus->iommu_opaque, devfn);
+    *pbus = iommu_bus;
+    *pdevfn = devfn;
+}
+
+AddressSpace *pci_device_iommu_address_space(PCIDevice *dev)
+{
+    PCIBus *bus;
+    uint8_t devfn;
+
+    pci_device_get_iommu_bus_devfn(dev, &bus, &devfn);
+    if (bus && bus->iommu_ops &&
+                     bus->iommu_ops->get_address_space) {
+        return bus->iommu_ops->get_address_space(bus,
+                                bus->iommu_opaque, devfn);
     }
     return &address_space_memory;
+}
+
+IOMMUContext *pci_device_iommu_context(PCIDevice *dev)
+{
+    PCIBus *bus;
+    uint8_t devfn;
+
+    pci_device_get_iommu_bus_devfn(dev, &bus, &devfn);
+    if (bus && bus->iommu_ops &&
+                    bus->iommu_ops->get_iommu_context) {
+        return bus->iommu_ops->get_iommu_context(bus,
+                              bus->iommu_opaque, devfn);
+    }
+    return NULL;
 }
 
 void pci_setup_iommu(PCIBus *bus, const PCIIOMMUOps *ops, void *opaque)
