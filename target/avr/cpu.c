3c1d0fd90960c6d2bdafce1e96a69b0991982127
Michael Rolnik <mrolnik@gmail.com>
2020-01-31 01:02:47 +0100

    target/avr: Add migration support
    Add migration functions for AVR cores.
    
    [AM: Split a larger AVR introduction patch into logical units]
    Suggested-by: Aleksandar Markovic <aleksandar.m.mail@gmail.com>
    
    Co-developed-by: Michael Rolnik <mrolnik@gmail.com>
    Co-developed-by: Sarah Harris <S.E.Harris@kent.ac.uk>
    Signed-off-by: Michael Rolnik <mrolnik@gmail.com>
    Signed-off-by: Sarah Harris <S.E.Harris@kent.ac.uk>
    Signed-off-by: Michael Rolnik <mrolnik@gmail.com>
    Acked-by: Igor Mammedov <imammedo@redhat.com>
    Tested-by: Philippe Mathieu-Daudé <philmd@redhat.com>
    Signed-off-by: Richard Henderson <richard.henderson@linaro.org>
    Signed-off-by: Aleksandar Markovic <aleksandar.m.mail@gmail.com>
    Message-Id: <1580428993-4767-4-git-send-email-aleksandar.markovic@rt-rk.com>
    Based-on: <1580428993-4767-1-git-send-email-aleksandar.markovic@rt-rk.com>

--- target/avr/cpu.c
+++ target/avr/cpu.c
@@ -205,6 +205,7 @@ static void avr_cpu_class_init(ObjectClass *oc, void *data)
     cc->set_pc = avr_cpu_set_pc;
     cc->memory_rw_debug = avr_cpu_memory_rw_debug;
     cc->get_phys_page_debug = avr_cpu_get_phys_page_debug;
+    cc->vmsd = &vms_avr_cpu;
     cc->disas_set_info = avr_cpu_disas_set_info;
     cc->tlb_fill = avr_cpu_tlb_fill;
     cc->tcg_initialize = avr_cpu_tcg_init;
