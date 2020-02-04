fed086a4a87ae51038ca5492f5fef1a68f56083a
Felipe Franciosi <felipe@nutanix.com>
2020-02-04 13:16:01 +0000

    qom/object: Use common get/set uint helpers
    Several objects implemented their own uint property getters and setters,
    despite them being straightforward (without any checks/validations on
    the values themselves) and identical across objects. This makes use of
    an enhanced API for object_property_add_uintXX_ptr() which offers
    default setters.
    
    Some of these setters used to update the value even if the type visit
    failed (eg. because the value being set overflowed over the given type).
    The new setter introduces a check for these errors, not updating the
    value if an error occurred. The error is propagated.
    
    Signed-off-by: Felipe Franciosi <felipe@nutanix.com>
    Message-Id: <20200204131601.125677-5-felipe@nutanix.com>
    Reviewed-by: Marc-André Lureau <marcandre.lureau@redhat.com>

--- target/arm/cpu.c
+++ target/arm/cpu.c
@@ -1043,22 +1043,6 @@ static void arm_set_pmu(Object *obj, bool value, Error **errp)
     cpu->has_pmu = value;
 }
 
-static void arm_get_init_svtor(Object *obj, Visitor *v, const char *name,
-                               void *opaque, Error **errp)
-{
-    ARMCPU *cpu = ARM_CPU(obj);
-
-    visit_type_uint32(v, name, &cpu->init_svtor, errp);
-}
-
-static void arm_set_init_svtor(Object *obj, Visitor *v, const char *name,
-                               void *opaque, Error **errp)
-{
-    ARMCPU *cpu = ARM_CPU(obj);
-
-    visit_type_uint32(v, name, &cpu->init_svtor, errp);
-}
-
 unsigned int gt_cntfrq_period_ns(ARMCPU *cpu)
 {
     /*
@@ -1183,9 +1167,9 @@ void arm_cpu_post_init(Object *obj)
          * a simple DEFINE_PROP_UINT32 for this because we want to permit
          * the property to be set after realize.
          */
-        object_property_add(obj, "init-svtor", "uint32",
-                            arm_get_init_svtor, arm_set_init_svtor,
-                            NULL, NULL, &error_abort);
+        object_property_add_uint32_ptr(obj, "init-svtor",
+                                       &cpu->init_svtor,
+                                       OBJ_PROP_FLAG_READWRITE, &error_abort);
     }
 
     qdev_property_add_static(DEVICE(obj), &arm_cpu_cfgend_property);
