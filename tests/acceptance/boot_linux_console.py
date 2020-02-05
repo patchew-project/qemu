59e10dd17548b59d73cdb6c7543e7a456b67d4cd
Liam Merwick <liam.merwick@oracle.com>
2020-02-05 14:56:00 +0000

    tests/boot_linux_console: add microvm acceptance test
    Refactor test_x86_64_pc() to test_x86_64_machine() so that separate
    functions which specify the Avocado tag of ':avocado: tags=machine:'
    as being either 'pc' or 'microvm' can be used to test booting a
    compressed kernel using either machine class.
    
    Signed-off-by: Liam Merwick <liam.merwick@oracle.com>
    Reviewed-by: Stefano Garzarella <sgarzare@redhat.com>
    Message-Id: <1580914565-19675-2-git-send-email-liam.merwick@oracle.com>

--- tests/acceptance/boot_linux_console.py
+++ tests/acceptance/boot_linux_console.py
@@ -51,10 +51,10 @@ class BootLinuxConsole(Test):
         os.chdir(cwd)
         return self.workdir + path
 
-    def test_x86_64_pc(self):
+    def do_test_x86_64_machine(self):
         """
-        :avocado: tags=arch:x86_64
-        :avocado: tags=machine:pc
+        Common routine to boot an x86_64 guest.
+        Caller must specify tags=arch and tags=machine
         """
         kernel_url = ('https://archives.fedoraproject.org/pub/archive/fedora'
                       '/linux/releases/29/Everything/x86_64/os/images/pxeboot'
@@ -69,6 +69,20 @@ class BootLinuxConsole(Test):
         self.vm.launch()
         console_pattern = 'Kernel command line: %s' % kernel_command_line
         self.wait_for_console_pattern(console_pattern)
+
+    def test_x86_64_pc(self):
+        """
+        :avocado: tags=arch:x86_64
+        :avocado: tags=machine:pc
+        """
+        self.do_test_x86_64_machine()
+
+    def test_x86_64_microvm(self):
+        """
+        :avocado: tags=arch:x86_64
+        :avocado: tags=machine:microvm
+        """
+        self.do_test_x86_64_machine()
 
     def test_mips_malta(self):
         """
