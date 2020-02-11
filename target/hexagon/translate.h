0f1412746a4b1d97f73456945fcf2e2bde983447
Taylor Simpson <tsimpson@quicinc.com>
2020-02-10 18:40:28 -0600

    Hexagon translation
    Read the instruction memory
    Create a packet data structure
    Generate TCG code for the start of the packet
    Invoke the generate function for each instruction
    Generate TCG code for the end of the packet
    
    Signed-off-by: Taylor Simpson <tsimpson@quicinc.com>
    Message-Id: <1581381644-13678-51-git-send-email-tsimpson@quicinc.com>

---
+++ target/hexagon/translate.h
@@ -0,0 +1,82 @@
+/*
+ *  Copyright (c) 2019 Qualcomm Innovation Center, Inc. All Rights Reserved.
+ *
+ *  This program is free software; you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation; either version 2 of the License, or
+ *  (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program; if not, see <http://www.gnu.org/licenses/>.
+ */
+
+#ifndef TRANSLATE_H
+#define TRANSLATE_H
+
+#include "cpu.h"
+#include "exec/translator.h"
+#include "exec/helper-proto.h"
+#include "exec/helper-gen.h"
+#include "internal.h"
+
+typedef struct DisasContext {
+    DisasContextBase base;
+    uint32_t mem_idx;
+    int ctx_reg_log[REG_WRITES_MAX];
+    int ctx_reg_log_idx;
+    int ctx_preg_log[PRED_WRITES_MAX];
+    int ctx_preg_log_idx;
+    uint8_t ctx_store_width[STORES_MAX];
+} DisasContext;
+
+static inline void ctx_log_reg_write(DisasContext *ctx, int rnum)
+{
+#if HEX_DEBUG
+    int i;
+    for (i = 0; i < ctx->ctx_reg_log_idx; i++) {
+        if (ctx->ctx_reg_log[i] == rnum) {
+            HEX_DEBUG_LOG("WARNING: Multiple writes to r%d\n", rnum);
+        }
+    }
+#endif
+    ctx->ctx_reg_log[ctx->ctx_reg_log_idx] = rnum;
+    ctx->ctx_reg_log_idx++;
+}
+
+static inline void ctx_log_pred_write(DisasContext *ctx, int pnum)
+{
+    ctx->ctx_preg_log[ctx->ctx_preg_log_idx] = pnum;
+    ctx->ctx_preg_log_idx++;
+}
+
+extern TCGv hex_gpr[TOTAL_PER_THREAD_REGS];
+extern TCGv hex_pred[NUM_PREGS];
+extern TCGv hex_next_PC;
+extern TCGv hex_this_PC;
+extern TCGv hex_slot_cancelled;
+extern TCGv hex_branch_taken;
+extern TCGv hex_new_value[TOTAL_PER_THREAD_REGS];
+extern TCGv hex_reg_written[TOTAL_PER_THREAD_REGS];
+extern TCGv hex_new_pred_value[NUM_PREGS];
+extern TCGv hex_pred_written[NUM_PREGS];
+extern TCGv hex_store_addr[STORES_MAX];
+extern TCGv hex_store_width[STORES_MAX];
+extern TCGv hex_store_val32[STORES_MAX];
+extern TCGv_i64 hex_store_val64[STORES_MAX];
+extern TCGv hex_dczero_addr;
+extern TCGv llsc_addr;
+extern TCGv llsc_val;
+extern TCGv_i64 llsc_val_i64;
+extern TCGv hex_is_gather_store_insn;
+extern TCGv hex_gather_issued;
+
+void hexagon_translate_init(void);
+extern void gen_exception(int excp);
+extern void gen_exception_debug(void);
+
+#endif
