9cc82ffd108afd4fb029a6f92a4b3dadbda69d21
Philippe Mathieu-Daudé <philmd@redhat.com>
2020-02-12 21:36:27 +0100

    tests/acceptance/machine_sparc_leon3: Do not run HelenOS test by default
    The www.helenos.org server is slow and downloading the Leon3 binary
    takes too long [*]. Do not include this test in the default suite.
    
    Similarly to commit 471c97a69b:
    
      Currently the Avocado framework does not distinct the time spent
      downloading assets vs. the time spent running a test. With big
      assets (like a full VM image) the tests likely fail.
    
      This is a limitation known by the Avocado team.
      Until this issue get fixed, do not run this tests automatically.
    
      Tests can still be run setting the AVOCADO_TIMEOUT_EXPECTED
      environment variable.
    
    [*] https://travis-ci.org/stsquad/qemu/jobs/649599880#L4198
    
    Reported-by: Alex Bennée <alex.bennee@linaro.org>
    Signed-off-by: Philippe Mathieu-Daudé <philmd@redhat.com>
    Message-Id: <20200212203627.16592-1-philmd@redhat.com>
    Reviewed-by: Wainer dos Santos Moschetta <wainersm@redhat.com>
    Reviewed-by: KONRAD Frederic <frederic.konrad@adacore.com>

--- tests/acceptance/machine_sparc_leon3.py
+++ tests/acceptance/machine_sparc_leon3.py
@@ -5,6 +5,9 @@
 # This work is licensed under the terms of the GNU GPL, version 2 or
 # later. See the COPYING file in the top-level directory.
 
+import os
+
+from avocado import skipUnless
 from avocado_qemu import Test
 from avocado_qemu import wait_for_console_pattern
 
@@ -13,6 +16,7 @@ class Leon3Machine(Test):
 
     timeout = 60
 
+    @skipUnless(os.getenv('AVOCADO_TIMEOUT_EXPECTED'), 'Test might timeout')
     def test_leon3_helenos_uimage(self):
         """
         :avocado: tags=arch:sparc
