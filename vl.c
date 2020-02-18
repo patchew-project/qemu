51ba4f9d2c7b111b0f72c0eebca8634fd128eecb
Coiby Xu <coiby.xu@gmail.com>
2020-02-18 13:07:09 +0800

    vhost-user block device backend server
    By making use of libvhost, multiple block device drives can be exported
    and each drive can serve multiple clients simultaneously.
    Since vhost-user-server needs a block drive to be created first, delay
    the creation of this object.
    
    Signed-off-by: Coiby Xu <coiby.xu@gmail.com>
    Message-Id: <20200218050711.8133-4-coiby.xu@gmail.com>

--- vl.c
+++ vl.c
@@ -2538,6 +2538,10 @@ static bool object_create_initial(const char *type, QemuOpts *opts)
     }
 #endif
 
+    /* Reason: vhost-user-server property "name" */
+    if (g_str_equal(type, "vhost-user-blk-server")) {
+        return false;
+    }
     /*
      * Reason: filter-* property "netdev" etc.
      */
