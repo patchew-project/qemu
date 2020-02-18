1e909f289147e9db787e903f9dfad88e8ffebb6b
John Snow <jsnow@redhat.com>
2020-02-18 11:07:36 +0100

    drive-mirror: add support for conditional and always bitmap sync modes
    Teach mirror two new tricks for using bitmaps:
    
    Always: no matter what, we synchronize the copy_bitmap back to the
    sync_bitmap. In effect, this allows us resume a failed mirror at a later
    date.
    
    Conditional: On success only, we sync the bitmap. This is akin to
    incremental backup modes; we can use this bitmap to later refresh a
    successfully created mirror.
    Message-Id: <20200218100740.2228521-3-f.gruenbichler@proxmox.com>

--- block/mirror.c
+++ block/mirror.c
@@ -645,8 +645,6 @@ static int mirror_exit_common(Job *job)
         bdrv_unfreeze_backing_chain(mirror_top_bs, target_bs);
     }
 
-    bdrv_release_dirty_bitmap(s->dirty_bitmap);
-
     /* Make sure that the source BDS doesn't go away during bdrv_replace_node,
      * before we can call bdrv_drained_end */
     bdrv_ref(src);
@@ -730,6 +728,18 @@ static int mirror_exit_common(Job *job)
     blk_remove_bs(bjob->blk);
     blk_set_perm(bjob->blk, 0, BLK_PERM_ALL, &error_abort);
     blk_insert_bs(bjob->blk, mirror_top_bs, &error_abort);
+
+    if (s->sync_bitmap) {
+        if (s->bitmap_mode == BITMAP_SYNC_MODE_ALWAYS ||
+            (s->bitmap_mode == BITMAP_SYNC_MODE_ON_SUCCESS &&
+             job->ret == 0 && ret == 0)) {
+            /* Success; synchronize copy back to sync. */
+            bdrv_clear_dirty_bitmap(s->sync_bitmap, NULL);
+            bdrv_merge_dirty_bitmap(s->sync_bitmap, s->dirty_bitmap,
+                                    NULL, &error_abort);
+        }
+    }
+    bdrv_release_dirty_bitmap(s->dirty_bitmap);
 
     bs_opaque->job = NULL;
 
@@ -1562,10 +1572,6 @@ static BlockJob *mirror_start_job(
                        " sync mode",
                        MirrorSyncMode_str(sync_mode));
             return NULL;
-        } else if (bitmap_mode != BITMAP_SYNC_MODE_NEVER) {
-            error_setg(errp,
-                       "Bitmap Sync Mode '%s' is not supported by Mirror",
-                       BitmapSyncMode_str(bitmap_mode));
         }
     } else if (bitmap) {
         error_setg(errp,
@@ -1582,6 +1588,12 @@ static BlockJob *mirror_start_job(
             return NULL;
         }
         granularity = bdrv_dirty_bitmap_granularity(bitmap);
+
+        if (bitmap_mode != BITMAP_SYNC_MODE_NEVER) {
+            if (bdrv_dirty_bitmap_check(bitmap, BDRV_BITMAP_DEFAULT, errp)) {
+                return NULL;
+            }
+        }
     } else if (granularity == 0) {
         granularity = bdrv_get_default_bitmap_granularity(target);
     }
