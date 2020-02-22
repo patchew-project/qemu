666c331755a3324ce24b18de72bc17c0f77d3164
Eric Blake <eblake@redhat.com>
2020-02-22 05:23:41 -0600

    qemu-img: Deprecate use of -b without -F
    Creating an image that requires format probing of the backing image is
    inherently unsafe (we've had several CVEs over the years based on
    probes leaking information to the guest on a subsequent boot).  If our
    probing algorithm ever changes, or if other tools like libvirt
    determine a different probe result than we do, then subsequent use of
    that backing file under a different format will present corrupted data
    to the guest.  Start a deprecation clock so that future qemu-img can
    refuse to create unsafe backing chains that would rely on probing.
    
    However, there is one time where probing is safe: when we first create
    an image, no guest has yet used the new image, so as long as we record
    what we probed, all future uses of the image will see the same data -
    so the code now records the probe results as if the user had passed
    -F.  When this happens, it is unconditionally safe to record a probe
    of 'raw', but any other probe is still worth warning the user in case
    our probe differed from their expectations.  Similarly, if the backing
    file name uses the json: psuedo-protocol, the backing name includes
    the format.
    
    iotest 114 specifically wants to create an unsafe image for later
    amendment rather than defaulting to our new default of recording a
    probed format, so it needs an update.
    
    Signed-off-by: Eric Blake <eblake@redhat.com>
    Message-Id: <20200222112341.4170045-4-eblake@redhat.com>

--- block.c
+++ block.c
@@ -6013,6 +6013,15 @@ void bdrv_img_create(const char *filename, const char *fmt,
                               "Could not open backing image to determine size.\n");
             goto out;
         } else {
+            if (!backing_fmt && !strstart(backing_file, "json:", NULL)) {
+                backing_fmt = bs->drv->format_name;
+                qemu_opt_set(opts, BLOCK_OPT_BACKING_FMT, backing_fmt, NULL);
+                if (bs->drv != &bdrv_raw) {
+                    warn_report("Deprecated use of non-raw backing file "
+                                "without explicit backing format, using "
+                                "detected format of %s", backing_fmt);
+                }
+            }
             if (size == -1) {
                 /* Opened BS, have no size */
                 size = bdrv_getlength(bs);
@@ -6026,7 +6035,13 @@ void bdrv_img_create(const char *filename, const char *fmt,
             }
             bdrv_unref(bs);
         }
-    } /* (backing_file && !(flags & BDRV_O_NO_BACKING)) */
+        /* (backing_file && !(flags & BDRV_O_NO_BACKING)) */
+    } else if (backing_file && !backing_fmt &&
+               !strstart(backing_file, "json:", NULL)) {
+        warn_report("Deprecated use of unopened backing file without "
+                    "explicit backing format, use of this image requires "
+                    "potentially unsafe format probing");
+    }
 
     if (size == -1) {
         error_setg(errp, "Image creation needs a size parameter");
