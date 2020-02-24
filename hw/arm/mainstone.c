5fb582f1605ca6a5bcb009d186a768d444ecadf8
Philippe Mathieu-Daudé <philmd@redhat.com>
2020-02-24 21:55:06 +0100

    hw/arm: Use memory_region_init_rom() with read-only regions
    This commit was produced with the Coccinelle script
    scripts/coccinelle/memory-region-housekeeping.cocci.
    
    Signed-off-by: Philippe Mathieu-Daudé <philmd@redhat.com>
    Message-Id: <20200224205533.23798-6-philmd@redhat.com>
    Supersedes: <20200221173049.18134-1-philmd@redhat.com>

--- hw/arm/mainstone.c
+++ hw/arm/mainstone.c
@@ -125,9 +125,8 @@ static void mainstone_common_init(MemoryRegion *address_space_mem,
     /* Setup CPU & memory */
     mpu = pxa270_init(address_space_mem, mainstone_binfo.ram_size,
                       machine->cpu_type);
-    memory_region_init_ram(rom, NULL, "mainstone.rom", MAINSTONE_ROM,
+    memory_region_init_rom(rom, NULL, "mainstone.rom", MAINSTONE_ROM,
                            &error_fatal);
-    memory_region_set_readonly(rom, true);
     memory_region_add_subregion(address_space_mem, 0, rom);
 
 #ifdef TARGET_WORDS_BIGENDIAN
