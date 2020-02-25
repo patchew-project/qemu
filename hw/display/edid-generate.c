1e59456ab6e70f1c2ff3df15a0c0d518f6016f0e
Anton V. Boyarshinov <boyarsh@altlinux.org>
2020-02-25 23:09:14 +0300

    Arithmetic error fixed in EDID generation
    To compute screen size in centimeters we should calculate:
    pixels/dpi*2.54
    but not
    pixels*dpi/2540
    
    Using wrong formula we actually get 65 DPI and very small fonts.
    
    Signed-off-by: Anton V. Boyarshinov <boyarsh@altlinux.org>
    Message-Id: <20200225200914.3ec2c07b@x230.localdomain>

--- hw/display/edid-generate.c
+++ hw/display/edid-generate.c
@@ -360,8 +360,8 @@ void qemu_edid_generate(uint8_t *edid, size_t size,
     edid[20] = 0xa5;
 
     /* screen size: undefined */
-    edid[21] = info->prefx * info->dpi / 2540;
-    edid[22] = info->prefy * info->dpi / 2540;
+    edid[21] = (uint8_t) ((float) info->prefx / info->dpi * 2.54);
+    edid[22] = (uint8_t) ((float) info->prefy / info->dpi * 2.54);
 
     /* display gamma: 2.2 */
     edid[23] = 220 - 100;
