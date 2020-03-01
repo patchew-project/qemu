55e5e2a980475d692c0e4d1e9c7dc3f50d0f5c0a
Niek Linnenbank <nieklinnenbank@gmail.com>
2020-03-01 22:50:19 +0100

    hw/arm/allwinner: add SD/MMC host controller
    The Allwinner System on Chip families sun4i and above contain
    an integrated storage controller for Secure Digital (SD) and
    Multi Media Card (MMC) interfaces. This commit adds support
    for the Allwinner SD/MMC storage controller with the following
    emulated features:
    
     * DMA transfers
     * Direct FIFO I/O
     * Short/Long format command responses
     * Auto-Stop command (CMD12)
     * Insert & remove card detection
    
    The following boards are extended with the SD host controller:
    
     * Cubieboard (hw/arm/cubieboard.c)
     * Orange Pi PC (hw/arm/orangepi.c)
    
    Signed-off-by: Niek Linnenbank <nieklinnenbank@gmail.com>
    Tested-by: Philippe Mathieu-Daudé <philmd@redhat.com>
    Message-Id: <20200301215029.15196-9-nieklinnenbank@gmail.com>

--- hw/arm/cubieboard.c
+++ hw/arm/cubieboard.c
@@ -21,6 +21,7 @@
 #include "cpu.h"
 #include "hw/sysbus.h"
 #include "hw/boards.h"
+#include "hw/qdev-properties.h"
 #include "hw/arm/allwinner-a10.h"
 
 static struct arm_boot_info cubieboard_binfo = {
@@ -32,6 +33,10 @@ static void cubieboard_init(MachineState *machine)
 {
     AwA10State *a10 = AW_A10(object_new(TYPE_AW_A10));
     Error *err = NULL;
+    DriveInfo *di;
+    BlockBackend *blk;
+    BusState *bus;
+    DeviceState *carddev;
 
     object_property_set_int(OBJECT(&a10->emac), 1, "phy-addr", &err);
     if (err != NULL) {
@@ -56,6 +61,16 @@ static void cubieboard_init(MachineState *machine)
         error_reportf_err(err, "Couldn't realize Allwinner A10: ");
         exit(1);
     }
+
+    /* Retrieve SD bus */
+    di = drive_get_next(IF_SD);
+    blk = di ? blk_by_legacy_dinfo(di) : NULL;
+    bus = qdev_get_child_bus(DEVICE(a10), "sd-bus");
+
+    /* Plug in SD card */
+    carddev = qdev_create(bus, TYPE_SD_CARD);
+    qdev_prop_set_drive(carddev, "drive", blk, &error_fatal);
+    object_property_set_bool(OBJECT(carddev), true, "realized", &error_fatal);
 
     memory_region_add_subregion(get_system_memory(), AW_A10_SDRAM_BASE,
                                 machine->ram);
