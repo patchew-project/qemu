61ae355c6ac5b0fcee12ad1c9be10b3450f21caf
Janosch Frank <frankja@linux.ibm.com>
2020-03-04 06:42:21 -0500

    s390x: Add SIDA memory ops
    Protected guests save the instruction control blocks in the SIDA
    instead of QEMU/KVM directly accessing the guest's memory.
    
    Let's introduce new functions to access the SIDA.
    
    Signed-off-by: Janosch Frank <frankja@linux.ibm.com>
    Message-Id: <20200304114231.23493-9-frankja@linux.ibm.com>

--- linux-headers/linux/kvm.h
+++ linux-headers/linux/kvm.h
@@ -483,6 +483,8 @@ struct kvm_s390_mem_op {
 /* types for kvm_s390_mem_op->op */
 #define KVM_S390_MEMOP_LOGICAL_READ	0
 #define KVM_S390_MEMOP_LOGICAL_WRITE	1
+#define KVM_S390_MEMOP_SIDA_READ	2
+#define KVM_S390_MEMOP_SIDA_WRITE	3
 /* flags for kvm_s390_mem_op->flags */
 #define KVM_S390_MEMOP_F_CHECK_ONLY		(1ULL << 0)
 #define KVM_S390_MEMOP_F_INJECT_EXCEPTION	(1ULL << 1)
