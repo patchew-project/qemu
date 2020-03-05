e87aa99376d6d7ccbc5f1030db35dd9b2315e55d
Pan Nengyuan <pannengyuan@huawei.com>
2020-03-05 14:54:22 +0800

    hw/misc/mos6522: move timer_new from init() into realize() to avoid memleaks
    There are some memleaks when we call 'device_list_properties'. This patch move timer_new from init into realize to fix it.
    
    Reported-by: Euler Robot <euler.robot@huawei.com>
    Signed-off-by: Pan Nengyuan <pannengyuan@huawei.com>
    Message-Id: <20200305065422.12707-4-pannengyuan@huawei.com>

--- hw/misc/mos6522.c
+++ hw/misc/mos6522.c
@@ -485,6 +485,11 @@ static void mos6522_init(Object *obj)
     for (i = 0; i < ARRAY_SIZE(s->timers); i++) {
         s->timers[i].index = i;
     }
+}
+
+static void mos6522_realize(DeviceState *dev, Error **errp)
+{
+    MOS6522State *s = MOS6522(dev);
 
     s->timers[0].timer = timer_new_ns(QEMU_CLOCK_VIRTUAL, mos6522_timer1, s);
     s->timers[1].timer = timer_new_ns(QEMU_CLOCK_VIRTUAL, mos6522_timer2, s);
@@ -502,6 +507,7 @@ static void mos6522_class_init(ObjectClass *oc, void *data)
 
     dc->reset = mos6522_reset;
     dc->vmsd = &vmstate_mos6522;
+    dc->realize = mos6522_realize;
     device_class_set_props(dc, mos6522_properties);
     mdc->parent_reset = dc->reset;
     mdc->set_sr_int = mos6522_set_sr_int;
