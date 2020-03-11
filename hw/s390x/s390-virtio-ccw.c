ce15917572b2e28a32ca6ae2de956efc16bb2467
Janosch Frank <frankja@linux.ibm.com>
2020-03-11 09:21:40 -0400

    s390x: protvirt: Inhibit balloon when switching to protected mode
    Ballooning in protected VMs can only be done when the guest shares the
    pages it gives to the host. If pages are not shared, the integrity
    checks will fail once those pages have been altered and are given back
    to the guest.
    
    As we currently do not yet have a solution for this we will continue
    like this:
    
    1. We block ballooning now in QEMU (with this patch)
    
    2. Later we will provide a change to virtio that removes the blocker
    and adds VIRTIO_F_IOMMU_PLATFORM automatically by QEMU when doing the
    protvirt switch. This is ok as the guest balloon driver will reject to
    work with the IOMMU change
    
    3. Later we can fix the guest balloon driver to accept the IOMMU
    feature bit and correctly exercise sharing and unsharing of balloon
    pages
    
    Signed-off-by: Janosch Frank <frankja@linux.ibm.com>
    Reviewed-by: David Hildenbrand <david@redhat.com>
    Reviewed-by: Christian Borntraeger <borntraeger@de.ibm.com>
    Message-Id: <20200311132151.172389-5-frankja@linux.ibm.com>

--- hw/s390x/s390-virtio-ccw.c
+++ hw/s390x/s390-virtio-ccw.c
@@ -41,6 +41,7 @@
 #include "hw/qdev-properties.h"
 #include "hw/s390x/tod.h"
 #include "sysemu/sysemu.h"
+#include "sysemu/balloon.h"
 #include "hw/s390x/pv.h"
 #include "migration/blocker.h"
 
@@ -326,6 +327,7 @@ static void s390_machine_unprotect(S390CcwMachineState *ms)
     ms->pv = false;
     migrate_del_blocker(pv_mig_blocker);
     error_free_or_abort(&pv_mig_blocker);
+    qemu_balloon_inhibit(false);
 }
 
 static int s390_machine_protect(S390CcwMachineState *ms)
@@ -333,10 +335,12 @@ static int s390_machine_protect(S390CcwMachineState *ms)
     Error *local_err = NULL;
     int rc;
 
+    qemu_balloon_inhibit(true);
     error_setg(&pv_mig_blocker,
                "protected VMs are currently not migrateable.");
     rc = migrate_add_blocker(pv_mig_blocker, &local_err);
     if (local_err) {
+        qemu_balloon_inhibit(false);
         error_report_err(local_err);
         error_free_or_abort(&pv_mig_blocker);
         return rc;
@@ -345,6 +349,7 @@ static int s390_machine_protect(S390CcwMachineState *ms)
     /* Create SE VM */
     rc = s390_pv_vm_enable();
     if (rc) {
+        qemu_balloon_inhibit(false);
         error_report_err(local_err);
         migrate_del_blocker(pv_mig_blocker);
         error_free_or_abort(&pv_mig_blocker);
