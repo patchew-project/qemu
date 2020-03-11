e0afdcb00f7b2bf0bde4d7bdd616bee4620d4b91
Yuri Benditovich <yuri.benditovich@daynix.com>
2020-03-11 14:35:17 +0200

    virtio-net: add migration support for RSS and hast report
    Save and restore RSS/hash report configuration.
    
    Signed-off-by: Yuri Benditovich <yuri.benditovich@daynix.com>
    Message-Id: <20200311123518.4025-6-yuri.benditovich@daynix.com>

--- hw/net/virtio-net.c
+++ hw/net/virtio-net.c
@@ -2869,6 +2869,13 @@ static int virtio_net_post_load_device(void *opaque, int version_id)
         }
     }
 
+    if (n->rss_data.enabled) {
+        trace_virtio_net_rss_enable(n->rss_data.hash_types,
+                                    n->rss_data.indirections_len,
+                                    sizeof(n->rss_data.key));
+    } else {
+        trace_virtio_net_rss_disable();
+    }
     return 0;
 }
 
@@ -3094,6 +3101,8 @@ static const VMStateDescription vmstate_virtio_net_device = {
                          vmstate_virtio_net_tx_waiting),
         VMSTATE_UINT64_TEST(curr_guest_offloads, VirtIONet,
                             has_ctrl_guest_offloads),
+        VMSTATE_UINT8_ARRAY(rss_data_migration, VirtIONet,
+                            sizeof(VirtioNetRssData)),
         VMSTATE_END_OF_LIST()
    },
 };
