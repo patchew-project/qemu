eb6bfccbab5ff57f714420b14810f4e205611b40
Eric Auger <eric.auger@redhat.com>
2020-03-20 17:58:35 +0100

    hw/arm/smmuv3: Store the PASID table GPA in the translation config
    For VFIO integration we will need to pass the Context Descriptor (CD)
    table GPA to the host. The CD table is also referred to as the PASID
    table. Its GPA corresponds to the s1ctrptr field of the Stream Table
    Entry. So let's decode and store it in the configuration structure.
    
    Signed-off-by: Eric Auger <eric.auger@redhat.com>
    Message-Id: <20200320165840.30057-20-eric.auger@redhat.com>

--- hw/arm/smmuv3.c
+++ hw/arm/smmuv3.c
@@ -352,6 +352,7 @@ static int decode_ste(SMMUv3State *s, SMMUTransCfg *cfg,
                       "SMMUv3 S1 stalling fault model not allowed yet\n");
         goto bad_ste;
     }
+    cfg->s1ctxptr = STE_CTXPTR(ste);
     return 0;
 
 bad_ste:
