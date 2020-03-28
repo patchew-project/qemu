29f832f3b807e85308ecc591c7d4bccf3aefcf87
Chenyi Qiang <chenyi.qiang@intel.com>
2020-03-28 11:06:25 +0800

    target/i386: modify Icelake-Client and Icelake-Server CPU model number
    According to the Intel Icelake family list, Icelake-Client uses model
    number 125(0x7D) and Icelake-Server uses model number 106(0x6A).
    
    Signed-off-by: Chenyi Qiang <chenyi.qiang@intel.com>
    Message-Id: <20200328030625.16428-5-chenyi.qiang@intel.com>

--- target/i386/cpu.c
+++ target/i386/cpu.c
@@ -3328,7 +3328,7 @@ static X86CPUDefinition builtin_x86_defs[] = {
         .level = 0xd,
         .vendor = CPUID_VENDOR_INTEL,
         .family = 6,
-        .model = 126,
+        .model = 125,
         .stepping = 0,
         .features[FEAT_1_EDX] =
             CPUID_VME | CPUID_SSE2 | CPUID_SSE | CPUID_FXSR | CPUID_MMX |
@@ -3441,7 +3441,7 @@ static X86CPUDefinition builtin_x86_defs[] = {
         .level = 0xd,
         .vendor = CPUID_VENDOR_INTEL,
         .family = 6,
-        .model = 134,
+        .model = 106,
         .stepping = 0,
         .features[FEAT_1_EDX] =
             CPUID_VME | CPUID_SSE2 | CPUID_SSE | CPUID_FXSR | CPUID_MMX |
