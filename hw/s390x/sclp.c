01ac6f634fa294c697d71e401b561daf96fd8c8b
Christian Borntraeger <borntraeger@de.ibm.com>
2020-04-01 04:50:14 -0400

    vl/s390: fixup ram sizes for compat machines
    Older QEMU versions did fixup the ram size to match what can be reported
    via sclp. We need to mimic this behaviour for machine types 4.2 and
    older to not fail on inbound migration for memory sizes that do not fit.
    Old machines with proper aligned memory sizes are not affected.
    
    Alignment table:
     VM size (<=) | Alignment
    --------------------------
          1020M   |     1M
          2040M   |     2M
          4080M   |     4M
          8160M   |     8M
         16320M   |    16M
         32640M   |    32M
         65280M   |    64M
        130560M   |   128M
        261120M   |   256M
        522240M   |   512M
       1044480M   |     1G
       2088960M   |     2G
       4177920M   |     4G
       8355840M   |     8G
    
    Suggested action is to replace unaligned -m value with a suitable
    aligned one or to use a machine version >= 5.0 as future versions might
    remove the compatibility handling.
    
    For machine types >= 5.0 we can simply use an increment size of 1M and
    use the full range of increment number which allows for all possible
    memory sizes. The old limitation of having a maximum of 1020 increments
    was added for standby memory, which we no longer support. With that we
    can now support even weird memory sizes like 10001234 MB.
    
    Fixes: 3a12fc61af5c ("390x/s390-virtio-ccw: use memdev for RAM")
    Reported-by: Lukáš Doktor <ldoktor@redhat.com>
    Cc: Igor Mammedov <imammedo@redhat.com>
    Cc: Dr. David Alan Gilbert <dgilbert@redhat.com>
    Signed-off-by: David Hildenbrand <david@redhat.com>
    Signed-off-by: Christian Borntraeger <borntraeger@de.ibm.com>
    Message-Id: <20200401085014.100125-1-borntraeger@de.ibm.com>

--- hw/s390x/sclp.c
+++ hw/s390x/sclp.c
@@ -312,7 +312,7 @@ static void sclp_realize(DeviceState *dev, Error **errp)
      */
     qdev_set_parent_bus(DEVICE(sclp->event_facility), sysbus_get_default());
 
-    ret = s390_set_memory_limit(machine->maxram_size, &hw_limit);
+    ret = s390_set_memory_limit(machine->ram_size, &hw_limit);
     if (ret == -E2BIG) {
         error_setg(&err, "host supports a maximum of %" PRIu64 " GB",
                    hw_limit / GiB);
@@ -327,27 +327,20 @@ out:
 static void sclp_memory_init(SCLPDevice *sclp)
 {
     MachineState *machine = MACHINE(qdev_get_machine());
+    MachineClass *machine_class = MACHINE_GET_CLASS(qdev_get_machine());
     ram_addr_t initial_mem = machine->ram_size;
     int increment_size = 20;
 
     /* The storage increment size is a multiple of 1M and is a power of 2.
-     * The number of storage increments must be MAX_STORAGE_INCREMENTS or fewer.
+     * For some machine types, the number of storage increments must be
+     * MAX_STORAGE_INCREMENTS or fewer.
      * The variable 'increment_size' is an exponent of 2 that can be
      * used to calculate the size (in bytes) of an increment. */
-    while ((initial_mem >> increment_size) > MAX_STORAGE_INCREMENTS) {
+    while (machine_class->fixup_ram_size != NULL &&
+           (initial_mem >> increment_size) > MAX_STORAGE_INCREMENTS) {
         increment_size++;
     }
     sclp->increment_size = increment_size;
-
-    /* The core memory area needs to be aligned with the increment size.
-     * In effect, this can cause the user-specified memory size to be rounded
-     * down to align with the nearest increment boundary. */
-    initial_mem = initial_mem >> increment_size << increment_size;
-
-    machine->ram_size = initial_mem;
-    machine->maxram_size = initial_mem;
-    /* let's propagate the changed ram size into the global variable. */
-    ram_size = initial_mem;
 }
 
 static void sclp_init(Object *obj)
