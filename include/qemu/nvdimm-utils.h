335c85e833ef28bc0e97b3427bf4cc64ad782f37
Igor Mammedov <imammedo@redhat.com>
2020-04-03 10:41:49 -0400

    nvdimm-utils: clean up headers and add license comment
    Fixes: 3f350f6bb36233be50fc2bc18dc78b6a948a5dbe
    Reported-by: Peter Maydell <peter.maydell@linaro.org>
    Signed-off-by: Igor Mammedov <imammedo@redhat.com>
    Message-Id: <20200403144149.14068-1-imammedo@redhat.com>

--- include/qemu/nvdimm-utils.h
+++ include/qemu/nvdimm-utils.h
@@ -1,7 +1,26 @@
 #ifndef NVDIMM_UTILS_H
 #define NVDIMM_UTILS_H
-
-#include "qemu/osdep.h"
+/*
+ * NVDIMM utilities
+ *
+ * Copyright(C) 2015 Intel Corporation.
+ *
+ * Author:
+ *  Xiao Guangrong <guangrong.xiao@linux.intel.com>
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2 of the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library; if not, see <http://www.gnu.org/licenses/>
+ */
 
 GSList *nvdimm_get_device_list(void);
 #endif
