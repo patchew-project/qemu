f79ca2e47cca939f2036d1375d229d5b591a29c5
Jon Doron <arilou@gmail.com>
2020-04-03 17:23:08 +0300

    i386: Hyper-V VMBus ACPI DSDT entry
    Guest OS uses ACPI to discover vmbus presence.  Add a corresponding
    entry to DSDT in case vmbus has been enabled.
    
    Experimentally Windows guests were found to require this entry to
    include two IRQ resources, so this patch adds two semi-arbitrarily
    chosen ones (7 and 13).  This results, in particular, in parallel port
    conflicting with vmbus.
    
    TODO: discover and use spare IRQs to avoid conflicts.
    
    Signed-off-by: Evgeny Yakovlev <eyakovlev@virtuozzo.com>
    Signed-off-by: Roman Kagan <rkagan@virtuozzo.com>
    Signed-off-by: Jon Doron <arilou@gmail.com>
    Message-Id: <20200403142308.82990-6-arilou@gmail.com>

--- hw/i386/acpi-build.c
+++ hw/i386/acpi-build.c
@@ -43,6 +43,7 @@
 #include "hw/acpi/tpm.h"
 #include "hw/acpi/vmgenid.h"
 #include "hw/boards.h"
+#include "hw/vmbus/vmbus.h"
 #include "sysemu/tpm_backend.h"
 #include "hw/rtc/mc146818rtc_regs.h"
 #include "migration/vmstate.h"
@@ -1270,6 +1271,43 @@ static Aml *build_com_device_aml(uint8_t uid)
     return dev;
 }
 
+static Aml *build_vmbus_device_aml(void)
+{
+    Aml *dev;
+    Aml *method;
+    Aml *crs;
+
+    dev = aml_device("VMBS");
+    aml_append(dev, aml_name_decl("STA", aml_int(0xF)));
+    aml_append(dev, aml_name_decl("_HID", aml_string("VMBus")));
+    aml_append(dev, aml_name_decl("_UID", aml_int(0x0)));
+    aml_append(dev, aml_name_decl("_DDN", aml_string("VMBUS")));
+
+    method = aml_method("_DIS", 0, AML_NOTSERIALIZED);
+    aml_append(method, aml_store(aml_and(aml_name("STA"), aml_int(0xD), NULL),
+                                     aml_name("STA")));
+    aml_append(dev, method);
+
+    method = aml_method("_PS0", 0, AML_NOTSERIALIZED);
+    aml_append(method, aml_store(aml_or(aml_name("STA"), aml_int(0xF), NULL),
+                                     aml_name("STA")));
+    aml_append(dev, method);
+
+    method = aml_method("_STA", 0, AML_NOTSERIALIZED);
+    aml_append(method, aml_store(aml_name("STA"), aml_local(0)));
+    aml_append(method, aml_return(aml_local(0)));
+    aml_append(dev, method);
+
+    aml_append(dev, aml_name_decl("_PS3", aml_int(0x0)));
+
+    crs = aml_resource_template();
+    aml_append(crs, aml_irq_no_flags(7));
+    aml_append(crs, aml_irq_no_flags(13));
+    aml_append(dev, aml_name_decl("_CRS", crs));
+
+    return dev;
+}
+
 static void build_isa_devices_aml(Aml *table)
 {
     ISADevice *fdc = pc_find_fdc0();
@@ -1294,6 +1332,10 @@ static void build_isa_devices_aml(Aml *table)
         error_report("No ISA bus, unable to define IPMI ACPI data");
     } else {
         build_acpi_ipmi_devices(scope, BUS(obj), "\\_SB.PCI0.ISA");
+    }
+
+    if (vmbus_exists()) {
+        aml_append(scope, build_vmbus_device_aml());
     }
 
     aml_append(table, scope);
