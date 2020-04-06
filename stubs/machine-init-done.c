30247e1547e1c0f0140f86f09a1f1444a655bff6
Elena Ufimtseva <elena.ufimtseva@oracle.com>
2020-04-06 02:40:52 -0700

    multi-process: Refactor machine_init and exit notifiers
    Relocate machine_int and exit notifiers into common code
    
    Signed-off-by: Elena Ufimtseva <elena.ufimtseva@oracle.com>
    Signed-off-by: John G Johnson <john.g.johnson@oracle.com>
    Signed-off-by: Jagannathan Raman <jag.raman@oracle.com>
    Message-Id: <a390c0cab4aca444ed4e2bde3bdb2a8e39fbec0d.1586165555.git.elena.ufimtseva@oracle.com>

--- stubs/machine-init-done.c
+++ stubs/machine-init-done.c
@@ -3,6 +3,18 @@
 
 bool machine_init_done = true;
 
+static NotifierList machine_init_done_notifiers =
+    NOTIFIER_LIST_INITIALIZER(machine_init_done_notifiers);
+
 void qemu_add_machine_init_done_notifier(Notifier *notify)
 {
+}
+
+void qemu_remove_machine_init_done_notifier(Notifier *notify)
+{
+}
+
+void qemu_run_machine_init_done_notifiers(void)
+{
+    notifier_list_notify(&machine_init_done_notifiers, NULL);
 }
