7e45ded4295725f6459ec78c8ae1a3816e421958
Philippe Mathieu-Daudé <philmd@redhat.com>
2020-04-14 18:38:52 +0200

    gdbstub: Introduce gdb_get_float32() to get 32-bit float registers
    Since we now use a GByteArray, we can not use stfl_p() directly.
    Introduce the gdb_get_float32() helper to load a float32 register.
    
    Fixes: a010bdbe719 ("extend GByteArray to read register helpers")
    Signed-off-by: Philippe Mathieu-Daudé <philmd@redhat.com>
    Message-Id: <20200414163853.12164-2-philmd@redhat.com>
    Based-on: <20200414102427.7459-1-philmd@redhat.com>

--- include/exec/gdbstub.h
+++ include/exec/gdbstub.h
@@ -125,6 +125,16 @@ static inline int gdb_get_reg128(GByteArray *buf, uint64_t val_hi,
     return 16;
 }
 
+static inline int gdb_get_float32(GByteArray *array, float32 val)
+{
+    uint8_t buf[sizeof(CPU_FloatU)];
+
+    stfl_p(buf, val);
+    g_byte_array_append(array, buf, sizeof(buf));
+
+    return sizeof(buf);
+}
+
 static inline int gdb_get_zeroes(GByteArray *array, size_t len)
 {
     for (size_t i = 0; i < len; i++) {
