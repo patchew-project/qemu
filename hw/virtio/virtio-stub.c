e658c99cb0bf58fb5a42186be8d83444cc244097
Laurent Vivier <lvivier@redhat.com>
2020-04-20 12:41:43 +0200

    qmp: add QMP command virtio-queue-status
    This new command shows internal status of a VirtQueue.
    (vrings and indexes).
    
    Signed-off-by: Laurent Vivier <lvivier@redhat.com>
    Message-Id: <20200420104145.205297-5-lvivier@redhat.com>

--- hw/virtio/virtio-stub.c
+++ hw/virtio/virtio-stub.c
@@ -17,3 +17,9 @@ VirtioStatus *qmp_virtio_status(const char* path, Error **errp)
 {
     return qmp_virtio_unsupported(errp);
 }
+
+VirtQueueStatus *qmp_virtio_queue_status(const char *path, uint16_t queue,
+                                         Error **errp)
+{
+    return qmp_virtio_unsupported(errp);
+}
