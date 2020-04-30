b1c4913d34d36b0914763b84498761dd3badd018
Huacai Chen <zltjiangshi@gmail.com>
2020-04-30 18:22:30 +0800

    hw/mips: Implement the kvm_type() hook in MachineClass
    MIPS has two types of KVM: TE & VZ, and TE is the default type. Now we
    can't create a VZ guest in QEMU because it lacks the kvm_type() hook in
    MachineClass. Besides, libvirt uses a null-machine to detect the kvm
    capability, so by default it will return "KVM not supported" on a VZ
    platform. Thus, null-machine also need the kvm_type() hook.
    
    Signed-off-by: Huacai Chen <chenhc@lemote.com>
    Co-developed-by: Jiaxun Yang <jiaxun.yang@flygoat.com>
    Message-Id: <1588242155-23924-3-git-send-email-chenhc@lemote.com>

---
+++ hw/mips/common.c
@@ -0,0 +1,29 @@
+/*
+ * Common MIPS routines
+ *
+ * Copyright (c) 2020 Huacai Chen (chenhc@lemote.com)
+ * This code is licensed under the GNU GPL v2.
+ */
+
+#include <linux/kvm.h>
+#include "qemu/osdep.h"
+#include "qemu-common.h"
+#include "hw/boards.h"
+#include "hw/mips/mips.h"
+#include "sysemu/kvm_int.h"
+
+int mips_kvm_type(MachineState *machine, const char *vm_type)
+{
+    int r;
+    KVMState *s = KVM_STATE(machine->accelerator);;
+
+    r = kvm_check_extension(s, KVM_CAP_MIPS_VZ);
+    if (r > 0)
+        return KVM_VM_MIPS_VZ;
+
+    r = kvm_check_extension(s, KVM_CAP_MIPS_TE);
+    if (r > 0)
+        return KVM_VM_MIPS_TE;
+
+    return -1;
+}
