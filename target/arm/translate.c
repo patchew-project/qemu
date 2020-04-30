fe5597a66162851cf0e84dd6b8b014d804a53867
Peter Maydell <peter.maydell@linaro.org>
2020-04-30 19:09:56 +0100

    target/arm: Convert Neon VPADD 3-reg-same insns to decodetree
    Convert the Neon integer VPADD 3-reg-same insns to decodetree.  These
    are 'pairwise' operations.  (Note that VQRDMLAH, which shares the
    same primary opcode but has U=1, has already been converted.)
    
    Signed-off-by: Peter Maydell <peter.maydell@linaro.org>
    Message-Id: <20200430181003.21682-30-peter.maydell@linaro.org>

--- target/arm/translate.c
+++ target/arm/translate.c
@@ -4749,13 +4749,6 @@ static int disas_neon_data_insn(DisasContext *s, uint32_t insn)
             return 1;
         }
         switch (op) {
-        case NEON_3R_VPADD_VQRDMLAH:
-            if (!u) {
-                break;  /* VPADD */
-            }
-            /* VQRDMLAH : handled by decodetree */
-            return 1;
-
         case NEON_3R_VFM_VQRDMLSH:
             if (!u) {
                 /* VFM, VFMS */
@@ -4790,6 +4783,7 @@ static int disas_neon_data_insn(DisasContext *s, uint32_t insn)
         case NEON_3R_VABA:
         case NEON_3R_VPMAX:
         case NEON_3R_VPMIN:
+        case NEON_3R_VPADD_VQRDMLAH:
             /* Already handled by decodetree */
             return 1;
         }
@@ -4800,9 +4794,6 @@ static int disas_neon_data_insn(DisasContext *s, uint32_t insn)
         }
         pairwise = 0;
         switch (op) {
-        case NEON_3R_VPADD_VQRDMLAH:
-            pairwise = 1;
-            break;
         case NEON_3R_FLOAT_ARITH:
             pairwise = (u && size < 2); /* if VPADD (float) */
             break;
@@ -4878,14 +4869,6 @@ static int disas_neon_data_insn(DisasContext *s, uint32_t insn)
                     break;
                 default: abort();
                 }
-            }
-            break;
-        case NEON_3R_VPADD_VQRDMLAH:
-            switch (size) {
-            case 0: gen_helper_neon_padd_u8(tmp, tmp, tmp2); break;
-            case 1: gen_helper_neon_padd_u16(tmp, tmp, tmp2); break;
-            case 2: tcg_gen_add_i32(tmp, tmp, tmp2); break;
-            default: abort();
             }
             break;
         case NEON_3R_FLOAT_ARITH: /* Floating point arithmetic. */
