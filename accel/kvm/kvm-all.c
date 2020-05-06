bec48cf76174cea059780ba59cafe7d6b44f4f87
David Hildenbrand <david@redhat.com>
2020-05-06 11:49:34 +0200

    accel/kvm: Convert to ram_block_discard_set_broken()
    Discarding memory does not work as expected. At the time this is called,
    we cannot have anyone active that relies on discards to work properly.
    
    Cc: Paolo Bonzini <pbonzini@redhat.com>
    Signed-off-by: David Hildenbrand <david@redhat.com>
    Message-Id: <20200506094948.76388-4-david@redhat.com>

--- accel/kvm/kvm-all.c
+++ accel/kvm/kvm-all.c
@@ -40,7 +40,6 @@
 #include "trace.h"
 #include "hw/irq.h"
 #include "sysemu/sev.h"
-#include "sysemu/balloon.h"
 #include "qapi/visitor.h"
 #include "qapi/qapi-types-common.h"
 #include "qapi/qapi-visit-common.h"
@@ -2143,7 +2142,7 @@ static int kvm_init(MachineState *ms)
 
     s->sync_mmu = !!kvm_vm_check_extension(kvm_state, KVM_CAP_SYNC_MMU);
     if (!s->sync_mmu) {
-        qemu_balloon_inhibit(true);
+        g_assert(ram_block_discard_set_broken(true));
     }
 
     return 0;
