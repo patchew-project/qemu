fe66187a2052aa56c0348a87f3c7738c91ae78c0
Eyal Moscovici <eyal.moscovici@oracle.com>
2020-05-07 00:34:59 +0300

    qemu-img: Add --start-offset and --max-length to map
    The mapping operation of large disks especially ones stored over a
    long chain of QCOW2 files can take a long time to finish.
    Additionally when mapping fails there was no way recover by
    restarting the mapping from the failed location.
    
    The new options, --start-offset and --max-length allows the user to
    divide these type of map operations into shorter independent tasks.
    
    Reviewed-by: Eric Blake <eblake@redhat.com>
    Acked-by: Mark Kanda <mark.kanda@oracle.com>
    Co-developed-by: Yoav Elnekave <yoav.elnekave@oracle.com>
    Signed-off-by: Yoav Elnekave <yoav.elnekave@oracle.com>
    Signed-off-by: Eyal Moscovici <eyal.moscovici@oracle.com>
    Message-Id: <20200506213459.44743-6-eyal.moscovici@oracle.com>

--- qemu-img.c
+++ qemu-img.c
@@ -3003,6 +3003,8 @@ static int img_map(int argc, char **argv)
     int ret = 0;
     bool image_opts = false;
     bool force_share = false;
+    int64_t start_offset = 0;
+    int64_t max_length = -1;
 
     fmt = NULL;
     output = NULL;
@@ -3015,9 +3017,11 @@ static int img_map(int argc, char **argv)
             {"object", required_argument, 0, OPTION_OBJECT},
             {"image-opts", no_argument, 0, OPTION_IMAGE_OPTS},
             {"force-share", no_argument, 0, 'U'},
+            {"start-offset", required_argument, 0, 's'},
+            {"max-length", required_argument, 0, 'l'},
             {0, 0, 0, 0}
         };
-        c = getopt_long(argc, argv, ":f:hU",
+        c = getopt_long(argc, argv, ":f:s:l:hU",
                         long_options, &option_index);
         if (c == -1) {
             break;
@@ -3040,6 +3044,18 @@ static int img_map(int argc, char **argv)
             break;
         case OPTION_OUTPUT:
             output = optarg;
+            break;
+        case 's':
+            start_offset = cvtnum("start offset", optarg);
+            if (start_offset < 0) {
+                return 1;
+            }
+            break;
+        case 'l':
+            max_length = cvtnum("max length", optarg);
+            if (max_length < 0) {
+                return 1;
+            }
             break;
         case OPTION_OBJECT: {
             QemuOpts *opts;
@@ -3091,7 +3107,11 @@ static int img_map(int argc, char **argv)
         error_report("Failed to get size for '%s'", filename);
         return 1;
     }
+    if (max_length != -1) {
+        length = MIN(start_offset + max_length, length);
+    }
 
+    curr.start = start_offset;
     while (curr.start + curr.length < length) {
         int64_t offset = curr.start + curr.length;
         int64_t n;
