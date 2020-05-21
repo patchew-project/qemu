a6f6377339f4564ad1b4de620020d964e6b030d6
Emilio G. Cota <cota@braap.org>
2020-05-21 12:39:58 -0400

    cpu: convert to interrupt_request
    This finishes the conversion to interrupt_request.
    
    Reviewed-by: Alex Bennée <alex.bennee@linaro.org>
    Reviewed-by: Richard Henderson <richard.henderson@linaro.org>
    Signed-off-by: Emilio G. Cota <cota@braap.org>
    Signed-off-by: Robert Foley <robert.foley@linaro.org>
    Message-Id: <20200521164011.638-62-robert.foley@linaro.org>

--- hw/core/cpu.c
+++ hw/core/cpu.c
@@ -271,7 +271,7 @@ static void cpu_common_reset(DeviceState *dev)
         log_cpu_state(cpu, cc->reset_dump_flags);
     }
 
-    cpu->interrupt_request = 0;
+    cpu_interrupt_request_set(cpu, 0);
     cpu_halted_set(cpu, 0);
     cpu->mem_io_pc = 0;
     cpu->icount_extra = 0;
@@ -411,7 +411,7 @@ static vaddr cpu_adjust_watchpoint_address(CPUState *cpu, vaddr addr, int len)
 
 static void generic_handle_interrupt(CPUState *cpu, int mask)
 {
-    cpu->interrupt_request |= mask;
+    cpu_interrupt_request_or(cpu, mask);
 
     if (!qemu_cpu_is_self(cpu)) {
         qemu_cpu_kick(cpu);
