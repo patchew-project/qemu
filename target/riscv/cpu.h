40f71aac243ee06b40ebf1b72eae2ce4ba576b77
LIU Zhiwei <zhiwei_liu@c-sky.com>
2020-05-21 17:43:13 +0800

    target/riscv: implementation-defined constant parameters
    vlen is the vector register length in bits.
    elen is the max element size in bits.
    vext_spec is the vector specification version, default value is v0.7.1.
    
    Signed-off-by: LIU Zhiwei <zhiwei_liu@c-sky.com>
    Reviewed-by: Alistair Francis <alistair.francis@wdc.com>
    Reviewed-by: Richard Henderson <richard.henderson@linaro.org>
    Message-Id: <20200521094413.10425-3-zhiwei_liu@c-sky.com>

--- target/riscv/cpu.h
+++ target/riscv/cpu.h
@@ -85,6 +85,8 @@ enum {
 #define PRIV_VERSION_1_10_0 0x00011000
 #define PRIV_VERSION_1_11_0 0x00011100
 
+#define VEXT_VERSION_0_07_1 0x00000701
+
 #define TRANSLATE_PMP_FAIL 2
 #define TRANSLATE_FAIL 1
 #define TRANSLATE_SUCCESS 0
@@ -120,6 +122,7 @@ struct CPURISCVState {
     target_ulong guest_phys_fault_addr;
 
     target_ulong priv_ver;
+    target_ulong vext_ver;
     target_ulong misa;
     target_ulong misa_mask;
 
@@ -282,6 +285,8 @@ typedef struct RISCVCPU {
 
         char *priv_spec;
         char *user_spec;
+        uint16_t vlen;
+        uint16_t elen;
         bool mmu;
         bool pmp;
     } cfg;
