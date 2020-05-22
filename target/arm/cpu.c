10048038372afcdd333e904d48a1e79d47e261a9
Robert Foley <robert.foley@linaro.org>
2020-05-22 12:07:54 -0400

    target/arm: Fix tsan warning in cpu.c
    For example:
    WARNING: ThreadSanitizer: data race (pid=11134)
      Atomic write of size 4 at 0x7bbc0000e0ac by main thread (mutexes: write M875):
        #0 __tsan_atomic32_store <null> (qemu-system-aarch64+0x394d84)
        #1 cpu_reset_interrupt hw/core/cpu.c:107:5 (qemu-system-aarch64+0x842f90)
        #2 arm_cpu_set_irq target/arm/cpu.c (qemu-system-aarch64+0x615a55)
    
      Previous read of size 4 at 0x7bbc0000e0ac by thread T7:
        #0 arm_cpu_has_work target/arm/cpu.c:78:16 (qemu-system-aarch64+0x6178ba)
        #1 cpu_has_work include/hw/core/cpu.h:700:12 (qemu-system-aarch64+0x68be2e)
    
    Cc: Peter Maydell <peter.maydell@linaro.org>
    Cc: Richard Henderson <richard.henderson@linaro.org>
    Signed-off-by: Robert Foley <robert.foley@linaro.org>
    Message-Id: <20200522160755.886-19-robert.foley@linaro.org>

--- target/arm/cpu.c
+++ target/arm/cpu.c
@@ -75,7 +75,7 @@ static bool arm_cpu_has_work(CPUState *cs)
     ARMCPU *cpu = ARM_CPU(cs);
 
     return (cpu->power_state != PSCI_OFF)
-        && cs->interrupt_request &
+        && atomic_read(&cs->interrupt_request) &
         (CPU_INTERRUPT_FIQ | CPU_INTERRUPT_HARD
          | CPU_INTERRUPT_VFIQ | CPU_INTERRUPT_VIRQ
          | CPU_INTERRUPT_EXITTB);
