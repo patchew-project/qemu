bdc55dc6de36d90855fa294d032ad02592da2690
Ying Fang <fangying1@huawei.com>
2020-06-03 10:02:08 +0800

    target/arm/cpu: adjust virtual time for arm cpu
    Virtual time adjustment was implemented for virt-5.0 machine type,
    but the cpu property was enabled only for host-passthrough and
    max cpu model. Let's add it for arm cpu which has the gernic
    timer feature enabled.
    
    Signed-off-by: Ying Fang <fangying1@huawei.com>
    
    Message-Id: <20200603020208.2089-1-fangying1@huawei.com>
    Reviewed-by: Richard Henderson <richard.henderson@linaro.org>

--- target/arm/cpu.c
+++ target/arm/cpu.c
@@ -1244,6 +1244,7 @@ void arm_cpu_post_init(Object *obj)
 
     if (arm_feature(&cpu->env, ARM_FEATURE_GENERIC_TIMER)) {
         qdev_property_add_static(DEVICE(cpu), &arm_cpu_gt_cntfrq_property);
+        kvm_arm_add_vcpu_properties(obj);
     }
 }
 
@@ -2029,7 +2030,6 @@ static void arm_max_initfn(Object *obj)
 
     if (kvm_enabled()) {
         kvm_arm_set_cpu_features_from_host(cpu);
-        kvm_arm_add_vcpu_properties(obj);
     } else {
         cortex_a15_initfn(obj);
 
@@ -2183,7 +2183,6 @@ static void arm_host_initfn(Object *obj)
     if (arm_feature(&cpu->env, ARM_FEATURE_AARCH64)) {
         aarch64_add_sve_properties(obj);
     }
-    kvm_arm_add_vcpu_properties(obj);
     arm_cpu_post_init(obj);
 }
 
