7cbcacfa513d51d5b800a809a8ec7837338fa85d
Eric Auger <eric.auger@redhat.com>
2020-06-11 18:14:56 +0200

    hw/arm/smmuv3: Store the starting level in SMMUTransTableInfo
    Compute the starting level on CD decoding and store it
    into SMMUTransTableInfo. We will need this information
    on IOTLB lookup so let's avoid to recompute it each time.
    
    Signed-off-by: Eric Auger <eric.auger@redhat.com>
    Message-Id: <20200611161500.23580-6-eric.auger@redhat.com>

--- include/hw/arm/smmu-common.h
+++ include/hw/arm/smmu-common.h
@@ -50,6 +50,7 @@ typedef struct SMMUTransTableInfo {
     uint64_t ttb;              /* TT base address */
     uint8_t tsz;               /* input range, ie. 2^(64 -tsz)*/
     uint8_t granule_sz;        /* granule page shift */
+    uint8_t starting_level;    /* starting level */
 } SMMUTransTableInfo;
 
 struct SMMUTLBEntry {
