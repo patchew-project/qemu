dc2bf5d5913ba5c69b263244df5108ee3d6e4d71
Mark Cave-Ayland <mark.cave-ayland@ilande.co.uk>
2020-06-14 15:28:23 +0100

    pmu: honour autopoll_rate_ms when rearming the ADB autopoll timer
    Don't use a fixed value but instead use the default value from the ADB bus
    state.
    
    Signed-off-by: Mark Cave-Ayland <mark.cave-ayland@ilande.co.uk>
    Message-Id: <20200614142840.10245-6-mark.cave-ayland@ilande.co.uk>
    Tested-by: Finn Thain <fthain@telegraphics.com.au>

--- hw/misc/macio/pmu.c
+++ hw/misc/macio/pmu.c
@@ -104,7 +104,7 @@ static void pmu_adb_poll(void *opaque)
     }
 
     timer_mod(s->adb_poll_timer,
-              qemu_clock_get_ms(QEMU_CLOCK_VIRTUAL) + 30);
+              qemu_clock_get_ms(QEMU_CLOCK_VIRTUAL) + s->autopoll_rate_ms);
 }
 
 static void pmu_one_sec_timer(void *opaque)
@@ -180,7 +180,7 @@ static void pmu_cmd_set_adb_autopoll(PMUState *s, uint16_t mask)
     s->adb_poll_mask = mask;
     if (mask) {
         timer_mod(s->adb_poll_timer,
-                  qemu_clock_get_ms(QEMU_CLOCK_VIRTUAL) + 30);
+                  qemu_clock_get_ms(QEMU_CLOCK_VIRTUAL) + s->autopoll_rate_ms);
     } else {
         timer_del(s->adb_poll_timer);
     }
