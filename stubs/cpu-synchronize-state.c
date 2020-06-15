f7ed6752da12a018615572e46a6dcc341d04b6c6
Claudio Fontana <cfontana@suse.de>
2020-06-15 20:03:46 +0200

    cpus: extract out accel-specific code to each accel
    each accelerator registers a new "CpusAccel" interface
    implementation on initialization, providing functions for
    starting a vcpu, kicking a vcpu, and sychronizing state.
    
    This way the code in cpus.c is now all general softmmu code,
    nothing accelerator-specific anymore.
    
    There is still some ifdeffery for WIN32 though.
    
    Signed-off-by: Claudio Fontana <cfontana@suse.de>
    Message-Id: <20200615180346.3992-5-cfontana@suse.de>

---
+++ stubs/cpu-synchronize-state.c
@@ -0,0 +1,15 @@
+#include "qemu/osdep.h"
+#include "sysemu/hw_accel.h"
+
+void cpu_synchronize_state(CPUState *cpu)
+{
+}
+void cpu_synchronize_post_reset(CPUState *cpu)
+{
+}
+void cpu_synchronize_post_init(CPUState *cpu)
+{
+}
+void cpu_synchronize_pre_loadvm(CPUState *cpu)
+{
+}
