f7879c328b698f6ea6e7d679c898ab4350d91c5a
Richard Henderson <richard.henderson@linaro.org>
2020-06-17 21:26:03 -0700

    target/arm: Implement SVE2 XAR
    In addition, use the same vector generator interface for AdvSIMD.
    This fixes a bug in which the AdvSIMD insn failed to clear the
    high bits of the SVE register.
    
    Signed-off-by: Richard Henderson <richard.henderson@linaro.org>
    Message-Id: <20200618042644.1685561-60-richard.henderson@linaro.org>

--- target/arm/helper.h
+++ target/arm/helper.h
@@ -783,6 +783,8 @@ DEF_HELPER_FLAGS_4(gvec_uaba_h, TCG_CALL_NO_RWG, void, ptr, ptr, ptr, i32)
 DEF_HELPER_FLAGS_4(gvec_uaba_s, TCG_CALL_NO_RWG, void, ptr, ptr, ptr, i32)
 DEF_HELPER_FLAGS_4(gvec_uaba_d, TCG_CALL_NO_RWG, void, ptr, ptr, ptr, i32)
 
+DEF_HELPER_FLAGS_4(gvec_xar_d, TCG_CALL_NO_RWG, void, ptr, ptr, ptr, i32)
+
 #ifdef TARGET_AARCH64
 #include "helper-a64.h"
 #include "helper-sve.h"
