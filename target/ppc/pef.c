ba56703fbe931d372399facca7f834baf2885bdf
David Gibson <david@gibson.dropbear.id.au>
2020-06-19 12:06:00 +1000

    spapr: Add PEF based host trust limitation
    Some upcoming POWER machines have a system called PEF (Protected
    Execution Facility) which uses a small ultravisor to allow guests to
    run in a way that they can't be eavesdropped by the hypervisor.  The
    effect is roughly similar to AMD SEV, although the mechanisms are
    quite different.
    
    Most of the work of this is done between the guest, KVM and the
    ultravisor, with little need for involvement by qemu.  However qemu
    does need to tell KVM to allow secure VMs.
    
    Because the availability of secure mode is a guest visible difference
    which depends on having the right hardware and firmware, we don't
    enable this by default.  In order to run a secure guest you need to
    create a "pef-guest" object and set the host-trust-limitation machine
    property to point to it.
    
    Note that this just *allows* secure guests, the architecture of PEF is
    such that the guest still needs to talk to the ultravisor to enter
    secure mode.  Qemu has no directly way of knowing if the guest is in
    secure mode, and certainly can't know until well after machine
    creation time.
    
    Signed-off-by: David Gibson <david@gibson.dropbear.id.au>
    Acked-by: Ram Pai <linuxram@us.ibm.com>
    Message-Id: <20200619020602.118306-8-david@gibson.dropbear.id.au>

---
+++ target/ppc/pef.c
@@ -0,0 +1,83 @@
+/*
+ * PEF (Protected Execution Facility) for POWER support
+ *
+ * Copyright David Gibson, Redhat Inc. 2020
+ *
+ * This work is licensed under the terms of the GNU GPL, version 2 or later.
+ * See the COPYING file in the top-level directory.
+ *
+ */
+
+#include "qemu/osdep.h"
+
+#include "qapi/error.h"
+#include "qom/object_interfaces.h"
+#include "sysemu/kvm.h"
+#include "migration/blocker.h"
+#include "exec/host-trust-limitation.h"
+
+#define TYPE_PEF_GUEST "pef-guest"
+#define PEF_GUEST(obj)                                  \
+    OBJECT_CHECK(PefGuestState, (obj), TYPE_PEF_GUEST)
+
+typedef struct PefGuestState PefGuestState;
+
+/**
+ * PefGuestState:
+ *
+ * The PefGuestState object is used for creating and managing a PEF
+ * guest.
+ *
+ * # $QEMU \
+ *         -object pef-guest,id=pef0 \
+ *         -machine ...,host-trust-limitation=pef0
+ */
+struct PefGuestState {
+    Object parent_obj;
+};
+
+static int pef_kvm_init(HostTrustLimitation *gmpo, Error **errp)
+{
+    if (!kvm_check_extension(kvm_state, KVM_CAP_PPC_SECURE_GUEST)) {
+        error_setg(errp,
+                   "KVM implementation does not support Secure VMs (is an ultravisor running?)");
+        return -1;
+    } else {
+        int ret = kvm_vm_enable_cap(kvm_state, KVM_CAP_PPC_SECURE_GUEST, 0, 1);
+
+        if (ret < 0) {
+            error_setg(errp,
+                       "Error enabling PEF with KVM");
+            return -1;
+        }
+    }
+
+    return 0;
+}
+
+static void pef_guest_class_init(ObjectClass *oc, void *data)
+{
+    HostTrustLimitationClass *gmpc = HOST_TRUST_LIMITATION_CLASS(oc);
+
+    gmpc->kvm_init = pef_kvm_init;
+}
+
+static const TypeInfo pef_guest_info = {
+    .parent = TYPE_OBJECT,
+    .name = TYPE_PEF_GUEST,
+    .instance_size = sizeof(PefGuestState),
+    .class_init = pef_guest_class_init,
+    .interfaces = (InterfaceInfo[]) {
+        { TYPE_HOST_TRUST_LIMITATION },
+        { TYPE_USER_CREATABLE },
+        { }
+    }
+};
+
+static void
+pef_register_types(void)
+{
+    type_register_static(&pef_guest_info);
+}
+
+type_init(pef_register_types);
