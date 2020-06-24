10e533e9c9c6db93712e5f4c70e84c4ecdfcfb10
Szymon Lukasz <noh4hss@gmail.com>
2020-06-24 13:26:37 +0200

    chardev: add support for notifying about terminal resizes
    Add a new chardev event, CHR_EVENT_RESIZE, which a backend should
    trigger if detects the size of the connected terminal changed.
    
    Signed-off-by: Szymon Lukasz <noh4hss@gmail.com>
    Message-Id: <20200624112640.82673-4-noh4hss@gmail.com>

--- chardev/char.c
+++ chardev/char.c
@@ -74,6 +74,7 @@ void qemu_chr_be_event(Chardev *s, QEMUChrEvent event)
     case CHR_EVENT_BREAK:
     case CHR_EVENT_MUX_IN:
     case CHR_EVENT_MUX_OUT:
+    case CHR_EVENT_RESIZE:
         /* Ignore */
         break;
     }
