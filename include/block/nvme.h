7d70a7996e59e36ca6b8abed10b09353f0d21c8d
Klaus Jensen <k.jensen@samsung.com>
2020-07-03 08:34:08 +0200

    hw/block/nvme: add support for the get log page command
    Add support for the Get Log Page command and basic implementations of
    the mandatory Error Information, SMART / Health Information and Firmware
    Slot Information log pages.
    
    In violation of the specification, the SMART / Health Information log
    page does not persist information over the lifetime of the controller
    because the device has no place to store such persistent state.
    
    Note that the LPA field in the Identify Controller data structure
    intentionally has bit 0 cleared because there is no namespace specific
    information in the SMART / Health information log page.
    
    Required for compliance with NVMe revision 1.3d. See NVM Express 1.3d,
    Section 5.14 ("Get Log Page command").
    
    Signed-off-by: Klaus Jensen <klaus.jensen@cnexlabs.com>
    Signed-off-by: Klaus Jensen <k.jensen@samsung.com>
    Acked-by: Keith Busch <kbusch@kernel.org>
    Message-Id: <20200703063420.2241014-7-its@irrelevant.dk>

--- include/block/nvme.h
+++ include/block/nvme.h
@@ -704,9 +704,9 @@ typedef struct NvmeErrorLog {
     uint8_t     resv[35];
 } NvmeErrorLog;
 
-typedef struct NvmeSmartLog {
+typedef struct QEMU_PACKED NvmeSmartLog {
     uint8_t     critical_warning;
-    uint8_t     temperature[2];
+    uint16_t    temperature;
     uint8_t     available_spare;
     uint8_t     available_spare_threshold;
     uint8_t     percentage_used;
@@ -844,6 +844,10 @@ enum NvmeIdCtrlOncs {
 
 enum NvmeIdCtrlFrmw {
     NVME_FRMW_SLOT1_RO = 1 << 0,
+};
+
+enum NvmeIdCtrlLpa {
+    NVME_LPA_EXTENDED = 1 << 2,
 };
 
 #define NVME_CTRL_SQES_MIN(sqes) ((sqes) & 0xf)
