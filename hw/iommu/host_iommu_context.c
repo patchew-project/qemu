3333e6cc7ab30c9da9e011ac40eb2f421c2f6636
Liu Yi L <yi.l.liu@intel.com>
2020-07-04 04:36:45 -0700

    vfio: add support for flush iommu stage-1 cache
    This patch adds flush_stage1_cache() definition in HostIOMUContextClass.
    And adds corresponding implementation in VFIO. This is to expose a way
    for vIOMMU to flush stage-1 cache in host side since guest owns stage-1
    translation structures in dual stage DMA translation configuration.
    
    Cc: Kevin Tian <kevin.tian@intel.com>
    Cc: Jacob Pan <jacob.jun.pan@linux.intel.com>
    Cc: Peter Xu <peterx@redhat.com>
    Cc: Eric Auger <eric.auger@redhat.com>
    Cc: Yi Sun <yi.y.sun@linux.intel.com>
    Cc: David Gibson <david@gibson.dropbear.id.au>
    Cc: Alex Williamson <alex.williamson@redhat.com>
    Acked-by: Peter Xu <peterx@redhat.com>
    Signed-off-by: Liu Yi L <yi.l.liu@intel.com>
    Message-Id: <1593862609-36135-22-git-send-email-yi.l.liu@intel.com>

--- hw/iommu/host_iommu_context.c
+++ hw/iommu/host_iommu_context.c
@@ -113,6 +113,25 @@ int host_iommu_ctx_unbind_stage1_pgtbl(HostIOMMUContext *iommu_ctx,
     return hicxc->unbind_stage1_pgtbl(iommu_ctx, unbind);
 }
 
+int host_iommu_ctx_flush_stage1_cache(HostIOMMUContext *iommu_ctx,
+                                 struct iommu_cache_invalidate_info *cache)
+{
+    HostIOMMUContextClass *hicxc;
+
+    hicxc = HOST_IOMMU_CONTEXT_GET_CLASS(iommu_ctx);
+
+    if (!hicxc) {
+        return -EINVAL;
+    }
+
+    if (!(iommu_ctx->flags & HOST_IOMMU_NESTING) ||
+        !hicxc->flush_stage1_cache) {
+        return -EINVAL;
+    }
+
+    return hicxc->flush_stage1_cache(iommu_ctx, cache);
+}
+
 void host_iommu_ctx_init(void *_iommu_ctx, size_t instance_size,
                          const char *mrtypename,
                          uint64_t flags,
