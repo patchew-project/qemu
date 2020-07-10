ae515ec9ba1d87b2daed06d8cc2307ced2b7fb59
Cindy Lu <lulu@redhat.com>
2020-07-10 14:46:42 +0800

    vhost-vdpa :Fix Coverity CID 1430270 / CID 1420267
    In the function vhost_vdpa_dma_map/unmap, The struct msg was not initialized all its fields.
    
    Signed-off-by: Cindy Lu <lulu@redhat.com>
    Message-Id: <20200710064642.24505-1-lulu@redhat.com>
    Acked-by: Jason Wang <jasowang@redhat.com>
    Reviewed-by: Peter Maydell <peter.maydell@linaro.org>
    Reviewed-by: Li Qiang <liq3ea@gmail.com>

--- hw/virtio/vhost-vdpa.c
+++ hw/virtio/vhost-vdpa.c
@@ -38,7 +38,7 @@ static bool vhost_vdpa_listener_skipped_section(MemoryRegionSection *section)
 static int vhost_vdpa_dma_map(struct vhost_vdpa *v, hwaddr iova, hwaddr size,
                               void *vaddr, bool readonly)
 {
-    struct vhost_msg_v2 msg;
+    struct vhost_msg_v2 msg = {};
     int fd = v->device_fd;
     int ret = 0;
 
@@ -61,7 +61,7 @@ static int vhost_vdpa_dma_map(struct vhost_vdpa *v, hwaddr iova, hwaddr size,
 static int vhost_vdpa_dma_unmap(struct vhost_vdpa *v, hwaddr iova,
                                 hwaddr size)
 {
-    struct vhost_msg_v2 msg;
+    struct vhost_msg_v2 msg = {};
     int fd = v->device_fd;
     int ret = 0;
 
