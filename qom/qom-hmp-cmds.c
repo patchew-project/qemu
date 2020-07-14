patchew-project/qemu

commit e8c9e65816f5dbfe18ad3b2be938d0d8192d459a

qom: Make "info qom-tree" show children sorted
"info qom-tree" prints children in unstable order.  This is a pain
when diffing output for different versions to find change.  Print it
sorted.

Signed-off-by: Markus Armbruster <armbru@redhat.com>
Message-Id: <20200527084754.7531-3-armbru@redhat.com>
Tested-by: Cédric Le Goater <clg@kaod.org>
Reviewed-by: Philippe Mathieu-Daudé <philmd@redhat.com>

qom/qom-hmp-cmds.c
@@ -81,22 +81,25 @@ typedef struct QOMCompositionState {
 
 static void print_qom_composition(Monitor *mon, Object *obj, int indent);
 
-static int print_qom_composition_child(Object *obj, void *opaque)
+static int qom_composition_compare(const void *a, const void *b, void *ignore)
 {
-    QOMCompositionState *s = opaque;
+    return g_strcmp0(a ? object_get_canonical_path_component(a) : NULL,
+                     b ? object_get_canonical_path_component(b) : NULL);
+}
 
-    print_qom_composition(s->mon, obj, s->indent);
+static int insert_qom_composition_child(Object *obj, void *opaque)
+{
+    GQueue *children = opaque;
 
+    g_queue_insert_sorted(children, obj, qom_composition_compare, NULL);
     return 0;
 }
 
 static void print_qom_composition(Monitor *mon, Object *obj, int indent)
 {
-    QOMCompositionState s = {
-        .mon = mon,
-        .indent = indent + 2,
-    };
     char *name;
+    GQueue children;
+    Object *child;
 
     if (obj == object_get_root()) {
         name = g_strdup("");
@@ -106,7 +109,12 @@ static void print_qom_composition(Monitor *mon, Object *obj, int indent)
     monitor_printf(mon, "%*s/%s (%s)\n", indent, "", name,
                    object_get_typename(obj));
     g_free(name);
-    object_child_foreach(obj, print_qom_composition_child, &s);
+
+    g_queue_init(&children);
+    object_child_foreach(obj, insert_qom_composition_child, &children);
+    while ((child = g_queue_pop_head(&children))) {
+        print_qom_composition(mon, child, indent + 2);
+    }
 }
 
 void hmp_info_qom_tree(Monitor *mon, const QDict *dict)

commit 13181693c1ffeaa5b9d25f806f78b8c862f79f81

hmp: fix memory leak in qom_composition_compare()
While 'make chekc', I got following error:

root@ubuntu:~/qemu# ./tests/qtest/device-introspect-test
/x86_64/device/introspect/list: OK
/x86_64/device/introspect/list-fields: OK
/x86_64/device/introspect/none:
=================================================================
==53741==ERROR: LeakSanitizer: detected memory leaks

Direct leak of 212 byte(s) in 20 object(s) allocated from:
    #0 0x7f3b6319cb40 in __interceptor_malloc (/usr/lib/x86_64-linux-gnu/libasan.so.4+0xdeb40)
    #1 0x7f3b62805ab8 in g_malloc (/usr/lib/x86_64-linux-gnu/libglib-2.0.so.0+0x51ab8)

SUMMARY: AddressSanitizer: 212 byte(s) leaked in 20 allocation(s).
tests/qtest/libqtest.c:166: kill_qemu() tried to terminate QEMU process but encountered exit status 1 (expected 0)
Aborted (core dumped)

This is because the 'info qom-tree' path has a memory leak and qemu
exit 1. The leak is in 'qom_composition_compare'. This patch fixes this.

Fixes: e8c9e65816f("qom: Make "info qom-tree" show children sorted")
Signed-off-by: Li Qiang <liq3ea@163.com>
Message-Id: <20200714151152.54760-1-liq3ea@163.com>
Reviewed-by: Philippe Mathieu-Daudé <philmd@redhat.com>

qom/qom-hmp-cmds.c
@@ -96,8 +96,10 @@ static void print_qom_composition(Monitor *mon, Object *obj, int indent);
 
 static int qom_composition_compare(const void *a, const void *b, void *ignore)
 {
-    return g_strcmp0(a ? object_get_canonical_path_component(a) : NULL,
-                     b ? object_get_canonical_path_component(b) : NULL);
+    g_autofree char *t1 = a ? object_get_canonical_path_component(a) : NULL;
+    g_autofree char *t2 = b ? object_get_canonical_path_component(b) : NULL;
+
+    return g_strcmp0(t1, t2);
 }
 
 static int insert_qom_composition_child(Object *obj, void *opaque)