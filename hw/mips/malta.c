73ffb9f336a79050cb1c4abcdc7a836c386b2ce1
Pavel Dovgalyuk <pavel.dovgalyuk@ispras.ru>
2020-07-15 13:19:11 +0300

    hw/mips: remove exit(1) in case of missing ROM
    This patch updates MIPS-based machines to allow starting them without ROM.
    In this case CPU starts to execute instructions from the empty memory,
    but QEMU allows introspecting the machine configuration.
    
    Signed-off-by: Pavel Dovgalyuk <Pavel.Dovgalyuk@ispras.ru>
    Message-Id: <159480835187.15819.781619322329640772.stgit@pasha-ThinkPad-X280>

--- hw/mips/malta.c
+++ hw/mips/malta.c
@@ -1334,10 +1334,8 @@ void mips_malta_init(MachineState *machine)
         /* Load firmware from flash. */
         if (!dinfo) {
             /* Load a BIOS image. */
-            if (bios_name == NULL) {
-                bios_name = BIOS_FILENAME;
-            }
-            filename = qemu_find_file(QEMU_FILE_TYPE_BIOS, bios_name);
+            filename = qemu_find_file(QEMU_FILE_TYPE_BIOS,
+                                      bios_name ?: BIOS_FILENAME);
             if (filename) {
                 bios_size = load_image_targphys(filename, FLASH_ADDRESS,
                                                 BIOS_SIZE);
@@ -1346,9 +1344,8 @@ void mips_malta_init(MachineState *machine)
                 bios_size = -1;
             }
             if ((bios_size < 0 || bios_size > BIOS_SIZE) &&
-                !kernel_filename && !qtest_enabled()) {
-                error_report("Could not load MIPS bios '%s', and no "
-                             "-kernel argument was specified", bios_name);
+                bios_name && !qtest_enabled()) {
+                error_report("Could not load MIPS bios '%s'", bios_name);
                 exit(1);
             }
         }
