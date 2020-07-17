b4d742e0900e69be852f5e2d63a743a07a35d0ea
Philippe Mathieu-Daudé <f4bug@amsat.org>
2020-07-17 09:53:17 +0200

    hw/ide: Cancel pending DMA requests before setting as inactive
    libFuzzer found a case where requests are queued for later in the
    AIO context, but a command set the bus inactive, then when finally
    the requests are processed by the DMA it aborts because it is
    inactive:
    
     include/hw/ide/pci.h:59: IDEState *bmdma_active_if(BMDMAState *): Assertion `bmdma->bus->retry_unit != (uint8_t)-1' failed.
    
    Reproducer available on the BugLink.
    
    Fix by draining the pending DMA requests before inactivating the bus.
    
    BugLink: https://bugs.launchpad.net/qemu/+bug/1887303
    Signed-off-by: Philippe Mathieu-Daudé <f4bug@amsat.org>
    Message-Id: <20200717075317.5376-1-f4bug@amsat.org>
    Reported-by: Alexander Bulekov <alxndr@bu.edu>

--- hw/ide/core.c
+++ hw/ide/core.c
@@ -804,7 +804,7 @@ void dma_buf_commit(IDEState *s, uint32_t tx_bytes)
 
 void ide_set_inactive(IDEState *s, bool more)
 {
-    s->bus->dma->aiocb = NULL;
+    ide_cancel_dma_sync(s);
     ide_clear_retry(s);
     if (s->bus->dma->ops->set_inactive) {
         s->bus->dma->ops->set_inactive(s->bus->dma, more);
