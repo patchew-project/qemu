222d6083fe9bf49c1d91315a7c707d530349473a
Philippe Mathieu-Daudé <f4bug@amsat.org>
2020-07-17 20:17:24 +0200

    hw/pci-host/q35: Ignore write of reserved PCIEXBAR LENGTH field
    libFuzzer triggered the following assertion:
    
      cat << EOF | qemu-system-i386 -M pc-q35-5.0 \
        -nographic -monitor none -serial none \
        -qtest stdio -d guest_errors -trace pci\*
      outl 0xcf8 0xf2000060
      outl 0xcfc 0x8400056e
      EOF
      pci_cfg_write mch 00:0 @0x60 <- 0x8400056e
      Aborted (core dumped)
    
    This is because guest wrote MCH_HOST_BRIDGE_PCIEXBAR_LENGTH_RVD
    (reserved value) to the PCIE XBAR register.
    
    There is no indication on the datasheet about what occurs when
    this value is written. Simply ignore it on QEMU (and report an
    guest error):
    
      pci_cfg_write mch 00:0 @0x60 <- 0x8400056e
      Q35: Reserved PCIEXBAR LENGTH
      pci_cfg_read mch 00:0 @0x0 -> 0x8086
      pci_cfg_read mch 00:0 @0x0 -> 0x29c08086
      ...
    
    Cc: qemu-stable@nongnu.org
    Reported-by: Alexander Bulekov <alxndr@bu.edu>
    BugLink: https://bugs.launchpad.net/qemu/+bug/1878641
    Fixes: df2d8b3ed4 ("q35: Introduce q35 pc based chipset emulator")
    Signed-off-by: Philippe Mathieu-Daudé <f4bug@amsat.org>
    Message-Id: <20200717181724.27493-1-f4bug@amsat.org>
    Reviewed-by: Richard Henderson <richard.henderson@linaro.org>

--- hw/pci-host/q35.c
+++ hw/pci-host/q35.c
@@ -29,6 +29,7 @@
  */
 
 #include "qemu/osdep.h"
+#include "qemu/log.h"
 #include "hw/i386/pc.h"
 #include "hw/pci-host/q35.h"
 #include "hw/qdev-properties.h"
@@ -318,8 +319,8 @@ static void mch_update_pciexbar(MCHPCIState *mch)
         addr_mask |= MCH_HOST_BRIDGE_PCIEXBAR_64ADMSK;
         break;
     case MCH_HOST_BRIDGE_PCIEXBAR_LENGTH_RVD:
-    default:
-        abort();
+        qemu_log_mask(LOG_GUEST_ERROR, "Q35: Reserved PCIEXBAR LENGTH\n");
+        return;
     }
     addr = pciexbar & addr_mask;
     pcie_host_mmcfg_update(pehb, enable, addr, length);
