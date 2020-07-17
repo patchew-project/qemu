7598cf0f903496e2f34e71c07c9dea8a4845b796
Marc Hartmayer <mhartmay@linux.ibm.com>
2020-07-17 11:29:28 +0200

    libvhost-user: handle endianness as mandated by the spec
    Since virtio existed even before it got standardized, the virtio
    standard defines the following types of virtio devices:
    
     + legacy device (pre-virtio 1.0)
     + non-legacy or VIRTIO 1.0 device
     + transitional device (which can act both as legacy and non-legacy)
    
    Virtio 1.0 defines the fields of the virtqueues as little endian,
    while legacy uses guest's native endian [1]. Currently libvhost-user
    does not handle virtio endianness at all, i.e. it works only if the
    native endianness matches with whatever is actually needed. That means
    things break spectacularly on big-endian targets. Let us handle virtio
    endianness for non-legacy as required by the virtio specification
    [1]. We will fence non-legacy virtio devices with the upcoming patch.
    
    [1] https://docs.oasis-open.org/virtio/virtio/v1.1/cs01/virtio-v1.1-cs01.html#x1-210003
    
    Signed-off-by: Marc Hartmayer <mhartmay@linux.ibm.com>
    
    Message-Id: <20200717092929.19453-3-mhartmay@linux.ibm.com>

---
+++ contrib/libvhost-user/libvhost-access.h
@@ -0,0 +1,61 @@
+#ifndef LIBVHOST_ACCESS_H
+
+#include "qemu/bswap.h"
+
+#include "libvhost-user.h"
+
+static inline bool vu_access_is_big_endian(VuDev *dev)
+{
+    /* Devices conforming to VIRTIO 1.0 or later are always LE. */
+    return false;
+}
+
+static inline void vu_stw_p(VuDev *vdev, void *ptr, uint16_t v)
+{
+    if (vu_access_is_big_endian(vdev)) {
+        stw_be_p(ptr, v);
+    } else {
+        stw_le_p(ptr, v);
+    }
+}
+
+static inline void vu_stl_p(VuDev *vdev, void *ptr, uint32_t v)
+{
+    if (vu_access_is_big_endian(vdev)) {
+        stl_be_p(ptr, v);
+    } else {
+        stl_le_p(ptr, v);
+    }
+}
+
+static inline void vu_stq_p(VuDev *vdev, void *ptr, uint64_t v)
+{
+    if (vu_access_is_big_endian(vdev)) {
+        stq_be_p(ptr, v);
+    } else {
+        stq_le_p(ptr, v);
+    }
+}
+
+static inline int vu_lduw_p(VuDev *vdev, const void *ptr)
+{
+    if (vu_access_is_big_endian(vdev))
+        return lduw_be_p(ptr);
+    return lduw_le_p(ptr);
+}
+
+static inline int vu_ldl_p(VuDev *vdev, const void *ptr)
+{
+    if (vu_access_is_big_endian(vdev))
+        return ldl_be_p(ptr);
+    return ldl_le_p(ptr);
+}
+
+static inline uint64_t vu_ldq_p(VuDev *vdev, const void *ptr)
+{
+    if (vu_access_is_big_endian(vdev))
+        return ldq_be_p(ptr);
+    return ldq_le_p(ptr);
+}
+
+#endif /* LIBVHOST_ACCESS_H */
