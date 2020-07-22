d20f2bf35a919181a8b1fd3ea3e57a7a4911bee0
Thiago Jung Bauermann <bauerman@linux.ibm.com>
2020-07-22 00:50:16 -0300

    target/s390x: Use start-powered-off CPUState property
    Instead of setting CPUState::halted to 1 in s390_cpu_initfn(), use the
    start-powered-off property which makes cpu_common_reset() initialize it to
    1 in common code.
    
    Signed-off-by: Thiago Jung Bauermann <bauerman@linux.ibm.com>
    Message-Id: <20200722035016.469075-10-bauerman@linux.ibm.com>

--- target/s390x/cpu.c
+++ target/s390x/cpu.c
@@ -291,7 +291,8 @@ static void s390_cpu_initfn(Object *obj)
     S390CPU *cpu = S390_CPU(obj);
 
     cpu_set_cpustate_pointers(cpu);
-    cs->halted = 1;
+    object_property_set_bool(OBJECT(cs), "start-powered-off", true,
+                             &error_abort);
     cs->exception_index = EXCP_HLT;
 #if !defined(CONFIG_USER_ONLY)
     object_property_add(obj, "crash-information", "GuestPanicInformation",
