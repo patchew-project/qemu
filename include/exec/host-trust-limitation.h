a24b271275250288fc1fc54cf79ca7c789b85e6c
David Gibson <david@gibson.dropbear.id.au>
2020-07-24 12:57:39 +1000

    host trust limitation: Decouple kvm_memcrypt_*() helpers from KVM
    The kvm_memcrypt_enabled() and kvm_memcrypt_encrypt_data() helper functions
    don't conceptually have any connection to KVM (although it's not possible
    in practice to use them without it).
    
    They also rely on looking at the global KVMState.  But the same information
    is available from the machine, and the only existing callers have natural
    access to the machine state.
    
    Therefore, move and rename them to helpers in host-trust-limitation.h,
    taking an explicit machine parameter.
    
    Signed-off-by: David Gibson <david@gibson.dropbear.id.au>
    Reviewed-by: Richard Henderson <richard.henderson@linaro.org>
    Message-Id: <20200724025744.69644-6-david@gibson.dropbear.id.au>

--- include/exec/host-trust-limitation.h
+++ include/exec/host-trust-limitation.h
@@ -14,6 +14,7 @@
 #define QEMU_HOST_TRUST_LIMITATION_H
 
 #include "qom/object.h"
+#include "hw/boards.h"
 
 #define TYPE_HOST_TRUST_LIMITATION "host-trust-limitation"
 #define HOST_TRUST_LIMITATION(obj)                                    \
@@ -32,5 +33,40 @@ typedef struct HostTrustLimitationClass {
     int (*kvm_init)(HostTrustLimitation *);
     int (*encrypt_data)(HostTrustLimitation *, uint8_t *, uint64_t);
 } HostTrustLimitationClass;
+
+/**
+ * host_trust_limitation_enabled - return whether guest memory is protected
+ *                                 from hypervisor access (with memory
+ *                                 encryption or otherwise)
+ * Returns: true guest memory is not directly accessible to qemu
+ *          false guest memory is directly accessible to qemu
+ */
+static inline bool host_trust_limitation_enabled(MachineState *machine)
+{
+    return !!machine->htl;
+}
+
+/**
+ * host_trust_limitation_encrypt: encrypt the memory range to make
+ *                                it guest accessible
+ *
+ * Return: 1 failed to encrypt the range
+ *         0 succesfully encrypted memory region
+ */
+static inline int host_trust_limitation_encrypt(MachineState *machine,
+                                                uint8_t *ptr, uint64_t len)
+{
+    HostTrustLimitation *htl = machine->htl;
+
+    if (htl) {
+        HostTrustLimitationClass *htlc = HOST_TRUST_LIMITATION_GET_CLASS(htl);
+
+        if (htlc->encrypt_data) {
+            return htlc->encrypt_data(htl, ptr, len);
+        }
+    }
+
+    return 1;
+}
 
 #endif /* QEMU_HOST_TRUST_LIMITATION_H */
