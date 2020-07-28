d1cf2dc68fb083405024e544ddbb98e89b8feb59
Thomas Huth <thuth@redhat.com>
2020-07-28 20:37:34 +0200

    pc-bios/s390-ccw: Allow booting in case the first virtio-blk disk is bad
    If you try to boot with two virtio-blk disks (without bootindex), and
    only the second one is bootable, the s390-ccw bios currently stops at
    the first disk and does not continue booting from the second one. This
    is annoying - and all other major QEMU firmwares succeed to boot from
    the second disk in this case, so we should do the same in the s390-ccw
    bios, too.
    
    Signed-off-by: Thomas Huth <thuth@redhat.com>
    Message-Id: <20200728183734.7838-7-thuth@redhat.com>

--- pc-bios/s390-ccw/main.c
+++ pc-bios/s390-ccw/main.c
@@ -239,7 +239,7 @@ static void ipl_boot_device(void)
         break;
     case CU_TYPE_VIRTIO:
         if (virtio_setup()) {
-            zipl_load(); /* no return */
+            zipl_load();             /* Only returns in case of errors */
         }
         break;
     default:
