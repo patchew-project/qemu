d28e3312f13ad2090d90b16f7d3ff727a87cc97c
Bin Meng <bin.meng@windriver.com>
2020-08-15 00:40:46 +0800

    hw/sd: sd: Correctly set the high capacity bit
    Per the SD spec, Standard Capacity SD Memory Card (SDSC) supports
    capacity up to and including 2 GiB.
    
    Signed-off-by: Bin Meng <bin.meng@windriver.com>
    Message-Id: <1597423256-14847-9-git-send-email-bmeng.cn@gmail.com>

--- hw/sd/sd.c
+++ hw/sd/sd.c
@@ -313,7 +313,7 @@ static void sd_ocr_powerup(void *opaque)
     /* card power-up OK */
     sd->ocr = FIELD_DP32(sd->ocr, OCR, CARD_POWER_UP, 1);
 
-    if (sd->size > 1 * GiB) {
+    if (sd->size > 2 * GiB) {
         sd->ocr = FIELD_DP32(sd->ocr, OCR, CARD_CAPACITY, 1);
     }
 }
