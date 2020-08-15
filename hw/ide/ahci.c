c015c996d554aa0e47d64b4b67b5948c74607c9f
Li Qiang <liq3ea@163.com>
2020-08-15 00:20:52 -0700

    hw: ide: check the pointer before do dma memory unmap
    In 'map_page' we need to check the return value of
    'dma_memory_map' to ensure the we actully maped something.
    Otherwise, we will hit an assert in 'address_space_unmap'.
    This is because we can't find the MR with the NULL buffer.
    This is the LP#1884693:
    
    -->https://bugs.launchpad.net/qemu/+bug/1884693
    
    Reported-by: Alexander Bulekov <alxndr@bu.edu>
    Signed-off-by: Li Qiang <liq3ea@163.com>
    Message-Id: <20200815072052.73228-1-liq3ea@163.com>
    Reviewed-by: Alexander Bulekov <alxndr@bu.edu>

--- hw/ide/ahci.c
+++ hw/ide/ahci.c
@@ -250,6 +250,11 @@ static void map_page(AddressSpace *as, uint8_t **ptr, uint64_t addr,
     }
 
     *ptr = dma_memory_map(as, addr, &len, DMA_DIRECTION_FROM_DEVICE);
+
+    if (!*ptr) {
+        return;
+    }
+
     if (len < wanted) {
         dma_memory_unmap(as, *ptr, len, DMA_DIRECTION_FROM_DEVICE, len);
         *ptr = NULL;
