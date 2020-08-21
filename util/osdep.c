922eed0c860b1e1bbf58b23d7ebfd39fb69fa570
Daniel P. Berrangé <berrange@redhat.com>
2020-08-21 18:21:04 +0100

    util: give a specific error message when O_DIRECT doesn't work
    A common error scenario is to tell QEMU to use O_DIRECT in combination
    with a filesystem that doesn't support it. To aid users to diagnosing
    their mistake we want to provide a clear error message when this happens.
    
    Reviewed-by: Eric Blake <eblake@redhat.com>
    Signed-off-by: Daniel P. Berrangé <berrange@redhat.com>
    Message-Id: <20200821172105.608752-6-berrange@redhat.com>

--- util/osdep.c
+++ util/osdep.c
@@ -345,6 +345,19 @@ qemu_open_internal(const char *name, int flags, mode_t mode, Error **errp)
 
     if (ret == -1) {
         const char *action = flags & O_CREAT ? "create" : "open";
+#ifdef O_DIRECT
+        if (errno == EINVAL && (flags & O_DIRECT)) {
+            ret = open(name, flags & ~O_DIRECT, mode);
+            if (ret != -1) {
+                close(ret);
+                error_setg(errp, "Could not %s '%s' flags 0x%x: "
+                           "filesystem does not support O_DIRECT",
+                           action, name, flags);
+                errno = EINVAL; /* close() clobbered earlier errno */
+                return -1;
+            }
+        }
+#endif /* O_DIRECT */
         error_setg_errno(errp, errno, "Could not %s '%s' flags 0x%x",
                          action, name, flags);
     }
