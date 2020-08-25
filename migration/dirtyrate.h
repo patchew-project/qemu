c8a29441d991304bb47fbe99fdf17d137e7b0b73
Chuan Zheng <zhengchuan@huawei.com>
2020-08-25 09:40:41 +0800

    migration/dirtyrate: Add RamlockDirtyInfo to store sampled page info
    Add RamlockDirtyInfo to store sampled page info of each ramblock.
    
    Signed-off-by: Chuan Zheng <zhengchuan@huawei.com>
    Message-Id: <1598319650-36762-4-git-send-email-zhengchuan@huawei.com>

--- migration/dirtyrate.h
+++ migration/dirtyrate.h
@@ -19,12 +19,30 @@
  */
 #define DIRTYRATE_DEFAULT_SAMPLE_PAGES            512
 
+/*
+ * Record ramblock idstr
+ */
+#define RAMBLOCK_INFO_MAX_LEN                     256
+
 /* Take 1s as default for calculation duration */
 #define DEFAULT_FETCH_DIRTYRATE_TIME_SEC          1
 
 struct DirtyRateConfig {
     uint64_t sample_pages_per_gigabytes; /* sample pages per GB */
     int64_t sample_period_seconds; /* time duration between two sampling */
+};
+
+/*
+ * Store dirtypage info for each ramblock.
+ */
+struct RamblockDirtyInfo {
+    char idstr[RAMBLOCK_INFO_MAX_LEN]; /* idstr for each ramblock */
+    uint8_t *ramblock_addr; /* base address of ramblock we measure */
+    uint64_t ramblock_pages; /* ramblock size in 4K-page */
+    uint64_t *sample_page_vfn; /* relative offset address for sampled page */
+    uint64_t sample_pages_count; /* count of sampled pages */
+    uint64_t sample_dirty_count; /* count of dirty pages we measure */
+    uint32_t *hash_result; /* array of hash result for sampled pages */
 };
 
 void *get_dirtyrate_thread(void *arg);
