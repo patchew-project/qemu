69c77434f6f69574916ae6f0008b3a172e077f84
Lukas Straub <lukasstraub2@web.de>
2020-09-01 11:15:25 +0200

    io: Document qmp oob suitability of qio_channel_shutdown and io_shutdown
    Migration and yank code assume that qio_channel_shutdown is thread
    -safe and can be called from qmp oob handler. Document this after
    checking the code.
    
    Signed-off-by: Lukas Straub <lukasstraub2@web.de>
    Acked-by: Stefan Hajnoczi <stefanha@redhat.com>
    Message-Id: <cd6eaf1915c834e7070a5c3cad083372a43edb82.1598951375.git.lukasstraub2@web.de>

--- include/io/channel.h
+++ include/io/channel.h
@@ -98,7 +98,8 @@ struct QIOChannel {
  * provide additional optional features.
  *
  * Consult the corresponding public API docs for a description
- * of the semantics of each callback
+ * of the semantics of each callback. io_shutdown in particular
+ * must be thread-safe, terminate quickly and must not block.
  */
 struct QIOChannelClass {
     ObjectClass parent;
@@ -515,6 +516,8 @@ int qio_channel_close(QIOChannel *ioc,
  * caller may check for the feature flag
  * QIO_CHANNEL_FEATURE_SHUTDOWN prior to calling
  * this method.
+ *
+ * This function is thread-safe, terminates quickly and does not block.
  *
  * Returns: 0 on success, -1 on error
  */
