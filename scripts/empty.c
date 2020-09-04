0d76f4e2b31f323cd4218fe793d5c263de724635
Paolo Bonzini <pbonzini@redhat.com>
2020-09-04 07:41:08 -0400

    configure: move C++ compiler handling to meson
    All configure tests are run with a C compiler, except for building
    QEMU_CXXFLAGS and checking for compatibility between the C and C++
    compilers.  Move this to Meson and get rid of the link_language
    property in the toolchain description.
    
    Signed-off-by: Paolo Bonzini <pbonzini@redhat.com>
    Message-Id: <20200904114122.31307-33-pbonzini@redhat.com>

---
+++ scripts/empty.c
@@ -0,0 +1,6 @@
+/*
+ * An empty C file.  We need to make it a file and not include it
+ * in meson.build, so that we force it to compile with the C front-end
+ * and link with the C++ front-end.
+ */
+int main(void) { return 0; }
