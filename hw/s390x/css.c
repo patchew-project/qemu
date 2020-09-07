patchew-project/qemu

commit bc994b74ea38579e18f9d9144021c6f8de597a34

s390x/css: Use static initialization for channel_subsys fields
machine_init() will be gone, but we don't need it if we just
initialize the channel_subsys fields statically.

Cc: Cornelia Huck <cornelia.huck@de.ibm.com>
Cc: Christian Borntraeger <borntraeger@de.ibm.com>
Cc: Richard Henderson <rth@twiddle.net>
Cc: Alexander Graf <agraf@suse.de>
Signed-off-by: Eduardo Habkost <ehabkost@redhat.com>
Message-Id: <1455656347-29033-4-git-send-email-ehabkost@redhat.com>
Reviewed-by: Cornelia Huck <cornelia.huck@de.ibm.com>
[adapted on top of indicator changes]
Signed-off-by: Cornelia Huck <cornelia.huck@de.ibm.com>

hw/s390x/css.c
@@ -63,7 +63,17 @@ typedef struct ChannelSubSys {
     QTAILQ_HEAD(, IndAddr) indicator_addresses;
 } ChannelSubSys;
 
-static ChannelSubSys channel_subsys;
+static ChannelSubSys channel_subsys = {
+    .pending_crws = QTAILQ_HEAD_INITIALIZER(channel_subsys.pending_crws),
+    .do_crw_mchk = true,
+    .sei_pending = false,
+    .do_crw_mchk = true,
+    .crws_lost = false,
+    .chnmon_active = false,
+    .io_adapters = QTAILQ_HEAD_INITIALIZER(channel_subsys.io_adapters),
+    .indicator_addresses =
+        QTAILQ_HEAD_INITIALIZER(channel_subsys.indicator_addresses),
+};
 
 IndAddr *get_indicator(hwaddr ind_addr, int len)
 {
@@ -1575,19 +1585,6 @@ int subch_device_load(SubchDev *s, QEMUFile *f)
     }
     return 0;
 }
-
-
-static void css_init(void)
-{
-    QTAILQ_INIT(&channel_subsys.pending_crws);
-    channel_subsys.sei_pending = false;
-    channel_subsys.do_crw_mchk = true;
-    channel_subsys.crws_lost = false;
-    channel_subsys.chnmon_active = false;
-    QTAILQ_INIT(&channel_subsys.io_adapters);
-    QTAILQ_INIT(&channel_subsys.indicator_addresses);
-}
-machine_init(css_init);
 
 void css_reset_sch(SubchDev *sch)
 {

commit 7cc7c6882b493f0811d2c036b2b2c73bd68d2047

hw/s390x/css: Remove double initialization
Fix eventual copy/paste mistake introduced in commit bc994b74ea
("s390x/css: Use static initialization for channel_subsys fields").

Signed-off-by: Philippe Mathieu-Daudé <philmd@redhat.com>
Message-Id: <20200907024020.854465-1-philmd@redhat.com>
Reviewed-by: Thomas Huth <thuth@redhat.com>

hw/s390x/css.c
@@ -353,7 +353,6 @@ static ChannelSubSys channel_subsys = {
     .pending_crws = QTAILQ_HEAD_INITIALIZER(channel_subsys.pending_crws),
     .do_crw_mchk = true,
     .sei_pending = false,
-    .do_crw_mchk = true,
     .crws_lost = false,
     .chnmon_active = false,
     .indicator_addresses =