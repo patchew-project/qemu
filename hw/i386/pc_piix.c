9b17ca727a2751be044e966c63c3163fdf2e1c8f
Daniel P. Berrangé <berrange@redhat.com>
2020-09-08 17:54:37 +0100

    hw/smbios: use qapi for SMBIOS entry point type enum
    This refactoring prepares for exposing the SMBIOS entry point type as a
    machine property on x86.
    
    Signed-off-by: Daniel P. Berrangé <berrange@redhat.com>
    Message-Id: <20200908165438.1008942-5-berrange@redhat.com>

--- hw/i386/pc_piix.c
+++ hw/i386/pc_piix.c
@@ -179,7 +179,7 @@ static void pc_init1(MachineState *machine,
         smbios_set_defaults("QEMU", "Standard PC (i440FX + PIIX, 1996)",
                             mc->name, pcmc->smbios_legacy_mode,
                             pcmc->smbios_uuid_encoded,
-                            SMBIOS_ENTRY_POINT_21);
+                            SMBIOS_ENTRY_POINT_TYPE_2_1);
     }
 
     /* allocate ram and load rom/bios */
