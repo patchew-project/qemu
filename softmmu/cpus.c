ed69243402477fdd349f9ba9189fe8ee54cc4acb
Kevin Wolf <kwolf@redhat.com>
2020-09-09 17:11:38 +0200

    monitor: Add Monitor parameter to monitor_get_cpu_index()
    Most callers actually don't have to rely on cur_mon, but already know
    for which monitor they call monitor_get_cpu_index().
    
    Signed-off-by: Kevin Wolf <kwolf@redhat.com>
    Message-Id: <20200909151149.490589-3-kwolf@redhat.com>
    Acked-by: Stefan Hajnoczi <stefanha@redhat.com>

--- softmmu/cpus.c
+++ softmmu/cpus.c
@@ -2224,7 +2224,7 @@ exit:
 
 void qmp_inject_nmi(Error **errp)
 {
-    nmi_monitor_handle(monitor_get_cpu_index(), errp);
+    nmi_monitor_handle(monitor_get_cpu_index(cur_mon), errp);
 }
 
 void dump_drift_info(void)
