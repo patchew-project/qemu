47e5949ae537ecb07dbcc6562fea75026bc44108
Chuan Zheng <zhengchuan@huawei.com>
2020-09-09 22:52:57 +0800

    migration/tls: add trace points for multifd-tls
    add trace points for multifd-tls for debug.
    
    Signed-off-by: Chuan Zheng <zhengchuan@huawei.com>
    Signed-off-by: Yan Jin <jinyan12@huawei.com>
    Message-Id: <1599663177-53993-8-git-send-email-zhengchuan@huawei.com>

--- migration/multifd.c
+++ migration/multifd.c
@@ -730,7 +730,11 @@ static void multifd_tls_outgoing_handshake(QIOTask *task,
     QIOChannel *ioc = QIO_CHANNEL(qio_task_get_source(task));
     Error *err = NULL;
 
-    qio_task_propagate_error(task, &err);
+    if (qio_task_propagate_error(task, &err)) {
+        trace_multifd_tls_outgoing_handshake_error(error_get_pretty(err));
+    } else {
+        trace_multifd_tls_outgoing_handshake_complete();
+    }
     multifd_channel_connect(p, ioc, err);
 }
 
@@ -747,6 +751,7 @@ static void multifd_tls_channel_connect(MultiFDSendParams *p,
         return;
     }
 
+    trace_multifd_tls_outgoing_handshake_start(hostname);
     qio_channel_set_name(QIO_CHANNEL(tioc), "multifd-tls-outgoing");
     qio_channel_tls_handshake(tioc,
                               multifd_tls_outgoing_handshake,
@@ -761,6 +766,9 @@ static bool multifd_channel_connect(MultiFDSendParams *p,
                                     Error *error)
 {
     MigrationState *s = p->s;
+
+    trace_multifd_set_outgoing_channel(
+        ioc, object_get_typename(OBJECT(ioc)), s->hostname, error);
 
     if (!error) {
         if (s->parameters.tls_creds &&
