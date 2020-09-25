f938b8a15f519ba00f56699bfab00dbdcd603d2a
Marc-André Lureau <marcandre.lureau@redhat.com>
2020-09-25 17:50:56 +0400

    spice: get monitors physical dimension
    Note that for consistency, we use the same logic as MonitorsConfig to
    figure out the associated monitor. However, I can't find traces of the
    discussion/patches about the "new spice-server" behaviour: it still uses
    the multiple-configurations path in git master.
    
    Signed-off-by: Marc-André Lureau <marcandre.lureau@redhat.com>
    Message-Id: <20200925135057.291556-6-marcandre.lureau@redhat.com>

--- ui/spice-display.c
+++ ui/spice-display.c
@@ -679,7 +679,16 @@ static int interface_client_monitors_config(QXLInstance *sin,
         info.width  = mc->monitors[head].width;
         info.height = mc->monitors[head].height;
     }
+#if SPICE_SERVER_VERSION >= 0x000e04 /* release 0.14.4 */
+    if (mc->flags & VD_AGENT_CONFIG_MONITORS_FLAG_PHYSICAL_SIZE) {
+        VDAgentMonitorMM *mm = (void *)&mc->monitors[mc->num_of_monitors];
 
+        if (mc->num_of_monitors > head) {
+            info.width_mm = mm[head].width;
+            info.height_mm = mm[head].height;
+        }
+    }
+#endif
     trace_qemu_spice_ui_info(ssd->qxl.id, info.width, info.height);
     dpy_set_ui_info(ssd->dcl.con, &info);
     return 1;
