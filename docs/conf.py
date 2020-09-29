cf7927e5561e08dcf6648c8996b598372b59d5ae
Havard Skinnemoen via <qemu-devel@nongnu.org>
2020-09-28 17:39:14 -0700

    docs: enable sphinx blockdiag extension
    Signed-off-by: Havard Skinnemoen <hskinnemoen@google.com>
    Message-Id: <20200929003916.4183696-2-hskinnemoen@google.com>

--- docs/conf.py
+++ docs/conf.py
@@ -67,7 +67,10 @@ needs_sphinx = '1.6'
 # Add any Sphinx extension module names here, as strings. They can be
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
-extensions = ['kerneldoc', 'qmp_lexer', 'hxtool', 'depfile']
+extensions = ['kerneldoc', 'qmp_lexer', 'hxtool', 'depfile', 'sphinxcontrib.blockdiag']
+
+# Fontpath for blockdiag (truetype font)
+blockdiag_fontpath = '/usr/share/fonts/truetype/liberation/LiberationSans-Regular.ttf'
 
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
