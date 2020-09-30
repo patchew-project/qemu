34b36a83b217e9015c1e26f84d20b961889919a2
Eric Blake <eblake@redhat.com>
2020-09-30 07:11:01 -0500

    qemu-nbd: Honor SIGINT and SIGHUP
    Honoring just SIGTERM on Linux is too weak; we also want to handle
    other common signals, and do so even on BSD.  Why?  Because at least
    'qemu-nbd -B bitmap' needs a chance to clean up the in-use bit on
    bitmaps when the server is shut down via a signal.
    
    See also: http://bugzilla.redhat.com/1883608
    
    Signed-off-by: Eric Blake <eblake@redhat.com>
    Message-Id: <20200930121105.667049-2-eblake@redhat.com>
    Based-on: <20200924152717.287415-1-kwolf@redhat.com>

--- qemu-nbd.c
+++ qemu-nbd.c
@@ -581,7 +581,7 @@ int main(int argc, char **argv)
     const char *pid_file_name = NULL;
     BlockExportOptions *export_opts;
 
-#if HAVE_NBD_DEVICE
+#ifdef CONFIG_POSIX
     /* The client thread uses SIGTERM to interrupt the server.  A signal
      * handler ensures that "qemu-nbd -v -c" exits with a nice status code.
      */
@@ -589,9 +589,9 @@ int main(int argc, char **argv)
     memset(&sa_sigterm, 0, sizeof(sa_sigterm));
     sa_sigterm.sa_handler = termsig_handler;
     sigaction(SIGTERM, &sa_sigterm, NULL);
-#endif /* HAVE_NBD_DEVICE */
+    sigaction(SIGINT, &sa_sigterm, NULL);
+    sigaction(SIGHUP, &sa_sigterm, NULL);
 
-#ifdef CONFIG_POSIX
     signal(SIGPIPE, SIG_IGN);
 #endif
 
