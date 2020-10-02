71b5893afa29f1fbf5528ab81bf944d39a60df9d
Philippe Mathieu-Daudé <philmd@redhat.com>
2020-10-02 15:39:19 +0200

    qapi: Restrict 'inject-nmi' command to machine code
    Restricting 'inject-nmi' to machine.json pulls slightly
    less QAPI-generated code into user-mode and tools.
    
    Signed-off-by: Philippe Mathieu-Daudé <philmd@redhat.com>
    Message-Id: <20201002133923.1716645-2-philmd@redhat.com>
    Acked-by: Paolo Bonzini <pbonzini@redhat.com>

--- softmmu/cpus.c
+++ softmmu/cpus.c
@@ -29,6 +29,7 @@
 #include "migration/vmstate.h"
 #include "monitor/monitor.h"
 #include "qapi/error.h"
+#include "qapi/qapi-commands-machine.h"
 #include "qapi/qapi-commands-misc.h"
 #include "qapi/qapi-events-run-state.h"
 #include "qapi/qmp/qerror.h"
