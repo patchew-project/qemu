9a1b6985d7c24a2c89a53bb08953cd678b1654b6
Philippe Mathieu-Daudé <philmd@redhat.com>
2020-10-06 13:19:09 +0200

    hw/nvram: Always register FW_CFG_DATA_GENERATOR_INTERFACE
    While the FW_CFG_DATA_GENERATOR_INTERFACE is only consumed
    by a device only available using system-mode (fw_cfg), it is
    implemented by a crypto component (tls-cipher-suites) which
    is always available when crypto is used.
    
    Commit 69699f3055 introduced the following error in the
    qemu-storage-daemon binary:
    
      $ echo -e \
        '{"execute": "qmp_capabilities"}\r\n{"execute": "qom-list-types"}\r\n{"execute": "quit"}\r\n' \
        | storage-daemon/qemu-storage-daemon --chardev stdio,id=qmp0  --monitor qmp0
      {"QMP": {"version": {"qemu": {"micro": 50, "minor": 1, "major": 5}, "package": ""}, "capabilities": ["oob"]}}
      {"return": {}}
      missing interface 'fw_cfg-data-generator' for object 'tls-creds'
      Aborted (core dumped)
    
    Since QOM dependencies are resolved at runtime, this issue
    could not be triggered at linktime, and we don't have test
    running the qemu-storage-daemon binary.
    
    Fix by always registering the QOM interface.
    
    Reported-by: Kevin Wolf <kwolf@redhat.com>
    Fixes: 69699f3055 ("crypto/tls-cipher-suites: Produce fw_cfg consumable blob")
    Signed-off-by: Philippe Mathieu-Daudé <philmd@redhat.com>
    Message-Id: <20201006111909.2302081-2-philmd@redhat.com>
    Supersedes: <20201005105442.2093105-1-philmd@redhat.com>
    Based-on: <20201006111219.2300921-1-philmd@redhat.com>

---
+++ hw/nvram/fw_cfg-interface.c
@@ -0,0 +1,15 @@
+#include "qemu/osdep.h"
+#include "hw/nvram/fw_cfg.h"
+
+static const TypeInfo fw_cfg_data_generator_interface_info = {
+    .parent = TYPE_INTERFACE,
+    .name = TYPE_FW_CFG_DATA_GENERATOR_INTERFACE,
+    .class_size = sizeof(FWCfgDataGeneratorClass),
+};
+
+static void fw_cfg_register_types(void)
+{
+    type_register_static(&fw_cfg_data_generator_interface_info);
+}
+
+type_init(fw_cfg_register_types)
