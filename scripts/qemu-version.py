62ed699af6319a0fbb40be013ec73a473a550b53
Yonggang Luo <luoyonggang@gmail.com>
2020-10-06 19:19:50 +0800

    scripts: Convert qemu-version.sh to qemu-version.py
    The sh script are harder to maintain for compatible different
    xsh environment so convert it to python script
    Also incorporate the fixes in
    https://patchew.org/QEMU/20200929143654.518157-1-marcandre.lureau@redhat.com/
    
    Testing args length and if not enough, setting pkgversion and version to ''
    
    Signed-off-by: Yonggang Luo <luoyonggang@gmail.com>
    Message-Id: <20201006111950.875-1-luoyonggang@gmail.com>

---
+++ scripts/qemu-version.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python3
+
+#
+# Script for retrieve qemu git version information
+#
+# Authors:
+#  Yonggang Luo <luoyonggang@gmail.com>
+#
+# This work is licensed under the terms of the GNU GPL, version 2
+# or, at your option, any later version.  See the COPYING file in
+# the top-level directory.
+
+import sys
+import subprocess
+
+def main(_program, dir, pkgversion, version, *unused):
+    if len(pkgversion) == 0:
+        pc = subprocess.run(['git', 'describe', '--match', "'v*'", '--dirty', '--always'],
+                            stdout=subprocess.PIPE, stderr=subprocess.DEVNULL, cwd=dir)
+        if pc.returncode == 0:
+            pkgversion = pc.stdout.decode('utf8').strip()
+
+    fullversion = version
+    if pkgversion:
+        fullversion = "{} ({})".format(version, pkgversion)
+
+    print('#define QEMU_PKGVERSION "%s"' % pkgversion)
+    print('#define QEMU_FULLVERSION "%s"' % fullversion)
+
+if __name__ == "__main__":
+    main(*sys.argv)
