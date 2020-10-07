34b468673deef4ebea605dcf7232c4352331b2b8
Yonggang Luo <luoyonggang@gmail.com>
2020-10-08 03:29:40 +0800

    scripts: Convert qemu-version.sh to qemu-version.py
    The sh script are harder to maintain for compatible different
    xsh environment so convert it to python script
    Also incorporate the fixes in
    https://patchew.org/QEMU/20200929143654.518157-1-marcandre.lureau@redhat.com/
    Using --match=v* instead of --match 'v*' for speed up the git version
    retrieve, and also resolve the Windows wildcard match problem. only when the
    .git parallel folder have filenames start with --match=v such as --match=v123
    would cause issue, and that's rare.
    
    Signed-off-by: Yonggang Luo <luoyonggang@gmail.com>
    Message-Id: <20201006112139.700-1-luoyonggang@gmail.com>
    Signed-off-by: Paolo Bonzini <pbonzini@redhat.com>
    Message-Id: <20201007192940.280-1-luoyonggang@gmail.com>

---
+++ scripts/qemu-version.py
@@ -0,0 +1,33 @@
+#!/usr/bin/env python3
+
+#
+# Script for retrieve qemu git version information
+#
+# Authors:
+#  Yonggang Luo <luoyonggang@gmail.com>
+#
+# This work is licensed under the terms of the GNU GPL, version 2
+# or, at your option, any later version.  See the COPYING file in
+# the top-level directory.
+
+import sys
+import subprocess
+import os, os.path
+
+def main(_program, dir, pkgversion, version, *unused):
+    os.chdir(dir)
+    if not pkgversion and os.path.exists('.git'):
+        pc = subprocess.run(['git', 'describe', '--match=v*', '--always'],
+                            stdout=subprocess.PIPE, stderr=subprocess.DEVNULL, encoding='utf8', shell=False)
+        if pc.returncode == 0:
+            pkgversion = pc.stdout.strip()
+
+    fullversion = version
+    if pkgversion:
+        fullversion = "{} ({})".format(version, pkgversion)
+
+    print('#define QEMU_PKGVERSION "%s"' % pkgversion)
+    print('#define QEMU_FULL_VERSION "%s"' % fullversion)
+
+if __name__ == "__main__":
+    main(*sys.argv)
