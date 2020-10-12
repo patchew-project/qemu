b8a288754f6424fb2168a20eedeb217d2e8372ce
Andrey Shinkevich via <qemu-devel@nongnu.org>
2020-10-12 20:43:18 +0300

    block: modify the comment for BDRV_REQ_PREFETCH flag
    Modify the comment for the flag BDRV_REQ_PREFETCH as we are going to
    use it alone and pass it to the COR-filter driver for further
    processing.
    
    Signed-off-by: Andrey Shinkevich <andrey.shinkevich@virtuozzo.com>
    Message-Id: <1602524605-481160-7-git-send-email-andrey.shinkevich@virtuozzo.com>

--- include/block/block.h
+++ include/block/block.h
@@ -81,9 +81,10 @@ typedef enum {
     BDRV_REQ_NO_FALLBACK        = 0x100,
 
     /*
-     * BDRV_REQ_PREFETCH may be used only together with BDRV_REQ_COPY_ON_READ
-     * on read request and means that caller doesn't really need data to be
-     * written to qiov parameter which may be NULL.
+     * BDRV_REQ_PREFETCH may be used together with the BDRV_REQ_COPY_ON_READ
+     * flag or when the COR-filter applied to read operations and means that
+     * caller doesn't really need data to be written to qiov parameter which
+     * may be NULL.
      */
     BDRV_REQ_PREFETCH  = 0x200,
     /* Mask of valid flags */
