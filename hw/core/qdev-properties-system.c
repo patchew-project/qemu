fd65307a1623cad4f3bd933f058e837255071e8f
Eduardo Habkost <ehabkost@redhat.com>
2020-10-30 16:21:17 -0400

    qdev: Stop using error_set_from_qdev_prop_error() for PCI host device property
    Just setting a reasonable error string using error_setg() is
    simpler and makes error messages clearer.
    
    Before:
    
      $ qemu-system-x86_64 -device vfio-pci,host=x
      qemu-system-x86_64: -device vfio-pci,host=x: Property 'vfio-pci.host' doesn't take value 'x'
    
    After:
    
      $ qemu-system-x86_64 -device vfio-pci,host=x
      qemu-system-x86_64: -device vfio-pci,host=x: Property 'vfio-pci.host' can't take value 'x': invalid host device address
    
    Signed-off-by: Eduardo Habkost <ehabkost@redhat.com>
    Message-Id: <20201030202131.796967-7-ehabkost@redhat.com>
    Based-on: 20201029220246.472693-1-ehabkost@redhat.com

--- hw/core/qdev-properties-system.c
+++ hw/core/qdev-properties-system.c
@@ -859,7 +859,7 @@ static void set_pci_host_devaddr(Object *obj, Visitor *v, const char *name,
     return;
 
 inval:
-    error_set_from_qdev_prop_error(errp, EINVAL, obj, prop, str);
+    error_setg(errp, "invalid host device address");
     g_free(str);
 }
 
