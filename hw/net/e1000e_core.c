c360b553f50fd2c9e6ba6fa3c82e61253fb4e24a
Mauro Matteo Cascella <mcascell@redhat.com>
2020-11-05 11:56:16 +0100

    net/e1000e_core: make sure RDH never exceeds RDT in e1000e_ring_advance()
    The e1000e_write_packet_to_guest() function iterates over a set of
    receive descriptors by advancing rx descriptor head register (RDH) from
    its initial value to rx descriptor tail register (RDT). The check in
    e1000e_ring_empty() is responsible for detecting whether RDH has reached
    RDT, terminating the loop if that's the case. Additional checks have
    been added in the past to deal with bogus values submitted by the guest
    to prevent possible infinite loop. This is done by "wrapping around" RDH
    at some point and detecting whether it assumes the original value during
    the loop.
    
    However, when e1000e is configured to use the packet split feature, RDH is
    incremented by two instead of one, as the packet split descriptors are
    32 bytes while regular descriptors are 16 bytes. A malicious or buggy
    guest may set RDT to an odd value and transmit only null RX descriptors.
    This corner case would prevent RDH from ever matching RDT, leading to an
    infinite loop. This patch adds a check in e1000e_ring_advance() to make
    sure RDH does never exceed RDT.
    
    This issue was independently reported by Gaoning Pan (Zhejiang University)
    and Cheolwoo Myung.
    
    Signed-off-by: Mauro Matteo Cascella <mcascell@redhat.com>
    Reported-by: Gaoning Pan <gaoning.pgn@antgroup.com>
    Reported-by: Cheolwoo Myung <330cjfdn@gmail.com>
    Message-Id: <20201105105616.327593-1-mcascell@redhat.com>
    Tested-by: Mauro Matteo Cascella <mcascell@redhat.com>

--- hw/net/e1000e_core.c
+++ hw/net/e1000e_core.c
@@ -831,6 +831,10 @@ e1000e_ring_advance(E1000ECore *core, const E1000E_RingInfo *r, uint32_t count)
 {
     core->mac[r->dh] += count;
 
+    if (core->mac[r->dh] > core->mac[r->dt]) {
+        core->mac[r->dh] = core->mac[r->dt];
+    }
+
     if (core->mac[r->dh] * E1000_RING_DESC_LEN >= core->mac[r->dlen]) {
         core->mac[r->dh] = 0;
     }
