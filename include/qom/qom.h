d8a39f11f9fac4d3ad5a5b4d8d1e638d290d83bb
Eduardo Habkost <ehabkost@redhat.com>
2020-11-12 16:43:41 -0500

    qom: Add new qom.h header
    Multiple QOM features will be provided by different headers.  Add a
    qom/qom.h header to make it easier to include all of them at once.
    
    Signed-off-by: Eduardo Habkost <ehabkost@redhat.com>
    Message-Id: <20201112214350.872250-45-ehabkost@redhat.com>
    Based-on: 20201111183823.283752-1-ehabkost@redhat.com

---
+++ include/qom/qom.h
@@ -0,0 +1,10 @@
+/*
+ * QEMU Object Model convenience header
+ *
+ * This header is not supposed to be included by other headers
+ * but by .c files that use QOM.
+ */
+
+#include "qom/object.h"
+#include "qom/object_interfaces.h"
+#include "qom/qom-qobject.h"
