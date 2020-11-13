patchew-project/qemu

commit 9083da1d4c9dfff30d411f8c73ea494e9d78de1b

net: Don't use error_is_set() to suppress additional errors
Using error_is_set(errp) that way can sweep programming errors under
the carpet when we get called incorrectly with an error set.

qmp_query_rx_filter() breaks its loop when it detects an error.  It
needs to set another error when the loop completes normally.

Return right away instead of merely breaking the loop.

Signed-off-by: Markus Armbruster <armbru@redhat.com>
Signed-off-by: Stefan Hajnoczi <stefanha@redhat.com>

net/net.c
@@ -1045,7 +1045,7 @@ RxFilterInfoList *qmp_query_rx_filter(bool has_name, const char *name,
         if (nc->info->type != NET_CLIENT_OPTIONS_KIND_NIC) {
             if (has_name) {
                 error_setg(errp, "net client(%s) isn't a NIC", name);
-                break;
+                return NULL;
             }
             continue;
         }
@@ -1064,7 +1064,7 @@ RxFilterInfoList *qmp_query_rx_filter(bool has_name, const char *name,
         } else if (has_name) {
             error_setg(errp, "net client(%s) doesn't support"
                        " rx-filter querying", name);
-            break;
+            return NULL;
         }
 
         if (has_name) {
@@ -1072,7 +1072,7 @@ RxFilterInfoList *qmp_query_rx_filter(bool has_name, const char *name,
         }
     }
 
-    if (filter_list == NULL && !error_is_set(errp) && has_name) {
+    if (filter_list == NULL && has_name) {
         error_setg(errp, "invalid net client name: %s", name);
     }
 

commit 88ded2ebc40ee24b4737ca94c6c5d98d5619f9a1

net: Fix memory leak on error
If qmp_query_rx_filter() encounters an error on a second iteration, it
leaks the memory from the first.

Fixes: 9083da1d4c
Signed-off-by: Eric Blake <eblake@redhat.com>
Message-Id: <20201113011340.463563-2-eblake@redhat.com>

net/net.c
@@ -1210,6 +1210,7 @@ RxFilterInfoList *qmp_query_rx_filter(bool has_name, const char *name,
         if (nc->info->type != NET_CLIENT_DRIVER_NIC) {
             if (has_name) {
                 error_setg(errp, "net client(%s) isn't a NIC", name);
+                qapi_free_RxFilterInfoList(filter_list);
                 return NULL;
             }
             continue;
@@ -1235,6 +1236,7 @@ RxFilterInfoList *qmp_query_rx_filter(bool has_name, const char *name,
         } else if (has_name) {
             error_setg(errp, "net client(%s) doesn't support"
                        " rx-filter querying", name);
+            qapi_free_RxFilterInfoList(filter_list);
             return NULL;
         }
 