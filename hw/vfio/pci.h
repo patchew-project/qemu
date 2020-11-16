759295101d859d02141434fbd885c8f718ae493b
Eric Auger <eric.auger@redhat.com>
2020-11-16 19:13:38 +0100

    vfio/pci: Register handler for iommu fault
    We use the new extended IRQ VFIO_IRQ_TYPE_NESTED type and
    VFIO_IRQ_SUBTYPE_DMA_FAULT subtype to set/unset
    a notifier for physical DMA faults. The associated eventfd is
    triggered, in nested mode, whenever a fault is detected at IOMMU
    physical level.
    
    The actual handler will be implemented in subsequent patches.
    
    Signed-off-by: Eric Auger <eric.auger@redhat.com>
    
    Message-Id: <20201116181349.11908-16-eric.auger@redhat.com>

--- hw/vfio/pci.h
+++ hw/vfio/pci.h
@@ -114,6 +114,12 @@ typedef struct VFIOMSIXInfo {
     unsigned long *pending;
 } VFIOMSIXInfo;
 
+typedef struct VFIOPCIExtIRQ {
+    struct VFIOPCIDevice *vdev;
+    EventNotifier notifier;
+    uint32_t index;
+} VFIOPCIExtIRQ;
+
 #define TYPE_VFIO_PCI "vfio-pci"
 OBJECT_DECLARE_SIMPLE_TYPE(VFIOPCIDevice, VFIO_PCI)
 
@@ -138,6 +144,7 @@ struct VFIOPCIDevice {
     PCIHostDeviceAddress host;
     EventNotifier err_notifier;
     EventNotifier req_notifier;
+    VFIOPCIExtIRQ *ext_irqs;
     int (*resetfn)(struct VFIOPCIDevice *);
     uint32_t vendor_id;
     uint32_t device_id;
