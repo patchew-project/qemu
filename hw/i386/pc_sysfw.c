439cf24402191fd15020eefeb96702d8d764af23
Brijesh Singh <brijesh.singh@amd.com>
2020-11-16 18:52:29 +0000

    hw/i386: set ram_debug_ops when memory encryption is enabled
    When memory encryption is enabled, the guest RAM and boot flash ROM will
    contain the encrypted data. By setting the debug ops allow us to invoke
    encryption APIs when accessing the memory for the debug purposes.
    
    Signed-off-by: Brijesh Singh <brijesh.singh@amd.com>
    Signed-off-by: Ashish Kalra <ashish.kalra@amd.com>
    Message-Id: <0afaef32bcf868297eff2837e2462a3c5efd21ba.1605316268.git.ashish.kalra@amd.com>

--- hw/i386/pc_sysfw.c
+++ hw/i386/pc_sysfw.c
@@ -209,6 +209,12 @@ static void pc_system_flash_map(PCMachineState *pcms,
                     error_report("failed to encrypt pflash rom");
                     exit(1);
                 }
+
+                /*
+                 * The pflash ROM is encrypted, set the debug ops so that any
+                 * debug accesses will use memory encryption APIs.
+                 */
+                kvm_memcrypt_set_debug_ops_memory_region(flash_mem);
             }
         }
     }
