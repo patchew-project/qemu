0f6a9b976059b4adb86b0bd2c4648a01b0f70299
Rémi Denis-Courmont <remi.denis.courmont@huawei.com>
2020-11-23 10:02:35 +0200

    target/arm: add ARMv8.4-SEL2 extension
    This adds handling for the SCR_EL3.EEL2 bit.
    
    A translation block flag is added in A32 mode to route exceptions
    correctly from AArch32 S-EL1 to (AArch64) S-EL2.
    
    Signed-off-by: Rémi Denis-Courmont <remi.denis.courmont@huawei.com>
    Reviewed-by: Richard Henderson <richard.henderson@linaro.org>
    Message-Id: <20201123080237.18465-15-remi.denis.courmont@huawei.com>

--- target/arm/cpu.h
+++ target/arm/cpu.h
@@ -2062,7 +2062,10 @@ static inline bool arm_is_secure(CPUARMState *env)
 static inline bool arm_is_el2_enabled(CPUARMState *env)
 {
     if (arm_feature(env, ARM_FEATURE_EL2)) {
-        return !arm_is_secure_below_el3(env);
+        if (arm_is_secure_below_el3(env)) {
+            return (env->cp15.scr_el3 & SCR_EEL2) != 0;
+        }
+        return true;
     }
     return false;
 }
@@ -2109,7 +2112,8 @@ static inline bool arm_el_is_aa64(CPUARMState *env, int el)
         return aa64;
     }
 
-    if (arm_feature(env, ARM_FEATURE_EL3)) {
+    if (arm_feature(env, ARM_FEATURE_EL3) &&
+        ((env->cp15.scr_el3 & SCR_NS) || !(env->cp15.scr_el3 & SCR_EEL2))) {
         aa64 = aa64 && (env->cp15.scr_el3 & SCR_RW);
     }
 
@@ -3265,7 +3269,7 @@ typedef ARMCPU ArchCPU;
  * We put flags which are shared between 32 and 64 bit mode at the top
  * of the word, and flags which apply to only one mode at the bottom.
  *
- *  31          20    18    14          9              0
+ *  31          20    19    14          9              0
  * +--------------+-----+-----+----------+--------------+
  * |              |     |   TBFLAG_A32   |              |
  * |              |     +-----+----------+  TBFLAG_AM32 |
@@ -3314,6 +3318,7 @@ FIELD(TBFLAG_A32, HSTR_ACTIVE, 16, 1)
  * the same thing as the current security state of the processor!
  */
 FIELD(TBFLAG_A32, NS, 17, 1)
+FIELD(TBFLAG_A32, EEL2, 18, 1)
 
 /*
  * Bit usage when in AArch32 state, for M-profile only.
