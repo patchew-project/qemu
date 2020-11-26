36cfd171f78244882a32cb197ad8e85e773859b0
Daniel P. Berrangé <berrange@redhat.com>
2020-11-26 17:35:17 +0000

    migration: control whether snapshots are ovewritten
    The traditional HMP "savevm" command will overwrite an existing snapshot
    if it already exists with the requested name. This new flag allows this
    to be controlled allowing for safer behaviour with a future QMP command.
    
    Reviewed-by: Eric Blake <eblake@redhat.com>
    Signed-off-by: Daniel P. Berrangé <berrange@redhat.com>
    Message-Id: <20201126173521.137580-8-berrange@redhat.com>

--- monitor/hmp-cmds.c
+++ monitor/hmp-cmds.c
@@ -1140,7 +1140,7 @@ void hmp_savevm(Monitor *mon, const QDict *qdict)
 {
     Error *err = NULL;
 
-    save_snapshot(qdict_get_try_str(qdict, "name"), &err);
+    save_snapshot(qdict_get_try_str(qdict, "name"), true, &err);
     hmp_handle_error(mon, err);
 }
 
