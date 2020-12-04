94af489bae324ae42c59e841a2ba277ae736d2f4
David Gibson <david@gibson.dropbear.id.au>
2020-12-04 16:44:15 +1100

    s390: Recognize securable-guest-memory option
    At least some s390 cpu models support "Protected Virtualization" (PV),
    a mechanism to protect guests from eavesdropping by a compromised
    hypervisor.
    
    This is similar in function to other mechanisms like AMD's SEV and
    POWER's PEF, which are controlled bythe "securable-guest-memory" machine
    option.  s390 is a slightly special case, because we already supported
    PV, simply by using a CPU model with the required feature
    (S390_FEAT_UNPACK).
    
    To integrate this with the option used by other platforms, we
    implement the following compromise:
    
     - When the securable-guest-memory option is set, s390 will recognize it,
       verify that the CPU can support PV (failing if not) and set virtio
       default options necessary for encrypted or protected guests, as on
       other platforms.  i.e. if securable-guest-memory is set, we will
       either create a guest capable of entering PV mode, or fail outright
    
     - If securable-guest-memory is not set, guest's might still be able to
       enter PV mode, if the CPU has the right model.  This may be a
       little surprising, but shouldn't actually be harmful.
    
    To start a guest supporting Protected Virtualization using the new
    option use the command line arguments:
        -object s390-pv-guest,id=pv0 -machine securable-guest-memory=pv0
    
    Signed-off-by: David Gibson <david@gibson.dropbear.id.au>
    Message-Id: <20201204054415.579042-14-david@gibson.dropbear.id.au>

--- hw/s390x/pv.c
+++ hw/s390x/pv.c
@@ -14,8 +14,11 @@
 #include <linux/kvm.h>
 
 #include "cpu.h"
+#include "qapi/error.h"
 #include "qemu/error-report.h"
 #include "sysemu/kvm.h"
+#include "qom/object_interfaces.h"
+#include "exec/securable-guest-memory.h"
 #include "hw/s390x/ipl.h"
 #include "hw/s390x/pv.h"
 
@@ -111,3 +114,58 @@ void s390_pv_inject_reset_error(CPUState *cs)
     /* Report that we are unable to enter protected mode */
     env->regs[r1 + 1] = DIAG_308_RC_INVAL_FOR_PV;
 }
+
+#define TYPE_S390_PV_GUEST "s390-pv-guest"
+#define S390_PV_GUEST(obj)                              \
+    OBJECT_CHECK(S390PVGuestState, (obj), TYPE_S390_PV_GUEST)
+
+typedef struct S390PVGuestState S390PVGuestState;
+
+/**
+ * S390PVGuestState:
+ *
+ * The S390PVGuestState object is basically a dummy used to tell the
+ * securable guest memory system to use s390's PV mechanism.
+ *
+ * # $QEMU \
+ *         -object s390-pv-guest,id=pv0 \
+ *         -machine ...,securable-guest-memory=pv0
+ */
+struct S390PVGuestState {
+    Object parent_obj;
+};
+
+int s390_pv_init(SecurableGuestMemory *sgm, Error **errp)
+{
+    if (!object_dynamic_cast(OBJECT(sgm), TYPE_S390_PV_GUEST)) {
+        return 0;
+    }
+
+    if (!s390_has_feat(S390_FEAT_UNPACK)) {
+        error_setg(errp,
+                   "CPU model does not support Protected Virtualization");
+        return -1;
+    }
+
+    sgm->ready = true;
+
+    return 0;
+}
+
+static const TypeInfo s390_pv_guest_info = {
+    .parent = TYPE_SECURABLE_GUEST_MEMORY,
+    .name = TYPE_S390_PV_GUEST,
+    .instance_size = sizeof(S390PVGuestState),
+    .interfaces = (InterfaceInfo[]) {
+        { TYPE_USER_CREATABLE },
+        { }
+    }
+};
+
+static void
+s390_pv_register_types(void)
+{
+    type_register_static(&s390_pv_guest_info);
+}
+
+type_init(s390_pv_register_types);
