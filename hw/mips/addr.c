e33780fa8b221408ea2fdab1e625fd5c1d8f0f57
Jiaxun Yang <jiaxun.yang@flygoat.com>
2020-12-07 13:13:35 +0800

    hw/mips/addr: Add translation helpers for KSEG1
    It's useful for bootloader to do IO opreations.
    
    Signed-off-by: Jiaxun Yang <jiaxun.yang@flygoat.com>
    Message-Id: <20201207051335.4758-1-jiaxun.yang@flygoat.com>

--- hw/mips/addr.c
+++ hw/mips/addr.c
@@ -35,6 +35,17 @@ uint64_t cpu_mips_phys_to_kseg0(void *opaque, uint64_t addr)
     return addr | ~0x7fffffffll;
 }
 
+uint64_t cpu_mips_kseg1_to_phys(void *opaque, uint64_t addr)
+{
+    return addr & 0x1fffffffll;
+}
+
+uint64_t cpu_mips_phys_to_kseg1(void *opaque, uint64_t addr)
+{
+    return (addr & 0x1fffffffll) | 0xffffffffa0000000ll;
+}
+
+
 uint64_t cpu_mips_kvm_um_phys_to_kseg0(void *opaque, uint64_t addr)
 {
     return addr | 0x40000000ll;
