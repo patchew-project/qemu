458d82ade9d51fbb3dc901a5c0b64dd1e80d6433
Eduardo Habkost <ehabkost@redhat.com>
2020-12-11 09:31:36 +0100

    cpu: Move synchronize_from_tb() to tcg_ops
    Signed-off-by: Eduardo Habkost <ehabkost@redhat.com>
    [claudio: wrapped in CONFIG_TCG]
    Signed-off-by: Claudio Fontana <cfontana@suse.de>
    Reviewed-by: Philippe Mathieu-Daudé <philmd@redhat.com>
    Reviewed-by: Alex Bennée <alex.bennee@linaro.org>
    Message-Id: <20201211083143.14350-19-cfontana@suse.de>

--- include/hw/core/tcg-cpu-ops.h
+++ include/hw/core/tcg-cpu-ops.h
@@ -10,6 +10,8 @@
 #ifndef TCG_CPU_OPS_H
 #define TCG_CPU_OPS_H
 
+#include "hw/core/cpu.h"
+
 /**
  * struct TcgCpuOperations: TCG operations specific to a CPU class
  */
@@ -20,6 +22,16 @@ typedef struct TcgCpuOperations {
      * Called when the first CPU is realized.
      */
     void (*initialize)(void);
+    /**
+     * @synchronize_from_tb: Synchronize state from a TCG #TranslationBlock
+     *
+     * This is called when we abandon execution of a TB before
+     * starting it, and must set all parts of the CPU state which
+     * the previous TB in the chain may not have updated. This
+     * will need to do more. If this hook is not implemented then
+     * the default is to call @set_pc(tb->pc).
+     */
+    void (*synchronize_from_tb)(CPUState *cpu, struct TranslationBlock *tb);
 } TcgCpuOperations;
 
 #endif /* TCG_CPU_OPS_H */
