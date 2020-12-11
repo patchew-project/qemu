1b2402df8725699bf180c90afb0ce7c5815c5cd8
Vladimir Sementsov-Ogievskiy <vsementsov@virtuozzo.com>
2020-12-11 21:39:26 +0300

    block: use int64_t as bytes type in tracked requests
    We are generally moving to int64_t for both offset and bytes parameters
    on all io paths.
    
    Main motivation is realization of 64-bit write_zeroes operation for
    fast zeroing large disk chunks, up to the whole disk.
    
    We chose signed type, to be consistent with off_t (which is signed) and
    with possibility for signed return type (where negative value means
    error).
    
    All requests in block/io must not overflow BDRV_MAX_LENGTH, all
    external users of BdrvTrackedRequest already have corresponding
    assertions, so we are safe. Add some assertions still.
    
    Signed-off-by: Vladimir Sementsov-Ogievskiy <vsementsov@virtuozzo.com>
    Message-Id: <20201211183934.169161-9-vsementsov@virtuozzo.com>
    Based-on: <20201211170812.228643-1-kwolf@redhat.com>

--- block/io.c
+++ block/io.c
@@ -717,10 +717,10 @@ static void tracked_request_end(BdrvTrackedRequest *req)
 static void tracked_request_begin(BdrvTrackedRequest *req,
                                   BlockDriverState *bs,
                                   int64_t offset,
-                                  uint64_t bytes,
+                                  int64_t bytes,
                                   enum BdrvTrackedRequestType type)
 {
-    assert(bytes <= INT64_MAX && offset <= INT64_MAX - bytes);
+    bdrv_check_request(offset, bytes, &error_abort);
 
     *req = (BdrvTrackedRequest){
         .bs = bs,
@@ -741,8 +741,10 @@ static void tracked_request_begin(BdrvTrackedRequest *req,
 }
 
 static bool tracked_request_overlaps(BdrvTrackedRequest *req,
-                                     int64_t offset, uint64_t bytes)
+                                     int64_t offset, int64_t bytes)
 {
+    bdrv_check_request(offset, bytes, &error_abort);
+
     /*        aaaa   bbbb */
     if (offset >= req->overlap_offset + req->overlap_bytes) {
         return false;
@@ -798,9 +800,11 @@ bool bdrv_mark_request_serialising(BdrvTrackedRequest *req, uint64_t align)
 {
     BlockDriverState *bs = req->bs;
     int64_t overlap_offset = req->offset & ~(align - 1);
-    uint64_t overlap_bytes = ROUND_UP(req->offset + req->bytes, align)
-                               - overlap_offset;
+    int64_t overlap_bytes =
+        ROUND_UP(req->offset + req->bytes, align) - overlap_offset;
     bool waited;
+
+    bdrv_check_request(req->offset, req->bytes, &error_abort);
 
     qemu_co_mutex_lock(&bs->reqs_lock);
     if (!req->serialising) {
