057caf0b2164d44a38d4f61676a4b5fc8478ce89
Kito Cheng <kito.cheng@sifive.com>
2020-12-16 10:01:40 +0800

    target/riscv: rvb: support and turn on B-extension from command line
    B-extension is default off, use cpu rv32 or rv64 with x-b=true to
    enable B-extension.
    
    Signed-off-by: Kito Cheng <kito.cheng@sifive.com>
    Signed-off-by: Frank Chang <frank.chang@sifive.com>
    Message-Id: <20201216020150.3157-16-frank.chang@sifive.com>

--- target/riscv/cpu.h
+++ target/riscv/cpu.h
@@ -66,6 +66,7 @@
 #define RVS RV('S')
 #define RVU RV('U')
 #define RVH RV('H')
+#define RVB RV('B')
 
 /* S extension denotes that Supervisor mode exists, however it is possible
    to have a core that support S mode but does not have an MMU and there
@@ -276,6 +277,7 @@ struct RISCVCPU {
         bool ext_f;
         bool ext_d;
         bool ext_c;
+        bool ext_b;
         bool ext_s;
         bool ext_u;
         bool ext_h;
