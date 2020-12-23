d12e9be8e69e91f6ab83f2422277302faccd837d
Jagannathan Raman <jag.raman@oracle.com>
2020-12-22 22:14:38 -0800

    memory: alloc RAM from file at offset
    Allow RAM MemoryRegion to be created from an offset in a file, instead
    of allocating at offset of 0 by default. This is needed to synchronize
    RAM between QEMU & remote process.
    
    Signed-off-by: Jagannathan Raman <jag.raman@oracle.com>
    Signed-off-by: John G Johnson <john.g.johnson@oracle.com>
    Signed-off-by: Elena Ufimtseva <elena.ufimtseva@oracle.com>
    Reviewed-by: Stefan Hajnoczi <stefanha@redhat.com>
    Message-Id: <ece5d4f658fe8ab61ea0a395aa488382d4c0071d.1608702853.git.elena.ufimtseva@oracle.com>

--- softmmu/memory.c
+++ softmmu/memory.c
@@ -1609,6 +1609,7 @@ void memory_region_init_ram_from_fd(MemoryRegion *mr,
                                     uint64_t size,
                                     bool share,
                                     int fd,
+                                    ram_addr_t offset,
                                     Error **errp)
 {
     Error *err = NULL;
@@ -1618,7 +1619,7 @@ void memory_region_init_ram_from_fd(MemoryRegion *mr,
     mr->destructor = memory_region_destructor_ram;
     mr->ram_block = qemu_ram_alloc_from_fd(size, mr,
                                            share ? RAM_SHARED : 0,
-                                           fd, &err);
+                                           fd, offset, &err);
     if (err) {
         mr->size = int128_zero();
         object_unparent(OBJECT(mr));
