1f412e0925f3b9d29eb15cea31e3d63eb34fc463
Richard Henderson <richard.henderson@linaro.org>
2020-12-22 22:01:52 -0800

    tcg/s390: Convert to tcg-target-constr.h
    Signed-off-by: Richard Henderson <richard.henderson@linaro.org>
    Message-Id: <20201223060204.576856-11-richard.henderson@linaro.org>

---
+++ tcg/s390/tcg-target-constr.h
@@ -0,0 +1,15 @@
+/* SPDX-License-Identifier: MIT */
+/*
+ * S390 target-specific operand constaints.
+ * Copyright (c) 2020 Linaro
+ */
+
+REGS('r', 0xffff)
+REGS('L', 0xffff & ~((1 << TCG_REG_R2) | (1 << TCG_REG_R3) | (1 << TCG_REG_R4)))
+REGS('a', 1u << TCG_REG_R2)
+REGS('b', 1u << TCG_REG_R3)
+
+CONST('A', TCG_CT_CONST_S33)
+CONST('I', TCG_CT_CONST_S16)
+CONST('J', TCG_CT_CONST_S32)
+CONST('Z', TCG_CT_CONST_ZERO)
