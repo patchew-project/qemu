8b65955e39e2d3c7c867dae03e92730a444d01dd
Richard Henderson <richard.henderson@linaro.org>
2020-12-22 22:02:04 -0800

    tcg: Remove TCG_TARGET_CONSET_H
    All backends have now been converted to tcg-target-conset.h,
    so we can remove the fallback code.
    
    Signed-off-by: Richard Henderson <richard.henderson@linaro.org>
    Message-Id: <20201223060204.576856-23-richard.henderson@linaro.org>

--- tcg/sparc/tcg-target.h
+++ tcg/sparc/tcg-target.h
@@ -179,6 +179,5 @@ static inline void flush_icache_range(uintptr_t start, uintptr_t stop)
 void tb_target_set_jmp_target(uintptr_t, uintptr_t, uintptr_t);
 
 #define TCG_TARGET_NEED_POOL_LABELS
-#define TCG_TARGET_CONSET_H
 
 #endif
