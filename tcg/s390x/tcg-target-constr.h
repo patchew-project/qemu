02f3a3ef8a9760dd0df4d38c3fcdd960f20094c2
Richard Henderson <richard.henderson@linaro.org>
2020-12-25 12:19:44 -0800

    tcg/s390x: Add host vector framework
    Add registers and function stubs.  The functionality
    is disabled via squashing s390_facilities[2] to 0.
    
    We must still include results for the mandatory opcodes in
    tcg_target_op_def, as all opcodes are checked during tcg init.
    
    Signed-off-by: Richard Henderson <richard.henderson@linaro.org>
    Message-Id: <20201225201956.692861-5-richard.henderson@linaro.org>
    Based-on: 20201223060204.576856-1-richard.henderson@linaro.org
    Based-on: 20201217145215.534637-1-richard.henderson@linaro.org

--- tcg/s390x/tcg-target-constr.h
+++ tcg/s390x/tcg-target-constr.h
@@ -8,6 +8,7 @@ REGS('r', 0xffff)
 REGS('L', 0xffff & ~((1 << TCG_REG_R2) | (1 << TCG_REG_R3) | (1 << TCG_REG_R4)))
 REGS('a', 1u << TCG_REG_R2)
 REGS('b', 1u << TCG_REG_R3)
+REGS('v', 0xffffffff00000000ull)
 
 CONST('A', TCG_CT_CONST_S33)
 CONST('I', TCG_CT_CONST_S16)
