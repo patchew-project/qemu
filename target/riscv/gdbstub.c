eb47c134186d4e6c04c2d26370c019d5c9d85c23
Hsiangkai Wang <kai.wang@sifive.com>
2021-01-12 17:39:42 +0800

    target/riscv: gdb: modify gdb csr xml file to align with csr register map
    Signed-off-by: Hsiangkai Wang <kai.wang@sifive.com>
    Acked-by: Richard Henderson <richard.henderson@linaro.org>
    Signed-off-by: Frank Chang <frank.chang@sifive.com>
    Message-Id: <20210112093950.17530-69-frank.chang@sifive.com>
    Based-on: <1596102747-20226-1-git-send-email-chihmin.chao@sifive.com/>

--- target/riscv/gdbstub.c
+++ target/riscv/gdbstub.c
@@ -418,13 +418,13 @@ void riscv_cpu_register_gdb_regs_for_features(CPUState *cs)
     }
 #if defined(TARGET_RISCV32)
     gdb_register_coprocessor(cs, riscv_gdb_get_csr, riscv_gdb_set_csr,
-                             240, "riscv-32bit-csr.xml", 0);
+                             241, "riscv-32bit-csr.xml", 0);
 
     gdb_register_coprocessor(cs, riscv_gdb_get_virtual, riscv_gdb_set_virtual,
                              1, "riscv-32bit-virtual.xml", 0);
 #elif defined(TARGET_RISCV64)
     gdb_register_coprocessor(cs, riscv_gdb_get_csr, riscv_gdb_set_csr,
-                             240, "riscv-64bit-csr.xml", 0);
+                             241, "riscv-64bit-csr.xml", 0);
 
     gdb_register_coprocessor(cs, riscv_gdb_get_virtual, riscv_gdb_set_virtual,
                              1, "riscv-64bit-virtual.xml", 0);
