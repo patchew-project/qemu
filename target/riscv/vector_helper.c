11c0e36258dba6162b65514a4295a89fb09aaf06
Frank Chang <frank.chang@sifive.com>
2021-01-12 17:39:20 +0800

    target/riscv: rvv-1.0: integer comparison instructions
    * Sign-extend vmselu.vi and vmsgtu.vi immediate values.
    * Remove "set tail elements to zeros" as tail elements can be unchanged
      for either VTA to have undisturbed or agnostic setting.
    
    Signed-off-by: Frank Chang <frank.chang@sifive.com>
    Reviewed-by: Richard Henderson <richard.henderson@linaro.org>
    Message-Id: <20210112093950.17530-47-frank.chang@sifive.com>
    Based-on: <1596102747-20226-1-git-send-email-chihmin.chao@sifive.com/>

--- target/riscv/vector_helper.c
+++ target/riscv/vector_helper.c
@@ -1399,7 +1399,6 @@ void HELPER(NAME)(void *vd, void *v0, void *vs1, void *vs2,   \
 {                                                             \
     uint32_t vm = vext_vm(desc);                              \
     uint32_t vl = env->vl;                                    \
-    uint32_t vlmax = vext_maxsz(desc) / sizeof(ETYPE);        \
     uint32_t i;                                               \
                                                               \
     for (i = 0; i < vl; i++) {                                \
@@ -1409,9 +1408,6 @@ void HELPER(NAME)(void *vd, void *v0, void *vs1, void *vs2,   \
             continue;                                         \
         }                                                     \
         vext_set_elem_mask(vd, i, DO_OP(s2, s1));             \
-    }                                                         \
-    for (; i < vlmax; i++) {                                  \
-        vext_set_elem_mask(vd, i, 0);                         \
     }                                                         \
 }
 
@@ -1451,7 +1447,6 @@ void HELPER(NAME)(void *vd, void *v0, target_ulong s1, void *vs2,   \
 {                                                                   \
     uint32_t vm = vext_vm(desc);                                    \
     uint32_t vl = env->vl;                                          \
-    uint32_t vlmax = vext_maxsz(desc) / sizeof(ETYPE);              \
     uint32_t i;                                                     \
                                                                     \
     for (i = 0; i < vl; i++) {                                      \
@@ -1461,9 +1456,6 @@ void HELPER(NAME)(void *vd, void *v0, target_ulong s1, void *vs2,   \
         }                                                           \
         vext_set_elem_mask(vd, i,                                   \
                 DO_OP(s2, (ETYPE)(target_long)s1));                 \
-    }                                                               \
-    for (; i < vlmax; i++) {                                        \
-        vext_set_elem_mask(vd, i, 0);                               \
     }                                                               \
 }
 
