7fb63618bbd9fefa9a91878aabd8ea4775c7c00a
Frank Chang <frank.chang@sifive.com>
2021-01-12 17:39:04 +0800

    target/riscv: rvv-1.0: set-X-first mask bit instructions
    Signed-off-by: Frank Chang <frank.chang@sifive.com>
    Reviewed-by: Richard Henderson <richard.henderson@linaro.org>
    Message-Id: <20210112093950.17530-31-frank.chang@sifive.com>
    Based-on: <1596102747-20226-1-git-send-email-chihmin.chao@sifive.com/>

--- target/riscv/vector_helper.c
+++ target/riscv/vector_helper.c
@@ -4466,7 +4466,6 @@ enum set_mask_type {
 static void vmsetm(void *vd, void *v0, void *vs2, CPURISCVState *env,
                    uint32_t desc, enum set_mask_type type)
 {
-    uint32_t vlmax = env_archcpu(env)->cfg.vlen;
     uint32_t vm = vext_vm(desc);
     uint32_t vl = env->vl;
     int i;
@@ -4495,9 +4494,6 @@ static void vmsetm(void *vd, void *v0, void *vs2, CPURISCVState *env,
                 vext_set_elem_mask(vd, i, 1);
             }
         }
-    }
-    for (; i < vlmax; i++) {
-        vext_set_elem_mask(vd, i, 0);
     }
 }
 
