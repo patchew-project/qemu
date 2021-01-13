483fd894776d5878c097b2358ff029de75274e2f
Vladislav Yaroshchuk <yaroshchuk2000@gmail.com>
2021-01-13 23:53:23 +0300

    target/i386/hvf: add rdmsr 35H MSR_CORE_THREAD_COUNT
    Some guests (ex. Darwin-XNU) can attemp to read this MSR to retrieve and
    validate CPU topology comparing it to ACPI MADT content
    
    MSR description from Intel Manual:
    35H: MSR_CORE_THREAD_COUNT: Configured State of Enabled Processor Core
      Count and Logical Processor Count
    
    Bits 15:0 THREAD_COUNT The number of logical processors that are
      currently enabled in the physical package
    
    Bits 31:16 Core_COUNT The number of processor cores that are currently
      enabled in the physical package
    
    Bits 63:32 Reserved
    
    Signed-off-by: Vladislav Yaroshchuk <yaroshchuk2000@gmail.com>
    Message-Id: <20210113205323.33310-1-yaroshchuk2000@gmail.com>

--- target/i386/cpu.h
+++ target/i386/cpu.h
@@ -371,6 +371,8 @@ typedef enum X86Seg {
 #define MSR_MTRRcap_FIXRANGE_SUPPORT    (1 << 8)
 #define MSR_MTRRcap_WC_SUPPORTED        (1 << 10)
 
+#define MSR_CORE_THREAD_COUNT           0x35
+
 #define MSR_IA32_SYSENTER_CS            0x174
 #define MSR_IA32_SYSENTER_ESP           0x175
 #define MSR_IA32_SYSENTER_EIP           0x176
