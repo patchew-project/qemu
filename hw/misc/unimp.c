7c78eca37edfc6beb24dd369c39951664101c180
Philippe Mathieu-Daudé <f4bug@amsat.org>
2021-01-17 20:24:40 +0100

    hw/misc/unimp: Mark the device with no migratable fields
    This device doesn't have fields to migrate. Be explicit
    by using vmstate_qdev_no_state_to_migrate.
    
    Signed-off-by: Philippe Mathieu-Daudé <f4bug@amsat.org>
    Message-Id: <20210117192446.23753-15-f4bug@amsat.org>

--- hw/misc/unimp.c
+++ hw/misc/unimp.c
@@ -81,6 +81,7 @@ static void unimp_class_init(ObjectClass *klass, void *data)
     DeviceClass *dc = DEVICE_CLASS(klass);
 
     dc->realize = unimp_realize;
+    dc->vmsd = vmstate_qdev_no_state_to_migrate;
     device_class_set_props(dc, unimp_properties);
 }
 
