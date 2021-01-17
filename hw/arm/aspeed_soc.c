349476f2d1857f1dc91729a29a0bbc7ce85996bb
Philippe Mathieu-Daudé <f4bug@amsat.org>
2021-01-17 20:24:30 +0100

    hw/arm/aspeed_soc: Mark the device with no migratable fields
    This device doesn't have fields to migrate. Be explicit
    by using vmstate_qdev_no_state_to_migrate.
    
    Signed-off-by: Philippe Mathieu-Daudé <f4bug@amsat.org>
    Message-Id: <20210117192446.23753-5-f4bug@amsat.org>

--- hw/arm/aspeed_soc.c
+++ hw/arm/aspeed_soc.c
@@ -407,6 +407,7 @@ static void aspeed_soc_class_init(ObjectClass *oc, void *data)
     dc->realize = aspeed_soc_realize;
     /* Reason: Uses serial_hds and nd_table in realize() directly */
     dc->user_creatable = false;
+    dc->vmsd = vmstate_qdev_no_state_to_migrate;
     device_class_set_props(dc, aspeed_soc_properties);
 }
 
