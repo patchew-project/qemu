0cdbfc9e6e0bec7584329d8cc606cef34e9814e6
Claudio Fontana <cfontana@suse.de>
2021-01-28 10:28:10 +0100

    accel: introduce AccelCPUClass extending CPUClass
    add a new optional interface to CPUClass,
    which allows accelerators to extend the CPUClass
    with additional accelerator-specific initializations.
    
    Add the field before tcg_ops, and mark tcg_ops as
    needing to be last in the struct until we rework this
    further in a later patch.
    
    Signed-off-by: Claudio Fontana <cfontana@suse.de>
    Message-Id: <20210128092814.8676-19-cfontana@suse.de>

---
+++ include/hw/core/accel-cpu.h
@@ -0,0 +1,35 @@
+/*
+ * Accelerator interface, specializes CPUClass
+ *
+ * Copyright 2020 SUSE LLC
+ *
+ * This work is licensed under the terms of the GNU GPL, version 2 or later.
+ * See the COPYING file in the top-level directory.
+ */
+
+#ifndef ACCEL_CPU_H
+#define ACCEL_CPU_H
+
+/*
+ * these defines cannot be in cpu.h, because we are using
+ * CPU_RESOLVING_TYPE here.
+ * Use this header to define your accelerator-specific
+ * cpu-specific accelerator interfaces.
+ */
+
+#define TYPE_ACCEL_CPU "accel-" CPU_RESOLVING_TYPE
+#define ACCEL_CPU_NAME(name) (name "-" TYPE_ACCEL_CPU)
+typedef struct AccelCPUClass AccelCPUClass;
+DECLARE_CLASS_CHECKERS(AccelCPUClass, ACCEL_CPU, TYPE_ACCEL_CPU)
+
+typedef struct AccelCPUClass {
+    /*< private >*/
+    ObjectClass parent_class;
+    /*< public >*/
+
+    void (*cpu_class_init)(CPUClass *cc);
+    void (*cpu_instance_init)(CPUState *cpu);
+    void (*cpu_realizefn)(CPUState *cpu, Error **errp);
+} AccelCPUClass;
+
+#endif /* ACCEL_CPU_H */
