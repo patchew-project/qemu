d98a0dee9bd733e05b032d74ff6ade2eac51fbd0
Prasad J Pandit <pjp@fedoraproject.org>
2021-02-02 18:51:03 +0530

    scsi: mptsas: dequeue request object in case of an error (CVE-2021-3392)
    While processing SCSI i/o requests in mptsas_process_scsi_io_request(),
    the Megaraid emulator appends new MPTSASRequest object 'req' to
    the 's->pending' queue. In case of an error, this same object gets
    dequeued in mptsas_free_request() only if SCSIRequest object
    'req->sreq' is initialised. This may lead to a use-after-free issue.
    Unconditionally dequeue 'req' object from 's->pending' to avoid it.
    
    Fixes: CVE-2021-3392
    Buglink: https://bugs.launchpad.net/qemu/+bug/1914236
    Reported-by: Cheolwoo Myung <cwmyung@snu.ac.kr>
    Signed-off-by: Prasad J Pandit <pjp@fedoraproject.org>
    Message-Id: <20210202132103.1096654-1-ppandit@redhat.com>
    Reviewed-by: Li Qiang <liq3ea@gmail.com>

--- hw/scsi/mptsas.c
+++ hw/scsi/mptsas.c
@@ -257,8 +257,8 @@ static void mptsas_free_request(MPTSASRequest *req)
         req->sreq->hba_private = NULL;
         scsi_req_unref(req->sreq);
         req->sreq = NULL;
-        QTAILQ_REMOVE(&s->pending, req, next);
     }
+    QTAILQ_REMOVE(&s->pending, req, next);
     qemu_sglist_destroy(&req->qsg);
     g_free(req);
 }
