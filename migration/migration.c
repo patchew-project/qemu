fe743dda0b37f44b49ea7dab0271015eb7862f98
Chuan Zheng <zhengchuan@huawei.com>
2021-02-03 16:01:34 +0800

    migration/rdma: add the 'migrate_rdma_pin_all' function
    Signed-off-by: Zhimin Feng <fengzhimin1@huawei.com>
    Signed-off-by: Chuan Zheng <zhengchuan@huawei.com>
    Reviewed-by: Dr. David Alan Gilbert <dgilbert@redhat.com>
    Message-Id: <1612339311-114805-2-git-send-email-zhengchuan@huawei.com>

--- migration/migration.c
+++ migration/migration.c
@@ -2382,6 +2382,15 @@ bool migrate_use_events(void)
     return s->enabled_capabilities[MIGRATION_CAPABILITY_EVENTS];
 }
 
+bool migrate_rdma_pin_all(void)
+{
+    MigrationState *s;
+
+    s = migrate_get_current();
+
+    return s->enabled_capabilities[MIGRATION_CAPABILITY_RDMA_PIN_ALL];
+}
+
 bool migrate_use_multifd(void)
 {
     MigrationState *s;
