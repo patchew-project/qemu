bb4837361770fdd6649d590b1c1fb28712bad2f6
Claudio Fontana <cfontana@suse.de>
2021-02-10 16:28:52 +0100

    i386: split user and softmmu functionality in tcg-cpu
    Signed-off-by: Claudio Fontana <cfontana@suse.de>
    Message-Id: <20210210152859.25920-8-cfontana@suse.de>

---
+++ target/i386/tcg/user/tcg-cpu-user.c
@@ -0,0 +1,27 @@
+/*
+ * i386 TCG cpu class initialization functions specific to user-mode
+ *
+ *  Copyright (c) 2003 Fabrice Bellard
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2 of the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library; if not, see <http://www.gnu.org/licenses/>.
+ */
+
+#include "qemu/osdep.h"
+#include "cpu.h"
+#include "tcg/tcg-cpu.h"
+
+bool tcg_cpu_realizefn(CPUState *cs, Error **errp)
+{
+    return true;
+}
