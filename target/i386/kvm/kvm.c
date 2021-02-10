7086ca5081d52978a08139836e314323b8e01bd2
Vitaly Kuznetsov <vkuznets@redhat.com>
2021-02-10 17:40:27 +0100

    i386: expand Hyper-V features during CPU feature expansion time
    To make Hyper-V features appear in e.g. QMP query-cpu-model-expansion we
    need to expand and set the corresponding CPUID leaves early. Modify
    x86_cpu_get_supported_feature_word() to call newly intoduced Hyper-V
    specific kvm_hv_get_supported_cpuid() instead of
    kvm_arch_get_supported_cpuid(). We can't use kvm_arch_get_supported_cpuid()
    as Hyper-V specific CPUID leaves intersect with KVM's.
    
    Note, early expansion will only happen when KVM supports system wide
    KVM_GET_SUPPORTED_HV_CPUID ioctl (KVM_CAP_SYS_HYPERV_CPUID).
    
    Signed-off-by: Vitaly Kuznetsov <vkuznets@redhat.com>
    Message-Id: <20210210164033.607612-16-vkuznets@redhat.com>

--- target/i386/kvm/kvm.c
+++ target/i386/kvm/kvm.c
@@ -1215,11 +1215,20 @@ static uint32_t hv_build_cpuid_leaf(CPUState *cs, uint32_t func, int reg)
  * of 'hv_passthrough' mode and fills the environment with all supported
  * Hyper-V features.
  */
-static void hyperv_expand_features(CPUState *cs, Error **errp)
+void kvm_hyperv_expand_features(X86CPU *cpu, Error **errp)
 {
-    X86CPU *cpu = X86_CPU(cs);
+    CPUState *cs = CPU(cpu);
 
     if (!hyperv_enabled(cpu))
+        return;
+
+    /*
+     * When kvm_hyperv_expand_features is called at CPU feature expansion
+     * time per-CPU kvm_state is not available yet so we can only proceed
+     * when KVM_CAP_SYS_HYPERV_CPUID is supported.
+     */
+    if (!cs->kvm_state &&
+        !kvm_check_extension(kvm_state, KVM_CAP_SYS_HYPERV_CPUID))
         return;
 
     if (cpu->hyperv_passthrough) {
@@ -1554,7 +1563,7 @@ int kvm_arch_init_vcpu(CPUState *cs)
     env->apic_bus_freq = KVM_APIC_BUS_FREQUENCY;
 
     /* Paravirtualization CPUIDs */
-    hyperv_expand_features(cs, &local_err);
+    kvm_hyperv_expand_features(cpu, &local_err);
     if (local_err) {
         error_report_err(local_err);
         return -ENOSYS;
