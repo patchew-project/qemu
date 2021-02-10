39b0a0493fbe15c50615c3737c27f46a12a20d00
Claudio Fontana <cfontana@suse.de>
2021-02-10 16:28:56 +0100

    i386: split misc helper into user and softmmu parts
    Signed-off-by: Claudio Fontana <cfontana@suse.de>
    Message-Id: <20210210152859.25920-12-cfontana@suse.de>

---
+++ target/i386/tcg/user/misc_helper_user.c
@@ -0,0 +1,77 @@
+/*
+ *  x86 misc helpers
+ *
+ *  Copyright (c) 2003 Fabrice Bellard
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2.1 of the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library; if not, see <http://www.gnu.org/licenses/>.
+ */
+
+#include "qemu/osdep.h"
+#include "qemu/main-loop.h"
+#include "cpu.h"
+#include "exec/helper-proto.h"
+#include "exec/exec-all.h"
+#include "exec/cpu_ldst.h"
+#include "exec/address-spaces.h"
+#include "tcg/helper-tcg.h"
+
+void helper_outb(CPUX86State *env, uint32_t port, uint32_t data)
+{
+    fprintf(stderr, "outb: port=0x%04x, data=%02x\n", port, data);
+}
+
+target_ulong helper_inb(CPUX86State *env, uint32_t port)
+{
+    fprintf(stderr, "inb: port=0x%04x\n", port);
+    return 0;
+}
+
+void helper_outw(CPUX86State *env, uint32_t port, uint32_t data)
+{
+    fprintf(stderr, "outw: port=0x%04x, data=%04x\n", port, data);
+}
+
+target_ulong helper_inw(CPUX86State *env, uint32_t port)
+{
+    fprintf(stderr, "inw: port=0x%04x\n", port);
+    return 0;
+}
+
+void helper_outl(CPUX86State *env, uint32_t port, uint32_t data)
+{
+    fprintf(stderr, "outl: port=0x%04x, data=%08x\n", port, data);
+}
+
+target_ulong helper_inl(CPUX86State *env, uint32_t port)
+{
+    fprintf(stderr, "inl: port=0x%04x\n", port);
+    return 0;
+}
+
+target_ulong helper_read_crN(CPUX86State *env, int reg)
+{
+    return 0;
+}
+
+void helper_write_crN(CPUX86State *env, int reg, target_ulong t0)
+{
+}
+
+void helper_wrmsr(CPUX86State *env)
+{
+}
+
+void helper_rdmsr(CPUX86State *env)
+{
+}
