5d7577d4d1bbdb610a6a6150c0b9e63684b63090
Claudio Fontana <cfontana@suse.de>
2021-02-10 16:28:55 +0100

    i386: split tcg btp_helper into softmmu and user parts
    Signed-off-by: Claudio Fontana <cfontana@suse.de>
    Message-Id: <20210210152859.25920-11-cfontana@suse.de>

---
+++ target/i386/tcg/user/bpt_helper_user.c
@@ -0,0 +1,33 @@
+/*
+ *  i386 breakpoint helpers - user-mode code
+ *
+ *  Copyright (c) 2003 Fabrice Bellard
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2.1 of the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library; if not, see <http://www.gnu.org/licenses/>.
+ */
+
+#include "qemu/osdep.h"
+#include "cpu.h"
+#include "exec/exec-all.h"
+#include "exec/helper-proto.h"
+#include "tcg/helper-tcg.h"
+
+void helper_set_dr(CPUX86State *env, int reg, target_ulong t0)
+{
+}
+
+void helper_bpt_io(CPUX86State *env, uint32_t port,
+                   uint32_t size, target_ulong next_eip)
+{
+}
