f14e683006872fdaf8bd1f31be1581639ab393b1
Claudio Fontana <cfontana@suse.de>
2021-02-10 16:28:53 +0100

    i386: split smm helper (softmmu)
    smm is only really useful for softmmu, split in two modules
    around the CONFIG_USER_ONLY, in order to remove the ifdef
    and use the build system instead.
    
    Signed-off-by: Claudio Fontana <cfontana@suse.de>
    Message-Id: <20210210152859.25920-9-cfontana@suse.de>

---
+++ target/i386/tcg/user/smm_helper_user.c
@@ -0,0 +1,31 @@
+/*
+ *  x86 SMM helpers, user mode
+ *
+ *  Copyright (c) 2003 Fabrice Bellard
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2.1 of the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library; if not, see <http://www.gnu.org/licenses/>.
+ */
+
+#include "qemu/osdep.h"
+#include "cpu.h"
+#include "exec/helper-proto.h"
+#include "tcg/helper-tcg.h"
+
+void do_smm_enter(X86CPU *cpu)
+{
+}
+
+void helper_rsm(CPUX86State *env)
+{
+}
