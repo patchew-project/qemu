a1970dcbcec335b2842eca96fa6cd0630b00cdfe
John Snow <jsnow@redhat.com>
2021-02-18 14:27:12 -0500

    python: create utils sub-package
    Create a space for miscellaneous things that don't belong strictly in
    "qemu.machine" nor "qemu.qmp" packages.
    
    Signed-off-by: John Snow <jsnow@redhat.com>
    Message-Id: <20210218192733.370968-5-jsnow@redhat.com>

--- tests/acceptance/avocado_qemu/__init__.py
+++ tests/acceptance/avocado_qemu/__init__.py
@@ -41,8 +41,8 @@ else:
 sys.path.append(os.path.join(SOURCE_DIR, 'python'))
 
 from qemu.machine import QEMUMachine
-from qemu.machine import kvm_available
-from qemu.machine import tcg_available
+from qemu.utils import kvm_available
+from qemu.utils import tcg_available
 
 def is_readable_executable_file(path):
     return os.path.isfile(path) and os.access(path, os.R_OK | os.X_OK)
