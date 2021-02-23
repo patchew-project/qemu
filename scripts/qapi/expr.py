52431ce1040937c578cf9ef918ca1221e7687ff1
John Snow <jsnow@redhat.com>
2021-02-22 19:34:06 -0500

    qapi/expr.py: Use tuples instead of lists for static data
    It is -- maybe -- possibly -- three nanoseconds faster.
    
    Signed-off-by: John Snow <jsnow@redhat.com>
    Reviewed-by: Eduardo Habkost <ehabkost@redhat.com>
    Reviewed-by: Cleber Rosa <crosa@redhat.com>
    
    Message-Id: <20210223003408.964543-15-jsnow@redhat.com>

--- scripts/qapi/expr.py
+++ scripts/qapi/expr.py
@@ -173,11 +173,11 @@ def check_flags(expr: Expression, info: QAPISourceInfo) -> None:
     :param expr: Expression to validate.
     :param info: QAPI source file information.
     """
-    for key in ['gen', 'success-response']:
+    for key in ('gen', 'success-response'):
         if key in expr and expr[key] is not False:
             raise QAPISemError(
                 info, "flag '%s' may only use false value" % key)
-    for key in ['boxed', 'allow-oob', 'allow-preconfig', 'coroutine']:
+    for key in ('boxed', 'allow-oob', 'allow-preconfig', 'coroutine'):
         if key in expr and expr[key] is not True:
             raise QAPISemError(
                 info, "flag '%s' may only use true value" % key)
