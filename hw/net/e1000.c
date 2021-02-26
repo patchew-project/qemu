330afb3980eb35d6b9951076fe148529f235bdeb
Bin Meng <bin.meng@windriver.com>
2021-02-26 18:03:05 +0800

    Revert "e1000: Pad short frames to minimum size (60 bytes)"
    This reverts commit 78aeb23eded2d0b765bf9145c71f80025b568acd.
    
    The short frames padding is now handled in the QEMU networking codes.
    Revert this workaround in e1000.
    
    Signed-off-by: Bin Meng <bin.meng@windriver.com>
    Message-Id: <1614333786-74258-3-git-send-email-bmeng.cn@gmail.com>

--- hw/net/e1000.c
+++ hw/net/e1000.c
@@ -898,15 +898,7 @@ e1000_receive_iov(NetClientState *nc, const struct iovec *iov, int iovcnt)
         return 0;
     }
 
-    /* Pad to minimum Ethernet frame length */
-    if (size < sizeof(min_buf)) {
-        iov_to_buf(iov, iovcnt, 0, min_buf, size);
-        memset(&min_buf[size], 0, sizeof(min_buf) - size);
-        min_iov.iov_base = filter_buf = min_buf;
-        min_iov.iov_len = size = sizeof(min_buf);
-        iovcnt = 1;
-        iov = &min_iov;
-    } else if (iov->iov_len < MAXIMUM_ETHERNET_HDR_LEN) {
+    if (iov->iov_len < MAXIMUM_ETHERNET_HDR_LEN) {
         /* This is very unlikely, but may happen. */
         iov_to_buf(iov, iovcnt, 0, min_buf, MAXIMUM_ETHERNET_HDR_LEN);
         filter_buf = min_buf;
