a07e4b842cd58ffe3486e57b1ea4d918d9b44a07
Philippe Mathieu-Daudé <f4bug@amsat.org>
2021-03-02 11:27:31 +0100

    sysemu/tcg: Restrict tcg_exec_init() to CONFIG_TCG
    Invert the #ifdef'ry to easily restrict tcg_exec_init() declaration
    to CONFIG_TCG.
    
    Signed-off-by: Philippe Mathieu-Daudé <f4bug@amsat.org>
    Message-Id: <20210302102737.1031287-2-f4bug@amsat.org>

--- include/sysemu/tcg.h
+++ include/sysemu/tcg.h
@@ -8,13 +8,15 @@
 #ifndef SYSEMU_TCG_H
 #define SYSEMU_TCG_H
 
+#ifndef CONFIG_TCG
+#define tcg_enabled() 0
+#else
+
 void tcg_exec_init(unsigned long tb_size, int splitwx);
 
-#ifdef CONFIG_TCG
 extern bool tcg_allowed;
 #define tcg_enabled() (tcg_allowed)
-#else
-#define tcg_enabled() 0
-#endif
+
+#endif /* CONFIG_TCG */
 
 #endif
