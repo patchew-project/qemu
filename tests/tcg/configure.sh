65aa1840558bc0905a7d50d33e2a6682cd97a2ae
Bastian Koppelmann <kbastian@mail.uni-paderborn.de>
2021-03-05 18:00:36 +0100

    tests/tcg/tricore: Add macros to create tests and first test 'abs'
    This kind of tests is inspired by the riscv-tests repository. This adds
    macros that makes it easy to create single instruction self containing
    tests.
    
    It is achieved by macros that create a test sequence for an
    instruction and check for a supplied correct value. If the value is correct the
    next instruction is tested. Otherwise we jump to fail handler that writes is
    test number as a status code back to qemu that then exits on that status code.
    If all tests pass we write back 0 as a status code and exit.
    
    Reviewed-by: Alex Bennée <alex.bennee@linaro.org>
    Signed-off-by: Bastian Koppelmann <kbastian@mail.uni-paderborn.de>
    Message-Id: <20210305170045.869437-7-kbastian@mail.uni-paderborn.de>

--- tests/tcg/configure.sh
+++ tests/tcg/configure.sh
@@ -102,7 +102,7 @@ for target in $target_list; do
     xtensa|xtensaeb)
       arches=xtensa
       ;;
-    alpha|cris|hexagon|hppa|i386|lm32|microblaze|microblazeel|m68k|openrisc|riscv64|s390x|sh4|sparc64)
+    alpha|cris|hexagon|hppa|i386|lm32|microblaze|microblazeel|m68k|openrisc|riscv64|s390x|sh4|sparc64|tricore)
       arches=$target
       ;;
     *)
@@ -186,6 +186,11 @@ for target in $target_list; do
       container_image=debian-sparc64-cross
       container_cross_cc=sparc64-linux-gnu-gcc
       ;;
+    tricore-softmmu)
+        container_image=debian-tricore-cross
+        container_cross_as=tricore-as
+        container_cross_ld=tricore-ld
+        ;;
     xtensa*-softmmu)
       container_image=debian-xtensa-cross
 
