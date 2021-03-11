35c58fc6c42400854369f0637590b12ef7e20761
Kevin Wolf <kwolf@redhat.com>
2021-03-11 15:48:10 +0100

    hmp: QAPIfy object_add
    This switches the HMP command object_add from a QemuOpts-based parser to
    user_creatable_add_from_str() which uses a keyval parser and enforces
    the QAPI schema.
    
    Apart from being a cleanup, this makes non-scalar properties and help
    accessible. In order for help to be printed to the monitor instead of
    stdout, the printf() calls in the help functions are changed to
    qemu_printf().
    
    Signed-off-by: Kevin Wolf <kwolf@redhat.com>
    Acked-by: Paolo Bonzini <pbonzini@redhat.com>
    Acked-by: Peter Krempa <pkrempa@redhat.com>
    Reviewed-by: Eric Blake <eblake@redhat.com>
    Reviewed-by: Dr. David Alan Gilbert <dgilbert@redhat.com>
    Message-Id: <20210311144811.313451-38-kwolf@redhat.com>

--- hmp-commands.hx
+++ hmp-commands.hx
@@ -1337,7 +1337,7 @@ ERST
 
     {
         .name       = "object_add",
-        .args_type  = "object:O",
+        .args_type  = "object:S",
         .params     = "[qom-type=]type,id=str[,prop=value][,...]",
         .help       = "create QOM object",
         .cmd        = hmp_object_add,
