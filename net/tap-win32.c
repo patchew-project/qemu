5d946be429fad9d726201d21275dc3e4e73d0399
Bin Meng <bmeng.cn@gmail.com>
2021-03-16 16:12:45 +0800

    net: tap: Pad short frames to minimum size before send
    Do the same for tap backend as what we did for slirp.
    
    Signed-off-by: Bin Meng <bmeng.cn@gmail.com>
    
    Message-Id: <20210316081254.72684-5-bmeng.cn@gmail.com>

--- net/tap-win32.c
+++ net/tap-win32.c
@@ -31,6 +31,7 @@
 
 #include "qemu-common.h"
 #include "clients.h"            /* net_init_tap */
+#include "net/eth.h"
 #include "net/net.h"
 #include "net/tap.h"            /* tap_has_ufo, ... */
 #include "qemu/error-report.h"
@@ -688,9 +689,17 @@ static void tap_win32_send(void *opaque)
     uint8_t *buf;
     int max_size = 4096;
     int size;
+    uint8_t min_pkt[ETH_ZLEN];
 
     size = tap_win32_read(s->handle, &buf, max_size);
     if (size > 0) {
+        if (!s->nc.peer->do_not_pad) {
+            if (pad_short_frame(min_pkt, buf, size)) {
+                buf = min_pkt;
+                size = ETH_ZLEN;
+            }
+        }
+
         qemu_send_packet(&s->nc, buf, size);
         tap_win32_free_buffer(s->handle, buf);
     }
--- net/tap.c
+++ net/tap.c
@@ -32,6 +32,7 @@
 #include <sys/socket.h>
 #include <net/if.h>
 
+#include "net/eth.h"
 #include "net/net.h"
 #include "clients.h"
 #include "monitor/monitor.h"
@@ -189,6 +190,7 @@ static void tap_send(void *opaque)
 
     while (true) {
         uint8_t *buf = s->buf;
+        uint8_t min_pkt[ETH_ZLEN];
 
         size = tap_read_packet(s->fd, s->buf, sizeof(s->buf));
         if (size <= 0) {
@@ -198,6 +200,13 @@ static void tap_send(void *opaque)
         if (s->host_vnet_hdr_len && !s->using_vnet_hdr) {
             buf  += s->host_vnet_hdr_len;
             size -= s->host_vnet_hdr_len;
+        }
+
+        if (!s->nc.peer->do_not_pad) {
+            if (pad_short_frame(min_pkt, buf, size)) {
+                buf = min_pkt;
+                size = ETH_ZLEN;
+            }
         }
 
         size = qemu_send_packet_async(&s->nc, buf, size, tap_send_completed);
