d56b98b5d5e5dc1a54d23ad99cf09cc8be3eb99a
Atish Patra <atish.patra@wdc.com>
2021-03-19 12:45:29 -0700

    target/riscv: Remove privilege v1.9 specific CSR related code
    Qemu doesn't support RISC-V privilege specification v1.9. Remove the
    remaining v1.9 specific references from the implementation.
    
    Signed-off-by: Atish Patra <atish.patra@wdc.com>
    Message-Id: <20210319194534.2082397-2-atish.patra@wdc.com>

--- target/riscv/translate.c
+++ target/riscv/translate.c
@@ -130,7 +130,7 @@ static void generate_exception(DisasContext *ctx, int excp)
     ctx->base.is_jmp = DISAS_NORETURN;
 }
 
-static void generate_exception_mbadaddr(DisasContext *ctx, int excp)
+static void generate_exception_mtval(DisasContext *ctx, int excp)
 {
     tcg_gen_movi_tl(cpu_pc, ctx->base.pc_next);
     tcg_gen_st_tl(cpu_pc, cpu_env, offsetof(CPURISCVState, badaddr));
@@ -174,7 +174,7 @@ static void gen_exception_illegal(DisasContext *ctx)
 
 static void gen_exception_inst_addr_mis(DisasContext *ctx)
 {
-    generate_exception_mbadaddr(ctx, RISCV_EXCP_INST_ADDR_MIS);
+    generate_exception_mtval(ctx, RISCV_EXCP_INST_ADDR_MIS);
 }
 
 static inline bool use_goto_tb(DisasContext *ctx, target_ulong dest)
