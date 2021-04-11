d4c26e3de35e631fbf31ea27c446321351f094d4
Eric Auger <eric.auger@redhat.com>
2021-04-11 14:09:02 +0200

    vfio/pci: Set up the DMA FAULT region
    Set up the fault region which is composed of the actual fault
    queue (mmappable) and a header used to handle it. The fault
    queue is mmapped.
    
    Signed-off-by: Eric Auger <eric.auger@redhat.com>
    
    Message-Id: <20210411120912.15770-20-eric.auger@redhat.com>

--- hw/vfio/pci.h
+++ hw/vfio/pci.h
@@ -145,6 +145,7 @@ struct VFIOPCIDevice {
     EventNotifier err_notifier;
     EventNotifier req_notifier;
     VFIOPCIExtIRQ *ext_irqs;
+    VFIORegion dma_fault_region;
     int (*resetfn)(struct VFIOPCIDevice *);
     uint32_t vendor_id;
     uint32_t device_id;
