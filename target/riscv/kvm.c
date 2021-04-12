624fca549842d353a6483b3928d8f161cd7f39bc
Yifei Jiang <jiangyifei@huawei.com>
2021-04-12 14:52:41 +0800

    hw/riscv: PLIC update external interrupt by KVM when kvm enabled
    Only support supervisor external interrupt currently.
    
    Signed-off-by: Yifei Jiang <jiangyifei@huawei.com>
    Signed-off-by: Yipeng Yin <yinyipeng1@huawei.com>
    Message-Id: <20210412065246.1853-8-jiangyifei@huawei.com>

--- target/riscv/kvm.c
+++ target/riscv/kvm.c
@@ -453,6 +453,26 @@ void kvm_riscv_reset_vcpu(RISCVCPU *cpu)
     env->gpr[11] = cpu->env.fdt_addr;          /* a1 */
 }
 
+void kvm_riscv_set_irq(RISCVCPU *cpu, int irq, int level)
+{
+    int ret;
+    unsigned virq = level ? KVM_INTERRUPT_SET : KVM_INTERRUPT_UNSET;
+
+    if (irq != IRQ_S_EXT) {
+        return;
+    }
+
+    if (!kvm_enabled()) {
+        return;
+    }
+
+    ret = kvm_vcpu_ioctl(CPU(cpu), KVM_INTERRUPT, &virq);
+    if (ret < 0) {
+        perror("Set irq failed");
+        abort();
+    }
+}
+
 bool kvm_arch_cpu_check_are_resettable(void)
 {
     return true;
