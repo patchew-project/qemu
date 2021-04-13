eb01cc59ebf814b1fa6fc66bf96f45956220e0d8
David Hildenbrand <david@redhat.com>
2021-04-13 11:14:20 +0200

    qmp: Include "reserve" property of memory backends
    Let's include the new property.
    
    Cc: Eric Blake <eblake@redhat.com>
    Cc: Markus Armbruster <armbru@redhat.com>
    Cc: Igor Mammedov <imammedo@redhat.com>
    Signed-off-by: David Hildenbrand <david@redhat.com>
    Message-Id: <20210413091421.7707-14-david@redhat.com>
    Based-on: 20210406080126.24010-1-david@redhat.com

--- hw/core/machine-qmp-cmds.c
+++ hw/core/machine-qmp-cmds.c
@@ -175,6 +175,7 @@ static int query_memdev(Object *obj, void *opaque)
         m->dump = object_property_get_bool(obj, "dump", &error_abort);
         m->prealloc = object_property_get_bool(obj, "prealloc", &error_abort);
         m->share = object_property_get_bool(obj, "share", &error_abort);
+        m->reserve = object_property_get_bool(obj, "reserve", &error_abort);
         m->policy = object_property_get_enum(obj, "policy", "HostMemPolicy",
                                              &error_abort);
         host_nodes = object_property_get_qobject(obj,
