31a15ccb2aca03713c83b120c6747e6e7e6a369d
Vladimir Sementsov-Ogievskiy <vsementsov@virtuozzo.com>
2021-04-16 11:09:07 +0300

    nbd/client-connection: add option for non-blocking connection attempt
    We'll need a possibility of non-blocking nbd_co_establish_connection(),
    so that it returns immediately, and it returns success only if
    connections was previously established in background.
    
    Signed-off-by: Vladimir Sementsov-Ogievskiy <vsementsov@virtuozzo.com>
    Message-Id: <20210416080911.83197-30-vsementsov@virtuozzo.com>
    Supersedes: <20210408140827.332915-1-vsementsov@virtuozzo.com>

--- block/nbd.c
+++ block/nbd.c
@@ -368,7 +368,7 @@ static int nbd_co_do_establish_connection(BlockDriverState *bs, Error **errp)
 
     assert(!s->ioc);
 
-    s->ioc = nbd_co_establish_connection(s->conn, &s->info, errp);
+    s->ioc = nbd_co_establish_connection(s->conn, &s->info, true, errp);
     if (!s->ioc) {
         return -ECONNREFUSED;
     }
