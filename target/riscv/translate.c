58820c81259a018e7beab4eeb3a6d9e259896300
Frank Chang <frank.chang@sifive.com>
2021-04-21 12:13:55 +0800

    target/riscv: rvb: generalized or-combine
    Signed-off-by: Frank Chang <frank.chang@sifive.com>
    Reviewed-by: Richard Henderson <richard.henderson@linaro.org>
    Message-Id: <20210421041400.22243-14-frank.chang@sifive.com>

--- target/riscv/translate.c
+++ target/riscv/translate.c
@@ -717,6 +717,12 @@ static void gen_grevw(TCGv ret, TCGv arg1, TCGv arg2)
     gen_helper_grev(ret, arg1, arg2);
 }
 
+static void gen_gorcw(TCGv ret, TCGv arg1, TCGv arg2)
+{
+    tcg_gen_ext32u_tl(arg1, arg1);
+    gen_helper_gorcw(ret, arg1, arg2);
+}
+
 #endif
 
 static bool gen_arith(DisasContext *ctx, arg_r *a,
