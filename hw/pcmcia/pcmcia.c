01c36bc6242107e7c45e4b2186c6c8ff2a8ea4f3
Philippe Mathieu-Daudé <f4bug@amsat.org>
2021-04-24 18:22:26 +0200

    hw/pcmcia/microdrive: Register machine reset handler
    The abstract PCMCIA_CARD is a bus-less TYPE_DEVICE, so devices
    implementing it are not reset automatically.
    Register a reset handler so children get reset on machine reset.
    
    Note, the DSCM-1XXXX device (TYPE_DSCM1XXXX) which inherits
    TYPE_MICRODRIVE and PCMCIA_CARD reset itself when a disk is
    attached or detached, but was not resetting itself on machine
    reset.
    
    It doesn't seem to be an issue because it is that way since the
    device QDev'ifycation 8 years ago, in commit d1f2c96a81a
    ("pcmcia: QOM'ify PCMCIACardState and MicroDriveState").
    Still, correct to have a proper API usage.
    
    Signed-off-by: Philippe Mathieu-Daudé <f4bug@amsat.org>
    Message-Id: <20210424162229.3312116-3-f4bug@amsat.org>

--- hw/pcmcia/pcmcia.c
+++ hw/pcmcia/pcmcia.c
@@ -6,7 +6,31 @@
 
 #include "qemu/osdep.h"
 #include "qemu/module.h"
+#include "sysemu/reset.h"
 #include "hw/pcmcia.h"
+
+static void pcmcia_card_reset_handler(void *dev)
+{
+    device_legacy_reset(DEVICE(dev));
+}
+
+static void pcmcia_card_realize(DeviceState *dev, Error **errp)
+{
+    qemu_register_reset(pcmcia_card_reset_handler, dev);
+}
+
+static void pcmcia_card_unrealize(DeviceState *dev)
+{
+    qemu_unregister_reset(pcmcia_card_reset_handler, dev);
+}
+
+static void pcmcia_card_class_init(ObjectClass *klass, void *data)
+{
+    DeviceClass *dc = DEVICE_CLASS(klass);
+
+    dc->realize = pcmcia_card_realize;
+    dc->unrealize = pcmcia_card_unrealize;
+}
 
 static const TypeInfo pcmcia_card_type_info = {
     .name = TYPE_PCMCIA_CARD,
@@ -14,6 +38,7 @@ static const TypeInfo pcmcia_card_type_info = {
     .instance_size = sizeof(PCMCIACardState),
     .abstract = true,
     .class_size = sizeof(PCMCIACardClass),
+    .class_init = pcmcia_card_class_init,
 };
 
 static void pcmcia_register_types(void)
