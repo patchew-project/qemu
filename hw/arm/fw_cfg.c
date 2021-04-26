48b90be9ac31b23fd058f9787ae63b3193424209
Philippe Mathieu-Daudé <philmd@redhat.com>
2021-04-26 21:35:19 +0200

    hw/{arm,hppa,riscv}: Add fw_cfg arch-specific stub
    The ARM, HPPA and RISC-V architectures don't declare any fw_cfg
    specific key. To simplify the buildsys machinery and allow building
    QEMU without the fw_cfg device (in the next commit), first add a
    per-architecture empty stub defining the fw_cfg_arch_key_name().
    
    Update the MAINTAINERS section to cover the various target-specific
    fw_cfg.c files.
    
    Signed-off-by: Philippe Mathieu-Daudé <philmd@redhat.com>
    Message-Id: <20210426193520.4115528-7-philmd@redhat.com>

---
+++ hw/arm/fw_cfg.c
@@ -0,0 +1,19 @@
+/*
+ * QEMU fw_cfg helpers (ARM specific)
+ *
+ * Copyright (c) 2021 Red Hat, Inc.
+ *
+ * Author:
+ *   Philippe Mathieu-Daudé <philmd@redhat.com>
+ *
+ * SPDX-License-Identifier: GPL-2.0-or-later
+ */
+
+#include "qemu/osdep.h"
+#include "hw/mips/fw_cfg.h"
+#include "hw/nvram/fw_cfg.h"
+
+const char *fw_cfg_arch_key_name(uint16_t key)
+{
+    return NULL;
+}
