78818fa9a2128241f1d02a4843b4bdb165271bd2
Philippe Mathieu-Daudé <philmd@redhat.com>
2021-04-28 14:51:02 +0200

    hw/sparc/sun4m: Inline sun4m_fdctrl_init()
    There is only one call site for sun4m_fdctrl_init(), and this
    function is specific to the sun4m machines, not part of the
    SYSBUS_FDC API. Move it locally with the machine code, and
    remove its declaration in "hw/block/fdc.h".
    
    Suggested-by: Mark Cave-Ayland <mark.cave-ayland@ilande.co.uk>
    Signed-off-by: Philippe Mathieu-Daudé <philmd@redhat.com>
    Message-Id: <20210428125104.358535-7-philmd@redhat.com>
    Reviewed-by: Mark Cave-Ayland <mark.cave-ayland@ilande.co.uk>

--- include/hw/block/fdc.h
+++ include/hw/block/fdc.h
@@ -14,8 +14,6 @@ void isa_fdc_init_drives(ISADevice *fdc, DriveInfo **fds);
 void sysbus_fdc_init_drives(SysBusDevice *dev, DriveInfo **fds);
 void fdctrl_init_sysbus(qemu_irq irq, int dma_chann,
                         hwaddr mmio_base, DriveInfo **fds);
-void sun4m_fdctrl_init(qemu_irq irq, hwaddr io_base,
-                       DriveInfo **fds, qemu_irq *fdc_tc);
 
 FloppyDriveType isa_fdc_get_drive_type(ISADevice *fdc, int i);
 int cmos_get_fd_drive_type(FloppyDriveType fd0);
