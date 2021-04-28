6a32d7f0f8dbcceab8438e2b036b933195751e29
Alexey Baturo <baturo.alexey@gmail.com>
2021-04-28 07:18:48 +0300

    Allow experimental J-ext to be turned on
    Signed-off-by: Alexey Baturo <space.monkey.delivers@gmail.com>
    Reviewed-by: Alistair Francis <alistair.francis@wdc.com>
    Message-Id: <20210428041848.12982-7-space.monkey.delivers@gmail.com>

--- target/riscv/cpu.c
+++ target/riscv/cpu.c
@@ -502,6 +502,7 @@ static void riscv_cpu_realize(DeviceState *dev, Error **errp)
 #ifndef CONFIG_USER_ONLY
             env->mmte |= PM_EXT_INITIAL;
 #endif
+            target_misa |= RVJ;
         }
         if (cpu->cfg.ext_v) {
             target_misa |= RVV;
@@ -574,6 +575,7 @@ static Property riscv_cpu_properties[] = {
     DEFINE_PROP_BOOL("u", RISCVCPU, cfg.ext_u, true),
     /* This is experimental so mark with 'x-' */
     DEFINE_PROP_BOOL("x-h", RISCVCPU, cfg.ext_h, false),
+    DEFINE_PROP_BOOL("x-j", RISCVCPU, cfg.ext_j, false),
     DEFINE_PROP_BOOL("x-v", RISCVCPU, cfg.ext_v, false),
     DEFINE_PROP_BOOL("Counters", RISCVCPU, cfg.ext_counters, true),
     DEFINE_PROP_BOOL("Zifencei", RISCVCPU, cfg.ext_ifencei, true),
