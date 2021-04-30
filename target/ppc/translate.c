ce86a31f4016c3e012a9b82a650682c406024d54
Richard Henderson <richard.henderson@linaro.org>
2021-04-29 18:15:37 -0700

    target/ppc: Move page crossing check to ppc_tr_translate_insn
    With prefixed instructions, the number of instructions
    remaining until the page crossing is no longer constant.
    
    Signed-off-by: Richard Henderson <richard.henderson@linaro.org>
    Message-Id: <20210430011543.1017113-25-richard.henderson@linaro.org>

--- target/ppc/translate.c
+++ target/ppc/translate.c
@@ -8019,9 +8019,6 @@ static void ppc_tr_init_disas_context(DisasContextBase *dcbase, CPUState *cs)
 
     if (ctx->singlestep_enabled & (CPU_SINGLE_STEP | GDBSTUB_SINGLE_STEP)) {
         ctx->base.max_insns = 1;
-    } else {
-        int bound = -(ctx->base.pc_first | TARGET_PAGE_MASK) / 4;
-        ctx->base.max_insns = MIN(ctx->base.max_insns, bound);
     }
 }
 
@@ -8097,6 +8094,11 @@ static void ppc_tr_translate_insn(DisasContextBase *dcbase, CPUState *cs)
 #if defined(DO_PPC_STATISTICS)
     handler->count++;
 #endif
+
+    /* End the TB when crossing a page boundary. */
+    if (ctx->base.is_jmp == DISAS_NEXT && !(pc & ~TARGET_PAGE_MASK)) {
+        ctx->base.is_jmp = DISAS_TOO_MANY;
+    }
 
     translator_loop_temp_check(&ctx->base);
 }
