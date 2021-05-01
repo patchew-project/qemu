f50d6f7e544e6e99b161e6e380e96fb5ed1fb23d
Taylor Simpson <tsimpson@quicinc.com>
2021-05-01 11:43:20 -0700

    Hexagon (target/hexagon) circular addressing
    The following instructions are added
        L2_loadrub_pci          Rd32 = memub(Rx32++#s4:0:circ(Mu2))
        L2_loadrb_pci           Rd32 = memb(Rx32++#s4:0:circ(Mu2))
        L2_loadruh_pci          Rd32 = memuh(Rx32++#s4:1:circ(Mu2))
        L2_loadrh_pci           Rd32 = memh(Rx32++#s4:1:circ(Mu2))
        L2_loadri_pci           Rd32 = memw(Rx32++#s4:2:circ(Mu2))
        L2_loadrd_pci           Rdd32 = memd(Rx32++#s4:3:circ(Mu2))
        S2_storerb_pci          memb(Rx32++#s4:0:circ(Mu2)) = Rt32
        S2_storerh_pci          memh(Rx32++#s4:1:circ(Mu2)) = Rt32
        S2_storerf_pci          memh(Rx32++#s4:1:circ(Mu2)) = Rt.H32
        S2_storeri_pci          memw(Rx32++#s4:2:circ(Mu2)) = Rt32
        S2_storerd_pci          memd(Rx32++#s4:3:circ(Mu2)) = Rtt32
        S2_storerbnew_pci       memb(Rx32++#s4:0:circ(Mu2)) = Nt8.new
        S2_storerhnew_pci       memw(Rx32++#s4:1:circ(Mu2)) = Nt8.new
        S2_storerinew_pci       memw(Rx32++#s4:2:circ(Mu2)) = Nt8.new
        L2_loadrub_pcr          Rd32 = memub(Rx32++I:circ(Mu2))
        L2_loadrb_pcr           Rd32 = memb(Rx32++I:circ(Mu2))
        L2_loadruh_pcr          Rd32 = memuh(Rx32++I:circ(Mu2))
        L2_loadrh_pcr           Rd32 = memh(Rx32++I:circ(Mu2))
        L2_loadri_pcr           Rd32 = memw(Rx32++I:circ(Mu2))
        L2_loadrd_pcr           Rdd32 = memd(Rx32++I:circ(Mu2))
        S2_storerb_pcr          memb(Rx32++I:circ(Mu2)) = Rt32
        S2_storerh_pcr          memh(Rx32++I:circ(Mu2)) = Rt32
        S2_storerf_pcr          memh(Rx32++I:circ(Mu2)) = Rt32.H32
        S2_storeri_pcr          memw(Rx32++I:circ(Mu2)) = Rt32
        S2_storerd_pcr          memd(Rx32++I:circ(Mu2)) = Rtt32
        S2_storerbnew_pcr       memb(Rx32++I:circ(Mu2)) = Nt8.new
        S2_storerhnew_pcr       memh(Rx32++I:circ(Mu2)) = Nt8.new
        S2_storerinew_pcr       memw(Rx32++I:circ(Mu2)) = Nt8.new
    
    Test cases in tests/tcg/hexagon/circ.c
    
    Signed-off-by: Taylor Simpson <tsimpson@quicinc.com>
    Reviewed-by: Richard Henderson <richard.henderson@linaro.org>
    Message-Id: <1617930474-31979-23-git-send-email-tsimpson@quicinc.com>
    Signed-off-by: Richard Henderson <richard.henderson@linaro.org>
    Message-Id: <20210501184324.1338186-28-richard.henderson@linaro.org>

--- target/hexagon/genptr.c
+++ target/hexagon/genptr.c
@@ -28,6 +28,12 @@
 #undef QEMU_GENERATE
 #include "gen_tcg.h"
 
+static inline TCGv gen_read_reg(TCGv result, int num)
+{
+    tcg_gen_mov_tl(result, hex_gpr[num]);
+    return result;
+}
+
 static inline TCGv gen_read_preg(TCGv pred, uint8_t num)
 {
     tcg_gen_mov_tl(pred, hex_pred[num]);
@@ -266,6 +272,16 @@ static inline void gen_write_ctrl_reg_pair(DisasContext *ctx, int reg_num,
     }
 }
 
+static TCGv gen_get_byte(TCGv result, int N, TCGv src, bool sign)
+{
+    if (sign) {
+        tcg_gen_sextract_tl(result, src, N * 8, 8);
+    } else {
+        tcg_gen_extract_tl(result, src, N * 8, 8);
+    }
+    return result;
+}
+
 static TCGv gen_get_byte_i64(TCGv result, int N, TCGv_i64 src, bool sign)
 {
     TCGv_i64 res64 = tcg_temp_new_i64();
@@ -278,6 +294,21 @@ static TCGv gen_get_byte_i64(TCGv result, int N, TCGv_i64 src, bool sign)
     tcg_temp_free_i64(res64);
 
     return result;
+}
+
+static inline TCGv gen_get_half(TCGv result, int N, TCGv src, bool sign)
+{
+    if (sign) {
+        tcg_gen_sextract_tl(result, src, N * 16, 16);
+    } else {
+        tcg_gen_extract_tl(result, src, N * 16, 16);
+    }
+    return result;
+}
+
+static inline void gen_set_byte(int N, TCGv result, TCGv src)
+{
+    tcg_gen_deposit_tl(result, result, src, N * 8, 8);
 }
 
 static void gen_set_byte_i64(int N, TCGv_i64 result, TCGv src)
@@ -359,6 +390,75 @@ static inline void gen_store_conditional8(CPUHexagonState *env,
 
     gen_set_label(done);
     tcg_gen_movi_tl(hex_llsc_addr, ~0);
+}
+
+static inline void gen_store32(TCGv vaddr, TCGv src, int width, int slot)
+{
+    tcg_gen_mov_tl(hex_store_addr[slot], vaddr);
+    tcg_gen_movi_tl(hex_store_width[slot], width);
+    tcg_gen_mov_tl(hex_store_val32[slot], src);
+}
+
+static inline void gen_store1(TCGv_env cpu_env, TCGv vaddr, TCGv src,
+                              DisasContext *ctx, int slot)
+{
+    gen_store32(vaddr, src, 1, slot);
+    ctx->store_width[slot] = 1;
+}
+
+static inline void gen_store1i(TCGv_env cpu_env, TCGv vaddr, int32_t src,
+                               DisasContext *ctx, int slot)
+{
+    TCGv tmp = tcg_const_tl(src);
+    gen_store1(cpu_env, vaddr, tmp, ctx, slot);
+    tcg_temp_free(tmp);
+}
+
+static inline void gen_store2(TCGv_env cpu_env, TCGv vaddr, TCGv src,
+                              DisasContext *ctx, int slot)
+{
+    gen_store32(vaddr, src, 2, slot);
+    ctx->store_width[slot] = 2;
+}
+
+static inline void gen_store2i(TCGv_env cpu_env, TCGv vaddr, int32_t src,
+                               DisasContext *ctx, int slot)
+{
+    TCGv tmp = tcg_const_tl(src);
+    gen_store2(cpu_env, vaddr, tmp, ctx, slot);
+    tcg_temp_free(tmp);
+}
+
+static inline void gen_store4(TCGv_env cpu_env, TCGv vaddr, TCGv src,
+                              DisasContext *ctx, int slot)
+{
+    gen_store32(vaddr, src, 4, slot);
+    ctx->store_width[slot] = 4;
+}
+
+static inline void gen_store4i(TCGv_env cpu_env, TCGv vaddr, int32_t src,
+                               DisasContext *ctx, int slot)
+{
+    TCGv tmp = tcg_const_tl(src);
+    gen_store4(cpu_env, vaddr, tmp, ctx, slot);
+    tcg_temp_free(tmp);
+}
+
+static inline void gen_store8(TCGv_env cpu_env, TCGv vaddr, TCGv_i64 src,
+                              DisasContext *ctx, int slot)
+{
+    tcg_gen_mov_tl(hex_store_addr[slot], vaddr);
+    tcg_gen_movi_tl(hex_store_width[slot], 8);
+    tcg_gen_mov_i64(hex_store_val64[slot], src);
+    ctx->store_width[slot] = 8;
+}
+
+static inline void gen_store8i(TCGv_env cpu_env, TCGv vaddr, int64_t src,
+                               DisasContext *ctx, int slot)
+{
+    TCGv_i64 tmp = tcg_const_i64(src);
+    gen_store8(cpu_env, vaddr, tmp, ctx, slot);
+    tcg_temp_free_i64(tmp);
 }
 
 static TCGv gen_8bitsof(TCGv result, TCGv value)
