7a3aaa5873141ff6be5b657557b6c2347e546504
Vladimir Sementsov-Ogievskiy <vsementsov@virtuozzo.com>
2021-05-05 10:49:59 +0300

    block: make BlockLimits::max_pdiscard 64bit
    We are going to support 64 bit discard requests. Now update the
    limit variable. It's absolutely safe. The variable is set in some
    drivers, and used in bdrv_co_pdiscard().
    
    Update also max_pdiscard variable in bdrv_co_pdiscard(), so that
    bdrv_co_pdiscard() is now prepared to 64bit requests. The remaining
    logic including num, offset and bytes variables is already
    supporting 64bit requests.
    
    So the only thing that prevents 64 bit requests is limiting
    max_pdiscard variable to INT_MAX in bdrv_co_pdiscard().
    We'll drop this limitation after updating all block drivers.
    
    Signed-off-by: Vladimir Sementsov-Ogievskiy <vsementsov@virtuozzo.com>
    Message-Id: <20210505075001.45041-10-vsementsov@virtuozzo.com>

--- block/io.c
+++ block/io.c
@@ -2991,7 +2991,8 @@ int coroutine_fn bdrv_co_pdiscard(BdrvChild *child, int64_t offset,
                                   int64_t bytes)
 {
     BdrvTrackedRequest req;
-    int max_pdiscard, ret;
+    int ret;
+    int64_t max_pdiscard;
     int head, tail, align;
     BlockDriverState *bs = child->bs;
 
