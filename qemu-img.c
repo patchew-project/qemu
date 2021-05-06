2680daa1335058a997106ad27613a848e0dc3ee2
Vladimir Sementsov-Ogievskiy <vsementsov@virtuozzo.com>
2021-05-06 17:13:53 +0300

    blockjob: implement and use block_job_get_aio_context
    We are going to drop BlockJob.blk. So let's retrieve block job context
    from underlying job instead of main node.
    
    Signed-off-by: Vladimir Sementsov-Ogievskiy <vsementsov@virtuozzo.com>
    Message-Id: <20210506141357.314257-2-vsementsov@virtuozzo.com>

--- qemu-img.c
+++ qemu-img.c
@@ -900,7 +900,7 @@ static void common_block_job_cb(void *opaque, int ret)
 
 static void run_block_job(BlockJob *job, Error **errp)
 {
-    AioContext *aio_context = blk_get_aio_context(job->blk);
+    AioContext *aio_context = block_job_get_aio_context(job);
     int ret = 0;
 
     aio_context_acquire(aio_context);
