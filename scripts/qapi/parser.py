b41764f3d72effb6b16c61fe424f6dd74be99ff9
John Snow <jsnow@redhat.com>
2021-05-11 18:05:45 -0400

    qapi/parser: Assert lexer value is a string
    The type checker can't narrow the type of the token value to string,
    because it's only loosely correlated with the return token.
    
    We know that a token of '#' should always have a "str" value.
    Add an assertion.
    
    Signed-off-by: John Snow <jsnow@redhat.com>
    Message-Id: <20210511220601.2110055-6-jsnow@redhat.com>

--- scripts/qapi/parser.py
+++ scripts/qapi/parser.py
@@ -304,6 +304,7 @@ class QAPISchemaParser:
         cur_doc = QAPIDoc(self, info)
         self.accept(False)
         while self.tok == '#':
+            assert isinstance(self.val, str)  # comment token MUST have str val
             if self.val.startswith('##'):
                 # End of doc comment
                 if self.val != '##':
