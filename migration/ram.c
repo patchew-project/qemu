40d85baed0789d94c73dc215aa287a65cc43f3e6
Andrey Gruzdev <andrey.gruzdev@virtuozzo.com>
2021-05-13 01:31:23 +0300

    migration/snapshot: Move RAM_SAVE_FLAG_xxx defines to migration/ram.h
    Move RAM_SAVE_FLAG_xxx defines from migration/ram.c to migration/ram.h
    
    Signed-off-by: Andrey Gruzdev <andrey.gruzdev@virtuozzo.com>
    Message-Id: <20210512223127.586885-4-andrey.gruzdev@virtuozzo.com>

--- migration/ram.c
+++ migration/ram.c
@@ -63,22 +63,6 @@
 /***********************************************************/
 /* ram save/restore */
 
-/* RAM_SAVE_FLAG_ZERO used to be named RAM_SAVE_FLAG_COMPRESS, it
- * worked for pages that where filled with the same char.  We switched
- * it to only search for the zero value.  And to avoid confusion with
- * RAM_SSAVE_FLAG_COMPRESS_PAGE just rename it.
- */
-
-#define RAM_SAVE_FLAG_FULL     0x01 /* Obsolete, not used anymore */
-#define RAM_SAVE_FLAG_ZERO     0x02
-#define RAM_SAVE_FLAG_MEM_SIZE 0x04
-#define RAM_SAVE_FLAG_PAGE     0x08
-#define RAM_SAVE_FLAG_EOS      0x10
-#define RAM_SAVE_FLAG_CONTINUE 0x20
-#define RAM_SAVE_FLAG_XBZRLE   0x40
-/* 0x80 is reserved in migration.h start with 0x100 next */
-#define RAM_SAVE_FLAG_COMPRESS_PAGE    0x100
-
 static inline bool is_zero_range(uint8_t *p, uint64_t size)
 {
     return buffer_is_zero(p, size);
