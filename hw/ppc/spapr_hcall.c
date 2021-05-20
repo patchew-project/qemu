71bd66c0abd6d19af8f130648176a56195337fe8
Alexey Kardashevskiy <aik@ozlabs.ru>
2021-05-20 19:05:57 +1000

    spapr: Implement Open Firmware client interface
    The PAPR platform describes an OS environment that's presented by
    a combination of a hypervisor and firmware. The features it specifies
    require collaboration between the firmware and the hypervisor.
    
    Since the beginning, the runtime component of the firmware (RTAS) has
    been implemented as a 20 byte shim which simply forwards it to
    a hypercall implemented in qemu. The boot time firmware component is
    SLOF - but a build that's specific to qemu, and has always needed to be
    updated in sync with it. Even though we've managed to limit the amount
    of runtime communication we need between qemu and SLOF, there's some,
    and it has become increasingly awkward to handle as we've implemented
    new features.
    
    This implements a boot time OF client interface (CI) which is
    enabled by a new "x-vof" pseries machine option (stands for "Virtual Open
    Firmware). When enabled, QEMU implements the custom H_OF_CLIENT hcall
    which implements Open Firmware Client Interface (OF CI). This allows
    using a smaller stateless firmware which does not have to manage
    the device tree.
    
    The new "vof.bin" firmware image is included with source code under
    pc-bios/. It also includes RTAS blob.
    
    This implements a handful of CI methods just to get -kernel/-initrd
    working. In particular, this implements the device tree fetching and
    simple memory allocator - "claim" (an OF CI memory allocator) and updates
    "/memory@0/available" to report the client about available memory.
    
    This implements changing some device tree properties which we know how
    to deal with, the rest is ignored. To allow changes, this skips
    fdt_pack() when x-vof=on as not packing the blob leaves some room for
    appending.
    
    In absence of SLOF, this assigns phandles to device tree nodes to make
    device tree traversing work.
    
    When x-vof=on, this adds "/chosen" every time QEMU (re)builds a tree.
    
    This adds basic instances support which are managed by a hash map
    ihandle -> [phandle].
    
    Before the guest started, the used memory is:
    0..e60 - the initial firmware
    8000..10000 - stack
    400000.. - kernel
    3ea0000.. - initramdisk
    
    This OF CI does not implement "interpret".
    
    Unlike SLOF, this does not format uninitialized nvram. Instead, this
    includes a disk image with pre-formatted nvram.
    
    With this basic support, this can only boot into kernel directly.
    However this is just enough for the petitboot kernel and initradmdisk to
    boot from any possible source. Note this requires reasonably recent guest
    kernel with:
    https://git.kernel.org/pub/scm/linux/kernel/git/torvalds/linux.git/commit/?id=df5be5be8735
    
    The immediate benefit is much faster booting time which especially
    crucial with fully emulated early CPU bring up environments. Also this
    may come handy when/if GRUB-in-the-userspace sees light of the day.
    
    This separates VOF and sPAPR in a hope that VOF bits may be reused by
    other POWERPC boards which do not support pSeries.
    
    This is coded in assumption that later on we might be adding support for
    booting from QEMU backends (blockdev is the first candidate) without
    devices/drivers in between as OF1275 does not require that and
    it is quite easy to so.
    
    Signed-off-by: Alexey Kardashevskiy <aik@ozlabs.ru>
    Message-Id: <20210520090557.435689-1-aik@ozlabs.ru>

--- hw/ppc/spapr_hcall.c
+++ hw/ppc/spapr_hcall.c
@@ -1233,7 +1233,7 @@ target_ulong do_client_architecture_support(PowerPCCPU *cpu,
         spapr_setup_hpt(spapr);
     }
 
-    fdt = spapr_build_fdt(spapr, false, fdt_bufsize);
+    fdt = spapr_build_fdt(spapr, spapr->vof != NULL, fdt_bufsize);
 
     g_free(spapr->fdt_blob);
     spapr->fdt_size = fdt_totalsize(fdt);
@@ -1273,6 +1273,25 @@ static target_ulong h_client_architecture_support(PowerPCCPU *cpu,
                                   spapr->fdt_size);
         trace_spapr_cas_continue(spapr->fdt_size + sizeof(hdr));
     }
+
+    return ret;
+}
+
+target_ulong spapr_vof_client_architecture_support(MachineState *ms,
+                                                   CPUState *cs,
+                                                   target_ulong ovec_addr)
+{
+    SpaprMachineState *spapr = SPAPR_MACHINE(ms);
+
+    target_ulong ret = do_client_architecture_support(POWERPC_CPU(cs), spapr,
+                                                      ovec_addr, FDT_MAX_SIZE);
+
+    /*
+     * This adds stdout and generates phandles for boottime and CAS FDTs.
+     * It is alright to update the FDT here as do_client_architecture_support()
+     * does not pack it.
+     */
+    spapr_vof_client_dt_finalize(spapr, spapr->fdt_blob);
 
     return ret;
 }
