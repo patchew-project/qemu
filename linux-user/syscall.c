21b29bd3f845e2e9c155194a6608c7ef6c7d99bc
Michael Forney <mforney@mforney.org>
2021-05-25 20:55:56 -0700

    linux-user: Use public sigev_notify_thread_id member if available
    _sigev_un._tid is an internal glibc field and is not available on
    musl libc. The sigevent(7) man page and Linux UAPI headers both use
    sigev_notify_thread_id as a public way to access this field.
    
    musl libc supports this field since 1.2.2[0], and glibc plans to
    add support as well[1][2].
    
    If sigev_notify_thread_id is not available, fall back to _sigev_un._tid
    as before.
    
    [0] http://git.musl-libc.org/cgit/musl/commit/?id=7c71792e87691451f2a6b76348e83ad1889f1dcb
    [1] https://www.openwall.com/lists/musl/2019/08/01/5
    [2] https://sourceware.org/bugzilla/show_bug.cgi?id=27417
    
    Signed-off-by: Michael Forney <mforney@mforney.org>
    Message-Id: <20210526035556.7931-1-mforney@mforney.org>
    Reviewed-by: Laurent Vivier <laurent@vivier.eu>

--- linux-user/syscall.c
+++ linux-user/syscall.c
@@ -7393,6 +7393,10 @@ static inline abi_long host_to_target_timex64(abi_long target_addr,
 }
 #endif
 
+#ifndef HAVE_SIGEV_NOTIFY_THREAD_ID
+#define sigev_notify_thread_id _sigev_un._tid
+#endif
+
 static inline abi_long target_to_host_sigevent(struct sigevent *host_sevp,
                                                abi_ulong target_addr)
 {
@@ -7413,7 +7417,7 @@ static inline abi_long target_to_host_sigevent(struct sigevent *host_sevp,
     host_sevp->sigev_signo =
         target_to_host_signal(tswap32(target_sevp->sigev_signo));
     host_sevp->sigev_notify = tswap32(target_sevp->sigev_notify);
-    host_sevp->_sigev_un._tid = tswap32(target_sevp->_sigev_un._tid);
+    host_sevp->sigev_notify_thread_id = tswap32(target_sevp->_sigev_un._tid);
 
     unlock_user_struct(target_sevp, target_addr, 1);
     return 0;
