2720be6149a8813b9cc499c208d81691f0b68cf8
Michael Forney <mforney@mforney.org>
2021-05-25 21:39:28 -0700

    linux-user: Remove unnecessary static assert involving __SIGRTMAX
    Since "linux-user: fix use of SIGRTMIN" (6bc024e7), qemu removed
    use of __SIGRTMAX except for in this QEMU_BUILD_BUG_ON assert.
    Presumably, this check is to ensure that the loop in signal_table_init
    from SIGRTMIN to SIGRTMAX falls within the bounds of
    host_to_target_signal_table (_NSIG).
    
    However, _NSIG is already defined to be the one larger than the
    largest signal supported by the system (as specified in the upcoming
    POSIX revision[0]), so the check is unnecessary.
    
    musl libc does not define __SIGRTMAX, so removing this check fixes
    one of the last remaining errors when building qemu.
    
    [0] https://www.austingroupbugs.net/view.php?id=741
    
    Signed-off-by: Michael Forney <mforney@mforney.org>
    Message-Id: <20210526043928.9586-1-mforney@mforney.org>

--- linux-user/signal.c
+++ linux-user/signal.c
@@ -38,7 +38,6 @@ static void host_signal_handler(int host_signum, siginfo_t *info,
  * Signal number 0 is reserved for use as kill(pid, 0), to test whether
  * a process exists without sending it a signal.
  */
-QEMU_BUILD_BUG_ON(__SIGRTMAX + 1 != _NSIG);
 static uint8_t host_to_target_signal_table[_NSIG] = {
     [SIGHUP] = TARGET_SIGHUP,
     [SIGINT] = TARGET_SIGINT,
