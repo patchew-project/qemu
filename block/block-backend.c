88794072d37c5f550a3bbd00a641edd05da8e3b0
Paolo Bonzini <pbonzini@redhat.com>
2021-06-03 15:37:18 +0200

    block: add max_hw_transfer to BlockLimits
    For block host devices, I/O can happen through either the kernel file
    descriptor I/O system calls (preadv/pwritev, io_submit, io_uring)
    or the SCSI passthrough ioctl SG_IO.
    
    In the latter case, the size of each transfer can be limited by the
    HBA, while for file descriptor I/O the kernel is able to split and
    merge I/O in smaller pieces as needed.  Applying the HBA limits to
    file descriptor I/O results in more system calls and suboptimal
    performance, so this patch splits the max_transfer limit in two:
    max_transfer remains valid and is used in general, while max_hw_transfer
    is limited to the maximum hardware size.  max_hw_transfer can then be
    included by the scsi-generic driver in the block limits page, to ensure
    that the stricter hardware limit is used.
    
    Signed-off-by: Paolo Bonzini <pbonzini@redhat.com>
    Message-Id: <20210603133722.218465-4-pbonzini@redhat.com>

--- block/block-backend.c
+++ block/block-backend.c
@@ -1953,6 +1953,18 @@ uint32_t blk_get_request_alignment(BlockBackend *blk)
     return bs ? bs->bl.request_alignment : BDRV_SECTOR_SIZE;
 }
 
+/* Returns the maximum hardware transfer length, in bytes; guaranteed nonzero */
+uint64_t blk_get_max_hw_transfer(BlockBackend *blk)
+{
+    BlockDriverState *bs = blk_bs(blk);
+    uint64_t max = INT_MAX;
+
+    if (bs) {
+        max = MIN_NON_ZERO(bs->bl.max_hw_transfer, bs->bl.max_transfer);
+    }
+    return max;
+}
+
 /* Returns the maximum transfer length, in bytes; guaranteed nonzero */
 uint32_t blk_get_max_transfer(BlockBackend *blk)
 {
