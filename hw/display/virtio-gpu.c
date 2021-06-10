0ded4d0662c7da8b52499e686b30079830fc6f8c
Gerd Hoffmann <kraxel@redhat.com>
2021-06-10 07:57:41 +0200

    modules: add virtio-gpu module annotations
    Signed-off-by: Gerd Hoffmann <kraxel@redhat.com>
    Message-Id: <20210610055755.538119-5-kraxel@redhat.com>

--- hw/display/virtio-gpu.c
+++ hw/display/virtio-gpu.c
@@ -1419,6 +1419,7 @@ static const TypeInfo virtio_gpu_info = {
     .class_size = sizeof(VirtIOGPUClass),
     .class_init = virtio_gpu_class_init,
 };
+module_obj(TYPE_VIRTIO_GPU);
 
 static void virtio_register_types(void)
 {
