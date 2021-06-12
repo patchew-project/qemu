e3f231495b82edc153c06d1e0f3f3295b591e7e0
cyruscyliu <cyruscyliu@gmail.com>
2021-06-12 14:03:17 +0800

    Subject: [PATCH] hw/vmxnet3: fix vmxnet3 g_assert_not_reached bug
    A g_assert_not_reached of vmxnet3 can be triggered by a guest with the root
    privilege.
    Remove the VMXNET3_REG_ICR branch thus get rid of this crash.
    
    Fixes: 786fd2b0f87b ("VMXNET3 device implementation")
    Resolves: https://gitlab.com/qemu-project/qemu/-/issues/309
    Buglink: https://bugs.launchpad.net/qemu/+bug/1913923
    Signed-off-by: cyruscyliu <cyruscyliu@gmail.com>
    Message-Id: <CAAKa2jmVMvbRviMTTC8d0cPhWVmOHBNcRE-d3aTTdot4aCZBxg@mail.gmail.com>

--- hw/net/vmxnet3.c
+++ hw/net/vmxnet3.c
@@ -1786,13 +1786,6 @@ vmxnet3_io_bar1_write(void *opaque,
         vmxnet3_set_variable_mac(s, val, s->temp_mac);
         break;
 
-    /* Interrupt Cause Register */
-    case VMXNET3_REG_ICR:
-        VMW_CBPRN("Write BAR1 [VMXNET3_REG_ICR] = %" PRIx64 ", size %d",
-                  val, size);
-        g_assert_not_reached();
-        break;
-
     /* Event Cause Register */
     case VMXNET3_REG_ECR:
         VMW_CBPRN("Write BAR1 [VMXNET3_REG_ECR] = %" PRIx64 ", size %d",
