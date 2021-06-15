956f636f991ffc9f589bccf02ac1ca36accf2756
Alexey Kardashevskiy <aik@ozlabs.ru>
2021-06-15 17:06:12 +1000

    spapr: Implement Open Firmware client interface
    The PAPR platform describes an OS environment that's presented by
    a combination of a hypervisor and firmware. The features it specifies
    require collaboration between the firmware and the hypervisor.
    
    Since the beginning, the runtime component of the firmware (RTAS) has
    been implemented as a 20 byte shim which simply forwards it to
    a hypercall implemented in qemu. The boot time firmware component is
    SLOF - but a build that's specific to qemu, and has always needed to be
    updated in sync with it. Even though we've managed to limit the amount
    of runtime communication we need between qemu and SLOF, there's some,
    and it has become increasingly awkward to handle as we've implemented
    new features.
    
    This implements a boot time OF client interface (CI) which is
    enabled by a new "x-vof" pseries machine option (stands for "Virtual Open
    Firmware). When enabled, QEMU implements the custom H_OF_CLIENT hcall
    which implements Open Firmware Client Interface (OF CI). This allows
    using a smaller stateless firmware which does not have to manage
    the device tree.
    
    The new "vof.bin" firmware image is included with source code under
    pc-bios/. It also includes RTAS blob.
    
    This implements a handful of CI methods just to get -kernel/-initrd
    working. In particular, this implements the device tree fetching and
    simple memory allocator - "claim" (an OF CI memory allocator) and updates
    "/memory@0/available" to report the client about available memory.
    
    This implements changing some device tree properties which we know how
    to deal with, the rest is ignored. To allow changes, this skips
    fdt_pack() when x-vof=on as not packing the blob leaves some room for
    appending.
    
    In absence of SLOF, this assigns phandles to device tree nodes to make
    device tree traversing work.
    
    When x-vof=on, this adds "/chosen" every time QEMU (re)builds a tree.
    
    This adds basic instances support which are managed by a hash map
    ihandle -> [phandle].
    
    Before the guest started, the used memory is:
    0..e60 - the initial firmware
    8000..10000 - stack
    400000.. - kernel
    3ea0000.. - initramdisk
    
    This OF CI does not implement "interpret".
    
    Unlike SLOF, this does not format uninitialized nvram. Instead, this
    includes a disk image with pre-formatted nvram.
    
    With this basic support, this can only boot into kernel directly.
    However this is just enough for the petitboot kernel and initradmdisk to
    boot from any possible source. Note this requires reasonably recent guest
    kernel with:
    https://git.kernel.org/pub/scm/linux/kernel/git/torvalds/linux.git/commit/?id=df5be5be8735
    
    The immediate benefit is much faster booting time which especially
    crucial with fully emulated early CPU bring up environments. Also this
    may come handy when/if GRUB-in-the-userspace sees light of the day.
    
    This separates VOF and sPAPR in a hope that VOF bits may be reused by
    other POWERPC boards which do not support pSeries.
    
    This make VOF optional, it is disabled by default, add --enable-vof
    to ./configure to enable it.
    
    This assumes potential support for booting from QEMU backends
    such as blockdev or netdev without devices/drivers used.
    
    Signed-off-by: Alexey Kardashevskiy <aik@ozlabs.ru>
    Message-Id: <20210615070612.22679-1-aik@ozlabs.ru>

--- include/hw/ppc/spapr.h
+++ include/hw/ppc/spapr.h
@@ -12,6 +12,9 @@
 #include "hw/ppc/spapr_xive.h"  /* For SpaprXive */
 #include "hw/ppc/xics.h"        /* For ICSState */
 #include "hw/ppc/spapr_tpm_proxy.h"
+#ifdef CONFIG_VOF
+#include "hw/ppc/vof.h"
+#endif
 
 struct SpaprVioBus;
 struct SpaprPhbState;
@@ -180,6 +183,9 @@ struct SpaprMachineState {
     uint64_t kernel_addr;
     uint32_t initrd_base;
     long initrd_size;
+#ifdef CONFIG_VOF
+    Vof *vof;
+#endif
     uint64_t rtc_offset; /* Now used only during incoming migration */
     struct PPCTimebase tb;
     bool has_graphics;
@@ -558,7 +564,9 @@ struct SpaprMachineState {
 /* Client Architecture support */
 #define KVMPPC_H_CAS            (KVMPPC_HCALL_BASE + 0x2)
 #define KVMPPC_H_UPDATE_DT      (KVMPPC_HCALL_BASE + 0x3)
-#define KVMPPC_HCALL_MAX        KVMPPC_H_UPDATE_DT
+/* 0x4 was used for KVMPPC_H_UPDATE_PHANDLE in SLOF */
+#define KVMPPC_H_VOF_CLIENT     (KVMPPC_HCALL_BASE + 0x5)
+#define KVMPPC_HCALL_MAX        KVMPPC_H_VOF_CLIENT
 
 /*
  * The hcall range 0xEF00 to 0xEF80 is reserved for use in facilitating
@@ -956,4 +964,19 @@ bool spapr_check_pagesize(SpaprMachineState *spapr, hwaddr pagesize,
 void spapr_set_all_lpcrs(target_ulong value, target_ulong mask);
 hwaddr spapr_get_rtas_addr(void);
 bool spapr_memory_hot_unplug_supported(SpaprMachineState *spapr);
+
+#ifdef CONFIG_VOF
+void spapr_vof_reset(SpaprMachineState *spapr, void *fdt,
+                     target_ulong *stack_ptr, Error **errp);
+void spapr_vof_quiesce(MachineState *ms);
+bool spapr_vof_setprop(MachineState *ms, const char *path, const char *propname,
+                       void *val, int vallen);
+target_ulong spapr_h_vof_client(PowerPCCPU *cpu, SpaprMachineState *spapr,
+                                target_ulong opcode, target_ulong *args);
+target_ulong spapr_vof_client_architecture_support(MachineState *ms,
+                                                   CPUState *cs,
+                                                   target_ulong ovec_addr);
+void spapr_vof_client_dt_finalize(SpaprMachineState *spapr, void *fdt);
+#endif
+
 #endif /* HW_SPAPR_H */
