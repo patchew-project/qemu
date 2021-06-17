91b7e3ec5e7828b23e580bbb59fddc7d451518af
Max Reitz <mreitz@redhat.com>
2021-06-17 17:52:44 +0200

    block/file-posix: Do not force-cap *pnum
    bdrv_co_block_status() does it for us, we do not need to do it here.
    
    The advantage of not capping *pnum is that bdrv_co_block_status() can
    cache larger data regions than requested by its caller.
    
    Signed-off-by: Max Reitz <mreitz@redhat.com>
    Message-Id: <20210617155247.442150-4-mreitz@redhat.com>

--- block/file-posix.c
+++ block/file-posix.c
@@ -2689,7 +2689,8 @@ static int find_allocation(BlockDriverState *bs, off_t start,
  * the specified offset) that are known to be in the same
  * allocated/unallocated state.
  *
- * 'bytes' is the max value 'pnum' should be set to.
+ * 'bytes' is a soft cap for 'pnum'.  If the information is free, 'pnum' may
+ * well exceed it.
  */
 static int coroutine_fn raw_co_block_status(BlockDriverState *bs,
                                             bool want_zero,
@@ -2727,7 +2728,7 @@ static int coroutine_fn raw_co_block_status(BlockDriverState *bs,
     } else if (data == offset) {
         /* On a data extent, compute bytes to the end of the extent,
          * possibly including a partial sector at EOF. */
-        *pnum = MIN(bytes, hole - offset);
+        *pnum = hole - offset;
 
         /*
          * We are not allowed to return partial sectors, though, so
@@ -2746,7 +2747,7 @@ static int coroutine_fn raw_co_block_status(BlockDriverState *bs,
     } else {
         /* On a hole, compute bytes to the beginning of the next extent.  */
         assert(hole == offset);
-        *pnum = MIN(bytes, data - offset);
+        *pnum = data - offset;
         ret = BDRV_BLOCK_ZERO;
     }
     *map = offset;
