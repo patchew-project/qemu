4318c6ce672c2e909bf5f451d657f90cab894fb0
Rao, Lei <lei.rao@intel.com>
2021-06-17 10:47:15 +0800

    Optimized the function of fill_connection_key.
    Remove some unnecessary code to improve the performance of
    the filter-rewriter module.
    
    Signed-off-by: Lei Rao <lei.rao@intel.com>
    Message-Id: <1623898035-18533-8-git-send-email-lei.rao@intel.com>

--- net/colo.h
+++ net/colo.h
@@ -89,9 +89,9 @@ typedef struct Connection {
 uint32_t connection_key_hash(const void *opaque);
 int connection_key_equal(const void *opaque1, const void *opaque2);
 int parse_packet_early(Packet *pkt);
-void extract_ip_and_port(uint32_t tmp_ports, ConnectionKey *key, Packet *pkt);
-void fill_connection_key(Packet *pkt, ConnectionKey *key);
-void reverse_connection_key(ConnectionKey *key);
+void extract_ip_and_port(uint32_t tmp_ports, ConnectionKey *key,
+                         Packet *pkt, int reverse);
+void fill_connection_key(Packet *pkt, ConnectionKey *key, int reverse);
 Connection *connection_new(ConnectionKey *key);
 void connection_destroy(void *opaque);
 Connection *connection_get(GHashTable *connection_track_table,
