97087396243e85c165c86cc55140da7056e20af0
Igor Mammedov <imammedo@redhat.com>
2021-06-25 05:17:27 -0400

    tests: acpi: whitelist expected tables for acpi/q35/numamem testcase
    Signed-off-by: Igor Mammedov <imammedo@redhat.com>
    Message-Id: <20210625091818.1047980-4-imammedo@redhat.com>

--- tests/qtest/bios-tables-test-allowed-diff.h
+++ tests/qtest/bios-tables-test-allowed-diff.h
@@ -1,1 +1,5 @@
 /* List of comma-separated changed AML files to ignore */
+"tests/data/acpi/q35/DSDT.numamem",
+"tests/data/acpi/q35/SRAT.numamem",
+"tests/data/acpi/q35/FACP",
+"tests/data/acpi/q35/APIC",
