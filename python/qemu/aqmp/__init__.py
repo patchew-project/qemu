7abd9766ac8c6efe061be5677b9100e3780f69f8
John Snow <jsnow@redhat.com>
2021-07-01 00:13:05 -0400

    python/aqmp: add QMP Message format
    The Message class is here primarily to serve as a solid type to use for
    mypy static typing for unambiguous annotation and documentation.
    
    We can also stuff JSON serialization and deserialization into this class
    itself so it can be re-used even outside this infrastructure.
    
    Signed-off-by: John Snow <jsnow@redhat.com>
    Message-Id: <20210701041313.1696009-13-jsnow@redhat.com>
    Acked-by: Stefan Hajnoczi <stefanha@redhat.com>
    Based-on: <20210701020921.1679468-1-jsnow@redhat.com>

--- python/qemu/aqmp/__init__.py
+++ python/qemu/aqmp/__init__.py
@@ -22,12 +22,14 @@ tutorial on managing QMP events.
 # the COPYING file in the top-level directory.
 
 from .error import AQMPError, MultiException
+from .message import Message
 from .protocol import ConnectError, Runstate
 
 
 # The order of these fields impact the Sphinx documentation order.
 __all__ = (
-    # Classes
+    # Classes, most to least important
+    'Message',
     'Runstate',
 
     # Exceptions, most generic to most explicit
