f705bd3d039a87d5e00a62324a03f67558913beb
Peter Lieven <pl@kamp.de>
2021-07-02 11:09:32 +0200

    block/rbd: update s->image_size in qemu_rbd_getlength
    while at it just call rbd_get_size and avoid rbd_stat.
    
    Signed-off-by: Peter Lieven <pl@kamp.de>
    Message-Id: <20210702090935.15300-4-pl@kamp.de>

--- block/rbd.c
+++ block/rbd.c
@@ -968,15 +968,14 @@ static int qemu_rbd_getinfo(BlockDriverState *bs, BlockDriverInfo *bdi)
 static int64_t qemu_rbd_getlength(BlockDriverState *bs)
 {
     BDRVRBDState *s = bs->opaque;
-    rbd_image_info_t info;
     int r;
 
-    r = rbd_stat(s->image, &info, sizeof(info));
+    r = rbd_get_size(s->image, &s->image_size);
     if (r < 0) {
         return r;
     }
 
-    return info.size;
+    return s->image_size;
 }
 
 static int coroutine_fn qemu_rbd_co_truncate(BlockDriverState *bs,
