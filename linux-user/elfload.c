50a43bde005b6d48458a31868394eb6df1b8560d
Richard Henderson <richard.henderson@linaro.org>
2021-07-06 16:49:28 -0700

    linux-user/riscv: Add vdso and use it for sigreturn
    Building the vdso itself is not actually wired up to anything, since
    we require a cross-compiler.  Just check in those files for now.
    
    This fixes a bug wrt libgcc fallback unwinding.  It expects the stack
    pointer to point to the siginfo_t, whereas we had inexplicably placed
    our private signal trampoline at the start of the signal frame instead
    of the end.  Now moot because we have removed it from the stack
    frame entirely.
    
    Cc: qemu-riscv@nongnu.org
    Signed-off-by: Richard Henderson <richard.henderson@linaro.org>
    Message-Id: <20210706234932.356913-33-richard.henderson@linaro.org>

--- linux-user/elfload.c
+++ linux-user/elfload.c
@@ -1484,9 +1484,13 @@ static void elf_core_copy_regs(target_elf_gregset_t *regs,
 
 #ifdef TARGET_RISCV32
 #define ELF_CLASS ELFCLASS32
+#include "vdso-32.c.inc"
 #else
 #define ELF_CLASS ELFCLASS64
+#include "vdso-64.c.inc"
 #endif
+
+#define vdso_image_info()    &vdso_image_info
 
 static inline void init_thread(struct target_pt_regs *regs,
                                struct image_info *infop)
