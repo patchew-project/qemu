300151f445e4e69aa475c42c9814908f7ed04069
Stefano Garzarella <sgarzare@redhat.com>
2021-07-07 17:00:19 +0200

    linux-aio: limit the batch size using `aio-max-batch` parameter
    When there are multiple queues attached to the same AIO context,
    some requests may experience high latency, since in the worst case
    the AIO engine queue is only flushed when it is full (MAX_EVENTS) or
    there are no more queues plugged.
    
    Commit 2558cb8dd4 ("linux-aio: increasing MAX_EVENTS to a larger
    hardcoded value") changed MAX_EVENTS from 128 to 1024, to increase
    the number of in-flight requests. But this change also increased
    the potential maximum batch to 1024 elements.
    
    When there is a single queue attached to the AIO context, the issue
    is mitigated from laio_io_unplug() that will flush the queue every
    time is invoked since there can't be others queue plugged.
    
    Let's use the new `aio-max-batch` IOThread parameter to mitigate
    this issue, limiting the number of requests in a batch.
    
    We also define a default value (32): this value is obtained running
    some benchmarks and it represents a good tradeoff between the latency
    increase while a request is queued and the cost of the io_submit(2)
    system call.
    
    Signed-off-by: Stefano Garzarella <sgarzare@redhat.com>
    Message-Id: <20210707150019.201442-4-sgarzare@redhat.com>

--- block/linux-aio.c
+++ block/linux-aio.c
@@ -28,6 +28,9 @@
  */
 #define MAX_EVENTS 1024
 
+/* Maximum number of requests in a batch. (default value) */
+#define DEFAULT_MAX_BATCH 32
+
 struct qemu_laiocb {
     Coroutine *co;
     LinuxAioState *ctx;
@@ -351,6 +354,7 @@ static int laio_do_submit(int fd, struct qemu_laiocb *laiocb, off_t offset,
     LinuxAioState *s = laiocb->ctx;
     struct iocb *iocbs = &laiocb->iocb;
     QEMUIOVector *qiov = laiocb->qiov;
+    int64_t max_batch = s->aio_context->aio_max_batch ?: DEFAULT_MAX_BATCH;
 
     switch (type) {
     case QEMU_AIO_WRITE:
@@ -371,7 +375,7 @@ static int laio_do_submit(int fd, struct qemu_laiocb *laiocb, off_t offset,
     s->io_q.in_queue++;
     if (!s->io_q.blocked &&
         (!s->io_q.plugged ||
-         s->io_q.in_flight + s->io_q.in_queue >= MAX_EVENTS)) {
+         s->io_q.in_queue >= max_batch)) {
         ioq_submit(s);
     }
 
