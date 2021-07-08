4ec95b3730662d7d5ba52614d0830487d118d2f9
Igor Mammedov <imammedo@redhat.com>
2021-07-08 11:46:05 -0400

    acpi: x86: set enabled when composing _MAT entries
    Instead of composing disabled _MAT entry and then later on
    patching it to enabled for hotpluggbale CPUs in DSDT,
    set it to enabled at the time _MAT entry is built.
    
    It will allow to drop usage of packed structures in
    following patches when build_madt() is switched to use
    build_append_int_noprefix() API.
    
    Signed-off-by: Igor Mammedov <imammedo@redhat.com>
    Message-Id: <20210708154617.1538485-24-imammedo@redhat.com>

--- hw/acpi/acpi-x86-stub.c
+++ hw/acpi/acpi-x86-stub.c
@@ -2,6 +2,7 @@
 #include "hw/i386/pc.h"
 
 void pc_madt_cpu_entry(AcpiDeviceIf *adev, int uid,
-                       const CPUArchIdList *apic_ids, GArray *entry)
+                       const CPUArchIdList *apic_ids, GArray *entry,
+                       bool force_enabled)
 {
 }
