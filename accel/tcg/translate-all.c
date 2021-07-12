53cfc04efda9b4790667e646c2002e73b1041d63
Richard Henderson <richard.henderson@linaro.org>
2021-07-12 08:39:55 -0700

    accel/tcg: Reduce CF_COUNT_MASK to match TCG_MAX_INSNS
    The space reserved for CF_COUNT_MASK was overly large.
    Reduce to free up cflags bits and eliminate an extra test.
    
    Signed-off-by: Richard Henderson <richard.henderson@linaro.org>
    Message-Id: <20210712154004.1410832-2-richard.henderson@linaro.org>
    Tested-by: Mark Cave-Ayland <mark.cave-ayland@ilande.co.uk>

--- accel/tcg/translate-all.c
+++ accel/tcg/translate-all.c
@@ -1430,9 +1430,8 @@ TranslationBlock *tb_gen_code(CPUState *cpu,
     if (max_insns == 0) {
         max_insns = CF_COUNT_MASK;
     }
-    if (max_insns > TCG_MAX_INSNS) {
-        max_insns = TCG_MAX_INSNS;
-    }
+    QEMU_BUILD_BUG_ON(CF_COUNT_MASK + 1 != TCG_MAX_INSNS);
+
     if (cpu->singlestep_enabled || singlestep) {
         max_insns = 1;
     }
