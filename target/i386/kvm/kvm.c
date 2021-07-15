a425bb1016908af246a3caf785dd6e4ea81483e4
Dov Murik <dovmurik@linux.ibm.com>
2021-07-15 07:16:56 +0000

    target/i386/kvm: Remove unused workaround for kernel header typo
    Commit 6f131f13e68d ("kvm: support -overcommit cpu-pm=on|off",
    2018-06-22) introduced a workaround for a kernel header typo
    (KVM_X86_DISABLE_EXITS_HTL instead of KVM_X86_DISABLE_EXITS_HLT).
    
    The problematic kernel header was fixed by commit 77d361b13c19
    ("linux-headers: Update to kernel mainline commit b357bf602",
    2018-06-22).  The wrong name doesn't appear anywhere in the QEMU code.
    
    Remove the unused typo workaround.
    
    Signed-off-by: Dov Murik <dovmurik@linux.ibm.com>
    Message-Id: <20210715071656.3592604-1-dovmurik@linux.ibm.com>

--- target/i386/kvm/kvm.c
+++ target/i386/kvm/kvm.c
@@ -2327,10 +2327,6 @@ int kvm_arch_init(MachineState *ms, KVMState *s)
         int disable_exits = kvm_check_extension(s, KVM_CAP_X86_DISABLE_EXITS);
         int ret;
 
-/* Work around for kernel header with a typo. TODO: fix header and drop. */
-#if defined(KVM_X86_DISABLE_EXITS_HTL) && !defined(KVM_X86_DISABLE_EXITS_HLT)
-#define KVM_X86_DISABLE_EXITS_HLT KVM_X86_DISABLE_EXITS_HTL
-#endif
         if (disable_exits) {
             disable_exits &= (KVM_X86_DISABLE_EXITS_MWAIT |
                               KVM_X86_DISABLE_EXITS_HLT |
