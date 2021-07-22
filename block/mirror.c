452b6b910424e333cc7a0401d28c81a61de6caa1
Max Reitz <mreitz@redhat.com>
2021-07-22 14:26:24 +0200

    jobs: Give Job.force_cancel more meaning
    We largely have two cancel modes for jobs:
    
    First, there is actual cancelling.  The job is terminated as soon as
    possible, without trying to reach a consistent result.
    
    Second, we have mirror in the READY state.  Technically, the job is not
    really cancelled, but it just is a different completion mode.  The job
    can still run for an indefinite amount of time while it tries to reach a
    consistent result.
    
    We want to be able to clearly distinguish which cancel mode a job is in
    (when it has been cancelled).  We can use Job.force_cancel for this, but
    right now it only reflects cancel requests from the user with
    force=true, but clearly, jobs that do not even distinguish between
    force=false and force=true are effectively always force-cancelled.
    
    So this patch has Job.force_cancel signify whether the job will
    terminate as soon as possible (force_cancel=true) or whether it will
    effectively remain running despite being "cancelled"
    (force_cancel=false).
    
    To this end, we let jobs that provide JobDriver.cancel() tell the
    generic job code whether they will terminate as soon as possible or not,
    and for jobs that do not provide that method we assume they will.
    
    Signed-off-by: Max Reitz <mreitz@redhat.com>
    Message-Id: <20210722122627.29605-4-mreitz@redhat.com>

--- block/mirror.c
+++ block/mirror.c
@@ -1089,9 +1089,7 @@ static int coroutine_fn mirror_run(Job *job, Error **errp)
         }
         trace_mirror_before_sleep(s, cnt, s->synced, delay_ns);
         job_sleep_ns(&s->common.job, delay_ns);
-        if (job_is_cancelled(&s->common.job) &&
-            (!s->synced || s->common.job.force_cancel))
-        {
+        if (job_is_cancelled(&s->common.job) && s->common.job.force_cancel) {
             break;
         }
         s->last_pause_ns = qemu_clock_get_ns(QEMU_CLOCK_REALTIME);
@@ -1103,7 +1101,7 @@ immediate_exit:
          * or it was cancelled prematurely so that we do not guarantee that
          * the target is a copy of the source.
          */
-        assert(ret < 0 || ((s->common.job.force_cancel || !s->synced) &&
+        assert(ret < 0 || (s->common.job.force_cancel &&
                job_is_cancelled(&s->common.job)));
         assert(need_drain);
         mirror_wait_for_all_io(s);
@@ -1189,14 +1187,27 @@ static bool mirror_drained_poll(BlockJob *job)
     return !!s->in_flight;
 }
 
-static void mirror_cancel(Job *job, bool force)
+static bool mirror_cancel(Job *job, bool force)
 {
     MirrorBlockJob *s = container_of(job, MirrorBlockJob, common.job);
     BlockDriverState *target = blk_bs(s->target);
 
-    if (force || !job_is_ready(job)) {
+    /*
+     * Before the job is READY, we treat any cancellation like a
+     * force-cancellation.
+     */
+    force = force || !job_is_ready(job);
+
+    if (force) {
         bdrv_cancel_in_flight(target);
     }
+    return force;
+}
+
+static bool commit_active_cancel(Job *job, bool force)
+{
+    /* Same as above in mirror_cancel() */
+    return force || !job_is_ready(job);
 }
 
 static const BlockJobDriver mirror_job_driver = {
@@ -1226,6 +1237,7 @@ static const BlockJobDriver commit_active_job_driver = {
         .abort                  = mirror_abort,
         .pause                  = mirror_pause,
         .complete               = mirror_complete,
+        .cancel                 = commit_active_cancel,
     },
     .drained_poll           = mirror_drained_poll,
 };
