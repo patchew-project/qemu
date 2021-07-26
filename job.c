ddee1a41241d68ea554e3fa8c55361e6d7f8027f
Max Reitz <mreitz@redhat.com>
2021-07-26 16:46:11 +0200

    job: Add job_cancel_requested()
    Most callers of job_is_cancelled() actually want to know whether the job
    is on its way to immediate termination.  For example, we refuse to pause
    jobs that are cancelled; but this only makes sense for jobs that are
    really actually cancelled.
    
    A mirror job that is cancelled during READY with force=false should
    absolutely be allowed to pause.  This "cancellation" (which is actually
    a kind of completion) may take an indefinite amount of time, and so
    should behave like any job during normal operation.  For example, with
    on-target-error=stop, the job should stop on write errors.  (In
    contrast, force-cancelled jobs should not get write errors, as they
    should just terminate and not do further I/O.)
    
    Therefore, redefine job_is_cancelled() to only return true for jobs that
    are force-cancelled (which as of HEAD^ means any job that interprets the
    cancellation request as a request for immediate termination), and add
    job_cancel_request() as the general variant, which returns true for any
    jobs which have been requested to be cancelled, whether it be
    immediately or after an arbitrarily long completion phase.
    
    Buglink: https://gitlab.com/qemu-project/qemu/-/issues/462
    Signed-off-by: Max Reitz <mreitz@redhat.com>
    Message-Id: <20210726144613.954844-6-mreitz@redhat.com>
    Reviewed-by: Kevin Wolf <kwolf@redhat.com>

--- job.c
+++ job.c
@@ -217,6 +217,11 @@ const char *job_type_str(const Job *job)
 
 bool job_is_cancelled(Job *job)
 {
+    return job->cancelled && job->force_cancel;
+}
+
+bool job_cancel_requested(Job *job)
+{
     return job->cancelled;
 }
 
@@ -1015,7 +1020,7 @@ void job_complete(Job *job, Error **errp)
     if (job_apply_verb(job, JOB_VERB_COMPLETE, errp)) {
         return;
     }
-    if (job_is_cancelled(job) || !job->driver->complete) {
+    if (job_cancel_requested(job) || !job->driver->complete) {
         error_setg(errp, "The active block job '%s' cannot be completed",
                    job->id);
         return;
