dcbb129a4d1272a2da89d773ce162dfb1ba2d3f1
Philippe Mathieu-Daudé <f4bug@amsat.org>
2021-08-02 01:59:24 +0200

    target/mips: Convert Vr54xx MACC* opcodes to decodetree
    Convert the following Integer Multiply-Accumulate opcodes:
    
     * MACC         Multiply, accumulate, and move LO
     * MACCHI       Multiply, accumulate, and move HI
     * MACCHIU      Unsigned multiply, accumulate, and move HI
     * MACCU        Unsigned multiply, accumulate, and move LO
    
    Since all opcodes are generated using the same pattern, we
    add the gen_helper_mult_acc_t typedef and MULT_ACC() macro
    to remove boilerplate code.
    
    Signed-off-by: Philippe Mathieu-Daudé <f4bug@amsat.org>
    Message-Id: <20210801235926.3178085-4-f4bug@amsat.org>

--- target/mips/tcg/vr54xx_translate.c
+++ target/mips/tcg/vr54xx_translate.c
@@ -17,3 +17,43 @@
 
 /* Include the auto-generated decoder. */
 #include "decode-vr54xx.c.inc"
+
+/*
+ * Integer Multiply-Accumulate Instructions
+ *
+ * MACC         Multiply, accumulate, and move LO
+ * MACCHI       Multiply, accumulate, and move HI
+ * MACCHIU      Unsigned multiply, accumulate, and move HI
+ * MACCU        Unsigned multiply, accumulate, and move LO
+ */
+
+typedef void gen_helper_mult_acc_t(TCGv, TCGv_ptr, TCGv, TCGv);
+
+static bool trans_mult_acc(DisasContext *ctx, arg_r *a,
+                           gen_helper_mult_acc_t *gen_helper_mult_acc)
+{
+    TCGv t0 = tcg_temp_new();
+    TCGv t1 = tcg_temp_new();
+
+    gen_load_gpr(t0, a->rs);
+    gen_load_gpr(t1, a->rt);
+
+    gen_helper_mult_acc(t0, cpu_env, t0, t1);
+
+    gen_store_gpr(t0, a->rd);
+
+    tcg_temp_free(t0);
+    tcg_temp_free(t1);
+
+    return false;
+}
+
+#define MULT_ACC(opcode, gen_helper) \
+static bool trans_##opcode(DisasContext *ctx, arg_r *a) \
+{ \
+    return trans_mult_acc(ctx, a, gen_helper); \
+}
+MULT_ACC(MACC,      gen_helper_macc);
+MULT_ACC(MACCHI,    gen_helper_macchi);
+MULT_ACC(MACCHIU,   gen_helper_macchiu);
+MULT_ACC(MACCU,     gen_helper_maccu);
