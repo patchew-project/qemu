d458509d7897c8116517009bedf01d750043def7
Richard Henderson <richard.henderson@linaro.org>
2021-08-02 18:13:49 -1000

    hw/core: Make do_unaligned_access noreturn
    While we may have had some thought of allowing system-mode
    to return from this hook, we have no guests that require this.
    
    Signed-off-by: Richard Henderson <richard.henderson@linaro.org>
    Message-Id: <20210803041443.55452-2-richard.henderson@linaro.org>

--- target/sh4/cpu.h
+++ target/sh4/cpu.h
@@ -211,8 +211,8 @@ hwaddr superh_cpu_get_phys_page_debug(CPUState *cpu, vaddr addr);
 int superh_cpu_gdb_read_register(CPUState *cpu, GByteArray *buf, int reg);
 int superh_cpu_gdb_write_register(CPUState *cpu, uint8_t *buf, int reg);
 void superh_cpu_do_unaligned_access(CPUState *cpu, vaddr addr,
-                                    MMUAccessType access_type,
-                                    int mmu_idx, uintptr_t retaddr);
+                                    MMUAccessType access_type, int mmu_idx,
+                                    uintptr_t retaddr) QEMU_NORETURN;
 
 void sh4_translate_init(void);
 int cpu_sh4_signal_handler(int host_signum, void *pinfo,
