969d5f355c6c951d3f2eebd85c691048deb36996
Vladimir Sementsov-Ogievskiy <vsementsov@virtuozzo.com>
2021-08-04 12:37:47 +0300

    block/block-copy: introduce block_copy_set_copy_opts()
    We'll need a possibility to set compress and use_copy_range options
    after initialization of the state. So make corresponding part of
    block_copy_state_new() separate and public.
    
    Signed-off-by: Vladimir Sementsov-Ogievskiy <vsementsov@virtuozzo.com>
    Message-Id: <20210804093813.20688-8-vsementsov@virtuozzo.com>

--- include/block/block-copy.h
+++ include/block/block-copy.h
@@ -28,6 +28,8 @@ BlockCopyState *block_copy_state_new(BdrvChild *source, BdrvChild *target,
                                      int64_t cluster_size, bool use_copy_range,
                                      bool compress, Error **errp);
 
+void block_copy_set_copy_opts(BlockCopyState *s, bool use_copy_range,
+                              bool compress);
 void block_copy_set_progress_meter(BlockCopyState *s, ProgressMeter *pm);
 
 void block_copy_state_free(BlockCopyState *s);
