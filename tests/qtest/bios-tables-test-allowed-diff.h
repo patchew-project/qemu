ad5ae29809ab2d3a5737a348f68523525bdfd99f
Eric DeVolder <eric.devolder@oracle.com>
2021-08-05 18:30:30 -0400

    ACPI ERST: bios-tables-test.c steps 1 and 2
    Following the guidelines in tests/qtest/bios-tables-test.c, this
    change adds empty placeholder files per step 1 for the new ERST
    table, and excludes resulting changed files in bios-tables-test-allowed-diff.h
    per step 2.
    
    Signed-off-by: Eric DeVolder <eric.devolder@oracle.com>
    Message-Id: <1628202639-16361-2-git-send-email-eric.devolder@oracle.com>
    Suggested-by: Konrad Wilk <konrad.wilk@oracle.com>

--- tests/qtest/bios-tables-test-allowed-diff.h
+++ tests/qtest/bios-tables-test-allowed-diff.h
@@ -1,1 +1,7 @@
 /* List of comma-separated changed AML files to ignore */
+"tests/data/acpi/pc/ERST",
+"tests/data/acpi/q35/ERST",
+"tests/data/acpi/microvm/ERST",
+"tests/data/acpi/pc/DSDT",
+"tests/data/acpi/q35/DSDT",
+"tests/data/acpi/microvm/DSDT",
