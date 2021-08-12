fa36805d3681d0bae16668bfe1d4cf10fb035fe3
Peter Maydell <peter.maydell@linaro.org>
2021-08-12 10:33:40 +0100

    clock: Provide builtin multiplier/divider
    It is quite common for a clock tree to involve possibly programmable
    clock multipliers or dividers, where the frequency of a clock is for
    instance divided by 8 to produce a slower clock to feed to a
    particular device.
    
    Currently we provide no convenient mechanism for modelling this.  You
    can implement it by having an input Clock and an output Clock, and
    manually setting the period of the output clock in the period-changed
    callback of the input clock, but that's quite clunky.
    
    This patch adds support in the Clock objects themselves for setting a
    multiplier or divider.  The effect of setting this on a clock is that
    when the clock's period is changed, all the children of the clock are
    set to period * multiplier / divider, rather than being set to the
    same period as the parent clock.
    
    Signed-off-by: Peter Maydell <peter.maydell@linaro.org>
    Message-Id: <20210812093356.1946-10-peter.maydell@linaro.org>

--- hw/core/clock-vmstate.c
+++ hw/core/clock-vmstate.c
@@ -14,6 +14,24 @@
 #include "migration/vmstate.h"
 #include "hw/clock.h"
 
+static bool muldiv_needed(void *opaque)
+{
+    Clock *clk = opaque;
+
+    return clk->multiplier != 1 || clk->divider != 1;
+}
+
+const VMStateDescription vmstate_muldiv = {
+    .name = "clock/muldiv",
+    .version_id = 1,
+    .minimum_version_id = 1,
+    .needed = muldiv_needed,
+    .fields = (VMStateField[]) {
+        VMSTATE_UINT32(multiplier, Clock),
+        VMSTATE_UINT32(divider, Clock),
+    },
+};
+
 const VMStateDescription vmstate_clock = {
     .name = "clock",
     .version_id = 0,
@@ -21,5 +39,9 @@ const VMStateDescription vmstate_clock = {
     .fields = (VMStateField[]) {
         VMSTATE_UINT64(period, Clock),
         VMSTATE_END_OF_LIST()
-    }
+    },
+    .subsections = (const VMStateDescription*[]) {
+        &vmstate_muldiv,
+        NULL
+    },
 };
