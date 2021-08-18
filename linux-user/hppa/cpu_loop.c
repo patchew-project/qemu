4b16e5e422866a13325dfbf2b758681066b01baa
Richard Henderson <richard.henderson@linaro.org>
2021-08-18 09:18:20 -1000

    target/hppa: Implement do_unaligned_access for user-only
    Signed-off-by: Richard Henderson <richard.henderson@linaro.org>
    Message-Id: <20210818191920.390759-7-richard.henderson@linaro.org>

--- linux-user/hppa/cpu_loop.c
+++ linux-user/hppa/cpu_loop.c
@@ -161,7 +161,7 @@ void cpu_loop(CPUHPPAState *env)
         case EXCP_UNALIGN:
             info.si_signo = TARGET_SIGBUS;
             info.si_errno = 0;
-            info.si_code = 0;
+            info.si_code = TARGET_BUS_ADRALN;
             info._sifields._sigfault._addr = env->cr[CR_IOR];
             queue_signal(env, info.si_signo, QEMU_SI_FAULT, &info);
             break;
