60c5c1a6b663bbfa5237c47dc20f4bd890b586d8
Richard Henderson <richard.henderson@linaro.org>
2021-08-18 09:18:16 -1000

    hw/core: Make do_unaligned_access noreturn
    While we may have had some thought of allowing system-mode
    to return from this hook, we have no guests that require this.
    
    Reviewed-by: Alex Bennée <alex.bennee@linaro.org>
    Reviewed-by: Philippe Mathieu-Daudé <f4bug@amsat.org>
    Signed-off-by: Richard Henderson <richard.henderson@linaro.org>
    Message-Id: <20210818191920.390759-3-richard.henderson@linaro.org>

--- target/microblaze/cpu.h
+++ target/microblaze/cpu.h
@@ -359,7 +359,7 @@ void mb_cpu_do_interrupt(CPUState *cs);
 bool mb_cpu_exec_interrupt(CPUState *cs, int int_req);
 void mb_cpu_do_unaligned_access(CPUState *cs, vaddr vaddr,
                                 MMUAccessType access_type,
-                                int mmu_idx, uintptr_t retaddr);
+                                int mmu_idx, uintptr_t retaddr) QEMU_NORETURN;
 void mb_cpu_dump_state(CPUState *cpu, FILE *f, int flags);
 hwaddr mb_cpu_get_phys_page_attrs_debug(CPUState *cpu, vaddr addr,
                                         MemTxAttrs *attrs);
