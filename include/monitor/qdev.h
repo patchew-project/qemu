c43f22ec55b55ebb5af85506c93f7f2e6904820f
Peter Xu <peterx@redhat.com>
2021-08-18 15:43:16 -0400

    qdev: Export qdev_get_device_class()
    It'll be used outside the current source file.
    
    Signed-off-by: Peter Xu <peterx@redhat.com>
    Message-Id: <20210818194316.110942-1-peterx@redhat.com>

--- include/monitor/qdev.h
+++ include/monitor/qdev.h
@@ -2,12 +2,14 @@
 #define MONITOR_QDEV_H
 
 /*** monitor commands ***/
+#include "hw/qdev-core.h"
 
 void hmp_info_qtree(Monitor *mon, const QDict *qdict);
 void hmp_info_qdm(Monitor *mon, const QDict *qdict);
 void qmp_device_add(QDict *qdict, QObject **ret_data, Error **errp);
 
 int qdev_device_help(QemuOpts *opts);
+DeviceClass *qdev_get_device_class(const char **driver, Error **errp);
 DeviceState *qdev_device_add(QemuOpts *opts, Error **errp);
 void qdev_set_id(DeviceState *dev, const char *id);
 
