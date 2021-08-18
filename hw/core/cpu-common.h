5ae3aff480a594ae8ed51692a7f78010d0c53f92
Richard Henderson <richard.henderson@linaro.org>
2021-08-18 09:19:04 -1000

    hw/core/cpu: Move cpu properties to cpu-sysemu.c
    The comment in cpu-common.c is absolutely correct, we can't
    rely on the ifdef in a file built once.  This was only "working"
    because we used ifndef.
    
    Signed-off-by: Richard Henderson <richard.henderson@linaro.org>
    Message-Id: <20210818191920.390759-51-richard.henderson@linaro.org>

---
+++ hw/core/cpu-common.h
@@ -0,0 +1,17 @@
+/*
+ * QEMU CPU model
+ *
+ * Copyright (c) 2014 SUSE LINUX Products GmbH
+ *
+ * SPDX-License-Identifier: GPL-2.0-or-later
+ *
+ * This work is licensed under the terms of the GNU GPL, version 2 or later.
+ * See the COPYING file in the top-level directory.
+ */
+
+#ifndef HW_CORE_CPU_COMMON_H
+#define HW_CORE_CPU_COMMON_H
+
+void cpu_class_init_props(DeviceClass *dc);
+
+#endif
