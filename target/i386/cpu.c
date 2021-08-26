cc1304ed2dc4f8728ad19f0991ec0d0795106c4c
Michael Roth <michael.roth@amd.com>
2021-08-26 17:26:23 -0500

    target/i386: set SEV-SNP CPUID bit when SNP enabled
    SNP guests will rely on this bit to determine certain feature support.
    
    Signed-off-by: Michael Roth <michael.roth@amd.com>
    Message-Id: <20210826222627.3556-9-michael.roth@amd.com>

--- target/i386/cpu.c
+++ target/i386/cpu.c
@@ -5619,6 +5619,7 @@ void cpu_x86_cpuid(CPUX86State *env, uint32_t index, uint32_t count,
     case 0x8000001F:
         *eax = sev_enabled() ? 0x2 : 0;
         *eax |= sev_es_enabled() ? 0x8 : 0;
+        *eax |= sev_snp_enabled() ? 0x10 : 0;
         *ebx = sev_get_cbit_position();
         *ebx |= sev_get_reduced_phys_bits() << 6;
         *ecx = 0;
