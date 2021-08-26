91e4069922274f7c728208ec377270de0ce1da8b
Warner Losh <imp@FreeBSD.org>
2021-08-26 15:11:50 -0600

    bsd-user: Make cpu_model and cpu_type visible to all of main.c
    cpu_model and cpu_type will be used future commits, so move them from
    main() scoped to file scoped.
    
    Signed-off-by: Warner Losh <imp@bsdimp.com>
    Acked-by: Richard Henderson <richard.henderson@linaro.org>
    Message-Id: <20210826211201.98877-33-imp@bsdimp.com>

--- bsd-user/main.c
+++ bsd-user/main.c
@@ -52,6 +52,8 @@
 int singlestep;
 unsigned long mmap_min_addr;
 uintptr_t guest_base;
+static const char *cpu_model;
+static const char *cpu_type;
 bool have_guest_base;
 unsigned long reserved_va;
 
@@ -198,8 +200,6 @@ static void save_proc_pathname(char *argv0)
 int main(int argc, char **argv)
 {
     const char *filename;
-    const char *cpu_model;
-    const char *cpu_type;
     const char *log_file = NULL;
     const char *log_mask = NULL;
     struct target_pt_regs regs1, *regs = &regs1;
