ce461bad0d8871c04737a798733e95600fa76717
Pierre Morel <pmorel@linux.ibm.com>
2021-08-27 12:50:49 +0200

    virtio: Linux: Update of virtio_ids
    The virtio IDs depends on Linux tree...
    
    Signed-off-by: Pierre Morel <pmorel@linux.ibm.com>
    Message-Id: <1630061450-18744-2-git-send-email-pmorel@linux.ibm.com>

--- include/standard-headers/linux/virtio_ids.h
+++ include/standard-headers/linux/virtio_ids.h
@@ -55,6 +55,7 @@
 #define VIRTIO_ID_FS			26 /* virtio filesystem */
 #define VIRTIO_ID_PMEM			27 /* virtio pmem */
 #define VIRTIO_ID_MAC80211_HWSIM	29 /* virtio mac80211-hwsim */
+#define VIRTIO_ID_PONG			30 /* virtio pong */
 #define VIRTIO_ID_BT			40 /* virtio bluetooth */
 
 #endif /* _LINUX_VIRTIO_IDS_H */
