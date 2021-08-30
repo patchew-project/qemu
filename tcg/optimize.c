718ffac3a170cf1145e15b617ed05f00e0930739
Richard Henderson <richard.henderson@linaro.org>
2021-08-29 23:24:50 -0700

    tcg/optimize: Propagate sign info for bit counting
    The results are generally 6 bit unsigned values, though
    the count leading and trailing bits may produce any value
    for a zero input.
    
    Signed-off-by: Richard Henderson <richard.henderson@linaro.org>
    Message-Id: <20210830062451.639572-48-richard.henderson@linaro.org>

--- tcg/optimize.c
+++ tcg/optimize.c
@@ -1216,6 +1216,7 @@ static bool fold_count_zeros(OptContext *ctx, TCGOp *op)
         g_assert_not_reached();
     }
     ctx->z_mask = arg_info(op->args[2])->z_mask | z_mask;
+    ctx->s_mask = smask_from_zmask(z_mask);
 
     return false;
 }
@@ -1236,6 +1237,7 @@ static bool fold_ctpop(OptContext *ctx, TCGOp *op)
     default:
         g_assert_not_reached();
     }
+    ctx->s_mask = smask_from_zmask(ctx->z_mask);
     return false;
 }
 
