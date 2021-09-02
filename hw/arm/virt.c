a7598ed2cce5da72738f1ec2060e122ff30824b5
Yanan Wang <wangyanan55@huawei.com>
2021-09-02 19:03:27 +0800

    machine: Move smp_prefer_sockets to struct SMPCompatProps
    Now we have a common structure SMPCompatProps used to store information
    about SMP compatibility stuff, so we can also move smp_prefer_sockets
    there for cleaner code.
    
    No functional change intended.
    
    Signed-off-by: Yanan Wang <wangyanan55@huawei.com>
    Acked-by: David Gibson <david@gibson.dropbear.id.au>
    Reviewed-by: Andrew Jones <drjones@redhat.com>
    Message-Id: <20210902110330.18036-12-wangyanan55@huawei.com>

--- hw/arm/virt.c
+++ hw/arm/virt.c
@@ -2793,7 +2793,7 @@ static void virt_machine_6_1_options(MachineClass *mc)
 {
     virt_machine_6_2_options(mc);
     compat_props_add(mc->compat_props, hw_compat_6_1, hw_compat_6_1_len);
-    mc->smp_prefer_sockets = true;
+    mc->smp_props.prefer_sockets = true;
 }
 DEFINE_VIRT_MACHINE(6, 1)
 
