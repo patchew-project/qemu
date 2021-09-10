174843408da4f5fc15ebc9469f809c8bb9a91f94
Luis Pires <luis.pires@eldorado.org.br>
2021-09-10 08:26:18 -0300

    target/ppc: Move dtstdc[q]/dtstdg[q] to decodetree
    Move the following instructions to decodetree:
    dtstdc:  DFP Test Data Class
    dtstdcq: DFP Test Data Class Quad
    dtstdg:  DFP Test Data Group
    dtstdgq: DFP Test Data Group Quad
    
    Signed-off-by: Luis Pires <luis.pires@eldorado.org.br>
    Reviewed-by: Philippe Mathieu-Daudé <f4bug@amsat.org>
    Message-Id: <20210910112624.72748-17-luis.pires@eldorado.org.br>
    Based-on: 20210823150235.35759-1-luis.pires@eldorado.org.br

--- target/ppc/dfp_helper.c
+++ target/ppc/dfp_helper.c
@@ -547,8 +547,8 @@ uint32_t helper_##op(CPUPPCState *env, ppc_fprp_t *a, uint32_t dcm)      \
     return dfp.crbf;                                                     \
 }
 
-DFP_HELPER_TSTDC(dtstdc, 64)
-DFP_HELPER_TSTDC(dtstdcq, 128)
+DFP_HELPER_TSTDC(DTSTDC, 64)
+DFP_HELPER_TSTDC(DTSTDCQ, 128)
 
 #define DFP_HELPER_TSTDG(op, size)                                       \
 uint32_t helper_##op(CPUPPCState *env, ppc_fprp_t *a, uint32_t dcm)      \
@@ -602,8 +602,8 @@ uint32_t helper_##op(CPUPPCState *env, ppc_fprp_t *a, uint32_t dcm)      \
     return dfp.crbf;                                                     \
 }
 
-DFP_HELPER_TSTDG(dtstdg, 64)
-DFP_HELPER_TSTDG(dtstdgq, 128)
+DFP_HELPER_TSTDG(DTSTDG, 64)
+DFP_HELPER_TSTDG(DTSTDGQ, 128)
 
 #define DFP_HELPER_TSTEX(op, size)                                       \
 uint32_t helper_##op(CPUPPCState *env, ppc_fprp_t *a, ppc_fprp_t *b)     \
