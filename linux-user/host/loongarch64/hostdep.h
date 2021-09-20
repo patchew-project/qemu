e29b4904a2b351d942ae33546551b0f152781ce7
WANG Xuerui <git@xen0n.name>
2021-09-20 16:04:50 +0800

    linux-user: Add host dependency for 64-bit LoongArch
    Currently nothing special is needed for LoongArch hosts to work, so only
    leave a placeholder there.
    
    Signed-off-by: WANG Xuerui <git@xen0n.name>
    Message-Id: <20210920080451.408655-30-git@xen0n.name>

---
+++ linux-user/host/loongarch64/hostdep.h
@@ -0,0 +1,11 @@
+/*
+ * hostdep.h : things which are dependent on the host architecture
+ *
+ * This work is licensed under the terms of the GNU GPL, version 2 or later.
+ * See the COPYING file in the top-level directory.
+ */
+
+#ifndef LOONGARCH64_HOSTDEP_H
+#define LOONGARCH64_HOSTDEP_H
+
+#endif
