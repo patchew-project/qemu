686b3aaff2fbd398ac187a0aa53fd95e6152ca4f
Philippe Mathieu-Daudé <philmd@redhat.com>
2021-10-02 14:53:13 +0200

    target/i386/sev: Move qmp_query_sev() & hmp_info_sev() to sev.c
    Move qmp_query_sev() & hmp_info_sev()() from monitor.c to sev.c
    and make sev_get_info() static. We don't need the stub anymore,
    remove it. Add a stub for hmp_info_sev().
    
    Signed-off-by: Philippe Mathieu-Daudé <philmd@redhat.com>
    Message-Id: <20211002125317.3418648-19-philmd@redhat.com>
    Supersedes: <20210616204328.2611406-1-philmd@redhat.com>

--- target/i386/sev_i386.h
+++ target/i386/sev_i386.h
@@ -15,7 +15,6 @@
 #define QEMU_SEV_I386_H
 
 #include "sysemu/sev.h"
-#include "qapi/qapi-types-misc-target.h"
 
 #define SEV_POLICY_NODBG        0x1
 #define SEV_POLICY_NOKS         0x2
@@ -23,8 +22,6 @@
 #define SEV_POLICY_NOSEND       0x8
 #define SEV_POLICY_DOMAIN       0x10
 #define SEV_POLICY_SEV          0x20
-
-extern SevInfo *sev_get_info(void);
 
 int sev_encrypt_flash(uint8_t *ptr, uint64_t len, Error **errp);
 int sev_inject_launch_secret(const char *hdr, const char *secret,
