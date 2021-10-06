5c993709c8bf55fd43ec2b0561ecd56185b9a947
Frédéric Pétrot <frederic.petrot@univ-grenoble-alpes.fr>
2021-10-06 23:28:11 +0200

    target/riscv: additional macros to check instruction support
    Given that the 128-bit version of the riscv spec adds new instructions, and
    that some instructions that were previously only available in 64-bit mode
    are now available for both 64-bit and 128-bit, we added new macros to check
    for the processor mode during translation.
    
    Signed-off-by: Frédéric Pétrot <frederic.petrot@univ-grenoble-alpes.fr>
    Co-authored-by: Fabien Portas <fabien.portas@grenoble-inp.org>
    Message-Id: <20211006212833.108706-6-frederic.petrot@univ-grenoble-alpes.fr>

--- target/riscv/translate.c
+++ target/riscv/translate.c
@@ -357,9 +357,27 @@ EX_SH(12)
 } while (0)
 
 #define REQUIRE_64BIT(ctx) do { \
-    if (is_32bit(ctx)) {        \
+    if (!is_64bit(ctx)) {       \
         return false;           \
     }                           \
+} while (0)
+
+#define REQUIRE_128BIT(ctx) do { \
+    if (!is_128bit(ctx)) {         \
+        return false;            \
+    }                            \
+} while (0)
+
+#define REQUIRE_32_OR_64BIT(ctx) do {  \
+    if (is_128bit(ctx)) {               \
+        return false;                  \
+    }                                  \
+} while (0)
+
+#define REQUIRE_64_OR_128BIT(ctx) do { \
+    if (is_32bit(ctx)) {               \
+        return false;                  \
+    }                                  \
 } while (0)
 
 static int ex_rvc_register(DisasContext *ctx, int reg)
