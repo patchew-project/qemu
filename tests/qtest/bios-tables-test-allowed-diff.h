3de15cbefab59397eb8f208263d9b8e42b3137a9
Yanan Wang <wangyanan55@huawei.com>
2021-10-07 11:07:46 +0800

    tests/data/acpi/virt: Update binary files for PPTT
    Run ./tests/data/acpi/rebuild-expected-aml.sh from build directory
    to update PPTT binary. Also empty bios-tables-test-allowed-diff.h.
    
    Disassembled output of the updated new files:
    
    /*
     * Intel ACPI Component Architecture
     * AML/ASL+ Disassembler version 20180810 (64-bit version)
     * Copyright (c) 2000 - 2018 Intel Corporation
     *
     * Disassembly of tests/data/acpi/virt/PPTT.pxb, Thu Oct  7 09:58:26 2021
     *
     * ACPI Data Table [PPTT]
     *
     * Format: [HexOffset DecimalOffset ByteLength]  FieldName : FieldValue
     */
    
    [000h 0000   4]                    Signature : "PPTT"    [Processor Properties Topology Table]
    [004h 0004   4]                 Table Length : 0000004C
    [008h 0008   1]                     Revision : 02
    [009h 0009   1]                     Checksum : A8
    [00Ah 0010   6]                       Oem ID : "BOCHS "
    [010h 0016   8]                 Oem Table ID : "BXPC    "
    [018h 0024   4]                 Oem Revision : 00000001
    [01Ch 0028   4]              Asl Compiler ID : "BXPC"
    [020h 0032   4]        Asl Compiler Revision : 00000001
    
    [024h 0036   1]                Subtable Type : 00 [Processor Hierarchy Node]
    [025h 0037   1]                       Length : 14
    [026h 0038   2]                     Reserved : 0000
    [028h 0040   4]        Flags (decoded below) : 00000001
                                Physical package : 1
                         ACPI Processor ID valid : 0
    [02Ch 0044   4]                       Parent : 00000000
    [030h 0048   4]            ACPI Processor ID : 00000000
    [034h 0052   4]      Private Resource Number : 00000000
    
    [038h 0056   1]                Subtable Type : 00 [Processor Hierarchy Node]
    [039h 0057   1]                       Length : 14
    [03Ah 0058   2]                     Reserved : 0000
    [03Ch 0060   4]        Flags (decoded below) : 0000000A
                                Physical package : 0
                         ACPI Processor ID valid : 1
    [040h 0064   4]                       Parent : 00000024
    [044h 0068   4]            ACPI Processor ID : 00000000
    [048h 0072   4]      Private Resource Number : 00000000
    
    Raw Table Data: Length 76 (0x4C)
    
        0000: 50 50 54 54 4C 00 00 00 02 A8 42 4F 43 48 53 20  // PPTTL.....BOCHS
        0010: 42 58 50 43 20 20 20 20 01 00 00 00 42 58 50 43  // BXPC    ....BXPC
        0020: 01 00 00 00 00 14 00 00 01 00 00 00 00 00 00 00  // ................
        0030: 00 00 00 00 00 00 00 00 00 14 00 00 0A 00 00 00  // ................
        0040: 24 00 00 00 00 00 00 00 00 00 00 00              // $...........
    
    Signed-off-by: Yanan Wang <wangyanan55@huawei.com>
    Message-Id: <20211007030746.10420-9-wangyanan55@huawei.com>

--- tests/qtest/bios-tables-test-allowed-diff.h
+++ tests/qtest/bios-tables-test-allowed-diff.h
@@ -1,5 +1,1 @@
 /* List of comma-separated changed AML files to ignore */
-"tests/data/acpi/virt/PPTT",
-"tests/data/acpi/virt/PPTT.memhp",
-"tests/data/acpi/virt/PPTT.numamem",
-"tests/data/acpi/virt/PPTT.pxb",
