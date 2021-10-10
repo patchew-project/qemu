3ce44006be9e9a2fb92f3fbcf0cf1f6f25d44b11
Richard Henderson <richard.henderson@linaro.org>
2021-10-10 10:44:01 -0700

    target/riscv: Support TCG_TARGET_SIGNED_ADDR32
    All RV64 32-bit operations sign-extend the output, so we are easily
    able to keep TCG_TYPE_I32 values sign-extended in host registers.
    
    Signed-off-by: Richard Henderson <richard.henderson@linaro.org>
    Message-Id: <20211010174401.141339-9-richard.henderson@linaro.org>

--- tcg/riscv/tcg-target-sa32.h
+++ tcg/riscv/tcg-target-sa32.h
@@ -1,1 +1,5 @@
-#define TCG_TARGET_SIGNED_ADDR32 0
+/*
+ * Do not set TCG_TARGET_SIGNED_ADDR32 for RV32;
+ * TCG expects this to only be set for 64-bit hosts.
+ */
+#define TCG_TARGET_SIGNED_ADDR32  (__riscv_xlen == 64)
