a0c93b65105d0b7d40a58bdfa2f16b2277845fe6
Richard Henderson <richard.henderson@linaro.org>
2021-10-10 10:43:59 -0700

    tcg/aarch64: Support TCG_TARGET_SIGNED_ADDR32
    AArch64 has both sign and zero-extending addressing modes, which
    means that either treatment of guest addresses is equally efficient.
    Enabling this for AArch64 gives us testing of the feature in CI.
    
    Signed-off-by: Richard Henderson <richard.henderson@linaro.org>
    Message-Id: <20211010174401.141339-7-richard.henderson@linaro.org>

--- tcg/aarch64/tcg-target-sa32.h
+++ tcg/aarch64/tcg-target-sa32.h
@@ -1,1 +1,7 @@
-#define TCG_TARGET_SIGNED_ADDR32 0
+/*
+ * AArch64 has both SXTW and UXTW addressing modes, which means that
+ * it is agnostic to how guest addresses should be represented.
+ * Because aarch64 is more common than the other hosts that will
+ * want to use this feature, enable it for continuous testing.
+ */
+#define TCG_TARGET_SIGNED_ADDR32 1
