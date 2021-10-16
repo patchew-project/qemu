f6cf90b52afd8ece4301c556c16432042401363a
Kito Cheng <kito.cheng@sifive.com>
2021-10-16 17:07:36 +0800

    target/riscv: zfh: half-precision load and store
    Signed-off-by: Kito Cheng <kito.cheng@sifive.com>
    Signed-off-by: Chih-Min Chao <chihmin.chao@sifive.com>
    Signed-off-by: Frank Chang <frank.chang@sifive.com>
    Reviewed-by: Richard Henderson <richard.henderson@linaro.org>
    Message-Id: <20211016090742.3034669-2-frank.chang@sifive.com>
    Based-on: <20211016085428.3001501-1-frank.chang@sifive.com>

--- target/riscv/cpu.c
+++ target/riscv/cpu.c
@@ -601,6 +601,7 @@ static Property riscv_cpu_properties[] = {
     DEFINE_PROP_BOOL("Counters", RISCVCPU, cfg.ext_counters, true),
     DEFINE_PROP_BOOL("Zifencei", RISCVCPU, cfg.ext_ifencei, true),
     DEFINE_PROP_BOOL("Zicsr", RISCVCPU, cfg.ext_icsr, true),
+    DEFINE_PROP_BOOL("Zfh", RISCVCPU, cfg.ext_zfh, false),
     DEFINE_PROP_STRING("priv_spec", RISCVCPU, cfg.priv_spec),
     DEFINE_PROP_STRING("vext_spec", RISCVCPU, cfg.vext_spec),
     DEFINE_PROP_UINT16("vlen", RISCVCPU, cfg.vlen, 128),
