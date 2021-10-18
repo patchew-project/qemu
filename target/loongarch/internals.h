c8f19f47751041ce3e13d13c867d2657e04e39c4
Song Gao <gaosong@loongson.cn>
2021-10-18 20:47:17 +0800

    target/loongarch: Add floating point comparison instruction translation
    This patch implement floating point comparison instruction translation.
    
    This includes:
    - FCMP.cond.{S/D}
    
    Signed-off-by: Song Gao <gaosong@loongson.cn>
    Signed-off-by: Xiaojuan Yang <yangxiaojuan@loongson.cn>
    Reviewed-by: Richard Henderson <richard.henderson@linaro.org>
    Message-Id: <1634561247-25499-12-git-send-email-gaosong@loongson.cn>

--- target/loongarch/internals.h
+++ target/loongarch/internals.h
@@ -9,6 +9,11 @@
 #ifndef LOONGARCH_INTERNALS_H
 #define LOONGARCH_INTERNALS_H
 
+#define FCMP_LT   0x0001  /* fp0 < fp1 */
+#define FCMP_EQ   0x0010  /* fp0 = fp1 */
+#define FCMP_UN   0x0100  /* unordered */
+#define FCMP_GT   0x1000  /* fp0 > fp1 */
+
 void loongarch_translate_init(void);
 
 void loongarch_cpu_dump_state(CPUState *cpu, FILE *f, int flags);
