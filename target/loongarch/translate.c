9a46a3bc2c228a4350c81b7d15442d843f15844f
Song Gao <gaosong@loongson.cn>
2021-10-18 20:47:14 +0800

    target/loongarch: Add fixed point atomic instruction translation
    This patch implement fixed point atomic instruction translation.
    
    This includes:
    - LL.{W/D}, SC.{W/D}
    - AM{SWAP/ADD/AND/OR/XOR/MAX/MIN}[_DB].{W/D}
    - AM{MAX/MIN}[_DB].{WU/DU}
    
    Signed-off-by: Song Gao <gaosong@loongson.cn>
    Signed-off-by: Xiaojuan Yang <yangxiaojuan@loongson.cn>
    Reviewed-by: Richard Henderson <richard.henderson@linaro.org>
    Message-Id: <1634561247-25499-9-git-send-email-gaosong@loongson.cn>

--- target/loongarch/translate.c
+++ target/loongarch/translate.c
@@ -174,6 +174,7 @@ static void gen_set_gpr(int reg_num, TCGv t, DisasExtend dst_ext)
 #include "insn_trans/trans_shift.c"
 #include "insn_trans/trans_bit.c"
 #include "insn_trans/trans_memory.c"
+#include "insn_trans/trans_atomic.c"
 
 static void loongarch_tr_translate_insn(DisasContextBase *dcbase, CPUState *cs)
 {
