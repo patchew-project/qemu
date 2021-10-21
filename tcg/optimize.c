36f803421b6cfa0582ddb2c9dcdac9567949e3f0
Richard Henderson <richard.henderson@linaro.org>
2021-10-21 14:05:10 -0700

    tcg/optimize: Split out fold_setcond
    Reviewed-by: Luis Pires <luis.pires@eldorado.org.br>
    Signed-off-by: Richard Henderson <richard.henderson@linaro.org>
    Message-Id: <20211021210539.825582-20-richard.henderson@linaro.org>

--- tcg/optimize.c
+++ tcg/optimize.c
@@ -933,6 +933,17 @@ static bool fold_qemu_st(OptContext *ctx, TCGOp *op)
     return false;
 }
 
+static bool fold_setcond(OptContext *ctx, TCGOp *op)
+{
+    TCGCond cond = op->args[3];
+    int i = do_constant_folding_cond(op->opc, op->args[1], op->args[2], cond);
+
+    if (i >= 0) {
+        return tcg_opt_gen_movi(ctx, op, op->args[0], i);
+    }
+    return false;
+}
+
 static bool fold_setcond2(OptContext *ctx, TCGOp *op)
 {
     TCGCond cond = op->args[5];
@@ -1633,15 +1644,6 @@ void tcg_optimize(TCGContext *s)
             }
             break;
 
-        CASE_OP_32_64(setcond):
-            i = do_constant_folding_cond(opc, op->args[1],
-                                         op->args[2], op->args[3]);
-            if (i >= 0) {
-                tcg_opt_gen_movi(&ctx, op, op->args[0], i);
-                continue;
-            }
-            break;
-
         CASE_OP_32_64(movcond):
             i = do_constant_folding_cond(opc, op->args[1],
                                          op->args[2], op->args[5]);
@@ -1799,6 +1801,9 @@ void tcg_optimize(TCGContext *s)
         CASE_OP_32_64(shl):
         CASE_OP_32_64(shr):
             done = fold_shift(&ctx, op);
+            break;
+        CASE_OP_32_64(setcond):
+            done = fold_setcond(&ctx, op);
             break;
         case INDEX_op_setcond2_i32:
             done = fold_setcond2(&ctx, op);
