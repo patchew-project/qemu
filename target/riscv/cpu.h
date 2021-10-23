b85c8e3a529b8bcb1ebd3b838d85e716e190419a
Anup Patel <anup.patel@wdc.com>
2021-10-23 14:16:27 +0530

    target/riscv: Implement AIA hvictl and hviprioX CSRs
    The AIA hvictl and hviprioX CSRs allow hypervisor to control
    interrupts visible at VS-level. This patch implements AIA hvictl
    and hviprioX CSRs.
    
    Signed-off-by: Anup Patel <anup.patel@wdc.com>
    Message-Id: <20211023084638.1697057-12-anup.patel@wdc.com>

--- target/riscv/cpu.h
+++ target/riscv/cpu.h
@@ -196,6 +196,7 @@ struct CPURISCVState {
     uint64_t htimedelta;
 
     /* Hypervisor controlled virtual interrupt priorities */
+    target_ulong hvictl;
     uint8_t hviprio[64];
 
     /* Virtual CSRs */
