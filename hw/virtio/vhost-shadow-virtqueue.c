bf992db1e951fdcd1bdad66c38fde08cd52ac7d6
Eugenio Pérez <eperezma@redhat.com>
2021-10-29 20:35:20 +0200

    vhost: Add vhost_svq_valid_guest_features to shadow vq
    This allows it to test if the guest has aknowledge an invalid transport
    feature for SVQ. This will include packed vq layout or event_idx,
    where VirtIO device needs help from SVQ.
    
    There is not needed at this moment, but since SVQ will not re-negotiate
    features again with the guest, a failure in acknowledge them is fatal
    for SVQ.
    
    Signed-off-by: Eugenio Pérez <eperezma@redhat.com>
    Message-Id: <20211029183525.1776416-22-eperezma@redhat.com>

--- hw/virtio/vhost-shadow-virtqueue.c
+++ hw/virtio/vhost-shadow-virtqueue.c
@@ -62,6 +62,12 @@ bool vhost_svq_valid_device_features(uint64_t *dev_features)
     return true;
 }
 
+/* If the guest is using some of these, SVQ cannot communicate */
+bool vhost_svq_valid_guest_features(uint64_t *guest_features)
+{
+    return true;
+}
+
 /* Forward guest notifications */
 static void vhost_handle_guest_kick(EventNotifier *n)
 {
