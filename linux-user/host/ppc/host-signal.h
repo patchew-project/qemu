420697386c4fbfb12a1c3a63eac40935de9a5242
Richard Henderson <richard.henderson@linaro.org>
2021-10-30 10:15:37 -0700

    linux-user/host/ppc: Populate host_signal.h
    Split host_signal_pc and host_signal_write out of user-exec.c.
    Drop the *BSD code, to be re-created under bsd-user/ later.
    
    Reviewed-by: Warner Losh <imp@bsdimp.com>
    Signed-off-by: Richard Henderson <richard.henderson@linaro.org>
    Message-Id: <20211030171635.1689530-9-richard.henderson@linaro.org>

--- linux-user/host/ppc/host-signal.h
+++ linux-user/host/ppc/host-signal.h
@@ -1,1 +1,24 @@
-#define HOST_SIGNAL_PLACEHOLDER
+/*
+ * host-signal.h: signal info dependent on the host architecture
+ *
+ * Copyright (C) 2021 Linaro Limited
+ *
+ * This work is licensed under the terms of the GNU GPL, version 2 or later.
+ * See the COPYING file in the top-level directory.
+ */
+
+#ifndef PPC_HOST_SIGNAL_H
+#define PPC_HOST_SIGNAL_H
+
+static inline uintptr_t host_signal_pc(ucontext_t *uc)
+{
+    return uc->uc_mcontext.regs->nip;
+}
+
+static inline bool host_signal_write(siginfo_t *info, ucontext_t *uc)
+{
+    return uc->uc_mcontext.regs->trap != 0x400
+        && (uc->uc_mcontext.regs->dsisr & 0x02000000);
+}
+
+#endif
