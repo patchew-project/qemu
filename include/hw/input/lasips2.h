d47d666193cbe6886aae18ca391490eac6b55bc4
Philippe Mathieu-Daudé <f4bug@amsat.org>
2021-11-01 09:27:46 +0100

    hw/input/lasips2: Move LASIPS2State declaration to 'hw/input/lasips2.h'
    We want to use the OBJECT_DECLARE_SIMPLE_TYPE() macro to QOM'ify
    this device in the next commit. To make its review simpler, as a
    first step move the LASIPS2State and LASIPS2Port declarations to
    'hw/input/lasips2.h'
    
    Signed-off-by: Philippe Mathieu-Daudé <f4bug@amsat.org>
    Reviewed-by: Damien Hedde <damien.hedde@greensocs.com>
    Message-Id: <20210920064048.2729397-3-f4bug@amsat.org>
    Signed-off-by: Laurent Vivier <laurent@vivier.eu>
    Message-Id: <20211101082747.2524909-6-laurent@vivier.eu>

--- include/hw/input/lasips2.h
+++ include/hw/input/lasips2.h
@@ -11,6 +11,24 @@
 
 #define TYPE_LASIPS2 "lasips2"
 
+struct LASIPS2State;
+typedef struct LASIPS2Port {
+    struct LASIPS2State *parent;
+    MemoryRegion reg;
+    void *dev;
+    uint8_t id;
+    uint8_t control;
+    uint8_t buf;
+    bool loopback_rbne;
+    bool irq;
+} LASIPS2Port;
+
+typedef struct LASIPS2State {
+    LASIPS2Port kbd;
+    LASIPS2Port mouse;
+    qemu_irq irq;
+} LASIPS2State;
+
 void lasips2_init(MemoryRegion *address_space, hwaddr base, qemu_irq irq);
 
 #endif /* HW_INPUT_LASIPS2_H */
