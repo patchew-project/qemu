5bcc8b9c13426682c486305ac0a33d43cde90ddb
Song Gao <gaosong@loongson.cn>
2021-11-01 17:51:51 +0800

    linux-user: Add safe syscall handling for loongarch64 hosts
    Base-on: <20210925173032.2434906-29-git@xen0n.name>
    Signed-off-by: Song Gao <gaosong@loongson.cn>
    Signed-off-by: Xiaojuan Yang <yangxiaojuan@loongson.cn>
    Message-Id: <1635760311-20015-30-git-send-email-gaosong@loongson.cn>

--- linux-user/host/loongarch64/hostdep.h
+++ linux-user/host/loongarch64/hostdep.h
@@ -8,4 +8,27 @@
 #ifndef LOONGARCH64_HOSTDEP_H
 #define LOONGARCH64_HOSTDEP_H
 
+/* We have a safe-syscall.inc.S */
+#define HAVE_SAFE_SYSCALL
+
+#ifndef __ASSEMBLER__
+
+/* These are defined by the safe-syscall.inc.S file */
+extern char safe_syscall_start[];
+extern char safe_syscall_end[];
+
+/* Adjust the signal context to rewind out of safe-syscall if we're in it */
+static inline void rewind_if_in_safe_syscall(void *puc)
+{
+    ucontext_t *uc = puc;
+    unsigned long long *pcreg = &uc->uc_mcontext.__pc;
+
+    if (*pcreg > (uintptr_t)safe_syscall_start
+        && *pcreg < (uintptr_t)safe_syscall_end) {
+        *pcreg = (uintptr_t)safe_syscall_start;
+    }
+}
+
+#endif /* __ASSEMBLER__ */
+
 #endif
