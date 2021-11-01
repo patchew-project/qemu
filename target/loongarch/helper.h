223dc3288444b5bb278ab421646e0b806c789433
Song Gao <gaosong@loongson.cn>
2021-11-01 17:51:31 +0800

    target/loongarch: Add fixed point extra instruction translation
    This includes:
    - CRC[C].W.{B/H/W/D}.W
    - SYSCALL
    - BREAK
    - ASRT{LE/GT}.D
    - RDTIME{L/H}.W, RDTIME.D
    - CPUCFG
    
    Reviewed-by: Richard Henderson <richard.henderson@linaro.org>
    Signed-off-by: Song Gao <gaosong@loongson.cn>
    Signed-off-by: Xiaojuan Yang <yangxiaojuan@loongson.cn>
    Message-Id: <1635760311-20015-10-git-send-email-gaosong@loongson.cn>

--- target/loongarch/helper.h
+++ target/loongarch/helper.h
@@ -14,3 +14,7 @@ DEF_HELPER_FLAGS_1(bitswap, TCG_CALL_NO_RWG_SE, tl, tl)
 
 DEF_HELPER_3(asrtle_d, void, env, tl, tl)
 DEF_HELPER_3(asrtgt_d, void, env, tl, tl)
+
+DEF_HELPER_3(crc32, tl, tl, tl, tl)
+DEF_HELPER_3(crc32c, tl, tl, tl, tl)
+DEF_HELPER_2(cpucfg, tl, env, tl)
