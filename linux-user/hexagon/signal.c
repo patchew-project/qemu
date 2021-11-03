c6225d30b0fc0e5d138af145d7a3f9ba891d73ec
Philippe Mathieu-Daudé <f4bug@amsat.org>
2021-11-03 21:31:46 +0100

    linux-user: Add GPL-2.0+ SPDX license identifier
    Use Qualcomm License Identifier tool [*] to classify linux-user/
    files per license type, filtering those matching at least 60% the
    "GPLv2 or later" type, and manually add the GPL-2.0-or-later SPDX
    comment.
    
      $ license-identifier --output_format easy_read \
          --threshold 0.6 --input_path linux-user \
        | egrep '(Name of the input file|Matched license type is|The found license text has the score of)'
        | fgrep -1 GPL-2.0+
      Name of the input file: linux-user/cpu_loop-common.h
      Matched license type is GPL-2.0+
      The found license text has the score of 0.69
      --
      Name of the input file: linux-user/exit.c
      Matched license type is GPL-2.0+
      The found license text has the score of 0.685
      --
      Name of the input file: linux-user/fd-trans.c
      Matched license type is GPL-2.0+
      The found license text has the score of 0.715
      --
      Name of the input file: linux-user/fd-trans.h
      Matched license type is GPL-2.0+
      The found license text has the score of 0.715
      --
      Name of the input file: linux-user/flatload.c
      Matched license type is GPL-2.0+
      The found license text has the score of 0.715
      --
      ...
    
    [*] Qualcomm Open Source Technology Group License Identifier tool
        https://www.codeaurora.org/projects/qualcomm-ostg-lid
        https://github.com/quic/lid#usage
    
    Signed-off-by: Philippe Mathieu-Daudé <f4bug@amsat.org>
    Message-Id: <20211103203146.3630550-1-f4bug@amsat.org>
    Reviewed-by: Warner Losh <imp@bsdiimp.com>

--- linux-user/hexagon/signal.c
+++ linux-user/hexagon/signal.c
@@ -4,6 +4,8 @@
  *  Copyright (c) 2003 Fabrice Bellard
  *  Copyright(c) 2019-2021 Qualcomm Innovation Center, Inc. All Rights Reserved.
  *
+ *  SPDX-License-Identifier: GPL-2.0-or-later
+ *
  *  This program is free software; you can redistribute it and/or modify
  *  it under the terms of the GNU General Public License as published by
  *  the Free Software Foundation; either version 2 of the License, or
