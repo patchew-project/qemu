c5e2da9d0d60ebe3db62baf3f2c6e987c29fd7f0
Philippe Mathieu-Daudé <f4bug@amsat.org>
2021-11-05 18:21:25 +0100

    hw/qdev: Correct qdev_connect_gpio_out_named() documentation
    qdev_connect_gpio_out_named() is described as qdev_connect_gpio_out(),
    and referring to itself in an endless loop, which is confusing. Fix.
    
    Signed-off-by: Philippe Mathieu-Daudé <f4bug@amsat.org>
    Message-Id: <20211105172127.224462-2-f4bug@amsat.org>

--- include/hw/qdev-core.h
+++ include/hw/qdev-core.h
@@ -497,7 +497,8 @@ qemu_irq qdev_get_gpio_in_named(DeviceState *dev, const char *name, int n);
  */
 void qdev_connect_gpio_out(DeviceState *dev, int n, qemu_irq pin);
 /**
- * qdev_connect_gpio_out: Connect one of a device's anonymous output GPIO lines
+ * qdev_connect_gpio_out_named: Connect one of a device's named output
+ *                              GPIO lines
  * @dev: Device whose GPIO to connect
  * @name: Name of the output GPIO array
  * @n: Number of the anonymous output GPIO line (which must be in range)
@@ -518,8 +519,6 @@ void qdev_connect_gpio_out(DeviceState *dev, int n, qemu_irq pin);
  * It is not valid to try to connect one outbound GPIO to multiple
  * qemu_irqs at once, or to connect multiple outbound GPIOs to the
  * same qemu_irq; see qdev_connect_gpio_out() for details.
- *
- * For named output GPIO lines, use qdev_connect_gpio_out_named().
  */
 void qdev_connect_gpio_out_named(DeviceState *dev, const char *name, int n,
                                  qemu_irq pin);
