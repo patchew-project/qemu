6c34c479c15f3446d60e58469e3d744d2167dc92
Song Gao <gaosong@loongson.cn>
2021-11-08 11:07:52 +0800

    target/loongarch: Add fixed point shift instruction translation
    This includes:
    - SLL.W, SRL.W, SRA.W, ROTR.W
    - SLLI.W, SRLI.W, SRAI.W, ROTRI.W
    - SLL.D, SRL.D, SRA.D, ROTR.D
    - SLLI.D, SRLI.D, SRAI.D, ROTRI.D
    
    Signed-off-by: Song Gao <gaosong@loongson.cn>
    Signed-off-by: Xiaojuan Yang <yangxiaojuan@loongson.cn>
    Reviewed-by: Richard Henderson <richard.henderson@linaro.org>
    Message-Id: <1636340895-5255-6-git-send-email-gaosong@loongson.cn>

--- target/loongarch/translate.c
+++ target/loongarch/translate.c
@@ -141,6 +141,7 @@ static void gen_set_gpr(int reg_num, TCGv t, DisasExtend dst_ext)
 
 #include "decode-insns.c.inc"
 #include "insn_trans/trans_arith.c.inc"
+#include "insn_trans/trans_shift.c.inc"
 
 static void loongarch_tr_translate_insn(DisasContextBase *dcbase, CPUState *cs)
 {
