061b6a2828c4608260198e0f9306d0dd434619dc
Collin Walling <walling@linux.ibm.com>
2021-11-08 16:13:11 -0500

    s390x: kvm: adjust diag318 resets to retain data
    The CPNC portion of the diag 318 data is erroneously reset during an
    initial CPU reset caused by SIGP. Let's go ahead and relocate the
    diag318_info field within the CPUS390XState struct such that it is
    only zeroed during a clear reset. This way, the CPNC will be retained
    for each VCPU in the configuration after the diag 318 instruction
    has been invoked.
    
    Additionally, the diag 318 data reset is handled via the CPU reset
    code during a clear reset. This means some of the diag 318-specific
    reset code can now be removed.
    
    Signed-off-by: Collin Walling <walling@linux.ibm.com>
    Fixes: fabdada9357b ("s390: guest support for diagnose 0x318")
    Reported-by: Christian Borntraeger <borntraeger@de.ibm.com>
    
    Message-Id: <20211108211311.33834-1-walling@linux.ibm.com>

--- target/s390x/cpu-sysemu.c
+++ target/s390x/cpu-sysemu.c
@@ -299,10 +299,3 @@ void s390_enable_css_support(S390CPU *cpu)
         kvm_s390_enable_css_support(cpu);
     }
 }
-
-void s390_do_cpu_set_diag318(CPUState *cs, run_on_cpu_data arg)
-{
-    if (kvm_enabled()) {
-        kvm_s390_set_diag318(cs, arg.host_ulong);
-    }
-}
