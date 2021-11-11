9005cc9a4ff7c29284d4b13d5e3240852ec1d9fe
LIU Zhiwei <zhiwei_liu@c-sky.com>
2021-11-11 13:57:51 +0800

    target/riscv: Split pm_enabled into mask and base
    Use cached cur_pmmask and cur_pmbase to infer the
    current PM mode.
    
    This may decrease the TCG IR by one when pm_enabled
    is true and pm_base_enabled is false.
    
    Signed-off-by: LIU Zhiwei <zhiwei_liu@c-sky.com>
    Message-Id: <20211111055800.42672-12-zhiwei_liu@c-sky.com>

--- target/riscv/cpu_helper.c
+++ target/riscv/cpu_helper.c
@@ -108,25 +108,14 @@ void cpu_get_tb_cpu_state(CPURISCVState *env, target_ulong *pc,
         flags = FIELD_DP32(flags, TB_FLAGS, MSTATUS_HS_FS,
                            get_field(env->mstatus_hs, MSTATUS_FS));
     }
-    if (riscv_has_ext(env, RVJ)) {
-        int priv = flags & TB_FLAGS_PRIV_MMU_MASK;
-        bool pm_enabled = false;
-        switch (priv) {
-        case PRV_U:
-            pm_enabled = env->mmte & U_PM_ENABLE;
-            break;
-        case PRV_S:
-            pm_enabled = env->mmte & S_PM_ENABLE;
-            break;
-        case PRV_M:
-            pm_enabled = env->mmte & M_PM_ENABLE;
-            break;
-        default:
-            g_assert_not_reached();
-        }
-        flags = FIELD_DP32(flags, TB_FLAGS, PM_ENABLED, pm_enabled);
-    }
 #endif
+
+    if (env->cur_pmmask < (xl == MXL_RV32 ? UINT32_MAX : UINT64_MAX)) {
+        flags = FIELD_DP32(flags, TB_FLAGS, PM_MASK_ENABLED, 1);
+    }
+    if (env->cur_pmbase != 0) {
+        flags = FIELD_DP32(flags, TB_FLAGS, PM_BASE_ENABLED, 1);
+    }
 
     flags = FIELD_DP32(flags, TB_FLAGS, XL, xl);
 
