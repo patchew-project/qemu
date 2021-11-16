6907079c30a26944de85626d596ab4d481fa037c
liweiwei <liweiwei@iscas.ac.cn>
2021-11-16 16:06:16 +0800

    target/riscv: rvk: add implementation of instructions for Zbk*
       - reuse partial instructions of Zbb/Zbc extensions
       - add brev8 packh, unzip, zip, etc.
    
    Signed-off-by: liweiwei <liweiwei@iscas.ac.cn>
    Signed-off-by: wangjunqiang <wangjunqiang@iscas.ac.cn>
    Message-Id: <20211116080621.2521-3-liweiwei@iscas.ac.cn>

--- target/riscv/helper.h
+++ target/riscv/helper.h
@@ -61,6 +61,11 @@ DEF_HELPER_FLAGS_1(fclass_d, TCG_CALL_NO_RWG_SE, tl, i64)
 /* Bitmanip */
 DEF_HELPER_FLAGS_2(clmul, TCG_CALL_NO_RWG_SE, tl, tl, tl)
 DEF_HELPER_FLAGS_2(clmulr, TCG_CALL_NO_RWG_SE, tl, tl, tl)
+DEF_HELPER_FLAGS_2(xperm4, TCG_CALL_NO_RWG_SE, tl, tl, tl)
+DEF_HELPER_FLAGS_2(xperm8, TCG_CALL_NO_RWG_SE, tl, tl, tl)
+DEF_HELPER_FLAGS_1(brev8, TCG_CALL_NO_RWG_SE, tl, tl)
+DEF_HELPER_FLAGS_1(unzip, TCG_CALL_NO_RWG_SE, tl, tl)
+DEF_HELPER_FLAGS_1(zip, TCG_CALL_NO_RWG_SE, tl, tl)
 
 /* Special functions */
 DEF_HELPER_2(csrr, tl, env, int)
