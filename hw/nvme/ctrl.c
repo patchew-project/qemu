3cd92116647814fa4b7891f1f768b32c3445697c
Philippe Mathieu-Daudé <philmd@redhat.com>
2021-11-17 13:35:34 +0100

    hw/nvme/ctrl: Prevent buffer overrun in nvme_error_info()
    Both 'buf_len' and 'off' arguments are under guest control.
    Since nvme_c2h() doesn't check out of boundary access, the
    caller must check for eventual buffer overrun on 'trans_len'.
    
    Cc: qemu-stable@nongnu.org
    Fixes: 94a7897c41d ("add support for the get log page command")
    Signed-off-by: Philippe Mathieu-Daudé <philmd@redhat.com>
    Message-Id: <20211117123534.2900334-3-philmd@redhat.com>

--- hw/nvme/ctrl.c
+++ hw/nvme/ctrl.c
@@ -4146,7 +4146,8 @@ static uint16_t nvme_error_info(NvmeCtrl *n, uint8_t rae, uint32_t buf_len,
     uint32_t trans_len;
     NvmeErrorLog errlog;
 
-    if (off >= sizeof(errlog)) {
+    trans_len = MIN(sizeof(errlog) - off, buf_len);
+    if (trans_len >= sizeof(errlog)) {
         return NVME_INVALID_FIELD | NVME_DNR;
     }
 
@@ -4155,7 +4156,6 @@ static uint16_t nvme_error_info(NvmeCtrl *n, uint8_t rae, uint32_t buf_len,
     }
 
     memset(&errlog, 0x0, sizeof(errlog));
-    trans_len = MIN(sizeof(errlog) - off, buf_len);
 
     return nvme_c2h(n, (uint8_t *)&errlog, trans_len, req);
 }
