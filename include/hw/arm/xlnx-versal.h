19b9b34590ddbc5d852d210b41085fd79ecf54d5
Francisco Iglesias <francisco.iglesias@xilinx.com>
2021-11-17 14:18:39 +0000

    hw/arm/xlnx-versal: Connect the OSPI flash memory controller model
    Connect the OSPI flash memory controller model (including the source and
    destination DMA).
    
    Signed-off-by: Francisco Iglesias <francisco.iglesias@xilinx.com>
    Message-Id: <20211117141841.4696-8-francisco.iglesias@xilinx.com>

--- include/hw/arm/xlnx-versal.h
+++ include/hw/arm/xlnx-versal.h
@@ -26,6 +26,8 @@
 #include "hw/misc/xlnx-versal-xramc.h"
 #include "hw/nvram/xlnx-bbram.h"
 #include "hw/nvram/xlnx-versal-efuse.h"
+#include "hw/ssi/xlnx-versal-ospi.h"
+#include "hw/dma/xlnx_csu_dma.h"
 #include "hw/misc/xlnx-versal-pmc-iou-slcr.h"
 
 #define TYPE_XLNX_VERSAL "xlnx-versal"
@@ -80,6 +82,12 @@ struct Versal {
         struct {
             SDHCIState sd[XLNX_VERSAL_NR_SDS];
             XlnxVersalPmcIouSlcr slcr;
+
+            XlnxVersalOspi ospi;
+            XlnxCSUDMA ospi_dma_src;
+            XlnxCSUDMA ospi_dma_dst;
+            MemoryRegion lospi_mr;
+            qemu_or_irq ospi_irq;
         } iou;
 
         XlnxZynqMPRTC rtc;
@@ -116,6 +124,7 @@ struct Versal {
 #define VERSAL_BBRAM_APB_IRQ_0     121
 #define VERSAL_RTC_APB_ERR_IRQ     121
 #define VERSAL_PMC_IOU_SLCR_IRQ    121
+#define VERSAL_OSPI_IRQ            124
 #define VERSAL_SD0_IRQ_0           126
 #define VERSAL_EFUSE_IRQ           139
 #define VERSAL_RTC_ALARM_IRQ       142
@@ -183,6 +192,15 @@ struct Versal {
 
 #define MM_PMC_PMC_IOU_SLCR         0xf1060000
 #define MM_PMC_PMC_IOU_SLCR_SIZE    0x10000
+
+#define MM_PMC_OSPI                 0xf1010000
+#define MM_PMC_OSPI_SIZE            0x10000
+
+#define MM_PMC_OSPI_DAC             0xc0000000
+#define MM_PMC_OSPI_DAC_SIZE        0x20000000
+
+#define MM_PMC_OSPI_DMA_DST         0xf1011800
+#define MM_PMC_OSPI_DMA_SRC         0xf1011000
 
 #define MM_PMC_SD0                  0xf1040000U
 #define MM_PMC_SD0_SIZE             0x10000
