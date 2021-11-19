83844de006c27297c98a355e12f9f433a8602799
Daniel Henrique Barboza <danielhb413@gmail.com>
2021-11-19 15:22:08 -0300

    target/ppc: PMU basic cycle count for pseries TCG
    This patch adds the barebones of the PMU logic by enabling cycle
    counting. The overall logic goes as follows:
    
    - a helper is added to control the PMU state on each MMCR0 write. This
    allows for the PMU to start/stop as the frozen counter bit (MMCR0_FC)
    is cleared or set;
    
    - MMCR0 reg initial value is set to 0x80000000 (MMCR0_FC set) to avoid
    having to spin the PMU right at system init;
    
    - to retrieve the events that are being profiled, getPMUEventType() will
    check the current MMCR1 value and return the appropriate PMUEventType.
    For PMCs 1-4, event 0x2 is the implementation dependent value of
    PMU_EVENT_INSTRUCTIONS and event 0x1E is the implementation dependent
    value of PMU_EVENT_CYCLES. These events are supported by IBM Power chips
    since Power8, at least, and the Linux Perf driver makes use of these
    events until kernel v5.15. For PMC1, event 0xF0 is the architected
    PowerISA event for cycles. Event 0xFE is the architected PowerISA event
    for instructions;
    
    - the intended usage is to freeze the counters by setting MMCR0_FC, do
    any additional setting of events to be counted via MMCR1 and enable
    the PMU by zeroing MMCR0_FC. Software must freeze counters to read the
    results - on the fly reading of the PMCs will return the starting value
    of each one. This act of unfreezing the PMU, counting cycles and then
    freezing the PMU again is being called a cycle count session.
    
    Given that the base CPU frequency is fixed at 1Ghz for both powernv and
    pseries clock, cycle calculation assumes that 1 nanosecond equals 1 CPU
    cycle. Cycle value is then calculated by subtracting the current time
    the PMU was frozen against the time in which the PMU started spining.
    
    The counter specific frozen bits MMCR0_FC14 and MMCR0_FC56 were also
    added as a mean to further control which PMCs were supposed to be
    counting cycles during the session.
    
    Signed-off-by: Daniel Henrique Barboza <danielhb413@gmail.com>
    Message-Id: <20211119182216.628676-3-danielhb413@gmail.com>

--- target/ppc/power8-pmu.c
+++ target/ppc/power8-pmu.c
@@ -23,6 +23,155 @@
 
 #if defined(TARGET_PPC64) && !defined(CONFIG_USER_ONLY)
 
+/*
+ * For PMCs 1-4, IBM POWER chips has support for an implementation
+ * dependent event, 0x1E, that enables cycle counting. The Linux kernel
+ * makes extensive use of 0x1E, so let's also support it.
+ *
+ * Likewise, event 0x2 is an implementation-dependent event that IBM
+ * POWER chips implement (at least since POWER8) that is equivalent to
+ * PM_INST_CMPL. Let's support this event on PMCs 1-4 as well.
+ */
+static PMUEventType getPMUEventType(CPUPPCState *env, int sprn)
+{
+    uint8_t mmcr1_evt_extr[] = { MMCR1_PMC1EVT_EXTR, MMCR1_PMC2EVT_EXTR,
+                                 MMCR1_PMC3EVT_EXTR, MMCR1_PMC4EVT_EXTR };
+    PMUEventType evt_type = PMU_EVENT_INVALID;
+    uint8_t pmcsel;
+    int i;
+
+    if (sprn == SPR_POWER_PMC5) {
+        return PMU_EVENT_INSTRUCTIONS;
+    }
+
+    if (sprn == SPR_POWER_PMC6) {
+        return PMU_EVENT_CYCLES;
+    }
+
+    i = sprn - SPR_POWER_PMC1;
+    pmcsel = extract64(env->spr[SPR_POWER_MMCR1], mmcr1_evt_extr[i],
+                       MMCR1_EVT_SIZE);
+
+    switch (pmcsel) {
+    case 0x2:
+        evt_type = PMU_EVENT_INSTRUCTIONS;
+        break;
+    case 0x1E:
+        evt_type = PMU_EVENT_CYCLES;
+        break;
+    case 0xF0:
+        /*
+         * PMC1SEL = 0xF0 is the architected PowerISA v3.1
+         * event that counts cycles using PMC1.
+         */
+        if (sprn == SPR_POWER_PMC1) {
+            evt_type = PMU_EVENT_CYCLES;
+        }
+        break;
+    case 0xFE:
+        /*
+         * PMC1SEL = 0xFE is the architected PowerISA v3.1
+         * event to sample instructions using PMC1.
+         */
+        if (sprn == SPR_POWER_PMC1) {
+            evt_type = PMU_EVENT_INSTRUCTIONS;
+        }
+        break;
+    default:
+        break;
+    }
+
+    return evt_type;
+}
+
+static bool pmc_is_active(CPUPPCState *env, int sprn, uint64_t mmcr0)
+{
+    if (sprn < SPR_POWER_PMC5) {
+        return !(mmcr0 & MMCR0_FC14);
+    }
+
+    return !(mmcr0 & MMCR0_FC56);
+}
+
+static void pmu_update_cycles(CPUPPCState *env, uint64_t old_mmcr0)
+{
+    uint64_t now = qemu_clock_get_ns(QEMU_CLOCK_VIRTUAL);
+    uint64_t time_delta = now - env->pmu_base_time;
+    int sprn;
+
+    for (sprn = SPR_POWER_PMC1; sprn <= SPR_POWER_PMC6; sprn++) {
+        if (!pmc_is_active(env, sprn, old_mmcr0) ||
+            getPMUEventType(env, sprn) != PMU_EVENT_CYCLES) {
+            continue;
+        }
+
+        /*
+         * The pseries and powernv clock runs at 1Ghz, meaning
+         * that 1 nanosec equals 1 cycle.
+         */
+        env->spr[sprn] += time_delta;
+    }
+
+    /* Update base_time for future calculations */
+    env->pmu_base_time = now;
+}
+
+/*
+ * A cycle count session consists of the basic operations we
+ * need to do to support PM_CYC events: redefine a new base_time
+ * to be used to calculate PMC values and start overflow timers.
+ */
+static void start_cycle_count_session(CPUPPCState *env)
+{
+    /* Just define pmu_base_time for now */
+    env->pmu_base_time = qemu_clock_get_ns(QEMU_CLOCK_VIRTUAL);
+}
+
+void helper_store_mmcr0(CPUPPCState *env, target_ulong value)
+{
+    target_ulong curr_value = env->spr[SPR_POWER_MMCR0];
+    bool curr_FC = curr_value & MMCR0_FC;
+    bool new_FC = value & MMCR0_FC;
+
+    env->spr[SPR_POWER_MMCR0] = value;
+
+    /* MMCR0 writes can change HFLAGS_PMCCCLEAR and HFLAGS_MMCR0FC */
+    if (((curr_value & MMCR0_PMCC) != (value & MMCR0_PMCC)) ||
+        (curr_FC != new_FC)) {
+        hreg_compute_hflags(env);
+    }
+
+    /*
+     * In an frozen count (FC) bit change:
+     *
+     * - if PMCs were running (curr_FC = false) and we're freezing
+     * them (new_FC = true), save the PMCs values in the registers.
+     *
+     * - if PMCs were frozen (curr_FC = true) and we're activating
+     * them (new_FC = false), set the new base_time for future cycle
+     * calculations.
+     */
+    if (curr_FC != new_FC) {
+        if (!curr_FC) {
+            pmu_update_cycles(env, curr_value);
+        } else {
+            start_cycle_count_session(env);
+        }
+    } else {
+        /*
+         * No change in MMCR0_FC state but, if the PMU is running and
+         * a change in one of the frozen counter bits is made, update
+         * the PMCs with the cycles counted so far.
+         */
+        if (!curr_FC) {
+            if ((curr_value & MMCR0_FC14) != (value & MMCR0_FC14) ||
+                (curr_value & MMCR0_FC56) != (value & MMCR0_FC56)) {
+                pmu_update_cycles(env, curr_value);
+            }
+        }
+    }
+}
+
 static void fire_PMC_interrupt(PowerPCCPU *cpu)
 {
     CPUPPCState *env = &cpu->env;
