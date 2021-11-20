4acf81ab207744ba24eb839cf5428617b2286dd7
Lin Ma <lma@suse.com>
2021-11-20 18:15:12 +0800

    scsi-generic: replace logical block count of response of READ CAPACITY
    While using SCSI passthrough, Following scenario makes qemu doesn't
    realized the capacity change of remote scsi target:
    1. online resize the scsi target.
    2. issue 'rescan-scsi-bus.sh -s ...' in host.
    3. issue 'rescan-scsi-bus.sh -s ...' in vm.
    
    In above scenario I used to experienced errors while accessing the
    additional disk space in vm. I think the reasonable operations should
    be:
    1. online resize the scsi target.
    2. issue 'rescan-scsi-bus.sh -s ...' in host.
    3. issue 'block_resize' via qmp to notify qemu.
    4. issue 'rescan-scsi-bus.sh -s ...' in vm.
    
    The errors disappear once I notify qemu by block_resize via qmp.
    
    So this patch replaces the number of logical blocks of READ CAPACITY
    response from scsi target by qemu's bs->total_sectors. If the user in
    vm wants to access the additional disk space, The administrator of
    host must notify qemu once resizeing the scsi target.
    
    Bonus is that domblkinfo of libvirt can reflect the consistent capacity
    information between host and vm in case of missing block_resize in qemu.
    E.g:
    ...
        <disk type='block' device='lun'>
          <driver name='qemu' type='raw'/>
          <source dev='/dev/sdc' index='1'/>
          <backingStore/>
          <target dev='sda' bus='scsi'/>
          <alias name='scsi0-0-0-0'/>
          <address type='drive' controller='0' bus='0' target='0' unit='0'/>
        </disk>
    ...
    
    Before:
    1. online resize the scsi target.
    2. host:~  # rescan-scsi-bus.sh -s /dev/sdc
    3. guest:~ # rescan-scsi-bus.sh -s /dev/sda
    4  host:~  # virsh domblkinfo --domain $DOMAIN --human --device sda
    Capacity:       4.000 GiB
    Allocation:     0.000 B
    Physical:       8.000 GiB
    
    5. guest:~ # lsblk /dev/sda
    NAME   MAJ:MIN RM SIZE RO TYPE MOUNTPOINT
    sda      8:0    0   8G  0 disk
    └─sda1   8:1    0   2G  0 part
    
    After:
    1. online resize the scsi target.
    2. host:~  # rescan-scsi-bus.sh -s /dev/sdc
    3. guest:~ # rescan-scsi-bus.sh -s /dev/sda
    4  host:~  # virsh domblkinfo --domain $DOMAIN --human --device sda
    Capacity:       4.000 GiB
    Allocation:     0.000 B
    Physical:       8.000 GiB
    
    5. guest:~ # lsblk /dev/sda
    NAME   MAJ:MIN RM SIZE RO TYPE MOUNTPOINT
    sda      8:0    0   4G  0 disk
    └─sda1   8:1    0   2G  0 part
    
    Signed-off-by: Lin Ma <lma@suse.com>
    Message-Id: <20211120101512.7770-1-lma@suse.com>
    Reviewed-by: Philippe Mathieu-Daudé <philmd@redhat.com>

--- hw/scsi/scsi-generic.c
+++ hw/scsi/scsi-generic.c
@@ -315,11 +315,17 @@ static void scsi_read_complete(void * opaque, int ret)
     if (r->req.cmd.buf[0] == READ_CAPACITY_10 &&
         (ldl_be_p(&r->buf[0]) != 0xffffffffU || s->max_lba == 0)) {
         s->blocksize = ldl_be_p(&r->buf[4]);
-        s->max_lba = ldl_be_p(&r->buf[0]) & 0xffffffffULL;
+        BlockBackend *blk = s->conf.blk;
+        BlockDriverState *bs = blk_bs(blk);
+        s->max_lba = bs->total_sectors - 1;
+        stl_be_p(&r->buf[0], s->max_lba);
     } else if (r->req.cmd.buf[0] == SERVICE_ACTION_IN_16 &&
                (r->req.cmd.buf[1] & 31) == SAI_READ_CAPACITY_16) {
         s->blocksize = ldl_be_p(&r->buf[8]);
-        s->max_lba = ldq_be_p(&r->buf[0]);
+        BlockBackend *blk = s->conf.blk;
+        BlockDriverState *bs = blk_bs(blk);
+        s->max_lba = bs->total_sectors - 1;
+        stq_be_p(&r->buf[0], s->max_lba);
     }
     blk_set_guest_block_size(s->conf.blk, s->blocksize);
 
