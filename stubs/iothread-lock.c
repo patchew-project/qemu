544f3309f6a5a217c2f4e8a4274ea2654171bbab
Emanuele Giuseppe Esposito <eesposit@redhat.com>
2021-11-24 01:43:48 -0500

    main-loop.h: introduce qemu_in_main_thread()
    When invoked from the main loop, this function is the same
    as qemu_mutex_iothread_locked, and returns true if the BQL is held.
    When invoked from iothreads or tests, it returns true only
    if the current AioContext is the Main Loop.
    
    This essentially just extends qemu_mutex_iothread_locked to work
    also in unit tests or other users like storage-daemon, that run
    in the Main Loop but end up using the implementation in
    stubs/iothread-lock.c.
    
    Using qemu_mutex_iothread_locked in unit tests defaults to false
    because they use the implementation in stubs/iothread-lock,
    making all assertions added in next patches fail despite the
    AioContext is still the main loop.
    
    Signed-off-by: Emanuele Giuseppe Esposito <eesposit@redhat.com>
    Reviewed-by: Stefan Hajnoczi <stefanha@redhat.com>
    Message-Id: <20211124064418.3120601-2-eesposit@redhat.com>
    Based-on: <20211124063640.3118897-1-eesposit@redhat.com>

--- stubs/iothread-lock.c
+++ stubs/iothread-lock.c
@@ -6,6 +6,11 @@ bool qemu_mutex_iothread_locked(void)
     return false;
 }
 
+bool qemu_in_main_thread(void)
+{
+    return qemu_get_current_aio_context() == qemu_get_aio_context();
+}
+
 void qemu_mutex_lock_iothread_impl(const char *file, int line)
 {
 }
