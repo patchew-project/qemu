6903ce77825d6624f32778aaf4f24d8599c3f7c7
WANG Xuerui <git@xen0n.name>
2021-11-25 02:32:28 +0800

    linux-user: Add safe syscall handling for loongarch64 hosts
    Signed-off-by: WANG Xuerui <git@xen0n.name>
    Reviewed-by: Richard Henderson <richard.henderson@linaro.org>
    Message-Id: <20211124183231.1503090-29-git@xen0n.name>

---
+++ linux-user/host/loongarch64/hostdep.h
@@ -0,0 +1,14 @@
+/*
+ * hostdep.h : things which are dependent on the host architecture
+ *
+ * This work is licensed under the terms of the GNU GPL, version 2 or later.
+ * See the COPYING file in the top-level directory.
+ */
+
+#ifndef LOONGARCH64_HOSTDEP_H
+#define LOONGARCH64_HOSTDEP_H
+
+/* We have a safe-syscall.inc.S */
+#define HAVE_SAFE_SYSCALL
+
+#endif
