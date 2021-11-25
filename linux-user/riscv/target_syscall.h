1f83e9118bcef13e4664120a94fdd92ebbd4e84b
Song Gao <gaosong@loongson.cn>
2021-11-25 16:58:01 +0800

    linux-user: target_syscall.h remove definition TARGET_MINSIGSTKSZ
    TARGET_MINSIGSTKSZ has been defined in generic/signal.h
    or target_signal.h, We don't need to define it again.
    
    Signed-off-by: Song Gao <gaosong@loongson.cn>
    Message-Id: <1637830681-4343-1-git-send-email-gaosong@loongson.cn>
    Reviewed-by: Philippe Mathieu-Daudé <f4bug@amsat.org>
    Reviewed-by: Laurent Vivier <laurent@vivier.eu>

--- linux-user/riscv/target_syscall.h
+++ linux-user/riscv/target_syscall.h
@@ -50,7 +50,6 @@ struct target_pt_regs {
 #endif
 #define UNAME_MINIMUM_RELEASE "4.15.0"
 
-#define TARGET_MINSIGSTKSZ 2048
 #define TARGET_MCL_CURRENT 1
 #define TARGET_MCL_FUTURE  2
 #define TARGET_MCL_ONFAULT 4
