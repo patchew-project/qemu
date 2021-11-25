eb866c10de2dcca902fb4a32b3c89abb11543884
LIU Zhiwei <zhiwei_liu@c-sky.com>
2021-11-25 15:39:42 +0800

    target/riscv: Split pm_enabled into mask and base
    Use cached cur_pmmask and cur_pmbase to infer the
    current PM mode.
    
    This may decrease the TCG IR by one when pm_enabled
    is true and pm_base_enabled is false.
    
    Signed-off-by: LIU Zhiwei <zhiwei_liu@c-sky.com>
    Reviewed-by: Richard Henderson <richard.henderson@linaro.org>
    Reviewed-by: Alistair Francis <alistair.francis@wdc.com>
    Message-Id: <20211125073951.57678-14-zhiwei_liu@c-sky.com>

--- target/riscv/cpu.h
+++ target/riscv/cpu.h
@@ -412,7 +412,8 @@ FIELD(TB_FLAGS, MSTATUS_HS_FS, 11, 2)
 /* The combination of MXL/SXL/UXL that applies to the current cpu mode. */
 FIELD(TB_FLAGS, XL, 13, 2)
 /* If PointerMasking should be applied */
-FIELD(TB_FLAGS, PM_ENABLED, 15, 1)
+FIELD(TB_FLAGS, PM_MASK_ENABLED, 15, 1)
+FIELD(TB_FLAGS, PM_BASE_ENABLED, 16, 1)
 
 #ifdef TARGET_RISCV32
 #define riscv_cpu_mxl(env)  ((void)(env), MXL_RV32)
