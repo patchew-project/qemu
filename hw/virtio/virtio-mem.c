2d0c9ed401752b796f07e27fb967e023a3d6c8e3
Gavin Shan <gshan@redhat.com>
2021-11-30 08:33:28 +0800

    hw/arm/virt: Support for virtio-mem-pci
    This supports virtio-mem-pci device on "virt" platform, by simply
    following the implementation on x86.
    
       * The patch was written by David Hildenbrand <david@redhat.com>
         modified by Jonathan Cameron <Jonathan.Cameron@huawei.com>
    
       * This implements the hotplug handlers to support virtio-mem-pci
         device hot-add, while the hot-remove isn't supported as we have
         on x86.
    
       * The block size is 1GB on ARM64 instead of 128MB on x86.
    
       * It has been passing the tests with various combinations like 64KB
         and 4KB page sizes on host and guest, different memory device
         backends like normal, transparent huge page and HugeTLB, plus
         migration.
    
    Signed-off-by: Gavin Shan <gshan@redhat.com>
    Message-Id: <20211130003328.201270-2-gshan@redhat.com>

--- hw/virtio/virtio-mem.c
+++ hw/virtio/virtio-mem.c
@@ -126,6 +126,8 @@ static uint64_t virtio_mem_default_block_size(RAMBlock *rb)
  */
 #if defined(TARGET_X86_64) || defined(TARGET_I386)
 #define VIRTIO_MEM_USABLE_EXTENT (2 * (128 * MiB))
+#elif defined(TARGET_ARM)
+#define VIRTIO_MEM_USABLE_EXTENT (2 * (1024 * MiB))
 #else
 #error VIRTIO_MEM_USABLE_EXTENT not defined
 #endif
