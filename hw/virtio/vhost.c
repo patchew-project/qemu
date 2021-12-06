a5515cd82465db8f39305fdddec24807f9cdd4b8
Jonah Palmer <jonah.palmer@oracle.com>
2021-12-06 07:43:20 -0500

    virtio: add vhost support for virtio devices
    This patch adds a get_vhost() callback function for VirtIODevices that
    returns the device's corresponding vhost_dev structure, if the vhost
    device is running. This patch also adds a vhost_started flag for
    VirtIODevices.
    
    Previously, a VirtIODevice wouldn't be able to tell if its corresponding
    vhost device was active or not.
    
    Signed-off-by: Jonah Palmer <jonah.palmer@oracle.com>
    Message-Id: <1638794606-19631-3-git-send-email-jonah.palmer@oracle.com>

--- hw/virtio/vhost.c
+++ hw/virtio/vhost.c
@@ -1745,6 +1745,7 @@ int vhost_dev_start(struct vhost_dev *hdev, VirtIODevice *vdev)
     /* should only be called after backend is connected */
     assert(hdev->vhost_ops);
 
+    vdev->vhost_started = true;
     hdev->started = true;
     hdev->vdev = vdev;
 
@@ -1819,7 +1820,7 @@ fail_vq:
 
 fail_mem:
 fail_features:
-
+    vdev->vhost_started = false;
     hdev->started = false;
     return r;
 }
@@ -1850,6 +1851,7 @@ void vhost_dev_stop(struct vhost_dev *hdev, VirtIODevice *vdev)
     }
     vhost_log_put(hdev, true);
     hdev->started = false;
+    vdev->vhost_started = false;
     hdev->vdev = NULL;
 }
 
