678201915d806b1fc5ff7a854f0f6c52414bd205
Francisco Iglesias <francisco.iglesias@xilinx.com>
2021-12-14 11:03:47 +0000

    hw/dma: Add the DMA control interface
    An option on real hardware when embedding a DMA engine into a peripheral
    is to make the peripheral control the engine through a custom DMA control
    (hardware) interface between the two. Software drivers in this scenario
    configure and trigger DMA operations through the controlling peripheral's
    register API (for example, writing a specific bit in a register could
    propagate down to a transfer start signal on the DMA control interface).
    At the same time the status, results and interrupts for the transfer might
    still be intended to be read and caught through the DMA engine's register
    API (and signals).
    
    This patch adds a QEMU DMA control interface that can be used for
    modelling above scenario. Through this new interface a peripheral model
    embedding a DMA engine model will be able to directly initiate transfers
    through the DMA. At the same time the transfer state, result and
    completion signaling will be read and caught through the DMA engine
    model's register API and signaling.
    
    Signed-off-by: Francisco Iglesias <francisco.iglesias@xilinx.com>
    Message-Id: <20211214110354.21816-6-francisco.iglesias@xilinx.com>

---
+++ include/hw/dma/dma-ctrl-if.h
@@ -0,0 +1,51 @@
+/*
+ * DMA control interface.
+ *
+ * Copyright (c) 2021 Xilinx Inc.
+ * Written by Francisco Iglesias <francisco.iglesias@xilinx.com>
+ *
+ * SPDX-License-Identifier: GPL-2.0-or-later
+ */
+#ifndef HW_DMA_CTRL_IF_H
+#define HW_DMA_CTRL_IF_H
+
+#include "hw/hw.h"
+#include "qom/object.h"
+
+#define TYPE_DMA_CTRL_IF "dma-ctrl-if"
+typedef struct DmaCtrlIfClass DmaCtrlIfClass;
+DECLARE_CLASS_CHECKERS(DmaCtrlIfClass, DMA_CTRL_IF,
+                       TYPE_DMA_CTRL_IF)
+
+#define DMA_CTRL_IF(obj) \
+     INTERFACE_CHECK(DmaCtrlIf, (obj), TYPE_DMA_CTRL_IF)
+
+typedef struct DmaCtrlIf {
+    Object Parent;
+} DmaCtrlIf;
+
+typedef struct DmaCtrlIfClass {
+    InterfaceClass parent;
+
+    /*
+     * read: Start a read transfer on the DMA engine implementing the DMA
+     * control interface
+     *
+     * @dma_ctrl: the DMA engine implementing this interface
+     * @addr: the address to read
+     * @len: the number of bytes to read at 'addr'
+     */
+    void (*read)(DmaCtrlIf *dma, hwaddr addr, uint32_t len);
+} DmaCtrlIfClass;
+
+/*
+ * Start a read transfer on a DMA engine implementing the DMA control
+ * interface.
+ *
+ * @dma_ctrl: the DMA engine implementing this interface
+ * @addr: the address to read
+ * @len: the number of bytes to read at 'addr'
+ */
+void dma_ctrl_if_read(DmaCtrlIf *dma, hwaddr addr, uint32_t len);
+
+#endif /* HW_DMA_CTRL_IF_H */
