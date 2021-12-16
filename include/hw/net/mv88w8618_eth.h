3d9a8d84cc8d67d9919aac5f5354a11488a4e5b5
Philippe Mathieu-Daudé <f4bug@amsat.org>
2021-12-16 09:45:30 +0100

    hw/net: Move MV88W8618 network device out of hw/arm/ directory
    The Marvell 88W8618 network device is hidden in the Musicpal
    machine. Move it into a new unit file under the hw/net/ directory.
    
    Signed-off-by: Philippe Mathieu-Daudé <f4bug@amsat.org>
    Message-Id: <20211216084530.698125-3-f4bug@amsat.org>

---
+++ include/hw/net/mv88w8618_eth.h
@@ -0,0 +1,16 @@
+/*
+ * Marvell MV88W8618 / Freecom MusicPal emulation.
+ *
+ * Copyright (c) 2008 Jan Kiszka
+ *
+ * This code is licensed under the GNU GPL v2.
+ *
+ * Contributions after 2012-01-13 are licensed under the terms of the
+ * GNU GPL, version 2 or (at your option) any later version.
+ */
+#ifndef HW_NET_MV88W8618_H
+#define HW_NET_MV88W8618_H
+
+#define TYPE_MV88W8618_ETH "mv88w8618_eth"
+
+#endif
