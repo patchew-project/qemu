760e89d5de1d1d40c1040ebb83ebd9b90254381e
Philippe Mathieu-Daudé <philmd@redhat.com>
2021-12-16 13:35:58 +0100

    dma: Let dma_buf_read() / dma_buf_write() propagate MemTxResult
    Since the previous commit, dma_buf_rw() returns a MemTxResult
    type. Do not discard it, return it to the caller.
    
    Since both dma_buf_read/dma_buf_write functions were previously
    returning the QEMUSGList size not consumed, add an extra argument
    where the unconsummed size can be stored.
    
    Update the few callers.
    
    Signed-off-by: Philippe Mathieu-Daudé <philmd@redhat.com>
    Message-Id: <20211216123558.799425-9-philmd@redhat.com>
    Reviewed-by: Klaus Jensen <k.jensen@samsung.com>
    Based-on: <20210702092439.989969-1-philmd@redhat.com>

--- include/sysemu/dma.h
+++ include/sysemu/dma.h
@@ -302,8 +302,10 @@ BlockAIOCB *dma_blk_read(BlockBackend *blk,
 BlockAIOCB *dma_blk_write(BlockBackend *blk,
                           QEMUSGList *sg, uint64_t offset, uint32_t align,
                           BlockCompletionFunc *cb, void *opaque);
-uint64_t dma_buf_read(void *ptr, int32_t len, QEMUSGList *sg, MemTxAttrs attrs);
-uint64_t dma_buf_write(void *ptr, int32_t len, QEMUSGList *sg, MemTxAttrs attrs);
+MemTxResult dma_buf_read(void *ptr, int32_t len, uint64_t *residp,
+                         QEMUSGList *sg, MemTxAttrs attrs);
+MemTxResult dma_buf_write(void *ptr, int32_t len, uint64_t *residp,
+                          QEMUSGList *sg, MemTxAttrs attrs);
 
 void dma_acct_start(BlockBackend *blk, BlockAcctCookie *cookie,
                     QEMUSGList *sg, enum BlockAcctType type);
