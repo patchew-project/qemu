94717ff4ccad9404df9cd4080ee99e50c0e4b335
Alexandre Ratchov <alex@caoua.org>
2021-12-17 10:38:32 +0100

    audio: Add sndio backend
    sndio is the native API used by OpenBSD, although it has been ported to
    other *BSD's and Linux (packages for Ubuntu, Debian, Void, Arch, etc.).
    
    Signed-off-by: Brad Smith <brad@comstyle.com>
    Signed-off-by: Alexandre Ratchov <alex@caoua.org>
    Message-Id: <YbxamMLKHp3IbtlW@moule.localdomain>
    Tested-by: Volker Rümelin <vr_qemu@t-online.de>
    Reviewed-by: Volker Rümelin <vr_qemu@t-online.de>

--- audio/audio.c
+++ audio/audio.c
@@ -2005,6 +2005,7 @@ void audio_create_pdos(Audiodev *dev)
         CASE(OSS, oss, Oss);
         CASE(PA, pa, Pa);
         CASE(SDL, sdl, Sdl);
+        CASE(SNDIO, sndio, );
         CASE(SPICE, spice, );
         CASE(WAV, wav, );
 
