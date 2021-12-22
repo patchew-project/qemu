83b15ab811a4a03680761b1589e7ecbc4b84edd9
Vladimir Sementsov-Ogievskiy <vsementsov@virtuozzo.com>
2021-12-22 17:02:52 +0100

    block/export: add blk_by_export_id()
    Signed-off-by: Vladimir Sementsov-Ogievskiy <vsementsov@virtuozzo.com>
    Message-Id: <20211222160254.226909-3-vsementsov@virtuozzo.com>

--- include/sysemu/block-backend.h
+++ include/sysemu/block-backend.h
@@ -124,6 +124,7 @@ DeviceState *blk_get_attached_dev(BlockBackend *blk);
 char *blk_get_attached_dev_id(BlockBackend *blk);
 BlockBackend *blk_by_dev(void *dev);
 BlockBackend *blk_by_qdev_id(const char *id, Error **errp);
+BlockBackend *blk_by_export_id(const char *id, Error **errp);
 void blk_set_dev_ops(BlockBackend *blk, const BlockDevOps *ops, void *opaque);
 int coroutine_fn blk_co_preadv(BlockBackend *blk, int64_t offset,
                                int64_t bytes, QEMUIOVector *qiov,
