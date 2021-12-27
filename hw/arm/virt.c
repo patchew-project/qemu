e04ccc1db1d59f0d8a0dd6844cd1d4a117823f32
Marc Zyngier <maz@kernel.org>
2021-12-27 21:16:39 +0000

    hw/arm/virt: Add a control for the the highmem redistributors
    Just like we can control the enablement of the highmem PCIe region
    using highmem_ecam, let's add a control for the highmem GICv3
    redistributor region.
    
    Similarily to highmem_ecam, these redistributors are disabled when
    highmem is off.
    
    Reviewed-by: Andrew Jones <drjones@redhat.com>
    Signed-off-by: Marc Zyngier <maz@kernel.org>
    Message-Id: <20211227211642.994461-3-maz@kernel.org>

--- hw/arm/virt.c
+++ hw/arm/virt.c
@@ -2105,6 +2105,8 @@ static void machvirt_init(MachineState *machine)
 
     virt_flash_fdt(vms, sysmem, secure_sysmem ?: sysmem);
 
+    vms->highmem_redists &= vms->highmem;
+
     create_gic(vms, sysmem);
 
     virt_cpu_post_init(vms, sysmem);
@@ -2802,6 +2804,7 @@ static void virt_instance_init(Object *obj)
     vms->gic_version = VIRT_GIC_VERSION_NOSEL;
 
     vms->highmem_ecam = !vmc->no_highmem_ecam;
+    vms->highmem_redists = true;
 
     if (vmc->no_its) {
         vms->its = false;
