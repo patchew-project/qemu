a8f3aa0ddacb805daa6140974d694385c2ebcd96
Emanuele Giuseppe Esposito <eesposit@redhat.com>
2022-01-05 09:02:08 -0500

    block_job_query: remove atomic read
    Not sure what the atomic here was supposed to do, since job.busy
    is protected by the job lock. Since the whole function
    is called under job_mutex, just remove the atomic.
    
    Signed-off-by: Emanuele Giuseppe Esposito <eesposit@redhat.com>
    Message-Id: <20220105140208.365608-17-eesposit@redhat.com>
    Based-on: <20211124064418.3120601-1-eesposit@redhat.com>

--- blockjob.c
+++ blockjob.c
@@ -335,13 +335,13 @@ BlockJobInfo *block_job_query(BlockJob *job, Error **errp)
     info = g_new0(BlockJobInfo, 1);
     info->type      = g_strdup(job_type_str(&job->job));
     info->device    = g_strdup(job->job.id);
-    info->busy      = qatomic_read(&job->job.busy);
+    info->busy      = job->job.busy;
     info->paused    = job->job.pause_count > 0;
     info->offset    = progress_current;
     info->len       = progress_total;
     info->speed     = job->speed;
     info->io_status = job->iostatus;
-    info->ready     = job_is_ready(&job->job),
+    info->ready     = job_is_ready_locked(&job->job),
     info->status    = job->job.status;
     info->auto_finalize = job->job.auto_finalize;
     info->auto_dismiss  = job->job.auto_dismiss;
