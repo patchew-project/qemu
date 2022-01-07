f293ccda794f0bfdb6a0cd20b3fb9cab83f27e61
Atish Patra <atish.patra@wdc.com>
2022-01-06 16:48:41 -0800

    target/riscv: Add support for hpmcounters/hpmevents
    With SBI PMU extension, user can use any of the available hpmcounters to
    track any perf events based on the value written to mhpmevent csr.
    Add read/write functionality for these csrs.
    
    Signed-off-by: Atish Patra <atish.patra@wdc.com>
    Signed-off-by: Atish Patra <atishp@rivosinc.com>
    Message-Id: <20220107004846.378859-7-atishp@rivosinc.com>

--- target/riscv/cpu.h
+++ target/riscv/cpu.h
@@ -101,6 +101,8 @@ typedef struct CPURISCVState CPURISCVState;
 #endif
 
 #define RV_VLEN_MAX 1024
+#define RV_MAX_MHPMEVENTS 29
+#define RV_MAX_MHPMCOUNTERS 32
 
 FIELD(VTYPE, VLMUL, 0, 3)
 FIELD(VTYPE, VSEW, 3, 3)
@@ -223,6 +225,15 @@ struct CPURISCVState {
     target_ulong mcounteren;
 
     target_ulong mcountinhibit;
+
+    /* PMU counter configured values */
+    target_ulong mhpmcounter_val[RV_MAX_MHPMCOUNTERS];
+
+    /* for RV32 */
+    target_ulong mhpmcounterh_val[RV_MAX_MHPMCOUNTERS];
+
+    /* PMU event selector configured values */
+    target_ulong mhpmevent_val[RV_MAX_MHPMEVENTS];
 
     target_ulong sscratch;
     target_ulong mscratch;
