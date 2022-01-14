a0a259e009d23e508c0ef380189e5477be5a2198
Francisco Iglesias <francisco.iglesias@xilinx.com>
2022-01-14 15:28:34 +0000

    hw/dma: Add the DMA control interface
    An option on real hardware when embedding a DMA engine into a peripheral
    is to make the peripheral control the engine through a custom DMA control
    (hardware) interface between the two. Software drivers in this scenario
    configure and trigger DMA operations through the controlling peripheral's
    register API (for example, writing a specific bit in a register could
    propagate down to a transfer start signal on the DMA control interface).
    At the same time the status, results and interrupts for the transfer might
    still be intended to be read and caught through the DMA engine's register
    API (and signals).
    
    This patch adds a QEMU DMA control interface that can be used for
    modelling above scenario. Through this new interface a peripheral model
    embedding a DMA engine model will be able to directly initiate transfers
    through the DMA. At the same time the transfer state, result and
    completion signaling will be read and caught through the DMA engine
    model's register API and signaling.
    
    Signed-off-by: Francisco Iglesias <francisco.iglesias@xilinx.com>
    Message-Id: <20220114152841.1740-6-francisco.iglesias@xilinx.com>

---
+++ hw/dma/dma-ctrl-if.c
@@ -0,0 +1,30 @@
+/*
+ * DMA control interface.
+ *
+ * Copyright (c) 2021 Xilinx Inc.
+ * Written by Francisco Iglesias <francisco.iglesias@xilinx.com>
+ *
+ * SPDX-License-Identifier: GPL-2.0-or-later
+ */
+#include "qemu/osdep.h"
+#include "exec/hwaddr.h"
+#include "hw/dma/dma-ctrl-if.h"
+
+MemTxResult dma_ctrl_if_read(DmaCtrlIf *dma, hwaddr addr, uint32_t len)
+{
+    DmaCtrlIfClass *dcic =  DMA_CTRL_IF_GET_CLASS(dma);
+    return dcic->read(dma, addr, len);
+}
+
+static const TypeInfo dma_ctrl_if_info = {
+    .name          = TYPE_DMA_CTRL_IF,
+    .parent        = TYPE_INTERFACE,
+    .class_size = sizeof(DmaCtrlIfClass),
+};
+
+static void dma_ctrl_if_register_types(void)
+{
+    type_register_static(&dma_ctrl_if_info);
+}
+
+type_init(dma_ctrl_if_register_types)
