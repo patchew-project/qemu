f6eb4029c08852cd807acc5a004a9569acdb2f94
Weiwei Li <liweiwei@iscas.ac.cn>
2022-01-18 09:17:11 +0800

    target/riscv: add support for svpbmt extension
    - add PTE_PBMT bits: It uses two PTE bits, but otherwise has no effect on QEMU, since QEMU is sequentially consistent and doesn't model PMAs currently
    - add PTE_PBMT bit check for inner PTE
    
    Signed-off-by: Weiwei Li <liweiwei@iscas.ac.cn>
    Signed-off-by: Junqiang Wang <wangjunqiang@iscas.ac.cn>
    Cc: Heiko Stuebner <heiko@sntech.de>
    Cc: Anup Patel <anup@brainfault.org>
    Message-Id: <20220118011711.7243-6-liweiwei@iscas.ac.cn>

--- target/riscv/cpu_helper.c
+++ target/riscv/cpu_helper.c
@@ -625,9 +625,11 @@ restart:
         if (!(pte & PTE_V)) {
             /* Invalid PTE */
             return TRANSLATE_FAIL;
+        } else if (!cpu->cfg.ext_svpbmt && (pte & (target_ulong)PTE_PBMT)) {
+            return TRANSLATE_FAIL;
         } else if (!(pte & (PTE_R | PTE_W | PTE_X))) {
             /* Inner PTE, continue walking */
-            if (pte & (target_ulong)(PTE_D | PTE_A | PTE_U | PTE_N)) {
+            if (pte & (target_ulong)(PTE_D | PTE_A | PTE_U | PTE_ATTR)) {
                 return TRANSLATE_FAIL;
             }
             base = ppn << PGSHIFT;
