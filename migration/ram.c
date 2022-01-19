94a8e6d1c47f2e3f6607b6f1d286168df27e9256
Peter Xu <peterx@redhat.com>
2022-01-19 16:09:25 +0800

    migration: Add pss.postcopy_requested status
    This boolean flag shows whether the current page during migration is triggered
    by postcopy or not.  Then in ram_save_host_page() and deeper stack we'll be
    able to have a reference on the priority of this page.
    
    Signed-off-by: Peter Xu <peterx@redhat.com>
    Message-Id: <20220119080929.39485-12-peterx@redhat.com>
    Based-on: <20211224065000.97572-1-peterx@redhat.com>

--- migration/ram.c
+++ migration/ram.c
@@ -400,6 +400,8 @@ struct PageSearchStatus {
     unsigned long page;
     /* Set once we wrap around */
     bool         complete_round;
+    /* Whether current page is explicitly requested by postcopy */
+    bool         postcopy_requested;
 };
 typedef struct PageSearchStatus PageSearchStatus;
 
@@ -1480,6 +1482,9 @@ retry:
  */
 static bool find_dirty_block(RAMState *rs, PageSearchStatus *pss, bool *again)
 {
+    /* This is not a postcopy requested page */
+    pss->postcopy_requested = false;
+
     pss->page = migration_bitmap_find_dirty(rs, pss->block, pss->page);
     if (pss->complete_round && pss->block == rs->last_seen_block &&
         pss->page >= rs->last_page) {
@@ -1971,6 +1976,7 @@ static bool get_queued_page(RAMState *rs, PageSearchStatus *pss)
          * really rare.
          */
         pss->complete_round = false;
+        pss->postcopy_requested = true;
     }
 
     return !!block;
