7bbb1e1abd7419b322f13d0cbee7c0f95bd35a4e
Klaus Jensen <k.jensen@samsung.com>
2022-01-20 09:01:55 +0100

    hw/nvme: fix CVE-2021-3929
    This fixes CVE-2021-3929 "locally" by denying DMA to the iomem of the
    device itself. This still allows DMA to MMIO regions of other devices
    (e.g. doing P2P DMA to the controller memory buffer of another NVMe
    device).
    
    Fixes: CVE-2021-3929
    Reported-by: Qiuhao Li <Qiuhao.Li@outlook.com>
    Signed-off-by: Klaus Jensen <k.jensen@samsung.com>
    Message-Id: <20220120080155.227246-1-its@irrelevant.dk>
    Reviewed-by: Keith Busch <kbusch@kernel.org>

--- hw/nvme/ctrl.c
+++ hw/nvme/ctrl.c
@@ -357,6 +357,16 @@ static inline void *nvme_addr_to_pmr(NvmeCtrl *n, hwaddr addr)
     return memory_region_get_ram_ptr(&n->pmr.dev->mr) + (addr - n->pmr.cba);
 }
 
+static inline bool nvme_addr_is_iomem(NvmeCtrl *n, hwaddr addr)
+{
+    hwaddr hi, lo;
+
+    lo = n->bar0.addr;
+    hi = lo + int128_get64(n->bar0.size);
+
+    return addr >= lo && addr < hi;
+}
+
 static int nvme_addr_read(NvmeCtrl *n, hwaddr addr, void *buf, int size)
 {
     hwaddr hi = addr + size - 1;
@@ -613,6 +623,10 @@ static uint16_t nvme_map_addr(NvmeCtrl *n, NvmeSg *sg, hwaddr addr, size_t len)
     }
 
     trace_pci_nvme_map_addr(addr, len);
+
+    if (nvme_addr_is_iomem(n, addr)) {
+        return NVME_DATA_TRAS_ERROR;
+    }
 
     if (nvme_addr_is_cmb(n, addr)) {
         cmb = true;
