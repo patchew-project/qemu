d06057ac437f71e02e9d55fa668ab53dc104dad7
Frank Chang <frank.chang@sifive.com>
2022-01-26 11:13:43 +0800

    hw/sd: Correct CMD58's R3 response "in idle state" bit in SPI-mode
    In SPI-mode, CMD58 returns R3 response with the format:
    
    39          32 31                                  0
    +------------+ +-----------------------------------+
    |     R1     | |                OCR                |
    +------------+ +-----------------------------------+
    
    Where R1 has bits[0] indicating whether SD card is "in idle state".
    However, according to SD card state transition table, CMD58 can only be
    transited from trans to data state, which the "in idle state" bit should
    not be set in CMD58's R3 response.
    (But CMD8 should still have "in idle state" bit to be set in its
    R7 response because it can only be transited from idle to idle state.)
    
    Signed-off-by: Frank Chang <frank.chang@sifive.com>
    Message-Id: <20220126031345.3372-1-frank.chang@sifive.com>

--- hw/sd/ssi-sd.c
+++ hw/sd/ssi-sd.c
@@ -176,11 +176,16 @@ static uint32_t ssi_sd_transfer(SSIPeripheral *dev, uint32_t val)
                 s->arglen = 1;
                 s->response[0] = 4;
                 DPRINTF("SD command failed\n");
-            } else if (s->cmd == 8 || s->cmd == 58) {
-                /* CMD8/CMD58 returns R3/R7 response */
-                DPRINTF("Returned R3/R7\n");
+            } else if (s->cmd == 8) {
+                /* CMD8 returns R7 response */
+                DPRINTF("Returned R7\n");
                 s->arglen = 5;
                 s->response[0] = 1;
+                memcpy(&s->response[1], longresp, 4);
+            } else if (s->cmd == 58) {
+                /* CMD58 returns R3 response */
+                DPRINTF("Returned R3\n");
+                s->arglen = 5;
                 memcpy(&s->response[1], longresp, 4);
             } else if (s->arglen != 4) {
                 BADF("Unexpected response to cmd %d\n", s->cmd);
