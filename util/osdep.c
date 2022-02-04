a8c8940bd22660837208c90b74ba97d3f2f01b86
Vitaly Chikunov <vt@altlinux.org>
2022-02-04 08:06:09 +0300

    9pfs: Fix segfault in do_readdir_many caused by struct dirent overread
    `struct dirent' returned from readdir(3) could be shorter (or longer)
    than `sizeof(struct dirent)', thus memcpy of sizeof length will overread
    into unallocated page causing SIGSEGV. Example stack trace:
    
     #0  0x00005555559ebeed v9fs_co_readdir_many (/usr/bin/qemu-system-x86_64 + 0x497eed)
     #1  0x00005555559ec2e9 v9fs_readdir (/usr/bin/qemu-system-x86_64 + 0x4982e9)
     #2  0x0000555555eb7983 coroutine_trampoline (/usr/bin/qemu-system-x86_64 + 0x963983)
     #3  0x00007ffff73e0be0 n/a (n/a + 0x0)
    
    While fixing, provide a helper for any future `struct dirent' cloning.
    
    Resolves: https://gitlab.com/qemu-project/qemu/-/issues/841
    Cc: qemu-stable@nongnu.org
    Co-authored-by: Christian Schoenebeck <qemu_oss@crudebyte.com>
    Signed-off-by: Vitaly Chikunov <vt@altlinux.org>
    Message-Id: <20220204050609.15510-1-vt@altlinux.org>
    Reviewed-by: Christian Schoenebeck <qemu_oss@crudebyte.com>
    Reviewed-by: Dmitry V. Levin <ldv@altlinux.org>
    Reviewed-by: Dmitry V. Levin" <ldv@altlinux.org>

--- util/osdep.c
+++ util/osdep.c
@@ -33,6 +33,7 @@
 extern int madvise(char *, size_t, int);
 #endif
 
+#include <dirent.h>
 #include "qemu-common.h"
 #include "qemu/cutils.h"
 #include "qemu/sockets.h"
@@ -615,3 +616,20 @@ writev(int fd, const struct iovec *iov, int iov_cnt)
     return readv_writev(fd, iov, iov_cnt, true);
 }
 #endif
+
+struct dirent *
+qemu_dirent_dup(struct dirent *dent)
+{
+    size_t sz = 0;
+#if defined _DIRENT_HAVE_D_RECLEN
+    /* Avoid use of strlen() if there's d_reclen. */
+    sz = dent->d_reclen;
+#endif
+    if (sz == 0) {
+        /* Fallback to the most portable way. */
+        sz = offsetof(struct dirent, d_name) +
+                      strlen(dent->d_name) + 1;
+    }
+    struct dirent *dst = g_malloc(sz);
+    return memcpy(dst, dent, sz);
+}
