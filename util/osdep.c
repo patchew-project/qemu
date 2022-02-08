a4d29946cbb8b5897dfc0f78aaf798496754f0e6
Peter Maydell <peter.maydell@linaro.org>
2022-02-08 20:08:53 +0000

    include: Move qemu_mprotect_*() to new qemu/mprotect.h
    The qemu_mprotect_*() family of functions are used in very few files;
    move them from osdep.h to a new qemu/mprotect.h.
    
    Signed-off-by: Peter Maydell <peter.maydell@linaro.org>
    Message-Id: <20220208200856.3558249-3-peter.maydell@linaro.org>
    Reviewed-by: Philippe Mathieu-Daudé <f4bug@amsat.org>

--- util/osdep.c
+++ util/osdep.c
@@ -38,6 +38,7 @@ extern int madvise(char *, size_t, int);
 #include "qemu/sockets.h"
 #include "qemu/error-report.h"
 #include "qemu/madvise.h"
+#include "qemu/mprotect.h"
 #include "monitor/monitor.h"
 
 static bool fips_enabled = false;
