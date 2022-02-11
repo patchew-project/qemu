47671aa6d1ba1a3b5b2be3a0d4f79191dd43d227
Emanuele Giuseppe Esposito <eesposit@redhat.com>
2022-02-11 09:51:46 -0500

    block/coroutines: I/O and "I/O or GS" API
    block coroutines functions run in different aiocontext, and are
    not protected by the BQL. Therefore are I/O.
    
    On the other side, generated_co_wrapper functions use BDRV_POLL_WHILE,
    meaning the caller can either be the main loop or a specific iothread.
    
    Signed-off-by: Emanuele Giuseppe Esposito <eesposit@redhat.com>
    Message-Id: <20220211145153.2861415-25-eesposit@redhat.com>
    Based-on: <20220209105452.1694545-1-eesposit@redhat.com>

--- block.c
+++ block.c
@@ -5453,6 +5453,7 @@ fail:
 int coroutine_fn bdrv_co_check(BlockDriverState *bs,
                                BdrvCheckResult *res, BdrvCheckMode fix)
 {
+    IO_CODE();
     if (bs->drv == NULL) {
         return -ENOMEDIUM;
     }
@@ -6662,6 +6663,7 @@ int bdrv_activate(BlockDriverState *bs, Error **errp)
 int coroutine_fn bdrv_co_invalidate_cache(BlockDriverState *bs, Error **errp)
 {
     Error *local_err = NULL;
+    IO_CODE();
 
     assert(!(bs->open_flags & BDRV_O_INACTIVE));
 
