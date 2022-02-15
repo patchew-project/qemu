74646e660c7dce0010f990bd866d99f1db66f037
Fabiano Rosas <farosas@linux.ibm.com>
2022-02-15 18:41:45 -0300

    target/ppc: cpu_init: Expose some SPR registration helpers
    The following patches will move CPU-specific code into separate files,
    so expose the most used SPR registration functions:
    
    register_sdr1_sprs         | 22 callers
    register_low_BATs          | 20 callers
    register_non_embedded_sprs | 19 callers
    register_high_BATs         | 10 callers
    register_thrm_sprs         | 8 callers
    register_usprgh_sprs       | 6 callers
    register_soft_tlb_sprs     | only 3 callers, but it helps to
                                 keep the soft TLB code consistent.
    
    Signed-off-by: Fabiano Rosas <farosas@linux.ibm.com>
    Message-Id: <20220215214148.1848266-25-farosas@linux.ibm.com>

--- target/ppc/spr_tcg.h
+++ target/ppc/spr_tcg.h
@@ -141,4 +141,12 @@ void spr_write_hmer(DisasContext *ctx, int sprn, int gprn);
 void spr_write_lpcr(DisasContext *ctx, int sprn, int gprn);
 #endif
 
+void register_low_BATs(CPUPPCState *env);
+void register_high_BATs(CPUPPCState *env);
+void register_sdr1_sprs(CPUPPCState *env);
+void register_thrm_sprs(CPUPPCState *env);
+void register_usprgh_sprs(CPUPPCState *env);
+void register_non_embedded_sprs(CPUPPCState *env);
+void register_soft_tlb_sprs(CPUPPCState *env, int nb_tlbs, int nb_ways);
+
 #endif
