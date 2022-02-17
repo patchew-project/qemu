51a93ba21515a4aa5626178b9ee593ef50a3cd58
Pierre Morel <pmorel@linux.ibm.com>
2022-02-17 14:41:20 +0100

    s390x: topology: Adding books to CPU topology
    S390 CPU topology may have up to 5 topology containers.
    The first container above the cores is level 2, the sockets.
    We introduce here the books, book is the level containing sockets.
    
    Let's add books, level3, containers to the CPU topology.
    
    Signed-off-by: Pierre Morel <pmorel@linux.ibm.com>
    Message-Id: <20220217134125.132150-7-pmorel@linux.ibm.com>

--- include/hw/boards.h
+++ include/hw/boards.h
@@ -130,11 +130,13 @@ typedef struct {
  * @prefer_sockets - whether sockets are preferred over cores in smp parsing
  * @dies_supported - whether dies are supported by the machine
  * @clusters_supported - whether clusters are supported by the machine
+ * @books_supported - whether books are supported by the machine
  */
 typedef struct {
     bool prefer_sockets;
     bool dies_supported;
     bool clusters_supported;
+    bool books_supported;
 } SMPCompatProps;
 
 /**
@@ -299,6 +301,7 @@ typedef struct DeviceMemoryState {
 /**
  * CpuTopology:
  * @cpus: the number of present logical processors on the machine
+ * @books: the number of books on the machine
  * @sockets: the number of sockets on the machine
  * @dies: the number of dies in one socket
  * @clusters: the number of clusters in one die
@@ -308,6 +311,7 @@ typedef struct DeviceMemoryState {
  */
 typedef struct CpuTopology {
     unsigned int cpus;
+    unsigned int books;
     unsigned int sockets;
     unsigned int dies;
     unsigned int clusters;
