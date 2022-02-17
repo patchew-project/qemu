740004ae28dc21e11f4d6c5c37dd16a3d599c6b9
Zeng Guang <guang.zeng@intel.com>
2022-02-16 22:04:33 -0800

    x86: Support XFD and AMX xsave data migration
    XFD(eXtended Feature Disable) allows to enable a
    feature on xsave state while preventing specific
    user threads from using the feature.
    
    Support save and restore XFD MSRs if CPUID.D.1.EAX[4]
    enumerate to be valid. Likewise migrate the MSRs and
    related xsave state necessarily.
    
    Signed-off-by: Zeng Guang <guang.zeng@intel.com>
    Signed-off-by: Wei Wang <wei.w.wang@intel.com>
    Signed-off-by: Yang Zhong <yang.zhong@intel.com>
    Message-Id: <20220217060434.52460-8-yang.zhong@intel.com>

--- target/i386/kvm/kvm.c
+++ target/i386/kvm/kvm.c
@@ -3275,6 +3275,13 @@ static int kvm_put_msrs(X86CPU *cpu, int level)
                               env->msr_ia32_sgxlepubkeyhash[3]);
         }
 
+        if (env->features[FEAT_XSAVE] & CPUID_D_1_EAX_XFD) {
+            kvm_msr_entry_add(cpu, MSR_IA32_XFD,
+                              env->msr_xfd);
+            kvm_msr_entry_add(cpu, MSR_IA32_XFD_ERR,
+                              env->msr_xfd_err);
+        }
+
         /* Note: MSR_IA32_FEATURE_CONTROL is written separately, see
          *       kvm_put_msr_feature_control. */
     }
@@ -3667,6 +3674,11 @@ static int kvm_get_msrs(X86CPU *cpu)
         kvm_msr_entry_add(cpu, MSR_IA32_SGXLEPUBKEYHASH3, 0);
     }
 
+    if (env->features[FEAT_XSAVE] & CPUID_D_1_EAX_XFD) {
+        kvm_msr_entry_add(cpu, MSR_IA32_XFD, 0);
+        kvm_msr_entry_add(cpu, MSR_IA32_XFD_ERR, 0);
+    }
+
     ret = kvm_vcpu_ioctl(CPU(cpu), KVM_GET_MSRS, cpu->kvm_msr_buf);
     if (ret < 0) {
         return ret;
@@ -3962,6 +3974,12 @@ static int kvm_get_msrs(X86CPU *cpu)
         case MSR_IA32_SGXLEPUBKEYHASH0 ... MSR_IA32_SGXLEPUBKEYHASH3:
             env->msr_ia32_sgxlepubkeyhash[index - MSR_IA32_SGXLEPUBKEYHASH0] =
                            msrs[i].data;
+            break;
+        case MSR_IA32_XFD:
+            env->msr_xfd = msrs[i].data;
+            break;
+        case MSR_IA32_XFD_ERR:
+            env->msr_xfd_err = msrs[i].data;
             break;
         }
     }
