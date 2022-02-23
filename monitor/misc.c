512c398ccffc12fe616e514942710e89b192ab5e
Mirela Grujic <mirela.grujic@greensocs.com>
2022-02-23 10:06:58 +0100

    qapi/device_add: Allow execution in machine initialized phase
    This commit allows to use the QMP command to add a cold-plugged
    device like we can do with the CLI option -device.
    
    Note: for device_add command in qdev.json adding the 'allow-preconfig'
    option has no effect because the command appears to bypass QAPI (see
    TODO at qapi/qdev.json:61). The option is added there solely to
    document the intent.
    For the same reason, the flags have to be explicitly set in
    monitor_init_qmp_commands() when the device_add command is registered.
    
    Signed-off-by: Mirela Grujic <mirela.grujic@greensocs.com>
    Signed-off-by: Damien Hedde <damien.hedde@greensocs.com>
    Message-Id: <20220223090706.4888-7-damien.hedde@greensocs.com>

--- monitor/misc.c
+++ monitor/misc.c
@@ -233,7 +233,7 @@ static void monitor_init_qmp_commands(void)
     qmp_init_marshal(&qmp_commands);
 
     qmp_register_command(&qmp_commands, "device_add",
-                         qmp_device_add, 0, 0);
+                         qmp_device_add, QCO_ALLOW_PRECONFIG, 0);
 
     QTAILQ_INIT(&qmp_cap_negotiation_commands);
     qmp_register_command(&qmp_cap_negotiation_commands, "qmp_capabilities",
