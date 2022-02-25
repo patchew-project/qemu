c5d73206395a0a61589eacb84fdd74a495bc5a93
Vladislav Yaroshchuk <vladislav.yaroshchuk@jetbrains.com>
2022-02-25 19:52:33 +0300

    net/vmnet: implement shared mode (vmnet-shared)
    Interaction with vmnet.framework in different modes
    differs only on configuration stage, so we can create
    common `send`, `receive`, etc. procedures and reuse them.
    
    vmnet.framework supports iov, but writing more than
    one iov into vmnet interface fails with
    'VMNET_INVALID_ARGUMENT'. Collecting provided iovs into
    one and passing it to vmnet works fine. That's the
    reason why receive_iov() left unimplemented. But it still
    works with good enough performance having .receive()
    net/vmnet: implement shared mode (vmnet-shared)
    
    Interaction with vmnet.framework in different modes
    differs only on configuration stage, so we can create
    common `send`, `receive`, etc. procedures and reuse them.
    
    vmnet.framework supports iov, but writing more than
    one iov into vmnet interface fails with
    'VMNET_INVALID_ARGUMENT'. Collecting provided iovs into
    one and passing it to vmnet works fine. That's the
    reason why receive_iov() left unimplemented. But it still
    works with good enough performance having .receive()
    implemented only.
    
    Also, there is no way to unsubscribe from vmnet packages
    receiving except registering and unregistering event
    callback or simply drop packages just ignoring and
    not processing them when related flag is set. Here we do
    using the second way.
    
    Signed-off-by: Phillip Tennen <phillip@axleos.com>
    Signed-off-by: Vladislav Yaroshchuk <Vladislav.Yaroshchuk@jetbrains.com>
    Message-Id: <20220225165238.63646-4-Vladislav.Yaroshchuk@jetbrains.com>

--- net/vmnet_int.h
+++ net/vmnet_int.h
@@ -15,11 +15,48 @@
 #include "clients.h"
 
 #include <vmnet/vmnet.h>
+#include <dispatch/dispatch.h>
+
+/**
+ *  From vmnet.framework documentation
+ *
+ *  Each read/write call allows up to 200 packets to be
+ *  read or written for a maximum of 256KB.
+ *
+ *  Each packet written should be a complete
+ *  ethernet frame.
+ *
+ *  https://developer.apple.com/documentation/vmnet
+ */
+#define VMNET_PACKETS_LIMIT 200
 
 typedef struct VmnetCommonState {
-  NetClientState nc;
+    NetClientState nc;
+    interface_ref vmnet_if;
 
+    bool send_scheduled;
+
+    uint64_t mtu;
+    uint64_t max_packet_size;
+
+    struct vmpktdesc packets_buf[VMNET_PACKETS_LIMIT];
+    struct iovec iov_buf[VMNET_PACKETS_LIMIT];
+
+    dispatch_queue_t if_queue;
+
+    QEMUBH *send_bh;
 } VmnetCommonState;
 
+const char *vmnet_status_map_str(vmnet_return_t status);
+
+int vmnet_if_create(NetClientState *nc,
+                    xpc_object_t if_desc,
+                    Error **errp);
+
+ssize_t vmnet_receive_common(NetClientState *nc,
+                             const uint8_t *buf,
+                             size_t size);
+
+void vmnet_cleanup_common(NetClientState *nc);
 
 #endif /* VMNET_INT_H */
