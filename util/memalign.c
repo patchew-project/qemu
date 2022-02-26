9bc452f5067dce622e584cd3410077347ab625a6
Peter Maydell <peter.maydell@linaro.org>
2022-02-26 18:07:21 +0000

    util: Use meson checks for valloc() and memalign() presence
    Instead of assuming that all CONFIG_BSD have valloc() and anything
    else is memalign(), explicitly check for those functions in
    meson.build and use the "is the function present" define.  Tests for
    specific functionality are better than which-OS checks; this also
    lets us give a helpful error message if somehow there's no usable
    function present.
    
    Signed-off-by: Peter Maydell <peter.maydell@linaro.org>
    Reviewed-by: Philippe Mathieu-Daudé <f4bug@amsat.org>
    Reviewed-by: Richard Henderson <richard.henderson@linaro.org>
    Message-Id: <20220226180723.1706285-8-peter.maydell@linaro.org>

--- util/memalign.c
+++ util/memalign.c
@@ -25,6 +25,8 @@
  */
 
 #include "qemu/osdep.h"
+#include "qemu/host-utils.h"
+#include "trace.h"
 
 void *qemu_try_memalign(size_t alignment, size_t size)
 {
@@ -50,10 +52,12 @@ void *qemu_try_memalign(size_t alignment, size_t size)
     } else {
         ptr = NULL;
     }
-#elif defined(CONFIG_BSD)
+#elif defined(CONFIG_VALLOC)
     ptr = valloc(size);
-#else
+#elif defined(CONFIG_MEMALIGN)
     ptr = memalign(alignment, size);
+#else
+    #error No function to allocate aligned memory available
 #endif
     trace_qemu_memalign(alignment, size, ptr);
     return ptr;
