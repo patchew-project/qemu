3038f5da1a47b7702b82813d2bf349892c1d4db8
Peter Maydell <peter.maydell@linaro.org>
2022-02-26 18:07:18 +0000

    util/oslib-win32: Return NULL on qemu_try_memalign() with zero size
    Currently if qemu_try_memalign() is asked to allocate 0 bytes, we assert.
    Instead return NULL; this is in line with the posix_memalign() API,
    and is valid to pass to _aligned_free() (which will do nothing).
    
    This change is a preparation for sharing the qemu_try_memalign()
    code between Windows and POSIX -- at the moment only the Windows
    version has the assert that size != 0.
    
    Signed-off-by: Peter Maydell <peter.maydell@linaro.org>
    Reviewed-by: Richard Henderson <richard.henderson@linaro.org>
    Message-Id: <20220226180723.1706285-5-peter.maydell@linaro.org>

--- util/oslib-win32.c
+++ util/oslib-win32.c
@@ -48,13 +48,16 @@ void *qemu_try_memalign(size_t alignment, size_t size)
 {
     void *ptr;
 
-    g_assert(size != 0);
     if (alignment < sizeof(void *)) {
         alignment = sizeof(void *);
     } else {
         g_assert(is_power_of_2(alignment));
     }
-    ptr = _aligned_malloc(size, alignment);
+    if (size) {
+        ptr = _aligned_malloc(size, alignment);
+    } else {
+        ptr = NULL;
+    }
     trace_qemu_memalign(alignment, size, ptr);
     return ptr;
 }
