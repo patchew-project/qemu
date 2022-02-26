b0c351714c91375ac15e87cc4f71bf6e76f4eb1a
Peter Maydell <peter.maydell@linaro.org>
2022-02-26 18:07:20 +0000

    util: Share qemu_try_memalign() implementation between POSIX and Windows
    The qemu_try_memalign() functions for POSIX and Windows used to be
    significantly different, but these days they are identical except for
    the actual allocation function called, and the POSIX version already
    has to have ifdeffery for different allocation functions.
    
    Move to a single implementation in memalign.c, which uses the Windows
    _aligned_malloc if we detect that function in meson.
    
    Signed-off-by: Peter Maydell <peter.maydell@linaro.org>
    Reviewed-by: Richard Henderson <richard.henderson@linaro.org>
    Message-Id: <20220226180723.1706285-7-peter.maydell@linaro.org>

--- util/memalign.c
+++ util/memalign.c
@@ -26,6 +26,39 @@
 
 #include "qemu/osdep.h"
 
+void *qemu_try_memalign(size_t alignment, size_t size)
+{
+    void *ptr;
+
+    if (alignment < sizeof(void*)) {
+        alignment = sizeof(void*);
+    } else {
+        g_assert(is_power_of_2(alignment));
+    }
+
+#if defined(CONFIG_POSIX_MEMALIGN)
+    int ret;
+    ret = posix_memalign(&ptr, alignment, size);
+    if (ret != 0) {
+        errno = ret;
+        ptr = NULL;
+    }
+#elif defined(CONFIG_ALIGNED_MALLOC)
+    /* _aligned_malloc() fails on 0 size */
+    if (size) {
+        ptr = _aligned_malloc(size, alignment);
+    } else {
+        ptr = NULL;
+    }
+#elif defined(CONFIG_BSD)
+    ptr = valloc(size);
+#else
+    ptr = memalign(alignment, size);
+#endif
+    trace_qemu_memalign(alignment, size, ptr);
+    return ptr;
+}
+
 void *qemu_memalign(size_t alignment, size_t size)
 {
     void *p = qemu_try_memalign(alignment, size);
