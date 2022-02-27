c7f38787024ef8b72ebe60c1f050cffd1d7150b0
Richard Henderson <richard.henderson@linaro.org>
2022-02-26 16:04:11 -1000

    tcg/mips: Support TCG_TARGET_SIGNED_ADDR32
    All 32-bit mips operations sign-extend the output, so we are easily
    able to keep TCG_TYPE_I32 values sign-extended in host registers.
    
    Cc: Philippe Mathieu-Daudé <f4bug@amsat.org>
    Cc: Aurelien Jarno <aurelien@aurel32.net>
    Cc: Huacai Chen <chenhuacai@kernel.org>
    Cc: Jiaxun Yang <jiaxun.yang@flygoat.com>
    Cc: Aleksandar Rikalo <aleksandar.rikalo@syrmia.com>
    Signed-off-by: Richard Henderson <richard.henderson@linaro.org>
    Reviewed-by: Philippe Mathieu-Daudé <f4bug@amsat.org>
    Message-Id: <20220227020413.11741-8-richard.henderson@linaro.org>

--- tcg/mips/tcg-target-sa32.h
+++ tcg/mips/tcg-target-sa32.h
@@ -1,1 +1,9 @@
+/*
+ * Do not set TCG_TARGET_SIGNED_ADDR32 for mips32;
+ * TCG expects this to only be set for 64-bit hosts.
+ */
+#ifdef __mips64
+#define TCG_TARGET_SIGNED_ADDR32 1
+#else
 #define TCG_TARGET_SIGNED_ADDR32 0
+#endif
