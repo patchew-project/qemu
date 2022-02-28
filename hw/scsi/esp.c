2f874f31a539bd9958e423329a11f041828222eb
Mark Cave-Ayland <mark.cave-ayland@ilande.co.uk>
2022-02-28 22:25:27 +0000

    esp: recreate ESPState current_req after migration
    Since PDMA reads/writes are driven by the guest, it is possible that migration
    can occur whilst a SCSIRequest is still active. Fortunately active SCSIRequests
    are already included in the migration stream and restarted post migration but
    this still leaves the reference in ESPState uninitialised.
    
    Implement the SCSIBusInfo .load_request callback to obtain a reference to the
    currently active SCSIRequest and use it to recreate ESPState current_req
    after migration.
    
    Suggested-by: Paolo Bonzini <pbonzini@redhat.com>
    Signed-off-by: Mark Cave-Ayland <mark.cave-ayland@ilande.co.uk>
    Message-Id: <20220228222527.8234-11-mark.cave-ayland@ilande.co.uk>

--- hw/scsi/esp.c
+++ hw/scsi/esp.c
@@ -1342,6 +1342,15 @@ static uint64_t sysbus_esp_pdma_read(void *opaque, hwaddr addr,
     return val;
 }
 
+static void *esp_load_request(QEMUFile *f, SCSIRequest *req)
+{
+    ESPState *s = container_of(req->bus, ESPState, bus);
+
+    scsi_req_ref(req);
+    s->current_req = req;
+    return s;
+}
+
 static const MemoryRegionOps sysbus_esp_pdma_ops = {
     .read = sysbus_esp_pdma_read,
     .write = sysbus_esp_pdma_write,
@@ -1357,6 +1366,7 @@ static const struct SCSIBusInfo esp_scsi_info = {
     .max_target = ESP_MAX_DEVS,
     .max_lun = 7,
 
+    .load_request = esp_load_request,
     .transfer_data = esp_transfer_data,
     .complete = esp_command_complete,
     .cancel = esp_request_cancelled
