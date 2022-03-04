dd761af0aeb4b72421321207e37cc102040f2a65
Marc-André Lureau <marcandre.lureau@redhat.com>
2022-03-05 01:16:17 +0400

    Replace qemu_gettimeofday() with g_get_real_time()
    GLib g_get_real_time() is an alternative to gettimeofday() which allows
    to simplify our code.
    
    For semihosting, a few bits are lost on POSIX host, but this shouldn't
    be a big concern.
    
    Signed-off-by: Marc-André Lureau <marcandre.lureau@redhat.com>
    Reviewed-by: Laurent Vivier <laurent@vivier.eu>
    Message-Id: <20220304211618.3715999-5-marcandre.lureau@redhat.com>

--- qapi/qmp-event.c
+++ qapi/qmp-event.c
@@ -20,15 +20,13 @@
 
 static void timestamp_put(QDict *qdict)
 {
-    int err;
     QDict *ts;
-    qemu_timeval tv;
+    int64_t rt = g_get_real_time();
 
-    err = qemu_gettimeofday(&tv);
-    /* Put -1 to indicate failure of getting host time */
-    ts = qdict_from_jsonf_nofail("{ 'seconds': %lld, 'microseconds': %lld }",
-                                 err < 0 ? -1LL : (long long)tv.tv_sec,
-                                 err < 0 ? -1LL : (long long)tv.tv_usec);
+    ts = qdict_from_jsonf_nofail("{ 'seconds': %" G_GINT64_FORMAT
+                                 ", 'microseconds': %" G_GINT64_FORMAT "}",
+                                 rt / G_USEC_PER_SEC,
+                                 rt % G_USEC_PER_SEC);
     qdict_put(qdict, "timestamp", ts);
 }
 
