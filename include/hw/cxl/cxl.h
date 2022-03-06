6aae0b73445d1eb0220f31d171cd07112c0eb9dd
Jonathan Cameron <jonathan.cameron@huawei.com>
2022-03-06 17:41:18 +0000

    hw/cxl/host: Add support for CXL Fixed Memory Windows.
    The concept of these is introduced in [1] in terms of the
    description the CEDT ACPI table. The principal is more general.
    Unlike once traffic hits the CXL root bridges, the host system
    memory address routing is implementation defined and effectively
    static once observable by standard / generic system software.
    Each CXL Fixed Memory Windows (CFMW) is a region of PA space
    which has fixed system dependent routing configured so that
    accesses can be routed to the CXL devices below a set of target
    root bridges. The accesses may be interleaved across multiple
    root bridges.
    
    For QEMU we could have fully specified these regions in terms
    of a base PA + size, but as the absolute address does not matter
    it is simpler to let individual platforms place the memory regions.
    
    ExampleS:
    -cxl-fixed-memory-window targets.0=cxl.0,size=128G
    -cxl-fixed-memory-window targets.0=cxl.1,size=128G
    -cxl-fixed-memory-window targets.0=cxl0,targets.1=cxl.1,size=256G,interleave-granularity=2k
    
    Specifies
    * 2x 128G regions not interleaved across root bridges, one for each of
      the root bridges with ids cxl.0 and cxl.1
    * 256G region interleaved across root bridges with ids cxl.0 and cxl.1
    with a 2k interleave granularity.
    
    When system software enumerates the devices below a given root bridge
    it can then decide which CFMW to use. If non interleave is desired
    (or possible) it can use the appropriate CFMW for the root bridge in
    question.  If there are suitable devices to interleave across the
    two root bridges then it may use the 3rd CFMS.
    
    A number of other designs were considered but the following constraints
    made it hard to adapt existing QEMU approaches to this particular problem.
    1) The size must be known before a specific architecture / board brings
       up it's PA memory map.  We need to set up an appropriate region.
    2) Using links to the host bridges provides a clean command line interface
       but these links cannot be established until command line devices have
       been added.
    
    Hence the two step process used here of first establishing the size,
    interleave-ways and granularity + caching the ids of the host bridges
    and then, once available finding the actual host bridges so they can
    be used later to support interleave decoding.
    
    [1] CXL 2.0 ECN: CEDT CFMWS & QTG DSM (computeexpresslink.org / specifications)
    
    Signed-off-by: Jonathan Cameron <jonathan.cameron@huawei.com>
    Message-Id: <20220306174137.5707-28-Jonathan.Cameron@huawei.com>

--- include/hw/cxl/cxl.h
+++ include/hw/cxl/cxl.h
@@ -10,6 +10,8 @@
 #ifndef CXL_H
 #define CXL_H
 
+#include "qapi/qapi-types-machine.h"
+#include "hw/pci/pci_bridge.h"
 #include "cxl_pci.h"
 #include "cxl_component.h"
 #include "cxl_device.h"
@@ -19,10 +21,28 @@
 
 #define CXL_WINDOW_MAX 10
 
+typedef struct CXLFixedWindow {
+    uint64_t size;
+    char **targets;
+    struct PXBDev *target_hbs[8];
+    uint8_t num_targets;
+    uint8_t enc_int_ways;
+    uint8_t enc_int_gran;
+    /* Todo: XOR based interleaving */
+    MemoryRegion mr;
+    hwaddr base;
+} CXLFixedWindow;
+
 typedef struct CXLState {
     bool is_enabled;
     MemoryRegion host_mr;
     unsigned int next_mr_idx;
+    GList *fixed_windows;
 } CXLState;
+
+void cxl_fixed_memory_window_options_set(MachineState *ms,
+                                         CXLFixedMemoryWindowOptions *object,
+                                         Error **errp);
+void cxl_fixed_memory_window_link_targets(Error **errp);
 
 #endif
