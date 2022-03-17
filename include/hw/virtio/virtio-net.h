a7dcdd03e4c2330eb933748b1e8a8a04187457b5
Eugenio Pérez <eperezma@redhat.com>
2022-03-17 19:23:46 +0100

    virtio-net: Expose ctrl virtqueue logic
    This allows external vhost-net devices to modify the state of the
    VirtIO device model once vhost-vdpa device has acknowledge the control
    commands.
    
    Signed-off-by: Eugenio Pérez <eperezma@redhat.com>
    Message-Id: <20220317182400.651508-6-eperezma@redhat.com>

--- include/hw/virtio/virtio-net.h
+++ include/hw/virtio/virtio-net.h
@@ -218,6 +218,9 @@ struct VirtIONet {
     struct EBPFRSSContext ebpf_rss;
 };
 
+unsigned virtio_net_handle_ctrl_iov(VirtIODevice *vdev,
+                                    const struct iovec *in_sg, size_t in_num,
+                                    struct iovec *out_sg, unsigned out_num);
 void virtio_net_set_netclient_name(VirtIONet *n, const char *name,
                                    const char *type);
 
