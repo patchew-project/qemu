9866624d5dd300c36536a13d2de824166756fc45
Peter Maydell <peter.maydell@linaro.org>
2022-03-18 17:30:38 +0000

    Fix 'writeable' typos
    We have about 25 instances of the typo/variant spelling 'writeable',
    and over 500 of the more common 'writable'.  Standardize on the
    latter.
    
    Change produced with:
    
     sed -i -e 's/writeable/writable/g' $(git grep -l writeable)
    
    and then hand-undoing the instance in linux-headers/linux/kvm.h.
    
    All these changes are in comments or documentation, except for the
    two local variables in accel/hvf/hvf-accel-ops.c and
    accel/kvm/kvm-all.c.
    
    Signed-off-by: Peter Maydell <peter.maydell@linaro.org>
    Reviewed-by: Philippe Mathieu-Daudé <f4bug@amsat.org>
    Reviewed-by: Stefan Weil <sw@weilnetz.de>
    Message-Id: <20220318173038.3350098-1-peter.maydell@linaro.org>

--- hw/intc/arm_gicv3_redist.c
+++ hw/intc/arm_gicv3_redist.c
@@ -491,7 +491,7 @@ static MemTxResult gicr_writel(GICv3CPUState *cs, hwaddr offset,
         /* RAZ/WI for our implementation */
         return MEMTX_OK;
     case GICR_WAKER:
-        /* Only the ProcessorSleep bit is writeable. When the guest sets
+        /* Only the ProcessorSleep bit is writable. When the guest sets
          * it it requests that we transition the channel between the
          * redistributor and the cpu interface to quiescent, and that
          * we set the ChildrenAsleep bit once the inteface has reached the
