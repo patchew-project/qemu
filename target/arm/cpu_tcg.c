b15cde5c3ce8d5dbb46900ccfb42d9dd0f421a79
Richard Henderson <richard.henderson@linaro.org>
2022-04-08 17:07:41 -0700

    target/arm: Enable FEAT_RAS for -cpu max
    Signed-off-by: Richard Henderson <richard.henderson@linaro.org>
    Reviewed-by: Peter Maydell <peter.maydell@linaro.org>
    Message-Id: <20220409000742.293691-16-richard.henderson@linaro.org>

--- target/arm/cpu_tcg.c
+++ target/arm/cpu_tcg.c
@@ -72,6 +72,7 @@ void arm32_max_features(ARMCPU *cpu)
 
     t = cpu->isar.id_pfr0;
     t = FIELD_DP32(t, ID_PFR0, DIT, 1);           /* FEAT_DIT */
+    t = FIELD_DP32(t, ID_PFR0, RAS, 1);           /* FEAT_RAS */
     cpu->isar.id_pfr0 = t;
 
     t = cpu->isar.id_pfr2;
