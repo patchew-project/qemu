7c92cbb234cb85d7023c7fefa9b07f78e9f38ca4
Gautam Agrawal <gautamnagrawal@gmail.com>
2022-04-13 22:52:46 +0530

    Warn user if the vga flag is passed but no vga device is created
    A global boolean variable "vga_interface_created"(declared in softmmu/globals.c)
    has been used to track the creation of vga interface. If the vga flag is passed
    in the command line "default_vga"(declared in softmmu/vl.c) variable is set to 0.
    To warn user, the condition checks if vga_interface_created is false
    and default_vga is equal to 0.If "-vga none" is passed, this patch will not warn the
    user regarding the creation of VGA device.
    
    The initialisation of Global variable "vga_interface_created" in softmmu/globals.c
    has also been corrected.
    
    The warning "A -vga option was passed but this
    machine type does not use that option; no VGA device has been created"
    is logged if vga flag is passed but no vga device is created.
    
    "vga_interface_created" has also been included in "xen_machine_pv.c" and
    "fuloong2e.c". This patch has been tested for x86_64, i386, sparc, sparc64 and arm boards.
    
    Signed-off-by: Gautam Agrawal <gautamnagrawal@gmail.com>
    Resolves: https://gitlab.com/qemu-project/qemu/-/issues/581
    Message-Id: <20220413172246.73708-1-gautamnagrawal@gmail.com>

--- softmmu/vl.c
+++ softmmu/vl.c
@@ -2734,6 +2734,12 @@ static void qemu_machine_creation_done(void)
     if (foreach_device_config(DEV_GDB, gdbserver_start) < 0) {
         exit(1);
     }
+    if (!vga_interface_created && !default_vga &&
+        vga_interface_type != VGA_NONE) {
+        warn_report("A -vga option was passed but this machine "
+                    "type does not use that option; "
+                    "No VGA device has been created");
+    }
 }
 
 void qmp_x_exit_preconfig(Error **errp)
