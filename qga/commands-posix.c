4c41e3a4f102670bd780ea5413f171453825bb0e
Marc-André Lureau <marcandre.lureau@redhat.com>
2022-05-13 20:08:12 +0200

    qga: use qemu_open_cloexec() for safe_open_or_create()
    The function takes care of setting CLOEXEC, and reporting error.
    
    The reported error message will differ, from:
      "failed to open file 'foo' (mode: 'r')"
    to:
      "Failed to open file 'foo'"
    
    Signed-off-by: Marc-André Lureau <marcandre.lureau@redhat.com>
    Reviewed-by: Markus Armbruster <armbru@redhat.com>
    Message-Id: <20220513180821.905149-7-marcandre.lureau@redhat.com>

--- qga/commands-posix.c
+++ qga/commands-posix.c
@@ -339,6 +339,7 @@ find_open_flag(const char *mode_str, Error **errp)
 static FILE *
 safe_open_or_create(const char *path, const char *mode, Error **errp)
 {
+    ERRP_GUARD();
     int oflag;
     int fd = -1;
     FILE *f = NULL;
@@ -370,20 +371,16 @@ safe_open_or_create(const char *path, const char *mode, Error **errp)
      * open() is decisive and its third argument is ignored, and the second
      * open() and the fchmod() are never called.
      */
-    fd = open(path, oflag | ((oflag & O_CREAT) ? O_EXCL : 0), 0);
+    fd = qemu_open_cloexec(path, oflag | ((oflag & O_CREAT) ? O_EXCL : 0), 0, errp);
     if (fd == -1 && errno == EEXIST) {
+        error_free(*errp);
+        *errp = NULL;
         oflag &= ~(unsigned)O_CREAT;
-        fd = open(path, oflag);
+        fd = qemu_open_cloexec(path, oflag, 0, errp);
     }
     if (fd == -1) {
-        error_setg_errno(errp, errno,
-                         "failed to open file '%s' "
-                         "(mode: '%s')",
-                         path, mode);
         goto end;
     }
-
-    qemu_set_cloexec(fd);
 
     if ((oflag & O_CREAT) && fchmod(fd, DEFAULT_NEW_FILE_MODE) == -1) {
         error_setg_errno(errp, errno,
