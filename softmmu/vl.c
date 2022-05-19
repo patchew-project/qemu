6f8a74c5c396198e8176babc2742237b419ef7f0
Damien Hedde <damien.hedde@greensocs.com>
2022-05-19 17:34:01 +0200

    RFC qapi/device_add: handle the rom_order_override when cold-plugging
    rom_set_order_override() and rom_reset_order_override() were called
    in qemu_create_cli_devices() to set the rom_order_override value
    once and for all when creating the devices added on CLI.
    
    Unfortunately this won't work with qapi commands.
    
    Move the calls inside device_add so that it will be done in every
    case:
    + CLI option: -device
    + QAPI command: device_add
    
    rom_[set|reset]_order_override() are implemented in hw/core/loader.c
    They either do nothing or call fw_cfg_[set|reset]_order_override().
    The later functions are implemented in hw/nvram/fw_cfg.c and only
    change an integer value of a "global" variable.
    In consequence, there are no complex side effects involved and we can
    safely move them from outside the -device option loop to the inner
    function.
    
    Signed-off-by: Damien Hedde <damien.hedde@greensocs.com>
    Message-Id: <20220519153402.41540-6-damien.hedde@greensocs.com>

--- softmmu/vl.c
+++ softmmu/vl.c
@@ -2635,7 +2635,6 @@ static void qemu_create_cli_devices(void)
     }
 
     /* init generic devices */
-    rom_set_order_override(FW_CFG_ORDER_OVERRIDE_DEVICE);
     qemu_opts_foreach(qemu_find_opts("device"),
                       device_init_func, NULL, &error_fatal);
     QTAILQ_FOREACH(opt, &device_opts, next) {
@@ -2652,7 +2651,6 @@ static void qemu_create_cli_devices(void)
         object_unref(OBJECT(dev));
         loc_pop(&opt->loc);
     }
-    rom_reset_order_override();
 }
 
 static void qemu_machine_creation_done(void)
