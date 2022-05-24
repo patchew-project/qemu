17f7fbc6be2ecf7e17b424c1590fb1a036683f6b
Peter Xu <peterx@redhat.com>
2022-05-24 18:11:40 -0400

    migration: Postcopy preemption enablement
    This patch enables postcopy-preempt feature.
    
    It contains two major changes to the migration logic:
    
    (1) Postcopy requests are now sent via a different socket from precopy
        background migration stream, so as to be isolated from very high page
        request delays.
    
    (2) For huge page enabled hosts: when there's postcopy requests, they can now
        intercept a partial sending of huge host pages on src QEMU.
    
    After this patch, we'll live migrate a VM with two channels for postcopy: (1)
    PRECOPY channel, which is the default channel that transfers background pages;
    and (2) POSTCOPY channel, which only transfers requested pages.
    
    There's no strict rule of which channel to use, e.g., if a requested page is
    already being transferred on precopy channel, then we will keep using the same
    precopy channel to transfer the page even if it's explicitly requested.  In 99%
    of the cases we'll prioritize the channels so we send requested page via the
    postcopy channel as long as possible.
    
    On the source QEMU, when we found a postcopy request, we'll interrupt the
    PRECOPY channel sending process and quickly switch to the POSTCOPY channel.
    After we serviced all the high priority postcopy pages, we'll switch back to
    PRECOPY channel so that we'll continue to send the interrupted huge page again.
    There's no new thread introduced on src QEMU.
    
    On the destination QEMU, one new thread is introduced to receive page data from
    the postcopy specific socket (done in the preparation patch).
    
    This patch has a side effect: after sending postcopy pages, previously we'll
    assume the guest will access follow up pages so we'll keep sending from there.
    Now it's changed.  Instead of going on with a postcopy requested page, we'll go
    back and continue sending the precopy huge page (which can be intercepted by a
    postcopy request so the huge page can be sent partially before).
    
    Whether that's a problem is debatable, because "assuming the guest will
    continue to access the next page" may not really suite when huge pages are
    used, especially if the huge page is large (e.g. 1GB pages).  So that locality
    hint is much meaningless if huge pages are used.
    
    Reviewed-by: Dr. David Alan Gilbert <dgilbert@redhat.com>
    Signed-off-by: Peter Xu <peterx@redhat.com>
    Message-Id: <20220524221151.18225-4-peterx@redhat.com>

--- migration/migration.c
+++ migration/migration.c
@@ -3200,6 +3200,8 @@ static int postcopy_start(MigrationState *ms)
                               MIGRATION_STATUS_FAILED);
     }
 
+    trace_postcopy_preempt_enabled(migrate_postcopy_preempt());
+
     return ret;
 
 fail_closefb:
