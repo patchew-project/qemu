f8905a8d93dce9dfa2bc41d8eb10a9f5f03e7c43
Damien Hedde <damien.hedde@greensocs.com>
2022-05-24 15:48:08 +0200

    softmmu/memory: add memory_region_try_add_subregion function
    It allows adding a subregion to a memory region with error handling.
    Like memory_region_add_subregion_overlap(), it handles priority as
    well. Apart from the error handling, the behavior is the same. It
    can be used to do the simple memory_region_add_subregion() (with no
    overlap) by setting the priority parameter to 0.
    
    This commit is a preparation to further use of this function in the
    context of qapi command which needs error handling support.
    
    Signed-off-by: Damien Hedde <damien.hedde@greensocs.com>
    Reviewed-by: Philippe Mathieu-Daudé <philmd@redhat.com>
    Reviewed-by: David Hildenbrand <david@redhat.com>
    Reviewed-by: Alistair Francis <alistair.francis@wdc.com>
    Message-Id: <20220524134809.40732-3-damien.hedde@greensocs.com>

--- softmmu/memory.c
+++ softmmu/memory.c
@@ -2541,27 +2541,34 @@ done:
     memory_region_transaction_commit();
 }
 
-static void memory_region_add_subregion_common(MemoryRegion *mr,
-                                               hwaddr offset,
-                                               MemoryRegion *subregion)
+bool memory_region_try_add_subregion(MemoryRegion *mr,
+                                     hwaddr offset,
+                                     MemoryRegion *subregion,
+                                     int priority,
+                                     Error **errp)
 {
     MemoryRegion *alias;
 
-    assert(!subregion->container);
+    if (subregion->container) {
+        error_setg(errp, "The memory region is already in another region");
+        return false;
+    }
+
+    subregion->priority = priority;
     subregion->container = mr;
     for (alias = subregion->alias; alias; alias = alias->alias) {
         alias->mapped_via_alias++;
     }
     subregion->addr = offset;
     memory_region_update_container_subregions(subregion);
+    return true;
 }
 
 void memory_region_add_subregion(MemoryRegion *mr,
                                  hwaddr offset,
                                  MemoryRegion *subregion)
 {
-    subregion->priority = 0;
-    memory_region_add_subregion_common(mr, offset, subregion);
+    memory_region_try_add_subregion(mr, offset, subregion, 0, &error_abort);
 }
 
 void memory_region_add_subregion_overlap(MemoryRegion *mr,
@@ -2569,8 +2576,8 @@ void memory_region_add_subregion_overlap(MemoryRegion *mr,
                                          MemoryRegion *subregion,
                                          int priority)
 {
-    subregion->priority = priority;
-    memory_region_add_subregion_common(mr, offset, subregion);
+    memory_region_try_add_subregion(mr, offset, subregion, priority,
+                                    &error_abort);
 }
 
 void memory_region_del_subregion(MemoryRegion *mr,
@@ -2626,7 +2633,8 @@ static void memory_region_readd_subregion(MemoryRegion *mr)
         memory_region_transaction_begin();
         memory_region_ref(mr);
         memory_region_del_subregion(container, mr);
-        memory_region_add_subregion_common(container, mr->addr, mr);
+        memory_region_try_add_subregion(container, mr->addr, mr, mr->priority,
+                                        &error_abort);
         memory_region_unref(mr);
         memory_region_transaction_commit();
     }
