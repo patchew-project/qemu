bab910b3f024d181028616d035c52f74e2fa35b7
Richard Henderson <richard.henderson@linaro.org>
2022-05-27 11:06:16 -0700

    target/arm: Add el_is_in_host
    This (newish) ARM pseudocode function is easier to work with
    than open-coded tests for HCR_E2H etc.  Use of the function
    will be staged into the code base in parts.
    
    Signed-off-by: Richard Henderson <richard.henderson@linaro.org>
    Reviewed-by: Peter Maydell <peter.maydell@linaro.org>
    Message-Id: <20220527180623.185261-9-richard.henderson@linaro.org>

--- target/arm/internals.h
+++ target/arm/internals.h
@@ -1295,6 +1295,8 @@ static inline void define_cortex_a72_a57_a53_cp_reginfo(ARMCPU *cpu) { }
 void define_cortex_a72_a57_a53_cp_reginfo(ARMCPU *cpu);
 #endif
 
+bool el_is_in_host(CPUARMState *env, int el);
+
 void aa32_max_features(ARMCPU *cpu);
 bool arm_singlestep_active(CPUARMState *env);
 bool arm_generate_debug_exceptions(CPUARMState *env, int cur_el);
