a999aef7d3d77ad2ab7b30e30cd97d403163d8af
Mark Cave-Ayland <mark.cave-ayland@ilande.co.uk>
2022-05-28 10:19:30 +0100

    hw/acpi/piix4: use qdev gpio to wire up sci_irq
    The sci_irq can now be wired up directly using a qdev gpio instead of having to
    set the IRQ externally in piix4_pm_initfn().
    
    Signed-off-by: Mark Cave-Ayland <mark.cave-ayland@ilande.co.uk>
    Reviewed-by: Philippe Mathieu-Daudé <f4bug@amsat.org>
    Message-Id: <20220528091934.15520-9-mark.cave-ayland@ilande.co.uk>

--- hw/acpi/piix4.c
+++ hw/acpi/piix4.c
@@ -506,8 +506,7 @@ static void piix4_pm_init(Object *obj)
 }
 
 PIIX4PMState *piix4_pm_initfn(PCIBus *bus, int devfn, uint32_t smb_io_base,
-                              qemu_irq sci_irq, qemu_irq smi_irq,
-                              int smm_enabled)
+                              qemu_irq smi_irq, int smm_enabled)
 {
     PCIDevice *pci_dev;
     DeviceState *dev;
@@ -519,7 +518,6 @@ PIIX4PMState *piix4_pm_initfn(PCIBus *bus, int devfn, uint32_t smb_io_base,
     qdev_prop_set_bit(dev, "smm-enabled", smm_enabled);
 
     s = PIIX4_PM(dev);
-    s->irq = sci_irq;
     s->smi_irq = smi_irq;
 
     pci_realize_and_unref(pci_dev, bus, &error_fatal);
