cfac3ff4a6e513d24db2bfd9c6243412e2dada57
Dongwon Kim <dongwon.kim@intel.com>
2022-05-31 13:23:27 -0700

    ui/gtk: a new array param monitor to specify the target displays
    New integer array parameter, 'monitor' is for specifying the target
    displays where individual QEMU windows are placed upon launching.
    
    The array contains a series of numbers representing the monitor where
    QEMU windows are placed.
    
    Numbers in the array are mapped to QEMU windows like,
    
    [1st detached window, 2nd detached window,.... Main window]
    
    Usage example: -display gtk,monitor.0=0,monitor.1=1.....
    
    Cc: Daniel P. Berrangé <berrange@redhat.com>
    Cc: Markus Armbruster <armbru@redhat.com>
    Cc: Philippe Mathieu-Daudé <philmd@redhat.com>
    Cc: Paolo Bonzini <pbonzini@redhat.com>
    Cc: Gerd Hoffmann <kraxel@redhat.com>
    Cc: Vivek Kasireddy <vivek.kasireddy@intel.com>
    Signed-off-by: Dongwon Kim <dongwon.kim@intel.com>
    Reviewed-by: Daniel P. Berrangé <berrange@redhat.com>
    Message-Id: <20220531202327.14636-3-dongwon.kim@intel.com>

--- qemu-options.hx
+++ qemu-options.hx
@@ -1943,7 +1943,7 @@ DEF("display", HAS_ARG, QEMU_OPTION_display,
 #endif
 #if defined(CONFIG_GTK)
     "-display gtk[,full-screen=on|off][,gl=on|off][,grab-on-hover=on|off]\n"
-    "            [,show-cursor=on|off][,window-close=on|off]\n"
+    "            [,monitor.<order>=<value>][,show-cursor=on|off][,window-close=on|off]\n"
 #endif
 #if defined(CONFIG_VNC)
     "-display vnc=<display>[,<optargs>]\n"
