011625cf5644cf074d8173239036cc9926b1b070
Richard Henderson <richard.henderson@linaro.org>
2022-06-02 14:48:04 -0700

    target/arm: Add SMEEXC_EL to TB flags
    This is CheckSMEAccess, which is the basis for a set of
    related tests for various SME cpregs and instructions.
    
    Signed-off-by: Richard Henderson <richard.henderson@linaro.org>
    Reviewed-by: Peter Maydell <peter.maydell@linaro.org>
    Message-Id: <20220602214853.496211-23-richard.henderson@linaro.org>

--- target/arm/translate.h
+++ target/arm/translate.h
@@ -42,6 +42,7 @@ typedef struct DisasContext {
     bool ns;        /* Use non-secure CPREG bank on access */
     int fp_excp_el; /* FP exception EL or 0 if enabled */
     int sve_excp_el; /* SVE exception EL or 0 if enabled */
+    int sme_excp_el; /* SME exception EL or 0 if enabled */
     int vl;          /* current vector length in bytes */
     /* Flag indicating that exceptions from secure mode are routed to EL3. */
     bool secure_routed_to_el3;
