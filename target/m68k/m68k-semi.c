cbc19377d03bb9c912c46194c8db126d5c2d1839
Luc Michel <lmichel@kalray.eu>
2022-06-21 14:59:13 +0200

    target/m68k: use semihosting_exit_request on semihosted exit syscall
    Use the new semihosting_exit_request instead of a call to exit when
    handling a semihosted exit syscall.
    
    Signed-off-by: Luc Michel <lmichel@kalray.eu>
    Reviewed-by: Laurent Vivier <laurent@vivier.eu>
    Message-Id: <20220621125916.25257-5-lmichel@kalray.eu>

--- target/m68k/m68k-semi.c
+++ target/m68k/m68k-semi.c
@@ -29,6 +29,7 @@
 #include "hw/boards.h"
 #endif
 #include "qemu/log.h"
+#include "semihosting/semihost.h"
 
 #define HOSTED_EXIT  0
 #define HOSTED_INIT_SIM 1
@@ -195,8 +196,7 @@ void do_m68k_semihosting(CPUM68KState *env, int nr)
     args = env->dregs[1];
     switch (nr) {
     case HOSTED_EXIT:
-        gdb_exit(env->dregs[0]);
-        exit(env->dregs[0]);
+        semihosting_exit_request(env->dregs[0]);
     case HOSTED_OPEN:
         GET_ARG(0);
         GET_ARG(1);
