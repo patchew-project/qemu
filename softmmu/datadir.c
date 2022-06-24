20742b60e45d8b1afe7136ce5708f650869d8ba8
Akihiko Odaki <akihiko.odaki@gmail.com>
2022-06-25 00:40:42 +0900

    meson: Prefix each element of firmware path
    Signed-off-by: Akihiko Odaki <akihiko.odaki@gmail.com>
    Message-Id: <20220624154042.51512-1-akihiko.odaki@gmail.com>

--- softmmu/datadir.c
+++ softmmu/datadir.c
@@ -105,15 +105,17 @@ static char *find_datadir(void)
 
 void qemu_add_default_firmwarepath(void)
 {
-    char **dirs;
+    static const char * const dirs[] = {
+        CONFIG_QEMU_FIRMWAREPATH
+        NULL
+    };
+
     size_t i;
 
     /* add configured firmware directories */
-    dirs = g_strsplit(CONFIG_QEMU_FIRMWAREPATH, G_SEARCHPATH_SEPARATOR_S, 0);
     for (i = 0; dirs[i] != NULL; i++) {
         qemu_add_data_dir(get_relocated_path(dirs[i]));
     }
-    g_strfreev(dirs);
 
     /* try to find datadir relative to the executable path */
     qemu_add_data_dir(find_datadir());
