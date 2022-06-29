28be93722e6985f36f10b4e8e08544226153bddf
Emanuele Giuseppe Esposito <eesposit@redhat.com>
2022-06-29 10:15:34 -0400

    jobs: protect job.aio_context with BQL and job_mutex
    In order to make it thread safe, implement a "fake rwlock",
    where we allow reads under BQL *or* job_mutex held, but
    writes only under BQL *and* job_mutex.
    
    The only write we have is in child_job_set_aio_ctx, which always
    happens under drain (so the job is paused).
    For this reason, introduce job_set_aio_context and make sure that
    the context is set under BQL, job_mutex and drain.
    Also make sure all other places where the aiocontext is read
    are protected.
    
    Note: at this stage, job_{lock/unlock} and job lock guard macros
    are *nop*.
    
    Suggested-by: Paolo Bonzini <pbonzini@redhat.com>
    Signed-off-by: Emanuele Giuseppe Esposito <eesposit@redhat.com>
    Reviewed-by: Stefan Hajnoczi <stefanha@redhat.com>
    Message-Id: <20220629141538.3400679-17-eesposit@redhat.com>

--- block/replication.c
+++ block/replication.c
@@ -148,8 +148,10 @@ static void replication_close(BlockDriverState *bs)
     }
     if (s->stage == BLOCK_REPLICATION_FAILOVER) {
         commit_job = &s->commit_job->job;
-        assert(commit_job->aio_context == qemu_get_current_aio_context());
-        job_cancel_sync(commit_job, false);
+        WITH_JOB_LOCK_GUARD() {
+            assert(commit_job->aio_context == qemu_get_current_aio_context());
+            job_cancel_sync_locked(commit_job, false);
+        }
     }
 
     if (s->mode == REPLICATION_MODE_SECONDARY) {
