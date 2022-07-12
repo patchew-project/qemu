94e2ddade07106f467ce08c140ca39b02001a024
Jason A. Donenfeld <Jason@zx2c4.com>
2022-07-12 18:46:27 +0200

    target/s390x: support PRNO_TRNG instruction
    In order for hosts running inside of TCG to initialize the kernel's
    random number generator, we should support the PRNO_TRNG instruction,
    backed in the usual way with the qemu_guest_getrandom helper. This is
    confirmed working on Linux 5.19-rc6.
    
    Cc: Thomas Huth <thuth@redhat.com>
    Cc: Richard Henderson <richard.henderson@linaro.org>
    Cc: Harald Freudenberger <freude@linux.ibm.com>
    Cc: Holger Dengler <dengler@linux.ibm.com>
    Signed-off-by: Jason A. Donenfeld <Jason@zx2c4.com>
    Message-Id: <20220712164627.581570-1-Jason@zx2c4.com>

--- target/s390x/tcg/crypto_helper.c
+++ target/s390x/tcg/crypto_helper.c
@@ -12,11 +12,27 @@
 
 #include "qemu/osdep.h"
 #include "qemu/main-loop.h"
+#include "qemu/guest-random.h"
 #include "s390x-internal.h"
 #include "tcg_s390x.h"
 #include "exec/helper-proto.h"
 #include "exec/exec-all.h"
 #include "exec/cpu_ldst.h"
+
+static void fill_buf_random(CPUS390XState *env, uintptr_t ra,
+                            uint64_t buf, uint64_t len)
+{
+        uint64_t addr = wrap_address(env, buf);
+        uint8_t tmp[256];
+
+        while (len) {
+                size_t block = MIN(len, sizeof(tmp));
+                qemu_guest_getrandom_nofail(tmp, block);
+                for (size_t i = 0; i < block; ++i)
+                        cpu_stb_data_ra(env, addr++, tmp[i], ra);
+                len -= block;
+        }
+}
 
 uint32_t HELPER(msa)(CPUS390XState *env, uint32_t r1, uint32_t r2, uint32_t r3,
                      uint32_t type)
@@ -52,6 +68,13 @@ uint32_t HELPER(msa)(CPUS390XState *env, uint32_t r1, uint32_t r2, uint32_t r3,
             cpu_stb_data_ra(env, param_addr, subfunc[i], ra);
         }
         break;
+    case 114: {
+        const uint64_t ucbuf = env->regs[r1], ucbuf_len = env->regs[r1 + 1];
+        const uint64_t cbuf = env->regs[r2], cbuf_len = env->regs[r2 + 1];
+        fill_buf_random(env, ra, ucbuf, ucbuf_len);
+        fill_buf_random(env, ra, cbuf, cbuf_len);
+        break;
+    }
     default:
         /* we don't implement any other subfunction yet */
         g_assert_not_reached();
