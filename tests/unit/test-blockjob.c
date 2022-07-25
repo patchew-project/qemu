4224d03239b98455026130a891666a0ea64ad1d4
Emanuele Giuseppe Esposito <eesposit@redhat.com>
2022-07-25 03:38:52 -0400

    job.c: enable job lock/unlock and remove Aiocontext locks
    Change the job_{lock/unlock} and macros to use job_mutex.
    
    Now that they are not nop anymore, remove the aiocontext
    to avoid deadlocks.
    
    Therefore:
    - when possible, remove completely the aiocontext lock/unlock pair
    - if it is used by some other function too, reduce the locking
      section as much as possible, leaving the job API outside.
    - change AIO_WAIT_WHILE in AIO_WAIT_WHILE_UNLOCKED, since we
      are not using the aiocontext lock anymore
    
    There is only one JobDriver callback, ->free() that assumes that
    the aiocontext lock is held (because it calls bdrv_unref), so for
    now keep that under aiocontext lock.
    
    Also remove real_job_{lock/unlock}, as they are replaced by the
    public functions.
    
    Signed-off-by: Emanuele Giuseppe Esposito <eesposit@redhat.com>
    Reviewed-by: Stefan Hajnoczi <stefanha@redhat.com>
    Message-Id: <20220725073855.76049-19-eesposit@redhat.com>

--- tests/unit/test-blockjob.c
+++ tests/unit/test-blockjob.c
@@ -228,10 +228,6 @@ static void cancel_common(CancelJob *s)
     BlockJob *job = &s->common;
     BlockBackend *blk = s->blk;
     JobStatus sts = job->job.status;
-    AioContext *ctx;
-
-    ctx = job->job.aio_context;
-    aio_context_acquire(ctx);
 
     job_cancel_sync(&job->job, true);
     WITH_JOB_LOCK_GUARD() {
@@ -244,7 +240,6 @@ static void cancel_common(CancelJob *s)
     }
     destroy_blk(blk);
 
-    aio_context_release(ctx);
 }
 
 static void test_cancel_created(void)
@@ -384,12 +379,10 @@ static void test_cancel_concluded(void)
     aio_poll(qemu_get_aio_context(), true);
     assert_job_status_is(job, JOB_STATUS_PENDING);
 
-    aio_context_acquire(job->aio_context);
     WITH_JOB_LOCK_GUARD() {
         job_finalize_locked(job, &error_abort);
+        assert(job->status == JOB_STATUS_CONCLUDED);
     }
-    aio_context_release(job->aio_context);
-    assert_job_status_is(job, JOB_STATUS_CONCLUDED);
 
     cancel_common(s);
 }
@@ -482,13 +475,11 @@ static void test_complete_in_standby(void)
 
     /* Wait for the job to become READY */
     job_start(job);
-    aio_context_acquire(ctx);
     /*
      * Here we are waiting for the status to change, so don't bother
      * protecting the read every time.
      */
-    AIO_WAIT_WHILE(ctx, job->status != JOB_STATUS_READY);
-    aio_context_release(ctx);
+    AIO_WAIT_WHILE_UNLOCKED(ctx, job->status != JOB_STATUS_READY);
 
     /* Begin the drained section, pausing the job */
     bdrv_drain_all_begin();
@@ -507,6 +498,7 @@ static void test_complete_in_standby(void)
         job_complete_locked(job, &error_abort);
 
         /* The test is done now, clean up. */
+        aio_context_release(ctx);
         job_finish_sync_locked(job, NULL, &error_abort);
         assert(job->status == JOB_STATUS_PENDING);
 
@@ -516,6 +508,7 @@ static void test_complete_in_standby(void)
         job_dismiss_locked(&job, &error_abort);
     }
 
+    aio_context_acquire(ctx);
     destroy_blk(blk);
     aio_context_release(ctx);
     iothread_join(iothread);
