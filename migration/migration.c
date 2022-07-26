ce4ac1c885e4be7b72e9f4aa849a154dae66e353
Steve Sistare <steven.sistare@oracle.com>
2022-07-26 09:10:00 -0700

    migration: file URI
    Extend the migration URI to support file:<filename>.  This can be used for
    any migration scenario that does not require a reverse path.  It is easier
    to use than the fd:<fdname> URI.  It can be use in HMP commands, and as
    a qemu command-line parameter.
    
    Signed-off-by: Steve Sistare <steven.sistare@oracle.com>
    Message-Id: <1658851843-236870-4-git-send-email-steven.sistare@oracle.com>

--- migration/migration.c
+++ migration/migration.c
@@ -20,6 +20,7 @@
 #include "migration/blocker.h"
 #include "exec.h"
 #include "fd.h"
+#include "file.h"
 #include "socket.h"
 #include "sysemu/runstate.h"
 #include "sysemu/sysemu.h"
@@ -505,6 +506,8 @@ static void qemu_start_incoming_migration(const char *uri, Error **errp)
         exec_start_incoming_migration(p, errp);
     } else if (strstart(uri, "fd:", &p)) {
         fd_start_incoming_migration(p, errp);
+    } else if (strstart(uri, "file:", &p)) {
+        file_start_incoming_migration(p, errp);
     } else {
         error_setg(errp, "unknown migration protocol: %s", uri);
     }
@@ -2414,6 +2417,8 @@ void qmp_migrate(const char *uri, bool has_blk, bool blk,
         exec_start_outgoing_migration(s, p, &local_err);
     } else if (strstart(uri, "fd:", &p)) {
         fd_start_outgoing_migration(s, p, &local_err);
+    } else if (strstart(uri, "file:", &p)) {
+        file_start_outgoing_migration(s, p, &local_err);
     } else {
         if (!(has_resume && resume)) {
             yank_unregister_instance(MIGRATION_YANK_INSTANCE);
