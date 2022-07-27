8e30d5dacfe63e71932ea8ce845557f15df4afbc
Aidan Khoury <aidan@revers.engineering>
2022-07-27 15:00:12 -0300

    WHPX: Add support for device backed memory regions
    Due to skipping the mapping of read only device memory, Windows
    Hypervisor Platform would fail to emulate such memory accesses when booting
    OVMF EDK2 firmware. This patch adds ROM device memory region support
    for WHPX since the Windows Hypervisor Platform supports mapping read-only
    device memory, which allows successful booting of OVMF EDK2 firmware.
    
    Resolves: https://gitlab.com/qemu-project/qemu/-/issues/513
              https://gitlab.com/qemu-project/qemu/-/issues/934
    Buglink: https://bugs.launchpad.net/bugs/1821595
    
    Signed-off-by: Aidan Khoury <aidan@revers.engineering>
    Tested-by: Helge Konetzka <hk@zapateado.de>
    Message-Id: <20220727180012.45615-1-aidan@revers.engineering>

--- target/i386/whpx/whpx-all.c
+++ target/i386/whpx/whpx-all.c
@@ -2363,11 +2363,18 @@ static void whpx_process_section(MemoryRegionSection *section, int add)
     MemoryRegion *mr = section->mr;
     hwaddr start_pa = section->offset_within_address_space;
     ram_addr_t size = int128_get64(section->size);
+    bool is_romd = false;
     unsigned int delta;
     uint64_t host_va;
 
     if (!memory_region_is_ram(mr)) {
-        return;
+        if (memory_region_is_romd(mr)) {
+            is_romd = true;
+            warn_report("WHPX: ROMD region 0x%016" PRIx64 "->0x%016" PRIx64,
+                        start_pa, start_pa + size);
+        } else {
+            return;
+        }
     }
 
     delta = qemu_real_host_page_size() - (start_pa & ~qemu_real_host_page_mask());
@@ -2386,7 +2393,7 @@ static void whpx_process_section(MemoryRegionSection *section, int add)
             + section->offset_within_region + delta;
 
     whpx_update_mapping(start_pa, size, (void *)(uintptr_t)host_va, add,
-                        memory_region_is_rom(mr), mr->name);
+                        memory_region_is_rom(mr) || is_romd, mr->name);
 }
 
 static void whpx_region_add(MemoryListener *listener,
