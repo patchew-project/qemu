ebc2f5c6326ccf6c74cb54cf2006e0863c8c0515
Xuan Zhuo <xuanzhuo@linux.alibaba.com>
2022-08-16 09:06:33 +0800

    virtio-net: support queue reset
    virtio-net, vhost-kernel, vhost-user implement queue reset.
    Queued packets in the corresponding queue pair are flushed
    or purged.
    
    Signed-off-by: Xuan Zhuo <xuanzhuo@linux.alibaba.com>
    Signed-off-by: Kangjie Xu <kangjie.xu@linux.alibaba.com>
    Message-Id: <58daee15b4c2247c7a31e56ce3e99e33888c1484.1660611460.git.kangjie.xu@linux.alibaba.com>

--- hw/net/virtio-net.c
+++ hw/net/virtio-net.c
@@ -540,6 +540,24 @@ static RxFilterInfo *virtio_net_query_rxfilter(NetClientState *nc)
     return info;
 }
 
+static void virtio_net_queue_reset(VirtIODevice *vdev, uint32_t queue_index)
+{
+    VirtIONet *n = VIRTIO_NET(vdev);
+    NetClientState *nc = qemu_get_subqueue(n->nic, vq2q(queue_index));
+
+    if (!nc->peer) {
+        return;
+    }
+
+    if (get_vhost_net(nc->peer) &&
+        ((nc->peer->info->type == NET_CLIENT_DRIVER_TAP) ||
+         (nc->peer->info->type == NET_CLIENT_DRIVER_VHOST_USER))) {
+        vhost_net_virtqueue_stop(vdev, nc, queue_index);
+    }
+
+    flush_or_purge_queued_packets(nc);
+}
+
 static void virtio_net_reset(VirtIODevice *vdev)
 {
     VirtIONet *n = VIRTIO_NET(vdev);
@@ -3784,6 +3802,7 @@ static void virtio_net_class_init(ObjectClass *klass, void *data)
     vdc->set_features = virtio_net_set_features;
     vdc->bad_features = virtio_net_bad_features;
     vdc->reset = virtio_net_reset;
+    vdc->queue_reset = virtio_net_queue_reset;
     vdc->set_status = virtio_net_set_status;
     vdc->guest_notifier_mask = virtio_net_guest_notifier_mask;
     vdc->guest_notifier_pending = virtio_net_guest_notifier_pending;
