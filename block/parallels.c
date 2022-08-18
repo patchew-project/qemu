9c51d15bb21270e3368efa68d084e9833e767ffa
Alexander Ivanov <alexander.ivanov@virtuozzo.com>
2022-08-18 17:14:55 +0200

    parallels: Move statistic collection to a separate function
    We will add more and more checks so we need a better code structure
    in parallels_co_check. Let each check performs in a separate loop
    in a separate helper.
    
    Signed-off-by: Alexander Ivanov <alexander.ivanov@virtuozzo.com>
    Message-Id: <20220818151456.2118470-9-alexander.ivanov@virtuozzo.com>

--- block/parallels.c
+++ block/parallels.c
@@ -528,14 +528,40 @@ static int parallels_check_leak(BlockDriverState *bs,
     return 0;
 }
 
+static void parallels_collect_statistics(BlockDriverState *bs,
+                                         BdrvCheckResult *res,
+                                         BdrvCheckMode fix)
+{
+    BDRVParallelsState *s = bs->opaque;
+    int64_t off, prev_off;
+    uint32_t i;
+
+    res->bfi.total_clusters = s->bat_size;
+    res->bfi.compressed_clusters = 0; /* compression is not supported */
+
+    prev_off = 0;
+    for (i = 0; i < s->bat_size; i++) {
+        off = bat2sect(s, i) << BDRV_SECTOR_BITS;
+        if (off == 0) {
+            prev_off = 0;
+            continue;
+        }
+
+        if (prev_off != 0 && (prev_off + s->cluster_size) != off) {
+            res->bfi.fragmented_clusters++;
+        }
+
+        prev_off = off;
+        res->bfi.allocated_clusters++;
+    }
+}
+
 static int coroutine_fn parallels_co_check(BlockDriverState *bs,
                                            BdrvCheckResult *res,
                                            BdrvCheckMode fix)
 {
     BDRVParallelsState *s = bs->opaque;
-    int64_t prev_off;
     int ret = 0;
-    uint32_t i;
 
     qemu_co_mutex_lock(&s->lock);
 
@@ -551,24 +577,7 @@ static int coroutine_fn parallels_co_check(BlockDriverState *bs,
         goto out;
     }
 
-    res->bfi.total_clusters = s->bat_size;
-    res->bfi.compressed_clusters = 0; /* compression is not supported */
-
-    prev_off = 0;
-    for (i = 0; i < s->bat_size; i++) {
-        int64_t off = bat2sect(s, i) << BDRV_SECTOR_BITS;
-        if (off == 0) {
-            prev_off = 0;
-            continue;
-        }
-
-        res->bfi.allocated_clusters++;
-
-        if (prev_off != 0 && (prev_off + s->cluster_size) != off) {
-            res->bfi.fragmented_clusters++;
-        }
-        prev_off = off;
-    }
+    parallels_collect_statistics(bs, res, fix);
 
 out:
     qemu_co_mutex_unlock(&s->lock);
