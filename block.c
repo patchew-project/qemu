6c6d6d9d3332c7dae3080ec79cc82d1840a61a03
Bin Meng <bin.meng@windriver.com>
2022-08-24 17:39:41 +0800

    block: Unify the get_tmp_filename() implementation
    At present get_tmp_filename() has platform specific implementations
    to get the directory to use for temporary files. Switch over to use
    g_get_tmp_dir() which works on all supported platforms.
    
    Signed-off-by: Bin Meng <bin.meng@windriver.com>
    Message-Id: <20220824094029.1634519-4-bmeng.cn@gmail.com>

--- block.c
+++ block.c
@@ -864,21 +864,10 @@ int bdrv_probe_geometry(BlockDriverState *bs, HDGeometry *geo)
  */
 int get_tmp_filename(char *filename, int size)
 {
-#ifdef _WIN32
-    char temp_dir[MAX_PATH];
-    /* GetTempFileName requires that its output buffer (4th param)
-       have length MAX_PATH or greater.  */
-    assert(size >= MAX_PATH);
-    return (GetTempPath(MAX_PATH, temp_dir)
-            && GetTempFileName(temp_dir, "qem", 0, filename)
-            ? 0 : -GetLastError());
-#else
     int fd;
     const char *tmpdir;
-    tmpdir = getenv("TMPDIR");
-    if (!tmpdir) {
-        tmpdir = "/var/tmp";
-    }
+    tmpdir = g_get_tmp_dir();
+
     if (snprintf(filename, size, "%s/vl.XXXXXX", tmpdir) >= size) {
         return -EOVERFLOW;
     }
@@ -891,7 +880,6 @@ int get_tmp_filename(char *filename, int size)
         return -errno;
     }
     return 0;
-#endif
 }
 
 /*
