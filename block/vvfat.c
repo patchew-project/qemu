711af6969e9d759aa3ce4cd2a116e8495b980925
Bin Meng <bin.meng@windriver.com>
2022-08-24 17:39:46 +0800

    block/vvfat: Unify the mkdir() call
    There is a difference in the mkdir() call for win32 and non-win32
    platforms, and currently is handled in the codes with #ifdefs.
    
    glib provides a portable g_mkdir_with_parents() API and we can use
    it to unify the codes without #ifdefs.
    
    Signed-off-by: Bin Meng <bin.meng@windriver.com>
    Message-Id: <20220824094029.1634519-9-bmeng.cn@gmail.com>

--- block/vvfat.c
+++ block/vvfat.c
@@ -2726,13 +2726,9 @@ static int handle_renames_and_mkdirs(BDRVVVFATState* s)
             mapping_t* mapping;
             int j, parent_path_len;
 
-#ifdef __MINGW32__
-            if (mkdir(commit->path))
+            if (g_mkdir_with_parents(commit->path, 0755)) {
                 return -5;
-#else
-            if (mkdir(commit->path, 0755))
-                return -5;
-#endif
+            }
 
             mapping = insert_mapping(s, commit->param.mkdir.cluster,
                     commit->param.mkdir.cluster + 1);
