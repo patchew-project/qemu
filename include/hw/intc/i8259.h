21bba0b99a44a8c3b7dd7ece8a28e150aa2479c4
Bernhard Beschow <shentey@gmail.com>
2022-09-01 18:25:38 +0200

    hw/intc/i8259: Introduce i8259 proxy "isa-pic"
    Having an i8259 proxy allows for ISA PICs to be created and wired up in
    southbridges. This is especially interesting for PIIX3 for two reasons:
    First, the southbridge doesn't need to care about the virtualization
    technology used (KVM, TCG, Xen) due to in-IRQs (where devices get
    attached) and out-IRQs (which will trigger the IRQs of the respective
    virtzalization technology) are separated. Second, since the in-IRQs are
    populated with fully initialized qemu_irq's, they can already be wired
    up inside PIIX3.
    
    Signed-off-by: Bernhard Beschow <shentey@gmail.com>
    Message-Id: <20220901162613.6939-8-shentey@gmail.com>

--- include/hw/intc/i8259.h
+++ include/hw/intc/i8259.h
@@ -1,6 +1,20 @@
 #ifndef HW_I8259_H
 #define HW_I8259_H
 
+#include "qom/object.h"
+#include "hw/isa/isa.h"
+#include "qemu/typedefs.h"
+
+#define TYPE_ISA_PIC "isa-pic"
+OBJECT_DECLARE_SIMPLE_TYPE(ISAPICState, ISA_PIC)
+
+struct ISAPICState {
+    ISADevice parent_obj;
+
+    qemu_irq in_irqs[ISA_NUM_IRQS];
+    qemu_irq out_irqs[ISA_NUM_IRQS];
+};
+
 /* i8259.c */
 
 extern DeviceState *isa_pic;
