b5ed72b20211a17d1c15bb53060abbb099c763fb
Bernhard Beschow <shentey@gmail.com>
2022-09-01 18:25:33 +0200

    hw/i386/pc: Create RTC controllers in south bridges
    Just like in the real hardware (and in PIIX4), create the RTC
    controllers in the south bridges.
    
    Signed-off-by: Bernhard Beschow <shentey@gmail.com>
    Message-Id: <20220901162613.6939-3-shentey@gmail.com>

--- include/hw/southbridge/piix.h
+++ include/hw/southbridge/piix.h
@@ -14,6 +14,7 @@
 
 #include "hw/pci/pci.h"
 #include "qom/object.h"
+#include "hw/rtc/mc146818rtc.h"
 
 /* PIRQRC[A:D]: PIRQx Route Control Registers */
 #define PIIX_PIRQCA 0x60
@@ -51,6 +52,8 @@ struct PIIXState {
 
     /* This member isn't used. Just for save/load compatibility */
     int32_t pci_irq_levels_vmstate[PIIX_NUM_PIRQS];
+
+    RTCState rtc;
 
     /* Reset Control Register contents */
     uint8_t rcr;
