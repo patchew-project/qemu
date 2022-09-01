acb72992a3a2ec634293963eb1d5307fc5387e4b
Richard Henderson <richard.henderson@linaro.org>
2022-09-01 11:15:09 +0100

    target/sh4: Fix TB_FLAG_UNALIGN
    The value previously chosen overlaps GUSA_MASK.
    
    Rename all DELAY_SLOT_* and GUSA_* defines to emphasize
    that they are included in TB_FLAGs.  Add aliases for the
    FPSCR and SR bits that are included in TB_FLAGS, so that
    we don't accidentally reassign those bits.
    
    Fixes: 4da06fb3062 ("target/sh4: Implement prctl_unalign_sigbus")
    Resolves: https://gitlab.com/qemu-project/qemu/-/issues/856
    Signed-off-by: Richard Henderson <richard.henderson@linaro.org>
    Reviewed-by: Yoshinori Sato <ysato@users.sourceforge.jp>
    Message-Id: <20220901101509.145758-1-richard.henderson@linaro.org>

--- target/sh4/cpu.c
+++ target/sh4/cpu.c
@@ -40,7 +40,7 @@ static void superh_cpu_synchronize_from_tb(CPUState *cs,
     SuperHCPU *cpu = SUPERH_CPU(cs);
 
     cpu->env.pc = tb->pc;
-    cpu->env.flags = tb->flags & TB_FLAG_ENVFLAGS_MASK;
+    cpu->env.flags = tb->flags;
 }
 
 #ifndef CONFIG_USER_ONLY
@@ -50,10 +50,10 @@ static bool superh_io_recompile_replay_branch(CPUState *cs,
     SuperHCPU *cpu = SUPERH_CPU(cs);
     CPUSH4State *env = &cpu->env;
 
-    if ((env->flags & ((DELAY_SLOT | DELAY_SLOT_CONDITIONAL))) != 0
+    if ((env->flags & (TB_FLAG_DELAY_SLOT | TB_FLAG_DELAY_SLOT_COND))
         && env->pc != tb->pc) {
         env->pc -= 2;
-        env->flags &= ~(DELAY_SLOT | DELAY_SLOT_CONDITIONAL);
+        env->flags &= ~(TB_FLAG_DELAY_SLOT | TB_FLAG_DELAY_SLOT_COND);
         return true;
     }
     return false;
