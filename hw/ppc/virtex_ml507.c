6a33a27b0faf02c2884af52d45c47d451f5f5ee1
Daniel Henrique Barboza <danielhb413@gmail.com>
2022-09-03 16:34:13 -0300

    hw/ppc: set machine->fdt in xilinx_load_device_tree()
    This will enable support for 'dumpdtb' QMP/HMP command for the
    virtex_ml507 machine.
    
    Cc: Edgar E. Iglesias <edgar.iglesias@gmail.com>
    Signed-off-by: Daniel Henrique Barboza <danielhb413@gmail.com>
    Message-Id: <20220903193420.115986-8-danielhb413@gmail.com>

--- hw/ppc/virtex_ml507.c
+++ hw/ppc/virtex_ml507.c
@@ -45,6 +45,8 @@
 #include "hw/qdev-properties.h"
 #include "ppc405.h"
 
+#include <libfdt.h>
+
 #define EPAPR_MAGIC    (0x45504150)
 #define FLASH_SIZE     (16 * MiB)
 
@@ -150,6 +152,7 @@ static int xilinx_load_device_tree(hwaddr addr,
                                       hwaddr initrd_size,
                                       const char *kernel_cmdline)
 {
+    MachineState *machine = MACHINE(qdev_get_machine());
     char *path;
     int fdt_size;
     void *fdt = NULL;
@@ -194,7 +197,13 @@ static int xilinx_load_device_tree(hwaddr addr,
     if (r < 0)
         fprintf(stderr, "couldn't set /chosen/bootargs\n");
     cpu_physical_memory_write(addr, fdt, fdt_size);
-    g_free(fdt);
+
+    /*
+     * Update the machine->fdt pointer to enable support for
+     * 'dumpdtb'QMP/HMP command.
+     */
+    machine->fdt = fdt;
+
     return fdt_size;
 }
 
