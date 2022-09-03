a8d318259177b613a2445fff1b301b2d9d61d65f
Daniel Henrique Barboza <danielhb413@gmail.com>
2022-09-03 16:34:08 -0300

    hw/microblaze: set machine->fdt in microblaze_load_dtb()
    This will enable support for 'dumpdtb' QMP/HMP command for all
    microblaze machines that uses microblaze_load_dtb().
    
    Cc: Edgar E. Iglesias <edgar.iglesias@gmail.com>
    Signed-off-by: Daniel Henrique Barboza <danielhb413@gmail.com>
    Message-Id: <20220903193420.115986-3-danielhb413@gmail.com>

--- hw/microblaze/boot.c
+++ hw/microblaze/boot.c
@@ -39,6 +39,8 @@
 
 #include "boot.h"
 
+#include <libfdt.h>
+
 static struct
 {
     void (*machine_cpu_reset)(MicroBlazeCPU *);
@@ -72,6 +74,7 @@ static int microblaze_load_dtb(hwaddr addr,
                                const char *kernel_cmdline,
                                const char *dtb_filename)
 {
+    MachineState *machine = MACHINE(qdev_get_machine());
     int fdt_size;
     void *fdt = NULL;
     int r;
@@ -100,7 +103,10 @@ static int microblaze_load_dtb(hwaddr addr,
     }
 
     cpu_physical_memory_write(addr, fdt, fdt_size);
-    g_free(fdt);
+
+    /* Set machine->fdt for 'dumpdtb' QMP/HMP command */
+    machine->fdt = fdt;
+
     return fdt_size;
 }
 
