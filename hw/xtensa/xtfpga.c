514d9a3c86fd5ab761608d74a09b39247f995d54
Daniel Henrique Barboza <danielhb413@gmail.com>
2022-09-04 20:34:55 -0300

    hw/xtensa: set machine->fdt in xtfpga_init()
    This will enable support for the 'dumpdtb' QMP/HMP command for all
    xtensa machines that uses a FDT.
    
    Signed-off-by: Daniel Henrique Barboza <danielhb413@gmail.com>
    Message-Id: <20220904233456.209027-14-danielhb413@gmail.com>

--- hw/xtensa/xtfpga.c
+++ hw/xtensa/xtfpga.c
@@ -50,6 +50,8 @@
 #include "hw/xtensa/mx_pic.h"
 #include "migration/vmstate.h"
 
+#include <libfdt.h>
+
 typedef struct XtfpgaFlashDesc {
     hwaddr base;
     size_t size;
@@ -377,7 +379,9 @@ static void xtfpga_init(const XtfpgaBoardDesc *board, MachineState *machine)
             cur_tagptr = put_tag(cur_tagptr, BP_TAG_FDT,
                                  sizeof(dtb_addr), &dtb_addr);
             cur_lowmem = QEMU_ALIGN_UP(cur_lowmem + fdt_size, 4 * KiB);
-            g_free(fdt);
+
+            /* Set machine->fdt for 'dumpdtb' QMP/HMP command */
+            machine->fdt = fdt;
         }
 #else
         if (dtb_filename) {
