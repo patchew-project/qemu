42e2142a8907af6295dccfa3025bc85e89995c1f
Milica Lazarevic <milica.lazarevic@syrmia.com>
2022-09-05 11:55:07 +0200

    disas/nanomips: Remove __cond methods from class
    NMD class methods with the conditional_function type like
    NMD::ADDIU_32__cond, NMD::ADDIU_RS5__cond, etc. are removed from the NMD
    class. They're now declared global static functions. Therefore, typedef
    of the function pointer, conditional_function is defined outside of the
    class.
    
    Now that conditional_function type functions are not part of the NMD
    class we can't access them using the this pointer. Thus, the use of
    the this pointer has been deleted.
    
    Signed-off-by: Milica Lazarevic <milica.lazarevic@syrmia.com>
    Reviewed-by: Philippe Mathieu-Daudé <f4bug@amsat.org>
    Reviewed-by: Richard Henderson <richard.henderson@linaro.org>
    Message-Id: <20220905095522.66941-6-milica.lazarevic@syrmia.com>

--- disas/nanomips.h
+++ disas/nanomips.h
@@ -31,6 +31,8 @@ typedef uint32_t uint32;
 typedef uint16_t uint16;
 typedef uint64_t img_address;
 
+typedef bool (*conditional_function)(uint64 instruction);
+
 enum TABLE_ENTRY_TYPE {
     instruction,
     call_instruction,
@@ -72,7 +74,6 @@ private:
 
     typedef std::string(NMD:: *disassembly_function)(uint64 instruction,
                                                      img_address m_pc);
-    typedef bool(NMD:: *conditional_function)(uint64 instruction);
 
     struct Pool {
         TABLE_ENTRY_TYPE     type;
@@ -90,17 +91,6 @@ private:
     int Disassemble(const uint16 *data, std::string & dis,
                     TABLE_ENTRY_TYPE & type, const Pool *table,
                     int table_size, img_address m_pc);
-
-    bool ADDIU_32__cond(uint64 instruction);
-    bool ADDIU_RS5__cond(uint64 instruction);
-    bool BALRSC_cond(uint64 instruction);
-    bool BEQC_16__cond(uint64 instruction);
-    bool BNEC_16__cond(uint64 instruction);
-    bool MOVE_cond(uint64 instruction);
-    bool P16_BR1_cond(uint64 instruction);
-    bool PREF_S9__cond(uint64 instruction);
-    bool PREFE_cond(uint64 instruction);
-    bool SLTU_cond(uint64 instruction);
 
     std::string ABS_D(uint64 instruction, img_address m_pc);
     std::string ABS_S(uint64 instruction, img_address m_pc);
