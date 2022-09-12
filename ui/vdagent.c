9c5db3b10c6dfc2e84778081addc828828869e78
Marc-André Lureau <marcandre.lureau@redhat.com>
2022-09-12 14:24:51 +0400

    ui: add some vdagent related traces
    This helps debugging clipboard serial sync issues.
    
    Signed-off-by: Marc-André Lureau <marcandre.lureau@redhat.com>
    Message-Id: <20220912102455.111765-2-marcandre.lureau@redhat.com>

--- ui/vdagent.c
+++ ui/vdagent.c
@@ -533,6 +533,7 @@ static void vdagent_clipboard_recv_grab(VDAgentChardev *vd, uint8_t s, uint32_t 
         info->has_serial = true;
         info->serial = *(uint32_t *)data;
         if (info->serial < vd->last_serial[s]) {
+            trace_vdagent_cb_grab_discard(GET_NAME(sel_name, s), vd->last_serial[s], info->serial);
             /* discard lower-ordering guest grab */
             return;
         }
@@ -853,6 +854,8 @@ static void vdagent_chr_accept_input(Chardev *chr)
 
 static void vdagent_disconnect(VDAgentChardev *vd)
 {
+    trace_vdagent_disconnect();
+
     buffer_reset(&vd->outbuf);
     vdagent_reset_bufs(vd);
     vd->caps = 0;
