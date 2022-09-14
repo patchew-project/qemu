fcf2e58d223506ef382ff5619ad3e194ccbe0d9a
BALATON Zoltan <balaton@eik.bme.hu>
2022-09-14 13:34:24 +0200

    ppc440_sdram: Get rid of the init RAM hack
    Remove the do_init parameter of ppc440_sdram_init and enable SDRAM
    controller from the board via DCR access instead. Firmware does this
    so it may not be needed when booting firmware only with -kernel but we
    enable it unconditionally to preserve previous behaviour.
    
    Signed-off-by: BALATON Zoltan <balaton@eik.bme.hu>
    Message-Id: <cd7fae004b752f235bcecaae6892793f86cd7d21.1663154398.git.balaton@eik.bme.hu>

--- hw/ppc/ppc440_uc.c
+++ hw/ppc/ppc440_uc.c
@@ -727,12 +727,11 @@ static void sdram_reset(void *opaque)
     ppc440_sdram_t *sdram = opaque;
 
     sdram->addr = 0;
-    sdram->mcopt2 = SDRAM_DDR2_MCOPT2_DCEN;
+    sdram->mcopt2 = 0;
 }
 
 void ppc440_sdram_init(CPUPPCState *env, int nbanks,
-                       Ppc4xxSdramBank *ram_banks,
-                       int do_init)
+                       Ppc4xxSdramBank *ram_banks)
 {
     ppc440_sdram_t *sdram;
     int i;
@@ -749,9 +748,6 @@ void ppc440_sdram_init(CPUPPCState *env, int nbanks,
                      sdram, &dcr_read_sdram, &dcr_write_sdram);
     ppc_dcr_register(env, SDRAM0_CFGDATA,
                      sdram, &dcr_read_sdram, &dcr_write_sdram);
-    if (do_init) {
-        sdram_map_bcr(sdram);
-    }
 
     ppc_dcr_register(env, SDRAM_R0BAS,
                      sdram, &dcr_read_sdram, &dcr_write_sdram);
