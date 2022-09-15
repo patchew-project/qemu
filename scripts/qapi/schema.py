6759329f1dbf666b394a0185e61c847647a7844f
Markus Armbruster <armbru@redhat.com>
2022-09-15 22:42:55 +0200

    qapi tests: Elide redundant has_FOO in generated C
    The has_FOO for pointer-valued FOO are redundant, except for arrays.
    They are also a nuisance to work with.  Recent commit "qapi: Start to
    elide redundant has_FOO in generated C" provided the means to elide
    them step by step.  This is the step for
    tests/qapi-schema/qapi-schema-test.json.
    
    Said commit explains the transformation in more detail.  The invariant
    violations mentioned there do not occur here.
    
    Signed-off-by: Markus Armbruster <armbru@redhat.com>
    Reviewed-by: Daniel P. Berrangé <berrange@redhat.com>
    Message-Id: <20220915204317.3766007-6-armbru@redhat.com>

--- scripts/qapi/schema.py
+++ scripts/qapi/schema.py
@@ -767,7 +767,6 @@ class QAPISchemaObjectTypeMember(QAPISchemaMember):
             'qapi/char.json',
             'qapi/crypto.json',
             'qapi/dump.json',
-            'qapi/introspect.json',
             'qapi/job.json',
             'qapi/machine.json',
             'qapi/machine-target.json',
@@ -784,8 +783,7 @@ class QAPISchemaObjectTypeMember(QAPISchemaMember):
             'qapi/tpm.json',
             'qapi/transaction.json',
             'qapi/ui.json',
-            'qga/qapi-schema.json',
-            'tests/qapi-schema/qapi-schema-test.json']
+            'qga/qapi-schema.json']
         if self.info and any(self.info.fname.endswith(mod)
                              for mod in opt_out):
             return self.optional
