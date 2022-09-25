6c4c38e7492768d4c214817ca3817799a40b284f
Bin Meng <bin.meng@windriver.com>
2022-09-25 19:30:23 +0800

    tests/qtest: libqtest: Replace the call to close a socket with closesocket()
    close() is a *nix function. It works on any file descriptor, and
    sockets in *nix are an example of a file descriptor.
    
    closesocket() is a Windows-specific function, which works only
    specifically with sockets. Sockets on Windows do not use *nix-style
    file descriptors, and socket() returns a handle to a kernel object
    instead, so it must be closed with closesocket().
    
    In QEMU there is already a logic to handle such platform difference
    in os-posix.h and os-win32.h, that:
    
      * closesocket maps to close on POSIX
      * closesocket maps to a wrapper that calls the real closesocket()
        on Windows
    
    Replace the call to close a socket with closesocket() instead.
    
    Signed-off-by: Bin Meng <bin.meng@windriver.com>
    Reviewed-by: Marc-André Lureau <marcandre.lureau@redhat.com>
    Message-Id: <20220925113032.1949844-46-bmeng.cn@gmail.com>

--- tests/qtest/libqtest.c
+++ tests/qtest/libqtest.c
@@ -113,7 +113,7 @@ static int socket_accept(int sock)
                    (void *)&timeout, sizeof(timeout))) {
         fprintf(stderr, "%s failed to set SO_RCVTIMEO: %s\n",
                 __func__, strerror(errno));
-        close(sock);
+        closesocket(sock);
         return -1;
     }
 
@@ -124,7 +124,7 @@ static int socket_accept(int sock)
     if (ret == -1) {
         fprintf(stderr, "%s failed: %s\n", __func__, strerror(errno));
     }
-    close(sock);
+    closesocket(sock);
 
     return ret;
 }
@@ -507,8 +507,8 @@ void qtest_quit(QTestState *s)
     qtest_remove_abrt_handler(s);
 
     qtest_kill_qemu(s);
-    close(s->fd);
-    close(s->qmp_fd);
+    closesocket(s->fd);
+    closesocket(s->qmp_fd);
     g_string_free(s->rx, true);
 
     for (GList *it = s->pending_events; it != NULL; it = it->next) {
