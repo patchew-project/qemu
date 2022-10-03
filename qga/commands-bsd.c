d35dd6cf50f1fbdc38d07c96445d3e7d0bde6234
Alexander Ivanov <alexander.ivanov@virtuozzo.com>
2022-10-03 11:39:11 +0200

    qga: Add HW address getting for FreeBSD
    Replace a dumb function in commands-bsd.c by the code of HW address
    getting.
    
    Reviewed-by: Marc-André Lureau <marcandre.lureau@redhat.com>
    Signed-off-by: Alexander Ivanov <alexander.ivanov@virtuozzo.com>
    Reviewed-by: Konstantin Kostiuk <kkostiuk@redhat.com>
    Message-Id: <20221003093911.1844031-8-alexander.ivanov@virtuozzo.com>

--- qga/commands-bsd.c
+++ qga/commands-bsd.c
@@ -20,6 +20,8 @@
 #include <sys/param.h>
 #include <sys/ucred.h>
 #include <sys/mount.h>
+#include <net/if_dl.h>
+#include <net/ethernet.h>
 #include <paths.h>
 
 #if defined(CONFIG_FSFREEZE) || defined(CONFIG_FSTRIM)
@@ -179,7 +181,19 @@ GuestCpuStatsList *qmp_guest_get_cpustats(Error **errp)
 int guest_get_hw_addr(struct ifaddrs *ifa, unsigned char *buf,
                       bool *obtained, Error **errp)
 {
+    struct sockaddr_dl *sdp;
+
     *obtained = false;
+    if (ifa->ifa_addr->sa_family != AF_LINK) {
+        /* We can get HW address only for AF_LINK family. */
+        g_debug("failed to get MAC address of %s", ifa->ifa_name);
+        return 0;
+    }
+
+    sdp = (struct sockaddr_dl *)ifa->ifa_addr;
+    memcpy(buf, sdp->sdl_data + sdp->sdl_nlen, ETHER_ADDR_LEN);
+    *obtained = true;
+
     return 0;
 }
 #endif /* HAVE_GETIFADDRS */
