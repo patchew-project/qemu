7e0ed9f87a8e13432715daa2cea1de39a6a4ce25
Nikolay Borisov <nborisov@suse.com>
2022-10-10 16:34:04 +0300

    migration: add qemu_get_buffer_at
    Restoring a 'fixed-ram' enabled migration stream would require reading
    from specific offsets in the file so add a helper to QEMUFile that uses
    the newly introduced qio_channel_file_preadv.
    
    Signed-off-by: Nikolay Borisov <nborisov@suse.com>
    Message-Id: <20221010133408.3214433-8-nborisov@suse.com>

--- migration/qemu-file.h
+++ migration/qemu-file.h
@@ -151,6 +151,7 @@ void qemu_file_set_blocking(QEMUFile *f, bool block);
 void qemu_set_offset(QEMUFile *f, off_t off, int whence);
 off_t qemu_get_offset(QEMUFile *f);
 void qemu_put_buffer_at(QEMUFile *f, const uint8_t *buf, size_t buflen, off_t pos);
+size_t qemu_get_buffer_at(QEMUFile *f, const uint8_t *buf, size_t buflen, off_t pos);
 
 void ram_control_before_iterate(QEMUFile *f, uint64_t flags);
 void ram_control_after_iterate(QEMUFile *f, uint64_t flags);
