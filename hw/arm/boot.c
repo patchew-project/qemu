8cb86be7b72c8d2a440c8b890e827b1419d478bf
Jason A. Donenfeld <Jason@zx2c4.com>
2022-10-11 14:46:39 -0600

    arm: re-randomize rng-seed on reboot
    When the system reboots, the rng-seed that the FDT has should be
    re-randomized, so that the new boot gets a new seed. Since the FDT is in
    the ROM region at this point, we add a hook right after the ROM has been
    added, so that we have a pointer to that copy of the FDT.
    
    Cc: Peter Maydell <peter.maydell@linaro.org>
    Cc: qemu-arm@nongnu.org
    Signed-off-by: Jason A. Donenfeld <Jason@zx2c4.com>
    Message-Id: <20221011204645.1160916-3-Jason@zx2c4.com>

--- hw/arm/boot.c
+++ hw/arm/boot.c
@@ -683,6 +683,7 @@ int arm_load_dtb(hwaddr addr, const struct arm_boot_info *binfo,
      * the DTB is copied again upon reset, even if addr points into RAM.
      */
     rom_add_blob_fixed_as("dtb", fdt, size, addr, as);
+    qemu_register_reset(qemu_fdt_randomize_seeds, rom_ptr_for_as(as, addr, size));
 
     g_free(fdt);
 
