5898ab1d1080336168445cd963f519354276612f
Alexander Bulekov <alxndr@bu.edu>
2022-10-28 15:16:48 -0400

    usb/libhw: switch to guarded DMA accesses
    Fixes: https://gitlab.com/qemu-project/qemu/-/issues/541
    Signed-off-by: Alexander Bulekov <alxndr@bu.edu>
    Message-Id: <20221028191648.964076-8-alxndr@bu.edu>

--- hw/usb/libhw.c
+++ hw/usb/libhw.c
@@ -36,7 +36,7 @@ int usb_packet_map(USBPacket *p, QEMUSGList *sgl)
 
         while (len) {
             dma_addr_t xlen = len;
-            mem = dma_memory_map(sgl->as, base, &xlen, dir,
+            mem = dma_memory_map_guarded(sgl->dev, sgl->as, base, &xlen, dir,
                                  MEMTXATTRS_UNSPECIFIED);
             if (!mem) {
                 goto err;
@@ -63,7 +63,7 @@ void usb_packet_unmap(USBPacket *p, QEMUSGList *sgl)
     int i;
 
     for (i = 0; i < p->iov.niov; i++) {
-        dma_memory_unmap(sgl->as, p->iov.iov[i].iov_base,
+        dma_memory_unmap_guarded(sgl->dev, sgl->as, p->iov.iov[i].iov_base,
                          p->iov.iov[i].iov_len, dir,
                          p->iov.iov[i].iov_len);
     }
