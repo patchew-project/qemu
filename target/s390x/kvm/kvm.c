62a8e56c2d23ef394bd24572186769c747e2abe6
Pierre Morel <pmorel@linux.ibm.com>
2022-11-03 18:01:49 +0100

    s390x/cpu_topology: activating CPU topology
    The KVM capability, KVM_CAP_S390_CPU_TOPOLOGY is used to
    activate the S390_FEAT_CONFIGURATION_TOPOLOGY feature and
    the topology facility for the guest in the case the topology
    is available in QEMU and in KVM.
    
    Signed-off-by: Pierre Morel <pmorel@linux.ibm.com>
    Message-Id: <20221103170150.20789-11-pmorel@linux.ibm.com>

--- target/s390x/kvm/kvm.c
+++ target/s390x/kvm/kvm.c
@@ -2471,6 +2471,22 @@ void kvm_s390_get_host_cpu_model(S390CPUModel *model, Error **errp)
         set_bit(S390_FEAT_UNPACK, model->features);
     }
 
+    /*
+     * If we have the CPU Topology allowed in QEMU and
+     * implemented in KVM, activate the CPU TOPOLOGY feature.
+     */
+    if (s390_has_topology()) {
+        if (!kvm_check_extension(kvm_state, KVM_CAP_S390_CPU_TOPOLOGY)) {
+            error_setg(errp, "KVM: S390 topology not present in kernel");
+            return;
+        }
+        if (kvm_vm_enable_cap(kvm_state, KVM_CAP_S390_CPU_TOPOLOGY, 0) < 0) {
+            error_setg(errp, "KVM: Error enabling KVM_CAP_S390_CPU_TOPOLOGY");
+            return;
+        }
+        set_bit(S390_FEAT_CONFIGURATION_TOPOLOGY, model->features);
+    }
+
     /* We emulate a zPCI bus and AEN, therefore we don't need HW support */
     set_bit(S390_FEAT_ZPCI, model->features);
     set_bit(S390_FEAT_ADAPTER_EVENT_NOTIFICATION, model->features);
