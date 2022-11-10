650914cbb3f68617e6415e6bc5d8a1af2af32115
Klaus Jensen <k.jensen@samsung.com>
2022-11-10 08:05:19 +0100

    hw/nvme: fix cancellation of format operations
    Cancelling a format operation neglects to set iocb->ret as well as
    clearing the iocb->aiocb after cancelling the underlying aiocb.
    
    Fix this.
    
    Fixes: 3bcf26d3d619 ("hw/nvme: reimplement format nvm to allow cancellation")
    Signed-off-by: Klaus Jensen <k.jensen@samsung.com>
    Message-Id: <20221110070523.36290-3-its@irrelevant.dk>

--- hw/nvme/ctrl.c
+++ hw/nvme/ctrl.c
@@ -5762,8 +5762,11 @@ static void nvme_format_cancel(BlockAIOCB *aiocb)
 {
     NvmeFormatAIOCB *iocb = container_of(aiocb, NvmeFormatAIOCB, common);
 
+    iocb->ret = -ECANCELED;
+
     if (iocb->aiocb) {
         blk_aio_cancel_async(iocb->aiocb);
+        iocb->aiocb = NULL;
     }
 }
 
