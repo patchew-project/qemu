cbb98155b25d59f5f62d03714eed9e9566844797
Kevin Wolf <kwolf@redhat.com>
2022-11-18 18:41:03 +0100

    block: Don't use subtree drains in bdrv_drop_intermediate()
    Instead of using a subtree drain from the top node (which also drains
    child nodes of base that we're not even interested in), use a normal
    drain for base, which automatically drains all of the parents, too.
    
    Signed-off-by: Kevin Wolf <kwolf@redhat.com>
    Reviewed-by: Vladimir Sementsov-Ogievskiy <vsementsov@yandex-team.ru>
    Reviewed-by: Hanna Reitz <hreitz@redhat.com>
    Message-Id: <20221118174110.55183-9-kwolf@redhat.com>

--- block.c
+++ block.c
@@ -5599,7 +5599,7 @@ int bdrv_drop_intermediate(BlockDriverState *top, BlockDriverState *base,
     GLOBAL_STATE_CODE();
 
     bdrv_ref(top);
-    bdrv_subtree_drained_begin(top);
+    bdrv_drained_begin(base);
 
     if (!top->drv || !base->drv) {
         goto exit;
@@ -5672,7 +5672,7 @@ int bdrv_drop_intermediate(BlockDriverState *top, BlockDriverState *base,
 
     ret = 0;
 exit:
-    bdrv_subtree_drained_end(top);
+    bdrv_drained_end(base);
     bdrv_unref(top);
     return ret;
 }
