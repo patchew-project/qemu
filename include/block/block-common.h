1a6e21ee82576ca2d9f19cff69b566aabaecd09c
Emanuele Giuseppe Esposito <eesposit@redhat.com>
2022-11-23 06:42:22 -0500

    block-coroutine-wrapper.py: introduce generated_co_wrapper_simple
    This new annotation creates just a function wrapper that creates
    a new coroutine. It assumes the caller is not a coroutine.
    
    This is much better as g_c_w, because it is clear if the caller
    is a coroutine or not, and provides the advantage of automating
    the code creation. In the future all g_c_w functions will be
    substituted on g_c_w_simple.
    
    Signed-off-by: Emanuele Giuseppe Esposito <eesposit@redhat.com>
    Reviewed-by: Kevin Wolf <kwolf@redhat.com>
    Message-Id: <20221123114227.85757-11-eesposit@redhat.com>

--- include/block/block-common.h
+++ include/block/block-common.h
@@ -54,6 +54,7 @@
  * Read more in docs/devel/block-coroutine-wrapper.rst
  */
 #define generated_co_wrapper
+#define generated_co_wrapper_simple
 
 /* block.c */
 typedef struct BlockDriver BlockDriver;
