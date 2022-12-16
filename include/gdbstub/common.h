a2c6343b08d6e390c5fdfd34c707833021a6e189
Alex Bennée <alex.bennee@linaro.org>
2022-12-16 11:22:03 +0000

    includes: add new gdbstub include directory
    To avoid having a messy set of include files containing system and
    user APIs lets prepare for a cleaner layout.
    
    Signed-off-by: Alex Bennée <alex.bennee@linaro.org>
    Message-Id: <20221216112206.3171578-8-alex.bennee@linaro.org>

---
+++ include/gdbstub/common.h
@@ -0,0 +1,14 @@
+/*
+ * gdbstub common APIs
+ *
+ * Copyright (c) 2022 Linaro Ltd
+ *
+ * SPDX-License-Identifier: LGPL-2.0+
+ */
+
+#ifndef GDBSTUB_COMMON_H
+#define GDBSTUB_COMMON_H
+
+
+
+#endif /* GDBSTUB_COMMON_H */
---
+++ include/gdbstub/user.h
@@ -0,0 +1,14 @@
+/*
+ * gdbstub user-mode only APIs
+ *
+ * Copyright (c) 2022 Linaro Ltd
+ *
+ * SPDX-License-Identifier: LGPL-2.0+
+ */
+
+#ifndef GDBSTUB_USER_H
+#define GDBSTUB_USER_H
+
+
+
+#endif /* GDBSTUB_USER_H */
