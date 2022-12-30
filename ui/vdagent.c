c59c397d5039df84dc16f785a950573870f9158b
dengpc12@chinatelecom.cn <dengpc12@chinatelecom.cn>
2022-12-30 17:27:56 +0800

    vdagent: refactor vdagent_chr_recv_caps function
    Abstract vdagent registry logic into
    vdagent_register_to_qemu_clipboard.
    
    Note that trace log of vdagent_recv_caps also be added.
    
    Signed-off-by: dengpc12@chinatelecom.cn <dengpc12@chinatelecom.cn>
    Signed-off-by: liuym16@chinatelecom.cn <liuym16@chinatelecom.cn>
    Reviewed-by: Marc-André Lureau <marcandre.lureau@redhat.com>
    Message-Id: <20221230092758.281805-3-dengpc12@chinatelecom.cn>

--- ui/vdagent.c
+++ ui/vdagent.c
@@ -696,6 +696,16 @@ static void vdagent_chr_open(Chardev *chr,
     *be_opened = true;
 }
 
+static void vdagent_register_to_qemu_clipboard(VDAgentChardev *vd)
+{
+    if (have_clipboard(vd) && vd->cbpeer.notifier.notify == NULL) {
+        vd->cbpeer.name = "vdagent";
+        vd->cbpeer.notifier.notify = vdagent_clipboard_notify;
+        vd->cbpeer.request = vdagent_clipboard_request;
+        qemu_clipboard_peer_register(&vd->cbpeer);
+    }
+}
+
 static void vdagent_chr_recv_caps(VDAgentChardev *vd, VDAgentMessage *msg)
 {
     VDAgentAnnounceCapabilities *caps = (void *)msg->data;
@@ -720,14 +730,10 @@ static void vdagent_chr_recv_caps(VDAgentChardev *vd, VDAgentMessage *msg)
         qemu_input_handler_activate(vd->mouse_hs);
     }
 
-    memset(vd->last_serial, 0, sizeof(vd->last_serial));
+    trace_vdagent_recv_caps(vd->caps);
 
-    if (have_clipboard(vd) && vd->cbpeer.notifier.notify == NULL) {
-        vd->cbpeer.name = "vdagent";
-        vd->cbpeer.notifier.notify = vdagent_clipboard_notify;
-        vd->cbpeer.request = vdagent_clipboard_request;
-        qemu_clipboard_peer_register(&vd->cbpeer);
-    }
+    memset(vd->last_serial, 0, sizeof(vd->last_serial));
+    vdagent_register_to_qemu_clipboard(vd);
 }
 
 static void vdagent_chr_recv_msg(VDAgentChardev *vd, VDAgentMessage *msg)
