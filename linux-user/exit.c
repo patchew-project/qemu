191100aedc15f7edac566ea9c5b54774149a4060
Alex Bennée <alex.bennee@linaro.org>
2023-01-05 16:43:19 +0000

    gdbstub: move syscall handling to new file
    Our GDB syscall support is the last chunk of code that needs target
    specific support so move it to a new file. We take the opportunity to
    move the syscall state into its own singleton instance and add in a
    few helpers for the main gdbstub to interact with the module.
    
    I also moved the gdb_exit() declaration into syscalls.h as it feels
    pretty related and most of the callers of it treat it as such.
    
    Signed-off-by: Alex Bennée <alex.bennee@linaro.org>
    Message-Id: <20230105164320.2164095-21-alex.bennee@linaro.org>

--- linux-user/exit.c
+++ linux-user/exit.c
@@ -17,7 +17,7 @@
  *  along with this program; if not, see <http://www.gnu.org/licenses/>.
  */
 #include "qemu/osdep.h"
-#include "exec/gdbstub.h"
+#include "gdbstub/syscalls.h"
 #include "qemu.h"
 #include "user-internals.h"
 #ifdef CONFIG_GPROF
