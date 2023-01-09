53923e698a8e531b9982e6daec93f713ea88a8ce
Marc-André Lureau <marcandre.lureau@redhat.com>
2023-01-09 15:21:09 +0400

    build-sys: fix crlf-ending C code
    On msys2, the shader-to-C script produces bad C:
    ./ui/shader/texture-blit-vert.h:2:5: error: missing terminating " character [-Werror]
    
    Fix it by changing the line ending from crlf to lf, and convert the
    script to Python (qemu build seems perl-free after that).
    
    Signed-off-by: Marc-André Lureau <marcandre.lureau@redhat.com>
    Acked-by: Thomas Huth <thuth@redhat.com>
    Message-Id: <20230109112110.128967-2-marcandre.lureau@redhat.com>

---
+++ scripts/shaderinclude.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python3
+
+import sys
+import os
+
+
+def main(args):
+    file_path = args[1]
+    basename = os.path.basename(file_path)
+    varname = basename.replace('-', '_').replace('.', '_')
+
+    with os.fdopen(sys.stdout.fileno(), "wt", closefd=False, newline='\n') as stdout:
+        with open(file_path, "r", encoding='utf-8') as file:
+            print(f'static GLchar {varname}_src[] =', file=stdout)
+            for line in file:
+                line = line.rstrip()
+                print(f'    "{line}\\n"', file=stdout)
+            print('    "\\n";', file=stdout)
+
+
+if __name__ == '__main__':
+    sys.exit(main(sys.argv))
