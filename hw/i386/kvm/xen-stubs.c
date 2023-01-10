e7c3ec71ac02d70773940b21134a334b05a8f7f9
Joao Martins <joao.m.martins@oracle.com>
2023-01-10 12:20:29 +0000

    i386/xen: add monitor commands to test event injection
    Specifically add listing, injection of event channels.
    
    Signed-off-by: Joao Martins <joao.m.martins@oracle.com>
    Signed-off-by: David Woodhouse <dwmw@amazon.co.uk>
    Acked-by: Dr. David Alan Gilbert <dgilbert@redhat.com>
    Message-Id: <20230110122042.1562155-39-dwmw2@infradead.org>

---
+++ hw/i386/kvm/xen-stubs.c
@@ -0,0 +1,25 @@
+/*
+ * QEMU Xen emulation: QMP stubs
+ *
+ * Copyright © 2023 Amazon.com, Inc. or its affiliates. All Rights Reserved.
+ *
+ * Authors: David Woodhouse <dwmw2@infradead.org>
+ *
+ * This work is licensed under the terms of the GNU GPL, version 2 or later.
+ * See the COPYING file in the top-level directory.
+ */
+
+#include "qemu/osdep.h"
+#include "qapi/error.h"
+#include "qapi/qapi-commands-misc.h"
+
+EvtchnInfoList *qmp_xen_event_list(Error **errp)
+{
+    error_setg(errp, "Xen event channel emulation not enabled\n");
+    return NULL;
+}
+
+void qmp_xen_event_inject(uint32_t port, Error **errp)
+{
+    error_setg(errp, "Xen event channel emulation not enabled\n");
+}
