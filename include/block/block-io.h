372ec65b862e03b4980819e94c9dac11bf470555
Emanuele Giuseppe Esposito <eesposit@redhat.com>
2023-01-13 21:42:01 +0100

    block: Convert bdrv_io_unplug() to co_wrapper
    BlockDriver->bdrv_io_unplug is categorized as IO callback, and it
    currently doesn't run in a coroutine. We should let it take a graph
    rdlock since the callback traverses the block nodes graph, which however
    is only possible in a coroutine.
    
    The only caller of this function is blk_io_unplug(), therefore make
    blk_io_unplug() a co_wrapper, so that we're always running in a
    coroutine where the lock can be taken.
    
    Signed-off-by: Emanuele Giuseppe Esposito <eesposit@redhat.com>
    Signed-off-by: Kevin Wolf <kwolf@redhat.com>
    Reviewed-by: Emanuele Giuseppe Esposito <eesposit@redhat.com>
    Message-Id: <20230113204212.359076-4-kwolf@redhat.com>

--- include/block/block-io.h
+++ include/block/block-io.h
@@ -218,8 +218,7 @@ void bdrv_coroutine_enter(BlockDriverState *bs, Coroutine *co);
 AioContext *child_of_bds_get_parent_aio_context(BdrvChild *c);
 
 void coroutine_fn bdrv_co_io_plug(BlockDriverState *bs);
-
-void bdrv_io_unplug(BlockDriverState *bs);
+void coroutine_fn bdrv_co_io_unplug(BlockDriverState *bs);
 
 bool coroutine_fn bdrv_co_can_store_new_dirty_bitmap(BlockDriverState *bs,
                                                      const char *name,
