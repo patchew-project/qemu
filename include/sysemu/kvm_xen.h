eabe4d98ed65ce10e7c1862e10b177931391bf93
David Woodhouse <dwmw@amazon.co.uk>
2023-01-16 21:57:27 +0000

    hw/xen: Add xen_overlay device for emulating shared xenheap pages
    For the shared info page and for grant tables, Xen shares its own pages
    from the "Xen heap" to the guest. The guest requests that a given page
    from a certain address space (XENMAPSPACE_shared_info, etc.) be mapped
    to a given GPA using the XENMEM_add_to_physmap hypercall.
    
    To support that in qemu when *emulating* Xen, create a memory region
    (migratable) and allow it to be mapped as an overlay when requested.
    
    Xen theoretically allows the same page to be mapped multiple times
    into the guest, but that's hard to track and reinstate over migration,
    so we automatically *unmap* any previous mapping when creating a new
    one. This approach has been used in production with.... a non-trivial
    number of guests expecting true Xen, without any problems yet being
    noticed.
    
    This adds just the shared info page for now. The grant tables will be
    a larger region, and will need to be overlaid one page at a time. I
    think that means I need to create separate aliases for each page of
    the overall grant_frames region, so that they can be mapped individually.
    
    Signed-off-by: David Woodhouse <dwmw@amazon.co.uk>
    Reviewed-by: Paul Durrant <paul@xen.org>
    Message-Id: <20230116215805.1123514-14-dwmw2@infradead.org>

--- include/sysemu/kvm_xen.h
+++ include/sysemu/kvm_xen.h
@@ -12,6 +12,10 @@
 #ifndef QEMU_SYSEMU_KVM_XEN_H
 #define QEMU_SYSEMU_KVM_XEN_H
 
+/* The KVM API uses these to indicate "no GPA" or "no GFN" */
+#define INVALID_GPA UINT64_MAX
+#define INVALID_GFN UINT64_MAX
+
 uint32_t kvm_xen_get_caps(void);
 
 #define kvm_xen_has_cap(cap) (!!(kvm_xen_get_caps() &           \
