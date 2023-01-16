073de220c99f087688fff83f43482123f6769a4b
Cédric Le Goater <clg@redhat.com>
2023-01-16 18:46:06 +0100

    s390x/pv: Introduce a s390_pv_check() helper for runtime
    If a secure kernel is started in a non-protected VM, the OS will hang
    during boot without giving a proper error message to the user.
    
    Perform the checks on Confidential Guest support at runtime with an
    helper called from the service call switching the guest to protected
    mode.
    
    Signed-off-by: Cédric Le Goater <clg@redhat.com>
    Message-Id: <20230116174607.2459498-3-clg@kaod.org>

--- hw/s390x/pv.c
+++ hw/s390x/pv.c
@@ -306,6 +306,19 @@ int s390_pv_kvm_init(ConfidentialGuestSupport *cgs, Error **errp)
     return 0;
 }
 
+bool s390_pv_check(Error **errp)
+{
+    MachineState *ms = MACHINE(qdev_get_machine());
+
+    if (!ms->cgs) {
+        error_setg(errp, "Protected VM is started without Confidential"
+                   " Guest support");
+        return false;
+    }
+
+    return true;
+}
+
 OBJECT_DEFINE_TYPE_WITH_INTERFACES(S390PVGuest,
                                    s390_pv_guest,
                                    S390_PV_GUEST,
