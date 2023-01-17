3fc3280830e5097c9900192ee0d371060126ae38
Peter Delevoryas <peter@pjd.dev>
2023-01-17 15:24:25 -0800

    hw/nvram/eeprom_at24c: Add init_rom field and at24c_eeprom_init_rom helper
    Allows users to specify binary data to initialize an EEPROM, allowing users to
    emulate data programmed at manufacturing time.
    
    - Added init_rom and init_rom_size attributes to TYPE_AT24C_EE
    - Added at24c_eeprom_init_rom helper function to initialize attributes
    - If -drive property is provided, it overrides init_rom data
    
    Signed-off-by: Peter Delevoryas <peter@pjd.dev>
    Reviewed-by: Joel Stanley <joel@jms.id.au>
    Message-Id: <20230117232427.74496-4-peter@pjd.dev>

--- include/hw/nvram/eeprom_at24c.h
+++ include/hw/nvram/eeprom_at24c.h
@@ -19,5 +19,7 @@
  * @bus, and drop the reference to it (the device is realized).
  */
 I2CSlave *at24c_eeprom_init(I2CBus *bus, uint8_t address, uint32_t rom_size);
+I2CSlave *at24c_eeprom_init_rom(I2CBus *bus, uint8_t address, uint32_t rom_size,
+                                const uint8_t *init_rom, uint32_t init_rom_size);
 
 #endif
