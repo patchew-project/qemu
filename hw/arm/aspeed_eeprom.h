2ff51be78be4e5d58a17e89c56e74f9b429b12ff
Peter Delevoryas <peter@pjd.dev>
2023-01-17 18:42:13 -0800

    hw/arm/aspeed: Add aspeed_eeprom.c
    - Create aspeed_eeprom.c and aspeed_eeprom.h
    - Include aspeed_eeprom.c in CONFIG_ASPEED meson source files
    - Include aspeed_eeprom.h in aspeed.c
    - Add fby35_bmc_fruid data
    - Use new at24c_eeprom_init_rom helper to initialize BMC FRUID EEPROM with data
      from aspeed_eeprom.c
    
    wget https://github.com/facebook/openbmc/releases/download/openbmc-e2294ff5d31d/fby35.mtd
    qemu-system-aarch64 -machine fby35-bmc -nographic -mtdblock fby35.mtd
    ...
    user: root
    pass: 0penBmc
    ...
    root@bmc-oob:~# fruid-util bb
    
    FRU Information           : Baseboard
    Reviewed-by: Corey Minyard <cminyard@mvista.com>
    Reviewed-by: Cédric Le Goater <clg@kaod.org>
    Reviewed-by: Joel Stanley <joel@jms.id.au>
    ---------------           : ------------------
    Chassis Type              : Rack Mount Chassis
    Chassis Part Number       : N/A
    Chassis Serial Number     : N/A
    Board Mfg Date            : Fri Jan  7 10:30:00 2022
    Board Mfg                 : XXXXXX
    Board Product             : Management Board wBMC
    Board Serial              : XXXXXXXXXXXXX
    Board Part Number         : XXXXXXXXXXXXXX
    Board FRU ID              : 1.0
    Board Custom Data 1       : XXXXXXXXX
    Board Custom Data 2       : XXXXXXXXXXXXXXXXXX
    Product Manufacturer      : XXXXXX
    Product Name              : Yosemite V3.5 EVT2
    Product Part Number       : XXXXXXXXXXXXXX
    Product Version           : EVT2
    Product Serial            : XXXXXXXXXXXXX
    Product Asset Tag         : XXXXXXX
    Product FRU ID            : 1.0
    Product Custom Data 1     : XXXXXXXXX
    Product Custom Data 2     : N/A
    root@bmc-oob:~# fruid-util bmc
    
    FRU Information           : BMC
    ---------------           : ------------------
    Board Mfg Date            : Mon Jan 10 21:42:00 2022
    Board Mfg                 : XXXXXX
    Board Product             : BMC Storage Module
    Board Serial              : XXXXXXXXXXXXX
    Board Part Number         : XXXXXXXXXXXXXX
    Board FRU ID              : 1.0
    Board Custom Data 1       : XXXXXXXXX
    Board Custom Data 2       : XXXXXXXXXXXXXXXXXX
    Product Manufacturer      : XXXXXX
    Product Name              : Yosemite V3.5 EVT2
    Product Part Number       : XXXXXXXXXXXXXX
    Product Version           : EVT2
    Product Serial            : XXXXXXXXXXXXX
    Product Asset Tag         : XXXXXXX
    Product FRU ID            : 1.0
    Product Custom Data 1     : XXXXXXXXX
    Product Custom Data 2     : Config A
    root@bmc-oob:~# fruid-util nic
    
    FRU Information           : NIC
    ---------------           : ------------------
    Board Mfg Date            : Tue Nov  2 08:51:00 2021
    Board Mfg                 : XXXXXXXX
    Board Product             : Mellanox ConnectX-6 DX OCP3.0
    Board Serial              : XXXXXXXXXXXXXXXXXXXXXXXX
    Board Part Number         : XXXXXXXXXXXXXXXXXXXXX
    Board FRU ID              : FRU Ver 0.02
    Product Manufacturer      : XXXXXXXX
    Product Name              : Mellanox ConnectX-6 DX OCP3.0
    Product Part Number       : XXXXXXXXXXXXXXXXXXXXX
    Product Version           : A9
    Product Serial            : XXXXXXXXXXXXXXXXXXXXXXXX
    Product Custom Data 3     : ConnectX-6 DX
    
    Signed-off-by: Peter Delevoryas <peter@pjd.dev>
    Reviewed-by: Cédric Le Goater <clg@kaod.org>
    Reviewed-by: Joel Stanley <joel@jms.id.au>
    Message-Id: <20230118024214.14413-5-peter@pjd.dev>

---
+++ hw/arm/aspeed_eeprom.h
@@ -0,0 +1,16 @@
+/*
+ * Copyright (c) Meta Platforms, Inc. and affiliates.
+ *
+ * SPDX-License-Identifier: GPL-2.0-only
+ */
+
+#ifndef ASPEED_EEPROM_H
+#define ASPEED_EEPROM_H
+
+#include "qemu/osdep.h"
+
+extern const uint8_t fby35_nic_fruid[309];
+extern const uint8_t fby35_bb_fruid[224];
+extern const uint8_t fby35_bmc_fruid[200];
+
+#endif
