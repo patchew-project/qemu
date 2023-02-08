1e70174f7fea1afb81680c1fe5697b475d03798c
Philippe Mathieu-Daudé <philmd@linaro.org>
2023-02-08 01:07:42 +0100

    hw/isa: Assert isa_register_portio_list() gets non-NULL ISA device
    The previous commit removed the single call to
    isa_register_portio_list() with dev=NULL. To be
    sure we won't reintroduce such weird (ab)use,
    add an assertion.
    
    Signed-off-by: Philippe Mathieu-Daudé <philmd@linaro.org>
    Reviewed-by: Richard Henderson <richard.henderson@linaro.org>
    Message-Id: <20230208000743.79415-7-philmd@linaro.org>

--- hw/isa/isa-bus.c
+++ hw/isa/isa-bus.c
@@ -107,7 +107,7 @@ IsaDma *isa_get_dma(ISABus *bus, int nchan)
 
 static inline void isa_init_ioport(ISADevice *dev, uint16_t ioport)
 {
-    if (dev && (dev->ioport_id == 0 || ioport < dev->ioport_id)) {
+    if (dev->ioport_id == 0 || ioport < dev->ioport_id) {
         dev->ioport_id = ioport;
     }
 }
@@ -123,6 +123,8 @@ int isa_register_portio_list(ISADevice *dev,
                              const MemoryRegionPortio *pio_start,
                              void *opaque, const char *name)
 {
+    assert(dev);
+
     if (!isabus) {
         return -ENODEV;
     }
