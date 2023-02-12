774d134d5baf48f401c77c18037ad5c48f9b1983
Bernhard Beschow <shentey@gmail.com>
2023-02-12 13:37:45 +0100

    hw/i386/pc_piix: Allow for setting properties before realizing PIIX3 south bridge
    The next patches will need to take advantage of it.
    
    Signed-off-by: Bernhard Beschow <shentey@gmail.com>
    Reviewed-by: Peter Maydell <peter.maydell@linaro.org>
    Reviewed-by: Michael S. Tsirkin <mst@redhat.com>
    Message-Id: <20221022150508.26830-3-shentey@gmail.com>
    Message-Id: <20230212123805.30799-4-shentey@gmail.com>

--- hw/i386/pc_piix.c
+++ hw/i386/pc_piix.c
@@ -235,7 +235,8 @@ static void pc_init1(MachineState *machine,
                                        : pc_pci_slot_get_pirq);
         pcms->bus = pci_bus;
 
-        pci_dev = pci_create_simple_multifunction(pci_bus, -1, true, type);
+        pci_dev = pci_new_multifunction(-1, true, type);
+        pci_realize_and_unref(pci_dev, pci_bus, &error_fatal);
         piix3 = PIIX3_PCI_DEVICE(pci_dev);
         piix3->pic = x86ms->gsi;
         piix3_devfn = piix3->dev.devfn;
