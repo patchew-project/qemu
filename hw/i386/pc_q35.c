65d099322c4d7cb4b6cc40cee4509ee62be567e0
Bernhard Beschow <shentey@gmail.com>
2023-02-14 14:14:34 +0100

    hw/pci-host/q35: Initialize "bypass-iommu" property from board code
    The Q35 PCI host already has a "bypass-iommu" property. However, the
    host initializes this property itself by accessing global machine state,
    thereby assuming it to be a PC machine. Avoid this by having board code
    set this property.
    
    Signed-off-by: Bernhard Beschow <shentey@gmail.com>
    Message-Id: <20230214131441.101760-6-shentey@gmail.com>

--- hw/i386/pc_q35.c
+++ hw/i386/pc_q35.c
@@ -231,6 +231,8 @@ static void pc_q35_init(MachineState *machine)
                             x86ms->below_4g_mem_size, NULL);
     object_property_set_int(phb, PCI_HOST_ABOVE_4G_MEM_SIZE,
                             x86ms->above_4g_mem_size, NULL);
+    object_property_set_bool(phb, "bypass-iommu",
+                             pcms->default_bus_bypass_iommu, NULL);
     sysbus_realize_and_unref(SYS_BUS_DEVICE(phb), &error_fatal);
 
     /* pci */
