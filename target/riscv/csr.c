d4837dd279772c3e287ff8c6ddff184643cf5d4b
Daniel Henrique Barboza <dbarboza@ventanamicro.com>
2023-02-15 15:57:25 -0300

    target/riscv: remove RISCV_FEATURE_MMU
    RISCV_FEATURE_MMU is set whether cpu->cfg.mmu is set, so let's just use
    the flag directly instead.
    
    With this change the enum is also removed. It is worth noticing that
    this enum, and all the RISCV_FEATURES_* that were contained in it,
    predates the existence of the cpu->cfg object. Today, using cpu->cfg is
    an easier way to retrieve all the features and extensions enabled in the
    hart.
    
    Signed-off-by: Daniel Henrique Barboza <dbarboza@ventanamicro.com>
    Reviewed-by: Weiwei Li <liweiwei@iscas.ac.cn>
    Reviewed-by: Bin Meng <bmeng@tinylab.org>
    Reviewed-by: Andrew Jones <ajones@ventanamicro.com>
    Message-Id: <20230215185726.691759-10-dbarboza@ventanamicro.com>

--- target/riscv/csr.c
+++ target/riscv/csr.c
@@ -2616,7 +2616,7 @@ static RISCVException rmw_siph(CPURISCVState *env, int csrno,
 static RISCVException read_satp(CPURISCVState *env, int csrno,
                                 target_ulong *val)
 {
-    if (!riscv_feature(env, RISCV_FEATURE_MMU)) {
+    if (!riscv_cpu_cfg(env).mmu) {
         *val = 0;
         return RISCV_EXCP_NONE;
     }
@@ -2635,7 +2635,7 @@ static RISCVException write_satp(CPURISCVState *env, int csrno,
 {
     target_ulong vm, mask;
 
-    if (!riscv_feature(env, RISCV_FEATURE_MMU)) {
+    if (!riscv_cpu_cfg(env).mmu) {
         return RISCV_EXCP_NONE;
     }
 
