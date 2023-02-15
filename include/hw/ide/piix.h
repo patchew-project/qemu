2ad13c09dccda5af8b69a923105c33349a9f698c
Philippe Mathieu-Daudé <philmd@linaro.org>
2023-02-15 17:16:27 +0100

    hw/ide/piix: Expose output IRQ as properties for late object population
    Signed-off-by: Philippe Mathieu-Daudé <philmd@linaro.org>
    Message-Id: <20230215161641.32663-5-philmd@linaro.org>

--- include/hw/ide/piix.h
+++ include/hw/ide/piix.h
@@ -1,6 +1,10 @@
 #ifndef HW_IDE_PIIX_H
 #define HW_IDE_PIIX_H
 
+/*
+ * QEMU interface:
+ *  + named GPIO outputs "ide-irq": asserted by each IDE channel
+ */
 #define TYPE_PIIX3_IDE "piix3-ide"
 #define TYPE_PIIX4_IDE "piix4-ide"
 
