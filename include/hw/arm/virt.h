77d42c916d7335bd891d980cbacb5162f248624d
Richard Henderson <richard.henderson@linaro.org>
2023-02-27 13:01:22 -1000

    NOTFORMERGE hw/arm/virt: Add some memory for Realm Management Monitor
    This is arbitrary, but used by the Huawei TF-A test code.
    
    Signed-off-by: Richard Henderson <richard.henderson@linaro.org>
    Message-Id: <20230227230122.816702-22-richard.henderson@linaro.org>

--- include/hw/arm/virt.h
+++ include/hw/arm/virt.h
@@ -86,6 +86,7 @@ enum {
     VIRT_ACPI_GED,
     VIRT_NVDIMM_ACPI,
     VIRT_PVTIME,
+    VIRT_RMM_MEM,
     VIRT_LOWMEMMAP_LAST,
 };
 
@@ -159,6 +160,7 @@ struct VirtMachineState {
     bool virt;
     bool ras;
     bool mte;
+    bool rmm;
     bool dtb_randomness;
     OnOffAuto acpi;
     VirtGICType gic_version;
