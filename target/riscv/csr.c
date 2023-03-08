43d92912eaaf45fcd029796f8922d9b7d7221715
Daniel Henrique Barboza <dbarboza@ventanamicro.com>
2023-03-08 17:19:24 -0300

    target/riscv: do not allow RVG in write_misa()
    We're getting ready to use riscv_cpu_validate_set_extensions() to unify
    the handling of write_misa() with the rest of the code base. But first
    we need to deal with RVG.
    
    The 'G' virtual extension enables a set of extensions in the CPU. At
    this moment, this is done at the start of our validation step in
    riscv_cpu_validate_set_extensions(). This means that enabling G will
    enable other extensions in the CPU before resuming the validation.
    
    This also means that, in case a write_misa() validation fails, we're
    going to set cpu->cfg attributes that are unrelated to misa_ext bits
    (icsr and ifencei). These would be 2 extra states that we would need to
    store to fallback from a validation failure.
    
    Since write_misa() is still on experimental state let's make our lives
    easier for now and disable RVG updates.
    
    Signed-off-by: Daniel Henrique Barboza <dbarboza@ventanamicro.com>
    Message-Id: <20230308201925.258223-17-dbarboza@ventanamicro.com>

--- target/riscv/csr.c
+++ target/riscv/csr.c
@@ -1347,6 +1347,11 @@ static RISCVException write_misa(CPURISCVState *env, int csrno,
         return RISCV_EXCP_NONE;
     }
 
+    /* Changing 'G' state is unsupported */
+    if (val & RVG) {
+        return RISCV_EXCP_NONE;
+    }
+
     /* 'I' or 'E' must be present */
     if (!(val & (RVI | RVE))) {
         /* It is not, drop write to misa */
