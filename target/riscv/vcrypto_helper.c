d798ec7bf6fc1be3bc8b0fa044419183ead67028
William Salmon <will.salmon@codethink.co.uk>
2023-03-10 09:11:55 +0000

    target/riscv: Add vaesem.vs decoding, translation and execution support
    Signed-off-by: William Salmon <will.salmon@codethink.co.uk>
    Message-Id: <20230310091215.931644-26-lawrence.hunter@codethink.co.uk>

--- target/riscv/vcrypto_helper.c
+++ target/riscv/vcrypto_helper.c
@@ -336,6 +336,9 @@ GEN_ZVKNED_HELPER_VS(vaesdf_vs, aes_inv_shift_bytes(round_state);
 GEN_ZVKNED_HELPER_VV(vaesem_vv, aes_shift_bytes(round_state);
                     aes_sub_bytes(round_state); aes_mix_cols(round_state);
                     xor_round_key(round_state, (uint8_t *)round_key);)
+GEN_ZVKNED_HELPER_VS(vaesem_vs, aes_shift_bytes(round_state);
+                    aes_sub_bytes(round_state); aes_mix_cols(round_state);
+                    xor_round_key(round_state, (uint8_t *)round_key);)
 GEN_ZVKNED_HELPER_VV(vaesdm_vv, aes_inv_shift_bytes(round_state);
                     aes_inv_sub_bytes(round_state);
                     xor_round_key(round_state, (uint8_t *)round_key);
