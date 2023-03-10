ab3e222a0c08cc88a55af021fed357198892690a
Lawrence Hunter <lawrence.hunter@codethink.co.uk>
2023-03-10 16:03:23 +0000

    target/riscv: Add vaesdm.vs decoding, translation and execution support
    Signed-off-by: Lawrence Hunter <lawrence.hunter@codethink.co.uk>
    Message-Id: <20230310160346.1193597-23-lawrence.hunter@codethink.co.uk>

--- target/riscv/vcrypto_helper.c
+++ target/riscv/vcrypto_helper.c
@@ -323,3 +323,7 @@ GEN_ZVKNED_HELPER_VV(vaesdm_vv, aes_inv_shift_bytes(round_state);
                     aes_inv_sub_bytes(round_state);
                     xor_round_key(round_state, (uint8_t *)round_key);
                     aes_inv_mix_cols(round_state);)
+GEN_ZVKNED_HELPER_VS(vaesdm_vs, aes_inv_shift_bytes(round_state);
+                    aes_inv_sub_bytes(round_state);
+                    xor_round_key(round_state, (uint8_t *)round_key);
+                    aes_inv_mix_cols(round_state);)
