ff191e7571695699059a314abf685571a2c690d4
Jonathan Cameron via <qemu-devel@nongnu.org>
2023-03-17 16:54:40 +0000

    tests/acpi: Updated DSDT and SSDT due to move of PXB info to SSDT
    Signed-off-by: Jonathan Cameron <Jonathan.Cameron@huawei.com>
    Suggested-by: Michael S. Tsirkin <mst@redhat.com>
    Message-Id: <20230317165440.24846-5-Jonathan.Cameron@huawei.com>

--- tests/qtest/bios-tables-test-allowed-diff.h
+++ tests/qtest/bios-tables-test-allowed-diff.h
@@ -1,5 +1,1 @@
 /* List of comma-separated changed AML files to ignore */
-"tests/data/acpi/q35/DSDT.cxl",
-"tests/data/acpi/q35/DSDT.viot",
-"tests/data/acpi/q35/SSDT.cxl",
-"tests/data/acpi/q35/SSDT.viot",
