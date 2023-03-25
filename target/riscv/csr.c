2492e25a6f0fd1b4c2a94f0df1fd294bb5b4e85f
Richard Henderson <richard.henderson@linaro.org>
2023-03-25 03:54:20 -0700

    target/riscv: Set MMU_2STAGE_BIT in riscv_cpu_mmu_index
    Incorporate the virt_enabled and MPV checks into the cpu_mmu_index
    function, so we don't have to keep doing it within tlb_fill and
    subroutines.  This also elides a flush on changes to MPV.
    
    Signed-off-by: Richard Henderson <richard.henderson@linaro.org>
    Reviewed-by: Alistair Francis <alistair.francis@wdc.com>
    Reviewed-by: Weiwei Li <liweiwei@iscas.ac.cn>
    Tested-by: Daniel Henrique Barboza <dbarboza@ventanamicro.com>
    Message-Id: <20230325105429.1142530-17-richard.henderson@linaro.org>

--- target/riscv/csr.c
+++ target/riscv/csr.c
@@ -1246,7 +1246,7 @@ static RISCVException write_mstatus(CPURISCVState *env, int csrno,
     RISCVMXL xl = riscv_cpu_mxl(env);
 
     /* flush tlb on mstatus fields that affect VM */
-    if ((val ^ mstatus) & (MSTATUS_MXR | MSTATUS_MPV)) {
+    if ((val ^ mstatus) & MSTATUS_MXR) {
         tlb_flush(env_cpu(env));
     }
     mask = MSTATUS_SIE | MSTATUS_SPIE | MSTATUS_MIE | MSTATUS_MPIE |
@@ -1293,10 +1293,6 @@ static RISCVException write_mstatush(CPURISCVState *env, int csrno,
 {
     uint64_t valh = (uint64_t)val << 32;
     uint64_t mask = MSTATUS_MPV | MSTATUS_GVA;
-
-    if ((valh ^ env->mstatus) & (MSTATUS_MPV)) {
-        tlb_flush(env_cpu(env));
-    }
 
     env->mstatus = (env->mstatus & ~mask) | (valh & mask);
 
