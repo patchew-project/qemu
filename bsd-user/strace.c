0493c10f533687196786b1896d8378593e76d8e5
Warner Losh <imp@bsdimp.com>
2023-03-31 08:18:29 -0600

    bsd-user: Remove netbsd system call tracing
    Remove NetBSD system call tracing. We've not supported building all the
    BSDs into one module for some time, and the NetBSD support hasn't even
    built since the meson conversion.
    
    Signed-off-by: Warner Losh <imp@bsdimp.com>
    Reviewed-by: Richard Henderson <richard.henderson@linaro.org>
    Message-Id: <20230331141833.3647-4-imp@bsdimp.com>

--- bsd-user/strace.c
+++ bsd-user/strace.c
@@ -152,9 +152,6 @@ static void print_syscall_ret_addr(const struct syscallname *name, abi_long ret)
 static const struct syscallname freebsd_scnames[] = {
 #include "freebsd/strace.list"
 };
-static const struct syscallname netbsd_scnames[] = {
-#include "netbsd/strace.list"
-};
 static const struct syscallname openbsd_scnames[] = {
 #include "openbsd/strace.list"
 };
@@ -227,20 +224,6 @@ void print_freebsd_syscall_ret(int num, abi_long ret)
 {
 
     print_syscall_ret(num, ret, freebsd_scnames, ARRAY_SIZE(freebsd_scnames));
-}
-
-void print_netbsd_syscall(int num, abi_long arg1, abi_long arg2, abi_long arg3,
-        abi_long arg4, abi_long arg5, abi_long arg6)
-{
-
-    print_syscall(num, netbsd_scnames, ARRAY_SIZE(netbsd_scnames),
-                  arg1, arg2, arg3, arg4, arg5, arg6);
-}
-
-void print_netbsd_syscall_ret(int num, abi_long ret)
-{
-
-    print_syscall_ret(num, ret, netbsd_scnames, ARRAY_SIZE(netbsd_scnames));
 }
 
 void print_openbsd_syscall(int num, abi_long arg1, abi_long arg2, abi_long arg3,
