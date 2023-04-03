ce3fa4c809603b42160be1c05eb20f6cd1e54e9d
Stefan Hajnoczi <stefanha@redhat.com>
2023-04-03 14:30:04 -0400

    aio: remove aio_disable_external() API
    All callers now pass is_external=false to aio_set_fd_handler() and
    aio_set_event_notifier(). The aio_disable_external() API that
    temporarily disables fd handlers that were registered is_external=true
    is therefore dead code.
    
    Remove aio_disable_external(), aio_enable_external(), and the
    is_external arguments to aio_set_fd_handler() and
    aio_set_event_notifier().
    
    The entire test-fdmon-epoll test is removed because its sole purpose was
    testing aio_disable_external().
    
    Parts of this patch were generated using the following coccinelle
    (https://coccinelle.lip6.fr/) semantic patch:
    
      @@
      expression ctx, fd, is_external, io_read, io_write, io_poll, io_poll_ready, opaque;
      @@
      - aio_set_fd_handler(ctx, fd, is_external, io_read, io_write, io_poll, io_poll_ready, opaque)
      + aio_set_fd_handler(ctx, fd, io_read, io_write, io_poll, io_poll_ready, opaque)
    
      @@
      expression ctx, notifier, is_external, io_read, io_poll, io_poll_ready;
      @@
      - aio_set_event_notifier(ctx, notifier, is_external, io_read, io_poll, io_poll_ready)
      + aio_set_event_notifier(ctx, notifier, io_read, io_poll, io_poll_ready)
    
    Signed-off-by: Stefan Hajnoczi <stefanha@redhat.com>
    Reviewed-by: Juan Quintela <quintela@redhat.com>
    Message-Id: <20230403183004.347205-14-stefanha@redhat.com>

--- util/fdmon-epoll.c
+++ util/fdmon-epoll.c
@@ -64,11 +64,6 @@ static int fdmon_epoll_wait(AioContext *ctx, AioHandlerList *ready_list,
     int i, ret = 0;
     struct epoll_event events[128];
 
-    /* Fall back while external clients are disabled */
-    if (qatomic_read(&ctx->external_disable_cnt)) {
-        return fdmon_poll_ops.wait(ctx, ready_list, timeout);
-    }
-
     if (timeout > 0) {
         ret = qemu_poll_ns(&pfd, 1, timeout);
         if (ret > 0) {
@@ -128,11 +123,6 @@ static bool fdmon_epoll_try_enable(AioContext *ctx)
 bool fdmon_epoll_try_upgrade(AioContext *ctx, unsigned npfd)
 {
     if (ctx->epollfd < 0) {
-        return false;
-    }
-
-    /* Do not upgrade while external clients are disabled */
-    if (qatomic_read(&ctx->external_disable_cnt)) {
         return false;
     }
 
