76117b91c7e9ab388ce1fd86663da2b5ec31c079
Pierre Morel <pmorel@linux.ibm.com>
2023-04-03 18:28:45 +0200

    s390x/cpu topology: add s390 specifics to CPU topology
    S390 adds two new SMP levels, drawers and books to the CPU
    topology.
    The S390 CPU have specific topology features like dedication
    and entitlement to give to the guest indications on the host
    vCPUs scheduling and help the guest take the best decisions
    on the scheduling of threads on the vCPUs.
    
    Let us provide the SMP properties with books and drawers levels
    and S390 CPU with dedication and entitlement,
    
    Signed-off-by: Pierre Morel <pmorel@linux.ibm.com>
    Reviewed-by: Thomas Huth <thuth@redhat.com>
    Message-Id: <20230403162905.17703-2-pmorel@linux.ibm.com>

---
+++ include/hw/s390x/cpu-topology.h
@@ -0,0 +1,15 @@
+/*
+ * CPU Topology
+ *
+ * Copyright IBM Corp. 2022
+ *
+ * This work is licensed under the terms of the GNU GPL, version 2 or (at
+ * your option) any later version. See the COPYING file in the top-level
+ * directory.
+ */
+#ifndef HW_S390X_CPU_TOPOLOGY_H
+#define HW_S390X_CPU_TOPOLOGY_H
+
+#define S390_TOPOLOGY_CPU_IFL   0x03
+
+#endif
