e06eefd0482b945a34607af43767690b153330fc
Pierre Morel <pmorel@linux.ibm.com>
2023-04-03 18:28:46 +0200

    s390x/cpu topology: add topology entries on CPU hotplug
    The topology information are attributes of the CPU and are
    specified during the CPU device creation.
    
    On hot plug we:
    - calculate the default values for the topology for drawers,
      books and sockets in the case they are not specified.
    - verify the CPU attributes
    - check that we have still room on the desired socket
    
    The possibility to insert a CPU in a mask is dependent on the
    number of cores allowed in a socket, a book or a drawer, the
    checking is done during the hot plug of the CPU to have an
    immediate answer.
    
    If the complete topology is not specified, the core is added
    in the physical topology based on its core ID and it gets
    defaults values for the modifier attributes.
    
    This way, starting QEMU without specifying the topology can
    still get some advantage of the CPU topology.
    
    Signed-off-by: Pierre Morel <pmorel@linux.ibm.com>
    Message-Id: <20230403162905.17703-3-pmorel@linux.ibm.com>

--- include/hw/s390x/cpu-topology.h
+++ include/hw/s390x/cpu-topology.h
@@ -10,6 +10,50 @@
 #ifndef HW_S390X_CPU_TOPOLOGY_H
 #define HW_S390X_CPU_TOPOLOGY_H
 
+#ifndef CONFIG_USER_ONLY
+
+#include "qemu/queue.h"
+#include "hw/boards.h"
+#include "qapi/qapi-types-machine-target.h"
+
 #define S390_TOPOLOGY_CPU_IFL   0x03
+
+typedef struct S390Topology {
+    uint8_t *cores_per_socket;
+    CpuTopology *smp;
+} S390Topology;
+
+#ifdef CONFIG_KVM
+bool s390_has_topology(void);
+void s390_topology_setup_cpu(MachineState *ms, S390CPU *cpu, Error **errp);
+#else
+static inline bool s390_has_topology(void)
+{
+       return false;
+}
+static inline void s390_topology_setup_cpu(MachineState *ms,
+                                           S390CPU *cpu,
+                                           Error **errp) {}
+#endif
+
+extern S390Topology s390_topology;
+int s390_socket_nb(S390CPU *cpu);
+
+static inline int s390_std_socket(int n, CpuTopology *smp)
+{
+    return (n / smp->cores) % smp->sockets;
+}
+
+static inline int s390_std_book(int n, CpuTopology *smp)
+{
+    return (n / (smp->cores * smp->sockets)) % smp->books;
+}
+
+static inline int s390_std_drawer(int n, CpuTopology *smp)
+{
+    return (n / (smp->cores * smp->sockets * smp->books)) % smp->drawers;
+}
+
+#endif /* CONFIG_USER_ONLY */
 
 #endif
