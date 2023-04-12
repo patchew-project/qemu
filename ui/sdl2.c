69eecb038889edfb5523f0b52340688d85320b59
Bernhard Beschow <shentey@gmail.com>
2023-04-12 22:34:23 +0200

    ui/sdl2: Grab Alt+Tab also in fullscreen mode
    By default, SDL grabs Alt+Tab only in non-fullscreen mode. This causes Alt+Tab
    to switch tasks on the host rather than in the VM in fullscreen mode while it
    switches tasks in non-fullscreen mode in the VM. Fix this confusing behavior
    by grabbing Alt+F4 in fullscreen mode, always causing tasks to be switched in
    the VM.
    
    Signed-off-by: Bernhard Beschow <shentey@gmail.com>
    Message-Id: <20230412203425.32566-2-shentey@gmail.com>

--- ui/sdl2.c
+++ ui/sdl2.c
@@ -856,6 +856,7 @@ static void sdl2_display_init(DisplayState *ds, DisplayOptions *o)
     SDL_SetHint(SDL_HINT_VIDEO_X11_NET_WM_BYPASS_COMPOSITOR, "0");
 #endif
     SDL_SetHint(SDL_HINT_GRAB_KEYBOARD, "1");
+    SDL_SetHint(SDL_HINT_ALLOW_ALT_TAB_WHILE_GRABBED, "0");
     memset(&info, 0, sizeof(info));
     SDL_VERSION(&info.version);
 
