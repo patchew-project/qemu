28b9414ef71bf31978b706742ca986569e5de8a0
Alex Bennée <alex.bennee@linaro.org>
2023-04-18 17:21:30 +0100

    hw/virtio: fix typo in VIRTIO_CONFIG_IRQ_IDX comments
    Fixes: 544f0278af (virtio: introduce macro VIRTIO_CONFIG_IRQ_IDX)
    Signed-off-by: Alex Bennée <alex.bennee@linaro.org>
    Reviewed-by: Mark Cave-Ayland <mark.cave-ayland@ilande.co.uk>
    Message-Id: <20230418162140.373219-4-alex.bennee@linaro.org>

--- hw/virtio/virtio-crypto.c
+++ hw/virtio/virtio-crypto.c
@@ -1208,7 +1208,7 @@ static void virtio_crypto_guest_notifier_mask(VirtIODevice *vdev, int idx,
 
     /*
      * Add the check for configure interrupt, Use VIRTIO_CONFIG_IRQ_IDX -1
-     * as the Marco of configure interrupt's IDX, If this driver does not
+     * as the macro of configure interrupt's IDX, If this driver does not
      * support, the function will return
      */
 
@@ -1227,7 +1227,7 @@ static bool virtio_crypto_guest_notifier_pending(VirtIODevice *vdev, int idx)
 
     /*
      * Add the check for configure interrupt, Use VIRTIO_CONFIG_IRQ_IDX -1
-     * as the Marco of configure interrupt's IDX, If this driver does not
+     * as the macro of configure interrupt's IDX, If this driver does not
      * support, the function will return
      */
 
