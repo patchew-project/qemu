85580f8c66f7e3410dc32db66ce9b02b94117615
Stefan Hajnoczi <stefanha@redhat.com>
2023-04-20 07:37:22 -0400

    block: drain from main loop thread in bdrv_co_yield_to_drain()
    For simplicity, always run BlockDevOps .drained_begin/end/poll()
    callbacks in the main loop thread. This makes it easier to implement the
    callbacks and avoids extra locks.
    
    Move the function pointer declarations from the I/O Code section to the
    Global State section in block-backend-common.h.
    
    Signed-off-by: Stefan Hajnoczi <stefanha@redhat.com>
    Message-Id: <20230420113732.336620-11-stefanha@redhat.com>

--- block/io.c
+++ block/io.c
@@ -331,7 +331,8 @@ static void coroutine_fn bdrv_co_yield_to_drain(BlockDriverState *bs,
     if (ctx != co_ctx) {
         aio_context_release(ctx);
     }
-    replay_bh_schedule_oneshot_event(ctx, bdrv_co_drain_bh_cb, &data);
+    replay_bh_schedule_oneshot_event(qemu_get_aio_context(),
+                                     bdrv_co_drain_bh_cb, &data);
 
     qemu_coroutine_yield();
     /* If we are resumed from some other event (such as an aio completion or a
