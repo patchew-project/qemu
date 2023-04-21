2d3a686dffbef02847d07924ca8ec8cea15c2a72
Antonio Caggiano <antonio.caggiano@collabora.com>
2023-04-20 18:12:14 -0700

    virtio-gpu: CONTEXT_INIT feature
    The feature can be enabled when a backend wants it.
    
    Signed-off-by: Antonio Caggiano <antonio.caggiano@collabora.com>
    Reviewed-by: Marc-André Lureau <marcandre.lureau@redhat.com>
    Signed-off-by: Gurchetan Singh <gurchetansingh@chromium.org>
    Message-Id: <20230421011223.718-5-gurchetansingh@chromium.org>

--- include/hw/virtio/virtio-gpu.h
+++ include/hw/virtio/virtio-gpu.h
@@ -93,6 +93,7 @@ enum virtio_gpu_base_conf_flags {
     VIRTIO_GPU_FLAG_EDID_ENABLED,
     VIRTIO_GPU_FLAG_DMABUF_ENABLED,
     VIRTIO_GPU_FLAG_BLOB_ENABLED,
+    VIRTIO_GPU_FLAG_CONTEXT_INIT_ENABLED,
 };
 
 #define virtio_gpu_virgl_enabled(_cfg) \
@@ -107,6 +108,8 @@ enum virtio_gpu_base_conf_flags {
     (_cfg.flags & (1 << VIRTIO_GPU_FLAG_BLOB_ENABLED))
 #define virtio_gpu_hostmem_enabled(_cfg) \
     (_cfg.hostmem > 0)
+#define virtio_gpu_context_init_enabled(_cfg) \
+    (_cfg.flags & (1 << VIRTIO_GPU_FLAG_CONTEXT_INIT_ENABLED))
 
 struct virtio_gpu_base_conf {
     uint32_t max_outputs;
