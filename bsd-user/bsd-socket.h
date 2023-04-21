610e05af683a583ee22d14a6837c9017e0eec8ae
Stacey Son <sson@FreeBSD.org>
2023-04-21 07:22:52 +0200

    Signed-off-by: Karim Taha <kariem.taha2.7@gmail.com>
    added the connect(2) syscall to bsd-user/bsd-socket.h.
    Message-Id: <20230421052255.5603-9-krm.taha@outlook.com>

--- bsd-user/bsd-socket.h
+++ bsd-user/bsd-socket.h
@@ -58,4 +58,25 @@ static inline abi_long do_bsd_bind(int sockfd, abi_ulong target_addr,
     return get_errno(bind(sockfd, addr, addrlen));
 }
 
+/* connect(2) */
+static inline abi_long do_bsd_connect(int sockfd, abi_ulong target_addr,
+                                      socklen_t addrlen)
+{
+    abi_long ret;
+    void *addr;
+
+    if ((int)addrlen < 0) {
+        return -TARGET_EINVAL;
+    }
+    addr = alloca(addrlen + 1);
+
+    ret = target_to_host_sockaddr(addr, target_addr, addrlen);
+
+    if (is_error(ret)) {
+        return ret;
+    }
+
+    return get_errno(connect(sockfd, addr, addrlen));
+}
+
 #endif /* BSD_SOCKET_H */
