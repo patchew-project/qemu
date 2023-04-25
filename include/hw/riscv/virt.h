df5f3561757e7c4136340449eb18e97f35b6b28a
Yong Li <yong.li@intel.com>
2023-04-25 15:35:09 +0800

    hw/riscv/virt: Add a second UART for secure world
    The virt machine can have two UARTs and the second UART
    can be used by the secure payload, firmware or OS residing
    in secure world. Will include the UART device to FDT in a
    seperated patch.
    
    Signed-off-by: Yong Li <yong.li@intel.com>
    Reviewed-by: LIU Zhiwei <zhiwei_liu@linux.alibaba.com>
    Reviewed-by: Philippe Mathieu-Daudé <philmd@linaro.org>
    Message-Id: <20230425073509.3618388-1-yong.li@intel.com>

--- include/hw/riscv/virt.h
+++ include/hw/riscv/virt.h
@@ -74,6 +74,7 @@ enum {
     VIRT_APLIC_S,
     VIRT_UART0,
     VIRT_VIRTIO,
+    VIRT_UART1,
     VIRT_FW_CFG,
     VIRT_IMSIC_M,
     VIRT_IMSIC_S,
@@ -88,6 +89,7 @@ enum {
 enum {
     UART0_IRQ = 10,
     RTC_IRQ = 11,
+    UART1_IRQ = 12,
     VIRTIO_IRQ = 1, /* 1 to 8 */
     VIRTIO_COUNT = 8,
     PCIE_IRQ = 0x20, /* 32 to 35 */
