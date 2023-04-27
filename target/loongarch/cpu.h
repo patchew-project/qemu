a45c27d5e5c2395db7ce60e5fa8b85dddf0a1ed1
Tianrui Zhao <zhaotianrui@loongson.cn>
2023-04-27 15:26:39 +0800

    target/loongarch: Supplement vcpu env initial when vcpu reset
    Supplement vcpu env initial when vcpu reset, including
    init vcpu mp_state value to KVM_MP_STATE_RUNNABLE and
    init vcpu CSR_CPUID,CSR_TID to cpu->cpu_index.
    
    Signed-off-by: Tianrui Zhao <zhaotianrui@loongson.cn>
    Message-Id: <20230427072645.3368102-4-zhaotianrui@loongson.cn>

--- target/loongarch/cpu.h
+++ target/loongarch/cpu.h
@@ -288,6 +288,7 @@ typedef struct CPUArchState {
     uint64_t CSR_PWCH;
     uint64_t CSR_STLBPS;
     uint64_t CSR_RVACFG;
+    uint64_t CSR_CPUID;
     uint64_t CSR_PRCFG1;
     uint64_t CSR_PRCFG2;
     uint64_t CSR_PRCFG3;
@@ -328,6 +329,7 @@ typedef struct CPUArchState {
     MemoryRegion iocsr_mem;
     bool load_elf;
     uint64_t elf_address;
+    uint32_t mp_state;
 #endif
 } CPULoongArchState;
 
