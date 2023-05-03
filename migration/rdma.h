f9a4b04f7fa7404134ef18c92f6834f2413c1524
Juan Quintela <quintela@redhat.com>
2023-05-03 15:18:41 +0200

    migration/rdma: Unfold hook_ram_load()
    There is only one flag called with: RAM_CONTROL_BLOCK_REG.
    
    Signed-off-by: Juan Quintela <quintela@redhat.com>
    Message-Id: <20230503131847.11603-11-quintela@redhat.com>

--- migration/rdma.h
+++ migration/rdma.h
@@ -27,9 +27,11 @@ void rdma_start_incoming_migration(const char *host_port, Error **errp);
 int qemu_rdma_registration_handle(QEMUFile *f);
 int qemu_rdma_registration_start(QEMUFile *f, uint64_t flags);
 int qemu_rdma_registration_stop(QEMUFile *f, uint64_t flags);
+int rdma_block_notification_handle(QEMUFile *f, const char *name);
 #else
 int qemu_rdma_registration_handle(QEMUFile *f) { return 0; }
 int qemu_rdma_registration_start(QEMUFile *f, uint64_t flags) { return 0; }
 int qemu_rdma_registration_stop(QEMUFile *f, uint64_t flags) { return 0; }
+int rdma_block_notification_handle(QEMUFile *f, const char *name) { return 0; }
 #endif
 #endif
