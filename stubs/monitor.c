9baf9a6afc7b196d32537335458839f140e771da
Stefano Garzarella <sgarzare@redhat.com>
2023-05-04 11:28:42 +0200

    stubs/monitor: add monitor_fd_param()
    The blkio block driver will use monitor_fd_param() to support
    file descriptor passing. This is possible in builds (e.g. softmmu)
    where the monitor API is available.
    
    Add the monitor_fd_param() stub so tools and tests that link the
    block layer can build successfully.
    
    Signed-off-by: Stefano Garzarella <sgarzare@redhat.com>
    Message-Id: <20230504092843.62493-2-sgarzare@redhat.com>

--- stubs/monitor.c
+++ stubs/monitor.c
@@ -9,6 +9,12 @@ int monitor_get_fd(Monitor *mon, const char *name, Error **errp)
     return -1;
 }
 
+int monitor_fd_param(Monitor *mon, const char *fdname, Error **errp)
+{
+    error_setg(errp, "only QEMU supports file descriptor passing");
+    return -1;
+}
+
 void monitor_init_hmp(Chardev *chr, bool use_readline, Error **errp)
 {
 }
