ff093554ef6fd9f77f65d4c26fbeac5b04dec6ee
Cédric Le Goater <clg@kaod.org>
2023-05-08 09:58:51 +0200

    hw/ssi: Add an "addr" property to SSIPeripheral
    Boards will use this new property to identify the device CS line and
    wire the SPI controllers accordingly.
    
    Cc: Alistair Francis <alistair@alistair23.me>
    Signed-off-by: Cédric Le Goater <clg@kaod.org>
    Reviewed-by: Philippe Mathieu-Daudé <philmd@linaro.org>
    Message-Id: <20230508075859.3326566-5-clg@kaod.org>

--- include/hw/ssi/ssi.h
+++ include/hw/ssi/ssi.h
@@ -64,6 +64,9 @@ struct SSIPeripheral {
 
     /* Chip select state */
     bool cs;
+
+    /* Chip select address/index */
+    uint32_t addr;
 };
 
 extern const VMStateDescription vmstate_ssi_peripheral;
