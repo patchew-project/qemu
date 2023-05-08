922993ac75d5f8b60f313b760d6bad233d3e1bbb
Juan Quintela <quintela@redhat.com>
2023-05-08 15:09:03 +0200

    migration: Don't abuse qemu_file transferred for RDMA
    Just create a variable for it, the same way that multifd does.  This
    way it is safe to use for other thread, etc, etc.
    
    Signed-off-by: Juan Quintela <quintela@redhat.com>
    Message-Id: <20230508130909.65420-16-quintela@redhat.com>

--- migration/migration-stats.h
+++ migration/migration-stats.h
@@ -78,6 +78,10 @@ typedef struct {
      */
     Stat64 rate_limit_max;
     /*
+     * Number of bytes sent through RDMA.
+     */
+    Stat64 rdma_bytes;
+    /*
      * How long has the setup stage took.
      */
     Stat64 setup_time;
