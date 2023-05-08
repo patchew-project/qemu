f81a5bc83aad29bcd4e1ed40d41c1f846ae86b6f
Cédric Le Goater <clg@kaod.org>
2023-05-08 09:58:52 +0200

    hw/ssi: Introduce a ssi_get_cs() helper
    Simple routine to retrieve a DeviceState object on a SPI bus using its
    address/cs. It will be useful for the board to wire the CS lines.
    
    Cc: Alistair Francis <alistair@alistair23.me>
    Signed-off-by: Cédric Le Goater <clg@kaod.org>
    Reviewed-by: Philippe Mathieu-Daudé <philmd@linaro.org>
    Message-Id: <20230508075859.3326566-6-clg@kaod.org>

--- hw/ssi/ssi.c
+++ hw/ssi/ssi.c
@@ -27,6 +27,21 @@ struct SSIBus {
 #define TYPE_SSI_BUS "SSI"
 OBJECT_DECLARE_SIMPLE_TYPE(SSIBus, SSI_BUS)
 
+DeviceState *ssi_get_cs(SSIBus *bus, int addr)
+{
+    BusState *b = BUS(bus);
+    BusChild *kid;
+
+    QTAILQ_FOREACH(kid, &b->children, sibling) {
+        SSIPeripheral *kid_ssi = SSI_PERIPHERAL(kid->child);
+        if (kid_ssi->addr == addr) {
+            return kid->child;
+        }
+    }
+
+    return NULL;
+}
+
 static const TypeInfo ssi_bus_info = {
     .name = TYPE_SSI_BUS,
     .parent = TYPE_BUS,
