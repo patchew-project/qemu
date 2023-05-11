645d2312c21a456708a75bce1ca63bc475acffce
Mark Cave-Ayland <mark.cave-ayland@ilande.co.uk>
2023-05-11 09:57:21 +0100

    hw/net/can/can_pcm3680_pci.c: switch SerialState to use PCI device input gpio
    This replaces the call to pci_allocate_irq() and also allows the corresponding
    qemu_free_irq() to be removed.
    
    Signed-off-by: Mark Cave-Ayland <mark.cave-ayland@ilande.co.uk>
    Message-Id: <20230511085731.171565-9-mark.cave-ayland@ilande.co.uk>

--- hw/net/can/can_pcm3680_pci.c
+++ hw/net/can/can_pcm3680_pci.c
@@ -163,7 +163,7 @@ static void pcm3680i_pci_realize(PCIDevice *pci_dev, Error **errp)
     pci_conf = pci_dev->config;
     pci_conf[PCI_INTERRUPT_PIN] = 0x01; /* interrupt pin A */
 
-    d->irq = pci_allocate_irq(&d->dev);
+    d->irq = qdev_get_gpio_in_named(DEVICE(pci_dev), "pci-input-irq", 0);
 
     for (i = 0; i < PCM3680i_PCI_SJA_COUNT; i++) {
         can_sja_init(&d->sja_state[i], d->irq);
@@ -196,8 +196,6 @@ static void pcm3680i_pci_exit(PCIDevice *pci_dev)
     for (i = 0; i < PCM3680i_PCI_SJA_COUNT; i++) {
         can_sja_disconnect(&d->sja_state[i]);
     }
-
-    qemu_free_irq(d->irq);
 }
 
 static const VMStateDescription vmstate_pcm3680i_pci = {
