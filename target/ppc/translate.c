6b09b7d99eb181dcff202af67a0b149a1ad713dd
Nicholas Piggin <npiggin@gmail.com>
2023-05-15 19:26:47 +1000

    target/ppc: Fix width of some 32-bit SPRs
    Some 32-bit SPRs are incorrectly implemented as 64-bits on 64-bit
    targets.
    
    This changes VRSAVE, DSISR, HDSISR, DAWRX0, PIDR, LPIDR, DEXCR,
    HDEXCR, CTRL, TSCR, MMCRH, and PMC[1-6] from to be 32-bit registers.
    
    This only goes by the 32/64 classification in the architecture, it
    does not try to implement finer details of SPR implementation (e.g.,
    not all bits implemented as simple read/write storage).
    
    Signed-off-by: Nicholas Piggin <npiggin@gmail.com>
    Reviewed-by: Harsh Prateek Bora <harshpb@linux.ibm.com>
    Message-Id: <20230515092655.171206-2-npiggin@gmail.com>

--- target/ppc/translate.c
+++ target/ppc/translate.c
@@ -413,7 +413,7 @@ void spr_write_generic(DisasContext *ctx, int sprn, int gprn)
 
 void spr_write_CTRL(DisasContext *ctx, int sprn, int gprn)
 {
-    spr_write_generic(ctx, sprn, gprn);
+    spr_write_generic32(ctx, sprn, gprn);
 
     /*
      * SPR_CTRL writes must force a new translation block,
