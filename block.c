f88d4d2dcffb629567158fb780d5344937b75e72
Stefan Hajnoczi <stefanha@redhat.com>
2023-05-16 15:02:38 -0400

    aio: remove aio_disable_external() API
    All callers now pass is_external=false to aio_set_fd_handler() and
    aio_set_event_notifier(). The aio_disable_external() API that
    temporarily disables fd handlers that were registered is_external=true
    is therefore dead code.
    
    Remove aio_disable_external(), aio_enable_external(), and the
    is_external arguments to aio_set_fd_handler() and
    aio_set_event_notifier().
    
    The entire test-fdmon-epoll test is removed because its sole purpose was
    testing aio_disable_external().
    
    Parts of this patch were generated using the following coccinelle
    (https://coccinelle.lip6.fr/) semantic patch:
    
      @@
      expression ctx, fd, is_external, io_read, io_write, io_poll, io_poll_ready, opaque;
      @@
      - aio_set_fd_handler(ctx, fd, is_external, io_read, io_write, io_poll, io_poll_ready, opaque)
      + aio_set_fd_handler(ctx, fd, io_read, io_write, io_poll, io_poll_ready, opaque)
    
      @@
      expression ctx, notifier, is_external, io_read, io_poll, io_poll_ready;
      @@
      - aio_set_event_notifier(ctx, notifier, is_external, io_read, io_poll, io_poll_ready)
      + aio_set_event_notifier(ctx, notifier, io_read, io_poll, io_poll_ready)
    
    Reviewed-by: Juan Quintela <quintela@redhat.com>
    Reviewed-by: Philippe Mathieu-Daudé <philmd@linaro.org>
    Signed-off-by: Stefan Hajnoczi <stefanha@redhat.com>
    Message-Id: <20230516190238.8401-21-stefanha@redhat.com>

--- block.c
+++ block.c
@@ -7283,9 +7283,6 @@ static void bdrv_detach_aio_context(BlockDriverState *bs)
         bs->drv->bdrv_detach_aio_context(bs);
     }
 
-    if (bs->quiesce_counter) {
-        aio_enable_external(bs->aio_context);
-    }
     bs->aio_context = NULL;
 }
 
@@ -7294,10 +7291,6 @@ static void bdrv_attach_aio_context(BlockDriverState *bs,
 {
     BdrvAioNotifier *ban, *ban_tmp;
     GLOBAL_STATE_CODE();
-
-    if (bs->quiesce_counter) {
-        aio_disable_external(new_context);
-    }
 
     bs->aio_context = new_context;
 
