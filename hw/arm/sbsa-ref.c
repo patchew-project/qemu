6817111a426331995342d1c1ee66204d2f163f55
Marcin Juszkiewicz <marcin.juszkiewicz@linaro.org>
2023-05-24 13:14:13 +0200

    hw/arm/sbsa-ref: set default display to Bochs
    This way we can use pci_vga_init() and have Bochs by default while still
    have an option to run with other VGA cards.
    
    Signed-off-by: Marcin Juszkiewicz <marcin.juszkiewicz@linaro.org>
    Reviewed-by: Thomas Huth <thuth@redhat.com>
    Message-Id: <20230524111413.833912-4-marcin.juszkiewicz@linaro.org>

--- hw/arm/sbsa-ref.c
+++ hw/arm/sbsa-ref.c
@@ -647,9 +647,9 @@ static void create_pcie(SBSAMachineState *sms)
 
             pci_nic_init_nofail(nd, pci->bus, nd->model, NULL);
         }
-    }
 
-    pci_create_simple(pci->bus, -1, "bochs-display");
+        pci_vga_init(pci->bus);
+    }
 
     create_smmu(sms, pci->bus);
 }
@@ -863,6 +863,7 @@ static void sbsa_ref_class_init(ObjectClass *oc, void *data)
     mc->default_ram_size = 1 * GiB;
     mc->default_ram_id = "sbsa-ref.ram";
     mc->default_cpus = 4;
+    mc->default_display = "bochs-display";
     mc->possible_cpu_arch_ids = sbsa_ref_possible_cpu_arch_ids;
     mc->cpu_index_to_instance_props = sbsa_ref_cpu_index_to_props;
     mc->get_default_cpu_node_id = sbsa_ref_get_default_cpu_node_id;
