817b33a065184ed6728a12b6f827f732aee0d8a2
Marcin Juszkiewicz <marcin.juszkiewicz@linaro.org>
2023-05-24 13:14:12 +0200

    Add Bochs to list of vga_interfaces
    arm/sbsa-ref uses Bochs-display graphics card and without it being
    present in vga_interfaces "-vga none" argument handling cannot be added.
    
    Signed-off-by: Marcin Juszkiewicz <marcin.juszkiewicz@linaro.org>
    Reviewed-by: Thomas Huth <thuth@redhat.com>
    Message-Id: <20230524111413.833912-3-marcin.juszkiewicz@linaro.org>

--- hw/pci/pci.c
+++ hw/pci/pci.c
@@ -1937,6 +1937,8 @@ PCIDevice *pci_vga_init(PCIBus *bus)
         return pci_create_simple(bus, -1, "vmware-svga");
     case VGA_VIRTIO:
         return pci_create_simple(bus, -1, "virtio-vga");
+    case VGA_BOCHS:
+        return pci_create_simple(bus, -1, "bochs-display");
     case VGA_NONE:
     default: /* Other non-PCI types. Checking for unsupported types is already
                 done in vl.c. */
--- include/sysemu/sysemu.h
+++ include/sysemu/sysemu.h
@@ -29,7 +29,7 @@ extern int autostart;
 
 typedef enum {
     VGA_NONE, VGA_STD, VGA_CIRRUS, VGA_VMWARE, VGA_XENFB, VGA_QXL,
-    VGA_TCX, VGA_CG3, VGA_DEVICE, VGA_VIRTIO,
+    VGA_TCX, VGA_CG3, VGA_DEVICE, VGA_VIRTIO, VGA_BOCHS,
     VGA_TYPE_MAX,
 } VGAInterfaceType;
 
