a28fcaa393e979e29093694cdff5e19739d99f3d
Rajnesh Kanwal <rkanwal@rivosinc.com>
2023-05-26 17:23:08 +0100

    target/riscv: Add HS-mode virtual interrupt and IRQ filtering support.
    This change adds support for inserting virtual interrupts from HS-mode
    into VS-mode using hvien and hvip csrs. This also allows for IRQ filtering
    from HS-mode.
    
    Also, the spec doesn't mandate the interrupt to be actually supported
    in hardware. Which allows HS-mode to assert virtual interrupts to VS-mode
    that have no connection to any real interrupt events.
    
    This is defined as part of the AIA specification [0], "6.3.2 Virtual
    interrupts for VS level".
    
    [0]: https://github.com/riscv/riscv-aia/releases/download/1.0-RC4/riscv-interrupts-1.0-RC4.pdf
    
    Signed-off-by: Rajnesh Kanwal <rkanwal@rivosinc.com>
    Reviewed-by: Daniel Henrique Barboza <dbarboza@ventanamicro.com>
    Message-Id: <20230526162308.22892-7-rkanwal@rivosinc.com>

--- target/riscv/machine.c
+++ target/riscv/machine.c
@@ -92,6 +92,8 @@ static const VMStateDescription vmstate_hyper = {
         VMSTATE_UINTTL(env.hgatp, RISCVCPU),
         VMSTATE_UINTTL(env.hgeie, RISCVCPU),
         VMSTATE_UINTTL(env.hgeip, RISCVCPU),
+        VMSTATE_UINT64(env.hvien, RISCVCPU),
+        VMSTATE_UINT64(env.hvip, RISCVCPU),
         VMSTATE_UINT64(env.htimedelta, RISCVCPU),
         VMSTATE_UINT64(env.vstimecmp, RISCVCPU),
 
@@ -106,6 +108,7 @@ static const VMStateDescription vmstate_hyper = {
         VMSTATE_UINTTL(env.vstval, RISCVCPU),
         VMSTATE_UINTTL(env.vsatp, RISCVCPU),
         VMSTATE_UINTTL(env.vsiselect, RISCVCPU),
+        VMSTATE_UINT64(env.vsie, RISCVCPU),
 
         VMSTATE_UINTTL(env.mtval2, RISCVCPU),
         VMSTATE_UINTTL(env.mtinst, RISCVCPU),
