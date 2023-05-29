f2b06635e3206b5c74ff6f96f1e3005aecbd0e78
Zhuocheng Ding <zhuocheng.ding@intel.com>
2023-05-29 20:30:53 +0800

    i386: Support module_id in X86CPUTopoIDs
    Add module_id member in X86CPUTopoIDs.
    
    module_id can be parsed from APIC ID, so also update APIC ID parsing
    rule to support module level. With this support, the conversions with
    module level between X86CPUTopoIDs, X86CPUTopoInfo and APIC ID are
    completed.
    
    module_id can be also generated from cpu topology, and before i386
    supports "clusters" in smp, the default "clusters per die" is only 1,
    thus the module_id generated in this way is 0, so that it will not
    conflict with the module_id generated by APIC ID.
    
    Signed-off-by: Zhuocheng Ding <zhuocheng.ding@intel.com>
    Co-developed-by: Zhao Liu <zhao1.liu@intel.com>
    Signed-off-by: Zhao Liu <zhao1.liu@intel.com>
    Acked-by: Michael S. Tsirkin <mst@redhat.com>
    Tested-by: Yongwei Ma <yongwei.ma@intel.com>
    Message-Id: <20230529123101.411267-10-zhao1.liu@linux.intel.com>

--- hw/i386/x86.c
+++ hw/i386/x86.c
@@ -314,11 +314,11 @@ void x86_cpu_pre_plug(HotplugHandler *hotplug_dev,
 
     /*
      * If APIC ID is not set,
-     * set it based on socket/die/core/thread properties.
+     * set it based on socket/die/cluster/core/thread properties.
      */
     if (cpu->apic_id == UNASSIGNED_APIC_ID) {
-        int max_socket = (ms->smp.max_cpus - 1) /
-                                smp_threads / smp_cores / ms->smp.dies;
+        int max_socket = (ms->smp.max_cpus - 1) / smp_threads / smp_cores /
+                                ms->smp.clusters / ms->smp.dies;
 
         /*
          * die-id was optional in QEMU 4.0 and older, so keep it optional
@@ -365,6 +365,14 @@ void x86_cpu_pre_plug(HotplugHandler *hotplug_dev,
         topo_ids.die_id = cpu->die_id;
         topo_ids.core_id = cpu->core_id;
         topo_ids.smt_id = cpu->thread_id;
+
+        /*
+         * TODO: This is the temporary initialization for topo_ids.module_id to
+         * avoid "maybe-uninitialized" compilation errors. Will remove when
+         * X86CPU supports cluster_id.
+         */
+        topo_ids.module_id = 0;
+
         cpu->apic_id = x86_apicid_from_topo_ids(&topo_info, &topo_ids);
     }
 
@@ -373,11 +381,13 @@ void x86_cpu_pre_plug(HotplugHandler *hotplug_dev,
         MachineState *ms = MACHINE(x86ms);
 
         x86_topo_ids_from_apicid(cpu->apic_id, &topo_info, &topo_ids);
+
         error_setg(errp,
-            "Invalid CPU [socket: %u, die: %u, core: %u, thread: %u] with"
-            " APIC ID %" PRIu32 ", valid index range 0:%d",
-            topo_ids.pkg_id, topo_ids.die_id, topo_ids.core_id, topo_ids.smt_id,
-            cpu->apic_id, ms->possible_cpus->len - 1);
+            "Invalid CPU [socket: %u, die: %u, module: %u, core: %u, thread: %u]"
+            " with APIC ID %" PRIu32 ", valid index range 0:%d",
+            topo_ids.pkg_id, topo_ids.die_id, topo_ids.module_id,
+            topo_ids.core_id, topo_ids.smt_id, cpu->apic_id,
+            ms->possible_cpus->len - 1);
         return;
     }
 
@@ -497,6 +507,10 @@ const CPUArchIdList *x86_possible_cpu_arch_ids(MachineState *ms)
         if (ms->smp.dies > 1) {
             ms->possible_cpus->cpus[i].props.has_die_id = true;
             ms->possible_cpus->cpus[i].props.die_id = topo_ids.die_id;
+        }
+        if (ms->smp.clusters > 1) {
+            ms->possible_cpus->cpus[i].props.has_cluster_id = true;
+            ms->possible_cpus->cpus[i].props.cluster_id = topo_ids.module_id;
         }
         ms->possible_cpus->cpus[i].props.has_core_id = true;
         ms->possible_cpus->cpus[i].props.core_id = topo_ids.core_id;
