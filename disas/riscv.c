19bb6c969310f837f73a1a8f8321c35d79f5460c
Christoph Müllner <christoph.muellner@vrull.eu>
2023-05-30 15:18:42 +0200

    disas/riscv: Add support for XVentanaCondOps
    This patch adds XVentanaCondOps support to the RISC-V disassembler.
    
    Co-developed-by: LIU Zhiwei <zhiwei_liu@linux.alibaba.com>
    Signed-off-by: Christoph Müllner <christoph.muellner@vrull.eu>
    Acked-by: Alistair Francis <alistair.francis@wdc.com>
    Message-Id: <20230530131843.1186637-9-christoph.muellner@vrull.eu>

--- disas/riscv.c
+++ disas/riscv.c
@@ -22,6 +22,9 @@
 #include "disas/riscv.h"
 #include "target/riscv/cpu-config.h"
 
+/* Vendor extensions */
+#include "disas/riscv-xventana.h"
+
 typedef enum {
     /* 0 is reserved for rv_op_illegal. */
     rv_op_lui = 1,
@@ -4614,6 +4617,7 @@ disasm_inst(char *buf, size_t buflen, rv_isa isa, uint64_t pc, rv_inst inst,
         void (*decode_func)(rv_decode *, rv_isa);
     } decoders[] = {
         { always_true_p, rvi_opcode_data, decode_inst_opcode },
+        { has_XVentanaCondOps_p, ventana_opcode_data, decode_xventanacondops },
     };
 
     for (size_t i = 0; i < ARRAY_SIZE(decoders); i++) {
