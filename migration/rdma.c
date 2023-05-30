6cd2d3f84de86542b0bb4070058fe9281e7573fb
Juan Quintela <quintela@redhat.com>
2023-05-30 13:54:21 +0200

    migration/rdma: Unfold ram_control_before_iterate()
    Once there:
    - Remove unused data parameter
    - unfold it in its callers.
    - change all callers to call qemu_rdma_registration_start()
    
    Reviewed-by: Peter Xu <peterx@redhat.com>
    Signed-off-by: Juan Quintela <quintela@redhat.com>
    Message-Id: <20230509120700.78359-3-quintela@redhat.com>
    Message-Id: <20230530115429.1998-14-quintela@redhat.com>

--- migration/rdma.c
+++ migration/rdma.c
@@ -3863,13 +3863,12 @@ static int rdma_load_hook(QEMUFile *f, uint64_t flags, void *data)
     }
 }
 
-static int qemu_rdma_registration_start(QEMUFile *f,
-                                        uint64_t flags, void *data)
+int qemu_rdma_registration_start(QEMUFile *f, uint64_t flags)
 {
     QIOChannelRDMA *rioc = QIO_CHANNEL_RDMA(qemu_file_get_ioc(f));
     RDMAContext *rdma;
 
-    if (migration_in_postcopy()) {
+    if (!migrate_rdma () || migration_in_postcopy()) {
         return 0;
     }
 
@@ -4007,7 +4006,6 @@ static const QEMUFileHooks rdma_read_hooks = {
 };
 
 static const QEMUFileHooks rdma_write_hooks = {
-    .before_ram_iterate = qemu_rdma_registration_start,
     .after_ram_iterate  = qemu_rdma_registration_stop,
     .save_page          = qemu_rdma_save_page,
 };
