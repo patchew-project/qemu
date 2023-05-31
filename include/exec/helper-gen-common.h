d17aa9847d03caa9ae08805852be1c7b7d0d6083
Richard Henderson <richard.henderson@linaro.org>
2023-05-30 21:03:05 -0700

    tcg: Split helper-gen.h
    Create helper-gen-common.h without the target specific portion.
    Use that in tcg-op-common.h.  Reorg headers in target/arm to
    ensure that helper-gen.h is included before helper-info.c.inc.
    All other targets are already correct in this regard.
    
    Signed-off-by: Richard Henderson <richard.henderson@linaro.org>
    Message-Id: <20230531040330.8950-24-richard.henderson@linaro.org>

---
+++ include/exec/helper-gen-common.h
@@ -0,0 +1,17 @@
+/*
+ * Helper file for declaring TCG helper functions.
+ * This one expands generation functions for tcg opcodes.
+ */
+
+#ifndef HELPER_GEN_COMMON_H
+#define HELPER_GEN_COMMON_H
+
+#define HELPER_H "accel/tcg/tcg-runtime.h"
+#include "exec/helper-gen.h.inc"
+#undef  HELPER_H
+
+#define HELPER_H "accel/tcg/plugin-helpers.h"
+#include "exec/helper-gen.h.inc"
+#undef  HELPER_H
+
+#endif /* HELPER_GEN_COMMON_H */
