3948d7fc50ccdf3bdfff614c882be4800d147860
Mark Cave-Ayland <mark.cave-ayland@ilande.co.uk>
2023-05-31 13:53:55 +0100

    q800: move SWIM device to Q800MachineState
    Also change the instantiation of the SWIM device to use object_initialize_child().
    
    Signed-off-by: Mark Cave-Ayland <mark.cave-ayland@ilande.co.uk>
    Reviewed-by: Laurent Vivier <laurent@vivier.eu>
    Reviewed-by: Philippe Mathieu-Daudé <philmd@linaro.org>
    Message-Id: <20230531125400.288917-19-mark.cave-ayland@ilande.co.uk>

--- include/hw/m68k/q800.h
+++ include/hw/m68k/q800.h
@@ -29,6 +29,7 @@
 #include "hw/char/escc.h"
 #include "hw/or-irq.h"
 #include "hw/scsi/esp.h"
+#include "hw/block/swim.h"
 
 /*
  * The main Q800 machine
@@ -46,6 +47,7 @@ struct Q800MachineState {
     ESCCState escc;
     OrIRQState escc_orgate;
     SysBusESPState esp;
+    Swim swim;
     MemoryRegion macio;
     MemoryRegion macio_alias;
 };
