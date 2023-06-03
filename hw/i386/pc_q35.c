3c354698925d0298189192093d56a978a92e730e
Suravee Suthikulpanit <suravee.suthikulpanit@amd.com>
2023-06-02 22:22:55 -0500

    pc: q35: Bump max_cpus to 1024
    Since KVM_MAX_VCPUS is currently defined to 1024 for x86 as shown in
    arch/x86/include/asm/kvm_host.h, update QEMU limits to the same number.
    
    In case KVM could not support the specified number of vcpus, QEMU would
    return the following error message:
    
      qemu-system-x86_64: kvm_init_vcpu: kvm_get_vcpu failed (xxx): Invalid argument
    
    Cc: Igor Mammedov <imammedo@redhat.com>
    Cc: Daniel P. Berrangé <berrange@redhat.com>
    Cc: Michael S. Tsirkin <mst@redhat.com>
    Cc: Julia Suvorova <jusual@redhat.com>
    Signed-off-by: Suravee Suthikulpanit <suravee.suthikulpanit@amd.com>
    Message-Id: <20230603032255.517970-3-suravee.suthikulpanit@amd.com>

--- hw/i386/pc_q35.c
+++ hw/i386/pc_q35.c
@@ -382,7 +382,7 @@ static void pc_q35_machine_options(MachineClass *m)
     machine_class_allow_dynamic_sysbus_dev(m, TYPE_INTEL_IOMMU_DEVICE);
     machine_class_allow_dynamic_sysbus_dev(m, TYPE_RAMFB_DEVICE);
     machine_class_allow_dynamic_sysbus_dev(m, TYPE_VMBUS_BRIDGE);
-    m->max_cpus = 288;
+    m->max_cpus = 1024;
 }
 
 static void pc_q35_8_1_machine_options(MachineClass *m)
