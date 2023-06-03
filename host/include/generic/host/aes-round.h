63fd897f9d84a60e16cfbb10b39cf485be3431ce
Richard Henderson <richard.henderson@linaro.org>
2023-06-02 19:34:16 -0700

    crypto: Add aesdec_ISB_ISR_IMC_AK
    Add a primitive for InvSubBytes + InvShiftRows +
    InvMixColumns + AddRoundKey.
    
    Signed-off-by: Richard Henderson <richard.henderson@linaro.org>
    Message-Id: <20230603023426.1064431-26-richard.henderson@linaro.org>

--- host/include/generic/host/aes-round.h
+++ host/include/generic/host/aes-round.h
@@ -25,4 +25,8 @@ void aesdec_IMC_accel(AESState *, const AESState *, bool)
 void aesdec_ISB_ISR_accel(AESState *, const AESState *, bool)
     QEMU_ERROR("unsupported accel");
 
+void aesdec_ISB_ISR_IMC_AK_accel(AESState *, const AESState *,
+                                 const AESState *, bool)
+    QEMU_ERROR("unsupported accel");
+
 #endif
