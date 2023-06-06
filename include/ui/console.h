331887b4877f2a5221449c636697f763094cb5ba
Marc-André Lureau <marcandre.lureau@redhat.com>
2023-06-06 15:56:46 +0400

    console/win32: allocate shareable display surface
    Introduce qemu_win32_map_alloc() and qemu_win32_map_free() to allocate
    shared memory mapping. The handle can be used to share the mapping with
    another process.
    
    Teach qemu_create_displaysurface() to allocate shared memory. Following
    patches will introduce other places for shared memory allocation.
    
    Other patches for -display dbus will share the memory when possible with
    the client, to avoid expensive memory copy between the processes.
    
    Signed-off-by: Marc-André Lureau <marcandre.lureau@redhat.com>
    Message-Id: <20230606115658.677673-10-marcandre.lureau@redhat.com>

--- include/ui/console.h
+++ include/ui/console.h
@@ -128,6 +128,10 @@ typedef struct DisplaySurface {
     GLenum gltype;
     GLuint texture;
 #endif
+#ifdef WIN32
+    HANDLE handle;
+    uint32_t handle_offset;
+#endif
 } DisplaySurface;
 
 typedef struct QemuUIInfo {
@@ -314,6 +318,10 @@ DisplaySurface *qemu_create_displaysurface_from(int width, int height,
 DisplaySurface *qemu_create_displaysurface_pixman(pixman_image_t *image);
 DisplaySurface *qemu_create_placeholder_surface(int w, int h,
                                                 const char *msg);
+#ifdef WIN32
+void qemu_displaysurface_win32_set_handle(DisplaySurface *surface,
+                                          HANDLE h, uint32_t offset);
+#endif
 PixelFormat qemu_default_pixelformat(int bpp);
 
 DisplaySurface *qemu_create_displaysurface(int width, int height);
