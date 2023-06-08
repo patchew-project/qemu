83877eab66ad9629b8b34c7604598a3cdc814ed4
Tommy Wu <tommy.wu@sifive.com>
2023-06-08 00:23:14 -0700

    target/riscv: Add Smrnmi mnret instruction.
    Signed-off-by: Frank Chang <frank.chang@sifive.com>
    Signed-off-by: Tommy Wu <tommy.wu@sifive.com>
    Message-Id: <20230608072314.3561109-5-tommy.wu@sifive.com>

--- target/riscv/helper.h
+++ target/riscv/helper.h
@@ -112,6 +112,7 @@ DEF_HELPER_6(csrrw_i128, tl, env, int, tl, tl, tl, tl)
 #ifndef CONFIG_USER_ONLY
 DEF_HELPER_1(sret, tl, env)
 DEF_HELPER_1(mret, tl, env)
+DEF_HELPER_1(mnret, tl, env)
 DEF_HELPER_1(wfi, void, env)
 DEF_HELPER_1(tlb_flush, void, env)
 DEF_HELPER_1(tlb_flush_all, void, env)
