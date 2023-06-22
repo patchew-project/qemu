1321cacd079882cc01f7911cd0876b1d57d64950
Philippe Mathieu-Daudé <philmd@linaro.org>
2023-06-22 18:08:14 +0200

    accel: Rename HAX 'struct hax_vcpu_state' -> AccelCPUState
    We want all accelerators to share the same opaque pointer in
    CPUState. Start with the HAX context, renaming its forward
    declarated structure 'hax_vcpu_state' as 'AccelCPUState'.
    Directly use the typedef.
    
    Signed-off-by: Philippe Mathieu-Daudé <philmd@linaro.org>
    Message-Id: <20230622160823.71851-8-philmd@linaro.org>

--- include/hw/core/cpu.h
+++ include/hw/core/cpu.h
@@ -240,7 +240,6 @@ typedef struct SavedIOTLB {
 struct KVMState;
 struct kvm_run;
 
-struct hax_vcpu_state;
 struct hvf_vcpu_state;
 
 /* work queue */
@@ -308,6 +307,7 @@ struct qemu_work_item;
  * @next_cpu: Next CPU sharing TB cache.
  * @opaque: User data.
  * @mem_io_pc: Host Program Counter at which the memory was accessed.
+ * @accel: Pointer to accelerator specific state.
  * @kvm_fd: vCPU file descriptor for KVM.
  * @work_mutex: Lock to prevent multiple access to @work_list.
  * @work_list: List of pending asynchronous work.
@@ -422,6 +422,7 @@ struct CPUState {
     uint32_t can_do_io;
     int32_t exception_index;
 
+    struct AccelvCPUState *accel;
     /* shared by kvm, hax and hvf */
     bool vcpu_dirty;
 
@@ -440,8 +441,6 @@ struct CPUState {
 
     /* Used for user-only emulation of prctl(PR_SET_UNALIGN). */
     bool prctl_unalign_sigbus;
-
-    struct hax_vcpu_state *accel;
 
     struct hvf_vcpu_state *hvf;
 
