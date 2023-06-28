562c1c55650a52234bfab75e0d863862ac706e0a
Zhangjin Wu <falcon@tinylab.org>
2023-06-28 23:27:25 +0800

    util/async-teardown: Use qemu_close_range() to close fds
    Based on the old close_all_open_fd() of util/async-teardown.c, a new
    generic qemu_close_range() has been added in osdep.c.
    
    Now, let's switch over to use the generic qemu_close_range().
    
    Signed-off-by: Zhangjin Wu <falcon@tinylab.org>
    Signed-off-by: Bin Meng <bmeng@tinylab.org>
    
    Message-Id: <20230628152726.110295-6-bmeng@tinylab.org>

--- util/async-teardown.c
+++ util/async-teardown.c
@@ -29,44 +29,6 @@
 
 static pid_t the_ppid;
 
-/*
- * Close all open file descriptors.
- */
-static void close_all_open_fd(void)
-{
-    struct dirent *de;
-    int fd, dfd;
-    DIR *dir;
-
-#ifdef CONFIG_CLOSE_RANGE
-    int r = close_range(0, ~0U, 0);
-    if (!r) {
-        /* Success, no need to try other ways. */
-        return;
-    }
-#endif
-
-    dir = opendir("/proc/self/fd");
-    if (!dir) {
-        /* If /proc is not mounted, close fds one by one. */
-        int open_max = sysconf(_SC_OPEN_MAX), i;
-        for (i = 0; i < open_max; i++) {
-                close(i);
-        }
-        return;
-    }
-    /* Avoid closing the directory. */
-    dfd = dirfd(dir);
-
-    for (de = readdir(dir); de; de = readdir(dir)) {
-        fd = atoi(de->d_name);
-        if (fd != dfd) {
-            close(fd);
-        }
-    }
-    closedir(dir);
-}
-
 static void hup_handler(int signal)
 {
     /* Check every second if this process has been reparented. */
@@ -92,9 +54,8 @@ static int async_teardown_fn(void *arg)
     /*
      * Close all file descriptors that might have been inherited from the
      * main qemu process when doing clone, needed to make libvirt happy.
-     * Not using close_range for increased compatibility with older kernels.
      */
-    close_all_open_fd();
+    qemu_close_range(0, sysconf(_SC_OPEN_MAX) - 1);
 
     /* Set up a handler for SIGHUP and unblock SIGHUP. */
     sigaction(SIGHUP, &sa, NULL);
