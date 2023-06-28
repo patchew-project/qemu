1f634036bcefe7197b08e86b6b54ff2a1e4ad653
Philippe Mathieu-Daudé <philmd@linaro.org>
2023-06-28 08:32:34 +0200

    target/riscv: Restrict riscv_cpu_do_interrupt() to sysemu
    riscv_cpu_do_interrupt() is not reachable on user emulation.
    
    Signed-off-by: Philippe Mathieu-Daudé <philmd@linaro.org>
    Acked-by: Alistair Francis <alistair.francis@wdc.com>
    Reviewed-by: Daniel Henrique Barboza <dbarboza@ventanamicro.com>
    Message-Id: <20230628063234.32544-5-philmd@linaro.org>

--- target/riscv/cpu_helper.c
+++ target/riscv/cpu_helper.c
@@ -1575,7 +1575,6 @@ static target_ulong riscv_transformed_insn(CPURISCVState *env,
 
     return xinsn;
 }
-#endif /* !CONFIG_USER_ONLY */
 
 /*
  * Handle Traps
@@ -1585,8 +1584,6 @@ static target_ulong riscv_transformed_insn(CPURISCVState *env,
  */
 void riscv_cpu_do_interrupt(CPUState *cs)
 {
-#if !defined(CONFIG_USER_ONLY)
-
     RISCVCPU *cpu = RISCV_CPU(cs);
     CPURISCVState *env = &cpu->env;
     bool write_gva = false;
@@ -1779,6 +1776,6 @@ void riscv_cpu_do_interrupt(CPUState *cs)
 
     env->two_stage_lookup = false;
     env->two_stage_indirect_lookup = false;
-#endif
-    cs->exception_index = RISCV_EXCP_NONE; /* mark handled to qemu */
 }
+
+#endif /* !CONFIG_USER_ONLY */
