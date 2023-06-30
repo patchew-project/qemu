d272c5cf36920ab762d4b4032f60c60e77d4037d
Song Gao <gaosong@loongson.cn>
2023-06-30 15:58:31 +0800

    target/loongarch: Implement xvadda
    This patch includes:
    - XVADDA.{B/H/W/D}.
    
    Signed-off-by: Song Gao <gaosong@loongson.cn>
    Message-Id: <20230630075904.45940-14-gaosong@loongson.cn>

--- target/loongarch/vec_helper.c
+++ target/loongarch/vec_helper.c
@@ -381,16 +381,16 @@ DO_3OP(vabsd_hu, 16, UH, DO_VABSD)
 DO_3OP(vabsd_wu, 32, UW, DO_VABSD)
 DO_3OP(vabsd_du, 64, UD, DO_VABSD)
 
-#define DO_VABS(a)  ((a < 0) ? (-a) : (a))
-
 #define DO_VADDA(NAME, BIT, E, DO_OP)                       \
 void HELPER(NAME)(void *vd, void *vj, void *vk, uint32_t v) \
 {                                                           \
-    int i;                                                  \
+    int i, len;                                             \
     VReg *Vd = (VReg *)vd;                                  \
     VReg *Vj = (VReg *)vj;                                  \
     VReg *Vk = (VReg *)vk;                                  \
-    for (i = 0; i < LSX_LEN/BIT; i++) {                     \
+                                                            \
+    len = (simd_oprsz(v) == 16) ? LSX_LEN : LASX_LEN;       \
+    for (i = 0; i < len / BIT; i++) {                       \
         Vd->E(i) = DO_OP(Vj->E(i)) + DO_OP(Vk->E(i));       \
     }                                                       \
 }
