e813b7351d97e4088d96a2e93c70c1210c42f802
Ilya Leoshkevich <iii@linux.ibm.com>
2023-06-30 22:36:40 +0200

    accel/tcg: Remove #ifdef TARGET_I386 from perf.c
    Preparation for moving perf.c to tcg/.
    
    This affects only profiling guest code, which has code in a non-0 based
    segment, e.g., 16-bit code, which is not particularly important.
    
    Suggested-by: Richard Henderson <richard.henderson@linaro.org>
    Signed-off-by: Ilya Leoshkevich <iii@linux.ibm.com>
    Message-Id: <20230630203720.528552-4-iii@linux.ibm.com>

--- accel/tcg/perf.c
+++ accel/tcg/perf.c
@@ -336,10 +336,6 @@ void perf_report_code(uint64_t guest_pc, TranslationBlock *tb,
         q[insn].address = gen_insn_data[insn * start_words + 0];
         if (tb_cflags(tb) & CF_PCREL) {
             q[insn].address |= (guest_pc & TARGET_PAGE_MASK);
-        } else {
-#if defined(TARGET_I386)
-            q[insn].address -= tb->cs_base;
-#endif
         }
         q[insn].flags = DEBUGINFO_SYMBOL | (jitdump ? DEBUGINFO_LINE : 0);
     }
