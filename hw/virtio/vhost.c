efbe4cc88cfbcf1c91db558ac0ec33d180e1afac
Zhu Lingshan <lingshan.zhu@intel.com>
2023-07-08 02:18:26 +0800

    vhost_vdpa: no need to fetch vring base when poweroff
    In the poweroff routine, no need to fetch last available index.
    
    This commit also provides a better debug message in the vhost
    caller vhost_virtqueue_stop, because if vhost does not fetch
    the last avail idx successfully, maybe the device does not
    suspend, vhost will sync last avail idx to vring used idx as a
    work around, not a failure.
    
    Signed-off-by: Zhu Lingshan <lingshan.zhu@intel.com>
    Message-Id: <20230707181826.10946-1-lingshan.zhu@intel.com>

--- hw/virtio/vhost.c
+++ hw/virtio/vhost.c
@@ -1262,7 +1262,7 @@ void vhost_virtqueue_stop(struct vhost_dev *dev,
 
     r = dev->vhost_ops->vhost_get_vring_base(dev, &state);
     if (r < 0) {
-        VHOST_OPS_DEBUG(r, "vhost VQ %u ring restore failed: %d", idx, r);
+        VHOST_OPS_DEBUG(r, "sync last avail idx to the used idx for vhost VQ %u", idx);
         /* Connection to the backend is broken, so let's sync internal
          * last avail idx to the device used idx.
          */
