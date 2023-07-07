f9beb8a5ad57717700bd45fd6a40f886726afb59
Ravi Bangoria <ravi.bangoria@linux.ibm.com>
2023-07-07 08:31:08 -0300

    ppc: Enable 2nd DAWR support on p10
    As per the PAPR, bit 0 of byte 64 in pa-features property
    indicates availability of 2nd DAWR registers. i.e. If this bit is set, 2nd
    DAWR is present, otherwise not. Use KVM_CAP_PPC_DAWR1 capability to find
    whether kvm supports 2nd DAWR or not. If it's supported, allow user to set
    the pa-feature bit in guest DT using cap-dawr1 machine capability. Though,
    watchpoint on powerpc TCG guest is not supported and thus 2nd DAWR is not
    enabled for TCG mode.
    
    Signed-off-by: Ravi Bangoria <ravi.bangoria@linux.ibm.com>
    Reviewed-by: Greg Kurz <groug@kaod.org>
    Reviewed-by: Cédric Le Goater <clg@kaod.org>
    Signed-off-by: Shivaprasad G Bhat <sbhat@linux.ibm.com>
    Message-ID: <168871963321.58984.15628382614621248470.stgit@ltcd89-lp2>
    Signed-off-by: Daniel Henrique Barboza <danielhb413@gmail.com>
    Message-Id: <20230707113108.7145-61-danielhb413@gmail.com>

--- hw/ppc/spapr.c
+++ hw/ppc/spapr.c
@@ -241,7 +241,7 @@ static void spapr_dt_pa_features(SpaprMachineState *spapr,
         0x80, 0x00, 0x80, 0x00, 0x80, 0x00, /* 48 - 53 */
         /* 54: DecFP, 56: DecI, 58: SHA */
         0x80, 0x00, 0x80, 0x00, 0x80, 0x00, /* 54 - 59 */
-        /* 60: NM atomic, 62: RNG */
+        /* 60: NM atomic, 62: RNG, 64: DAWR1 (ISA 3.1) */
         0x80, 0x00, 0x80, 0x00, 0x00, 0x00, /* 60 - 65 */
     };
     uint8_t *pa_features = NULL;
@@ -281,6 +281,9 @@ static void spapr_dt_pa_features(SpaprMachineState *spapr,
          * mode when they can't handle it, if they see the radix bit set
          * in pa-features. So hide it from them. */
         pa_features[40 + 2] &= ~0x80; /* Radix MMU */
+    }
+    if (spapr_get_cap(spapr, SPAPR_CAP_DAWR1)) {
+        pa_features[66] |= 0x80;
     }
 
     _FDT((fdt_setprop(fdt, offset, "ibm,pa-features", pa_features, pa_size)));
@@ -2084,6 +2087,7 @@ static const VMStateDescription vmstate_spapr = {
         &vmstate_spapr_cap_fwnmi,
         &vmstate_spapr_fwnmi,
         &vmstate_spapr_cap_rpt_invalidate,
+        &vmstate_spapr_cap_dawr1,
         NULL
     }
 };
@@ -4683,6 +4687,7 @@ static void spapr_machine_class_init(ObjectClass *oc, void *data)
     smc->default_caps.caps[SPAPR_CAP_CCF_ASSIST] = SPAPR_CAP_ON;
     smc->default_caps.caps[SPAPR_CAP_FWNMI] = SPAPR_CAP_ON;
     smc->default_caps.caps[SPAPR_CAP_RPT_INVALIDATE] = SPAPR_CAP_OFF;
+    smc->default_caps.caps[SPAPR_CAP_DAWR1] = SPAPR_CAP_OFF;
 
     /*
      * This cap specifies whether the AIL 3 mode for
