373c266a4c5ae5f4a58205142b0a23766ecb864d
Konstantin Kostiuk <kkostiuk@redhat.com>
2023-07-07 11:31:02 +0300

    QGA VSS: Add wrapper to send log to debugger and stderr
    Signed-off-by: Konstantin Kostiuk <kkostiuk@redhat.com>
    Message-Id: <20230707083105.746811-2-kkostiuk@redhat.com>

---
+++ qga/vss-win32/vss-debug.h
@@ -0,0 +1,24 @@
+/*
+ * QEMU Guest Agent VSS debug declarations
+ *
+ * Copyright (C) 2023 Red Hat Inc
+ *
+ * Authors:
+ *  Konstantin Kostiuk <kkostiuk@redhat.com>
+ *
+ * This work is licensed under the terms of the GNU GPL, version 2 or later.
+ * See the COPYING file in the top-level directory.
+ */
+
+#include <vss-handles.h>
+
+#ifndef VSS_DEBUG_H
+#define VSS_DEBUG_H
+
+void qga_debug_internal(const char *funcname, const char *fmt, ...);
+
+#define qga_debug(fmt, ...) qga_debug_internal(__func__, fmt, ## __VA_ARGS__)
+#define qga_debug_begin qga_debug("begin")
+#define qga_debug_end qga_debug("end")
+
+#endif
