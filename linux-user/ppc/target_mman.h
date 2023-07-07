0ae755696a109bc1987f06feee6020336a2a70e8
Richard Henderson <richard.henderson@linaro.org>
2023-07-07 21:40:34 +0100

    linux-user: Split TARGET_MAP_* out of syscall_defs.h
    Move the values into the per-target target_mman.h headers
    
    Reviewed-by: Alex Bennée <alex.bennee@linaro.org>
    Signed-off-by: Richard Henderson <richard.henderson@linaro.org>
    Message-Id: <20230707204054.8792-7-richard.henderson@linaro.org>

--- linux-user/ppc/target_mman.h
+++ linux-user/ppc/target_mman.h
@@ -1,1 +1,4 @@
+#define TARGET_MAP_NORESERVE            0x40
+#define TARGET_MAP_LOCKED               0x80
+
 #include "../generic/target_mman.h"
--- linux-user/sparc/target_mman.h
+++ linux-user/sparc/target_mman.h
@@ -1,1 +1,5 @@
+#define TARGET_MAP_NORESERVE           0x40
+#define TARGET_MAP_LOCKED              0x100
+#define TARGET_MAP_GROWSDOWN           0x0200
+
 #include "../generic/target_mman.h"
