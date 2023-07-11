6b42d4ccca5d126441c81de21c4e82dc38b3b48e
Philippe Mathieu-Daudé <philmd@linaro.org>
2023-07-11 14:14:44 +0200

    target/riscv: Move TCG-specific files to target/riscv/tcg/
    Move TCG-specific files to the a new 'tcg' sub-directory. Add
    stubs for riscv_cpu_[get/set]_fflags and riscv_raise_exception().
    Adapt meson rules.
    
    Signed-off-by: Philippe Mathieu-Daudé <philmd@linaro.org>
    Acked-by: Alistair Francis <alistair.francis@wdc.com>
    Message-Id: <20230711121453.59138-8-philmd@linaro.org>

---
+++ target/riscv/tcg/tcg-stub.c
@@ -0,0 +1,25 @@
+/*
+ * QEMU RISC-V TCG stubs
+ *
+ * Copyright (c) 2023 Linaro
+ *
+ * SPDX-License-Identifier: GPL-2.0-or-later
+ */
+#include "qemu/osdep.h"
+#include "cpu.h"
+
+target_ulong riscv_cpu_get_fflags(CPURISCVState *env)
+{
+    g_assert_not_reached();
+}
+
+void riscv_cpu_set_fflags(CPURISCVState *env, target_ulong)
+{
+    g_assert_not_reached();
+}
+
+G_NORETURN void riscv_raise_exception(CPURISCVState *env,
+                                      uint32_t exception, uintptr_t pc)
+{
+    g_assert_not_reached();
+}
