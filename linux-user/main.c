117a30c00d4e7646bd46c9491f95af93f9936b23
Helge Deller <deller@gmx.de>
2023-07-27 09:05:59 +0200

    linux-user: Show heap address in /proc/pid/maps
    Show the memory location of the heap in the /proc/pid/maps file inside the
    guest. The heap address will be stored in ts->heap_base, so make that
    variable visible for all guest architectures, not just architectures for
    semihosted binaries (arm, m68k, riscv).
    
    Show 32- and 64-bit pointers with 8 digits and leading zeros (%08x/%08lx).
    For 64-bit we could use %16lx, but we mimic the Linux kernel, which shows
    it with %08lx too.
    
    Example:
    
    user@machine:/# uname -a
    Linux paq 5.15.88+ #47 SMP Sun Jan 15 12:53:11 CET 2023 aarch64 GNU/Linux
    
    user@machine:/# cat /proc/self/maps
    00000000-00009000 r-xp 00000000 08:01 2380521                            /usr/bin/cat
    00009000-0001f000 ---p 00000000 00:00 0
    0001f000-00020000 r--p 0000f000 08:01 2380521                            /usr/bin/cat
    00020000-00021000 rw-p 00010000 08:01 2380521                            /usr/bin/cat
    00021000-00042000 rw-p 00000000 00:00 0                                  [heap]
    5500000000-5500001000 ---p 00000000 00:00 0
    5500001000-5500801000 rw-p 00000000 00:00 0                              [stack]
    5500801000-5500827000 r-xp 00000000 08:01 2395258                        /usr/lib/aarch64-linux-gnu/ld-linux-aarch64.so.1
    5500827000-550083f000 ---p 00000000 00:00 0
    550083f000-5500841000 r--p 0002e000 08:01 2395258                        /usr/lib/aarch64-linux-gnu/ld-linux-aarch64.so.1
    5500841000-5500843000 rw-p 00030000 08:01 2395258                        /usr/lib/aarch64-linux-gnu/ld-linux-aarch64.so.1
    5500843000-5500844000 r-xp 00000000 00:00 0
    5500844000-5500846000 rw-p 00000000 00:00 0
    5500850000-55009d7000 r-xp 00000000 08:01 2395261                        /usr/lib/aarch64-linux-gnu/libc.so.6
    55009d7000-55009ed000 ---p 00187000 08:01 2395261                        /usr/lib/aarch64-linux-gnu/libc.so.6
    55009ed000-55009f0000 r--p 0018d000 08:01 2395261                        /usr/lib/aarch64-linux-gnu/libc.so.6
    55009f0000-55009f2000 rw-p 00190000 08:01 2395261                        /usr/lib/aarch64-linux-gnu/libc.so.6
    55009f2000-55009ff000 rw-p 00000000 00:00 0
    
    Signed-off-by: Helge Deller <deller@gmx.de>
    Message-Id: <20230727070601.31383-2-deller@gmx.de>

--- linux-user/main.c
+++ linux-user/main.c
@@ -955,6 +955,7 @@ int main(int argc, char **argv, char **envp)
        the real value of GUEST_BASE into account.  */
     tcg_prologue_init(tcg_ctx);
 
+    ts->heap_base = info->brk;
     target_cpu_copy_regs(env, regs);
 
     if (gdbstub) {
