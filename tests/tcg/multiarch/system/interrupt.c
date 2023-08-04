7ed42cfe53f1cb303297ce9eb865bfc4f4e33e07
Matheus Branco Borella <dark.ryu.550@gmail.com>
2023-08-04 15:26:28 -0300

    gdbstub: fixes cases where wrong threads were reported to GDB on SIGINT
    Alex Bennée <alex.bennee@linaro.org> writes:
    > Can gdb switch which packet sequence it uses to halt and restart
    > threads?
    
    Yes, but the way it does it does not trigger the behavior I was concerned
    about. GDB falls back to the old sequence when either (1) the target does not
    support the vCont command it's trying to send or (2) you step backwards. In both
    cases, though, whenever it does fall back, it will first send an Hc packet
    before continuing or stepping, which means we won't ever see a sequence such as
    ["Hc", "vCont;c:*", "c"]. This means, in short, that, while the shortcoming does
    exist in the code, GDB never actually triggers it.
    
    > The test I would like see is pretty much your test case
    >
    >  - load a multi-threaded program
    >  - wait until threads running
    >  - pause
    >  - resume thread
    >  - check resumed thread was the right one
    
    What I have here should be pretty much that.
    
    Is there something else you think I'm missing?
    
    Message-Id: <20230804182633.47300-2-dark.ryu.550@gmail.com>

---
+++ tests/tcg/multiarch/system/interrupt.c
@@ -0,0 +1,25 @@
+/*
+ * External interruption test. This test is structured in such a way that it
+ * passes the cases that require it to exit, but we can make it enter an
+ * infinite loop from GDB.
+ *
+ * We don't have the benefit of libc, just builtin C primitives and
+ * whatever is in minilib.
+ */
+
+#include <minilib.h>
+
+void loop(void)
+{
+    while(1)
+        /* Loop forever. Just make sure the condition is always a constant
+         * expression, so that this loop is not UB, as per the C standard. */
+        ;
+}
+
+int main(void)
+{
+    return 0;
+}
+
+
