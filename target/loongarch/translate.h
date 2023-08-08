3fdb5b0f9be1147fb1154839f7caec031e0aa233
Jiajie Chen <c@jia.je>
2023-08-08 09:54:34 +0800

    target/loongarch: Reject la64-only instructions in la32 mode
    LoongArch64-only instructions are marked with regard to the instruction
    manual Table 2. LSX instructions are not marked for now for lack of
    public manual.
    
    Signed-off-by: Jiajie Chen <c@jia.je>
    Message-Id: <20230808015506.1705140-9-c@jia.je>

--- target/loongarch/translate.h
+++ target/loongarch/translate.h
@@ -14,6 +14,16 @@
     static bool trans_##NAME(DisasContext *ctx, arg_##NAME * a) \
     { return FUNC(ctx, a, __VA_ARGS__); }
 
+/* for LoongArch64-only instructions */
+#define TRANS_64(NAME, FUNC, ...) \
+    static bool trans_##NAME(DisasContext *ctx, arg_##NAME * a) \
+    { \
+        if (ctx->la32) { \
+            return false; \
+        } \
+        return FUNC(ctx, a, __VA_ARGS__); \
+    }
+
 /*
  * If an operation is being performed on less than TARGET_LONG_BITS,
  * it may require the inputs to be sign- or zero-extended; which will
