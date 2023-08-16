bc09ad08d12dc35bfe2bc2707c2b332ef11b893f
Akihiko Odaki <akihiko.odaki@daynix.com>
2023-08-16 22:39:35 +0900

    contrib/plugins: Add cc plugin
    This demonstrates how to write a plugin in C++.
    
    Signed-off-by: Akihiko Odaki <akihiko.odaki@daynix.com>
    Message-Id: <20230816133938.18310-26-akihiko.odaki@daynix.com>

---
+++ contrib/plugins/cc.cc
@@ -0,0 +1,17 @@
+/* SPDX-License-Identifier: GPL-2.0-or-later */
+
+#include <qemu-plugin.h>
+
+extern "C" {
+
+QEMU_PLUGIN_EXPORT int qemu_plugin_version = QEMU_PLUGIN_VERSION;
+
+QEMU_PLUGIN_EXPORT int qemu_plugin_install(qemu_plugin_id_t id,
+                                           const qemu_info_t *info, int argc,
+                                           char **argv)
+{
+    qemu_plugin_outs("hello, world\n");
+    return 0;
+}
+
+};
