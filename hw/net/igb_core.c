1db70ed2c685d6b47a1b488388d18e89cd960ec6
Cédric Le Goater <clg@redhat.com>
2023-08-29 11:05:28 +0200

    igb: Add a VF reset handler
    Export the igb_vf_reset() helper routine from the PF model to let the
    IGBVF model implement its own device reset.
    
    Cc: Akihiko Odaki <akihiko.odaki@daynix.com>
    Suggested-by: Sriram Yagnaraman <sriram.yagnaraman@est.tech>
    Signed-off-by: Cédric Le Goater <clg@redhat.com>
    Reviewed-by: Akihiko Odaki <akihiko.odaki@daynix.com>
    Reviewed-by: Philippe Mathieu-Daudé <philmd@linaro.org>
    Reviewed-by: Sriram Yagnaraman <sriram.yagnaraman@est.tech>
    Message-Id: <20230829090529.184438-2-clg@kaod.org>

--- hw/net/igb_core.c
+++ hw/net/igb_core.c
@@ -2153,10 +2153,12 @@ static void igb_set_vfmailbox(IGBCore *core, int index, uint32_t val)
     }
 }
 
-static void igb_vf_reset(IGBCore *core, uint16_t vfn)
+void igb_core_vf_reset(IGBCore *core, uint16_t vfn)
 {
     uint16_t qn0 = vfn;
     uint16_t qn1 = vfn + IGB_NUM_VM_POOLS;
+
+    trace_igb_core_vf_reset(vfn);
 
     /* disable Rx and Tx for the VF*/
     core->mac[RXDCTL0 + (qn0 * 16)] &= ~E1000_RXDCTL_QUEUE_ENABLE;
@@ -2236,7 +2238,7 @@ static void igb_set_vtctrl(IGBCore *core, int index, uint32_t val)
 
     if (val & E1000_CTRL_RST) {
         vfn = (index - PVTCTRL0) / 0x40;
-        igb_vf_reset(core, vfn);
+        igb_core_vf_reset(core, vfn);
     }
 }
 
