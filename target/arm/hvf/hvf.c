4d01074748a589fa3c2bb4afe37768c40534f224
Alexander Graf <graf@amazon.com>
2023-08-30 16:14:17 +0000

    hvf: arm: Ignore writes to CNTP_CTL_EL0
    MacOS unconditionally disables interrupts of the physical timer on boot
    and then continues to use the virtual one. We don't really want to support
    a full physical timer emulation, so let's just ignore those writes.
    
    Signed-off-by: Alexander Graf <graf@amazon.com>
    
    Reviewed-by: Philippe Mathieu-Daudé <philmd@linaro.org>
    Message-Id: <20230830161425.91946-5-graf@amazon.com>

--- target/arm/hvf/hvf.c
+++ target/arm/hvf/hvf.c
@@ -11,6 +11,7 @@
 
 #include "qemu/osdep.h"
 #include "qemu/error-report.h"
+#include "qemu/log.h"
 
 #include "sysemu/runstate.h"
 #include "sysemu/hvf.h"
@@ -179,6 +180,7 @@ void hvf_arm_init_debug(void)
 #define SYSREG_OSLSR_EL1      SYSREG(2, 0, 1, 1, 4)
 #define SYSREG_OSDLR_EL1      SYSREG(2, 0, 1, 3, 4)
 #define SYSREG_CNTPCT_EL0     SYSREG(3, 3, 14, 0, 1)
+#define SYSREG_CNTP_CTL_EL0   SYSREG(3, 3, 14, 2, 1)
 #define SYSREG_PMCR_EL0       SYSREG(3, 3, 9, 12, 0)
 #define SYSREG_PMUSERENR_EL0  SYSREG(3, 3, 9, 14, 0)
 #define SYSREG_PMCNTENSET_EL0 SYSREG(3, 3, 9, 12, 1)
@@ -1550,6 +1552,13 @@ static int hvf_sysreg_write(CPUState *cpu, uint32_t reg, uint64_t val)
         break;
     case SYSREG_OSLAR_EL1:
         env->cp15.oslsr_el1 = val & 1;
+        break;
+    case SYSREG_CNTP_CTL_EL0:
+        /*
+         * Guests should not rely on the physical counter, but macOS emits
+         * disable writes to it. Let it do so, but ignore the requests.
+         */
+        qemu_log_mask(LOG_UNIMP, "Unsupported write to CNTP_CTL_EL0\n");
         break;
     case SYSREG_OSDLR_EL1:
         /* Dummy register */
