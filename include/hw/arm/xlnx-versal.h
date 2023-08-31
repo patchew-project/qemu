8702b95106c6fd94d0bca634b373cf1d3fa0979e
Tong Ho <tong.ho@amd.com>
2023-08-31 10:18:48 -0700

    hw/arm: xlnx-versal-virt: Add AMD/Xilinx TRNG device
    Connect the support for Versal True Random Number Generator
    (TRNG) device.
    
    Warning: unlike the TRNG component in a real device from the
    Versal device familiy, the connected TRNG model is not of
    cryptographic grade and is not intended for use cases when
    cryptograpically strong TRNG is needed.
    
    Signed-off-by: Tong Ho <tong.ho@amd.com>
    Message-Id: <20230831171849.3297497-3-tong.ho@amd.com>

--- include/hw/arm/xlnx-versal.h
+++ include/hw/arm/xlnx-versal.h
@@ -31,6 +31,7 @@
 #include "hw/dma/xlnx_csu_dma.h"
 #include "hw/misc/xlnx-versal-crl.h"
 #include "hw/misc/xlnx-versal-pmc-iou-slcr.h"
+#include "hw/misc/xlnx-versal-trng.h"
 #include "hw/net/xlnx-versal-canfd.h"
 
 #define TYPE_XLNX_VERSAL "xlnx-versal"
@@ -113,6 +114,7 @@ struct Versal {
         } iou;
 
         XlnxZynqMPRTC rtc;
+        XlnxVersalTRng trng;
         XlnxBBRam bbram;
         XlnxEFuse efuse;
         XlnxVersalEFuseCtrl efuse_ctrl;
@@ -151,6 +153,7 @@ struct Versal {
 #define VERSAL_OSPI_IRQ            124
 #define VERSAL_SD0_IRQ_0           126
 #define VERSAL_EFUSE_IRQ           139
+#define VERSAL_TRNG_IRQ            141
 #define VERSAL_RTC_ALARM_IRQ       142
 #define VERSAL_RTC_SECONDS_IRQ     143
 
@@ -244,4 +247,6 @@ struct Versal {
 #define MM_PMC_CRP_SIZE             0x10000
 #define MM_PMC_RTC                  0xf12a0000
 #define MM_PMC_RTC_SIZE             0x10000
+#define MM_PMC_TRNG                 0xf1230000
+#define MM_PMC_TRNG_SIZE            0x10000
 #endif
