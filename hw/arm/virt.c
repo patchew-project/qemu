530f0fef724cabcc02871e272f88c3d4eac4752b
Gavin Shan <gshan@redhat.com>
2023-09-07 10:35:50 +1000

    hw/arm/virt: Hide host CPU model for tcg
    The 'host' CPU model isn't available until KVM or HVF is enabled.
    For example, the following error messages are seen when the guest
    is started with option '-cpu cortex-a8' on tcg.
    
      qemu-system-aarch64: Invalid CPU type: cortex-a8
      The valid types are: cortex-a7, cortex-a15, cortex-a35, cortex-a55,
                           cortex-a72, cortex-a76, a64fx, neoverse-n1,
                           neoverse-v1, cortex-a53, cortex-a57, (null),
                           max
    
    Hide 'host' CPU model until KVM or HVF is enabled.
    
    Signed-off-by: Gavin Shan <gshan@redhat.com>
    Message-Id: <20230907003553.1636896-30-gshan@redhat.com>

--- hw/arm/virt.c
+++ hw/arm/virt.c
@@ -217,7 +217,9 @@ static const char * const valid_cpu_types[] = {
 #endif
     ARM_CPU_TYPE_NAME("cortex-a53"),
     ARM_CPU_TYPE_NAME("cortex-a57"),
+#if defined(CONFIG_KVM) || defined(CONFIG_HVF)
     ARM_CPU_TYPE_NAME("host"),
+#endif
     ARM_CPU_TYPE_NAME("max"),
     NULL
 };
