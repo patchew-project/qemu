fef3971a9a7618bec1bfed7415740547d0d44014
Richard Henderson <richard.henderson@linaro.org>
2023-09-13 19:44:15 -0700

    target/*: Use __alignof not __alignof__
    No functional change, just using a common spelling.
    
    Signed-off-by: Richard Henderson <richard.henderson@linaro.org>
    Reviewed-by: Philippe Mathieu-Daudé <philmd@linaro.org>
    Message-Id: <20230914024435.1381329-5-richard.henderson@linaro.org>

--- target/s390x/cpu.c
+++ target/s390x/cpu.c
@@ -363,7 +363,7 @@ static const TypeInfo s390_cpu_type_info = {
     .name = TYPE_S390_CPU,
     .parent = TYPE_CPU,
     .instance_size = sizeof(S390CPU),
-    .instance_align = __alignof__(S390CPU),
+    .instance_align = __alignof(S390CPU),
     .instance_init = s390_cpu_initfn,
 
 #ifndef CONFIG_USER_ONLY
