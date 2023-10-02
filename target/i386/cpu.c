b6afaecd8e2adf32a1bf18d37a99ca28f1b86b8e
Xin Li <xin3.li@intel.com>
2023-10-02 00:23:12 -0700

    target/i386: enumerate VMX nested-exception support
    Allow VMX nested-exception support to be exposed in KVM guests, thus
    nested KVM guests can enumerate it.
    
    Tested-by: Shan Kang <shan.kang@intel.com>
    Signed-off-by: Xin Li <xin3.li@intel.com>
    Message-Id: <20231002072313.17603-4-xin3.li@intel.com>

--- target/i386/cpu.c
+++ target/i386/cpu.c
@@ -1341,6 +1341,7 @@ FeatureWordInfo feature_word_info[FEATURE_WORDS] = {
             [54] = "vmx-ins-outs",
             [55] = "vmx-true-ctls",
             [56] = "vmx-any-errcode",
+            [58] = "vmx-nested-exception",
         },
         .msr = {
             .index = MSR_IA32_VMX_BASIC,
