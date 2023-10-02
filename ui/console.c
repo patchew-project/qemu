patchew-project/qemu

commit a92e7bb4cad57cc5c8817fb18fb25650507b69f8

ui: add precondition for dpy_get_ui_info()
Ensure that it only get called when dpy_ui_info_supported(). The
function should always return a result. There should be a non-null
console or active_console.

Modify the argument to be const as well.

Signed-off-by: Marc-André Lureau <marcandre.lureau@redhat.com>
Reviewed-by: Albert Esteve <aesteve@redhat.com>

ui/console.c
@@ -801,7 +801,7 @@ static void dpy_set_ui_info_timer(void *opaque)
     con->hw_ops->ui_info(con->hw, head, &con->ui_info);
 }
 
-bool dpy_ui_info_supported(QemuConsole *con)
+bool dpy_ui_info_supported(const QemuConsole *con)
 {
     if (con == NULL) {
         con = active_console;
@@ -815,6 +815,8 @@ bool dpy_ui_info_supported(QemuConsole *con)
 
 const QemuUIInfo *dpy_get_ui_info(const QemuConsole *con)
 {
+    assert(dpy_ui_info_supported(con));
+
     if (con == NULL) {
         con = active_console;
     }

include/ui/console.h
@@ -329,7 +329,7 @@ void update_displaychangelistener(DisplayChangeListener *dcl,
                                   uint64_t interval);
 void unregister_displaychangelistener(DisplayChangeListener *dcl);
 
-bool dpy_ui_info_supported(QemuConsole *con);
+bool dpy_ui_info_supported(const QemuConsole *con);
 const QemuUIInfo *dpy_get_ui_info(const QemuConsole *con);
 int dpy_set_ui_info(QemuConsole *con, QemuUIInfo *info, bool delay);
 

commit 863a7a68aae121698909812dcaa4ea2a54125bbb

ui: fix dpy_ui_info_supported(con) assertion
VGA does not support getting the physical video size or refresh rate.
This is causing an assertion failure when the GTK+ user interface
calls dpy_get_ui_info().  Return NULL from dpy_get_ui_info() if the
information is not supported, and just ignore the request to set
refresh rate or size in that case.

While the assertion failure was introduced by commit a92e7bb4cad
("ui: add precondition for dpy_get_ui_info()", 2023-09-12), QEMU had
been using con->ui_info incorrectly since before.

Fixes: a92e7bb4cad ("ui: add precondition for dpy_get_ui_info()", 2023-09-12)
Fixes: aeffd071ed8 ("ui: Deliver refresh rate via QemuUIInfo", 2022-06-14)
Signed-off-by: Paolo Bonzini <pbonzini@redhat.com>
Message-Id: <20231002095344.64509-1-pbonzini@redhat.com>

ui/console.c
@@ -815,7 +815,9 @@ bool dpy_ui_info_supported(const QemuConsole *con)
 
 const QemuUIInfo *dpy_get_ui_info(const QemuConsole *con)
 {
-    assert(dpy_ui_info_supported(con));
+    if (!dpy_ui_info_supported(con)) {
+        return NULL;
+    }
 
     if (con == NULL) {
         con = active_console;

ui/gtk.c
@@ -724,18 +724,30 @@ static gboolean gd_window_close(GtkWidget *widget, GdkEvent *event,
 
 static void gd_set_ui_refresh_rate(VirtualConsole *vc, int refresh_rate)
 {
-    QemuUIInfo info;
+    QemuUIInfo *p_info, info;
 
-    info = *dpy_get_ui_info(vc->gfx.dcl.con);
+    p_info = dpy_get_ui_info(vc->gfx.dcl.con);
+    if (!p_info) {
+        /* not supported by guest */
+        return;
+    }
+
+    info = *p_info;
     info.refresh_rate = refresh_rate;
     dpy_set_ui_info(vc->gfx.dcl.con, &info, true);
 }
 
 static void gd_set_ui_size(VirtualConsole *vc, gint width, gint height)
 {
-    QemuUIInfo info;
+    QemuUIInfo *p_info, info;
 
-    info = *dpy_get_ui_info(vc->gfx.dcl.con);
+    p_info = dpy_get_ui_info(vc->gfx.dcl.con);
+    if (!p_info) {
+        /* not supported by guest */
+        return;
+    }
+
+    info = *p_info;
     info.width = width;
     info.height = height;
     dpy_set_ui_info(vc->gfx.dcl.con, &info, true);