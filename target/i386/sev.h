58a4d3bd73d0e2af86804b5c991a1deb6d545794
Tyler Fanelli <tfanelli@redhat.com>
2023-10-04 16:34:16 -0400

    i386/sev: Replace LAUNCH_SECRET ioctl with sev library equivalent
    The LAUNCH_SECRET API can inject a secret into the VM once the
    measurement has been retrieved.
    
    If this API ioctl call fails, fw_error will be set accordingly.
    
    Signed-off-by: Tyler Fanelli <tfanelli@redhat.com>
    Message-Id: <20231004203418.56508-8-tfanelli@redhat.com>

--- target/i386/sev.h
+++ target/i386/sev.h
@@ -53,8 +53,6 @@ uint32_t sev_get_reduced_phys_bits(void);
 bool sev_add_kernel_loader_hashes(SevKernelLoaderContext *ctx, Error **errp);
 
 int sev_encrypt_flash(uint8_t *ptr, uint64_t len, Error **errp);
-int sev_inject_launch_secret(const char *hdr, const char *secret,
-                             uint64_t gpa, Error **errp);
 
 int sev_es_save_reset_vector(void *flash_ptr, uint64_t flash_size);
 void sev_es_set_reset_vector(CPUState *cpu);
