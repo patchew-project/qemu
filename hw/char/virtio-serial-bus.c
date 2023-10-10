patchew-project/qemu

commit d4c19cdeeb2f1e474bc426a6da261f1d7346eb5b

virtio-serial: add missing virtio_detach_element() call
Ports enter a "throttled" state when writing to the chardev would block.
The current output VirtQueueElement is kept around until the chardev
becomes writable again.

There are several places in the virtio-serial lifecycle where the
VirtQueueElement should be thrown away.  For example, if the virtio
device is reset then virtqueue elements are no longer valid.

This patch adds the discard_throttle_data() function to unmap the
scatter-gather list and decrement vq->inuse.  This ensures that the
VirtQueueElement is freed properly.

Cc: amit.shah@redhat.com
Signed-off-by: Stefan Hajnoczi <stefanha@redhat.com>
Tested-by: Ladi Prosek <lprosek@redhat.com>
Reviewed-by: Ladi Prosek <lprosek@redhat.com>
Reviewed-by: Michael S. Tsirkin <mst@redhat.com>
Signed-off-by: Michael S. Tsirkin <mst@redhat.com>

hw/char/virtio-serial-bus.c
@@ -145,6 +145,15 @@ static void discard_vq_data(VirtQueue *vq, VirtIODevice *vdev)
     virtio_notify(vdev, vq);
 }
 
+static void discard_throttle_data(VirtIOSerialPort *port)
+{
+    if (port->elem) {
+        virtqueue_detach_element(port->ovq, port->elem, 0);
+        g_free(port->elem);
+        port->elem = NULL;
+    }
+}
+
 static void do_flush_queued_data(VirtIOSerialPort *port, VirtQueue *vq,
                                  VirtIODevice *vdev)
 {
@@ -267,6 +276,7 @@ int virtio_serial_close(VirtIOSerialPort *port)
      * consume, reset the throttling flag and discard the data.
      */
     port->throttled = false;
+    discard_throttle_data(port);
     discard_vq_data(port->ovq, VIRTIO_DEVICE(port->vser));
 
     send_control_event(port->vser, port->id, VIRTIO_CONSOLE_PORT_OPEN, 0);
@@ -591,6 +601,9 @@ static void guest_reset(VirtIOSerial *vser)
 
     QTAILQ_FOREACH(port, &vser->ports, next) {
         vsc = VIRTIO_SERIAL_PORT_GET_CLASS(port);
+
+        discard_throttle_data(port);
+
         if (port->guest_connected) {
             port->guest_connected = false;
             if (vsc->set_guest_connected) {
@@ -901,6 +914,7 @@ static void remove_port(VirtIOSerial *vser, uint32_t port_id)
     assert(port);
 
     /* Flush out any unconsumed buffers first */
+    discard_throttle_data(port);
     discard_vq_data(port->ovq, VIRTIO_DEVICE(port->vser));
 
     send_control_event(vser, port->id, VIRTIO_CONSOLE_PORT_REMOVE, 1);

commit 87cce30097c8c034f172be84c6059ea4d0efdfef

virtio-serial-bus: Discard throttled VirtQueueElement when virtio-serial closed
With commit d4c19cde("virtio-serial: add missing virtio_detach_element() call"),
when virtio serial is throttled and closed by host, port->elem should be discard with virtqueue_push,
otherwise virtqueue will not rewind, guest will blocked finally and cannot write anymore data.

It can be reproduced with following steps:
Create a vm with virtio-serial device through libvirt:

    <channel type='unix'>
      <source mode='bind' path='/tmp/test'/>
      <target type='virtio' name='com.test.channel.0'/>
      <address type='virtio-serial' controller='0' bus='0' port='1'/>
    </channel>

Host run:
watch -n0.5 'timeout 0.5 nc -U /tmp/test'

Guest run:
hexdump -C -v /dev/zero > /dev/vport1p1

After sometime, guest can not write any data to serial.
We can see vq->last_avail_idx - vq->used_idx = 128 with gdb.

(gdb) p *(struct VirtQueue *) 0x565161c5c788
$4 = {vring = {num = 128, num_default = 128, align = 4096, desc = 4316049408, avail = 4316051456, used = 4316051776,
    caches = 0x7fc530067e60}, used_elems = 0x565161c88dc0, last_avail_idx = 7929, last_avail_wrap_counter = true,
  shadow_avail_idx = 7929, shadow_avail_wrap_counter = true, used_idx = 7801, used_wrap_counter = true, signalled_used = 7801,
  signalled_used_valid = true, notification = true, queue_index = 5, inuse = 0, vector = 1,
  handle_output = 0x56515da8aea0 <handle_output>, handle_aio_output = 0x0, vdev = 0x565161c54e30, guest_notifier = {rfd = 0,
    wfd = 0}, host_notifier = {rfd = 74, wfd = 74}, host_notifier_enabled = true, node = {le_next = 0x565161c5c6f0,
    le_prev = 0x565161c5c8a8}}

Fixes: d4c19cde("virtio-serial: add missing virtio_detach_element() call")
Signed-off-by: Sun Feng <loyou85@gmail.com>
Message-Id: <20231010062216.16144-1-loyou85@gmail.com>

hw/char/virtio-serial-bus.c
@@ -151,7 +151,7 @@ static void discard_vq_data(VirtQueue *vq, VirtIODevice *vdev)
 static void discard_throttle_data(VirtIOSerialPort *port)
 {
     if (port->elem) {
-        virtqueue_detach_element(port->ovq, port->elem, 0);
+        virtqueue_push(port->ovq, port->elem, 0);
         g_free(port->elem);
         port->elem = NULL;
     }