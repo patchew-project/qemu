f97285b65bb132b40d291cb5c03012634a3da28b
Juan Quintela <quintela@redhat.com>
2023-10-13 12:47:31 +0200

    migration: Deprecate block migration
    It is obsolete.  It is better to use driver-mirror with NBD instead.
    
    CC: Kevin Wolf <kwolf@redhat.com>
    CC: Eric Blake <eblake@redhat.com>
    CC: Stefan Hajnoczi <stefanha@redhat.com>
    CC: Hanna Czenczek <hreitz@redhat.com>
    
    Signed-off-by: Juan Quintela <quintela@redhat.com>
    Acked-by: Stefan Hajnoczi <stefanha@redhat.com>
    Message-Id: <20231013104736.31722-6-quintela@redhat.com>

--- migration/options.c
+++ migration/options.c
@@ -12,6 +12,7 @@
  */
 
 #include "qemu/osdep.h"
+#include "qemu/error-report.h"
 #include "exec/target_page.h"
 #include "qapi/clone-visitor.h"
 #include "qapi/error.h"
@@ -464,10 +465,14 @@ bool migrate_caps_check(bool *old_caps, bool *new_caps, Error **errp)
     if (new_caps[MIGRATION_CAPABILITY_BLOCK]) {
         error_setg(errp, "QEMU compiled without old-style (blk/-b, inc/-i) "
                    "block migration");
-        error_append_hint(errp, "Use drive_mirror+NBD instead.\n");
+        error_append_hint(errp, "Use blockdev-mirror with NBD instead.\n");
         return false;
     }
 #endif
+    if (new_caps[MIGRATION_CAPABILITY_BLOCK]) {
+        warn_report("Block migration is deprecated. "
+                    "Use blockdev-mirror with NBD instead.");
+    }
 
 #ifndef CONFIG_REPLICATION
     if (new_caps[MIGRATION_CAPABILITY_X_COLO]) {
@@ -1351,6 +1356,8 @@ static void migrate_params_apply(MigrateSetParameters *params, Error **errp)
     }
 
     if (params->has_block_incremental) {
+        warn_report("Block migration is deprecated. "
+                    "Use blockdev-mirror with NBD instead.");
         s->parameters.block_incremental = params->block_incremental;
     }
     if (params->has_multifd_channels) {
