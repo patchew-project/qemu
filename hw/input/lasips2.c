ec2aa3e34540eb8dd13aaae1cacc709f0c078dbe
Helge Deller <deller@gmx.de>
2023-10-14 21:57:12 +0200

    lasips2: LASI PS/2 devices are not user-createable
    Those PS/2 ports are created with the LASI controller when
    a 32-bit PA-RISC machine is created.
    
    Mark them not user-createable to avoid showing them in
    the qemu device list.
    
    Signed-off-by: Helge Deller <deller@gmx.de>
    Cc: qemu-stable@nongnu.org
    Message-Id: <20231014195719.151397-6-deller@kernel.org>

--- hw/input/lasips2.c
+++ hw/input/lasips2.c
@@ -351,6 +351,8 @@ static void lasips2_port_class_init(ObjectClass *klass, void *data)
 {
     DeviceClass *dc = DEVICE_CLASS(klass);
 
+    /* Lasi devices can not be created by users */
+    dc->user_creatable = false;
     dc->realize = lasips2_port_realize;
 }
 
@@ -397,6 +399,8 @@ static void lasips2_kbd_port_class_init(ObjectClass *klass, void *data)
     DeviceClass *dc = DEVICE_CLASS(klass);
     LASIPS2PortDeviceClass *lpdc = LASIPS2_PORT_CLASS(klass);
 
+    /* Lasi devices can not be created by users */
+    dc->user_creatable = false;
     device_class_set_parent_realize(dc, lasips2_kbd_port_realize,
                                     &lpdc->parent_realize);
 }
