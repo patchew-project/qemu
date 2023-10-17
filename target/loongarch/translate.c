6374ea46b656134a37421c1644b5ddca23bda9ec
Philippe Mathieu-Daudé <philmd@linaro.org>
2023-10-17 14:38:49 +0200

    target/loongarch: Use i128 for 128-bit load/store in XVLD
    Signed-off-by: Philippe Mathieu-Daudé <philmd@linaro.org>
    Tested-by: Song Gao <gaosong@loongson.cn>
    Message-Id: <20231017123849.40834-3-philmd@linaro.org>

--- target/loongarch/translate.c
+++ target/loongarch/translate.c
@@ -67,6 +67,12 @@ static inline void get_vreg128(TCGv_i128 dest, int regno, int index)
                     offsetof(CPULoongArchState, fpr[regno].vreg.Q(index)));
 }
 
+static inline void set_vreg128(TCGv_i128 src, int regno, int index)
+{
+    tcg_gen_st_i128(src, tcg_env,
+                    offsetof(CPULoongArchState, fpr[regno].vreg.Q(index)));
+}
+
 static inline int plus_1(DisasContext *ctx, int x)
 {
     return x + 1;
