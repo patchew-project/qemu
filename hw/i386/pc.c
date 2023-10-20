7c9d8f51947f7764a58dc6be7cb1a44ccccd8712
Philippe Mathieu-Daudé <philmd@linaro.org>
2023-10-20 12:54:44 +0200

    hw/i386/pc: Inline legacy pcspk_init() in pc_basic_device_init()
    Reviewed-by: Mark Cave-Ayland <mark.cave-ayland@ilande.co.uk>
    Signed-off-by: Philippe Mathieu-Daudé <philmd@linaro.org>
    Message-Id: <20231020105447.43482-4-philmd@linaro.org>

--- hw/i386/pc.c
+++ hw/i386/pc.c
@@ -1286,7 +1286,11 @@ bool pc_basic_device_init(struct PCMachineState *pcms,
             /* connect PIT to output control line of the HPET */
             qdev_connect_gpio_out(hpet, 0, qdev_get_gpio_in(DEVICE(pit), 0));
         }
-        pcspk_init(pcms->pcspk, isa_bus, pit);
+        object_property_set_link(OBJECT(pcms->pcspk), "pit",
+                                 OBJECT(pit), &error_fatal);
+        if (!isa_realize_and_unref(pcms->pcspk, isa_bus, errp)) {
+            return false;
+        }
     }
 
     /* Super I/O */
