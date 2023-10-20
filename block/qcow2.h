cb744be6a877aedee17eff62f4f98f4607c901c4
Andrey Drobyshev <andrey.drobyshev@virtuozzo.com>
2023-10-21 00:56:16 +0300

    qcow2: make function update_refcount_discard() global
    We are going to need it for discarding separate subclusters.  The
    function itself doesn't do anything with the refcount tables, it simply
    adds a discard request to the queue, so rename it to qcow2_queue_discard().
    
    Signed-off-by: Andrey Drobyshev <andrey.drobyshev@virtuozzo.com>
    Reviewed-by: Hanna Czenczek <hreitz@redhat.com>
    Message-Id: <20231020215622.789260-2-andrey.drobyshev@virtuozzo.com>

--- block/qcow2.h
+++ block/qcow2.h
@@ -891,6 +891,8 @@ int coroutine_fn qcow2_check_refcounts(BlockDriverState *bs, BdrvCheckResult *re
                                        BdrvCheckMode fix);
 
 void qcow2_process_discards(BlockDriverState *bs, int ret);
+void qcow2_queue_discard(BlockDriverState *bs, uint64_t offset,
+                         uint64_t length);
 
 int qcow2_check_metadata_overlap(BlockDriverState *bs, int ign, int64_t offset,
                                  int64_t size);
