a1b7b819c9a613d3e449dfbc2f37a15e613c9af4
Ninad Palsule <ninad@linux.ibm.com>
2023-10-21 16:17:12 -0500

    hw/fsi: Introduce IBM's cfam,fsi-slave
    This is a part of patchset where IBM's Flexible Service Interface is
    introduced.
    
    The Common FRU Access Macro (CFAM), an address space containing
    various "engines" that drive accesses on busses internal and external
    to the POWER chip. Examples include the SBEFIFO and I2C masters. The
    engines hang off of an internal Local Bus (LBUS) which is described
    by the CFAM configuration block.
    
    The FSI slave: The slave is the terminal point of the FSI bus for
    FSI symbols addressed to it. Slaves can be cascaded off of one
    another. The slave's configuration registers appear in address space
    of the CFAM to which it is attached.
    
    Signed-off-by: Andrew Jeffery <andrew@aj.id.au>
    Signed-off-by: Ninad Palsule <ninad@linux.ibm.com>
    Message-Id: <20231021211720.3571082-4-ninad@linux.ibm.com>

--- include/hw/fsi/fsi.h
+++ include/hw/fsi/fsi.h
@@ -7,10 +7,31 @@
 #ifndef FSI_FSI_H
 #define FSI_FSI_H
 
+#include "hw/qdev-core.h"
+
+/*
+ * TODO: Maybe unwind this dependency with const links? Store a
+ * pointer in FSIBus?
+ */
+#include "hw/fsi/cfam.h"
+
 /* Bitwise operations at the word level. */
 #define BE_BIT(x)                          BIT(31 - (x))
 #define GENMASK(t, b) \
     (((1ULL << ((t) + 1)) - 1) & ~((1ULL << (b)) - 1))
 #define BE_GENMASK(t, b)                   GENMASK(BE_BIT(t), BE_BIT(b))
+
+#define TYPE_FSI_BUS "fsi.bus"
+OBJECT_DECLARE_SIMPLE_TYPE(FSIBus, FSI_BUS)
+
+/* TODO: Figure out what's best with a point-to-point bus */
+typedef struct FSISlaveState FSISlaveState;
+
+typedef struct FSIBus {
+    BusState bus;
+
+    /* XXX: It's point-to-point, just instantiate the slave directly for now */
+    FSICFAMState slave;
+} FSIBus;
 
 #endif
