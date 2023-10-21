4d3312c9f7d4cafb7495a66942275103f9b31988
Ninad Palsule <ninad@linux.ibm.com>
2023-10-21 16:17:13 -0500

    hw/fsi: Introduce IBM's FSI
    This is a part of patchset where IBM's Flexible Service Interface is
    introduced.
    
    This commit models the FSI bus. CFAM is hanging out of FSI bus. The bus
    is model such a way that it is embedded inside the FSI master which is a
    bus controller.
    
    The FSI master: A controller in the platform service processor (e.g.
    BMC) driving CFAM engine accesses into the POWER chip. At the
    hardware level FSI is a bit-based protocol supporting synchronous and
    DMA-driven accesses of engines in a CFAM.
    
    Signed-off-by: Andrew Jeffery <andrew@aj.id.au>
    Signed-off-by: Ninad Palsule <ninad@linux.ibm.com>
    Reviewed-by: Joel Stanley <joel@jms.id.au>
    Message-Id: <20231021211720.3571082-5-ninad@linux.ibm.com>

---
+++ hw/fsi/fsi.c
@@ -0,0 +1,25 @@
+/*
+ * SPDX-License-Identifier: GPL-2.0-or-later
+ * Copyright (C) 2023 IBM Corp.
+ *
+ * IBM Flexible Service Interface
+ */
+#include "qemu/osdep.h"
+
+#include "qapi/error.h"
+
+#include "hw/fsi/fsi.h"
+#include "hw/fsi/cfam.h"
+
+static const TypeInfo fsi_bus_info = {
+    .name = TYPE_FSI_BUS,
+    .parent = TYPE_BUS,
+    .instance_size = sizeof(FSIBus),
+};
+
+static void fsi_bus_register_types(void)
+{
+    type_register_static(&fsi_bus_info);
+}
+
+type_init(fsi_bus_register_types);
