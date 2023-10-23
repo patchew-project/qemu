e688668a96f6c5495438c37ad7894ff4db127d25
Marc-André Lureau <marcandre.lureau@redhat.com>
2023-10-23 15:30:31 +0400

    vl: move display early init before default devices
    The next commit needs to have the display registered itself before
    creating the default VCs.
    
    Signed-off-by: Marc-André Lureau <marcandre.lureau@redhat.com>
    Acked-by: Michael S. Tsirkin <mst@redhat.com>
    Message-Id: <20231023113047.2572137-5-marcandre.lureau@redhat.com>

--- system/vl.c
+++ system/vl.c
@@ -1349,6 +1349,28 @@ static void qemu_disable_default_devices(void)
     }
 }
 
+static void qemu_early_display_init(void)
+{
+#if defined(CONFIG_VNC)
+    if (!QTAILQ_EMPTY(&(qemu_find_opts("vnc")->head))) {
+        display_remote++;
+    }
+#endif
+    if (dpy.type == DISPLAY_TYPE_DEFAULT && !display_remote) {
+        if (!qemu_display_find_default(&dpy)) {
+            dpy.type = DISPLAY_TYPE_NONE;
+#if defined(CONFIG_VNC)
+            vnc_parse("localhost:0,to=99,id=default");
+#endif
+        }
+    }
+    if (dpy.type == DISPLAY_TYPE_DEFAULT) {
+        dpy.type = DISPLAY_TYPE_NONE;
+    }
+
+    qemu_display_early_init(&dpy);
+}
+
 static void qemu_create_default_devices(void)
 {
     MachineClass *machine_class = MACHINE_GET_CLASS(current_machine);
@@ -1396,23 +1418,6 @@ static void qemu_create_default_devices(void)
 #ifdef CONFIG_SLIRP
         qemu_opts_parse(net, "user", true, &error_abort);
 #endif
-    }
-
-#if defined(CONFIG_VNC)
-    if (!QTAILQ_EMPTY(&(qemu_find_opts("vnc")->head))) {
-        display_remote++;
-    }
-#endif
-    if (dpy.type == DISPLAY_TYPE_DEFAULT && !display_remote) {
-        if (!qemu_display_find_default(&dpy)) {
-            dpy.type = DISPLAY_TYPE_NONE;
-#if defined(CONFIG_VNC)
-            vnc_parse("localhost:0,to=99,id=default");
-#endif
-        }
-    }
-    if (dpy.type == DISPLAY_TYPE_DEFAULT) {
-        dpy.type = DISPLAY_TYPE_NONE;
     }
 
     /* If no default VGA is requested, the default is "none".  */
@@ -1939,7 +1944,6 @@ static void qemu_create_early_backends(void)
                      "ignoring option");
     }
 
-    qemu_display_early_init(&dpy);
     qemu_console_early_init();
 
     if (dpy.has_gl && dpy.gl != DISPLAYGL_MODE_OFF && display_opengl == 0) {
@@ -3670,6 +3674,7 @@ void qemu_init(int argc, char **argv)
     suspend_mux_open();
 
     qemu_disable_default_devices();
+    qemu_early_display_init();
     qemu_create_default_devices();
     qemu_create_early_backends();
 
