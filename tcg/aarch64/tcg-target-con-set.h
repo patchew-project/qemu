06462d08599848b943ac492d90a0b529430b885c
Richard Henderson <richard.henderson@linaro.org>
2023-10-25 17:13:53 -0700

    tcg/aarch64: Support TCG_COND_TST{EQ,NE}
    Signed-off-by: Richard Henderson <richard.henderson@linaro.org>
    Message-Id: <20231026001542.1141412-15-richard.henderson@linaro.org>

--- tcg/aarch64/tcg-target-con-set.h
+++ tcg/aarch64/tcg-target-con-set.h
@@ -10,7 +10,7 @@
  * tcg-target-con-str.h; the constraint combination is inclusive or.
  */
 C_O0_I1(r)
-C_O0_I2(r, rA)
+C_O0_I2(r, rAL)
 C_O0_I2(rZ, r)
 C_O0_I2(w, r)
 C_O0_I3(rZ, rZ, r)
@@ -31,6 +31,6 @@ C_O1_I2(w, w, wN)
 C_O1_I2(w, w, wO)
 C_O1_I2(w, w, wZ)
 C_O1_I3(w, w, w, w)
-C_O1_I4(r, r, rA, rZ, rZ)
+C_O1_I4(r, r, rAL, rZ, rZ)
 C_O2_I1(r, r, r)
 C_O2_I4(r, r, rZ, rZ, rA, rMZ)
