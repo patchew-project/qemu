34474624ae41cd2ccd34680881a6f1f5a8d4a08a
Joelle van Dyne <j@getutm.app>
2023-10-28 23:03:26 -0700

    tests: acpi: updated expected blobs for TPM CRB
    Signed-off-by: Joelle van Dyne <j@getutm.app>
    Message-Id: <20231029060404.71196-14-j@getutm.app>

--- tests/qtest/bios-tables-test-allowed-diff.h
+++ tests/qtest/bios-tables-test-allowed-diff.h
@@ -1,5 +1,1 @@
 /* List of comma-separated changed AML files to ignore */
-"tests/data/acpi/q35/DSDT.crb.tpm2",
-"tests/data/acpi/q35/TPM2.crb.tpm2",
-"tests/data/acpi/virt/DSDT.crb.tpm2",
-"tests/data/acpi/virt/TPM2.crb.tpm2",
