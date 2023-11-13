b8fae3e692c8b2c0975f1a54ad30d9863bbab7bc
Salil Mehta via <qemu-devel@nongnu.org>
2023-11-13 20:12:29 +0000

    accel/kvm: Extract common KVM vCPU {creation, parking} code
    KVM vCPU creation is done once during the vCPU realization when Qemu vCPU thread
    is spawned. This is common to all the architectures as of now.
    
    Hot-unplug of vCPU results in destruction of the vCPU object in QOM but the
    corresponding KVM vCPU object in the Host KVM is not destroyed as KVM doesn't
    support vCPU removal. Therefore, its representative KVM vCPU object/context in
    Qemu is parked.
    
    Refactor architecture common logic so that some APIs could be reused by vCPU
    Hotplug code of some architectures likes ARM, Loongson etc. Update new/old APIs
    with trace events instead of DPRINTF. No functional change is intended here.
    
    Signed-off-by: Salil Mehta <salil.mehta@huawei.com>
    Reviewed-by: Gavin Shan <gshan@redhat.com>
    Tested-by: Vishnu Pajjuri <vishnu@os.amperecomputing.com>
    Reviewed-by: Jonathan Cameron <Jonathan.Cameron@huawei.com>
    Tested-by: Xianglai Li <lixianglai@loongson.cn>
    Tested-by: Miguel Luis <miguel.luis@oracle.com>
    Reviewed-by: Shaoqin Huang <shahuang@redhat.com>
    Message-Id: <20231113201236.30668-2-salil.mehta@huawei.com>

--- include/sysemu/kvm.h
+++ include/sysemu/kvm.h
@@ -435,6 +435,22 @@ void kvm_set_sigmask_len(KVMState *s, unsigned int sigmask_len);
 int kvm_physical_memory_addr_from_host(KVMState *s, void *ram_addr,
                                        hwaddr *phys_addr);
 
+/**
+ * kvm_create_vcpu - Gets a parked KVM vCPU or creates a KVM vCPU
+ * @cpu: QOM CPUState object for which KVM vCPU has to be fetched/created.
+ *
+ * @returns: 0 when success, errno (<0) when failed.
+ */
+int kvm_create_vcpu(CPUState *cpu);
+
+/**
+ * kvm_park_vcpu - Park QEMU KVM vCPU context
+ * @cpu: QOM CPUState object for which QEMU KVM vCPU context has to be parked.
+ *
+ * @returns: none
+ */
+void kvm_park_vcpu(CPUState *cpu);
+
 #endif /* NEED_CPU_H */
 
 void kvm_cpu_synchronize_state(CPUState *cpu);
