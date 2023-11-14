3e3a3d6370fcbccf43e4df492b29873bdb350dea
Joelle van Dyne <j@getutm.app>
2023-11-13 18:09:17 -0800

    hw/arm/virt: connect TPM to platform bus
    Signed-off-by: Joelle van Dyne <j@getutm.app>
    Reviewed-by: Stefan Berger <stefanb@linux.ibm.com>
    Message-Id: <20231114020927.62315-8-j@getutm.app>

--- hw/arm/virt.c
+++ hw/arm/virt.c
@@ -2811,6 +2811,13 @@ static void virt_machine_device_plug_cb(HotplugHandler *hotplug_dev,
         vms->virtio_iommu_bdf = pci_get_bdf(pdev);
         create_virtio_iommu_dt_bindings(vms);
     }
+
+#ifdef CONFIG_TPM
+    if (object_dynamic_cast(OBJECT(dev), TYPE_TPM_IF)) {
+        tpm_sysbus_plug(TPM_IF(dev), OBJECT(vms->platform_bus_dev),
+                        vms->memmap[VIRT_PLATFORM_BUS].base);
+    }
+#endif
 }
 
 static void virt_dimm_unplug_request(HotplugHandler *hotplug_dev,
