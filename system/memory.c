3a4968f768c548aec6db21b60a92cb727c2693fe
Philippe Mathieu-Daudé <philmd@linaro.org>
2023-11-20 22:32:40 +0100

    memory: Have memory_region_init_ram() handler return a boolean
    Following the example documented since commit e3fe3988d7 ("error:
    Document Error API usage rules"), have cpu_exec_realizefn()
    return a boolean indicating whether an error is set or not.
    
    Signed-off-by: Philippe Mathieu-Daudé <philmd@linaro.org>
    Reviewed-by: Gavin Shan <gshan@redhat.com>
    Reviewed-by: Peter Xu <peterx@redhat.com>
    Message-Id: <20231120213301.24349-7-philmd@linaro.org>

--- system/memory.c
+++ system/memory.c
@@ -3597,7 +3597,7 @@ void mtree_info(bool flatview, bool dispatch_tree, bool owner, bool disabled)
     }
 }
 
-void memory_region_init_ram(MemoryRegion *mr,
+bool memory_region_init_ram(MemoryRegion *mr,
                             Object *owner,
                             const char *name,
                             uint64_t size,
@@ -3606,7 +3606,7 @@ void memory_region_init_ram(MemoryRegion *mr,
     DeviceState *owner_dev;
 
     if (!memory_region_init_ram_nomigrate(mr, owner, name, size, errp)) {
-        return;
+        return false;
     }
     /* This will assert if owner is neither NULL nor a DeviceState.
      * We only want the owner here for the purposes of defining a
@@ -3616,6 +3616,8 @@ void memory_region_init_ram(MemoryRegion *mr,
      */
     owner_dev = DEVICE(owner);
     vmstate_register_ram(mr, owner_dev);
+
+    return true;
 }
 
 void memory_region_init_rom(MemoryRegion *mr,
