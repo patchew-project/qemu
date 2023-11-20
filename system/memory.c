5934774a8722538b550299cf0c5ac8c3e42e717b
Philippe Mathieu-Daudé <philmd@linaro.org>
2023-11-20 22:32:45 +0100

    memory: Have memory_region_init_resizeable_ram() return a boolean
    Following the example documented since commit e3fe3988d7 ("error:
    Document Error API usage rules"), have cpu_exec_realizefn()
    return a boolean indicating whether an error is set or not.
    
    Signed-off-by: Philippe Mathieu-Daudé <philmd@linaro.org>
    Reviewed-by: Gavin Shan <gshan@redhat.com>
    Reviewed-by: Peter Xu <peterx@redhat.com>
    Message-Id: <20231120213301.24349-12-philmd@linaro.org>

--- system/memory.c
+++ system/memory.c
@@ -1606,7 +1606,7 @@ bool memory_region_init_ram_flags_nomigrate(MemoryRegion *mr,
     return true;
 }
 
-void memory_region_init_resizeable_ram(MemoryRegion *mr,
+bool memory_region_init_resizeable_ram(MemoryRegion *mr,
                                        Object *owner,
                                        const char *name,
                                        uint64_t size,
@@ -1627,7 +1627,9 @@ void memory_region_init_resizeable_ram(MemoryRegion *mr,
         mr->size = int128_zero();
         object_unparent(OBJECT(mr));
         error_propagate(errp, err);
+        return false;
     }
+    return true;
 }
 
 #ifdef CONFIG_POSIX
