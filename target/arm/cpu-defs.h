c868eccb1578ce26a102b9c281b2112b1b0fc4b3
Philippe Mathieu-Daudé <philmd@linaro.org>
2023-11-22 19:30:41 +0100

    target/arm: Move GTIMER definitions to 'cpu-defs.h'
    To allow GTIMER_* definitions to be used by non-ARM specific
    hardware models, move them to a new target agnostic "cpu-defs.h"
    header.
    
    Signed-off-by: Philippe Mathieu-Daudé <philmd@linaro.org>
    Message-Id: <20231122183048.17150-6-philmd@linaro.org>

---
+++ target/arm/cpu-defs.h
@@ -0,0 +1,19 @@
+/*
+ * ARM "target agnostic" CPU definitions
+ *
+ *  Copyright (c) 2003 Fabrice Bellard
+ *
+ * SPDX-License-Identifier: LGPL-2.1-or-later
+ */
+
+#ifndef ARM_CPU_DEFS_H
+#define ARM_CPU_DEFS_H
+
+#define GTIMER_PHYS     0
+#define GTIMER_VIRT     1
+#define GTIMER_HYP      2
+#define GTIMER_SEC      3
+#define GTIMER_HYPVIRT  4
+#define NUM_GTIMERS     5
+
+#endif
