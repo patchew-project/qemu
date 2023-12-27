67120407b5c3afd221eaed9bdf2a0aa0554ef7a9
Song Gao <gaosong@loongson.cn>
2023-12-27 16:08:15 +0800

    hw/loongarch: fdt adds Extend I/O Interrupt Controller
    fdt adds Extend I/O Interrupt Controller,
    we use 'loongson,ls2k2000-eiointc'.
    
    See:
      drivers/irqchip/irq-loongson-eiointc.c
    
    Signed-off-by: Song Gao <gaosong@loongson.cn>
    Message-Id: <20231227080821.3216113-12-gaosong@loongson.cn>

--- include/hw/intc/loongarch_extioi.h
+++ include/hw/intc/loongarch_extioi.h
@@ -39,6 +39,7 @@
 #define EXTIOI_COREISR_END           (0xB20 - APIC_OFFSET)
 #define EXTIOI_COREMAP_START         (0xC00 - APIC_OFFSET)
 #define EXTIOI_COREMAP_END           (0xD00 - APIC_OFFSET)
+#define EXTIOI_SIZE                  0x800
 
 #define TYPE_LOONGARCH_EXTIOI "loongarch.extioi"
 OBJECT_DECLARE_SIMPLE_TYPE(LoongArchExtIOI, LOONGARCH_EXTIOI)
