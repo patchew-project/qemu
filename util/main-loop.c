6fc34eaede81291aeff1653e45e45647e8e0f07c
Stefan Hajnoczi <stefanha@redhat.com>
2024-01-02 10:35:25 -0500

    system/cpus: rename qemu_mutex_lock_iothread() to bql_lock()
    The Big QEMU Lock (BQL) has many names and they are confusing. The
    actual QemuMutex variable is called qemu_global_mutex but it's commonly
    referred to as the BQL in discussions and some code comments. The
    locking APIs, however, are called qemu_mutex_lock_iothread() and
    qemu_mutex_unlock_iothread().
    
    The "iothread" name is historic and comes from when the main thread was
    split into into KVM vcpu threads and the "iothread" (now called the main
    loop thread). I have contributed to the confusion myself by introducing
    a separate --object iothread, a separate concept unrelated to the BQL.
    
    The "iothread" name is no longer appropriate for the BQL. Rename the
    locking APIs to:
    - void bql_lock(void)
    - void bql_unlock(void)
    - bool bql_locked(void)
    
    There are more APIs with "iothread" in their names. Subsequent patches
    will rename them. There are also comments and documentation that will be
    updated in later patches.
    
    Signed-off-by: Stefan Hajnoczi <stefanha@redhat.com>
    Reviewed-by: Paul Durrant <paul@xen.org>
    Acked-by: Fabiano Rosas <farosas@suse.de>
    Acked-by: David Woodhouse <dwmw@amazon.co.uk>
    Reviewed-by: Cédric Le Goater <clg@kaod.org>
    Acked-by: Peter Xu <peterx@redhat.com>
    Acked-by: Eric Farman <farman@linux.ibm.com>
    Reviewed-by: Harsh Prateek Bora <harshpb@linux.ibm.com>
    Acked-by: Hyman Huang <yong.huang@smartx.com>
    Reviewed-by: Akihiko Odaki <akihiko.odaki@daynix.com>
    Message-Id: <20240102153529.486531-2-stefanha@redhat.com>

--- util/main-loop.c
+++ util/main-loop.c
@@ -302,13 +302,13 @@ static int os_host_main_loop_wait(int64_t timeout)
 
     glib_pollfds_fill(&timeout);
 
-    qemu_mutex_unlock_iothread();
+    bql_unlock();
     replay_mutex_unlock();
 
     ret = qemu_poll_ns((GPollFD *)gpollfds->data, gpollfds->len, timeout);
 
     replay_mutex_lock();
-    qemu_mutex_lock_iothread();
+    bql_lock();
 
     glib_pollfds_poll();
 
@@ -517,7 +517,7 @@ static int os_host_main_loop_wait(int64_t timeout)
 
     poll_timeout_ns = qemu_soonest_timeout(poll_timeout_ns, timeout);
 
-    qemu_mutex_unlock_iothread();
+    bql_unlock();
 
     replay_mutex_unlock();
 
@@ -525,7 +525,7 @@ static int os_host_main_loop_wait(int64_t timeout)
 
     replay_mutex_lock();
 
-    qemu_mutex_lock_iothread();
+    bql_lock();
     if (g_poll_ret > 0) {
         for (i = 0; i < w->num; i++) {
             w->revents[i] = poll_fds[n_poll_fds + i].revents;
