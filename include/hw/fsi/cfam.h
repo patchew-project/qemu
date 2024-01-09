9c04cb8fa3f3db0329f822e3e5d2ee25620534f4
Ninad Palsule <ninad@linux.ibm.com>
2024-01-09 16:23:26 -0600

    hw/fsi: Introduce IBM's cfam,fsi-slave,scratchpad
    This is a part of patchset where IBM's Flexible Service Interface is
    introduced.
    
    The Common FRU Access Macro (CFAM), an address space containing
    various "engines" that drive accesses on busses internal and external
    to the POWER chip. Examples include the SBEFIFO and I2C masters. The
    engines hang off of an internal Local Bus (LBUS) which is described
    by the CFAM configuration block.
    
    The FSI slave: The slave is the terminal point of the FSI bus for
    FSI symbols addressed to it. Slaves can be cascaded off of one
    another. The slave's configuration registers appear in address space
    of the CFAM to which it is attached.
    
    The scratchpad provides a set of non-functional registers. The firmware
    is free to use them, hardware does not support any special management
    support. The scratchpad registers can be read or written from LBUS
    slave. The scratch pad is managed under FSI CFAM state.
    
    [ clg: - moved object FSIScratchPad under FSICFAMState
           - moved FSIScratchPad code under cfam.c
           - introduced fsi_cfam_instance_init()
           - reworked fsi_cfam_realize() ]
    
    Signed-off-by: Andrew Jeffery <andrew@aj.id.au>
    Signed-off-by: Cédric Le Goater <clg@kaod.org>
    Signed-off-by: Ninad Palsule <ninad@linux.ibm.com>
    Message-Id: <20240109222333.1225031-4-ninad@linux.ibm.com>

---
+++ include/hw/fsi/cfam.h
@@ -0,0 +1,46 @@
+/*
+ * SPDX-License-Identifier: GPL-2.0-or-later
+ * Copyright (C) 2023 IBM Corp.
+ *
+ * IBM Common FRU Access Macro
+ */
+#ifndef FSI_CFAM_H
+#define FSI_CFAM_H
+
+#include "exec/memory.h"
+
+#include "hw/fsi/fsi-slave.h"
+#include "hw/fsi/lbus.h"
+
+
+#define TYPE_FSI_SCRATCHPAD "fsi.scratchpad"
+#define SCRATCHPAD(obj) OBJECT_CHECK(FSIScratchPad, (obj), TYPE_FSI_SCRATCHPAD)
+
+#define FSI_SCRATCHPAD_NR_REGS 4
+
+typedef struct FSIScratchPad {
+        FSILBusDevice parent;
+
+        uint32_t reg[FSI_SCRATCHPAD_NR_REGS];
+} FSIScratchPad;
+
+#define TYPE_FSI_CFAM "cfam"
+#define FSI_CFAM(obj) OBJECT_CHECK(FSICFAMState, (obj), TYPE_FSI_CFAM)
+
+/* P9-ism */
+#define CFAM_CONFIG_NR_REGS 0x28
+
+typedef struct FSICFAMState {
+    /* < private > */
+    FSISlaveState parent;
+
+    /* CFAM config address space */
+    MemoryRegion config_iomem;
+
+    MemoryRegion mr;
+
+    FSILBus lbus;
+    FSIScratchPad scratchpad;
+} FSICFAMState;
+
+#endif /* FSI_CFAM_H */
