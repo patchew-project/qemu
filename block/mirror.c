8920b2e1ed21e0f973fa6ad9f73581e2b53b6981
Alexander Ivanov <alexander.ivanov@virtuozzo.com>
2024-01-09 10:31:28 +0100

    blockcommit: Reopen base image as RO after abort
    If a blockcommit is aborted the base image remains in RW mode, that leads
    to a fail of subsequent live migration.
    
    How to reproduce:
      $ virsh snapshot-create-as vm snp1 --disk-only
    
      *** write something to the disk inside the guest ***
    
      $ virsh blockcommit vm vda --active --shallow && virsh blockjob vm vda --abort
      $ lsof /vzt/vm.qcow2
      COMMAND      PID USER   FD   TYPE DEVICE   SIZE/OFF NODE NAME
      qemu-syst 433203 root   45u   REG  253,0 1724776448  133 /vzt/vm.qcow2
      $ cat /proc/433203/fdinfo/45
      pos:    0
      flags:  02140002 <==== The last 2 means RW mode
    
    Reopen the base image in RO mode in mirror_exit_common() if the blockjob
    is aborted and the base image was opened in RO mode before the blockcommit.
    
    Signed-off-by: Alexander Ivanov <alexander.ivanov@virtuozzo.com>
    Message-Id: <20240109093128.157460-1-alexander.ivanov@virtuozzo.com>

--- block/mirror.c
+++ block/mirror.c
@@ -93,6 +93,7 @@ typedef struct MirrorBlockJob {
     int64_t active_write_bytes_in_flight;
     bool prepared;
     bool in_drain;
+    bool base_ro;
 } MirrorBlockJob;
 
 typedef struct MirrorBDSOpaque {
@@ -792,6 +793,10 @@ static int mirror_exit_common(Job *job)
 
     bdrv_drained_end(target_bs);
     bdrv_unref(target_bs);
+
+    if (abort && s->base_ro && !bdrv_is_read_only(target_bs)) {
+        bdrv_reopen_set_read_only(target_bs, true, NULL);
+    }
 
     bs_opaque->job = NULL;
 
@@ -1715,6 +1720,7 @@ static BlockJob *mirror_start_job(
                              bool is_none_mode, BlockDriverState *base,
                              bool auto_complete, const char *filter_node_name,
                              bool is_mirror, MirrorCopyMode copy_mode,
+                             bool base_ro,
                              Error **errp)
 {
     MirrorBlockJob *s;
@@ -1798,6 +1804,7 @@ static BlockJob *mirror_start_job(
     bdrv_unref(mirror_top_bs);
 
     s->mirror_top_bs = mirror_top_bs;
+    s->base_ro = base_ro;
 
     /* No resize for the target either; while the mirror is still running, a
      * consistent read isn't necessarily possible. We could possibly allow
@@ -2027,7 +2034,7 @@ void mirror_start(const char *job_id, BlockDriverState *bs,
                      speed, granularity, buf_size, backing_mode, zero_target,
                      on_source_error, on_target_error, unmap, NULL, NULL,
                      &mirror_job_driver, is_none_mode, base, false,
-                     filter_node_name, true, copy_mode, errp);
+                     filter_node_name, true, copy_mode, false, errp);
 }
 
 BlockJob *commit_active_start(const char *job_id, BlockDriverState *bs,
@@ -2056,7 +2063,7 @@ BlockJob *commit_active_start(const char *job_id, BlockDriverState *bs,
                      on_error, on_error, true, cb, opaque,
                      &commit_active_job_driver, false, base, auto_complete,
                      filter_node_name, false, MIRROR_COPY_MODE_BACKGROUND,
-                     errp);
+                     base_read_only, errp);
     if (!job) {
         goto error_restore_flags;
     }
