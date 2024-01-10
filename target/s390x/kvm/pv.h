d1c2492f3b80dcaa890b58f05b958c70b1c2a6a2
Thomas Huth <thuth@redhat.com>
2024-01-10 15:29:16 +0100

    target/s390x/kvm/pv: Provide some more useful information if decryption fails
    It's a common scenario to copy guest images from one host to another
    to run the guest on the other machine. This (of course) does not work
    with "secure exection" guests since they are encrypted with one certain
    host key. However, if you still (accidentally) do it, you only get a
    very user-unfriendly error message that looks like this:
    
     qemu-system-s390x: KVM PV command 2 (KVM_PV_SET_SEC_PARMS) failed:
      header rc 108 rrc 5 IOCTL rc: -22
    
    Let's provide at least a somewhat nicer hint to the users so that they
    are able to figure out what might have gone wrong.
    
    Buglink: https://issues.redhat.com/browse/RHEL-18212
    Signed-off-by: Thomas Huth <thuth@redhat.com>
    Reviewed-by: Claudio Imbrenda <imbrenda@linux.ibm.com>
    Reviewed-by: Cédric Le Goater <clg@redhat.com>
    Reviewed-by: Philippe Mathieu-Daudé <philmd@linaro.org>
    Message-Id: <20240110142916.850605-1-thuth@redhat.com>

--- target/s390x/kvm/pv.h
+++ target/s390x/kvm/pv.h
@@ -42,7 +42,7 @@ int s390_pv_query_info(void);
 int s390_pv_vm_enable(void);
 void s390_pv_vm_disable(void);
 bool s390_pv_vm_try_disable_async(S390CcwMachineState *ms);
-int s390_pv_set_sec_parms(uint64_t origin, uint64_t length);
+int s390_pv_set_sec_parms(uint64_t origin, uint64_t length, Error **errp);
 int s390_pv_unpack(uint64_t addr, uint64_t size, uint64_t tweak);
 void s390_pv_prep_reset(void);
 int s390_pv_verify(void);
