29b4da2f0f77449012465ebce9bac4977542a070
Pierrick Bouvier <pierrick.bouvier@linaro.org>
2024-01-11 18:23:17 +0400

    tests/plugin/inline: migrate to new per_vcpu API
    Signed-off-by: Pierrick Bouvier <pierrick.bouvier@linaro.org>
    Message-Id: <20240111142326.1743444-5-pierrick.bouvier@linaro.org>

--- tests/plugin/inline.c
+++ tests/plugin/inline.c
@@ -18,15 +18,12 @@
 static uint64_t count_tb;
 static uint64_t count_tb_per_vcpu[MAX_CPUS];
 static uint64_t count_tb_inline_per_vcpu[MAX_CPUS];
-static uint64_t count_tb_inline_racy;
 static uint64_t count_insn;
 static uint64_t count_insn_per_vcpu[MAX_CPUS];
 static uint64_t count_insn_inline_per_vcpu[MAX_CPUS];
-static uint64_t count_insn_inline_racy;
 static uint64_t count_mem;
 static uint64_t count_mem_per_vcpu[MAX_CPUS];
 static uint64_t count_mem_inline_per_vcpu[MAX_CPUS];
-static uint64_t count_mem_inline_racy;
 static GMutex tb_lock;
 static GMutex insn_lock;
 static GMutex mem_lock;
@@ -50,11 +47,9 @@ static void stats_insn(void)
     printf("insn: %" PRIu64 "\n", expected);
     printf("insn: %" PRIu64 " (per vcpu)\n", per_vcpu);
     printf("insn: %" PRIu64 " (per vcpu inline)\n", inl_per_vcpu);
-    printf("insn: %" PRIu64 " (inline racy)\n", count_insn_inline_racy);
     g_assert(expected > 0);
     g_assert(per_vcpu == expected);
     g_assert(inl_per_vcpu == expected);
-    g_assert(count_insn_inline_racy <= expected);
 }
 
 static void stats_tb(void)
@@ -65,11 +60,9 @@ static void stats_tb(void)
     printf("tb: %" PRIu64 "\n", expected);
     printf("tb: %" PRIu64 " (per vcpu)\n", per_vcpu);
     printf("tb: %" PRIu64 " (per vcpu inline)\n", inl_per_vcpu);
-    printf("tb: %" PRIu64 " (inline racy)\n", count_tb_inline_racy);
     g_assert(expected > 0);
     g_assert(per_vcpu == expected);
     g_assert(inl_per_vcpu == expected);
-    g_assert(count_tb_inline_racy <= expected);
 }
 
 static void stats_mem(void)
@@ -80,11 +73,9 @@ static void stats_mem(void)
     printf("mem: %" PRIu64 "\n", expected);
     printf("mem: %" PRIu64 " (per vcpu)\n", per_vcpu);
     printf("mem: %" PRIu64 " (per vcpu inline)\n", inl_per_vcpu);
-    printf("mem: %" PRIu64 " (inline racy)\n", count_mem_inline_racy);
     g_assert(expected > 0);
     g_assert(per_vcpu == expected);
     g_assert(inl_per_vcpu == expected);
-    g_assert(count_mem_inline_racy <= expected);
 }
 
 static void plugin_exit(qemu_plugin_id_t id, void *udata)
@@ -142,8 +133,6 @@ static void vcpu_tb_trans(qemu_plugin_id_t id, struct qemu_plugin_tb *tb)
 {
     qemu_plugin_register_vcpu_tb_exec_cb(tb, vcpu_tb_exec,
                                          QEMU_PLUGIN_CB_NO_REGS, 0);
-    qemu_plugin_register_vcpu_tb_exec_inline(tb, QEMU_PLUGIN_INLINE_ADD_U64,
-                                             &count_tb_inline_racy, 1);
     qemu_plugin_register_vcpu_tb_exec_inline_per_vcpu(
         tb, QEMU_PLUGIN_INLINE_ADD_U64,
         count_tb_inline_per_vcpu, sizeof(uint64_t), 1);
@@ -152,18 +141,12 @@ static void vcpu_tb_trans(qemu_plugin_id_t id, struct qemu_plugin_tb *tb)
         struct qemu_plugin_insn *insn = qemu_plugin_tb_get_insn(tb, idx);
         qemu_plugin_register_vcpu_insn_exec_cb(insn, vcpu_insn_exec,
                                                QEMU_PLUGIN_CB_NO_REGS, 0);
-        qemu_plugin_register_vcpu_insn_exec_inline(
-            insn, QEMU_PLUGIN_INLINE_ADD_U64,
-            &count_insn_inline_racy, 1);
         qemu_plugin_register_vcpu_insn_exec_inline_per_vcpu(
             insn, QEMU_PLUGIN_INLINE_ADD_U64,
             count_insn_inline_per_vcpu, sizeof(uint64_t), 1);
         qemu_plugin_register_vcpu_mem_cb(insn, &vcpu_mem_access,
                                          QEMU_PLUGIN_CB_NO_REGS,
                                          QEMU_PLUGIN_MEM_RW, 0);
-        qemu_plugin_register_vcpu_mem_inline(insn, QEMU_PLUGIN_MEM_RW,
-                                             QEMU_PLUGIN_INLINE_ADD_U64,
-                                             &count_mem_inline_racy, 1);
         qemu_plugin_register_vcpu_mem_inline_per_vcpu(
             insn, QEMU_PLUGIN_MEM_RW,
             QEMU_PLUGIN_INLINE_ADD_U64,
