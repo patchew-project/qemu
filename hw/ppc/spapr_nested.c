cb11f87f3c3e907752ff57f572c57f881dc3b052
Harsh Prateek Bora <harshpb@linux.ibm.com>
2024-01-18 10:54:25 +0530

    spapr: nested: move nested part of spapr_get_pate into spapr_nested.c
    Most of the nested code has already been moved to spapr_nested.c
    This logic inside spapr_get_pate is related to nested guests and
    better suited for spapr_nested.c, hence moving there.
    
    Signed-off-by: Harsh Prateek Bora <harshpb@linux.ibm.com>
    Reviewed-by: Nicholas Piggin <npiggin@gmail.com>
    Message-Id: <20240118052438.1475437-3-harshpb@linux.ibm.com>

--- hw/ppc/spapr_nested.c
+++ hw/ppc/spapr_nested.c
@@ -6,8 +6,38 @@
 #include "hw/ppc/spapr.h"
 #include "hw/ppc/spapr_cpu_core.h"
 #include "hw/ppc/spapr_nested.h"
+#include "mmu-book3s-v3.h"
 
 #ifdef CONFIG_TCG
+
+bool spapr_get_pate_nested_hv(SpaprMachineState *spapr, PowerPCCPU *cpu,
+                              target_ulong lpid, ppc_v3_pate_t *entry)
+{
+    uint64_t patb, pats;
+
+    assert(lpid != 0);
+
+    patb = spapr->nested_ptcr & PTCR_PATB;
+    pats = spapr->nested_ptcr & PTCR_PATS;
+
+    /* Check if partition table is properly aligned */
+    if (patb & MAKE_64BIT_MASK(0, pats + 12)) {
+        return false;
+    }
+
+    /* Calculate number of entries */
+    pats = 1ull << (pats + 12 - 4);
+    if (pats <= lpid) {
+        return false;
+    }
+
+    /* Grab entry */
+    patb += 16 * lpid;
+    entry->dw0 = ldq_phys(CPU(cpu)->as, patb);
+    entry->dw1 = ldq_phys(CPU(cpu)->as, patb + 8);
+    return true;
+}
+
 #define PRTS_MASK      0x1f
 
 static target_ulong h_set_ptbl(PowerPCCPU *cpu,
@@ -391,5 +421,11 @@ void spapr_exit_nested(PowerPCCPU *cpu, int excp)
 void spapr_register_nested_hv(void)
 {
     /* DO NOTHING */
+}
+
+bool spapr_get_pate_nested_hv(SpaprMachineState *spapr, PowerPCCPU *cpu,
+                              target_ulong lpid, ppc_v3_pate_t *entry)
+{
+    return false;
 }
 #endif
