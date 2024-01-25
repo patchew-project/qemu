82927fbe437ca2eb58071c92bbc6d50c291fc30a
Andrew Jones <ajones@ventanamicro.com>
2024-01-25 16:53:19 -0300

    target/riscv: Promote svade to a normal extension
    Named features are extensions which don't make sense for users to
    control and are therefore not exposed on the command line. However,
    svade is an extension which makes sense for users to control, so treat
    it like a "normal" extension. The default is false, since QEMU has
    always implemented hardware A/D PTE bit updating, so users must opt into
    svade (or get it from a CPU type which enables it by default).
    
    Signed-off-by: Andrew Jones <ajones@ventanamicro.com>
    Reviewed-by: Daniel Henrique Barboza <dbarboza@ventanamicro.com>
    Message-Id: <20240125195319.329181-7-dbarboza@ventanamicro.com>

--- target/riscv/cpu.c
+++ target/riscv/cpu.c
@@ -1367,6 +1367,7 @@ const RISCVCPUMultiExtConfig riscv_cpu_extensions[] = {
 
     MULTI_EXT_CFG_BOOL("smepmp", ext_smepmp, false),
     MULTI_EXT_CFG_BOOL("smstateen", ext_smstateen, false),
+    MULTI_EXT_CFG_BOOL("svade", ext_svade, false),
     MULTI_EXT_CFG_BOOL("svadu", ext_svadu, true),
     MULTI_EXT_CFG_BOOL("svinval", ext_svinval, false),
     MULTI_EXT_CFG_BOOL("svnapot", ext_svnapot, false),
@@ -1474,7 +1475,6 @@ const RISCVCPUMultiExtConfig riscv_cpu_experimental_exts[] = {
  * and priv_ver like regular extensions.
  */
 const RISCVCPUMultiExtConfig riscv_cpu_named_features[] = {
-    MULTI_EXT_CFG_BOOL("svade", ext_svade, true),
     MULTI_EXT_CFG_BOOL("zic64b", ext_zic64b, true),
 
     /*
@@ -1606,8 +1606,6 @@ static RISCVCPUProfile RVA22U64 = {
  * Other named features that we already implement: Sstvecd, Sstvala,
  * Sscounterenw
  *
- * Named features that we need to enable: svade
- *
  * The remaining features/extensions comes from RVA22U64.
  */
 static RISCVCPUProfile RVA22S64 = {
@@ -1619,11 +1617,11 @@ static RISCVCPUProfile RVA22S64 = {
     .ext_offsets = {
         /* rva22s64 exts */
         CPU_CFG_OFFSET(ext_zifencei), CPU_CFG_OFFSET(ext_svpbmt),
-        CPU_CFG_OFFSET(ext_svinval),
+        CPU_CFG_OFFSET(ext_svinval), CPU_CFG_OFFSET(ext_svade),
 
         /* rva22s64 named features */
         CPU_CFG_OFFSET(ext_sstvecd), CPU_CFG_OFFSET(ext_sstvala),
-        CPU_CFG_OFFSET(ext_sscounterenw), CPU_CFG_OFFSET(ext_svade),
+        CPU_CFG_OFFSET(ext_sscounterenw),
 
         RISCV_PROFILE_EXT_LIST_END
     }
