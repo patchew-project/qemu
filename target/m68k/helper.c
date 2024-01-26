784b4a50637bf9e875286c76a1b7b50ad54a9781
Philippe Mathieu-Daudé <philmd@linaro.org>
2024-01-26 23:03:53 +0100

    target/m68k: Prefer fast cpu_env() over slower CPU QOM cast macro
    Mechanical patch produced running the command documented
    in scripts/coccinelle/cpu_env.cocci_template header.
    
    Signed-off-by: Philippe Mathieu-Daudé <philmd@linaro.org>
    Reviewed-by: Richard Henderson <richard.henderson@linaro.org>
    Reviewed-by: Thomas Huth <thuth@redhat.com>
    Message-Id: <20240126220407.95022-12-philmd@linaro.org>

--- target/m68k/helper.c
+++ target/m68k/helper.c
@@ -894,8 +894,7 @@ txfail:
 
 hwaddr m68k_cpu_get_phys_page_debug(CPUState *cs, vaddr addr)
 {
-    M68kCPU *cpu = M68K_CPU(cs);
-    CPUM68KState *env = &cpu->env;
+    CPUM68KState *env = cpu_env(cs);
     hwaddr phys_addr;
     int prot;
     int access_type;
