747898d827f97c79d392c9dfb60314e638a12618
Zhenzhong Duan <zhenzhong.duan@intel.com>
2024-02-01 15:28:07 +0800

    vfio/container: Implement host_iommu_device_init callback in legacy mode
    This callback will be used to initialize base and public elements
    in IOMMULegacyDevice.
    
    Signed-off-by: Zhenzhong Duan <zhenzhong.duan@intel.com>
    Message-Id: <20240201072818.327930-8-zhenzhong.duan@intel.com>

--- hw/vfio/container.c
+++ hw/vfio/container.c
@@ -1120,6 +1120,12 @@ out_single:
     return ret;
 }
 
+static void vfio_legacy_host_iommu_device_init(VFIODevice *vbasedev)
+{
+    host_iommu_base_device_init(&vbasedev->base_hdev, HID_LEGACY,
+                                sizeof(IOMMULegacyDevice));
+}
+
 static void vfio_iommu_legacy_class_init(ObjectClass *klass, void *data)
 {
     VFIOIOMMUClass *vioc = VFIO_IOMMU_CLASS(klass);
@@ -1132,6 +1138,7 @@ static void vfio_iommu_legacy_class_init(ObjectClass *klass, void *data)
     vioc->set_dirty_page_tracking = vfio_legacy_set_dirty_page_tracking;
     vioc->query_dirty_bitmap = vfio_legacy_query_dirty_bitmap;
     vioc->pci_hot_reset = vfio_legacy_pci_hot_reset;
+    vioc->host_iommu_device_init = vfio_legacy_host_iommu_device_init;
 };
 
 static const TypeInfo types[] = {
