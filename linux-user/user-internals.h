ef206aee6ba00b38692250082784d1a118aab739
Ilya Leoshkevich <iii@linux.ibm.com>
2024-02-05 12:22:18 +0100

    {linux,bsd}-user: Pass pid to fork_end()
    The upcoming follow-fork-mode child support requires knowing the child
    pid. Pass it down.
    
    Signed-off-by: Ilya Leoshkevich <iii@linux.ibm.com>
    Message-Id: <20240205112251.121569-5-iii@linux.ibm.com>

--- linux-user/user-internals.h
+++ linux-user/user-internals.h
@@ -71,7 +71,7 @@ const char *target_strerror(int err);
 int get_osversion(void);
 void init_qemu_uname_release(void);
 void fork_start(void);
-void fork_end(int child);
+void fork_end(abi_long pid);
 
 /**
  * probe_guest_base:
