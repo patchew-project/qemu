46dd5928a5969ca69f508858c74f5fc00752a9f2
Mark Cave-Ayland <mark.cave-ayland@ilande.co.uk>
2024-02-16 21:06:23 +0000

    esp.c: replace cmdfifo use of esp_fifo_pop() in do_message_phase()
    Signed-off-by: Mark Cave-Ayland <mark.cave-ayland@ilande.co.uk>
    Message-Id: <20240216210633.1376066-4-mark.cave-ayland@ilande.co.uk>

--- hw/scsi/esp.c
+++ hw/scsi/esp.c
@@ -312,7 +312,8 @@ static void do_message_phase(ESPState *s)
     uint32_t n;
 
     if (s->cmdfifo_cdb_offset) {
-        uint8_t message = esp_fifo_pop(&s->cmdfifo);
+        uint8_t message = fifo8_is_empty(&s->cmdfifo) ? 0 :
+                          fifo8_pop(&s->cmdfifo);
 
         trace_esp_do_identify(message);
         s->lun = message & 7;
