acc773144f15498cb54a5695090ab98857e99ca4
Dmitriy Sharikhin <d.sharikhin@yadro.com>
2024-03-01 07:36:42 +0000

    hw: gpio: introduce pcf8574 driver
    NXP PCF8574 and compatible ICs are simple I2C GPIO expanders.
    PCF8574 incorporates quasi-bidirectional IO, and simple
    communication protocol, when IO read is I2C byte read, and
    IO write is I2C byte write. User can think of it as
    open-drain port, when line high state is input and line low
    state is output.
    
    This patch allow to instantiate virtual I2C device called
    "pcf8574" in machine init code via generic mechanism.
    
    Signed-off-by: Dmitrii Sharikhin <d.sharikhin@yadro.com>
    Message-Id: <ee386866ca7ba80add0c428b59efb936fd7ce797.camel@yadro.com>

---
+++ include/hw/gpio/pcf8574.h
@@ -0,0 +1,15 @@
+/*
+ * NXP PCF8574 8-port I2C GPIO expansion chip.
+ *
+ * Copyright (c) 2024 KNS Group (YADRO).
+ * Written by Dmitrii Sharikhin <d.sharikhin@yadro.com>
+ *
+ * This file is licensed under GNU GPL.
+ */
+
+#ifndef _HW_GPIO_PCF8574
+#define _HW_GPIO_PCF8574
+
+#define TYPE_PCF8574 "pcf8574"
+
+#endif /* _HW_GPIO_PCF8574 */
