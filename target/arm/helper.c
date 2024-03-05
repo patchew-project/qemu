f2ce0cf8f49fd6c0d1aa431a00e2b83949ec7867
Jinjie Ruan via <qemu-devel@nongnu.org>
2024-03-05 07:03:20 +0000

    target/arm: Handle NMI in arm_cpu_do_interrupt_aarch64()
    According to Arm GIC section 4.6.3 Interrupt superpriority, the interrupt
    with superpriority is always IRQ, never FIQ, so the NMI exception trap entry
    behave like IRQ. And VNMI(vIRQ with Superpriority) can be raised from the
    GIC or come from the hcrx_el2.HCRX_VINMI bit.
    
    Signed-off-by: Jinjie Ruan <ruanjinjie@huawei.com>
    Reviewed-by: Richard Henderson <richard.henderson@linaro.org>
    Message-Id: <20240305070331.2151131-13-ruanjinjie@huawei.com>

--- target/arm/helper.c
+++ target/arm/helper.c
@@ -11460,6 +11460,8 @@ static void arm_cpu_do_interrupt_aarch64(CPUState *cs)
         break;
     case EXCP_IRQ:
     case EXCP_VIRQ:
+    case EXCP_NMI:
+    case EXCP_VNMI:
         addr += 0x80;
         break;
     case EXCP_FIQ:
