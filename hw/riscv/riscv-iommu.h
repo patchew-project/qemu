eb9da193712e8eb5788f7779402039e817c20bc2
Tomasz Jeznach <tjeznach@rivosinc.com>
2024-03-07 13:03:12 -0300

    hw/riscv/riscv-iommu: add s-stage and g-stage support
    Add support for s-stage (sv32, sv39, sv48, sv57 caps) and g-stage
    (sv32x4, sv39x4, sv48x4, sv57x4 caps). Most of the work is done in the
    riscv_iommu_spa_fetch() function that now has to consider how many
    translation stages we need to walk the page table.
    
    Signed-off-by: Tomasz Jeznach <tjeznach@rivosinc.com>
    Signed-off-by: Daniel Henrique Barboza <dbarboza@ventanamicro.com>
    Message-Id: <20240307160319.675044-10-dbarboza@ventanamicro.com>

--- hw/riscv/riscv-iommu.h
+++ hw/riscv/riscv-iommu.h
@@ -38,6 +38,8 @@ struct RISCVIOMMUState {
 
     bool enable_off;      /* Enable out-of-reset OFF mode (DMA disabled) */
     bool enable_msi;      /* Enable MSI remapping */
+    bool enable_s_stage;  /* Enable S/VS-Stage translation */
+    bool enable_g_stage;  /* Enable G-Stage translation */
 
     /* IOMMU Internal State */
     uint64_t ddtp;        /* Validated Device Directory Tree Root Pointer */
