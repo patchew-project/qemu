b76ec6a9e450416297408ca9fcdea98a1dda2adf
Alexander Ivanov <alexander.ivanov@virtuozzo.com>
2024-03-11 19:18:44 +0100

    parallels: Make a loaded dirty bitmap persistent
    After bitmap loading the bitmap is not persistent and is removed on image
    saving. Set bitmap persistence to true.
    
    Signed-off-by: Alexander Ivanov <alexander.ivanov@virtuozzo.com>
    Reviewed-by: Denis V. Lunev <den@openvz.org>
    Message-Id: <20240311181850.73013-17-alexander.ivanov@virtuozzo.com>

--- block/parallels-ext.c
+++ block/parallels-ext.c
@@ -278,6 +278,7 @@ parallels_parse_format_extension(BlockDriverState *bs, int64_t ext_off,
             if (!bitmap) {
                 goto fail;
             }
+            bdrv_dirty_bitmap_set_persistence(bitmap, true);
             bitmaps = g_slist_append(bitmaps, bitmap);
             break;
 
