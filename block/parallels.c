6551ca77bd0eabf1ff6df43253fedf4b7905e053
Alexander Ivanov <alexander.ivanov@virtuozzo.com>
2024-03-11 19:18:35 +0100

    parallels: Set data_end value in parallels_check_leak()
    In parallels_check_leak() we change file size but don't correct data_end
    field of BDRVParallelsState structure. Fix it.
    
    Signed-off-by: Alexander Ivanov <alexander.ivanov@virtuozzo.com>
    Reviewed-by: Denis V. Lunev <den@openvz.org>
    Message-Id: <20240311181850.73013-8-alexander.ivanov@virtuozzo.com>

--- block/parallels.c
+++ block/parallels.c
@@ -811,6 +811,7 @@ parallels_check_leak(BlockDriverState *bs, BdrvCheckResult *res,
                 res->check_errors++;
                 return ret;
             }
+            s->data_end = res->image_end_offset >> BDRV_SECTOR_BITS;
             if (explicit) {
                 res->leaks_fixed += count;
             }
