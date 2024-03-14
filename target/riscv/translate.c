e28a92924b32cb0da5b872976ba2fac8450a1d22
Huang Tao <eric.huang@linux.alibaba.com>
2024-03-14 17:21:58 +0800

    target/riscv: Implement dynamic establishment of custom decoder
    In this patch, we modify the decoder to be a freely composable data
    structure instead of a hardcoded one. It can be dynamically builded up
    according to the extensions.
    This approach has several benefits:
    1. Provides support for heterogeneous cpu architectures. As we add decoder in
       RISCVCPU, each cpu can have their own decoder, and the decoders can be
       different due to cpu's features.
    2. Improve the decoding efficiency. We run the guard_func to see if the decoder
       can be added to the dynamic_decoder when building up the decoder. Therefore,
       there is no need to run the guard_func when decoding each instruction. It can
       improve the decoding efficiency
    3. For vendor or dynamic cpus, it allows them to customize their own decoder
       functions to improve decoding efficiency, especially when vendor-defined
       instruction sets increase. Because of dynamic building up, it can skip the other
       decoder guard functions when decoding.
    4. Pre patch for allowing adding a vendor decoder before decode_insn32() with minimal
       overhead for users that don't need this particular vendor decoder.
    
    Signed-off-by: Huang Tao <eric.huang@linux.alibaba.com>
    Suggested-by: Christoph Muellner <christoph.muellner@vrull.eu>
    Co-authored-by: LIU Zhiwei <zhiwei_liu@linux.alibaba.com>
    Reviewed-by: Richard Henderson <richard.henderson@linaro.org>
    Message-Id: <20240314092158.65866-1-eric.huang@linux.alibaba.com>

--- target/riscv/translate.c
+++ target/riscv/translate.c
@@ -37,6 +37,8 @@
 #include "exec/helper-info.c.inc"
 #undef  HELPER_H
 
+#include "tcg/tcg-cpu.h"
+
 /* global register indices */
 static TCGv cpu_gpr[32], cpu_gprh[32], cpu_pc, cpu_vl, cpu_vstart;
 static TCGv_i64 cpu_fpr[32]; /* assume F and D extensions */
@@ -117,6 +119,7 @@ typedef struct DisasContext {
     bool frm_valid;
     /* TCG of the current insn_start */
     TCGOp *insn_start;
+    const GPtrArray *decoders;
 } DisasContext;
 
 static inline bool has_ext(DisasContext *ctx, uint32_t ext)
@@ -1120,21 +1123,16 @@ static inline int insn_len(uint16_t first_word)
     return (first_word & 3) == 3 ? 4 : 2;
 }
 
+const RISCVDecoder decoder_table[] = {
+    { always_true_p, decode_insn32 },
+    { has_xthead_p, decode_xthead},
+    { has_XVentanaCondOps_p, decode_XVentanaCodeOps},
+};
+
+const size_t decoder_table_size = ARRAY_SIZE(decoder_table);
+
 static void decode_opc(CPURISCVState *env, DisasContext *ctx, uint16_t opcode)
 {
-    /*
-     * A table with predicate (i.e., guard) functions and decoder functions
-     * that are tested in-order until a decoder matches onto the opcode.
-     */
-    static const struct {
-        bool (*guard_func)(const RISCVCPUConfig *);
-        bool (*decode_func)(DisasContext *, uint32_t);
-    } decoders[] = {
-        { always_true_p,  decode_insn32 },
-        { has_xthead_p, decode_xthead },
-        { has_XVentanaCondOps_p,  decode_XVentanaCodeOps },
-    };
-
     ctx->virt_inst_excp = false;
     ctx->cur_insn_len = insn_len(opcode);
     /* Check for compressed insn */
@@ -1155,9 +1153,9 @@ static void decode_opc(CPURISCVState *env, DisasContext *ctx, uint16_t opcode)
                                              ctx->base.pc_next + 2));
         ctx->opcode = opcode32;
 
-        for (size_t i = 0; i < ARRAY_SIZE(decoders); ++i) {
-            if (decoders[i].guard_func(ctx->cfg_ptr) &&
-                decoders[i].decode_func(ctx, opcode32)) {
+        for (guint i = 0; i < ctx->decoders->len; ++i) {
+            riscv_cpu_decode_fn func = g_ptr_array_index(ctx->decoders, i);
+            if (func(ctx, opcode32)) {
                 return;
             }
         }
@@ -1202,6 +1200,7 @@ static void riscv_tr_init_disas_context(DisasContextBase *dcbase, CPUState *cs)
     ctx->itrigger = FIELD_EX32(tb_flags, TB_FLAGS, ITRIGGER);
     ctx->zero = tcg_constant_tl(0);
     ctx->virt_inst_excp = false;
+    ctx->decoders = cpu->decoders;
 }
 
 static void riscv_tr_tb_start(DisasContextBase *db, CPUState *cpu)
