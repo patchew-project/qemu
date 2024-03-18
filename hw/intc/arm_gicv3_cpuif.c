062c47359d0f5a5030fd45945a47bc7573e8b289
Jinjie Ruan via <qemu-devel@nongnu.org>
2024-03-18 09:35:42 +0000

    hw/intc/arm_gicv3: Report the NMI interrupt in gicv3_cpuif_update()
    In CPU Interface, if the IRQ has the superpriority property, report
    NMI to the corresponding PE.
    
    Signed-off-by: Jinjie Ruan <ruanjinjie@huawei.com>
    Reviewed-by: Richard Henderson <richard.henderson@linaro.org>
    Message-Id: <20240318093546.2786144-21-ruanjinjie@huawei.com>

--- hw/intc/arm_gicv3_cpuif.c
+++ hw/intc/arm_gicv3_cpuif.c
@@ -969,6 +969,7 @@ void gicv3_cpuif_update(GICv3CPUState *cs)
     /* Tell the CPU about its highest priority pending interrupt */
     int irqlevel = 0;
     int fiqlevel = 0;
+    int nmilevel = 0;
     ARMCPU *cpu = ARM_CPU(cs->cpu);
     CPUARMState *env = &cpu->env;
 
@@ -1007,6 +1008,8 @@ void gicv3_cpuif_update(GICv3CPUState *cs)
 
         if (isfiq) {
             fiqlevel = 1;
+        } else if (cs->hppi.superprio) {
+            nmilevel = 1;
         } else {
             irqlevel = 1;
         }
@@ -1016,6 +1019,7 @@ void gicv3_cpuif_update(GICv3CPUState *cs)
 
     qemu_set_irq(cs->parent_fiq, fiqlevel);
     qemu_set_irq(cs->parent_irq, irqlevel);
+    qemu_set_irq(cs->parent_nmi, nmilevel);
 }
 
 static uint64_t icc_pmr_read(CPUARMState *env, const ARMCPRegInfo *ri)
