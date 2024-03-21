1d4baac97ff531eb6adf97831bbd4658e64e6d79
Philippe Mathieu-Daudé <philmd@linaro.org>
2024-03-21 16:48:28 +0100

    target/nios2: Replace qemu_printf() by monitor_printf() in monitor
    Signed-off-by: Philippe Mathieu-Daudé <philmd@linaro.org>
    Message-Id: <20240321154838.95771-13-philmd@linaro.org>

--- target/nios2/monitor.c
+++ target/nios2/monitor.c
@@ -22,7 +22,6 @@
  * THE SOFTWARE.
  */
 #include "qemu/osdep.h"
-#include "qemu/qemu-print.h"
 #include "cpu.h"
 #include "monitor/monitor.h"
 #include "monitor/hmp-target.h"
@@ -32,24 +31,24 @@ void nios2_info_mmu(Monitor *mon, CPUNios2State *env)
 {
     Nios2CPU *cpu = env_archcpu(env);
 
-    qemu_printf("MMU: ways %d, entries %d, pid bits %d\n",
-                cpu->tlb_num_ways, cpu->tlb_num_entries,
-                cpu->pid_num_bits);
+    monitor_printf(mon, "MMU: ways %d, entries %d, pid bits %d\n",
+                   cpu->tlb_num_ways, cpu->tlb_num_entries,
+                   cpu->pid_num_bits);
 
     for (int i = 0; i < cpu->tlb_num_entries; i++) {
         Nios2TLBEntry *entry = &env->mmu.tlb[i];
-        qemu_printf("TLB[%d] = %08X %08X %c VPN %05X "
-                    "PID %02X %c PFN %05X %c%c%c%c\n",
-                    i, entry->tag, entry->data,
-                    (entry->tag & (1 << 10)) ? 'V' : '-',
-                    entry->tag >> 12,
-                    entry->tag & ((1 << cpu->pid_num_bits) - 1),
-                    (entry->tag & (1 << 11)) ? 'G' : '-',
-                    FIELD_EX32(entry->data, CR_TLBACC, PFN),
-                    (entry->data & CR_TLBACC_C) ? 'C' : '-',
-                    (entry->data & CR_TLBACC_R) ? 'R' : '-',
-                    (entry->data & CR_TLBACC_W) ? 'W' : '-',
-                    (entry->data & CR_TLBACC_X) ? 'X' : '-');
+        monitor_printf(mon, "TLB[%d] = %08X %08X %c VPN %05X "
+                       "PID %02X %c PFN %05X %c%c%c%c\n",
+                       i, entry->tag, entry->data,
+                       (entry->tag & (1 << 10)) ? 'V' : '-',
+                       entry->tag >> 12,
+                       entry->tag & ((1 << cpu->pid_num_bits) - 1),
+                       (entry->tag & (1 << 11)) ? 'G' : '-',
+                       FIELD_EX32(entry->data, CR_TLBACC, PFN),
+                       (entry->data & CR_TLBACC_C) ? 'C' : '-',
+                       (entry->data & CR_TLBACC_R) ? 'R' : '-',
+                       (entry->data & CR_TLBACC_W) ? 'W' : '-',
+                       (entry->data & CR_TLBACC_X) ? 'X' : '-');
     }
 }
 
