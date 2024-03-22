1772e0d1a278adae6d2d1247d6c21935482d9007
Aidan Leuck <aidan_leuck@selinc.com>
2024-03-22 11:46:36 -0600

    Refactor common functions between POSIX and Windows implementation
    Signed-off-by: Aidan Leuck <aidan_leuck@selinc.com>
    Message-Id: <20240322174637.499113-2-aidan_leuck@selinc.com>

---
+++ qga/commands-ssh-core.h
@@ -0,0 +1,8 @@
+/*
+ * This work is licensed under the terms of the GNU GPL, version 2 or later.
+ * See the COPYING file in the top-level directory.
+ */
+
+GStrv read_authkeys(const char *path, Error **errp);
+bool check_openssh_pub_keys(strList *keys, size_t *nkeys, Error **errp);
+bool check_openssh_pub_key(const char *key, Error **errp);
