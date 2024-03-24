a6d54bdeb7f9b16ee41b6f64285c22c5a63a4535
Arnaud Minier <arnaud.minier@telecom-paris.fr>
2024-03-24 17:55:41 +0100

    hw/misc/stm32l4x5_rcc: Propagate period when enabling a clock
    The "clock_set_mul_div" function doesn't propagate the clock period to
    the children if it is changed (e.g. by enabling/disabling
    a clock multiplexer).
    This was overlooked during the implementation due to late changes.
    
    This commit propagates the change if the multiplier or divider changes.
    
    The usart tests will ensure that this behavior will not regress.
    
    Signed-off-by: Arnaud Minier <arnaud.minier@telecom-paris.fr>
    Signed-off-by: Inès Varhol <ines.varhol@telecom-paris.fr>
    Message-Id: <20240324165545.201908-2-arnaud.minier@telecom-paris.fr>

--- hw/misc/stm32l4x5_rcc.c
+++ hw/misc/stm32l4x5_rcc.c
@@ -59,7 +59,12 @@ static void clock_mux_update(RccClockMuxState *mux, bool bypass_source)
         freq_multiplier = mux->divider;
     }
 
-    clock_set_mul_div(mux->out, freq_multiplier, mux->multiplier);
+    if ((mux->out->multiplier != freq_multiplier) ||
+        mux->out->divider != mux->multiplier) {
+        clock_set_mul_div(mux->out, freq_multiplier, mux->multiplier);
+        clock_propagate(mux->out);
+    }
+
     clock_update(mux->out, clock_get(current_source));
 
     src_freq = clock_get_hz(current_source);
