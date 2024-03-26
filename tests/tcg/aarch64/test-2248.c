6f6ce1234f3aae6f2d2aaf8cc55de56dc7bcfd86
Richard Henderson <richard.henderson@linaro.org>
2024-03-26 12:22:59 -1000

    tcg/optimize: Fix sign_mask for logical right-shift
    The 'sign' computation is attempting to locate the sign bit that has
    been repeated, so that we can test if that bit is known zero.  That
    computation can be zero if there are no known sign repetitions.
    
    Cc: qemu-stable@nongnu.org
    Fixes: 93a967fbb57 ("tcg/optimize: Propagate sign info for shifting")
    Resolves: https://gitlab.com/qemu-project/qemu/-/issues/2248
    Signed-off-by: Richard Henderson <richard.henderson@linaro.org>
    Reviewed-by: Philippe Mathieu-Daudé <philmd@linaro.org>
    Message-Id: <20240326222259.528099-1-richard.henderson@linaro.org>

---
+++ tests/tcg/aarch64/test-2248.c
@@ -0,0 +1,25 @@
+#include <assert.h>
+
+__attribute__((noinline))
+long test(long x, long y, long sh)
+{
+    long r;
+    asm("cmp   %1, %2\n\t"
+        "cset  x12, lt\n\t"
+        "and   w11, w12, #0xff\n\t"
+        "cmp   w11, #0\n\t"
+        "csetm x14, ne\n\t"
+        "lsr   x13, x14, %3\n\t"
+        "sxtb  %0, w13"
+        : "=r"(r)
+        : "r"(x), "r"(y), "r"(sh)
+        : "x11", "x12", "x13", "x14");
+    return r;
+}
+
+int main()
+{
+    long r = test(0, 1, 2);
+    assert(r == -1);
+    return 0;
+}
