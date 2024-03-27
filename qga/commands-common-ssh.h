259e979db11f922a02c00bcd9c233a858b416849
aidaleuc <aidan_leuck@selinc.com>
2024-03-27 10:24:49 -0600

    Refactor common functions between POSIX and Windows implementation
    Signed-off-by: aidaleuc <aidan_leuck@selinc.com>
    Message-Id: <20240327162450.888453-2-aidan_leuck@selinc.com>

---
+++ qga/commands-common-ssh.h
@@ -0,0 +1,12 @@
+/*
+ * This work is licensed under the terms of the GNU GPL, version 2 or later.
+ * See the COPYING file in the top-level directory.
+ */
+
+#include "qapi/error.h"
+#include <qga-qapi-types.h>
+#include <stdbool.h>
+
+GStrv read_authkeys(const char *path, Error **errp);
+bool check_openssh_pub_keys(strList *keys, size_t *nkeys, Error **errp);
+bool check_openssh_pub_key(const char *key, Error **errp);
