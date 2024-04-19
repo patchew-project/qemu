6876198a14ba7b051011b5b42af8b5fa8378cd1d
Zhao Liu <zhao1.liu@intel.com>
2024-04-19 14:57:11 +0800

    target/s390x/cpu_models: Make kvm_s390_apply_cpu_model() return boolean
    As error.h suggested, the best practice for callee is to return
    something to indicate success / failure.
    
    So make kvm_s390_apply_cpu_model() return boolean and check the
    returned boolean in apply_cpu_model() instead of accessing @err.
    
    Signed-off-by: Zhao Liu <zhao1.liu@intel.com>
    Reviewed-by: Philippe Mathieu-Daudé <philmd@linaro.org>
    Reviewed-by: Thomas Huth <thuth@redhat.com>
    Message-Id: <20240419065712.1225038-6-zhao1.liu@linux.intel.com>

--- target/s390x/cpu_models.h
+++ target/s390x/cpu_models.h
@@ -117,16 +117,17 @@ S390CPUDef const *s390_find_cpu_def(uint16_t type, uint8_t gen, uint8_t ec_ga,
 #ifdef CONFIG_KVM
 bool kvm_s390_cpu_models_supported(void);
 bool kvm_s390_get_host_cpu_model(S390CPUModel *model, Error **errp);
-void kvm_s390_apply_cpu_model(const S390CPUModel *model,  Error **errp);
+bool kvm_s390_apply_cpu_model(const S390CPUModel *model,  Error **errp);
 #else
 static inline bool kvm_s390_get_host_cpu_model(S390CPUModel *model,
                                                Error **errp)
 {
     return true;
 }
-static inline void kvm_s390_apply_cpu_model(const S390CPUModel *model,
+static inline bool kvm_s390_apply_cpu_model(const S390CPUModel *model,
                                             Error **errp)
 {
+    return true;
 }
 static inline bool kvm_s390_cpu_models_supported(void)
 {
