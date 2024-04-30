98bd9b07bd9d1e34aaece5c5155c0077753f7b92
Marek Marczykowski-Górecki <marmarek@invisiblethingslab.com>
2024-04-30 19:33:10 +0200

    Update Xen's features.h header
    Update it to get XENFEAT_dm_msix_all_writes for the next patch.
    
    Signed-off-by: Marek Marczykowski-Górecki <marmarek@invisiblethingslab.com>
    Message-Id: <bd499d1c412502afad60bff73a088b94a4ba43da.1714498385.git-series.marmarek@invisiblethingslab.com>

--- include/hw/xen/interface/features.h
+++ include/hw/xen/interface/features.h
@@ -111,6 +111,23 @@
 #define XENFEAT_not_direct_mapped         16
 #define XENFEAT_direct_mapped             17
 
+/*
+ * Signal whether the domain is able to use the following hypercalls:
+ *
+ * VCPUOP_register_runstate_phys_area
+ * VCPUOP_register_vcpu_time_phys_area
+ */
+#define XENFEAT_runstate_phys_area        18
+#define XENFEAT_vcpu_time_phys_area       19
+
+/*
+ * If set, Xen will passthrough all MSI-X vector ctrl writes to device model,
+ * not only those unmasking an entry. This allows device model to properly keep
+ * track of the MSI-X table without having to read it from the device behind
+ * Xen's backs. This information is relevant only for device models.
+ */
+#define XENFEAT_dm_msix_all_writes        20
+
 #define XENFEAT_NR_SUBMAPS 1
 
 #endif /* __XEN_PUBLIC_FEATURES_H__ */
