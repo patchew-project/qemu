8b3ae27173e1b6eddf0206c70e0158c68a25e60a
Paolo Bonzini <pbonzini@redhat.com>
2024-05-02 17:49:59 +0200

    kvm: move target-dependent interrupt routing out of kvm-all.c
    Let hw/hyperv/hyperv.c and hw/intc/s390_flic.c handle (respectively)
    SynIC and adapter routes, removing the code from target-independent
    files.  This also removes the only occurrence of AdapterInfo outside
    s390 code, so remove that from typedefs.h.
    
    Signed-off-by: Paolo Bonzini <pbonzini@redhat.com>
    Message-Id: <20240502154959.108165-1-pbonzini@redhat.com>

--- hw/intc/s390_flic.c
+++ hw/intc/s390_flic.c
@@ -23,6 +23,34 @@
 #include "qapi/error.h"
 #include "hw/s390x/s390-virtio-ccw.h"
 
+static int kvm_irqchip_add_adapter_route(KVMState *s, AdapterInfo *adapter)
+{
+    struct kvm_irq_routing_entry kroute = {};
+    int virq;
+
+    if (!kvm_gsi_routing_enabled()) {
+        return -ENOSYS;
+    }
+
+    virq = kvm_irqchip_get_virq(s);
+    if (virq < 0) {
+        return virq;
+    }
+
+    kroute.gsi = virq;
+    kroute.type = KVM_IRQ_ROUTING_S390_ADAPTER;
+    kroute.flags = 0;
+    kroute.u.adapter.summary_addr = adapter->summary_addr;
+    kroute.u.adapter.ind_addr = adapter->ind_addr;
+    kroute.u.adapter.summary_offset = adapter->summary_offset;
+    kroute.u.adapter.ind_offset = adapter->ind_offset;
+    kroute.u.adapter.adapter_id = adapter->adapter_id;
+
+    kvm_add_routing_entry(s, &kroute);
+
+    return virq;
+}
+
 S390FLICStateClass *s390_get_flic_class(S390FLICState *fs)
 {
     static S390FLICStateClass *class;
