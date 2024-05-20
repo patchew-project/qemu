27fba38d7878dc7bfd3df66fe230cbc9af9b8a1b
Salil Mehta via <qemu-devel@nongnu.org>
2024-05-21 00:32:34 +0100

    accel/kvm: Extract common KVM vCPU {creation, parking} code
    KVM vCPU creation is done once during the vCPU realization when Qemu vCPU thread
    is spawned. This is common to all the architectures as of now.
    
    Hot-unplug of vCPU results in destruction of the vCPU object in QOM but the
    corresponding KVM vCPU object in the Host KVM is not destroyed as KVM doesn't
    support vCPU removal. Therefore, its representative KVM vCPU object/context in
    Qemu is parked.
    
    Refactor architecture common logic so that some APIs could be reused by vCPU
    Hotplug code of some architectures likes ARM, Loongson etc. Update new/old APIs
    with trace events. No functional change is intended here.
    
    Signed-off-by: Salil Mehta <salil.mehta@huawei.com>
    Reviewed-by: Gavin Shan <gshan@redhat.com>
    Tested-by: Vishnu Pajjuri <vishnu@os.amperecomputing.com>
    Reviewed-by: Jonathan Cameron <Jonathan.Cameron@huawei.com>
    Tested-by: Xianglai Li <lixianglai@loongson.cn>
    Tested-by: Miguel Luis <miguel.luis@oracle.com>
    Reviewed-by: Shaoqin Huang <shahuang@redhat.com>
    Reviewed-by: Vishnu Pajjuri <vishnu@os.amperecomputing.com>
    Reviewed-by: Nicholas Piggin <npiggin@gmail.com>
    Message-Id: <20240520233241.229675-2-salil.mehta@huawei.com>

--- accel/kvm/kvm-cpus.h
+++ accel/kvm/kvm-cpus.h
@@ -22,5 +22,28 @@ bool kvm_supports_guest_debug(void);
 int kvm_insert_breakpoint(CPUState *cpu, int type, vaddr addr, vaddr len);
 int kvm_remove_breakpoint(CPUState *cpu, int type, vaddr addr, vaddr len);
 void kvm_remove_all_breakpoints(CPUState *cpu);
+/**
+ * kvm_create_vcpu - Gets a parked KVM vCPU or creates a KVM vCPU
+ * @cpu: QOM CPUState object for which KVM vCPU has to be fetched/created.
+ *
+ * @returns: 0 when success, errno (<0) when failed.
+ */
+int kvm_create_vcpu(CPUState *cpu);
 
+/**
+ * kvm_park_vcpu - Park QEMU KVM vCPU context
+ * @cpu: QOM CPUState object for which QEMU KVM vCPU context has to be parked.
+ *
+ * @returns: none
+ */
+void kvm_park_vcpu(CPUState *cpu);
+
+/**
+ * kvm_unpark_vcpu - unpark QEMU KVM vCPU context
+ * @s: KVM State
+ * @cpu: Architecture vCPU ID of the parked vCPU
+ *
+ * @returns: KVM fd
+ */
+int kvm_unpark_vcpu(KVMState *s, unsigned long vcpu_id);
 #endif /* KVM_CPUS_H */
