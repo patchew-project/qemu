cc9c28c709f0dca60985e8a27a52d65fdc5096a6
LIU Zhiwei <zhiwei_liu@linux.alibaba.com>
2024-05-22 14:29:04 +0800

    target/riscv: Add zcmop extension
    Zcmop defines eight 16-bit MOP instructions named C.MOP.n, where n is
    an odd integer between 1 and 15, inclusive. C.MOP.n is encoded in
    the reserved encoding space corresponding to C.LUI xn, 0.
    
    Unlike the MOPs defined in the Zimop extension, the C.MOP.n instructions
    are defined to not write any register.
    
    In current implementation, C.MOP.n only has an check function, without any
    other more behavior.
    
    Signed-off-by: LIU Zhiwei <zhiwei_liu@linux.alibaba.com>
    Reviewed-by: Alistair Francis <alistair.francis@wdc.com>
    Message-Id: <20240522062905.1799-4-zhiwei_liu@linux.alibaba.com>

--- target/riscv/translate.c
+++ target/riscv/translate.c
@@ -1112,6 +1112,7 @@ static uint32_t opcode_at(DisasContextBase *dcbase, target_ulong pc)
 /* Include the auto-generated decoder for 16 bit insn */
 #include "decode-insn16.c.inc"
 #include "insn_trans/trans_rvzce.c.inc"
+#include "insn_trans/trans_rvzcmop.c.inc"
 
 /* Include decoders for factored-out extensions */
 #include "decode-XVentanaCondOps.c.inc"
