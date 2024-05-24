d6c422e0137e00e83b872ab24739039a950ef53a
Sunil V L <sunilvl@ventanamicro.com>
2024-05-24 11:44:09 +0530

    tests/qtest/bios-tables-test: Add empty ACPI data files for RISC-V
    As per process documented (steps 1-3) in bios-tables-test.c, add empty
    AML data files for RISC-V ACPI tables and add the entries in
    bios-tables-test-allowed-diff.h.
    
    Signed-off-by: Sunil V L <sunilvl@ventanamicro.com>
    Acked-by: Alistair Francis <alistair.francis@wdc.com>
    Reviewed-by: Igor Mammedov <imammedo@redhat.com>
    Message-Id: <20240524061411.341599-11-sunilvl@ventanamicro.com>

--- tests/qtest/bios-tables-test-allowed-diff.h
+++ tests/qtest/bios-tables-test-allowed-diff.h
@@ -1,1 +1,7 @@
 /* List of comma-separated changed AML files to ignore */
+"tests/data/acpi/virt/riscv64/APIC",
+"tests/data/acpi/virt/riscv64/DSDT",
+"tests/data/acpi/virt/riscv64/FACP",
+"tests/data/acpi/virt/riscv64/MCFG",
+"tests/data/acpi/virt/riscv64/RHCT",
+"tests/data/acpi/virt/riscv64/SPCR",
