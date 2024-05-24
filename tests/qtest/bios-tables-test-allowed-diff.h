885da787dfd01dd2f1d9eef181de56054097a7a7
Sunil V L <sunilvl@ventanamicro.com>
2024-05-24 11:44:11 +0530

    tests/qtest/bios-tables-test: Add expected ACPI data files for RISC-V
    As per the step 5 in the process documented in bios-tables-test.c,
    generate the expected ACPI AML data files for RISC-V using the
    rebuild-expected-aml.sh script and update the
    bios-tables-test-allowed-diff.h.
    
    These are all new files being added for the first time. Hence, iASL diff
    output is not added.
    
    Signed-off-by: Sunil V L <sunilvl@ventanamicro.com>
    Acked-by: Alistair Francis <alistair.francis@wdc.com>
    Acked-by: Igor Mammedov <imammedo@redhat.com>
    Message-Id: <20240524061411.341599-13-sunilvl@ventanamicro.com>

--- tests/qtest/bios-tables-test-allowed-diff.h
+++ tests/qtest/bios-tables-test-allowed-diff.h
@@ -1,7 +1,1 @@
 /* List of comma-separated changed AML files to ignore */
-"tests/data/acpi/virt/riscv64/APIC",
-"tests/data/acpi/virt/riscv64/DSDT",
-"tests/data/acpi/virt/riscv64/FACP",
-"tests/data/acpi/virt/riscv64/MCFG",
-"tests/data/acpi/virt/riscv64/RHCT",
-"tests/data/acpi/virt/riscv64/SPCR",
