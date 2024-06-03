60b1364c1013f3c13ebec93c65562785cf8b748a
Peter Maydell <peter.maydell@linaro.org>
2024-06-03 17:09:33 +0100

    accel/tcg: Make TCGCPUOps::cpu_exec_halt mandatory
    Now that all targets set TCGCPUOps::cpu_exec_halt, we can make it
    mandatory and remove the fallback handling that calls cpu_has_work.
    
    Signed-off-by: Peter Maydell <peter.maydell@linaro.org>
    Reviewed-by: Philippe Mathieu-Daudé <philmd@linaro.org>
    Reviewed-by: Richard Henderson <richard.henderson@linaro.org>
    Message-Id: <20240603160933.1141717-4-peter.maydell@linaro.org>

--- accel/tcg/cpu-exec.c
+++ accel/tcg/cpu-exec.c
@@ -682,13 +682,8 @@ static inline bool cpu_handle_halt(CPUState *cpu)
 #ifndef CONFIG_USER_ONLY
     if (cpu->halted) {
         const TCGCPUOps *tcg_ops = cpu->cc->tcg_ops;
-        bool leave_halt;
+        bool leave_halt = tcg_ops->cpu_exec_halt(cpu);
 
-        if (tcg_ops->cpu_exec_halt) {
-            leave_halt = tcg_ops->cpu_exec_halt(cpu);
-        } else {
-            leave_halt = cpu_has_work(cpu);
-        }
         if (!leave_halt) {
             return true;
         }
