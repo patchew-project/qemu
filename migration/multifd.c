54edcbbd853632e0ba306968798913b951508771
Peter Xu <peterx@redhat.com>
2024-06-12 10:42:26 -0400

    migration: Rename thread debug names
    The postcopy thread names on dest QEMU are slightly confusing, partly I'll
    need to blame myself on 36f62f11e4 ("migration: Postcopy preemption
    preparation on channel creation").  E.g., "fault-fast" reads like a fast
    version of "fault-default", but it's actually the fast version of
    "postcopy/listen".
    
    Taking this chance, rename all the migration threads with proper rules.
    Considering we only have 15 chars usable, prefix all threads with "mig/",
    meanwhile identify src/dst threads properly this time.  So now most thread
    names will look like "mig/DIR/xxx", where DIR will be "src"/"dst", except
    the bg-snapshot thread which doesn't have a direction.
    
    For multifd threads, making them "mig/{src|dst}/{send|recv}_%d".
    
    We used to have "live_migration" thread for a very long time, now it's
    called "mig/src/main".  We may hope to have "mig/dst/main" soon but not
    yet.
    
    Signed-off-by: Peter Xu <peterx@redhat.com>
    Reviewed-by: Fabiano Rosas <farosas@suse.de>
    Message-Id: <20240612144228.1179240-3-peterx@redhat.com>

--- migration/multifd.c
+++ migration/multifd.c
@@ -1059,7 +1059,7 @@ static bool multifd_tls_channel_connect(MultiFDSendParams *p,
     args->p = p;
 
     p->tls_thread_created = true;
-    qemu_thread_create(&p->tls_thread, "multifd-tls-handshake-worker",
+    qemu_thread_create(&p->tls_thread, "mig/src/tls",
                        multifd_tls_handshake_thread, args,
                        QEMU_THREAD_JOINABLE);
     return true;
@@ -1185,7 +1185,7 @@ bool multifd_send_setup(void)
         } else {
             p->iov = g_new0(struct iovec, page_count);
         }
-        p->name = g_strdup_printf("multifdsend_%d", i);
+        p->name = g_strdup_printf("mig/src/send_%d", i);
         p->page_size = qemu_target_page_size();
         p->page_count = page_count;
         p->write_flags = 0;
@@ -1601,7 +1601,7 @@ int multifd_recv_setup(Error **errp)
                 + sizeof(uint64_t) * page_count;
             p->packet = g_malloc0(p->packet_len);
         }
-        p->name = g_strdup_printf("multifdrecv_%d", i);
+        p->name = g_strdup_printf("mig/dst/recv_%d", i);
         p->iov = g_new0(struct iovec, page_count);
         p->normal = g_new0(ram_addr_t, page_count);
         p->zero = g_new0(ram_addr_t, page_count);
