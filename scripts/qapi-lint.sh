33a3fab6394a99141cc14928f9277e2df3a903fe
John Snow <jsnow@redhat.com>
2024-06-18 20:30:00 -0400

    Add some ad-hoc linting helpers.
    These aren't ready for upstream inclusion, because they do not properly
    manage version dependencies, execution environment and so on. These are
    just the tools I use in my Own Special Environment :tm: for testing and
    debugging.
    
    They've been tested only on Fedora 38 for right now, which means:
    
    Python 3.11.4-1.fc38
    pylint 2.17.4-2.fc38
    mypy 1.4.0-1.fc38
    isort 5.12.0-1.fc38
    flake8 5.0.3-2.fc38
    
    "Soon" :tm: I'll move the qapi generator code under the python/
    directory to take advantage of the more robust linting infrastructure
    there, but that's going to happen after this series. Sorry about that!
    
    Signed-off-by: John Snow <jsnow@redhat.com>
    Reviewed-by: Michael S. Tsirkin <mst@redhat.com>
    Message-Id: <20240619003012.1753577-2-jsnow@redhat.com>

---
+++ scripts/qapi-lint.sh
@@ -0,0 +1,59 @@
+#!/usr/bin/env bash
+set -e
+
+if [[ -f qapi/.flake8 ]]; then
+    echo "flake8 --config=qapi/.flake8 qapi/"
+    flake8 --config=qapi/.flake8 qapi/
+fi
+if [[ -f qapi/pylintrc ]]; then
+    echo "pylint --rcfile=qapi/pylintrc qapi/"
+    pylint --rcfile=qapi/pylintrc qapi/
+fi
+if [[ -f qapi/mypy.ini ]]; then
+    echo "mypy --config-file=qapi/mypy.ini qapi/"
+    mypy --config-file=qapi/mypy.ini qapi/
+fi
+
+if [[ -f qapi/.isort.cfg ]]; then
+    pushd qapi
+    echo "isort -c ."
+    isort -c .
+    popd
+fi
+
+if [[ -f ../docs/sphinx/qapi-domain.py ]]; then
+    pushd ../docs/sphinx
+
+    echo "mypy --strict qapi-domain.py"
+    mypy --strict qapi-domain.py
+    echo "flake8 qapi-domain.py --max-line-length=99"
+    flake8 qapi-domain.py --max-line-length=99
+    echo "isort qapi-domain.py"
+    isort qapi-domain.py
+    echo "black --check qapi-domain.py"
+    black --check qapi-domain.py
+
+    popd
+fi
+
+if [[ -f ../docs/sphinx/qapidoc.py ]]; then
+    pushd ../docs/sphinx
+
+    echo "pylint --rc-file ../../scripts/qapi/pylintrc qapidoc.py"
+    PYTHONPATH=${HOME}/src/qemu/scripts/ pylint --rc-file ../../scripts/qapi/pylintrc qapidoc.py
+    echo "flake8 qapidoc.py --max-line-length=80"
+    flake8 qapidoc.py --max-line-length=80
+    echo "isort qapidoc.py"
+    isort qapidoc.py
+    echo "black --line-length 80 --check qapidoc.py"
+    black --line-length 80 --check qapidoc.py
+
+    popd
+fi
+
+pushd ../build
+make -j13
+make check-qapi-schema
+make docs
+make sphinxdocs
+popd
