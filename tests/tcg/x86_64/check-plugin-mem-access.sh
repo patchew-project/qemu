7e7942f9638e87015fb9dc629e7fdd57276e6e66
Pierrick Bouvier <pierrick.bouvier@linaro.org>
2024-07-06 12:13:35 -0700

    tests/tcg/x86_64: add test for plugin memory access
    Add an explicit test to check expected memory values are read/written.
    For sizes 8, 16, 32, 64 and 128, we generate a load/store operation.
    For size 8 -> 64, we generate an atomic __sync_val_compare_and_swap too.
    For 128bits memory access, we rely on SSE2 instructions.
    
    By default, atomic accesses are non atomic if a single cpu is running,
    so we force creation of a second one by creating a new thread first.
    
    load/store helpers code path can't be triggered easily in user mode (no
    softmmu), so we can't test it here.
    
    Can be run with:
    make -C build/tests/tcg/x86_64-linux-user run-plugin-test-plugin-mem-access-with-libmem.so
    
    Tested-by: Xingtao Yao <yaoxt.fnst@fujitsu.com>
    Signed-off-by: Pierrick Bouvier <pierrick.bouvier@linaro.org>
    Message-Id: <20240706191335.878142-8-pierrick.bouvier@linaro.org>

---
+++ tests/tcg/x86_64/check-plugin-mem-access.sh
@@ -0,0 +1,48 @@
+#!/usr/bin/env bash
+
+set -euo pipefail
+
+die()
+{
+    echo "$@" 1>&2
+    exit 1
+}
+
+check()
+{
+    file=$1
+    pattern=$2
+    grep "$pattern" "$file" > /dev/null || die "\"$pattern\" not found in $file"
+}
+
+[ $# -eq 1 ] || die "usage: plugin_out_file"
+
+plugin_out=$1
+
+expected()
+{
+    cat << EOF
+,store_u8,.*,8,store,0xf1
+,atomic_op_u8,.*,8,load,0x42
+,atomic_op_u8,.*,8,store,0xf1
+,load_u8,.*,8,load,0xf1
+,store_u16,.*,16,store,0xf123
+,atomic_op_u16,.*,16,load,0x0042
+,atomic_op_u16,.*,16,store,0xf123
+,load_u16,.*,16,load,0xf123
+,store_u32,.*,32,store,0xff112233
+,atomic_op_u32,.*,32,load,0x00000042
+,atomic_op_u32,.*,32,store,0xff112233
+,load_u32,.*,32,load,0xff112233
+,store_u64,.*,64,store,0xf123456789abcdef
+,atomic_op_u64,.*,64,load,0x0000000000000042
+,atomic_op_u64,.*,64,store,0xf123456789abcdef
+,load_u64,.*,64,load,0xf123456789abcdef
+,store_u128,.*,128,store,0xf122334455667788f123456789abcdef
+,load_u128,.*,128,load,0xf122334455667788f123456789abcdef
+EOF
+}
+
+expected | while read line; do
+    check "$plugin_out" "$line"
+done
