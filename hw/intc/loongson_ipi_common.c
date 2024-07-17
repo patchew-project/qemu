17ee55f4d54585fdf757642d79565bc3829e6327
Bibo Mao <maobibo@loongson.cn>
2024-07-17 23:46:59 +0200

    hw/intc/loongson_ipi: Move IPICore structure to loongson_ipi_common.h
    Move the IPICore structure and corresponding common fields
    of LoongsonIPICommonState to "hw/intc/loongson_ipi_common.h".
    
    Signed-off-by: Bibo Mao <maobibo@loongson.cn>
    [PMD: Extracted from bigger commit, added commit description]
    Signed-off-by: Philippe Mathieu-Daudé <philmd@linaro.org>
    Message-Id: <20240717214708.78403-9-philmd@linaro.org>

--- hw/intc/loongson_ipi_common.c
+++ hw/intc/loongson_ipi_common.c
@@ -8,12 +8,62 @@
 #include "qemu/osdep.h"
 #include "hw/sysbus.h"
 #include "hw/intc/loongson_ipi_common.h"
+#include "hw/qdev-properties.h"
+#include "migration/vmstate.h"
+
+static void loongson_ipi_common_finalize(Object *obj)
+{
+    LoongsonIPICommonState *s = LOONGSON_IPI_COMMON(obj);
+
+    g_free(s->cpu);
+}
+
+static const VMStateDescription vmstate_ipi_core = {
+    .name = "ipi-single",
+    .version_id = 2,
+    .minimum_version_id = 2,
+    .fields = (const VMStateField[]) {
+        VMSTATE_UINT32(status, IPICore),
+        VMSTATE_UINT32(en, IPICore),
+        VMSTATE_UINT32(set, IPICore),
+        VMSTATE_UINT32(clear, IPICore),
+        VMSTATE_UINT32_ARRAY(buf, IPICore, IPI_MBX_NUM * 2),
+        VMSTATE_END_OF_LIST()
+    }
+};
+
+static const VMStateDescription vmstate_loongson_ipi = {
+    .name = "loongson_ipi",
+    .version_id = 2,
+    .minimum_version_id = 2,
+    .fields = (const VMStateField[]) {
+        VMSTATE_STRUCT_VARRAY_POINTER_UINT32(cpu, LoongsonIPICommonState,
+                                             num_cpu, vmstate_ipi_core,
+                                             IPICore),
+        VMSTATE_END_OF_LIST()
+    }
+};
+
+static Property ipi_properties[] = {
+    DEFINE_PROP_UINT32("num-cpu", LoongsonIPICommonState, num_cpu, 1),
+    DEFINE_PROP_END_OF_LIST(),
+};
+
+static void loongson_ipi_common_class_init(ObjectClass *klass, void *data)
+{
+    DeviceClass *dc = DEVICE_CLASS(klass);
+
+    device_class_set_props(dc, ipi_properties);
+    dc->vmsd = &vmstate_loongson_ipi;
+}
 
 static const TypeInfo loongson_ipi_common_info = {
     .name           = TYPE_LOONGSON_IPI_COMMON,
     .parent         = TYPE_SYS_BUS_DEVICE,
     .instance_size  = sizeof(LoongsonIPICommonState),
     .class_size     = sizeof(LoongsonIPICommonClass),
+    .class_init     = loongson_ipi_common_class_init,
+    .instance_finalize = loongson_ipi_common_finalize,
     .abstract       = true,
 };
 
