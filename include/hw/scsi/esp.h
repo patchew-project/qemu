4557b600b9ffc733a5f78b25add3956b111dabe0
Mark Cave-Ayland <mark.cave-ayland@ilande.co.uk>
2024-11-01 22:00:52 +0000

    esp.c: only allow ESP commands permitted in the current asc_mode
    If an ESP command is issued in an incorrect mode then an illegal command
    interrupt should be generated. Add a new esp_cmd_is_valid() function to
    indicate whether the ESP command is valid for the current mode, and if not
    then raise the illegal command interrupt.
    
    This fixes WinNT MIPS which issues ICCS after a Chip Reset which is not
    permitted, but will fail with an INACCESSIBLE_BOOT_DEVICE error unless an
    interrupt is generated.
    
    Signed-off-by: Mark Cave-Ayland <mark.cave-ayland@ilande.co.uk>
    Fixes: 83428f7a97 ("esp.c: move write_response() non-DMA logic to esp_do_nodma()")
    Resolves: https://gitlab.com/qemu-project/qemu/-/issues/2464
    Message-Id: <20241101220052.1463805-6-mark.cave-ayland@ilande.co.uk>

--- include/hw/scsi/esp.h
+++ include/hw/scsi/esp.h
@@ -111,6 +111,13 @@ struct SysBusESPState {
 #define CMD_DMA 0x80
 #define CMD_CMD 0x7f
 
+#define CMD_GRP_MASK 0x70
+
+#define CMD_GRP_MISC 0x00
+#define CMD_GRP_INIT 0x01
+#define CMD_GRP_TRGT 0x02
+#define CMD_GRP_DISC 0x04
+
 #define CMD_NOP      0x00
 #define CMD_FLUSH    0x01
 #define CMD_RESET    0x02
