78c1933c1cb7a3d10521eaa68ddd2fd1b7c4a40d
Philippe Mathieu-Daudé <philmd@linaro.org>
2024-11-12 18:20:21 +0100

    target/mips: Convert microMIPS LSA opcode to decodetree
    Simply call the generic gen_lsa(), using the plus_1()
    helper to add 1 to the shift amount.
    
    Signed-off-by: Philippe Mathieu-Daudé <philmd@linaro.org>
    Reviewed-by: Richard Henderson <richard.henderson@linaro.org>
    Message-Id: <20241112172022.88348-6-philmd@linaro.org>

--- target/mips/tcg/micromips_translate.c
+++ target/mips/tcg/micromips_translate.c
@@ -9,6 +9,16 @@
 #include "qemu/osdep.h"
 #include "translate.h"
 
+static inline int plus_1(DisasContext *ctx, int x)
+{
+    return x + 1;
+}
+
 /* Include the auto-generated decoders.  */
 #include "decode-micromips16.c.inc"
 #include "decode-micromips32.c.inc"
+
+static bool trans_LSA(DisasContext *ctx, arg_r *a)
+{
+    return gen_lsa(ctx, a->rd, a->rt, a->rs, a->sa);
+}
