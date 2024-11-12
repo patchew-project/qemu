ffbeb58af1122f4e561f29d76bdb8ebb7957b643
Philippe Mathieu-Daudé <f4bug@amsat.org>
2024-11-12 18:20:22 +0100

    target/mips: Convert nanoMIPS LSA opcode to decodetree
    Simply call the generic gen_lsa() helper.
    
    Signed-off-by: Philippe Mathieu-Daudé <f4bug@amsat.org>
    Signed-off-by: Philippe Mathieu-Daudé <philmd@linaro.org>
    Reviewed-by: Richard Henderson <richard.henderson@linaro.org>
    Message-Id: <20241112172022.88348-7-philmd@linaro.org>

--- target/mips/tcg/nanomips_translate.c
+++ target/mips/tcg/nanomips_translate.c
@@ -12,3 +12,10 @@
 /* Include the auto-generated decoders.  */
 #include "decode-nanomips16.c.inc"
 #include "decode-nanomips32.c.inc"
+
+static bool trans_LSA(DisasContext *ctx, arg_r *a)
+{
+    gen_lsa(ctx, a->rd, a->rt, a->rs, a->sa);
+
+    return true;
+}
