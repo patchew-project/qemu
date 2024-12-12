6db5b484e1d327e9c4af51ee87789af4f3495481
Mark Cave-Ayland <mark.cave-ayland@ilande.co.uk>
2024-12-12 15:14:08 +0000

    target/ppc: implement address swizzle for instruction translation
    Ensure that the address swizzle is implemented when retrieving instructions from
    guest memory for translation.
    
    Signed-off-by: Mark Cave-Ayland <mark.cave-ayland@ilande.co.uk>
    Message-Id: <20241212151412.570454-8-mark.cave-ayland@ilande.co.uk>

--- target/ppc/translate.c
+++ target/ppc/translate.c
@@ -6625,7 +6625,11 @@ static void ppc_tr_translate_insn(DisasContextBase *dcbase, CPUState *cs)
               ctx->base.pc_next, ctx->mem_idx, (int)msr_ir);
 
     ctx->cia = pc = ctx->base.pc_next;
-    insn = translator_ldl_swap(env, dcbase, pc, need_byteswap(ctx));
+    if (!need_addrswizzle_le(ctx)) {
+        insn = translator_ldl_swap(env, dcbase, pc, need_byteswap(ctx));
+    } else {
+        insn = translator_ldl(env, dcbase, pc ^ 4);
+    }
     ctx->base.pc_next = pc += 4;
 
     if (!is_prefix_insn(ctx, insn)) {
@@ -6641,8 +6645,13 @@ static void ppc_tr_translate_insn(DisasContextBase *dcbase, CPUState *cs)
         gen_exception_err(ctx, POWERPC_EXCP_ALIGN, POWERPC_EXCP_ALIGN_INSN);
         ok = true;
     } else {
-        uint32_t insn2 = translator_ldl_swap(env, dcbase, pc,
-                                             need_byteswap(ctx));
+        uint32_t insn2;
+
+        if (!need_addrswizzle_le(ctx)) {
+            insn2 = translator_ldl_swap(env, dcbase, pc, need_byteswap(ctx));
+        } else {
+            insn2 = translator_ldl(env, dcbase, pc ^ 4);
+        }
         ctx->base.pc_next = pc += 4;
         ok = decode_insn64(ctx, deposit64(insn2, 32, 32, insn));
     }
