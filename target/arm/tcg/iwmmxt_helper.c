2b64e9256ed2f4dab2b2332b202ff3c6d20e6d56
Denis Rastyogin <gerben@altlinux.org>
2024-12-12 17:27:09 +0300

    target/arm/tcg: fix potential integer overflow in iwmmxt_macuw()
    The function iwmmxt_macuw() could potentially cause an integer
    overflow when summing up four 32-bit multiplications.
    This occurs because the intermediate results may exceed the 32-bit
    range before being cast to uint64_t. The fix ensures each
    multiplication is explicitly cast to uint64_t prior to summation,
    preventing potential issues and ensuring correctness.
    
    Found by Linux Verification Center (linuxtesting.org) with SVACE.
    
    Signed-off-by: Denis Sergeev <zeff@altlinux.org>
    Signed-off-by: Denis Rastyogin <gerben@altlinux.org>
    Reviewed-by: Richard Henderson <richard.henderson@linaro.org>
    Message-Id: <20241212142716.523980-1-gerben@altlinux.org>

--- target/arm/tcg/iwmmxt_helper.c
+++ target/arm/tcg/iwmmxt_helper.c
@@ -140,7 +140,7 @@ uint64_t HELPER(iwmmxt_macsw)(uint64_t a, uint64_t b)
 
 uint64_t HELPER(iwmmxt_macuw)(uint64_t a, uint64_t b)
 {
-#define MACU(SHR) ( \
+#define MACU(SHR) (uint64_t)( \
         (uint32_t) ((a >> SHR) & 0xffff) * \
         (uint32_t) ((b >> SHR) & 0xffff))
     return MACU(0) + MACU(16) + MACU(32) + MACU(48);
