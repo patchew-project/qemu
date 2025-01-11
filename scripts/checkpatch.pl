8ac938f67c34e79af46292b83af72aa8e05d6715
Akihiko Odaki <akihiko.odaki@daynix.com>
2025-01-11 17:07:48 +0900

    checkpatch: Check .m, .build, .hx, .json and .plist
    Check more text files: Objective-C, Meson, "hx", JSON, and property
    list.
    
    Signed-off-by: Akihiko Odaki <akihiko.odaki@daynix.com>
    Message-Id: <20250111-checkpatch-v2-1-db77a522ab6a@daynix.com>

--- scripts/checkpatch.pl
+++ scripts/checkpatch.pl
@@ -12,7 +12,7 @@ use Term::ANSIColor qw(:constants);
 my $P = $0;
 $P =~ s@.*/@@g;
 
-our $SrcFile    = qr{\.(?:(h|c)(\.inc)?|cpp|s|S|pl|py|sh)$};
+our $SrcFile = qr{\.(?:(h|c|m)(\.inc)?|cpp|s|S|pl|py|sh|build|hx|json|plist)$};
 
 my $V = '0.31';
 
