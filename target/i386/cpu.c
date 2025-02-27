56c148aab034c5564b65ed2f01a832d87508d2c5
Chuang Xu <xuchuangxclwt@bytedance.com>
2025-02-27 14:25:20 +0800

    i386/cpu: Fix number of addressable IDs field for CPUID.01H.EBX[23:16]
    When QEMU is started with:
    -cpu host,migratable=on,host-cache-info=on,l3-cache=off
    -smp 180,sockets=2,dies=1,cores=45,threads=2
    
    On Intel platform:
    CPUID.01H.EBX[23:16] is defined as "max number of addressable IDs for
    logical processors in the physical package".
    
    When executing "cpuid -1 -l 1 -r" in the guest, we obtain a value of 90 for
    CPUID.01H.EBX[23:16], whereas the expected value is 128. Additionally,
    executing "cpuid -1 -l 4 -r" in the guest yields a value of 63 for
    CPUID.04H.EAX[31:26], which matches the expected result.
    
    As (1+CPUID.04H.EAX[31:26]) rounds up to the nearest power-of-2 integer,
    it's necessary to round up CPUID.01H.EBX[23:16] to the nearest power-of-2
    integer too. Otherwise there would be unexpected results in guest with
    older kernel.
    
    For example, when QEMU is started with CLI above and xtopology is disabled,
    guest kernel 5.15.120 uses CPUID.01H.EBX[23:16]/(1+CPUID.04H.EAX[31:26]) to
    calculate threads-per-core in detect_ht(). Then guest will get "90/(1+63)=1"
    as the result, even though threads-per-core should actually be 2.
    
    And on AMD platform:
    CPUID.01H.EBX[23:16] is defined as "Logical processor count". Current
    result meets our expectation.
    
    So round up CPUID.01H.EBX[23:16] to the nearest power-of-2 integer only
    for Intel platform to solve the unexpected result.
    
    This change doesn't need to add compat property since it does not affect
    live migration between different versions of pc machines.
    
    Cc: qemu-stable@nongnu.org
    Reviewed-by: Zhao Liu <zhao1.liu@intel.com>
    Acked-by: Igor Mammedov <imammedo@redhat.com>
    Signed-off-by: Guixiong Wei <weiguixiong@bytedance.com>
    Signed-off-by: Yipeng Yin <yinyipeng@bytedance.com>
    Signed-off-by: Chuang Xu <xuchuangxclwt@bytedance.com>
    Signed-off-by: Zhao Liu <zhao1.liu@intel.com>
    Message-Id: <20250227062523.124601-2-zhao1.liu@intel.com>

--- target/i386/cpu.c
+++ target/i386/cpu.c
@@ -6837,7 +6837,16 @@ void cpu_x86_cpuid(CPUX86State *env, uint32_t index, uint32_t count,
         }
         *edx = env->features[FEAT_1_EDX];
         if (threads_per_pkg > 1) {
-            *ebx |= threads_per_pkg << 16;
+            /*
+             * For CPUID.01H.EBX[Bits 23-16], AMD requires logical processor
+             * count, but Intel needs maximum number of addressable IDs for
+             * logical processors per package.
+             */
+            if (cpu->vendor_cpuid_only && IS_AMD_CPU(env)) {
+                *ebx |= threads_per_pkg << 16;
+            } else {
+                *ebx |= 1 << apicid_pkg_offset(topo_info) << 16;
+            }
         }
         if (!cpu->enable_pmu) {
             *ecx &= ~CPUID_EXT_PDCM;
