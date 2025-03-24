fe7ef0dfc295af98c951b620990119ef4634dcc7
Alex Bennée <alex.bennee@linaro.org>
2025-03-24 10:21:32 +0000

    include/exec: fix assert in size_memop
    We can handle larger sized memops now, expand the range of the assert.
    
    Fixes: 4b473e0c60 (tcg: Expand MO_SIZE to 3 bits)
    Signed-off-by: Alex Bennée <alex.bennee@linaro.org>
    
    Reviewed-by: Philippe Mathieu-Daudé <philmd@linaro.org>
    Reviewed-by: Pierrick Bouvier <pierrick.bouvier@linaro.org>
    Reviewed-by: Richard Henderson <richard.henderson@linaro.org>
    Message-Id: <20250324102142.67022-2-alex.bennee@linaro.org>

--- include/exec/memop.h
+++ include/exec/memop.h
@@ -162,8 +162,8 @@ static inline unsigned memop_size(MemOp op)
 static inline MemOp size_memop(unsigned size)
 {
 #ifdef CONFIG_DEBUG_TCG
-    /* Power of 2 up to 8.  */
-    assert((size & (size - 1)) == 0 && size >= 1 && size <= 8);
+    /* Power of 2 up to 128.  */
+    assert((size & (size - 1)) == 0 && size >= 1 && size <= (1 << MO_SIZE));
 #endif
     return (MemOp)ctz32(size);
 }
