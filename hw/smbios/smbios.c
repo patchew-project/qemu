5b1de766ee0ff0c2f8df39eed535823b371402fe
Valentin David <valentin.david@gmail.com>
2025-03-26 16:52:54 +0100

    hw/smbios/smbios.c: Add missing NUL terminal to string from path= parameter
    Without it, it seems the data gets garbage at the end of the string.
    
    Signed-off-by: Valentin David <valentin.david@canonical.com>
    Message-Id: <20250326155254.2273939-1-valentin.david@canonical.com>

--- hw/smbios/smbios.c
+++ hw/smbios/smbios.c
@@ -1283,6 +1283,7 @@ static int save_opt_one(void *opaque,
                 return -1;
             }
             g_byte_array_append(data, (guint8 *)buf, ret);
+            g_byte_array_append(data, (guint8 *)"\0", 1);
         }
 
         buf[0] = '\0';
