182b247920cea8e4f99044d096ee8308d2cb6e99
Dietmar Maurer <dietmar@proxmox.com>
2025-04-18 13:29:52 +0200

    vnc: initialize gst during argument processing
    So that we can set --gst- options on the qemu command line.
    
    Signed-off-by: Dietmar Maurer <dietmar@proxmox.com>
    Message-Id: <20250418112953.1744442-9-dietmar@proxmox.com>

--- ui/vnc.c
+++ ui/vnc.c
@@ -4348,10 +4348,6 @@ int vnc_init_func(void *opaque, QemuOpts *opts, Error **errp)
     Error *local_err = NULL;
     char *id = (char *)qemu_opts_id(opts);
 
-#ifdef CONFIG_GSTREAMER
-    gst_init(NULL, NULL);
-#endif
-
     assert(id);
     vnc_display_init(id, &local_err);
     if (local_err) {
