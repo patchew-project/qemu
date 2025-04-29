81f7dc0243f9bce356923bd2cbe787aee34d1484
Jean-Louis Dupond <jean-louis@dupond.be>
2025-04-29 12:31:10 +0200

    qcow2: put discards in discard queue when discard-no-unref is enabled
    When discard-no-unref is enabled, discards are not queued like it
    should.
    This was broken since discard-no-unref was added.
    
    Add some helper function qcow2_discard_cluster which handles some common
    checks and calls the queue_discards function if needed to add the
    discard request to the queue.
    
    Signed-off-by: Jean-Louis Dupond <jean-louis@dupond.be>
    Reviewed-by: Hanna Czenczek <hreitz@redhat.com>
    Message-Id: <20250429103110.761910-3-jean-louis@dupond.be>

--- block/qcow2-refcount.c
+++ block/qcow2-refcount.c
@@ -1205,6 +1205,23 @@ void qcow2_free_any_cluster(BlockDriverState *bs, uint64_t l2_entry,
     }
 }
 
+void qcow2_discard_cluster(BlockDriverState *bs, uint64_t offset,
+                           uint64_t length, QCow2ClusterType ctype,
+                           enum qcow2_discard_type dtype) {
+
+    BDRVQcow2State *s = bs->opaque;
+
+    if (s->discard_passthrough[dtype] &&
+        (ctype == QCOW2_CLUSTER_NORMAL ||
+         ctype == QCOW2_CLUSTER_ZERO_ALLOC)) {
+        if (has_data_file(bs)) {
+            bdrv_pdiscard(s->data_file, offset, length);
+        } else {
+            queue_discard(bs, offset, length);
+        }
+    }
+}
+
 int qcow2_write_caches(BlockDriverState *bs)
 {
     BDRVQcow2State *s = bs->opaque;
@@ -3619,7 +3636,7 @@ qcow2_discard_refcount_block(BlockDriverState *bs, uint64_t discard_block_offs)
         /* discard refblock from the cache if refblock is cached */
         qcow2_cache_discard(s->refcount_block_cache, refblock);
     }
-    update_refcount_discard(bs, discard_block_offs, s->cluster_size);
+    queue_discard(bs, discard_block_offs, s->cluster_size);
 
     return 0;
 }
