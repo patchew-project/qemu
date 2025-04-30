f55f607dea610959a3c5bb3b2f61cb251f8d1c6a
Mario Fleischmann <mario.fleischmann@lauterbach.com>
2025-04-30 07:27:39 +0200

    mcd: Implement single stepping
    gdbstub: abort gdb_set_stop_cpu when step is issued by mcdserver
    Signed-off-by: Mario Fleischmann <mario.fleischmann@lauterbach.com>
    Message-Id: <20250430052741.21145-19-mario.fleischmann@lauterbach.com>

--- mcd/mcd_stub.c
+++ mcd/mcd_stub.c
@@ -577,6 +577,24 @@ MCDStopResult *qmp_mcd_stop(uint32_t core_uid, bool global, Error **errp)
     return result;
 }
 
+MCDStepResult *qmp_mcd_step(uint32_t core_uid, bool global, uint32_t step_type,
+                            uint32_t n_steps, Error **errp)
+{
+    MCDStepResult *result = g_malloc0(sizeof(*result));
+    mcd_core_st *core = NULL;
+
+    result->return_status = retrieve_open_core(core_uid, &core);
+    if (result->return_status != MCD_RET_ACT_NONE) {
+        g_stub_state.on_error_ask_server = false;
+        return result;
+    }
+    result->return_status = mcd_step_f(core, global, step_type, n_steps);
+
+    g_stub_state.on_error_ask_server = true;
+    return result;
+}
+
+
 MCDSetGlobalResult *qmp_mcd_set_global(uint32_t core_uid, bool enable,
                                        Error **errp)
 {
