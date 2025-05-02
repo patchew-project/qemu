d74db2dacf0e2243a58be67b47e37bb401c6cfc5
Nicholas Piggin <npiggin@gmail.com>
2025-05-02 13:17:01 +1000

    hw/net/e1000e: Do not auto-clear cause on postponed msix interrupt
    If an interrupt is postponed, it should not do cause auto-clearing
    or auto-masking. That is done when the interrupt processing occurs.
    
    Signed-off-by: Nicholas Piggin <npiggin@gmail.com>
    Message-Id: <20250502031705.100768-10-npiggin@gmail.com>

--- hw/net/e1000e_core.c
+++ hw/net/e1000e_core.c
@@ -2057,10 +2057,11 @@ e1000e_msix_notify_one(E1000ECore *core, uint32_t cause, uint32_t int_cfg)
     if (E1000_IVAR_ENTRY_VALID(int_cfg)) {
         uint32_t vec = E1000_IVAR_ENTRY_VEC(int_cfg);
         if (vec < E1000E_MSIX_VEC_NUM) {
-            if (!e1000e_eitr_should_postpone(core, vec)) {
-                trace_e1000e_irq_msix_notify_vec(vec);
-                msix_notify(core->owner, vec);
+            if (e1000e_eitr_should_postpone(core, vec)) {
+                return;
             }
+            trace_e1000e_irq_msix_notify_vec(vec);
+            msix_notify(core->owner, vec);
         } else {
             trace_e1000e_wrn_msix_vec_wrong(cause, int_cfg);
         }
