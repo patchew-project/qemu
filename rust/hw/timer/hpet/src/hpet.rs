7d4901a824b00609db159e5b33f286c74fce9f66
Zhao Liu <zhao1.liu@intel.com>
2025-05-20 23:27:48 +0800

    rust/hpet: Drop BalCell wrapper for num_timers
    Currently, HPET adjusts num_timers in HPETState::realize(), and doesn't
    change it in any other method. And this field is initialized as a
    property.
    
    Meanwhile, please note that as a property, someone may change its
    default value in the future using qdev_prop_set_uint8() binding on
    either the C side or Rust side after HPET object creation. However,
    since this depends on QOM core code (on the C side) and all subsequent
    processes occur on the C side, there's no need for additional safety
    considerations as it doesn't cross FFI boundaries.
    
    Therefore, this field could be immutable after init() so as not to be
    necessary to have a BqlCell wrapper.
    
    Adjust num_timers in HPETState::init() and drop the BqlCell wrapper.
    
    Note, when num_timers came out of BqlCell, the capability field doesn't
    need BqlCell as well since it's read-only. But from the view of
    readability, it's best to keep consistent with the other registers.
    
    Signed-off-by: Zhao Liu <zhao1.liu@intel.com>
    Message-Id: <20250520152750.2542612-4-zhao1.liu@intel.com>

--- rust/hw/timer/hpet/src/hpet.rs
+++ rust/hw/timer/hpet/src/hpet.rs
@@ -562,7 +562,7 @@ pub struct HPETState {
     /// HPET timer array managed by this timer block.
     #[doc(alias = "timer")]
     timers: [BqlRefCell<HPETTimer>; HPET_MAX_TIMERS as usize],
-    num_timers: BqlCell<u8>,
+    num_timers: u8,
     num_timers_save: BqlCell<u8>,
 
     /// Instance id (HPET timer block ID).
@@ -570,11 +570,6 @@ pub struct HPETState {
 }
 
 impl HPETState {
-    // Get num_timers with `usize` type, which is useful to play with array index.
-    fn get_num_timers(&self) -> usize {
-        self.num_timers.get().into()
-    }
-
     const fn has_msi_flag(&self) -> bool {
         self.flags & (1 << HPET_FLAG_MSI_SUPPORT_SHIFT) != 0
     }
@@ -636,7 +631,7 @@ impl HPETState {
             self.hpet_offset
                 .set(ticks_to_ns(self.counter.get()) - CLOCK_VIRTUAL.get_ns());
 
-            for timer in self.timers.iter().take(self.get_num_timers()) {
+            for timer in self.timers.iter().take(self.num_timers.into()) {
                 let mut t = timer.borrow_mut();
 
                 if t.is_int_enabled() && t.is_int_active() {
@@ -648,7 +643,7 @@ impl HPETState {
             // Halt main counter and disable interrupt generation.
             self.counter.set(self.get_ticks());
 
-            for timer in self.timers.iter().take(self.get_num_timers()) {
+            for timer in self.timers.iter().take(self.num_timers.into()) {
                 timer.borrow_mut().del_timer();
             }
         }
@@ -671,7 +666,7 @@ impl HPETState {
         let new_val = val << shift;
         let cleared = new_val & self.int_status.get();
 
-        for (index, timer) in self.timers.iter().take(self.get_num_timers()).enumerate() {
+        for (index, timer) in self.timers.iter().take(self.num_timers.into()).enumerate() {
             if cleared & (1 << index) != 0 {
                 timer.borrow_mut().update_irq(false);
             }
@@ -715,6 +710,12 @@ impl HPETState {
             "hpet",
             HPET_REG_SPACE_LEN,
         );
+
+        if self.num_timers < HPET_MIN_TIMERS {
+            self.num_timers = HPET_MIN_TIMERS;
+        } else if self.num_timers > HPET_MAX_TIMERS {
+            self.num_timers = HPET_MAX_TIMERS;
+        }
     }
 
     fn post_init(&self) {
@@ -732,12 +733,6 @@ impl HPETState {
 
         self.hpet_id.set(HPETFwConfig::assign_hpet_id());
 
-        if self.num_timers.get() < HPET_MIN_TIMERS {
-            self.num_timers.set(HPET_MIN_TIMERS);
-        } else if self.num_timers.get() > HPET_MAX_TIMERS {
-            self.num_timers.set(HPET_MAX_TIMERS);
-        }
-
         self.init_timer();
         // 64-bit General Capabilities and ID Register; LegacyReplacementRoute.
         self.capability.set(
@@ -745,7 +740,7 @@ impl HPETState {
             1 << HPET_CAP_COUNT_SIZE_CAP_SHIFT |
             1 << HPET_CAP_LEG_RT_CAP_SHIFT |
             HPET_CAP_VENDER_ID_VALUE << HPET_CAP_VENDER_ID_SHIFT |
-            ((self.get_num_timers() - 1) as u64) << HPET_CAP_NUM_TIM_SHIFT | // indicate the last timer
+            ((self.num_timers - 1) as u64) << HPET_CAP_NUM_TIM_SHIFT | // indicate the last timer
             (HPET_CLK_PERIOD * FS_PER_NS) << HPET_CAP_CNT_CLK_PERIOD_SHIFT, // 10 ns
         );
 
@@ -754,7 +749,7 @@ impl HPETState {
     }
 
     fn reset_hold(&self, _type: ResetType) {
-        for timer in self.timers.iter().take(self.get_num_timers()) {
+        for timer in self.timers.iter().take(self.num_timers.into()) {
             timer.borrow_mut().reset();
         }
 
@@ -782,7 +777,7 @@ impl HPETState {
             GlobalRegister::try_from(addr).map(HPETRegister::Global)
         } else {
             let timer_id: usize = ((addr - 0x100) / 0x20) as usize;
-            if timer_id <= self.get_num_timers() {
+            if timer_id <= self.num_timers.into() {
                 // TODO: Add trace point - trace_hpet_ram_[read|write]_timer_id(timer_id)
                 TimerRegister::try_from(addr & 0x18)
                     .map(|reg| HPETRegister::Timer(&self.timers[timer_id], reg))
@@ -853,12 +848,12 @@ impl HPETState {
          * also added to the migration stream.  Check that it matches the value
          * that was configured.
          */
-        self.num_timers_save.set(self.num_timers.get());
+        self.num_timers_save.set(self.num_timers);
         0
     }
 
     fn post_load(&self, _version_id: u8) -> i32 {
-        for timer in self.timers.iter().take(self.get_num_timers()) {
+        for timer in self.timers.iter().take(self.num_timers.into()) {
             let mut t = timer.borrow_mut();
 
             t.cmp64 = t.calculate_cmp64(t.get_state().counter.get(), t.cmp);
@@ -883,7 +878,7 @@ impl HPETState {
     }
 
     fn validate_num_timers(&self, _version_id: u8) -> bool {
-        self.num_timers.get() == self.num_timers_save.get()
+        self.num_timers == self.num_timers_save.get()
     }
 }
 
