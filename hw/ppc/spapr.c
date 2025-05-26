patchew-project/qemu

commit 4ee9ced979e68522ee38ea759e7630ac70f15fa8

hw/ppc/spapr: simplify usb controller creation logic
Signed-off-by: Marcel Apfelbaum <marcel@redhat.com>
Reviewed-by: Stefan Hajnoczi <stefanha@redhat.com>
Reviewed-by: Paolo Bonzini <pbonzini@redhat.com>
Signed-off-by: Alexander Graf <agraf@suse.de>

hw/ppc/spapr.c
@@ -1528,9 +1528,10 @@ static void ppc_spapr_init(MachineState *machine)
     /* Graphics */
     if (spapr_vga_init(phb->bus)) {
         spapr->has_graphics = true;
+        machine->usb |= defaults_enabled();
     }
 
-    if ((spapr->has_graphics && defaults_enabled()) || usb_enabled()) {
+    if (machine->usb) {
         pci_create_simple(phb->bus, -1, "pci-ohci");
         if (spapr->has_graphics) {
             usbdevice_create("keyboard");

commit 401646983c52c0dce4e59114029859f5e305c30c

hw/ppc/spapr: Only create default devices when requested
Only create default devices when requested, avoid have the
machine implementation to modify the generic 'usb' boolean
set from command line.

Fixes: 4ee9ced979e ("hw/ppc/spapr: simplify usb controller creation logic")
Signed-off-by: Philippe Mathieu-Daudé <philmd@linaro.org>
Message-Id: <20250526130006.49817-2-philmd@linaro.org>

hw/ppc/spapr.c
@@ -3032,7 +3032,6 @@ static void spapr_machine_init(MachineState *machine)
     has_vga = spapr_vga_init(phb->bus, &error_fatal);
     if (has_vga) {
         spapr->want_stdout_path = !machine->enable_graphics;
-        machine->usb |= defaults_enabled() && !machine->usb_disabled;
     } else {
         spapr->want_stdout_path = true;
     }
@@ -3040,7 +3039,7 @@ static void spapr_machine_init(MachineState *machine)
     if (machine->usb) {
         pci_create_simple(phb->bus, -1, "nec-usb-xhci");
 
-        if (has_vga) {
+        if (has_vga && defaults_enabled()) {
             USBBus *usb_bus;
 
             usb_bus = USB_BUS(object_resolve_type_unambiguous(TYPE_USB_BUS,