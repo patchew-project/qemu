f4439e7b97f9c91e6b16333a1260145ba10da1d9
Annie Li <annie.li@oracle.com>
2025-05-28 12:40:10 -0400

    test/acpi: allow DSDT table changes for microvm
    List changed files in tests/qtest/bios-tables-test-allowed-diff.h
    
    Signed-off-by: Annie Li <annie.li@oracle.com>
    Message-Id: <20250528164010.2191-1-annie.li@oracle.com>

--- tests/qtest/bios-tables-test-allowed-diff.h
+++ tests/qtest/bios-tables-test-allowed-diff.h
@@ -1,1 +1,6 @@
 /* List of comma-separated changed AML files to ignore */
+"tests/data/acpi/x86/microvm/DSDT",
+"tests/data/acpi/x86/microvm/DSDT.ioapic2",
+"tests/data/acpi/x86/microvm/DSDT.pcie",
+"tests/data/acpi/x86/microvm/DSDT.rtc",
+"tests/data/acpi/x86/microvm/DSDT.usb",
