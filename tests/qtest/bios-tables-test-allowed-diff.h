fea676a2a4e7578432b3aae0e90dca12bd097bfa
Annie Li <annie.li@oracle.com>
2025-05-28 12:41:23 -0400

    tests/qtest/bios-table-tests: Update ACPI table binaries for microvm
    Following is the diff generated by the step 5 and 6 in
    tests/qtest/bios-tables-test.c
    
    Diff from iasl to show changes in DSDT table:
    
    @@ -1,30 +1,30 @@
     /*
      * Intel ACPI Component Architecture
      * AML/ASL+ Disassembler version 20210604 (64-bit version)
      * Copyright (c) 2000 - 2021 Intel Corporation
      *
      * Disassembling to symbolic ASL+ operators
      *
      * Disassembly of tests/data/acpi/x86/microvm/DSDT, Tue May 27 16:20:08 2025
      *
      * Original Table Header:
      *     Signature        "DSDT"
    - *     Length           0x0000016D (365)
    + *     Length           0x000001BA (442)
      *     Revision         0x02
    - *     Checksum         0x11
    + *     Checksum         0xD5
      *     OEM ID           "BOCHS "
      *     OEM Table ID     "BXPC    "
      *     OEM Revision     0x00000001 (1)
      *     Compiler ID      "BXPC"
      *     Compiler Version 0x00000001 (1)
      */
     DefinitionBlock ("", "DSDT", 2, "BOCHS ", "BXPC    ", 0x00000001)
     {
         Scope (_SB)
         {
             Device (FWCF)
             {
                 Name (_HID, "QEMU0002")  // _HID: Hardware ID
                 Name (_STA, 0x0B)  // _STA: Status
                 Name (_CRS, ResourceTemplate ()  // _CRS: Current Resource Settings
                 {
    @@ -66,56 +66,82 @@
                         0x00000009,
                     }
                 })
                 OperationRegion (EREG, SystemMemory, 0xFEA00000, 0x04)
                 Field (EREG, DWordAcc, NoLock, WriteAsZeros)
                 {
                     ESEL,   32
                 }
    
                 Method (_EVT, 1, Serialized)  // _EVT: Event
                 {
                     Local0 = ESEL /* \_SB_.GED_.ESEL */
                     If (((Local0 & 0x02) == 0x02))
                     {
                         Notify (PWRB, 0x80) // Status Change
                     }
    +
    +                If (((Local0 & 0x10) == 0x10))
    +                {
    +                    Notify (SLPB, 0x80) // Status Change
    +                }
                 }
             }
    
             Device (PWRB)
             {
                 Name (_HID, "PNP0C0C" /* Power Button Device */)  // _HID: Hardware ID
                 Name (_UID, Zero)  // _UID: Unique ID
             }
    
    +        Device (SLPB)
    +        {
    +            Name (_HID, EisaId ("PNP0C0E") /* Sleep Button Device */)  // _HID: Hardware ID
    +            OperationRegion (\SLP, SystemIO, 0x0201, One)
    +            Field (\SLP, ByteAcc, NoLock, WriteAsZeros)
    +            {
    +                SBP,    1
    +            }
    +        }
    +
             Device (VR07)
             {
                 Name (_HID, "LNRO0005")  // _HID: Hardware ID
                 Name (_UID, 0x07)  // _UID: Unique ID
                 Name (_CCA, One)  // _CCA: Cache Coherency Attribute
                 Name (_CRS, ResourceTemplate ()  // _CRS: Current Resource Settings
                 {
                     Memory32Fixed (ReadWrite,
                         0xFEB00E00,         // Address Base
                         0x00000200,         // Address Length
                         )
                     Interrupt (ResourceConsumer, Level, ActiveHigh, Exclusive, ,, )
                     {
                         0x00000017,
                     }
                 })
             }
         }
    
         Scope (\)
         {
    +        Name (_S3, Package (0x04)  // _S3_: S3 System State
    +        {
    +            0x03,
    +            Zero,
    +            Zero,
    +            Zero
    +        })
    +    }
    +
    +    Scope (\)
    +    {
             Name (_S5, Package (0x04)  // _S5_: S5 System State
             {
                 0x05,
                 Zero,
                 Zero,
                 Zero
             })
         }
     }
    
    Signed-off-by: Annie Li <annie.li@oracle.com>
    Message-Id: <20250528164123.2223-1-annie.li@oracle.com>

--- tests/qtest/bios-tables-test-allowed-diff.h
+++ tests/qtest/bios-tables-test-allowed-diff.h
@@ -1,6 +1,1 @@
 /* List of comma-separated changed AML files to ignore */
-"tests/data/acpi/x86/microvm/DSDT",
-"tests/data/acpi/x86/microvm/DSDT.ioapic2",
-"tests/data/acpi/x86/microvm/DSDT.pcie",
-"tests/data/acpi/x86/microvm/DSDT.rtc",
-"tests/data/acpi/x86/microvm/DSDT.usb",
