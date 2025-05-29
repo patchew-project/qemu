f82030bac7fd675e1445db588894e2219523a1ec
Vivek Kasireddy <vivek.kasireddy@intel.com>
2025-05-28 22:11:15 -0700

    ui/spice: Add an option to submit gl_draw requests at fixed rate
    In the specific case where the display layer (virtio-gpu) is using
    dmabuf, and if remote clients are enabled (-spice gl=on,port=xxxx),
    it makes sense to limit the maximum (streaming) rate (refresh rate)
    to a fixed value using the GUI refresh timer. Otherwise, the updates
    or gl_draw requests would be sent as soon as the Guest submits a new
    frame which is not optimal as it would lead to increased network
    traffic and wastage of GPU cycles if the frames get dropped.
    
    Cc: Gerd Hoffmann <kraxel@redhat.com>
    Cc: Marc-André Lureau <marcandre.lureau@redhat.com>
    Cc: Dmitry Osipenko <dmitry.osipenko@collabora.com>
    Cc: Frediano Ziglio <freddy77@gmail.com>
    Cc: Dongwon Kim <dongwon.kim@intel.com>
    Cc: Michael Scherle <michael.scherle@rz.uni-freiburg.de>
    Signed-off-by: Vivek Kasireddy <vivek.kasireddy@intel.com>
    Message-Id: <20250529051352.1409904-5-vivek.kasireddy@intel.com>

--- ui/spice-core.c
+++ ui/spice-core.c
@@ -56,6 +56,8 @@ struct SpiceTimer {
     QEMUTimer *timer;
 };
 
+#define DEFAULT_MAX_REFRESH_RATE 30
+
 static SpiceTimer *timer_add(SpiceTimerFunc func, void *opaque)
 {
     SpiceTimer *timer;
@@ -492,6 +494,9 @@ static QemuOptsList qemu_spice_opts = {
             .name = "video-codecs",
             .type = QEMU_OPT_STRING,
         },{
+            .name = "max-refresh-rate",
+            .type = QEMU_OPT_NUMBER,
+        },{
             .name = "agent-mouse",
             .type = QEMU_OPT_BOOL,
         },{
@@ -811,6 +816,13 @@ static void qemu_spice_init(void)
             error_report("invalid video codecs");
             exit(1);
         }
+    }
+
+    spice_max_refresh_rate = qemu_opt_get_number(opts, "max-refresh-rate",
+                                                 DEFAULT_MAX_REFRESH_RATE);
+    if (spice_max_refresh_rate <= 0) {
+        error_report("max refresh rate/fps is invalid");
+        exit(1);
     }
 
     spice_server_set_agent_mouse
