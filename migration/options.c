497eb2e7ddc89aaff2f8f1cac0dd4e1e40c744fd
Fabiano Rosas <farosas@suse.de>
2025-06-02 22:37:51 -0300

    migration: Remove MigrateSetParameters
    Now that the TLS options have been made the same between
    migrate-set-parameters and query-migrate-parameters, a single type can
    be used. Remove MigrateSetParameters.
    
    The TLS options documentation from MigrationParameters were replaced
    with the ones from MigrateSetParameters which was more complete.
    
    I'm choosing to somewhat ignore any ambiguity between "query" and
    "set" because other options' docs are already ambiguous in that
    regard.
    
    Signed-off-by: Fabiano Rosas <farosas@suse.de>
    Message-Id: <20250603013810.4772-3-farosas@suse.de>

--- migration/options.c
+++ migration/options.c
@@ -1227,7 +1227,7 @@ bool migrate_params_check(MigrationParameters *params, Error **errp)
     return true;
 }
 
-static void migrate_params_test_apply(MigrateSetParameters *params,
+static void migrate_params_test_apply(MigrationParameters *params,
                                       MigrationParameters *dest)
 {
     *dest = migrate_get_current()->parameters;
@@ -1350,7 +1350,7 @@ static void migrate_params_test_apply(MigrateSetParameters *params,
     }
 }
 
-static void migrate_params_apply(MigrateSetParameters *params, Error **errp)
+static void migrate_params_apply(MigrationParameters *params, Error **errp)
 {
     MigrationState *s = migrate_get_current();
 
@@ -1479,7 +1479,7 @@ static void migrate_params_apply(MigrateSetParameters *params, Error **errp)
     }
 }
 
-void qmp_migrate_set_parameters(MigrateSetParameters *params, Error **errp)
+void qmp_migrate_set_parameters(MigrationParameters *params, Error **errp)
 {
     MigrationParameters tmp;
 
