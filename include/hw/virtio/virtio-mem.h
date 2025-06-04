de97cf6fb64f6b6f9df550371ebd2a3b7b544f68
Zhenzhong Duan <zhenzhong.duan@intel.com>
2025-06-04 16:47:56 +0800

    virtio-mem: Fix definition of VirtIOMEMClass
    Parent of VirtIOMEMClass is VirtioDeviceClass rather than VirtIODevice.
    
    Fixes: 910b25766b33 ("virtio-mem: Paravirtualized memory hot(un)plug")
    Signed-off-by: Zhenzhong Duan <zhenzhong.duan@intel.com>
    Reviewed-by: David Hildenbrand <david@redhat.com>
    Reviewed-by: Philippe Mathieu-Daudé <philmd@linaro.org>
    Message-Id: <20250604084757.4035354-1-zhenzhong.duan@intel.com>

--- include/hw/virtio/virtio-mem.h
+++ include/hw/virtio/virtio-mem.h
@@ -134,7 +134,7 @@ struct VirtioMemSystemReset {
 
 struct VirtIOMEMClass {
     /* private */
-    VirtIODevice parent;
+    VirtioDeviceClass parent;
 
     /* public */
     void (*fill_device_info)(const VirtIOMEM *vmen, VirtioMEMDeviceInfo *vi);
