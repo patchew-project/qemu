452617351642013dae12937d7a3ed31faa05e005
John Levon <john.levon@nutanix.com>
2025-06-06 17:10:38 -0700

    vfio-user: add vfio-user class and container
    Introduce basic plumbing for vfio-user behind a new
    --enable-vfio-user-client option.
    
    We introduce VFIOUserContainer in hw/vfio-user/container.c, which is a
    container type for the "IOMMU" type "vfio-iommu-user", and share some
    common container code from hw/vfio/container.c.
    
    Add hw/vfio-user/pci.c for instantiating VFIOUserPCIDevice objects,
    sharing some common code from hw/vfio/pci.c.
    
    Originally-by: John Johnson <john.g.johnson@oracle.com>
    Signed-off-by: Elena Ufimtseva <elena.ufimtseva@oracle.com>
    Signed-off-by: Jagannathan Raman <jag.raman@oracle.com>
    Signed-off-by: John Levon <john.levon@nutanix.com>
    Message-Id: <20250607001056.335310-7-john.levon@nutanix.com>

---
+++ hw/vfio-user/container.h
@@ -0,0 +1,25 @@
+/*
+ * vfio-user specific definitions.
+ *
+ * This work is licensed under the terms of the GNU GPL, version 2.  See
+ * the COPYING file in the top-level directory.
+ *
+ * SPDX-License-Identifier: GPL-2.0-or-later
+ */
+
+#ifndef HW_VFIO_USER_CONTAINER_H
+#define HW_VFIO_USER_CONTAINER_H
+
+#include <inttypes.h>
+#include <stdbool.h>
+
+#include "hw/vfio/vfio-container-base.h"
+
+/* MMU container sub-class for vfio-user. */
+typedef struct VFIOUserContainer {
+    VFIOContainerBase bcontainer;
+} VFIOUserContainer;
+
+OBJECT_DECLARE_SIMPLE_TYPE(VFIOUserContainer, VFIO_IOMMU_USER);
+
+#endif /* HW_VFIO_USER_CONTAINER_H */
