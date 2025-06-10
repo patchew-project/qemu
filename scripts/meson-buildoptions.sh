88881ba6d87cc43e0149761d0dab7f426cd6c936
Bernhard Beschow <shentey@gmail.com>
2025-06-10 22:41:30 +0200

    configure: Add dtc option
    The next patch will make dtc mandatory for boards with a bundled DTB, causing
    these boards to be omitted when dtc is missing. Allow packagers to force the
    build of those boards by providing an option.
    
    Signed-off-by: Bernhard Beschow <shentey@gmail.com>
    Message-Id: <20250610204131.2862-5-shentey@gmail.com>

--- scripts/meson-buildoptions.sh
+++ scripts/meson-buildoptions.sh
@@ -116,6 +116,7 @@ meson_options_help() {
   printf "%s\n" '  dmg             dmg image format support'
   printf "%s\n" '  docs            Documentations build support'
   printf "%s\n" '  dsound          DirectSound sound support'
+  printf "%s\n" '  dtc             Whether to find the device tree compiler'
   printf "%s\n" '  fuse            FUSE block device export'
   printf "%s\n" '  fuse-lseek      SEEK_HOLE/SEEK_DATA support for FUSE exports'
   printf "%s\n" '  gcrypt          libgcrypt cryptography support'
@@ -310,6 +311,8 @@ _meson_option_parse() {
     --disable-docs) printf "%s" -Ddocs=disabled ;;
     --enable-dsound) printf "%s" -Ddsound=enabled ;;
     --disable-dsound) printf "%s" -Ddsound=disabled ;;
+    --enable-dtc) printf "%s" -Ddtc=enabled ;;
+    --disable-dtc) printf "%s" -Ddtc=disabled ;;
     --enable-fdt) printf "%s" -Dfdt=enabled ;;
     --disable-fdt) printf "%s" -Dfdt=disabled ;;
     --enable-fdt=*) quote_sh "-Dfdt=$2" ;;
