c1399c6226a813c2957ec6cc4c6d306ba05ab495
Bernhard Beschow <shentey@gmail.com>
2025-06-10 22:21:10 +0200

    rust/hw/char/pl011/src/device: Implement logging
    Now that there is logging support in Rust for QEMU, use it in the pl011
    device.
    
    Signed-off-by: Bernhard Beschow <shentey@gmail.com>
    Message-Id: <20250610202110.2243-3-shentey@gmail.com>

--- rust/hw/char/pl011/src/device.rs
+++ rust/hw/char/pl011/src/device.rs
@@ -8,6 +8,8 @@ use qemu_api::{
     chardev::{CharBackend, Chardev, Event},
     impl_vmstate_forward,
     irq::{IRQState, InterruptSource},
+    log::Log,
+    log_mask,
     memory::{hwaddr, MemoryRegion, MemoryRegionOps, MemoryRegionOpsBuilder},
     prelude::*,
     qdev::{Clock, ClockEvent, DeviceImpl, DeviceState, Property, ResetType, ResettablePhasesImpl},
@@ -275,8 +277,7 @@ impl PL011Registers {
             DMACR => {
                 self.dmacr = value;
                 if value & 3 > 0 {
-                    // qemu_log_mask(LOG_UNIMP, "pl011: DMA not implemented\n");
-                    eprintln!("pl011: DMA not implemented");
+                    log_mask!(Log::Unimp, "pl011: DMA not implemented\n");
                 }
             }
         }
@@ -538,7 +539,7 @@ impl PL011State {
                 u64::from(device_id[(offset - 0xfe0) >> 2])
             }
             Err(_) => {
-                // qemu_log_mask(LOG_GUEST_ERROR, "pl011_read: Bad offset 0x%x\n", (int)offset);
+                log_mask!(Log::GuestError, "pl011_read: Bad offset {offset}\n");
                 0
             }
             Ok(field) => {
@@ -570,7 +571,10 @@ impl PL011State {
                 .borrow_mut()
                 .write(field, value as u32, &self.char_backend);
         } else {
-            eprintln!("write bad offset {offset} value {value}");
+            log_mask!(
+                Log::GuestError,
+                "pl011_write: Bad offset {offset} value {value}\n"
+            );
         }
         if update_irq {
             self.update();
