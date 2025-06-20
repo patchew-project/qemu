ad73797eca502162ad24a52932515e44b3dd6881
Zhenzhong Duan <zhenzhong.duan@intel.com>
2025-06-20 15:17:57 +0800

    intel_iommu: Implement get_viommu_cap() callback
    Implement get_viommu_cap() callback and expose stage-1 capability for now.
    
    VFIO uses it to create nested parent domain which is further used to create
    nested domain in vIOMMU. All these will be implemented in following patches.
    
    Suggested-by: Yi Liu <yi.l.liu@intel.com>
    Signed-off-by: Zhenzhong Duan <zhenzhong.duan@intel.com>
    Message-Id: <20250620071813.55571-4-zhenzhong.duan@intel.com>

---
+++ include/hw/iommu.h
@@ -0,0 +1,14 @@
+/*
+ * General vIOMMU capabilities, flags, etc
+ *
+ * Copyright (C) 2025 Intel Corporation.
+ *
+ * SPDX-License-Identifier: GPL-2.0-or-later
+ */
+
+#ifndef HW_IOMMU_H
+#define HW_IOMMU_H
+
+#define VIOMMU_CAP_STAGE1    BIT_ULL(0)
+
+#endif /* HW_IOMMU_H */
