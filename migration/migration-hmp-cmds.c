2ce075b09cf6a2f7fff7e6b6464ece84f4c4036d
Fabiano Rosas <farosas@suse.de>
2025-06-30 16:58:52 -0300

    migration: Normalize tls arguments
    The migration parameters tls_creds, tls_authz and tls_hostname
    currently have a non-uniform handling. When used as arguments to
    migrate-set-parameters, their type is StrOrNull and when used as
    return value from query-migrate-parameters their type is a plain
    string.
    
    Not only having to convert between the types is cumbersome, but it
    also creates the issue of requiring two different QAPI types to be
    used, one for each command. MigrateSetParameters is used for
    migrate-set-parameters with the TLS arguments as StrOrNull while
    MigrationParameters is used for query-migrate-parameters with the TLS
    arguments as str.
    
    Since StrOrNull could be considered a superset of str, change the type
    of the TLS arguments in MigrationParameters to StrOrNull. Also ensure
    that QTYPE_QNULL is never used.
    
    1) migrate-set-parameters will always write QTYPE_QSTRING to
      s->parameters, either an empty or non-empty string.
    
    2) query-migrate-parameters will always return a QTYPE_QSTRING, either
      empty or non-empty.
    
    3) the migrate_tls_* helpers will always return a non-empty string or
      NULL, for the internal migration code's consumption.
    
    Points (1) and (2) above help simplify the parameters validation and
    the query command handling because s->parameters is already kept in
    the format that query-migrate-parameters (and info migrate_paramters)
    expect. Point (3) is so people don't need to care about StrOrNull in
    migration code.
    
    This will allow the type duplication to be removed in the next
    patches.
    
    Note that the type of @tls_creds, @tls-hostname, @tls-authz changes
    from str to StrOrNull in introspection of the query-migrate-parameters
    command. We accept this imprecision to enable de-duplication.
    
    There's no need to free the TLS options in
    migration_instance_finalize() because they're freed by the qdev
    properties .release method.
    
    Temporary in this patch:
    migrate_params_test_apply() copies s->parameters into a temporary
    structure, so it's necessary to drop the references to the TLS options
    if they were not set by the user to avoid double-free. This is fixed
    in the next patches.
    
    Signed-off-by: Fabiano Rosas <farosas@suse.de>
    Acked-by: Markus Armbruster <armbru@redhat.com>
    Message-Id: <20250630195913.28033-4-farosas@suse.de>

--- migration/migration-hmp-cmds.c
+++ migration/migration-hmp-cmds.c
@@ -279,11 +279,11 @@ void hmp_info_migrate_parameters(Monitor *mon, const QDict *qdict)
         assert(params->tls_creds);
         monitor_printf(mon, "%s: '%s'\n",
             MigrationParameter_str(MIGRATION_PARAMETER_TLS_CREDS),
-            params->tls_creds);
+                       params->tls_creds->u.s);
         assert(params->tls_hostname);
         monitor_printf(mon, "%s: '%s'\n",
             MigrationParameter_str(MIGRATION_PARAMETER_TLS_HOSTNAME),
-            params->tls_hostname);
+                       params->tls_hostname->u.s);
         assert(params->has_max_bandwidth);
         monitor_printf(mon, "%s: %" PRIu64 " bytes/second\n",
             MigrationParameter_str(MIGRATION_PARAMETER_MAX_BANDWIDTH),
@@ -317,9 +317,10 @@ void hmp_info_migrate_parameters(Monitor *mon, const QDict *qdict)
         monitor_printf(mon, "%s: %" PRIu64 "\n",
             MigrationParameter_str(MIGRATION_PARAMETER_MAX_POSTCOPY_BANDWIDTH),
             params->max_postcopy_bandwidth);
+        assert(params->tls_authz);
         monitor_printf(mon, "%s: '%s'\n",
             MigrationParameter_str(MIGRATION_PARAMETER_TLS_AUTHZ),
-            params->tls_authz);
+                       params->tls_authz->u.s);
 
         if (params->has_block_bitmap_mapping) {
             const BitmapMigrationNodeAliasList *bmnal;
