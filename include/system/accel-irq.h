ab82355de7b53e5afde68f724ce3889e06eb8d07
Magnus Kulke <magnuskulke@linux.microsoft.com>
2025-07-01 19:28:11 +0200

    hw/intc: Generalize APIC helper names from kvm_* to accel_*
    Rename APIC helper functions to use an accel_* prefix instead of kvm_*
    to support use by accelerators other than KVM. This is a preparatory
    step for integrating MSHV support with common APIC logic.
    
    Signed-off-by: Magnus Kulke <magnuskulke@linux.microsoft.com>
    Message-Id: <20250701172834.44849-5-magnuskulke@linux.microsoft.com>

---
+++ include/system/accel-irq.h
@@ -0,0 +1,26 @@
+#ifndef SYSEMU_ACCEL_H
+#define SYSEMU_ACCEL_H
+#include "hw/pci/msi.h"
+#include "qemu/osdep.h"
+#include "system/kvm.h"
+#include "system/mshv.h"
+
+static inline bool accel_msi_via_irqfd_enabled(void)
+{
+  return mshv_msi_via_irqfd_enabled() || kvm_msi_via_irqfd_enabled();
+}
+
+static inline bool accel_irqchip_is_split(void)
+{
+  return mshv_msi_via_irqfd_enabled() || kvm_irqchip_is_split();
+}
+
+int accel_irqchip_add_msi_route(KVMRouteChange *c, int vector, PCIDevice *dev);
+int accel_irqchip_update_msi_route(int vector, MSIMessage msg, PCIDevice *dev);
+void accel_irqchip_commit_route_changes(KVMRouteChange *c);
+void accel_irqchip_commit_routes(void);
+void accel_irqchip_release_virq(int virq);
+int accel_irqchip_add_irqfd_notifier_gsi(EventNotifier *n, EventNotifier *rn,
+                                         int virq);
+int accel_irqchip_remove_irqfd_notifier_gsi(EventNotifier *n, int virq);
+#endif
