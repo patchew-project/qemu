78686d89566951320012bcddd628480527a16506
Philippe Mathieu-Daudé <philmd@linaro.org>
2025-07-02 20:52:44 +0200

    qapi: Move definitions related to accelerators in their own file
    Extract TCG and KVM definitions from machine.json to accelerator.json.
    
    Signed-off-by: Philippe Mathieu-Daudé <philmd@linaro.org>
    Reviewed-by: Pierrick Bouvier <pierrick.bouvier@linaro.org>
    Reviewed-by: Zhao Liu <zhao1.liu@intel.com>
    Message-Id: <20250702185332.43650-23-philmd@linaro.org>

--- accel/tcg/monitor.c
+++ accel/tcg/monitor.c
@@ -11,7 +11,7 @@
 #include "qemu/qht.h"
 #include "qapi/error.h"
 #include "qapi/type-helpers.h"
-#include "qapi/qapi-commands-machine.h"
+#include "qapi/qapi-commands-accelerator.h"
 #include "monitor/monitor.h"
 #include "system/cpu-timers.h"
 #include "exec/icount.h"
