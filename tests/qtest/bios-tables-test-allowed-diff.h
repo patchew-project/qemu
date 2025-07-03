23a978f9fe1eda0a23d9ca06663c0c8e6a6a6f76
Eric Auger <eric.auger@redhat.com>
2025-07-03 14:35:10 +0200

    tests/qtest/bios-tables-test: Update DSDT blobs after GPEX _OSC change
    Update the reference DSDT blobs after GPEX _OSC change. The _OSC change
    affects the aarch64 'virt' and the x86 'microvm' machines.
    
    DSDT diff is the same for all the machines/tests:
    
      * Original Table Header:
      *     Signature        "DSDT"
    - *     Length           0x00001A4F (6735)
    + *     Length           0x00001A35 (6709)
      *     Revision         0x02
    - *     Checksum         0xBF
    + *     Checksum         0xDD
      *     OEM ID           "BOCHS "
      *     OEM Table ID     "BXPC    "
      *     OEM Revision     0x00000001 (1)
    @@ -1849,27 +1849,26 @@ DefinitionBlock ("", "DSDT", 2, "BOCHS ", "BXPC    ", 0x00000001)
                     {
                         CreateDWordField (Arg3, 0x04, CDW2)
                         CreateDWordField (Arg3, 0x08, CDW3)
    -                    SUPP = CDW2 /* \_SB_.PCI0._OSC.CDW2 */
    -                    CTRL = CDW3 /* \_SB_.PCI0._OSC.CDW3 */
    -                    CTRL &= 0x1F
    +                    Local0 = CDW3 /* \_SB_.PCI0._OSC.CDW3 */
    +                    Local0 &= 0x1F
                         If ((Arg1 != One))
                         {
                             CDW1 |= 0x08
                         }
    
    -                    If ((CDW3 != CTRL))
    +                    If ((CDW3 != Local0))
                         {
                             CDW1 |= 0x10
                         }
    
    -                    CDW3 = CTRL /* \_SB_.PCI0.CTRL */
    -                    Return (Arg3)
    +                    CDW3 = Local0
                     }
                     Else
                     {
                         CDW1 |= 0x04
    -                    Return (Arg3)
                     }
    +
    +                Return (Arg3)
                 }
    
                 Method (_DSM, 4, NotSerialized)  // _DSM: Device-Specific Method
    
    Signed-off-by: Eric Auger <eric.auger@redhat.com>
    Signed-off-by: Gustavo Romero <gustavo.romero@linaro.org>
    Reviewed-by: Jonathan Cameron <jonathan.cameron@huawei.com>
    
    Message-Id: <20250703123728.414386-11-eric.auger@redhat.com>

--- tests/qtest/bios-tables-test-allowed-diff.h
+++ tests/qtest/bios-tables-test-allowed-diff.h
@@ -1,8 +1,1 @@
 /* List of comma-separated changed AML files to ignore */
-"tests/data/acpi/aarch64/virt/DSDT",
-"tests/data/acpi/aarch64/virt/DSDT.acpihmatvirt",
-"tests/data/acpi/aarch64/virt/DSDT.memhp",
-"tests/data/acpi/aarch64/virt/DSDT.pxb",
-"tests/data/acpi/aarch64/virt/DSDT.topology",
-"tests/data/acpi/x86/microvm/DSDT.pcie",
-"tests/data/acpi/riscv64/virt/DSDT",
