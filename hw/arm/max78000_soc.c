1e7892f14317eb7bc427ab596f9d5836b2c0d14f
Jackson Donaldson <jackson88044@gmail.com>
2025-07-04 12:57:29 -0400

    MAX78000: Add AES to SOC
    This commit adds AES to max78000_soc
    
    Signed-off-by: Jackson Donaldson <jcksn@duck.com>
    Reviewed-by: Peter Maydell <peter.maydell@linaro.org>
    Message-Id: <20250704165729.208381-12-jcksn@duck.com>

--- hw/arm/max78000_soc.c
+++ hw/arm/max78000_soc.c
@@ -45,6 +45,8 @@ static void max78000_soc_initfn(Object *obj)
 
     object_initialize_child(obj, "trng", &s->trng, TYPE_MAX78000_TRNG);
 
+    object_initialize_child(obj, "aes", &s->aes, TYPE_MAX78000_AES);
+
     s->sysclk = qdev_init_clock_in(DEVICE(s), "sysclk", NULL, NULL, 0);
 }
 
@@ -54,6 +56,7 @@ static void max78000_soc_realize(DeviceState *dev_soc, Error **errp)
     MemoryRegion *system_memory = get_system_memory();
     DeviceState *dev, *gcrdev, *armv7m;
     g_autofree char *trng_link = NULL;
+    g_autofree char *aes_link = NULL;
     SysBusDevice *busdev;
     Error *err = NULL;
     int i;
@@ -137,6 +140,14 @@ static void max78000_soc_realize(DeviceState *dev_soc, Error **errp)
     trng_link = g_strdup("trng");
     object_property_set_link(OBJECT(gcrdev), trng_link, OBJECT(dev), &err);
 
+    dev = DEVICE(&s->aes);
+    sysbus_realize(SYS_BUS_DEVICE(dev), errp);
+    sysbus_mmio_map(SYS_BUS_DEVICE(dev), 0, 0x40007400);
+    sysbus_connect_irq(SYS_BUS_DEVICE(dev), 0, qdev_get_gpio_in(armv7m, 5));
+
+    aes_link = g_strdup("aes");
+    object_property_set_link(OBJECT(gcrdev), aes_link, OBJECT(dev), &err);
+
     dev = DEVICE(&s->gcr);
     sysbus_realize(SYS_BUS_DEVICE(dev), errp);
     sysbus_mmio_map(SYS_BUS_DEVICE(dev), 0, 0x40000000);
@@ -153,9 +164,6 @@ static void max78000_soc_realize(DeviceState *dev_soc, Error **errp)
     create_unimplemented_device("wakeupTimer",          0x40006400, 0x400);
     create_unimplemented_device("powerSequencer",       0x40006800, 0x400);
     create_unimplemented_device("miscControl",          0x40006c00, 0x400);
-
-    create_unimplemented_device("aes",                  0x40007400, 0x400);
-    create_unimplemented_device("aesKey",               0x40007800, 0x400);
 
     create_unimplemented_device("gpio0",                0x40008000, 0x1000);
     create_unimplemented_device("gpio1",                0x40009000, 0x1000);
