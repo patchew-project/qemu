08ee160b198d67b00ae01daf28edc78c607ddf34
Thomas Huth <thuth@redhat.com>
2025-07-08 14:56:26 +0200

    pc-bios/s390-ccw: Allow to select a different pxelinux.cfg entry via loadparm
    Since we're linking the network booting code into the main firmware
    binary nowadays, we can support the "loadparm" parameter now quite
    easily for pxelinux.cfg config files that contain multiple entries.
    
    Signed-off-by: Thomas Huth <thuth@redhat.com>
    Message-Id: <20250708125630.58676-2-thuth@redhat.com>

--- pc-bios/s390-ccw/netmain.c
+++ pc-bios/s390-ccw/netmain.c
@@ -332,6 +332,28 @@ static int load_kernel_with_initrd(filename_ip_t *fn_ip,
     return rc;
 }
 
+static int net_select_and_load_kernel(filename_ip_t *fn_ip,
+                                      int num_ent, int selected,
+                                      struct pl_cfg_entry *entries)
+{
+    unsigned int loadparm = get_loadparm_index();
+
+    if (num_ent <= 0) {
+        return -1;
+    }
+
+    if (loadparm > num_ent) {
+        printf("Error: loadparm is set to entry #%d, but there are only "
+               "%d entries in the pxelinux.cfg file!\n"
+               "Using default entry now instead.\n",
+               loadparm, num_ent);
+    } else if (loadparm > 0) {
+        selected = loadparm - 1;
+    }
+
+    return load_kernel_with_initrd(fn_ip, &entries[selected]);
+}
+
 #define MAX_PXELINUX_ENTRIES 16
 
 static int net_try_pxelinux_cfg(filename_ip_t *fn_ip)
@@ -343,11 +365,8 @@ static int net_try_pxelinux_cfg(filename_ip_t *fn_ip)
                                       DEFAULT_TFTP_RETRIES,
                                       cfgbuf, sizeof(cfgbuf),
                                       entries, MAX_PXELINUX_ENTRIES, &def_ent);
-    if (num_ent > 0) {
-        return load_kernel_with_initrd(fn_ip, &entries[def_ent]);
-    }
 
-    return -1;
+    return net_select_and_load_kernel(fn_ip, num_ent, def_ent, entries);
 }
 
 /**
@@ -433,10 +452,8 @@ static int net_try_direct_tftp_load(filename_ip_t *fn_ip)
 
             num_ent = pxelinux_parse_cfg(cfgbuf, sizeof(cfgbuf), entries,
                                          MAX_PXELINUX_ENTRIES, &def_ent);
-            if (num_ent <= 0) {
-                return -1;
-            }
-            return load_kernel_with_initrd(fn_ip, &entries[def_ent]);
+            return net_select_and_load_kernel(fn_ip, num_ent, def_ent,
+                                              entries);
         }
     }
 
