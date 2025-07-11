4ad024f636f5540d8f9086003becdc57f226857d
Collin Walling <walling@linux.ibm.com>
2025-07-11 17:10:48 -0400

    s390x/diag: Implement DIAG 508 subcode 1 for signature verification
    DIAG 508 subcode 1 performs signature-verification on signed components.
    A signed component may be a Linux kernel image, or any other signed
    binary. **Verification of initrd is not supported.**
    
    The instruction call expects two item-pairs: an address of a device
    component, an address of the analogous signature file (in PKCS#7 DER format),
    and their respective lengths. All of this data should be encapsulated
    within a Diag508SignatureVerificationBlock, with the CertificateStoreInfo
    fields ignored. The DIAG handler will read from the provided addresses
    to retrieve the necessary data, parse the signature file, then
    perform the signature-verification. Because there is no way to
    correlate a specific certificate to a component, each certificate
    in the store is tried until either verification succeeds, or all
    certs have been exhausted.
    
    The subcode value is denoted by setting the second-to-left-most bit of
    a 2-byte field.
    
    A return code of 1 indicates success, and the index and length of the
    corresponding certificate will be set in the CertificateStoreInfo
    portion of the SigVerifBlock. The following values indicate failure:
    
    	0x0202: component data is invalid
    	0x0302: signature is not in PKCS#7 format
    	0x0402: signature-verification failed
    
    Signed-off-by: Collin Walling <walling@linux.ibm.com>
    Signed-off-by: Zhuoying Cai <zycai@linux.ibm.com>
    Message-Id: <20250711211105.439554-13-zycai@linux.ibm.com>

--- include/hw/s390x/ipl/diag508.h
+++ include/hw/s390x/ipl/diag508.h
@@ -11,5 +11,28 @@
 #define S390X_DIAG508_H
 
 #define DIAG_508_SUBC_QUERY_SUBC    0x0000
+#define DIAG_508_SUBC_SIG_VERIF     0x8000
+
+#define DIAG_508_RC_OK              0x0001
+#define DIAG_508_RC_NO_CERTS        0x0102
+#define DIAG_508_RC_INVAL_COMP_DATA 0x0202
+#define DIAG_508_RC_INVAL_PKCS7_SIG 0x0302
+#define DIAG_508_RC_FAIL_VERIF      0x0402
+
+struct Diag508CertificateStoreInfo {
+    uint8_t  idx;
+    uint8_t  reserved[7];
+    uint64_t len;
+};
+typedef struct Diag508CertificateStoreInfo Diag508CertificateStoreInfo;
+
+struct Diag508SignatureVerificationBlock {
+    Diag508CertificateStoreInfo csi;
+    uint64_t comp_len;
+    uint64_t comp_addr;
+    uint64_t sig_len;
+    uint64_t sig_addr;
+};
+typedef struct Diag508SignatureVerificationBlock Diag508SignatureVerificationBlock;
 
 #endif
