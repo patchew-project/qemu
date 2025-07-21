3d2672922f49b83112e7ef446962bda7a66453b1
Vacha Bhavsar <vacha.bhavsar@oss.qualcomm.com>
2025-07-21 21:19:51 +0000

    target/arm: Fix big-endian handling of NEON gdb remote debugging
    This patch adds big endian support for NEON GDB remote
    debugging. It replaces the use of ldq_le_p() with the use of ldq_p() as
    explained in the first part of this patch series. Additionally, the order in
    which the buffer content is loaded into the CPU struct is switched depending
    on target endianness to ensure the most significant bits are always in second
    element.
    
    Signed-off-by: Vacha Bhavsar <vacha.bhavsar@oss.qualcomm.com>
    Message-Id: <20250721211952.2239714-2-vacha.bhavsar@oss.qualcomm.com>

--- target/arm/gdbstub64.c
+++ target/arm/gdbstub64.c
@@ -115,8 +115,16 @@ int aarch64_gdb_set_fpu_reg(CPUState *cs, uint8_t *buf, int reg)
         /* 128 bit FP register */
         {
             uint64_t *q = aa64_vfp_qreg(env, reg);
-            q[0] = ldq_le_p(buf);
-            q[1] = ldq_le_p(buf + 8);
+
+            if (target_big_endian()){
+                q[1] = ldq_p(buf);
+                q[0] = ldq_p(buf + 8);
+            }
+            else{
+                q[0] = ldq_p(buf);
+                q[1] = ldq_p(buf + 8);
+            }
+
             return 16;
         }
     case 32:
