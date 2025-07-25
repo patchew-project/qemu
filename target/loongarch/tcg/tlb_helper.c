96c3234b39ad93205b9bfc381c51b2c3c05d370b
Bibo Mao <maobibo@loongson.cn>
2025-07-25 09:47:11 +0800

    target/loongarch: Add parameter tlb pointer with fill_tlb_entry
    With function fill_tlb_entry(), it will update LoongArch emulated
    TLB information. Here parameter tlb pointer is added so that TLB
    entry will be updated based on relative TLB CSR registers.
    
    Signed-off-by: Bibo Mao <maobibo@loongson.cn>
    Reviewed-by: Richard Henderson <richard.henderson@linaro.org>
    Message-Id: <20250725014711.1025794-1-maobibo@loongson.cn>

--- target/loongarch/tcg/tlb_helper.c
+++ target/loongarch/tcg/tlb_helper.c
@@ -167,9 +167,8 @@ static void invalidate_tlb(CPULoongArchState *env, int index)
     invalidate_tlb_entry(env, index);
 }
 
-static void fill_tlb_entry(CPULoongArchState *env, int index)
+static void fill_tlb_entry(CPULoongArchState *env, LoongArchTLB *tlb)
 {
-    LoongArchTLB *tlb = &env->tlb[index];
     uint64_t lo0, lo1, csr_vppn;
     uint16_t csr_asid;
     uint8_t csr_ps;
@@ -337,7 +336,7 @@ void helper_tlbwr(CPULoongArchState *env)
         return;
     }
 
-    fill_tlb_entry(env, index);
+    fill_tlb_entry(env, env->tlb + index);
 }
 
 void helper_tlbfill(CPULoongArchState *env)
@@ -375,7 +374,7 @@ void helper_tlbfill(CPULoongArchState *env)
     }
 
     invalidate_tlb(env, index);
-    fill_tlb_entry(env, index);
+    fill_tlb_entry(env, env->tlb + index);
 }
 
 void helper_tlbclr(CPULoongArchState *env)
