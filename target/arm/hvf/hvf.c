0e1088680730e5468b8b8cf1e468c52212272634
Mohamed Mediouni <mohamed@unpredictable.fr>
2025-07-28 15:41:14 +0200

    target/arm: hvf: add asserts for code paths not leveraged when using the vGIC
    When using the vGIC, timers are directly handled by the platform.
    No vmexits ought to happen in that case. Abort if reaching those code paths.
    
    Signed-off-by: Mohamed Mediouni <mohamed@unpredictable.fr>
    Message-Id: <20250728134114.77545-14-mohamed@unpredictable.fr>

--- target/arm/hvf/hvf.c
+++ target/arm/hvf/hvf.c
@@ -1476,6 +1476,7 @@ static int hvf_sysreg_read(CPUState *cpu, uint32_t reg, uint64_t *val)
     case SYSREG_ICC_SGI1R_EL1:
     case SYSREG_ICC_SRE_EL1:
     case SYSREG_ICC_CTLR_EL1:
+        assert(!hvf_irqchip_in_kernel());
         /* Call the TCG sysreg handler. This is only safe for GICv3 regs. */
         if (hvf_sysreg_read_cp(cpu, reg, val)) {
             return 0;
@@ -1802,6 +1803,7 @@ static int hvf_sysreg_write(CPUState *cpu, uint32_t reg, uint64_t val)
     case SYSREG_ICC_SGI0R_EL1:
     case SYSREG_ICC_SGI1R_EL1:
     case SYSREG_ICC_SRE_EL1:
+        assert(!hvf_irqchip_in_kernel());
         /* Call the TCG sysreg handler. This is only safe for GICv3 regs. */
         if (hvf_sysreg_write_cp(cpu, reg, val)) {
             return 0;
@@ -2065,6 +2067,7 @@ int hvf_vcpu_exec(CPUState *cpu)
         /* This is the main one, handle below. */
         break;
     case HV_EXIT_REASON_VTIMER_ACTIVATED:
+        assert(!hvf_irqchip_in_kernel());
         qemu_set_irq(arm_cpu->gt_timer_outputs[GTIMER_VIRT], 1);
         cpu->accel->vtimer_masked = true;
         return 0;
