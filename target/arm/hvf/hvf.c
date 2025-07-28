9bd3899616df75e276f98a02b5674cff8f114f54
Mohamed Mediouni <mohamed@unpredictable.fr>
2025-07-28 15:41:10 +0200

    hvf: only call hvf_sync_vtimer() when running without the platform vGIC
    When running with the Apple vGIC, the EL1 vtimer is handled by the platform.
    
    Signed-off-by: Mohamed Mediouni <mohamed@unpredictable.fr>
    Message-Id: <20250728134114.77545-10-mohamed@unpredictable.fr>

--- target/arm/hvf/hvf.c
+++ target/arm/hvf/hvf.c
@@ -2007,7 +2007,8 @@ int hvf_vcpu_exec(CPUState *cpu)
         g_assert_not_reached();
     }
 
-    hvf_sync_vtimer(cpu);
+    if (!hvf_irqchip_in_kernel())
+        hvf_sync_vtimer(cpu);
 
     switch (ec) {
     case EC_SOFTWARESTEP: {
