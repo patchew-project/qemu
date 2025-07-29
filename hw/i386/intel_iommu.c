c99bc8e1970d57c0bcf2346a95a15b26a0bfeaff
Zhenzhong Duan <zhenzhong.duan@intel.com>
2025-07-29 05:20:42 -0400

    intel_iommu: Enable host device when x-flts=on in scalable mode
    Now that all infrastructures of supporting passthrough device running
    with stage-1 translation are there, enable it now.
    
    Signed-off-by: Zhenzhong Duan <zhenzhong.duan@intel.com>
    Message-Id: <20250729092043.785836-21-zhenzhong.duan@intel.com>

--- hw/i386/intel_iommu.c
+++ hw/i386/intel_iommu.c
@@ -5219,6 +5219,8 @@ static bool vtd_check_hiod(IntelIOMMUState *s, VTDHostIOMMUDevice *vtd_hiod,
                    "when x-flts=on");
         return false;
     }
+
+    return true;
 #endif
 
     error_setg(errp, "host IOMMU is incompatible with stage-1 translation");
