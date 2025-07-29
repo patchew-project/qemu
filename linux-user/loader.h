75de445e697c45b296bc7d5083de51dff89373cb
Richard Henderson <richard.henderson@linaro.org>
2025-07-29 13:58:38 -1000

    linux-user: Move hwcap functions to ppc/elfload.c
    Change the return type to abi_ulong, and pass in the cpu.
    
    Signed-off-by: Richard Henderson <richard.henderson@linaro.org>
    Reviewed-by: Peter Maydell <peter.maydell@linaro.org>
    Message-Id: <20250730000003.599084-6-richard.henderson@linaro.org>

--- linux-user/loader.h
+++ linux-user/loader.h
@@ -99,7 +99,7 @@ abi_long memcpy_to_target(abi_ulong dest, const void *src,
 extern unsigned long guest_stack_size;
 
 #if defined(TARGET_I386) || defined(TARGET_X86_64) || defined(TARGET_ARM) \
-    || defined(TARGET_SPARC)
+    || defined(TARGET_SPARC) || defined(TARGET_PPC)
 abi_ulong get_elf_hwcap(CPUState *cs);
 abi_ulong get_elf_hwcap2(CPUState *cs);
 #endif
