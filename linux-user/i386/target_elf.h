a107edf6d051a8157e6dc4cd0c94841005c3a52c
Richard Henderson <richard.henderson@linaro.org>
2025-07-29 13:59:36 -1000

    linux-user: Move elf parameters to {i386, x86_64}/target_elf.h
    Signed-off-by: Richard Henderson <richard.henderson@linaro.org>
    Message-Id: <20250730000003.599084-64-richard.henderson@linaro.org>

--- linux-user/i386/target_elf.h
+++ linux-user/i386/target_elf.h
@@ -8,4 +8,27 @@
 #ifndef I386_TARGET_ELF_H
 #define I386_TARGET_ELF_H
 
+#define ELF_CLASS               ELFCLASS32
+#define ELF_ARCH                EM_386
+#define EXSTACK_DEFAULT         true
+#define VDSO_HEADER             "vdso.c.inc"
+#define USE_ELF_CORE_DUMP
+
+/*
+ * This is used to ensure we don't load something for the wrong architecture.
+ */
+#define elf_check_arch(x) ( ((x) == EM_386) || ((x) == EM_486) )
+
+/*
+ * i386 is the only target which supplies AT_SYSINFO for the vdso.
+ * All others only supply AT_SYSINFO_EHDR.
+ */
+#define DLINFO_ARCH_ITEMS (vdso_info != NULL)
+#define ARCH_DLINFO                                     \
+    do {                                                \
+        if (vdso_info) {                                \
+            NEW_AUX_ENT(AT_SYSINFO, vdso_info->entry);  \
+        }                                               \
+    } while (0)
+
 #endif
