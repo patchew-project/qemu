e3903dd4df5ea0d7bfc89a39a30b5199d65b1b71
Richard Henderson <richard.henderson@linaro.org>
2025-07-29 13:59:50 -1000

    linux-user: Move elf parameters to xtensa/target_elf.h
    Signed-off-by: Richard Henderson <richard.henderson@linaro.org>
    Message-Id: <20250730000003.599084-78-richard.henderson@linaro.org>

--- linux-user/xtensa/target_elf.h
+++ linux-user/xtensa/target_elf.h
@@ -8,4 +8,8 @@
 #ifndef XTENSA_TARGET_ELF_H
 #define XTENSA_TARGET_ELF_H
 
+#define ELF_CLASS               ELFCLASS32
+#define ELF_ARCH                EM_XTENSA
+#define USE_ELF_CORE_DUMP
+
 #endif
