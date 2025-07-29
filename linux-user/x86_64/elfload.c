f35aa9e3603c457849f752a10e99aa6791c30d1e
Richard Henderson <richard.henderson@linaro.org>
2025-07-29 13:58:35 -1000

    linux-user: Move get_elf_hwcap to {i386, x86_64}/elfload.c
    Change the return type to abi_ulong, and pass in the cpu.
    Duplicate the one line function between i386 and x86_64,
    as most other additions to elfload.c won't be common.
    
    Signed-off-by: Richard Henderson <richard.henderson@linaro.org>
    Reviewed-by: Peter Maydell <peter.maydell@linaro.org>
    Message-Id: <20250730000003.599084-3-richard.henderson@linaro.org>

--- linux-user/x86_64/elfload.c
+++ linux-user/x86_64/elfload.c
@@ -1,1 +1,11 @@
 /* SPDX-License-Identifier: GPL-2.0-or-later */
+
+#include "qemu/osdep.h"
+#include "qemu.h"
+#include "loader.h"
+
+
+abi_ulong get_elf_hwcap(CPUState *cs)
+{
+    return cpu_env(cs)->features[FEAT_1_EDX];
+}
