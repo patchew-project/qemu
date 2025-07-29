1e8f3cfbea17b5f4221c689844d2a1ef0f97aeab
Richard Henderson <richard.henderson@linaro.org>
2025-07-29 13:58:43 -1000

    linux-user: Move get_elf_hwcap to riscv/elfload.c
    Change the return type to abi_ulong, and pass in the cpu.
    As this is the last instance of get_elf_hwcap to be converted,
    remove the ifdef around the declaration in loader.h.
    
    Signed-off-by: Richard Henderson <richard.henderson@linaro.org>
    Message-Id: <20250730000003.599084-11-richard.henderson@linaro.org>

--- linux-user/loader.h
+++ linux-user/loader.h
@@ -98,13 +98,8 @@ abi_long memcpy_to_target(abi_ulong dest, const void *src,
 
 extern unsigned long guest_stack_size;
 
-#if defined(TARGET_I386) || defined(TARGET_X86_64) || defined(TARGET_ARM) \
-    || defined(TARGET_SPARC) || defined(TARGET_PPC) \
-    || defined(TARGET_LOONGARCH64) || defined(TARGET_MIPS) \
-    || defined(TARGET_SH4) || defined(TARGET_S390X)
 abi_ulong get_elf_hwcap(CPUState *cs);
 abi_ulong get_elf_hwcap2(CPUState *cs);
-#endif
 const char *elf_hwcap_str(uint32_t bit);
 const char *elf_hwcap2_str(uint32_t bit);
 
