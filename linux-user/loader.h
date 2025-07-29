c47f0ea7a6cf969aefe250463fa84425b18473ba
Richard Henderson <richard.henderson@linaro.org>
2025-07-29 13:58:41 -1000

    linux-user: Move get_elf_hwcap to sh4/elfload.c
    Change the return type to abi_ulong, and pass in the cpu.
    
    Signed-off-by: Richard Henderson <richard.henderson@linaro.org>
    Message-Id: <20250730000003.599084-9-richard.henderson@linaro.org>

--- linux-user/loader.h
+++ linux-user/loader.h
@@ -100,7 +100,8 @@ extern unsigned long guest_stack_size;
 
 #if defined(TARGET_I386) || defined(TARGET_X86_64) || defined(TARGET_ARM) \
     || defined(TARGET_SPARC) || defined(TARGET_PPC) \
-    || defined(TARGET_LOONGARCH64) || defined(TARGET_MIPS)
+    || defined(TARGET_LOONGARCH64) || defined(TARGET_MIPS) \
+    || defined(TARGET_SH4)
 abi_ulong get_elf_hwcap(CPUState *cs);
 abi_ulong get_elf_hwcap2(CPUState *cs);
 #endif
