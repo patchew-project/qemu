9946667bcfa044ef863edc1f49210cb7c8370093
Richard Henderson <richard.henderson@linaro.org>
2025-08-03 09:04:42 +1000

    linux-user: Move elf parameters to m68k/target_elf.h
    Reviewed-by: Peter Maydell <peter.maydell@linaro.org>
    Signed-off-by: Richard Henderson <richard.henderson@linaro.org>
    Message-Id: <20250802230459.412251-79-richard.henderson@linaro.org>

--- linux-user/m68k/target_elf.h
+++ linux-user/m68k/target_elf.h
@@ -8,6 +8,9 @@
 #ifndef M68K_TARGET_ELF_H
 #define M68K_TARGET_ELF_H
 
+#define ELF_CLASS               ELFCLASS32
+#define ELF_ARCH                EM_68K
+
 /* See linux kernel: arch/m68k/include/asm/elf.h.  */
 #define ELF_NREG                20
 
