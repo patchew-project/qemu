5daf1c0d4405422d10ec595703b6eb2b2ece7c7d
Tanish Desai <tanishdesai37@gmail.com>
2025-08-04 11:20:39 +0000

    tracetool/format: remove redundent trace-event checks
    Remove redundent if(check_trace_event) check
    from individual backends.
    Adding CHECK_TRACE_EVENT_GET_STATE in log,syslog,
    dtrace and simple backend.
    
    Signed-off-by: Tanish Desai <tanishdesai37@gmail.com>
    Message-Id: <20250804112039.16377-3-tanishdesai37@gmail.com>

--- scripts/tracetool/backend/ftrace.py
+++ scripts/tracetool/backend/ftrace.py
@@ -16,6 +16,7 @@ from tracetool import out
 
 
 PUBLIC = True
+CHECK_TRACE_EVENT_GET_STATE = True
 
 
 def generate_h_begin(events, group):
@@ -32,18 +33,15 @@ def generate_h(event, group):
         '        char ftrace_buf[MAX_TRACE_STRLEN];',
         '        int unused __attribute__ ((unused));',
         '        int trlen;',
-        '        if (trace_event_get_state(%(event_id)s)) {',
         '#line %(event_lineno)d "%(event_filename)s"',
         '            trlen = snprintf(ftrace_buf, MAX_TRACE_STRLEN,',
         '                             "%(name)s " %(fmt)s "\\n" %(argnames)s);',
         '#line %(out_next_lineno)d "%(out_filename)s"',
         '            trlen = MIN(trlen, MAX_TRACE_STRLEN - 1);',
         '            unused = write(trace_marker_fd, ftrace_buf, trlen);',
-        '        }',
         '    }',
         name=event.name,
         args=event.args,
-        event_id="TRACE_" + event.name.upper(),
         event_lineno=event.lineno,
         event_filename=event.filename,
         fmt=event.fmt.rstrip("\n"),
