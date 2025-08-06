296f3811f0371ae95840ff4848b16dea009c2b38
Daniel P. Berrangé <berrange@redhat.com>
2025-08-06 17:48:30 +0100

    tracetool: add test suite for tracetool with reference output
    When reviewing tracetool patches it is often very unclear what the
    expected output will be for the generated backends. Compounding
    this is that a default build will only enable the 'log' trace
    backend, so developers won't see generated code for other backends
    without making a special effort. Some backends are also platform
    specific, so can't be enabled in QEMU builds, even though tracetool
    could generate the code.
    
    To address this, introduce a test suite for tracetool which is
    conceptually similar to the qapi-schema test. It is a simple
    python program that runs tracetool and compares the actual output
    to historical reference output kept in git. The test directly
    emits TAP format logs for ease of integration with meson.
    
    This can be run with
    
      make check-tracetool
    
    to make it easier for developers changing generated output, the
    sample expected content can be auto-recreated
    
      QEMU_TEST_REGENERATE=1 make check-tracetool
    
    and the changes reviewed and added to the commit. This will also
    assist reviewers interpreting the change.
    
    Developers are reminded of this in the test output on failure:
    
      $ make check-tracetool
      1/6 qemu:tracetool / dtrace        OK              0.14s
      2/6 qemu:tracetool / ftrace        FAIL            0.06s   exit status 1
      ――――――――――――――――――――――――――――――――――――――――――――――――――――――――――
      1..2
      ok 1 - ftrace.c
      #
      not ok 1 - ftrace.h (set QEMU_TEST_REGENERATE=1 to recreate reference output if tracetool generator was intentionally changed)
      ――――――――――――――――――――――――――――――――――――――――――――――――――――――――――
    
      3/6 qemu:tracetool / log           OK              0.06s
      4/6 qemu:tracetool / simple        OK              0.06s
      5/6 qemu:tracetool / syslog        OK              0.06s
      6/6 qemu:tracetool / ust           OK              0.11s
    
      Summary of Failures:
    
      2/6 qemu:tracetool / ftrace FAIL            0.06s   exit status 1
    
    Signed-off-by: Daniel P. Berrangé <berrange@redhat.com>
    Reviewed-by: Stefan Hajnoczi <stefanha@redhat.com>
    Message-Id: <20250806164832.1382919-7-berrange@redhat.com>

---
+++ tests/tracetool/tracetool-test.py
@@ -0,0 +1,91 @@
+#!/usr/bin/python3
+# SPDX-License-Identifier: GPL-2.0-or-later
+
+import os
+from subprocess import check_output
+import sys
+
+
+def get_backends():
+    return [
+    ]
+
+def get_formats(backend):
+    formats = [
+        "c",
+        "h",
+    ]
+    if backend == "dtrace":
+        formats += [
+            "d",
+            "log-stap",
+            "simpletrace-stap",
+            "stap",
+        ]
+    if backend == "ust":
+        formats += [
+            "ust-events-c",
+            "ust-events-h",
+        ]
+    return formats
+
+def test_tracetool_one(tracetool, events, backend, fmt):
+    filename = backend + "." + fmt
+
+    args = [tracetool,
+            f"--format={fmt}",
+            f"--backends={backend}",
+            "--group=testsuite"]
+
+    if fmt.find("stap") != -1:
+        args += ["--binary=qemu",
+                 "--probe-prefix=qemu"]
+
+    args += [events, "-"]
+
+    actual = check_output(args)
+
+    if os.getenv("QEMU_TEST_REGENERATE", False):
+        print(f"# regenerate {filename}")
+        with open(filename, "wb") as fh:
+            fh.write(actual)
+
+    with open(filename, "rb") as fh:
+        expect = fh.read()
+
+    assert(expect == actual)
+
+def test_tracetool(tracetool, events, backend):
+    fail = False
+    scenarios = len(get_formats(backend))
+
+    print(f"1..{scenarios}")
+
+    num = 1
+    for fmt in get_formats(backend):
+        status = "not ok"
+        hint = ""
+        try:
+            test_tracetool_one(tracetool, events, backend, fmt)
+            status = "ok"
+        except Exception as e:
+            print(f"# {e}")
+            fail = True
+            hint = " (set QEMU_TEST_REGENERATE=1 to recreate reference " + \
+                "output if tracetool generator was intentionally changed)"
+        finally:
+            print(f"{status} {num} - {backend}.{fmt}{hint}")
+
+    return fail
+
+
+if __name__ == '__main__':
+    if len(sys.argv) != 4:
+        argv0 = sys.argv[0]
+        print("syntax: {argv0} TRACE-TOOL TRACE-EVENTS BACKEND", file=sys.stderr)
+        sys.exit(1)
+
+    fail = test_tracetool(sys.argv[1], sys.argv[2], sys.argv[3])
+    if fail:
+        sys.exit(1)
+    sys.exit(0)
