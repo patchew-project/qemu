a8b4bec3eb22af94f94ce2a211014d5f294cbbda
Magnus Kulke <magnuskulke@linux.microsoft.com>
2025-08-07 16:39:33 +0200

    accel/mshv: Register memory region listeners
    Add memory listener hooks for the MSHV accelerator to track guest
    memory regions. This enables the backend to respond to region
    additions, removals and will be used to manage guest memory mappings
    inside the hypervisor.
    
    Actually registering physical memory in the hypervisor is still stubbed
    out.
    
    Signed-off-by: Magnus Kulke <magnuskulke@linux.microsoft.com>
    Message-Id: <20250807143951.1154713-9-magnuskulke@linux.microsoft.com>

---
+++ accel/mshv/mem.c
@@ -0,0 +1,24 @@
+/*
+ * QEMU MSHV support
+ *
+ * Copyright Microsoft, Corp. 2025
+ *
+ * Authors:
+ *  Magnus Kulke      <magnuskulke@microsoft.com>
+ *
+ * SPDX-License-Identifier: GPL-2.0-or-later
+ *
+ */
+
+#include "qemu/osdep.h"
+#include "qemu/error-report.h"
+#include "system/address-spaces.h"
+#include "system/mshv.h"
+
+void mshv_set_phys_mem(MshvMemoryListener *mml, MemoryRegionSection *section,
+                       bool add)
+{
+    error_report("unimplemented");
+    abort();
+}
+
