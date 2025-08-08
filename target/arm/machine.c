8a4d634f396a4d361ccf3938a97cd66c610ab0fe
Arun Menon <armenon@redhat.com>
2025-08-08 12:43:51 +0530

    migration: Rename post_save() to cleanup_save() and make it void
    The post_save() function call is responsible for cleaning up resources
    after the device state has been saved.
    Currently it is infallible, and does not return an error.
    
    It is called regardless of whether there is a preceding error from
    save or subsection save. That is, save and post_save() together are
    considered to be an atomic logical operation.
    
    It should not be confused as a counterpart of the post_load() function
    because post_load() does some sanity checks and returns an error if it
    fails. This commit, therefore, renames post_save() to cleanup_save()
    and makes it a void function.
    
    Signed-off-by: Arun Menon <armenon@redhat.com>
    Message-Id: <20250808-propagate_tpm_error-v10-25-3e81a1d419b2@redhat.com>

--- target/arm/machine.c
+++ target/arm/machine.c
@@ -903,15 +903,13 @@ static int cpu_pre_save(void *opaque)
     return 0;
 }
 
-static int cpu_post_save(void *opaque)
+static void cpu_cleanup_save(void *opaque)
 {
     ARMCPU *cpu = opaque;
 
     if (!kvm_enabled()) {
         pmu_op_finish(&cpu->env);
     }
-
-    return 0;
 }
 
 static int cpu_pre_load(void *opaque)
@@ -1048,7 +1046,7 @@ const VMStateDescription vmstate_arm_cpu = {
     .version_id = 22,
     .minimum_version_id = 22,
     .pre_save = cpu_pre_save,
-    .post_save = cpu_post_save,
+    .cleanup_save = cpu_cleanup_save,
     .pre_load = cpu_pre_load,
     .post_load = cpu_post_load,
     .fields = (const VMStateField[]) {
