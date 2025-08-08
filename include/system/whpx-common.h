b196acb3a6c658d95d73c6dbeaeb2b274677054b
Mohamed Mediouni <mohamed@unpredictable.fr>
2025-08-08 08:54:06 +0200

    whpx: ifdef out winhvemulation on non-x86_64
    winhvemulation is x86_64 only.
    
    In the future, we might want to get rid of winhvemulation usage
    entirely.
    
    Signed-off-by: Mohamed Mediouni <mohamed@unpredictable.fr>
    
    Reviewed-by: Pierrick Bouvier <pierrick.bouvier@linaro.org>
    Message-Id: <20250808065419.47415-6-mohamed@unpredictable.fr>

--- include/system/whpx-common.h
+++ include/system/whpx-common.h
@@ -3,7 +3,9 @@
 #define SYSTEM_WHPX_COMMON_H
 
 struct AccelCPUState {
+#ifdef __x86_64__
     WHV_EMULATOR_HANDLE emulator;
+#endif
     bool window_registered;
     bool interruptable;
     bool ready_for_pic_interrupt;
