8e85ad84922ef194bd76cd4ca2e9e7f7ea90688d
Kohei Tokunaga <ktokunaga.mail@gmail.com>
2025-08-27 01:10:35 +0900

    tcg/wasm: Enable instantiation of TBs executed many times
    This commit enables the instantiation and execution of TBs in wasm.c. As in
    TCI, the tcg_qemu_tb_exec function serves as the entrypoint for the TB
    execution, handling both instantiation and invocation of the Wasm
    module. Since browsers cause out of memory error if too many Wasm instances
    are created, this commit restricts instantiation to TBs that are called many
    times.
    
    This commit adds a counter (or its array if there are multiple threads) to
    the TB. Each time a TB is executed on TCI, the counter on TB is
    incremented. If it reaches to a threshold, that TB is instantiated as Wasm
    via instantiate_wasm.
    
    The total number of instances are tracked by the instances_global variable
    and its maximum number is limited by MAX_INSTANCES. When a Wasm module is
    instantiated, instances_global is incremented and the instance's function
    pointer is recorded to an array of WasmInstanceInfo.
    
    Each TB refers to the WasmInstanceInfo entry via WasmTBHeader's info_ptr (or
    its array if there are multiple threads). This allows tcg_qemu_tb_exec to
    resolve the instance's function pointer from the TB.
    
    When a new instantiation would exceed the limit, the Wasm backend doesn't
    perform instantiation (i.e. TB continues execution on TCI). Instead, it
    triggers the removal of older Wasm instances using Emscripten's
    removeFunction function. Once the removal is completed and detected via
    FinalizationRegistry API[1], instances_global is decremented, allowing new
    modules to be instantiated.
    
    [1] https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/FinalizationRegistry
    
    Signed-off-by: Kohei Tokunaga <ktokunaga.mail@gmail.com>
    Message-Id: <9051ee1caff3d49d199abc82f788d8abdcdeeffc.1756216429.git.ktokunaga.mail@gmail.com>

--- tcg/wasm.h
+++ tcg/wasm.h
@@ -47,6 +47,14 @@ static inline uintptr_t call_wasm_tb(wasm_tb_func f, struct WasmContext *ctx)
 }
 
 /*
+ * WasmInstanceInfo holds the relationship between TB and Wasm instance.
+ */
+struct WasmInstanceInfo {
+    void *tb_ptr;
+    wasm_tb_func tb_func;
+};
+
+/*
  * A TB of the Wasm backend starts from a header which contains pointers for
  * each data stored in the following region in the TB.
  */
@@ -67,6 +75,43 @@ struct WasmTBHeader {
      */
     void *import_ptr;
     int import_size;
+
+    /*
+     * Counter holds how many times the TB is executed before the instantiation
+     * for each thread.
+     */
+    int32_t *counter_ptr;
+
+    /*
+     * Pointer to the instance information on each thread.
+     */
+    struct WasmInstanceInfo **info_ptr;
 };
+
+static inline void *get_tci_ptr(void *tb_ptr)
+{
+    return ((struct WasmTBHeader *)tb_ptr)->tci_ptr;
+}
+
+static inline int32_t get_counter(void *tb_ptr, int idx)
+{
+    return ((struct WasmTBHeader *)tb_ptr)->counter_ptr[idx];
+}
+
+static inline void set_counter(void *tb_ptr, int idx, int v)
+{
+    ((struct WasmTBHeader *)tb_ptr)->counter_ptr[idx] = v;
+}
+
+static inline struct WasmInstanceInfo *get_info(void *tb_ptr, int idx)
+{
+    return ((struct WasmTBHeader *)tb_ptr)->info_ptr[idx];
+}
+
+static inline void set_info(void *tb_ptr, int idx,
+                            struct WasmInstanceInfo *info)
+{
+    ((struct WasmTBHeader *)tb_ptr)->info_ptr[idx] = info;
+}
 
 #endif
