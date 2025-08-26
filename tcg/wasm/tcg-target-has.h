b47ead5edd73328210f1d6d0ece6553fd9e741a6
Kohei Tokunaga <ktokunaga.mail@gmail.com>
2025-08-27 01:10:21 +0900

    tcg/wasm: Add ext instructions
    The ext operations are implemented using the corresponding instructions in
    Wasm. TCI instructions are also generated in the same way as the original
    TCI backend.
    
    Signed-off-by: Kohei Tokunaga <ktokunaga.mail@gmail.com>
    Message-Id: <004734452e90077ea9f98ab7b093ce16fc1f5bae.1756216429.git.ktokunaga.mail@gmail.com>

--- tcg/wasm/tcg-target-has.h
+++ tcg/wasm/tcg-target-has.h
@@ -3,6 +3,7 @@
 #define TCG_TARGET_HAS_H
 
 #define TCG_TARGET_HAS_tst 0
+#define TCG_TARGET_HAS_extr_i64_i32 0
 
 #define TCG_TARGET_extract_valid(type, ofs, len) 0
 #define TCG_TARGET_sextract_valid(type, ofs, len) \
