f9c82c00c4238d5043c4f8ebce9dec3a676ead5a
Kohei Tokunaga <ktokunaga.mail@gmail.com>
2025-09-01 20:44:29 +0900

    tcg/wasm: Write wasm binary to TB
    This commit updates tcg_out_tb_start and tcg_out_tb_end to emit Wasm
    binaries into the TB code buffer. The generated Wasm binary defines a
    function of type wasm_tb_func which takes a WasmContext, executes the TB,
    and returns a result. In the Wasm backend, each TB starts with a
    WasmTBHeader which contains pointers to the following data:
    
    - TCI code
    - Wasm code
    - Array of helper function pointers imported into the Wasm instance
    
    tcg_out_tb_start writes the WasmTBHeader to the code buffer. tcg_out_tb_end
    generates the full Wasm executable binary by creating the Wasm module header
    following the spec[1][2][3] and copying the Wasm code body from sub_buf to
    the TB. This Wasm binary is placed after the TCI code which was emitted
    earlier.
    
    Additionally, an array of imported function pointers is appended to the
    TB. They are used during Wasm module instantiation. Function are imported to
    Wasm with names like "helper.0", "helper.1", etc., where the number
    corresponds to the array index.
    
    Each function's type signature must also be encoded in the Wasm module
    header. To support this, every emission of "call", "qemu_ld" and "qemu_st"
    operations also records the target function's type information in a buffer
    which will be copied to the code buffer during tcg_out_tb_end.
    
    Memory is shared between QEMU and the TBs and is imported to the Wasm module
    with the name "env.memory".
    
    [1] https://webassembly.github.io/spec/core/binary/modules.html
    [2] https://github.com/WebAssembly/threads/blob/b2567bff61ee6fbe731934f0ed17a5d48dc9ab01/proposals/threads/Overview.md
    [3] https://github.com/WebAssembly/memory64/blob/9003cd5e24e53b84cd9027ea3dd7ae57159a6db1/proposals/memory64/Overview.md
    
    Signed-off-by: Kohei Tokunaga <ktokunaga.mail@gmail.com>
    Message-Id: <4d80cd33e4a7d49d4d1e778d58fbedd8b6528bdc.1756724464.git.ktokunaga.mail@gmail.com>

--- tcg/wasm.h
+++ tcg/wasm.h
@@ -32,4 +32,30 @@ struct WasmContext {
     uint64_t *stack;
 };
 
+/* Instantiated Wasm function of a TB */
+typedef uintptr_t (*wasm_tb_func)(struct WasmContext *);
+
+/*
+ * A TB of the Wasm backend starts from a header which contains pointers for
+ * each data stored in the following region in the TB.
+ */
+struct WasmTBHeader {
+    /*
+     * Pointer to the region containing TCI instructions.
+     */
+    void *tci_ptr;
+
+    /*
+     * Pointer to the region containing Wasm instructions.
+     */
+    void *wasm_ptr;
+    int wasm_size;
+
+    /*
+     * Pointer to the array containing imported function pointers.
+     */
+    void *import_ptr;
+    int import_size;
+};
+
 #endif
