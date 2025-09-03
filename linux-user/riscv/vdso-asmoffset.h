a3bec43ce513325513d47d5108fcef2a9fb5f24e
Nicholas Piggin <npiggin@gmail.com>
2025-09-03 14:25:08 +1000

    linux-user/riscv: Add extended state to sigcontext
    Linux/riscv has extended the sigcontext with padding and an
    extended state structure that can save various optional
    features like vector in a flexible format. Update the
    linux-user signal handling to this new structure.
    
    Signed-off-by: Nicholas Piggin <npiggin@gmail.com>
    Message-Id: <20250903042510.279954-3-npiggin@gmail.com>

--- linux-user/riscv/vdso-asmoffset.h
+++ linux-user/riscv/vdso-asmoffset.h
@@ -3,7 +3,7 @@
 # define offsetof_uc_mcontext   0x120
 # define offsetof_freg0         0x80
 #else
-# define sizeof_rt_sigframe     0x340
+# define sizeof_rt_sigframe     0x440
 # define offsetof_uc_mcontext   0x130
 # define offsetof_freg0         0x100
 #endif
