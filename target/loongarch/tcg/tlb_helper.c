71a32192b12ba1d9dbf7a935078b47ea66b97930
Bibo Mao <maobibo@loongson.cn>
2025-09-03 16:48:16 +0800

    target/loongarch: Use mmu idx bitmap method when flush TLB
    With API tlb_flush_range_by_mmuidx(), bitmap of mmu idx should be used
    rather than itself. Also MMU_KERNEL_IDX and MMU_USER_IDX are used rather
    than current running mmu idx when flush TLB.
    
    Signed-off-by: Bibo Mao <maobibo@loongson.cn>
    Reviewed-by: Richard Henderson <richard.henderson@linaro.org>
    Message-Id: <20250903084827.3085911-2-maobibo@loongson.cn>

--- target/loongarch/tcg/tlb_helper.c
+++ target/loongarch/tcg/tlb_helper.c
@@ -101,8 +101,7 @@ static void invalidate_tlb_entry(CPULoongArchState *env, int index)
     target_ulong addr, mask, pagesize;
     uint8_t tlb_ps;
     LoongArchTLB *tlb = &env->tlb[index];
-
-    int mmu_idx = cpu_mmu_index(env_cpu(env), false);
+    int mmu_idx = BIT(MMU_KERNEL_IDX) | BIT(MMU_USER_IDX);
     uint8_t tlb_v0 = FIELD_EX64(tlb->tlb_entry0, TLBENTRY, V);
     uint8_t tlb_v1 = FIELD_EX64(tlb->tlb_entry1, TLBENTRY, V);
     uint64_t tlb_vppn = FIELD_EX64(tlb->tlb_misc, TLB_MISC, VPPN);
