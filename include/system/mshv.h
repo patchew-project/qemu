b68c1a53a2c1d9c79175e09d58fad981f5cb5123
Magnus Kulke <magnuskulke@linux.microsoft.com>
2025-09-16 18:48:43 +0200

    accel/mshv: Handle overlapping mem mappings
    QEMU maps certain regions into the guest multiple times, as seen in the
    trace below. Currently the MSHV kernel driver will reject those
    mappings. To workaround this, a record is kept (a static global list of
    "slots", inspired by what the HVF accelerator has implemented). An
    overlapping region is not registered at the hypervisor, and marked as
    mapped=false. If there is an UNMAPPED_GPA exit, we can look for a slot
    that is unmapped and would cover the GPA. In this case we map out the
    conflicting slot and map in the requested region.
    
    mshv_set_phys_mem       add=1 name=pc.bios
    mshv_map_memory      => u_a=7ffff4e00000 gpa=00fffc0000 size=00040000
    mshv_set_phys_mem       add=1 name=ioapic
    mshv_set_phys_mem       add=1 name=hpet
    mshv_set_phys_mem       add=0 name=pc.ram
    mshv_unmap_memory       u_a=7fff67e00000 gpa=0000000000 size=80000000
    mshv_set_phys_mem       add=1 name=pc.ram
    mshv_map_memory         u_a=7fff67e00000 gpa=0000000000 size=000c0000
    mshv_set_phys_mem       add=1 name=pc.rom
    mshv_map_memory         u_a=7ffff4c00000 gpa=00000c0000 size=00020000
    mshv_set_phys_mem       add=1 name=pc.bios
    mshv_remap_attempt   => u_a=7ffff4e20000 gpa=00000e0000 size=00020000
    
    The mapping table is guarded by a mutex for concurrent modification and
    RCU mechanisms for concurrent reads. Writes occur rarely, but we'll have
    to verify whether an unmapped region exist for each UNMAPPED_GPA exit,
    which happens frequently.
    
    Signed-off-by: Magnus Kulke <magnuskulke@linux.microsoft.com>
    Message-Id: <20250916164847.77883-24-magnuskulke@linux.microsoft.com>

--- include/system/mshv.h
+++ include/system/mshv.h
@@ -40,6 +40,8 @@ typedef struct hyperv_message hv_message;
 
 #define MSHV_MSR_ENTRIES_COUNT 64
 
+#define MSHV_MAX_MEM_SLOTS 32
+
 #ifdef CONFIG_MSHV_IS_POSSIBLE
 extern bool mshv_allowed;
 #define mshv_enabled() (mshv_allowed)
@@ -54,6 +56,12 @@ typedef struct MshvAddressSpace {
     AddressSpace *as;
 } MshvAddressSpace;
 
+typedef struct MshvMemorySlotManager {
+    size_t n_slots;
+    GList *slots;
+    QemuMutex mutex;
+} MshvMemorySlotManager;
+
 typedef struct MshvState {
     AccelState parent_obj;
     int vm;
@@ -62,6 +70,7 @@ typedef struct MshvState {
     int nr_as;
     MshvAddressSpace *as;
     int fd;
+    MshvMemorySlotManager msm;
 } MshvState;
 extern MshvState *mshv_state;
 
@@ -103,6 +112,12 @@ typedef enum MshvVmExit {
     MshvVmExitSpecial  = 2,
 } MshvVmExit;
 
+typedef enum MshvRemapResult {
+    MshvRemapOk = 0,
+    MshvRemapNoMapping = 1,
+    MshvRemapNoOverlap = 2,
+} MshvRemapResult;
+
 void mshv_init_mmio_emu(void);
 int mshv_create_vcpu(int vm_fd, uint8_t vp_index, int *cpu_fd);
 void mshv_remove_vcpu(int vm_fd, int cpu_fd);
@@ -139,21 +154,22 @@ int mshv_configure_msr(const CPUState *cpu, const MshvMsrEntry *msrs,
                        size_t n_msrs);
 
 /* memory */
-typedef struct MshvMemoryRegion {
+typedef struct MshvMemorySlot {
     uint64_t guest_phys_addr;
     uint64_t memory_size;
     uint64_t userspace_addr;
     bool readonly;
-} MshvMemoryRegion;
+    bool mapped;
+} MshvMemorySlot;
 
-int mshv_add_mem(int vm_fd, const MshvMemoryRegion *mr);
-int mshv_remove_mem(int vm_fd, const MshvMemoryRegion *mr);
+MshvRemapResult mshv_remap_overlap_region(int vm_fd, uint64_t gpa);
 int mshv_guest_mem_read(uint64_t gpa, uint8_t *data, uintptr_t size,
                         bool is_secure_mode, bool instruction_fetch);
 int mshv_guest_mem_write(uint64_t gpa, const uint8_t *data, uintptr_t size,
                          bool is_secure_mode);
 void mshv_set_phys_mem(MshvMemoryListener *mml, MemoryRegionSection *section,
                        bool add);
+void mshv_init_memory_slot_manager(MshvState *mshv_state);
 
 /* interrupt */
 void mshv_init_msicontrol(void);
