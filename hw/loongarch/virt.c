2f9c2eb7cb3c9fcda9ea7c567dc23d305ed9f158
Song Gao <gaosong@loongson.cn>
2025-09-16 20:21:09 +0800

    hw/loongarch: Implement DINTC plug/unplug interfaces
    when cpu added, connect dintc irq to cpu INT_DMSI irq pin.
    
    Reviewed-by: Bibo Mao <maobibo@loongson.cn>
    Signed-off-by: Song Gao <gaosong@loongson.cn>
    Message-Id: <20250916122109.749813-12-gaosong@loongson.cn>

--- hw/loongarch/virt.c
+++ hw/loongarch/virt.c
@@ -381,6 +381,8 @@ static void virt_cpu_irq_init(LoongArchVirtMachineState *lvms)
                              &error_abort);
         hotplug_handler_plug(HOTPLUG_HANDLER(lvms->extioi), DEVICE(cs),
                              &error_abort);
+        hotplug_handler_plug(HOTPLUG_HANDLER(lvms->dintc), DEVICE(cs),
+                             &error_abort);
     }
 }
 
@@ -1103,6 +1105,9 @@ static void virt_cpu_unplug(HotplugHandler *hotplug_dev,
     /* Notify ipi and extioi irqchip to remove interrupt routing to CPU */
     hotplug_handler_unplug(HOTPLUG_HANDLER(lvms->ipi), dev, &error_abort);
     hotplug_handler_unplug(HOTPLUG_HANDLER(lvms->extioi), dev, &error_abort);
+    if (lvms->dintc) {
+        hotplug_handler_unplug(HOTPLUG_HANDLER(lvms->dintc), dev, &error_abort);
+    }
 
     /* Notify acpi ged CPU removed */
     hotplug_handler_unplug(HOTPLUG_HANDLER(lvms->acpi_ged), dev, &error_abort);
@@ -1124,6 +1129,10 @@ static void virt_cpu_plug(HotplugHandler *hotplug_dev,
 
     if (lvms->extioi) {
         hotplug_handler_plug(HOTPLUG_HANDLER(lvms->extioi), dev, &error_abort);
+    }
+
+    if (lvms->dintc) {
+        hotplug_handler_plug(HOTPLUG_HANDLER(lvms->dintc), dev, &error_abort);
     }
 
     if (lvms->acpi_ged) {
