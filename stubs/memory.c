41f13dca0f0b3f2ee2b0ec7aa438db8cb5409fcc
Akihiko Odaki <odaki@rsg.ci.i.u-tokyo.ac.jp>
2025-09-17 19:32:48 +0900

    qdev: Automatically delete memory subregions
    It is a common requirement of qdev to delete memory subregions to hide
    them from address spaces during unrealization. pci automatically
    deletes the IO subregions, but this process is manually implemented
    in other places, which is tedious and error-prone.
    
    Implement the logic to delete subregions in qdev to cover all devices.
    
    Signed-off-by: Akihiko Odaki <odaki@rsg.ci.i.u-tokyo.ac.jp>
    Message-Id: <20250917-subregion-v1-2-bef37d9b4f73@rsg.ci.i.u-tokyo.ac.jp>

---
+++ stubs/memory.c
@@ -0,0 +1,9 @@
+/* SPDX-License-Identifier: GPL-2.0-or-later */
+
+#include "qemu/osdep.h"
+#include "system/memory.h"
+
+void memory_region_del_subregion(MemoryRegion *mr,
+                                 MemoryRegion *subregion)
+{
+}
