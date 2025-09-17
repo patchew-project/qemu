de80429619ce9abfa5e609efedc1eac6472e297a
Zhuoying Cai <zycai@linux.ibm.com>
2025-09-17 19:21:11 -0400

    s390x/diag: Implement DIAG 320 subcode 2
    DIAG 320 subcode 2 provides verification-certificates (VCs) that are in the
    certificate store. Only X509 certificates in DER format and SHA-256 hash
    type are recognized.
    
    The subcode value is denoted by setting the second-left-most bit
    of an 8-byte field.
    
    The Verification Certificate Block (VCB) contains the output data
    when the operation completes successfully. It includes a common
    header followed by zero or more Verification Certificate Entries (VCEs),
    depending on the VCB input length and the VC range (from the first VC
    index to the last VC index) in the certificate store.
    
    Each VCE contains information about a certificate retrieved from
    the S390IPLCertificateStore, such as the certificate name, key type,
    key ID length, hash length, and the raw certificate data.
    The key ID and hash are extracted from the raw certificate by the crypto API.
    
    Note: SHA2-256 VC hash type is required for retrieving the hash
    (fingerprint) of the certificate.
    
    Signed-off-by: Zhuoying Cai <zycai@linux.ibm.com>
    Message-Id: <20250917232131.495848-10-zycai@linux.ibm.com>

--- include/hw/s390x/ipl/diag320.h
+++ include/hw/s390x/ipl/diag320.h
@@ -12,18 +12,29 @@
 
 #define DIAG_320_SUBC_QUERY_ISM     0
 #define DIAG_320_SUBC_QUERY_VCSI    1
+#define DIAG_320_SUBC_STORE_VC      2
 
 #define DIAG_320_RC_OK              0x0001
 #define DIAG_320_RC_NOT_SUPPORTED   0x0102
 #define DIAG_320_RC_INVAL_VCSSB_LEN 0x0202
+#define DIAG_320_RC_INVAL_VCB_LEN   0x0204
+#define DIAG_320_RC_BAD_RANGE       0x0302
 
 #define DIAG_320_ISM_QUERY_SUBCODES 0x80000000
 #define DIAG_320_ISM_QUERY_VCSI     0x40000000
+#define DIAG_320_ISM_STORE_VC       0x20000000
 
 #define VCSSB_NO_VC     4
 #define VCSSB_MIN_LEN   128
 #define VCE_HEADER_LEN  128
+#define VCE_INVALID_LEN 72
 #define VCB_HEADER_LEN  64
+
+#define DIAG_320_VCE_FLAGS_VALID                0x80
+#define DIAG_320_VCE_KEYTYPE_SELF_DESCRIBING    0
+#define DIAG_320_VCE_KEYTYPE_ECDSA_P521         1
+#define DIAG_320_VCE_FORMAT_X509_DER            1
+#define DIAG_320_VCE_HASHTYPE_SHA2_256          1
 
 struct VCStorageSizeBlock {
     uint32_t length;
@@ -38,5 +49,43 @@ struct VCStorageSizeBlock {
     uint32_t reserved4[10];
 };
 typedef struct VCStorageSizeBlock VCStorageSizeBlock;
+
+struct VCBlock {
+    uint32_t in_len;
+    uint32_t reserved0;
+    uint16_t first_vc_index;
+    uint16_t last_vc_index;
+    uint32_t reserved1[5];
+    uint32_t out_len;
+    uint8_t reserved2[3];
+    uint8_t version;
+    uint16_t stored_ct;
+    uint16_t remain_ct;
+    uint32_t reserved3[5];
+    uint8_t vce_buf[];
+};
+typedef struct VCBlock VCBlock;
+
+struct VCEntry {
+    uint32_t len;
+    uint8_t flags;
+    uint8_t key_type;
+    uint16_t cert_idx;
+    uint32_t name[16];
+    uint8_t format;
+    uint8_t reserved0;
+    uint16_t keyid_len;
+    uint8_t reserved1;
+    uint8_t hash_type;
+    uint16_t hash_len;
+    uint32_t reserved2;
+    uint32_t cert_len;
+    uint32_t reserved3[2];
+    uint16_t hash_offset;
+    uint16_t cert_offset;
+    uint32_t reserved4[7];
+    uint8_t cert_buf[];
+};
+typedef struct VCEntry VCEntry;
 
 #endif
