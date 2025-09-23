5b5d264de94440aa2f07b627e333437a3fe7d859
Vladimir Sementsov-Ogievskiy <vsementsov@yandex-team.ru>
2025-09-23 13:01:00 +0300

    net/tap: rework tap_set_sndbuf()
    Keep NetdevTapOptions related logic in tap.c, and make tap_set_sndbuf a
    simple system call wrapper, more like other functions in tap-linux.c
    
    Signed-off-by: Vladimir Sementsov-Ogievskiy <vsementsov@yandex-team.ru>
    Reviewed-by: davydov-max@yandex-team.ru
    Tested-by: Lei Yang <leiyang@redhat.com>
    Message-Id: <20250923100110.70862-10-vsementsov@yandex-team.ru>

--- net/tap.c
+++ net/tap.c
@@ -690,10 +690,14 @@ static void net_init_tap_one(const NetdevTapOptions *tap, NetClientState *peer,
 {
     TAPState *s = net_tap_new(peer, model, name);
     int vhostfd;
+    bool sndbuf_required = tap->has_sndbuf;
+    int sndbuf =
+        (tap->has_sndbuf && tap->sndbuf) ? MIN(tap->sndbuf, INT_MAX) : INT_MAX;
 
     net_tap_set_fd(s, fd, vnet_hdr);
 
-    if (!tap_set_sndbuf(s->fd, tap, errp)) {
+    if (!tap_set_sndbuf(fd, sndbuf, sndbuf_required ? errp : NULL) &&
+        sndbuf_required) {
         goto failed;
     }
 
