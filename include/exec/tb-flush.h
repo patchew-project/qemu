8649bfd3c83a7316c232ea7abaacffa36be84ab2
Richard Henderson <richard.henderson@linaro.org>
2025-09-22 19:39:15 -0700

    accel/tcg: Remove tb_flush
    All uses have been replaced with tb_flush__exclusive.
    
    Reviewed-by: Pierrick Bouvier <pierrick.bouvier@linaro.org>
    Signed-off-by: Richard Henderson <richard.henderson@linaro.org>
    Reviewed-by: Philippe Mathieu-Daudé <philmd@linaro.org>
    Tested-by: Philippe Mathieu-Daudé <philmd@linaro.org>
    Message-Id: <20250923023922.3102471-12-richard.henderson@linaro.org>

--- include/exec/tb-flush.h
+++ include/exec/tb-flush.h
@@ -21,21 +21,6 @@
  */
 void tb_flush__exclusive(void);
 
-/**
- * tb_flush() - flush all translation blocks
- * @cs: CPUState (must be valid, but treated as anonymous pointer)
- *
- * Used to flush all the translation blocks in the system. Sometimes
- * it is simpler to flush everything than work out which individual
- * translations are now invalid and ensure they are not called
- * anymore.
- *
- * tb_flush() takes care of running the flush in an exclusive context
- * if it is not already running in one. This means no guest code will
- * run until this complete.
- */
-void tb_flush(CPUState *cs);
-
 void tcg_flush_jmp_cache(CPUState *cs);
 
 #endif /* _TB_FLUSH_H_ */
