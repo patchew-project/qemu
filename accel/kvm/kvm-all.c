cf4f9053bcfe706d8fbd7be761006c72bf147a42
Zhenzhong Duan <zhenzhong.duan@intel.com>
2025-09-28 04:54:32 -0400

    accel/kvm: Fix SIGSEGV when execute "query-balloon" after CPR transfer
    After CPR transfer, source QEMU closes kvm fd and sets kvm_state to NULL,
    "query-balloon" will check kvm_state->sync_mmu and trigger NULL pointer
    reference.
    
    We don't need to NULL kvm_state as all states in kvm_state aren't released
    actually. Just closing kvm fd is enough so we could still query states
    through "query_*" qmp command.
    
    Opportunistically drop an unnecessary check in kvm_close().
    
    Fixes: 7ed0919119b0 ("migration: close kvm after cpr")
    Suggested-by: Markus Armbruster <armbru@redhat.com>
    Signed-off-by: Zhenzhong Duan <zhenzhong.duan@intel.com>
    Message-Id: <20250928085432.40107-7-zhenzhong.duan@intel.com>

--- accel/kvm/kvm-all.c
+++ accel/kvm/kvm-all.c
@@ -639,13 +639,10 @@ void kvm_close(void)
         cpu->kvm_vcpu_stats_fd = -1;
     }
 
-    if (kvm_state && kvm_state->fd != -1) {
-        close(kvm_state->vmfd);
-        kvm_state->vmfd = -1;
-        close(kvm_state->fd);
-        kvm_state->fd = -1;
-    }
-    kvm_state = NULL;
+    close(kvm_state->vmfd);
+    kvm_state->vmfd = -1;
+    close(kvm_state->fd);
+    kvm_state->fd = -1;
 }
 
 /*
