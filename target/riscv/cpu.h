c0e921cf839b3a0e306c9e5a7bb42b19a21390ab
Anton Johansson via <qemu-devel@nongnu.org>
2025-10-01 09:32:48 +0200

    target/riscv: Fix size of priv_ver and vext_ver
    Fix these fields to 32 bits, also update corresponding priv_ver field
    in DisasContext as well as function arguments. 32 bits was chosen
    since it's large enough to fit all stored values and int/int32_t is
    used in RISCVCPUDef and a few functions.
    
    Signed-off-by: Anton Johansson <anjo@rev.ng>
    Reviewed-by: Pierrick Bouvier <pierrick.bouvier@linaro.org>
    Message-Id: <20251001073306.28573-16-anjo@rev.ng>

--- target/riscv/cpu.h
+++ target/riscv/cpu.h
@@ -238,8 +238,8 @@ struct CPUArchState {
 
     uint64_t guest_phys_fault_addr;
 
-    target_ulong priv_ver;
-    target_ulong vext_ver;
+    uint32_t priv_ver;
+    uint32_t vext_ver;
 
     /* RISCVMXL, but uint32_t for vmstate migration */
     uint32_t misa_mxl;      /* current mxl */
@@ -798,7 +798,7 @@ static inline RISCVMXL riscv_cpu_sxl(CPURISCVState *env)
 #endif
 
 static inline bool riscv_cpu_allow_16bit_insn(const RISCVCPUConfig *cfg,
-                                              target_long priv_ver,
+                                              uint32_t priv_ver,
                                               uint32_t misa_ext)
 {
     /* In priv spec version 1.12 or newer, C always implies Zca */
