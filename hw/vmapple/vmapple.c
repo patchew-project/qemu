6705803a629e0e84d1efa07a4fd70181d2662359
Mohamed Mediouni <mohamed@unpredictable.fr>
2025-10-07 22:31:50 +0200

    hw: vmapple: include missing headers
    Disablement by default led to:
    
    ../hw/vmapple/vmapple.c:276:39: error: use of undeclared identifier 'GTIMER_VIRT'
      276 |         qdev_connect_gpio_out(cpudev, GTIMER_VIRT,
          |                                       ^
    ../hw/vmapple/vmapple.c:479:54: error: use of undeclared identifier 'QEMU_PSCI_CONDUIT_HVC'
      479 |         object_property_set_int(cpu, "psci-conduit", QEMU_PSCI_CONDUIT_HVC,
          |                                                      ^
    ../hw/vmapple/vmapple.c:556:13: error: call to undeclared function 'arm_build_mp_affinity'; ISO C99 and later do not support implicit function declarations [-Wimplicit-function-declaration]
      556 |             arm_build_mp_affinity(n, GICV3_TARGETLIST_BITS);
          |             ^
    3 errors generated.
    
    pretty quickly.
    
    Signed-off-by: Mohamed Mediouni <mohamed@unpredictable.fr>
    Message-Id: <20251007203153.30136-2-mohamed@unpredictable.fr>

--- hw/vmapple/vmapple.c
+++ hw/vmapple/vmapple.c
@@ -51,6 +51,8 @@
 #include "system/reset.h"
 #include "system/runstate.h"
 #include "system/system.h"
+#include "gtimer.h"
+#include "cpu.h"
 
 struct VMAppleMachineState {
     MachineState parent;
