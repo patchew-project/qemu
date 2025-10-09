955bbc3a460d96dcabf00801e57d67e6006cffe4
Harsh Prateek Bora <harshpb@linux.ibm.com>
2025-10-09 14:40:56 -0400

    ppc/spapr: remove deprecated machine pseries-4.1
    Remove the pseries-4.1 machine specific logic as had been deprecated and
    due for removal now as per policy.
    
    Suggested-by: Cédric Le Goater <clg@kaod.org>
    Signed-off-by: Harsh Prateek Bora <harshpb@linux.ibm.com>
    Reviewed-by: Cédric Le Goater <clg@redhat.com>
    Message-Id: <20251009184057.19973-5-harshpb@linux.ibm.com>

--- include/hw/ppc/spapr.h
+++ include/hw/ppc/spapr.h
@@ -144,8 +144,6 @@ struct SpaprMachineClass {
 
     /*< public >*/
     uint32_t nr_xirqs;
-    bool linux_pci_probe;
-    bool smp_threads_vsmt; /* set VSMT to smp_threads by default */
     hwaddr rma_limit;          /* clamp the RMA to this size */
     bool pre_5_1_assoc_refpoints;
     bool pre_5_2_numa_associativity;
