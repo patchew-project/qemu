74cf7d14071575ed86d66ce4d1187efd4c404db5
Thomas Huth <thuth@redhat.com>
2025-10-10 11:32:43 +0200

    tests: Evict stale files in the functional download cache after a while
    The download cache of the functional tests is currently only growing.
    But sometimes tests get removed or changed to use different assets,
    thus we should clean up the stale old assets after a while when they
    are not in use anymore. So add a script that looks at the time stamps
    of the assets and removes them if they haven't been touched for more
    than half of a year. Since there might also be some assets around that
    have been added to the cache before we added the time stamp files,
    assume a default time stamp that is close to the creation date of this
    patch, so that we don't delete these files too early.
    
    Signed-off-by: Thomas Huth <thuth@redhat.com>
    Message-Id: <20251010093244.807544-3-thuth@redhat.com>

---
+++ scripts/clean_functional_cache.py
@@ -0,0 +1,47 @@
+#!/usr/bin/env python3
+#
+# SPDX-License-Identifier: GPL-2.0-or-later
+#
+"""Delete stale assets from the download cache of the functional tests"""
+
+import os
+import stat
+import sys
+import time
+from pathlib import Path
+
+
+cache_dir_env = os.getenv('QEMU_TEST_CACHE_DIR')
+if cache_dir_env:
+    cache_dir = Path(cache_dir_env, "download")
+else:
+    cache_dir = Path(Path("~").expanduser(), ".cache", "qemu", "download")
+
+if not os.path.exists(cache_dir):
+    print(f"Cache dir {cache_dir} does not exist!", file=sys.stderr)
+    sys.exit(1)
+
+os.chdir(cache_dir)
+
+for file in os.listdir(cache_dir):
+    filename = os.fsdecode(file)
+    # Only consider the files that use a sha256 as filename:
+    if len(filename) != 64:
+        continue
+
+    try:
+        with open(filename + ".stamp", "r", encoding='utf-8') as fh:
+            timestamp = int(fh.read())
+    except FileNotFoundError:
+        # Assume it's an old file that was already in the cache before we
+        # added the code for evicting stale assets. Use the release date
+        # of QEMU v10.1 as a default timestamp.
+        timestamp = time.mktime((2025, 8, 26, 0, 0, 0, 0, 0, 0))
+
+    age = time.time() - timestamp
+
+    # Delete files older than half of a year (183 days * 24h * 60m * 60s)
+    if age > 15811200:
+        print(f"Removing {cache_dir}/{filename}.")
+        os.chmod(filename, stat.S_IWRITE)
+        os.remove(filename)
