9ad4deaf782b5350bb5fe76f0c27f7c29f4fbc0c
Philippe Mathieu-Daudé <philmd@linaro.org>
2025-10-10 15:42:12 +0200

    ui: Replace HOST_BIG_ENDIAN #ifdef with runtime if() check
    Replace compile-time #ifdef with a runtime check to ensure all code
    paths are built and tested. This reduces build-time configuration
    complexity and improves maintainability.
    
    No functional change intended.
    
    Signed-off-by: Philippe Mathieu-Daudé <philmd@linaro.org>
    Reviewed-by: Marc-André Lureau <marcandre.lureau@redhat.com>
    Message-Id: <20251010134226.72221-4-philmd@linaro.org>

--- ui/vnc.c
+++ ui/vnc.c
@@ -2348,11 +2348,7 @@ static void pixel_format_message (VncState *vs) {
     vnc_write_u8(vs, vs->client_pf.bits_per_pixel); /* bits-per-pixel */
     vnc_write_u8(vs, vs->client_pf.depth); /* depth */
 
-#if HOST_BIG_ENDIAN
-    vnc_write_u8(vs, 1);             /* big-endian-flag */
-#else
-    vnc_write_u8(vs, 0);             /* big-endian-flag */
-#endif
+    vnc_write_u8(vs, HOST_BIG_ENDIAN);         /* big-endian-flag */
     vnc_write_u8(vs, 1);             /* true-color-flag */
     vnc_write_u16(vs, vs->client_pf.rmax);     /* red-max */
     vnc_write_u16(vs, vs->client_pf.gmax);     /* green-max */
