cd22e50159a8ee94d6d4723abb3ab76a43e65834
Philippe Mathieu-Daudé <philmd@linaro.org>
2025-10-17 15:29:53 +0200

    hw/s390x/ccw: Remove deprecated s390-ccw-virtio-5.1 machine
    This machine has been supported for a period of more than 6 years.
    According to our versioned machine support policy (see commit
    ce80c4fa6ff "docs: document special exception for machine type
    deprecation & removal") it can now be removed.
    
    Signed-off-by: Philippe Mathieu-Daudé <philmd@linaro.org>
    Message-Id: <20251017133002.61410-5-philmd@linaro.org>

--- hw/s390x/s390-virtio-ccw.c
+++ hw/s390x/s390-virtio-ccw.c
@@ -1121,18 +1121,6 @@ static void ccw_machine_5_2_class_options(MachineClass *mc)
 }
 DEFINE_CCW_MACHINE(5, 2);
 
-static void ccw_machine_5_1_instance_options(MachineState *machine)
-{
-    ccw_machine_5_2_instance_options(machine);
-}
-
-static void ccw_machine_5_1_class_options(MachineClass *mc)
-{
-    ccw_machine_5_2_class_options(mc);
-    compat_props_add(mc->compat_props, hw_compat_5_1, hw_compat_5_1_len);
-}
-DEFINE_CCW_MACHINE(5, 1);
-
 static void ccw_machine_register_types(void)
 {
     type_register_static(&ccw_machine_info);
