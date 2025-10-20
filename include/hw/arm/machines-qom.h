724ee0915849873487de3f4e71b72dedaa0eb607
Philippe Mathieu-Daudé <philmd@linaro.org>
2025-10-21 00:09:16 +0200

    hw/arm: Add DEFINE_MACHINE_[ARM_]AARCH64() macros
    A machine defined with the DEFINE_MACHINE_ARM_AARCH64() macro
    will be available on qemu-system-arm and qemu-system-aarch64
    binaries.
    
    One defined with DEFINE_MACHINE_AARCH64() will only be available
    in the qemu-system-aarch64 binary.
    
    Signed-off-by: Philippe Mathieu-Daudé <philmd@linaro.org>
    Reviewed-by: Pierrick Bouvier <pierrick.bouvier@linaro.org>
    Message-Id: <20251020220941.65269-8-philmd@linaro.org>

--- include/hw/arm/machines-qom.h
+++ include/hw/arm/machines-qom.h
@@ -9,10 +9,23 @@
 #ifndef HW_ARM_MACHINES_QOM_H
 #define HW_ARM_MACHINES_QOM_H
 
+#include "hw/boards.h"
+
 #define TYPE_TARGET_ARM_MACHINE \
         "target-info-arm-machine"
 
 #define TYPE_TARGET_AARCH64_MACHINE \
         "target-info-aarch64-machine"
+
+extern const InterfaceInfo arm_aarch64_machine_interfaces[];
+extern const InterfaceInfo aarch64_machine_interfaces[];
+
+#define DEFINE_MACHINE_ARM_AARCH64(namestr, machine_initfn) \
+        DEFINE_MACHINE_WITH_INTERFACE_ARRAY(namestr, machine_initfn, \
+                                            arm_aarch64_machine_interfaces)
+
+#define DEFINE_MACHINE_AARCH64(namestr, machine_initfn) \
+        DEFINE_MACHINE_WITH_INTERFACE_ARRAY(namestr, machine_initfn, \
+                                            aarch64_machine_interfaces)
 
 #endif
