e38481eebb61259f783c4b23fda76dc3acab8ce4
Marc-André Lureau <marcandre.lureau@redhat.com>
2025-10-21 13:02:46 +0400

    hw/audio: drop audio_model.isa
    That's no longer necessary, the code is bus-agnostic.
    
    Signed-off-by: Marc-André Lureau <marcandre.lureau@redhat.com>
    Message-Id: <20251021090317.425409-16-marcandre.lureau@redhat.com>

--- include/hw/audio/model.h
+++ include/hw/audio/model.h
@@ -4,7 +4,7 @@
 void audio_register_model_with_cb(const char *name, const char *descr,
                                   int (*init_audio_model)(const char *audiodev));
 void audio_register_model(const char *name, const char *descr,
-                          int isa, const char *typename);
+                          const char *typename);
 
 void audio_model_init(void);
 void audio_print_available_models(void);
