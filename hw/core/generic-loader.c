bb9322a6b9998ec0229277e5472e75e12cbb9ff7
Vishal Chourasia <vishalc@linux.ibm.com>
2025-10-21 16:24:43 +0530

    hw/core: Pass errp to load_image_targphys_as()
    Pass errp to load_image_targphys_as() in generic-loader and
    guest-loader to capture detailed error information from the
    loader functions.
    
    Use error_prepend() instead of error_setg() to preserve the
    underlying error details while adding context about which image
    failed to load.
    
    Signed-off-by: Vishal Chourasia <vishalc@linux.ibm.com>
    Message-Id: <20251021105442.1474602-8-vishalc@linux.ibm.com>

--- hw/core/generic-loader.c
+++ hw/core/generic-loader.c
@@ -60,6 +60,7 @@ static void generic_loader_reset(void *opaque)
 
 static void generic_loader_realize(DeviceState *dev, Error **errp)
 {
+    ERRP_GUARD();
     GenericLoaderState *s = GENERIC_LOADER(dev);
     hwaddr entry;
     ssize_t size = 0;
@@ -149,13 +150,13 @@ static void generic_loader_realize(DeviceState *dev, Error **errp)
         if (size < 0 || s->force_raw) {
             /* Default to the maximum size being the machine's ram size */
             size = load_image_targphys_as(s->file, s->addr,
-                    current_machine->ram_size, as, NULL);
+                    current_machine->ram_size, as, errp);
         } else {
             s->addr = entry;
         }
 
-        if (size < 0) {
-            error_setg(errp, "Cannot load specified image %s", s->file);
+        if (*errp) {
+            error_prepend(errp, "Cannot load specified image %s: ", s->file);
             return;
         }
     }
