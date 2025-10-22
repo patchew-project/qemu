d57789d2f5af2d4ae53968494d5f483b4523ecab
Vishal Chourasia <vishalc@linux.ibm.com>
2025-10-23 00:57:22 +0530

    hw/ppc: Pass errp to load_image_targphys() and report errors
    Pass errp to load_image_targphys() calls in ppc machine initialization
    to capture detailed error information when loading firmware, kernel,
    and initrd images.
    
    Pass error_fatal which automatically reports detailed error messages and
    exits immediately on failure. Eliminating redundant exit(1) calls, as
    error_fatal handles termination
    
    The behavior remains functionally identical, but error messages now
    come directly from the loader function with more context about the
    failure cause.
    
    Signed-off-by: Vishal Chourasia <vishalc@linux.ibm.com>
    Message-Id: <20251022192717.1644251-11-vishalc@linux.ibm.com>

--- hw/ppc/pegasos2.c
+++ hw/ppc/pegasos2.c
@@ -164,7 +164,7 @@ static void pegasos2_init(MachineState *machine)
                   ELFDATA2MSB, PPC_ELF_MACHINE, 0, 0);
     if (sz <= 0) {
         sz = load_image_targphys(filename, pm->vof ? 0 : PROM_ADDR, PROM_SIZE,
-                                 NULL);
+                                 &error_fatal);
     }
     if (sz <= 0 || sz > PROM_SIZE) {
         error_report("Could not load firmware '%s'", filename);
@@ -260,12 +260,7 @@ static void pegasos2_init(MachineState *machine)
         pm->initrd_addr = ROUND_UP(pm->initrd_addr, 4);
         pm->initrd_addr = MAX(pm->initrd_addr, INITRD_MIN_ADDR);
         sz = load_image_targphys(machine->initrd_filename, pm->initrd_addr,
-                                 machine->ram_size - pm->initrd_addr, NULL);
-        if (sz <= 0) {
-            error_report("Could not load initrd '%s'",
-                         machine->initrd_filename);
-            exit(1);
-        }
+                            machine->ram_size - pm->initrd_addr, &error_fatal);
         pm->initrd_size = sz;
     }
 
