4fd34f0e1d14c7dec6284f70317b96af0ae976fd
Philippe Mathieu-Daudé <philmd@linaro.org>
2025-10-27 12:23:06 +0100

    hw/pci-host: Set DEVICE_CATEGORY_BRIDGE once in parent class_init()
    No need to set DEVICE_CATEGORY_BRIDGE for each device
    implementing TYPE_PCI_HOST_BRIDGE: set it once in the
    parent.
    
    Signed-off-by: Philippe Mathieu-Daudé <philmd@linaro.org>
    Reviewed-by: Marc-André Lureau <marcandre.lureau@redhat.com>
    Message-Id: <20251027112306.57634-1-philmd@linaro.org>

--- hw/pci-host/raven.c
+++ hw/pci-host/raven.c
@@ -365,7 +365,6 @@ static void raven_pcihost_class_init(ObjectClass *klass, const void *data)
 {
     DeviceClass *dc = DEVICE_CLASS(klass);
 
-    set_bit(DEVICE_CATEGORY_BRIDGE, dc->categories);
     dc->realize = raven_pcihost_realizefn;
     dc->fw_name = "pci";
 }
