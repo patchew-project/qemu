75d1e1502e123c816385ae921417f5b8748ed419
Philippe Mathieu-Daudé <philmd@linaro.org>
2025-10-28 06:42:28 +0100

    accel/hvf: Introduce hvf_arch_cpu_synchronize_[pre/post]exec() hooks
    Signed-off-by: Philippe Mathieu-Daudé <philmd@linaro.org>
    Reviewed-by: Richard Henderson <richard.henderson@linaro.org>
    Message-Id: <20251028054238.14949-53-philmd@linaro.org>

--- accel/hvf/hvf-accel-ops.c
+++ accel/hvf/hvf-accel-ops.c
@@ -181,10 +181,13 @@ static void *hvf_cpu_thread_fn(void *arg)
     do {
         qemu_process_cpu_events(cpu);
         if (cpu_can_run(cpu)) {
+
+            hvf_arch_cpu_synchronize_pre_exec(cpu);
             r = hvf_arch_vcpu_exec(cpu);
             if (r == EXCP_DEBUG) {
                 cpu_handle_guest_debug(cpu);
             }
+            hvf_arch_cpu_synchronize_post_exec(cpu);
         }
     } while (!cpu->unplug || cpu_can_run(cpu));
 
