6fa4fe4b5b8e8d04d6c6a51e51bf770ca9b09363
RemZapCypher <soumyajyotisarkar23@gmail.com>
2025-11-01 13:52:31 +0530

    hw/scsi/ncr53c710: Fixing defects reported by Coverity Scan for QEMU
    Fixing Null pointer dereference & Async/Sync IDENTICAL_BRANCHES
    
    Reported-by: Stefan Hajnoczi <stefanha@gmail.com>
    Message-Id: <20251101082231.8681-1-soumyajyotisarkar23@gmail.com>

--- hw/scsi/ncr53c710.c
+++ hw/scsi/ncr53c710.c
@@ -831,13 +831,11 @@ void ncr710_transfer_data(SCSIRequest *req, uint32_t len)
         }
     }
 
-    /* Host adapter (re)connected */
-    s->current->dma_len = len;
     s->command_complete = NCR710_CMD_DATA_READY;
-
     if (!s->current) {
-        return;
+        s->current = (NCR710Request *)req->hba_private;
     }
+    s->current->dma_len = len;
 
     if (s->waiting) {
         s->scntl1 |= NCR710_SCNTL1_CON;
@@ -1362,11 +1360,6 @@ again:
         case PHASE_DI:
             s->waiting = NCR710_WAIT_DMA;
             ncr710_do_dma(s, 0);
-            if (s->waiting != NCR710_WAIT_NONE) {
-                /* Async - stop and wait */
-                break;
-            }
-            /* Sync - continue execution */
             break;
         case PHASE_CO:
             ncr710_do_command(s);
