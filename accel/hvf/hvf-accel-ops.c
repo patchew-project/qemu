3fc75f3f9dcc81d1919fe5e05dbf52ae79492a9b
Philippe Mathieu-Daudé <philmd@linaro.org>
2025-11-03 11:10:22 +0100

    accel/hvf: Implement WFI without using pselect()
    Return to the main loop where we'll be waken again.
    This avoid a tricky race with signals introduced in
    commit 219c101fa7f ("Add HVF WFI handler").
    
    Signed-off-by: Philippe Mathieu-Daudé <philmd@linaro.org>
    Reviewed-by: Akihiko Odaki <odaki@rsg.ci.i.u-tokyo.ac.jp>
    Message-Id: <20251103101034.59039-14-philmd@linaro.org>

--- accel/hvf/hvf-accel-ops.c
+++ accel/hvf/hvf-accel-ops.c
@@ -134,9 +134,6 @@ static int hvf_init_vcpu(CPUState *cpu)
     sigact.sa_handler = dummy_signal;
     sigaction(SIG_IPI, &sigact, NULL);
 
-    pthread_sigmask(SIG_BLOCK, NULL, &cpu->accel->unblock_ipi_mask);
-    sigdelset(&cpu->accel->unblock_ipi_mask, SIG_IPI);
-
 #ifdef __aarch64__
     r = hv_vcpu_create(&cpu->accel->fd,
                        (hv_vcpu_exit_t **)&cpu->accel->exit, NULL);
