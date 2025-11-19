3e9202500110fb56bb024e523183b39acb4e67e3
Stefan Hajnoczi <stefanha@redhat.com>
2025-11-19 15:52:00 -0500

    tracetool: add Rust DTrace/SystemTap SDT support
    Implement DTrace/SystemTap SDT by emitting the following:
    - The probe crate's probe!() macro is used to emit a DTrace/SystemTap
      SDT probe.
    - Every trace event gets a corresponding trace_<name>_enabled() -> bool
      generated function that Rust code can use to avoid expensive
      computation when a trace event is disabled. This API works for other
      trace backends too.
    
    `#[allow(dead_code)]` additions are necessary for QEMU's dstate in
    generated trace-<dir>.rs files since they are unused by the dtrace
    backend. `./configure --enable-trace-backends=` can enable multiple
    backends, so keep it simple and just silence the warning instead of
    trying to detect the condition when generating the dstate code can be
    skipped.
    
    The tracetool tests are updated. Take a look at
    tests/tracetool/dtrace.rs to see what the new generated code looks like.
    
    Signed-off-by: Stefan Hajnoczi <stefanha@redhat.com>
    Message-Id: <20251119205200.173170-5-stefanha@redhat.com>

--- rust/trace/src/lib.rs
+++ rust/trace/src/lib.rs
@@ -7,6 +7,10 @@
 /// Re-exported item to avoid adding libc as a dependency everywhere.
 pub use libc::{syslog, LOG_INFO};
 
+#[doc(hidden)]
+/// Re-exported item to avoid adding probe as a dependency everywhere.
+pub use probe::probe;
+
 #[macro_export]
 /// Define the trace-points from the named directory (which should have slashes
 /// replaced by underscore characters) as functions in a module called `trace`.
