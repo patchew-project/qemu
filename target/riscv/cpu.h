226082e1d6879e5672f764ff0e9819cbd9b1078b
Frank Chang <frank.chang@sifive.com>
2025-11-21 13:04:09 +0800

    target/riscv: Add a helper to return the current effective priv mode
    This helper returns the current effective privilege mode.
    
    Signed-off-by: Frank Chang <frank.chang@sifive.com>
    Reviewed-by: Radim Krčmář <rkrcmar@ventanamicro.com>
    Message-Id: <20251121050413.3718427-3-frank.chang@sifive.com>

--- target/riscv/cpu.h
+++ target/riscv/cpu.h
@@ -613,6 +613,7 @@ target_ulong riscv_cpu_get_geilen(CPURISCVState *env);
 void riscv_cpu_set_geilen(CPURISCVState *env, target_ulong geilen);
 bool riscv_cpu_vector_enabled(CPURISCVState *env);
 void riscv_cpu_set_virt_enabled(CPURISCVState *env, bool enable);
+bool riscv_cpu_eff_priv(CPURISCVState *env, int *priv, bool *virt);
 int riscv_env_mmu_index(CPURISCVState *env, bool ifetch);
 bool cpu_get_fcfien(CPURISCVState *env);
 bool cpu_get_bcfien(CPURISCVState *env);
