9cf06debbf5377c965b36bd3992612db9ffcb3b0
Gaurav Sharma <gaurav.sharma_7@nxp.com>
2025-12-05 11:08:07 +0530

    hw/arm/fsl-imx8mm: Add Analog device IP to iMX8MM SOC
    Add the Analog IP to i.MX8MM SoC. iMX8MM and i.MX8MP uses
    the same Analog IP so the analog ip source will be shared.
    
    The ARM PLL divider control register (arm-pll-fdiv-ctl0) has
    a different reset value on i.MX8MM (0x000fa030) compared to
    i.MX8MP (0x000fa031). So iMX8MM will be overriding this property
    with its own reset-value.
    
    Signed-off-by: Gaurav Sharma <gaurav.sharma_7@nxp.com>
    Reviewed-by: Bernhard Beschow <shentey@gmail.com>
    Message-Id: <20251205053819.2021772-4-gaurav.sharma_7@nxp.com>

--- include/hw/arm/fsl-imx8mm.h
+++ include/hw/arm/fsl-imx8mm.h
@@ -13,6 +13,7 @@
 #include "cpu.h"
 #include "hw/char/imx_serial.h"
 #include "hw/intc/arm_gicv3_common.h"
+#include "hw/misc/imx8mp_analog.h"
 #include "qom/object.h"
 #include "qemu/units.h"
 
@@ -33,6 +34,7 @@ struct FslImx8mmState {
 
     ARMCPU             cpu[FSL_IMX8MM_NUM_CPUS];
     GICv3State         gic;
+    IMX8MPAnalogState  analog;
     IMXSerialState     uart[FSL_IMX8MM_NUM_UARTS];
 };
 
