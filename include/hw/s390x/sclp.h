5352467931c7472bed9218bab35a60fd9e269d54
Zhuoying Cai <zycai@linux.ibm.com>
2025-12-08 16:32:33 -0500

    s390x: Guest support for Secure-IPL Facility
    Introduce Secure-IPL (SIPL) facility.
    
    Use fac_ipl to represent bytes 136 and 137 for IPL device facilities
    of the SCLP Read Info block.
    
    Availability of SIPL facility is determined by byte 136 bit 1 of the
    SCLP Read Info block. Byte 136's facilities cannot be represented
    without the availability of the extended-length-SCCB, so add it as a
    check for consistency.
    
    Secure IPL is not available for guests under protected virtualization.
    
    This feature is available starting with the gen16 CPU model.
    
    Signed-off-by: Zhuoying Cai <zycai@linux.ibm.com>
    Reviewed-by: Collin Walling <walling@linux.ibm.com>
    Message-Id: <20251208213247.702569-17-zycai@linux.ibm.com>

--- include/hw/s390x/sclp.h
+++ include/hw/s390x/sclp.h
@@ -136,7 +136,9 @@ typedef struct ReadInfo {
     uint32_t hmfai;
     uint8_t  _reserved7[134 - 128];     /* 128-133 */
     uint8_t  fac134;
-    uint8_t  _reserved8[144 - 135];     /* 135-143 */
+    uint8_t  _reserved8;
+    uint8_t  fac_ipl[2];                /* 136-137 */
+    uint8_t  _reserved9[144 - 137];     /* 138-143 */
     struct CPUEntry entries[];
     /*
      * When the Extended-Length SCCB (ELS) feature is enabled the
