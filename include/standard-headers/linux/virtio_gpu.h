5cab40d43897a3c1c76f4e0fdde288959440b64a
Honglei Huang <honghuan@amd.com>
2026-01-07 15:18:56 +0800

    virtio-gpu: Add support for VIRTIO_GPU_BLOB_FLAG_USE_USERPTR flag
    Add support for the USE_USERPTR blob flag in virtio-gpu to enable
    user pointer mapping for blob resources. This allows guest applications
    to use user-allocated memory for GPU resources more efficiently.
    
    Changes include:
    - Add VIRTIO_GPU_BLOB_FLAG_USE_USERPTR flag definition
    - Enhance blob resource creation to handle userptr flag properly
    - Remove arbitrary nr_entries limit (16384) in mapping creation
    - Add conditional handling for userptr vs regular blob mapping
    - Support guest_blob_mapped parameter for virgl renderer
    - Fix value check issue in virtio-gpu
    
    This enables more flexible memory management between guest and host
    for GPU virtualization scenarios.
    
    Signed-off-by: Honglei Huang <honghuan@amd.com>
    Message-Id: <20260107071858.3452614-2-honghuan@amd.com>

--- include/standard-headers/linux/virtio_gpu.h
+++ include/standard-headers/linux/virtio_gpu.h
@@ -405,6 +405,7 @@ struct virtio_gpu_resource_create_blob {
 #define VIRTIO_GPU_BLOB_FLAG_USE_MAPPABLE     0x0001
 #define VIRTIO_GPU_BLOB_FLAG_USE_SHAREABLE    0x0002
 #define VIRTIO_GPU_BLOB_FLAG_USE_CROSS_DEVICE 0x0004
+#define VIRTIO_GPU_BLOB_FLAG_USE_USERPTR      0x0008
 	/* zero is invalid blob mem */
 	uint32_t blob_mem;
 	uint32_t blob_flags;
