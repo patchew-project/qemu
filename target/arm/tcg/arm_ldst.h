62033be0c5f40e5debfdfae24390cf9cb23ce808
Pierrick Bouvier <pierrick.bouvier@linaro.org>
2026-01-08 21:31:54 -0800

    target/arm/tcg/arm_ldst.h: use translator_ld*_end variants
    This is needed to call this header from common code.
    
    Signed-off-by: Pierrick Bouvier <pierrick.bouvier@linaro.org>
    Message-Id: <20260109053158.2800705-26-pierrick.bouvier@linaro.org>

--- target/arm/tcg/arm_ldst.h
+++ target/arm/tcg/arm_ldst.h
@@ -21,13 +21,21 @@
 #define ARM_LDST_H
 
 #include "exec/translator.h"
+#include "target/arm/internals.h"
 #include "qemu/bswap.h"
+
+static inline MemOp arm_memop_endian_swap(bool sctlr_b)
+{
+    MemOp t = target_big_endian() ? MO_BE : MO_LE;
+    bool do_swap = bswap_code(sctlr_b);
+    return t ^ (do_swap * MO_BSWAP);
+}
 
 /* Load an instruction and return it in the standard little-endian order */
 static inline uint32_t arm_ldl_code(CPUARMState *env, DisasContextBase *s,
                                     uint64_t addr, bool sctlr_b)
 {
-    return translator_ldl_swap(env, s, addr, bswap_code(sctlr_b));
+    return translator_ldl_end(env, s, addr, arm_memop_endian_swap(sctlr_b));
 }
 
 /* Ditto, for a halfword (Thumb) instruction */
@@ -41,7 +49,7 @@ static inline uint16_t arm_lduw_code(CPUARMState *env, DisasContextBase* s,
         addr ^= 2;
     }
 #endif
-    return translator_lduw_swap(env, s, addr, bswap_code(sctlr_b));
+    return translator_lduw_end(env, s, addr, arm_memop_endian_swap(sctlr_b));
 }
 
 #endif
