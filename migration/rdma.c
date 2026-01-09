dd48e8c666cd5142343bfd8f47eefde5aa1871ea
Fabiano Rosas <farosas@suse.de>
2026-01-09 09:40:41 -0300

    migration/channel: Centralize calling migration_channel_connect_outgoing
    Make the synchronous calls evident by not hiding the call to
    migration_channel_connect_outgoing() in the transport code. Have those
    functions return and call the function at the upper level.
    
    This helps with navigation: the transport code returns the ioc,
    there's no need to look into them when browsing the code.
    
    It also allows RDMA in the source side to use the same path as the
    rest of the transports.
    
    While here, document the async calls which are the exception.
    
    Reviewed-by: Peter Xu <peterx@redhat.com>
    Signed-off-by: Fabiano Rosas <farosas@suse.de>
    Message-Id: <20260109124043.25019-26-farosas@suse.de>

--- migration/rdma.c
+++ migration/rdma.c
@@ -3934,8 +3934,8 @@ err:
     g_free(rdma);
 }
 
-void rdma_connect_outgoing(void *opaque,
-                           InetSocketAddress *host_port, Error **errp)
+QIOChannel *rdma_connect_outgoing(void *opaque,
+                                  InetSocketAddress *host_port, Error **errp)
 {
     MigrationState *s = opaque;
     RDMAContext *rdma_return_path = NULL;
@@ -3945,7 +3945,7 @@ void rdma_connect_outgoing(void *opaque,
     /* Avoid ram_block_discard_disable(), cannot change during migration. */
     if (ram_block_discard_is_required()) {
         error_setg(errp, "RDMA: cannot disable RAM discard");
-        return;
+        return NULL;
     }
 
     rdma = qemu_rdma_data_init(host_port, errp);
@@ -3995,12 +3995,11 @@ void rdma_connect_outgoing(void *opaque,
     trace_rdma_connect_outgoing_after_rdma_connect();
 
     s->rdma_migration = true;
-    migration_outgoing_setup(rdma_new_output(rdma));
-    migration_start_outgoing(s);
-    return;
+    return rdma_new_output(rdma);
 return_path_err:
     qemu_rdma_cleanup(rdma);
 err:
     g_free(rdma);
     g_free(rdma_return_path);
+    return NULL;
 }
