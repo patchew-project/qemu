c4c0efcd2c02a7e2bbf96c44061b81766dcb1e43
Manos Pitsidianakis <manos.pitsidianakis@linaro.org>
2026-01-14 10:35:03 +0200

    hvf/arm: handle FEAT_SME2 migration
    SME2 support adds the following state for HVF guests:
    
    - Vector registers Z0, ... , Z31 (introduced by FEAT_SVE but HVF does
      not support it)
    - Predicate registers P0, .., P15 (also FEAT_SVE)
    - ZA register
    - ZT0 register
    - PSTATE.{SM,ZA} bits (SVCR pseudo-register)
    - SMPRI_EL1 which handles the PE's priority in the SMCU
    - TPIDR2_EL0 the thread local ID register for SME
    
    Signed-off-by: Manos Pitsidianakis <manos.pitsidianakis@linaro.org>
    Reviewed-by: Mohamed Mediouni <mohamed@unpredictable.fr>
    Message-Id: <20260114-sme2-hvf-v1-1-ce9599596736@linaro.org>

--- target/arm/hvf_arm.h
+++ target/arm/hvf_arm.h
@@ -13,6 +13,14 @@
 
 #include "target/arm/cpu-qom.h"
 
+#ifdef __MAC_OS_X_VERSION_MAX_ALLOWED
+  #define HVF_SME2_AVAILABLE (__MAC_OS_X_VERSION_MAX_ALLOWED >= 150200)
+  #include "system/hvf_int.h"
+#else
+  #define HVF_SME2_AVAILABLE 0
+#endif
+
+
 /**
  * hvf_arm_init_debug() - initialize guest debug capabilities
  *
@@ -24,5 +32,43 @@ void hvf_arm_set_cpu_features_from_host(ARMCPU *cpu);
 
 uint32_t hvf_arm_get_default_ipa_bit_size(void);
 uint32_t hvf_arm_get_max_ipa_bit_size(void);
+
+#if HVF_SME2_AVAILABLE
+static inline bool hvf_arm_sme2_supported(void)
+{
+    if (__builtin_available(macOS 15.2, *)) {
+        size_t svl_bytes;
+        hv_return_t result = hv_sme_config_get_max_svl_bytes(&svl_bytes);
+        if (result == HV_UNSUPPORTED) {
+            return false;
+        }
+        assert_hvf_ok(result);
+        return svl_bytes > 0;
+    } else {
+        return false;
+    }
+}
+
+static inline uint32_t hvf_arm_sme2_get_svl(void)
+{
+    if (__builtin_available(macOS 15.2, *)) {
+        size_t svl_bytes;
+        hv_return_t result = hv_sme_config_get_max_svl_bytes(&svl_bytes);
+        assert_hvf_ok(result);
+        return svl_bytes;
+    } else {
+        abort();
+    }
+}
+#else /* HVF_SME2_AVAILABLE */
+static inline bool hvf_arm_sme2_supported(void)
+{
+    return false;
+}
+static inline uint32_t hvf_arm_sme2_get_svl(void)
+{
+    abort();
+}
+#endif /* HVF_SME2_AVAILABLE */
 
 #endif
