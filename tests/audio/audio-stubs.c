0846dd2d2a31596c498d63b60efdcbf2ef656f99
Marc-André Lureau <marcandre.lureau@redhat.com>
2026-01-23 11:49:32 +0400

    replay: remove dependency on audio/
    The replay infrastructure shouldn't depend on internals of audio/.
    (st_sample is an internal implementation detail and could be different)
    
    Let audio drive the audio samples recording/replaying.
    
    Notice also we don't need to save & restore the internal ring "wpos",
    all replay should care about is the number of samples and the samples.
    
    Bump the replay version.
    
    Signed-off-by: Marc-André Lureau <marcandre.lureau@redhat.com>
    Message-Id: <20260123074945.2563196-30-marcandre.lureau@redhat.com>

--- tests/audio/audio-stubs.c
+++ tests/audio/audio-stubs.c
@@ -19,12 +19,19 @@ GQuark dbus_display_error_quark(void)
     return g_quark_from_static_string("dbus-display-error-quark");
 }
 
-void replay_audio_in(size_t *recorded, st_sample *samples,
-                     size_t *wpos, size_t size)
+void replay_audio_out(size_t *played)
 {
 }
 
-void replay_audio_out(size_t *played)
+void replay_audio_in_start(size_t *nsamples)
+{
+}
+
+void replay_audio_in_sample_lr(uint64_t *left, uint64_t *right)
+{
+}
+
+void replay_audio_in_finish(void)
 {
 }
 
