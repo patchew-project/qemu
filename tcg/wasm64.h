f10d903b715d09cd641fb44c756c416965c5dd2b
Kohei Tokunaga <ktokunaga.mail@gmail.com>
2026-01-26 07:03:31 +0000

    tcg/wasm64: Add call instruction
    To call QEMU functions from a TB's Wasm module, the functions must be
    imported into the module.
    
    Wasm's call instruction can invoke an imported function using a locally
    assigned function index. When a call TCG operation is generated, the Wasm
    backend assigns the ID (starting from 0) to the target function. The mapping
    between the function pointer and its assigned ID is recorded in a list of
    HelperInfo.
    
    Since Wasm's call instruction requires arguments to be pushed onto the Wasm
    stack, the backend retrieves the function arguments from TCG's stack array
    and pushes them to the Wasm stack before the call. After the function
    returns, the result is retrieved from the Wasm stack and set in the
    corresponding TCG variable.
    
    In the Emscripten build configured with !has_int128_type, a 128bit value is
    represented by the Int128 struct. Such values are passed to the function via
    pointer parameters and returned via a prepended pointer argument, as
    described in [1]. For this prepended buffer area, the module expects a
    pre-allocated Int128 buffer from the caller via ctx.buf128.
    
    Helper functions expect the target of the return instruction via the GETPC
    macro (the tci_tb_ptr variable in TCI). However, unlike other architectures,
    Wasm doesn't have a register pointing to the return target. To emulate this
    behaviour, the Wasm module sets the instruction pointer to the corresponding
    TCI instruction (s->code_ptr) in tci_tb_ptr passed via the WasmContext.
    
    TCI instructions are also generated in the same way as the original TCI
    backend.
    
    [1] https://github.com/WebAssembly/tool-conventions/blob/060cf4073e46931160c2e9ecd43177ee1fe93866/BasicCABI.md#function-arguments-and-return-values
    
    Signed-off-by: Kohei Tokunaga <ktokunaga.mail@gmail.com>
    Message-Id: <8e288d8a54cecea24c88def7aab9a221806abbc3.1769407033.git.ktokunaga.mail@gmail.com>

--- tcg/wasm64.h
+++ tcg/wasm64.h
@@ -10,6 +10,16 @@ struct WasmContext {
      * Pointer to the TB to be executed.
      */
     void *tb_ptr;
+
+    /*
+     * Pointer to the tci_tb_ptr variable.
+     */
+    void *tci_tb_ptr;
+
+    /*
+     * Buffer to store 128bit return value on call.
+     */
+    void *buf128;
 };
 
 #endif
