55acd96ccba053c1975eb338b87f3b771ddae127
Kohei Tokunaga <ktokunaga.mail@gmail.com>
2026-01-26 07:03:18 +0000

    tcg/wasm64: Add and/or/xor instructions
    This commit adds support for generateing the and, or and xor operations. The
    generated Wasm codes will be instantiated and executed in the browser.
    
    Browsers tipycally limit the number of active Wasm instances and the
    instantiating Wasm modules introduces overhead. As a result, instantiating
    TBs that are rarely called is undesirable. To address this, the Wasm backend
    relies on the a forked subset of the TCI interpreter (tcg_qemu_tb_exec_tci
    function in tcg/wasm64.c) for executing such TBs.
    
    The Wasm backend emits both Wasm and TCI instructions. TCI instructions are
    emitted to s->code_ptr, while the corresponding Wasm instructions are
    generated into a separate buffer allocated via tcg_malloc(). This buffer
    intends to be merged into the TB before tcg_gen_code returns.
    
    In the Wasm code, each TCG variable is mapped to a 64bit Wasm
    variable. Execution works by first pushing the operands into the Wasm's
    stack using get instructions. The result is left on the stack and this can
    be assigned to a variable by popping it using a set instruction. The Wasm
    binary format is documented at [1].
    
    Additionally, since the Wasm instuction's index operand must be
    LEB128-encoded, this commit introduces an encoder function implemented
    following [2].
    
    [1] https://webassembly.github.io/spec/core/binary/index.html
    [2] https://en.wikipedia.org/wiki/LEB128
    
    Signed-off-by: Kohei Tokunaga <ktokunaga.mail@gmail.com>
    Message-Id: <98489736a6ad338cda86ed9fe31af5cba239fa0c.1769407033.git.ktokunaga.mail@gmail.com>

---
+++ tcg/wasm64.c
@@ -0,0 +1,66 @@
+/* SPDX-License-Identifier: GPL-2.0-or-later */
+/*
+ * WebAssembly backend with forked TCI, based on tci.c
+ *
+ * Copyright (c) 2009, 2011, 2016 Stefan Weil
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#include "qemu/osdep.h"
+#include "tcg/tcg.h"
+
+static void tci_args_rrr(uint32_t insn, TCGReg *r0, TCGReg *r1, TCGReg *r2)
+{
+    *r0 = extract32(insn, 8, 4);
+    *r1 = extract32(insn, 12, 4);
+    *r2 = extract32(insn, 16, 4);
+}
+
+static uintptr_t tcg_qemu_tb_exec_tci(CPUArchState *env, const void *v_tb_ptr)
+{
+    const uint32_t *tb_ptr = v_tb_ptr;
+    tcg_target_ulong regs[TCG_TARGET_NB_REGS];
+    uint64_t stack[(TCG_STATIC_CALL_ARGS_SIZE + TCG_STATIC_FRAME_SIZE)
+                   / sizeof(uint64_t)];
+
+    regs[TCG_AREG0] = (tcg_target_ulong)env;
+    regs[TCG_REG_CALL_STACK] = (uintptr_t)stack;
+
+    for (;;) {
+        uint32_t insn;
+        TCGOpcode opc;
+        TCGReg r0, r1, r2;
+
+        insn = *tb_ptr++;
+        opc = extract32(insn, 0, 8);
+
+        switch (opc) {
+        case INDEX_op_and:
+            tci_args_rrr(insn, &r0, &r1, &r2);
+            regs[r0] = regs[r1] & regs[r2];
+            break;
+        case INDEX_op_or:
+            tci_args_rrr(insn, &r0, &r1, &r2);
+            regs[r0] = regs[r1] | regs[r2];
+            break;
+        case INDEX_op_xor:
+            tci_args_rrr(insn, &r0, &r1, &r2);
+            regs[r0] = regs[r1] ^ regs[r2];
+            break;
+        default:
+            g_assert_not_reached();
+        }
+    }
+}
