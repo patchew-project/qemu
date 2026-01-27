eb9a6bea15aa2508e2c82b84638e88abe66fa017
Ani Sinha <anisinha@redhat.com>
2026-01-27 10:45:31 +0530

    hw/accel: add a per-accelerator callback to change VM accelerator handle
    When a confidential virtual machine is reset, a new guest context in the
    accelerator must be generated post reset. Therefore, the old accelerator guest
    file handle must closed and a new one created. To this end, a per-accelerator
    callback, "reset_vmfd" is introduced that would get called when a confidential
    guest is reset. Subsequent patches will introduce specific implementation of
    this callback for KVM accelerator.
    
    Signed-off-by: Ani Sinha <anisinha@redhat.com>
    Message-Id: <20260127051612.219475-4-anisinha@redhat.com>

--- system/runstate.c
+++ system/runstate.c
@@ -42,6 +42,7 @@
 #include "qapi/qapi-commands-run-state.h"
 #include "qapi/qapi-events-run-state.h"
 #include "qemu/accel.h"
+#include "accel/accel-ops.h"
 #include "qemu/error-report.h"
 #include "qemu/job.h"
 #include "qemu/log.h"
@@ -509,6 +510,8 @@ void qemu_system_reset(ShutdownCause reason)
 {
     MachineClass *mc;
     ResetType type;
+    AccelClass *ac = ACCEL_GET_CLASS(current_accel());
+    int ret;
 
     mc = current_machine ? MACHINE_GET_CLASS(current_machine) : NULL;
 
@@ -521,6 +524,26 @@ void qemu_system_reset(ShutdownCause reason)
     default:
         type = RESET_TYPE_COLD;
     }
+
+    /*
+     * different accelerators implement how to close the old file handle of
+     * the accelerator descriptor and create a new one here. Resetting
+     * file handle is necessary to create a new confidential VM context post
+     * VM reset.
+     */
+    if (!cpus_are_resettable() && reason == SHUTDOWN_CAUSE_GUEST_RESET) {
+        if (ac->reset_vmfd) {
+            ret = ac->reset_vmfd(current_machine);
+            if (ret < 0) {
+                error_report("unable to reset vmfd: %s(%d)",
+                             strerror(-ret), ret);
+                vm_stop(RUN_STATE_INTERNAL_ERROR);
+            }
+        } else {
+            error_report("accelerator does not support reset");
+        }
+    }
+
     if (mc && mc->reset) {
         mc->reset(current_machine, type);
     } else {
