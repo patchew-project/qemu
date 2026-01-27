7e484bec28212bc13dd878786364ed02f6a76489
Ani Sinha <anisinha@redhat.com>
2026-01-27 10:45:31 +0530

    hw/accel: add a per-accelerator callback to change VM accelerator handle
    When a confidential virtual machine is reset, a new guest context in the
    accelerator must be generated post reset. Therefore, the old accelerator guest
    file handle must closed and a new one created. To this end, a per-accelerator
    callback, "reset_vmfd" is introduced that would get called when a confidential
    guest is reset. Subsequent patches will introduce specific implementation of
    this callback for KVM accelerator.
    
    Signed-off-by: Ani Sinha <anisinha@redhat.com>
    Message-Id: <20260127051612.219475-4-anisinha@redhat.com>

--- include/accel/accel-ops.h
+++ include/accel/accel-ops.h
@@ -23,6 +23,7 @@ struct AccelClass {
     AccelOpsClass *ops;
 
     int (*init_machine)(AccelState *as, MachineState *ms);
+    int (*reset_vmfd)(MachineState *ms);
     bool (*cpu_common_realize)(CPUState *cpu, Error **errp);
     void (*cpu_common_unrealize)(CPUState *cpu);
     /* get_stats: Append statistics to @buf */
