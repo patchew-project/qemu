066553183dfa003e41d5601083727941dfb7da17
BALATON Zoltan <balaton@eik.bme.hu>
2026-02-03 17:22:11 +0100

    hw/display/{cg3,tcx}: Do not use memory_region_init_rom_nomigrate()
    We could simply convert these to use memory_region_init_rom but that
    would be a migration compatibility break so preserve current behaviour
    for now.
    
    Signed-off-by: BALATON Zoltan <balaton@eik.bme.hu>
    Reviewed-by: Mark Cave-Ayland <mark.cave-ayland@ilande.co.uk>
    Message-Id: <0cf39eb5f0feff66c9a775b3502a4e0ba4cf63cc.1770131168.git.balaton@eik.bme.hu>

--- hw/display/tcx.c
+++ hw/display/tcx.c
@@ -756,8 +756,10 @@ static void tcx_initfn(Object *obj)
     SysBusDevice *sbd = SYS_BUS_DEVICE(obj);
     TCXState *s = TCX(obj);
 
-    memory_region_init_rom_nomigrate(&s->rom, obj, "tcx.prom",
-                                     FCODE_MAX_ROM_SIZE, &error_fatal);
+    memory_region_init_ram_flags_nomigrate(&s->rom, obj, "tcx.prom",
+                                           FCODE_MAX_ROM_SIZE, 0,
+                                           &error_fatal);
+    memory_region_set_readonly(&s->rom, true);
     sysbus_init_mmio(sbd, &s->rom);
 
     /* 2/STIP : Stippler */
