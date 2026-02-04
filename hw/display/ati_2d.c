1cd6e6d5daff94c275caa5a8608da7f3ce53a859
Chad Jablonski <chad@jablonski.xyz>
2026-02-04 11:46:01 -0500

    ati-vga: Split ati_2d_do_blt from ati_2d_blt
    ati_2d_blt remains the public interface to the blitter but the bulk of
    the implementation is moved down into ati_2d_do_blt which is passed an
    ATI2DCtx.
    
    Signed-off-by: Chad Jablonski <chad@jablonski.xyz>
    Reviewed-by: BALATON Zoltan <balaton@eik.bme.hu>
    Message-Id: <20260204164606.3425246-14-chad@jablonski.xyz>

--- hw/display/ati_2d.c
+++ hw/display/ati_2d.c
@@ -123,15 +123,12 @@ static void setup_2d_blt_ctx(const ATIVGAState *s, ATI2DCtx *ctx)
             (ctx->top_to_bottom ? 'v' : '^'));
 }
 
-void ati_2d_blt(ATIVGAState *s)
+static void ati_2d_do_blt(ATIVGAState *s, ATI2DCtx *ctx)
 {
     /* FIXME it is probably more complex than this and may need to be */
     /* rewritten but for now as a start just to get some output: */
     bool use_pixman_fill = s->use_pixman & BIT(0);
     bool use_pixman_blt = s->use_pixman & BIT(1);
-    ATI2DCtx ctx_;
-    ATI2DCtx *ctx = &ctx_;
-    setup_2d_blt_ctx(s, ctx);
     if (!ctx->bpp) {
         qemu_log_mask(LOG_GUEST_ERROR, "Invalid bpp\n");
         return;
@@ -263,8 +260,13 @@ void ati_2d_blt(ATIVGAState *s)
     default:
         qemu_log_mask(LOG_UNIMP, "Unimplemented ati_2d blt op %x\n",
                       ctx->rop3 >> 16);
-        return;
     }
+}
 
-    ati_set_dirty(&s->vga, ctx);
+void ati_2d_blt(ATIVGAState *s)
+{
+    ATI2DCtx ctx;
+    setup_2d_blt_ctx(s, &ctx);
+    ati_2d_do_blt(s, &ctx);
+    ati_set_dirty(&s->vga, &ctx);
 }
