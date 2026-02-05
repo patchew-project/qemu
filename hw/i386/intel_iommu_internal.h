ce9a5bef5e84816d8f0a04b398a5781adf83343c
Zhenzhong Duan <zhenzhong.duan@intel.com>
2026-02-04 22:11:30 -0500

    intel_iommu_accel: Add pasid bits size check
    If pasid bits size is bigger than host side, host could fail to emulate
    all bindings in guest. Add a check to fail device plug early.
    
    Signed-off-by: Zhenzhong Duan <zhenzhong.duan@intel.com>
    Message-Id: <20260205031133.74357-13-zhenzhong.duan@intel.com>

--- hw/i386/intel_iommu_internal.h
+++ hw/i386/intel_iommu_internal.h
@@ -194,6 +194,7 @@
 #define VTD_ECAP_PRS                (1ULL << 29)
 #define VTD_ECAP_MHMV               (15ULL << 20)
 #define VTD_ECAP_SRS                (1ULL << 31)
+#define VTD_ECAP_PSS(x)             extract64(x, 35, 5)
 #define VTD_ECAP_PASID              (1ULL << 40)
 #define VTD_ECAP_SMTS               (1ULL << 43)
 #define VTD_ECAP_SSTS               (1ULL << 46)
