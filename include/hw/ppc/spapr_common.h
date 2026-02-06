bdece41e953b46cdbe0c459d2443bbefd5cdd512
Pierrick Bouvier <pierrick.bouvier@linaro.org>
2026-02-06 14:19:05 -0800

    include/hw/ppc/spapr: extract SPAPR_MAX_RAM_SLOTS in a new header
    Allow to include it from common code (vhost-user, in next commit), else
    it pulls ppc/cpu.h which has target specifics.
    
    Signed-off-by: Pierrick Bouvier <pierrick.bouvier@linaro.org>
    Message-Id: <20260206221908.1451528-7-pierrick.bouvier@linaro.org>

---
+++ include/hw/ppc/spapr_common.h
@@ -0,0 +1,16 @@
+/*
+ * Common definitions for PPC SPAPR
+ * SPDX-License-Identifier: GPL-2.0-or-later
+ */
+
+#ifndef HW_SPAPR_COMMON_H
+#define HW_SPAPR_COMMON_H
+
+/*
+ * This defines the maximum number of DIMM slots we can have for sPAPR
+ * guest. This is not defined by sPAPR but we are defining it to 32 slots
+ * based on default number of slots provided by PowerPC kernel.
+ */
+#define SPAPR_MAX_RAM_SLOTS     32
+
+#endif /* HW_SPAPR_COMMON_H */
