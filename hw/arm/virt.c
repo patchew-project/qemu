d121a049d5840b46da335e6c9eaec2914205aab5
Mohammadfaiz Bawa <mbawa@redhat.com>
2026-02-11 17:57:59 +0530

    hw/arm/virt: Add virtio-transports property
    Windows ARM64 guests detect virtio-mmio devices declared in ACPI
    tables even when no backend is attached. This causes "Unknown
    devices" (ACPI\LNRO0005) to appear in Device Manager.
    
    Until Windows fixes that by supporting, adding a new machine
    property 'virtio-transports' to control the number of
    virtio-mmio transports instantiated. The default remains
    NUM_VIRTIO_TRANSPORTS (32) for backward compatibility.
    Setting it to 0 allows users to disable virtio-mmio entirely.
    
    Usage: -machine virt,virtio-transports=0
    
    Signed-off-by: Mohammadfaiz Bawa <mbawa@redhat.com>
    Reviewed-by: Mohamed Mediouni <mohamed@unpredictable.fr>
    Message-Id: <20260211122759.125094-1-mbawa@redhat.com>

--- hw/arm/virt.c
+++ hw/arm/virt.c
@@ -1206,7 +1206,7 @@ static void create_virtio_devices(const VirtMachineState *vms)
      * between kernel versions). For reliable and stable identification
      * of disks users must use UUIDs or similar mechanisms.
      */
-    for (i = 0; i < NUM_VIRTIO_TRANSPORTS; i++) {
+    for (i = 0; i < vms->virtio_transports; i++) {
         int irq = vms->irqmap[VIRT_MMIO] + i;
         hwaddr base = vms->memmap[VIRT_MMIO].base + i * size;
 
@@ -1221,7 +1221,7 @@ static void create_virtio_devices(const VirtMachineState *vms)
      * loop influences virtio device to virtio transport assignment, whereas
      * this loop controls how virtio transports are laid out in the dtb.
      */
-    for (i = NUM_VIRTIO_TRANSPORTS - 1; i >= 0; i--) {
+    for (i = vms->virtio_transports - 1; i >= 0; i--) {
         char *nodename;
         int irq = vms->irqmap[VIRT_MMIO] + i;
         hwaddr base = vms->memmap[VIRT_MMIO].base + i * size;
@@ -2705,6 +2705,36 @@ static void virt_set_highmem_mmio_size(Object *obj, Visitor *v,
     extended_memmap[VIRT_HIGH_PCIE_MMIO].size = size;
 }
 
+static void virt_get_virtio_transports(Object *obj, Visitor *v,
+                                       const char *name, void *opaque,
+                                       Error **errp)
+{
+    VirtMachineState *vms = VIRT_MACHINE(obj);
+    uint8_t transports = vms->virtio_transports;
+
+    visit_type_uint8(v, name, &transports, errp);
+}
+
+static void virt_set_virtio_transports(Object *obj, Visitor *v,
+                                       const char *name, void *opaque,
+                                       Error **errp)
+{
+    VirtMachineState *vms = VIRT_MACHINE(obj);
+    uint8_t transports;
+
+    if (!visit_type_uint8(v, name, &transports, errp)) {
+        return;
+    }
+
+    if (transports > NUM_VIRTIO_TRANSPORTS) {
+        error_setg(errp, "virtio-transports must not exceed %d",
+                   NUM_VIRTIO_TRANSPORTS);
+        return;
+    }
+
+    vms->virtio_transports = transports;
+}
+
 static bool virt_get_its(Object *obj, Error **errp)
 {
     VirtMachineState *vms = VIRT_MACHINE(obj);
@@ -3427,6 +3457,13 @@ static void virt_machine_class_init(ObjectClass *oc, const void *data)
                                           "Set the high memory region size "
                                           "for PCI MMIO");
 
+    object_class_property_add(oc, "virtio-transports", "uint8",
+                                   virt_get_virtio_transports,
+                                   virt_set_virtio_transports,
+                                   NULL, NULL);
+    object_class_property_set_description(oc, "virtio-transports",
+                                          "Set the number of virtio-mmio transports to instantiate");
+
     object_class_property_add_str(oc, "gic-version", virt_get_gic_version,
                                   virt_set_gic_version);
     object_class_property_set_description(oc, "gic-version",
@@ -3539,6 +3576,8 @@ static void virt_instance_init(Object *obj)
     vms->dtb_randomness = true;
 
     vms->irqmap = a15irqmap;
+
+    vms->virtio_transports = NUM_VIRTIO_TRANSPORTS;
 
     virt_flash_create(vms);
 
