0c02443013798fb3f96528c70f787904ad7d46d1
Nicolin Chen <nicolinc@nvidia.com>
2026-02-11 08:34:13 +0000

    hw/arm/smmuv3-accel: Allocate vEVENTQ for accelerated SMMUv3 devices
    When the guest enables the Event Queue and a vIOMMU is present, allocate a
    vEVENTQ object so that host-side events related to the vIOMMU can be
    received and propagated back to the guest.
    
    For cold-plugged devices using SMMUv3 acceleration, the vIOMMU is created
    before the guest boots. In this case, the vEVENTQ is allocated when the
    guest writes to SMMU_CR0 and sets EVENTQEN = 1.
    
    If no cold-plugged device exists at boot (i.e. no vIOMMU initially), the
    vEVENTQ is allocated when a vIOMMU is created, i.e. during the first
    device hot-plug.
    
    Event read and propagation will be added in a later patch.
    
    Signed-off-by: Nicolin Chen <nicolinc@nvidia.com>
    Tested-by: Nicolin Chen <nicolinc@nvidia.com>
    Reviewed-by: Eric Auger <eric.auger@redhat.com>
    Signed-off-by: Shameer Kolothum <skolothumtho@nvidia.com>
    Message-Id: <20260211083415.133534-4-skolothumtho@nvidia.com>

--- hw/arm/smmuv3.c
+++ hw/arm/smmuv3.c
@@ -1605,6 +1605,10 @@ static MemTxResult smmu_writel(SMMUv3State *s, hwaddr offset,
         s->cr0ack = data & ~SMMU_CR0_RESERVED;
         /* in case the command queue has been enabled */
         smmuv3_cmdq_consume(s, &local_err);
+        /* Allocate vEVENTQ if EventQ is enabled and a vIOMMU is available */
+        if (local_err == NULL) {
+            smmuv3_accel_alloc_veventq(s, &local_err);
+        }
         break;
     case A_CR1:
         s->cr[1] = data;
