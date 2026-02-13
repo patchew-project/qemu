95d79e606dee86aa2071cfd4b1b8c5c00cd9131d
Philippe Mathieu-Daudé <philmd@linaro.org>
2026-02-13 19:27:13 +0100

    page-vary: Inline set_preferred_target_page_bits_common()
    set_preferred_target_page_bits_common() is only used once
    in set_preferred_target_page_bits(): inline it there and
    remove its prototype.
    
    Signed-off-by: Philippe Mathieu-Daudé <philmd@linaro.org>
    Reviewed-by: Pierrick Bouvier <pierrick.bouvier@linaro.org>
    Message-Id: <20260213182713.44924-6-philmd@linaro.org>

--- page-vary-common.c
+++ page-vary-common.c
@@ -27,29 +27,26 @@
 
 TargetPageBits target_page;
 
-bool set_preferred_target_page_bits_common(int bits)
-{
-    /*
-     * The target page size is the lowest common denominator for all
-     * the CPUs in the system, so we can only make it smaller, never
-     * larger. And we can't make it smaller once we've committed to
-     * a particular size.
-     */
-    if (target_page.bits == 0 || target_page.bits > bits) {
-        if (target_page.decided) {
-            return false;
-        }
-        target_page.bits = bits;
-    }
-    return true;
-}
-
 bool set_preferred_target_page_bits(int bits)
 {
     assert(bits >= TARGET_PAGE_BITS_MIN);
+
     if (target_page_bits_vary()) {
-        return set_preferred_target_page_bits_common(bits);
+        /*
+         * The target page size is the lowest common denominator for all
+         * the CPUs in the system, so we can only make it smaller, never
+         * larger. And we can't make it smaller once we've committed to
+         * a particular size.
+         */
+        if (target_page.bits == 0 || target_page.bits > bits) {
+            if (target_page.decided) {
+                return false;
+            }
+            target_page.bits = bits;
+        }
+        return true;
     }
+
     return true;
 }
 
