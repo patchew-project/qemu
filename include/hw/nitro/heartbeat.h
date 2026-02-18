5b8df97c44656ff58b8c51c23fc975adb64f836b
Alexander Graf <graf@amazon.com>
2026-02-18 01:51:45 +0000

    hw/nitro: Introduce Nitro Enclave Heartbeat device
    Nitro Enclaves expect the parent instance to host a vsock heartbeat listener
    at port 9000. To host a Nitro Enclave with the nitro accel in QEMU, add
    such a heartbeat listener as device model, so that the machine can
    easily instantiate it.
    
    Signed-off-by: Alexander Graf <graf@amazon.com>
    Message-Id: <20260218015151.4052-6-graf@amazon.com>

---
+++ include/hw/nitro/heartbeat.h
@@ -0,0 +1,25 @@
+/*
+ * Nitro Heartbeat device
+ *
+ * SPDX-License-Identifier: GPL-2.0-or-later
+ */
+
+#ifndef HW_MISC_NITRO_HEARTBEAT_H
+#define HW_MISC_NITRO_HEARTBEAT_H
+
+#include "hw/core/qdev.h"
+#include "hw/core/sysbus.h"
+#include "chardev/char-fe.h"
+#include "qom/object.h"
+
+#define TYPE_NITRO_HEARTBEAT "nitro-heartbeat"
+OBJECT_DECLARE_SIMPLE_TYPE(NitroHeartbeatState, NITRO_HEARTBEAT)
+
+struct NitroHeartbeatState {
+    SysBusDevice parent_obj;
+
+    CharFrontend vsock;     /* vsock server chardev for heartbeat */
+    bool done;
+};
+
+#endif /* HW_MISC_NITRO_HEARTBEAT_H */
