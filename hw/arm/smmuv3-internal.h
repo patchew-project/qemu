fe3bbe4838defb243a2e1c49add2d92a07f59ad0
Tao Tang <tangtao1634@phytium.com.cn>
2026-02-21 18:02:25 +0800

    hw/arm/smmuv3: Track SEC_SID in configs and events
    Cache the SEC_SID inside SMMUTransCfg to keep configuration lookups
    tied to the correct register bank.
    
    Plumb the SEC_SID through tracepoints and queue helpers so diagnostics
    and event logs always show which security interface emitted the record.
    To support this, the SEC_SID is placed in SMMUEventInfo so the bank is
    identified as soon as an event record is built.
    
    Signed-off-by: Tao Tang <tangtao1634@phytium.com.cn>
    Reviewed-by: Eric Auger <eric.auger@redhat.com>
    Message-Id: <20260221100250.2976287-7-tangtao1634@phytium.com.cn>

--- hw/arm/smmuv3-internal.h
+++ hw/arm/smmuv3-internal.h
@@ -274,6 +274,7 @@ static inline const char *smmu_event_string(SMMUEventType type)
 
 /*  Encode an event record */
 typedef struct SMMUEventInfo {
+    SMMUSecSID sec_sid;
     SMMUEventType type;
     uint32_t sid;
     bool recorded;
