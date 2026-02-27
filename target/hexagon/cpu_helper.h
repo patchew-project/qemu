edaac2e7b2695b0b3dace1c4b3498f2eb11dcf31
Brian Cain <bcain@quicinc.com>
2026-02-27 12:36:15 -0800

    target/hexagon: Add sysemu TCG overrides
    Define TCG overrides for setprio(), crswap(,sgp{0,1,1:0}).
    
    Signed-off-by: Brian Cain <brian.cain@oss.qualcomm.com>
    Reviewed-by: Taylor Simpson <ltaylorsimpson@gmail.com>
    Message-Id: <20260227203627.932864-26-brian.cain@oss.qualcomm.com>

---
+++ target/hexagon/cpu_helper.h
@@ -0,0 +1,34 @@
+/*
+ * Copyright (c) Qualcomm Technologies, Inc. and/or its subsidiaries.
+ *
+ * SPDX-License-Identifier: GPL-2.0-or-later
+ */
+
+#ifndef HEXAGON_CPU_HELPER_H
+#define HEXAGON_CPU_HELPER_H
+
+static inline void arch_set_thread_reg(CPUHexagonState *env, uint32_t reg,
+                                       uint32_t val)
+{
+    g_assert(reg < TOTAL_PER_THREAD_REGS);
+    g_assert_not_reached();
+}
+
+static inline uint32_t arch_get_thread_reg(CPUHexagonState *env, uint32_t reg)
+{
+    g_assert(reg < TOTAL_PER_THREAD_REGS);
+    g_assert_not_reached();
+}
+
+static inline void arch_set_system_reg(CPUHexagonState *env, uint32_t reg,
+                                       uint32_t val)
+{
+    g_assert_not_reached();
+}
+
+static inline uint32_t arch_get_system_reg(CPUHexagonState *env, uint32_t reg)
+{
+    g_assert_not_reached();
+}
+
+#endif
