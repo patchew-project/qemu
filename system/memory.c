97f46daa0b562928a0f4a8b6ad74257461ba3e4c
BALATON Zoltan <balaton@eik.bme.hu>
2026-03-03 22:47:54 +0100

    system/memory: Reduce memory_region_init_ram_nomigrate() usage
    This function is kept until the last two usages can be removed but
    convert the last usage in memory.c and remove it from the
    documentation.
    
    Signed-off-by: BALATON Zoltan <balaton@eik.bme.hu>
    Message-Id: <c99082f66cc760ccebdf050d5a9d683d817e7bb2.1772572714.git.balaton@eik.bme.hu>

--- system/memory.c
+++ system/memory.c
@@ -3695,7 +3695,8 @@ bool memory_region_init_ram(MemoryRegion *mr,
 {
     DeviceState *owner_dev;
 
-    if (!memory_region_init_ram_nomigrate(mr, owner, name, size, errp)) {
+    if (!memory_region_init_ram_flags_nomigrate(mr, owner, name,
+                                                size, 0, errp)) {
         return false;
     }
     /* This will assert if owner is neither NULL nor a DeviceState.
