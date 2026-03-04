540541493aca86fdd99f11ae20a7e34e1920247e
Zide Chen <zide.chen@intel.com>
2026-03-04 10:07:08 -0800

    target/i386: Clean up LBR format handling
    Since the lbr-fmt property is masked with PERF_CAP_LBR_FMT in
    DEFINE_PROP_UINT64_CHECKMASK(), there is no need to explicitly validate
    user-requested lbr-fmt values.
    
    The PMU feature is only supported when running under KVM, so initialize
    cpu->lbr_fmt in kvm_cpu_instance_init().  Use -1 as the default lbr-fmt,
    rather than initializing it with ~PERF_CAP_LBR_FMT, which is misleading
    as it suggests a semantic relationship that does not exist.
    
    Rename requested_lbr_fmt to a more generic guest_fmt.  When lbr-fmt is
    not specified and cpu->migratable is false, the guest lbr_fmt value is
    not user-requested.
    
    Reviewed-by: Dapeng Mi <dapeng1.mi@linux.intel.com>
    Signed-off-by: Zide Chen <zide.chen@intel.com>
    Message-Id: <20260304180713.360471-10-zide.chen@intel.com>

--- target/i386/cpu.c
+++ target/i386/cpu.c
@@ -9815,7 +9815,7 @@ static void x86_cpu_realizefn(DeviceState *dev, Error **errp)
     X86CPUClass *xcc = X86_CPU_GET_CLASS(dev);
     CPUX86State *env = &cpu->env;
     Error *local_err = NULL;
-    unsigned requested_lbr_fmt;
+    unsigned guest_fmt;
 
     if (!kvm_enabled())
         cpu->enable_pmu = false;
@@ -9855,11 +9855,7 @@ static void x86_cpu_realizefn(DeviceState *dev, Error **errp)
      * Override env->features[FEAT_PERF_CAPABILITIES].LBR_FMT
      * with user-provided setting.
      */
-    if (cpu->lbr_fmt != ~PERF_CAP_LBR_FMT) {
-        if ((cpu->lbr_fmt & PERF_CAP_LBR_FMT) != cpu->lbr_fmt) {
-            error_setg(errp, "invalid lbr-fmt");
-            return;
-        }
+    if (cpu->lbr_fmt != -1) {
         env->features[FEAT_PERF_CAPABILITIES] &= ~PERF_CAP_LBR_FMT;
         env->features[FEAT_PERF_CAPABILITIES] |= cpu->lbr_fmt;
     }
@@ -9868,9 +9864,8 @@ static void x86_cpu_realizefn(DeviceState *dev, Error **errp)
      * vPMU LBR is supported when 1) KVM is enabled 2) Option pmu=on and
      * 3)vPMU LBR format matches that of host setting.
      */
-    requested_lbr_fmt =
-        env->features[FEAT_PERF_CAPABILITIES] & PERF_CAP_LBR_FMT;
-    if (requested_lbr_fmt && kvm_enabled()) {
+    guest_fmt = env->features[FEAT_PERF_CAPABILITIES] & PERF_CAP_LBR_FMT;
+    if (guest_fmt) {
         uint64_t host_perf_cap =
             x86_cpu_get_supported_feature_word(NULL, FEAT_PERF_CAPABILITIES);
         unsigned host_lbr_fmt = host_perf_cap & PERF_CAP_LBR_FMT;
@@ -9879,10 +9874,10 @@ static void x86_cpu_realizefn(DeviceState *dev, Error **errp)
             error_setg(errp, "vPMU: LBR is unsupported without pmu=on");
             return;
         }
-        if (requested_lbr_fmt != host_lbr_fmt) {
+        if (guest_fmt != host_lbr_fmt) {
             error_setg(errp, "vPMU: the lbr-fmt value (0x%x) does not match "
                         "the host value (0x%x).",
-                        requested_lbr_fmt, host_lbr_fmt);
+                        guest_fmt, host_lbr_fmt);
             return;
         }
     }
@@ -10303,7 +10298,6 @@ static void x86_cpu_initfn(Object *obj)
     object_property_add_alias(obj, "sse4_2", obj, "sse4.2");
 
     object_property_add_alias(obj, "hv-apicv", obj, "hv-avic");
-    cpu->lbr_fmt = ~PERF_CAP_LBR_FMT;
     object_property_add_alias(obj, "lbr_fmt", obj, "lbr-fmt");
 
     if (xcc->model) {
