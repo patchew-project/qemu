5580a41696fd832d376ee4a312832ff337713208
James Wainwright <james.wainwright@lowrisc.org>
2026-03-04 12:11:11 +0000

    util: export CRC32[C] lookup tables
    These are needed for the RISC-V Zbr0p93 CRC32 instructions which pre-XOR
    the data into the CRC state before the instruction is executed, making
    the zlib crc32 and QEMU crc32c implementations inappropriate.
    
    https://github.com/riscv/riscv-bitmanip/releases/download/v0.93/bitmanip-0.93.pdf
    
    Signed-off-by: James Wainwright <james.wainwright@lowrisc.org>
    Reviewed-by: Alistair Francis <alistair.francis@wdc.com>
    Message-Id: <20260304121113.117299-2-james.wainwright@lowrisc.org>

---
+++ include/qemu/crc32.h
@@ -0,0 +1,18 @@
+/*
+ * CRC32 Checksum
+ *
+ * Copyright (c) 2026 QEMU contributors
+ *
+ * This program is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU General Public License as published by the Free
+ * Software Foundation; either version 2 of the License, or (at your option)
+ * any later version.
+ *
+ */
+
+#ifndef QEMU_CRC32_H
+#define QEMU_CRC32_H
+
+extern const uint32_t crc32_table[256];
+
+#endif
