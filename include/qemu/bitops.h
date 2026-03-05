051fc0dcec98f70fcdfaf4da18a7a809995788ec
Xuemei Liu <liu.xuemei1@zte.com.cn>
2026-03-05 17:26:00 +0800

    hw/intc/imsic: Add in-kernel imsic save and restore function
    Add save and restore funtction if kvm_irqchip_in_kernel() return
    true, it is to get and set IMSIC irqchip state from KVM kernel.
    
    Signed-off-by: Xuemei Liu <liu.xuemei1@zte.com.cn>
    Message-Id: <20260305172600336tzCExHGy8QY2jMA9e7S_g@zte.com.cn>

--- include/qemu/bitops.h
+++ include/qemu/bitops.h
@@ -16,10 +16,11 @@
 #include "host-utils.h"
 #include "atomic.h"
 
-#define BITS_PER_BYTE           CHAR_BIT
-#define BITS_PER_LONG           (sizeof (unsigned long) * BITS_PER_BYTE)
-#define BITS_TO_LONGS(nr)       DIV_ROUND_UP(nr, BITS_PER_BYTE * sizeof(long))
-#define BITS_TO_U32S(nr)        DIV_ROUND_UP(nr, BITS_PER_BYTE * sizeof(uint32_t))
+#define BITS_PER_BYTE       CHAR_BIT
+#define BITS_PER_LONG       (sizeof(unsigned long) * BITS_PER_BYTE)
+#define BITS_TO_LONGS(nr)   DIV_ROUND_UP(nr, BITS_PER_BYTE * sizeof(long))
+#define BITS_TO_U64S(nr)    DIV_ROUND_UP(nr, BITS_PER_BYTE * sizeof(uint64_t))
+#define BITS_TO_U32S(nr)    DIV_ROUND_UP(nr, BITS_PER_BYTE * sizeof(uint32_t))
 
 #define BIT(nr)                 (1UL << (nr))
 #define BIT_ULL(nr)             (1ULL << (nr))
