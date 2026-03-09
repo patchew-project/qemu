1603bf42b91c9b0196aa7a8b208b687cc134db44
Aditya Gupta <adityag@linux.ibm.com>
2026-03-09 12:49:52 +0530

    ppc/pnv: Handle stash command in PowerNV SBE
    Earlier since the SBE_CMD_STASH_MPIPL_CONFIG command was not handled, so
    skiboot used to not get any response from SBE:
    
        [  106.350742821,3] SBE: Message timeout [chip id = 0], cmd = d7, subcmd = 7
        [  106.352067746,3] SBE: Failed to send stash MPIPL config [chip id = 0x0, rc = 254]
    
    Fix this by handling the command in PowerNV SBE, and sending a response so
    skiboot knows SBE has handled the STASH command
    
    The stashed skiboot base is later used to access the relocated MDST/MDDT
    tables when MPIPL is implemented.
    
    The purpose of stashing relocated base address is explained in following
    skiboot commit:
    
        author Vasant Hegde <hegdevasant@linux.vnet.ibm.com> Fri Jul 12 16:47:51 2019 +0530
        committer Oliver O'Halloran <oohall@gmail.com> Thu Aug 15 17:53:39 2019 +1000
    
        SBE: Send OPAL relocated base address to SBE
    
          OPAL relocates itself during boot. During memory preserving IPL hostboot needs
          to access relocated OPAL base address to get MDST, MDDT tables. Hence send
          relocated base address to SBE via 'stash MPIPL config' chip-op. During next
          IPL SBE will send stashed data to hostboot... so that hostboot can access
          these data.
    
    Reviewed-by: Hari Bathini <hbathini@linux.ibm.com>
    Signed-off-by: Aditya Gupta <adityag@linux.ibm.com>
    Tested-by: Shivang Upadhyay <shivangu@linux.ibm.com>
    Message-Id: <20260309072001.3136261-4-adityag@linux.ibm.com>

--- include/hw/ppc/pnv_mpipl.h
+++ include/hw/ppc/pnv_mpipl.h
@@ -8,11 +8,14 @@
 #define PNV_MPIPL_H
 
 #include "qemu/osdep.h"
+#include "exec/hwaddr.h"
 
 typedef struct MpiplPreservedState MpiplPreservedState;
 
 /* Preserved state to be saved in PnvMachineState */
 struct MpiplPreservedState {
+    /* skiboot_base will be valid only after OPAL sends relocated base to SBE */
+    hwaddr     skiboot_base;
     bool       is_next_boot_mpipl;
 };
 
