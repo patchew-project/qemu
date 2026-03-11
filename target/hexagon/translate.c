82da12cc1254063ad25232bfb6ab8ab076390df1
Brian Cain <brian.cain@oss.qualcomm.com>
2026-03-10 20:48:57 -0700

    target/hexagon: Add privilege check, use tag_ignore()
    Add system event and cause code definitions needed for exception
    handling in sysemu mode.  Add privilege checks that raise exceptions
    for guest/supervisor-only instructions executed without appropriate
    privilege.
    
    Reviewed-by: Taylor Simpson <ltaylorsimpson@gmail.com>
    Signed-off-by: Brian Cain <brian.cain@oss.qualcomm.com>
    Message-Id: <20260311034923.1044737-10-brian.cain@oss.qualcomm.com>

--- target/hexagon/translate.c
+++ target/hexagon/translate.c
@@ -65,6 +65,10 @@ TCGv hex_vstore_addr[VSTORES_MAX];
 TCGv hex_vstore_size[VSTORES_MAX];
 TCGv hex_vstore_pending[VSTORES_MAX];
 
+#ifndef CONFIG_USER_ONLY
+TCGv_i32 hex_cause_code;
+#endif
+
 static const char * const hexagon_prednames[] = {
   "p0", "p1", "p2", "p3"
 };
@@ -1153,4 +1157,8 @@ void hexagon_translate_init(void)
             offsetof(CPUHexagonState, vstore_pending[i]),
             vstore_pending_names[i]);
     }
+#ifndef CONFIG_USER_ONLY
+    hex_cause_code = tcg_global_mem_new(tcg_env,
+        offsetof(CPUHexagonState, cause_code), "cause_code");
+#endif
 }
