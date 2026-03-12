23a4d4a731e07484e1c07e68bd87ad99765abac9
Nathan Chen <nathanc@nvidia.com>
2026-03-12 14:03:24 -0700

    hw/arm/smmuv3-accel: Change SSIDSIZE property to SsidSizeMode
    Change accel SMMUv3 SSIDSIZE property from uint8_t to SsidSizeMode.
    Setting 'auto' will use the default value, i.e. 0 in IDR1. A future
    patch will implement resolution of 'auto' value to match the host SMMUv3
    SSIDSIZE value.
    
    Signed-off-by: Nathan Chen <nathanc@nvidia.com>
    Message-Id: <20260312210328.2016191-6-nathanc@nvidia.com>

--- hw/arm/smmuv3-accel.c
+++ hw/arm/smmuv3-accel.c
@@ -803,7 +803,7 @@ static uint64_t smmuv3_accel_get_viommu_flags(void *opaque)
     SMMUState *bs = opaque;
     SMMUv3State *s = ARM_SMMUV3(bs);
 
-    if (s->ssidsize) {
+    if (s->ssidsize > SSID_SIZE_MODE_0) {
         flags |= VIOMMU_FLAG_PASID_SUPPORTED;
     }
     return flags;
@@ -817,6 +817,16 @@ static const PCIIOMMUOps smmuv3_accel_ops = {
     .unset_iommu_device = smmuv3_accel_unset_iommu_device,
     .get_msi_direct_gpa = smmuv3_accel_get_msi_gpa,
 };
+
+static uint8_t ssidsize_mode_to_value(SsidSizeMode mode)
+{
+    /* SSID_SIZE_MODE_0 = 1, SSID_SIZE_MODE_1 = 2, etc. */
+    /* SSID_SIZE_MODE_AUTO = 0 */
+    if (mode == SSID_SIZE_MODE_AUTO) {
+        return 0;
+    }
+    return mode - 1;  /* Enum values are offset by 1 from actual values */
+}
 
 void smmuv3_accel_idr_override(SMMUv3State *s)
 {
@@ -847,7 +857,10 @@ void smmuv3_accel_idr_override(SMMUv3State *s)
      * By default QEMU SMMUv3 has no SubstreamID support. Update IDR1 if user
      * has enabled it.
      */
-    s->idr[1] = FIELD_DP32(s->idr[1], IDR1, SSIDSIZE, s->ssidsize);
+    if (s->ssidsize != SSID_SIZE_MODE_AUTO) {
+        s->idr[1] = FIELD_DP32(s->idr[1], IDR1, SSIDSIZE,
+                               ssidsize_mode_to_value(s->ssidsize));
+    }
 }
 
 /* Based on SMUUv3 GPBA.ABORT configuration, attach a corresponding HWPT */
