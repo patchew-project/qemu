e4116545cbd03f521a9ae219e1248845b22407ca
Philippe Mathieu-Daudé <philmd@linaro.org>
2026-03-13 07:20:31 +0100

    system/kvm_xen: Include missing 'exec/target_page.h' header
    The "exec/target_page.h" header is indirectly included. Include
    it explicitly otherwise we get when refactoring unrelated headers:
    
      include/system/kvm_xen.h:41:55: error: ‘TARGET_PAGE_BITS’ undeclared (first use in this function)
         41 | #define XEN_SPECIAL_PFN(x) ((XEN_SPECIAL_AREA_ADDR >> TARGET_PAGE_BITS) + \
            |                                                       ^~~~~~~~~~~~~~~~
    
    Signed-off-by: Philippe Mathieu-Daudé <philmd@linaro.org>
    Reviewed-by: Pierrick Bouvier <pierrick.bouvier@linaro.org>
    Message-Id: <20260313062055.2188-16-philmd@linaro.org>

--- include/system/kvm_xen.h
+++ include/system/kvm_xen.h
@@ -12,6 +12,8 @@
 #ifndef QEMU_SYSTEM_KVM_XEN_H
 #define QEMU_SYSTEM_KVM_XEN_H
 
+#include "exec/target_page.h"
+
 /* The KVM API uses these to indicate "no GPA" or "no GFN" */
 #define INVALID_GPA UINT64_MAX
 #define INVALID_GFN UINT64_MAX
