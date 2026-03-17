13feb671d1cea8574c6dfc47a7725e0d193398d6
Marc-André Lureau <marcandre.lureau@redhat.com>
2026-03-17 12:50:52 +0400

    ui: make qemu_default_pixelformat() static inline
    The function is a trivial wrapper around qemu_default_pixman_format()
    and qemu_pixelformat_from_pixman(), so make it static inline in
    qemu-pixman.h instead of a standalone function in console.c, allowing to
    be easily reused.
    
    Signed-off-by: Marc-André Lureau <marcandre.lureau@redhat.com>
    Message-Id: <20260317-qemu-vnc-v1-38-48eb1dcf7b76@redhat.com>

--- include/ui/qemu-pixman.h
+++ include/ui/qemu-pixman.h
@@ -73,6 +73,14 @@ typedef struct PixelFormat {
 
 PixelFormat qemu_pixelformat_from_pixman(pixman_format_code_t format);
 pixman_format_code_t qemu_default_pixman_format(int bpp, bool native_endian);
+
+static inline PixelFormat qemu_default_pixelformat(int bpp)
+{
+    pixman_format_code_t fmt = qemu_default_pixman_format(bpp, true);
+    PixelFormat pf = qemu_pixelformat_from_pixman(fmt);
+    return pf;
+}
+
 pixman_format_code_t qemu_drm_format_to_pixman(uint32_t drm_format);
 uint32_t qemu_pixman_to_drm_format(pixman_format_code_t pixman);
 int qemu_pixman_get_type(int rshift, int gshift, int bshift, int endian);
