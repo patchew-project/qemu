07bbee750729aa580c3feaa4468b47171070091d
Marc-André Lureau <marcandre.lureau@redhat.com>
2026-03-17 12:50:23 +0400

    ui/vnc-jobs: drop redundant (and needless) qemu_thread_get_self()
    The call is unnecessary, since "thread" is already set at creation time.
    Furthermore, the "thread" field is mostly useless as the thread is
    created DETACHED and isn't used for anything but perhaps debugging.
    
    Signed-off-by: Marc-André Lureau <marcandre.lureau@redhat.com>
    Message-Id: <20260317-qemu-vnc-v1-9-48eb1dcf7b76@redhat.com>

--- ui/vnc-jobs.c
+++ ui/vnc-jobs.c
@@ -349,8 +349,6 @@ static void *vnc_worker_thread(void *arg)
 {
     VncJobQueue *queue = arg;
 
-    qemu_thread_get_self(&queue->thread);
-
     while (!vnc_worker_thread_loop(queue)) ;
     g_assert_not_reached();
     return NULL;
