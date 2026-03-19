28687015383a38c3201ab927abbf7e540db472fd
Ani Sinha <anisinha@redhat.com>
2026-03-19 17:51:35 +0530

    i386/pc_sysfw: stub out x86_firmware_configure
    x86_firmware_configure requires ovmf support. Add a stub for this function call
    for cases where OVMF is not supported.
    
    Reported-by: Michael Tokarev <mjt@tls.msk.ru>
    Signed-off-by: Ani Sinha <anisinha@redhat.com>
    Reviewed-by: Michael Tokarev <mjt@tls.msk.ru>
    Tested-by: Michael Tokarev <mjt@tls.msk.ru>
    Message-Id: <20260319122137.142178-2-anisinha@redhat.com>

--- hw/i386/pc_sysfw.c
+++ hw/i386/pc_sysfw.c
@@ -283,34 +283,3 @@ void pc_system_firmware_init(PCMachineState *pcms,
         }
     }
 }
-
-void x86_firmware_configure(hwaddr gpa, void *ptr, int size)
-{
-    int ret;
-
-    /*
-     * OVMF places a GUIDed structures in the flash, so
-     * search for them
-     */
-    pc_system_parse_ovmf_flash(ptr, size);
-
-    if (sev_enabled()) {
-
-        /* Copy the SEV metadata table (if it exists) */
-        pc_system_parse_sev_metadata(ptr, size);
-
-        ret = sev_es_save_reset_vector(ptr, size);
-        if (ret) {
-            error_report("failed to locate and/or save reset vector");
-            exit(1);
-        }
-
-        sev_encrypt_flash(gpa, ptr, size, &error_fatal);
-    } else if (is_tdx_vm()) {
-        ret = tdx_parse_tdvf(ptr, size);
-        if (ret) {
-            error_report("failed to parse TDVF for TDX VM");
-            exit(1);
-        }
-    }
-}
