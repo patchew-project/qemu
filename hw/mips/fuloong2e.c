ae70c177f8ba4b86457f19d07b3ac8506f0b33cb
BALATON Zoltan <balaton@eik.bme.hu>
2026-03-21 02:04:24 +0100

    ati-vga: Add work around for fuloong2e
    With the linear aperture size fixed to match real card fuloong2e no
    longer works due to running out of PCI memory because only one PCI bus
    is emulated on that machine. Add a property to allow fuloong2e to set
    a smaller linear aperture size to work around that problem until the
    machine model is improved.
    
    Signed-off-by: BALATON Zoltan <balaton@eik.bme.hu>
    Reviewed-by: Chad Jablonski <chad@jablonski.xyz>
    Message-Id: <47cbdc7ad2291f22467f9fc86e7287eb8983c927.1774054635.git.balaton@eik.bme.hu>

--- hw/mips/fuloong2e.c
+++ hw/mips/fuloong2e.c
@@ -316,6 +316,7 @@ static void mips_fuloong2e_init(MachineState *machine)
         dev = DEVICE(pci_dev);
         qdev_prop_set_uint32(dev, "vgamem_mb", 16);
         qdev_prop_set_uint16(dev, "x-device-id", 0x5159);
+        qdev_prop_set_uint64(dev, "x-linear-aper-size", 16 * MiB);
         pci_realize_and_unref(pci_dev, pci_bus, &error_fatal);
     }
 
