5f1343782a0c32866472313b2626aa3545c38867
Zhuoying Cai <zycai@linux.ibm.com>
2026-04-02 18:14:29 -0400

    s390x/diag: Implement DIAG 320 subcode 1
    DIAG 320 subcode 1 provides information needed to determine
    the amount of storage to store one or more certificates from the
    certificate store.
    
    Upon successful completion, this subcode returns information of the current
    cert store, such as the number of certificates stored and allowed in the cert
    store, amount of space may need to be allocate to store a certificate,
    etc for verification-certificate blocks (VCBs).
    
    The subcode value is denoted by setting the left-most bit
    of an 8-byte field.
    
    The verification-certificate-storage-size block (VCSSB) contains
    the output data when the operation completes successfully. A VCSSB
    length of 4 indicates that no certificate are available in the cert
    store.
    
    Signed-off-by: Zhuoying Cai <zycai@linux.ibm.com>
    Reviewed-by: Farhan Ali <alifm@linux.ibm.com>
    Reviewed-by: Collin Walling <walling@linux.ibm.com>
    Message-Id: <20260402221453.1602899-8-zycai@linux.ibm.com>

--- include/hw/s390x/ipl/diag320.h
+++ include/hw/s390x/ipl/diag320.h
@@ -11,10 +11,32 @@
 #define S390X_DIAG320_H
 
 #define DIAG_320_SUBC_QUERY_ISM     0
+#define DIAG_320_SUBC_QUERY_VCSI    1
 
 #define DIAG_320_RC_OK              0x0001
 #define DIAG_320_RC_NOT_SUPPORTED   0x0102
+#define DIAG_320_RC_INVAL_VCSSB_LEN 0x0202
 
 #define DIAG_320_ISM_QUERY_SUBCODES 0x80000000
+#define DIAG_320_ISM_QUERY_VCSI     0x40000000
+
+#define VCSSB_NO_VC     4
+#define VCSSB_MIN_LEN   128
+#define VCE_HEADER_LEN  128
+#define VCB_HEADER_LEN  64
+
+struct VCStorageSizeBlock {
+    uint32_t length;
+    uint8_t reserved0[3];
+    uint8_t version;
+    uint32_t reserved1[6];
+    uint16_t total_vc_ct;
+    uint16_t max_vc_ct;
+    uint32_t reserved3[11];
+    uint32_t max_single_vcb_len;
+    uint32_t total_vcb_len;
+    uint32_t reserved4[10];
+};
+typedef struct VCStorageSizeBlock VCStorageSizeBlock;
 
 #endif
