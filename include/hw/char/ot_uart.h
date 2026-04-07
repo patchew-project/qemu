966d090371e89fd6283cec2b77c070aee48b9173
Lex Bailey <lex.bailey@lowrisc.org>
2026-04-07 15:11:43 +0100

    ot_uart: update file headers with new authorship and documentation URL
    The documentation has moved, URL updated.
    This file is now a combination of code from various people/organisations.
    Took the opportunity while editing this to switch to just using
    the SPDX ID.
    
    Signed-off-by: Lex Bailey <lex.bailey@lowrisc.org>
    Message-Id: <177564643888.23414.7922925369077631439-6@git.sr.ht>

--- include/hw/char/ot_uart.h
+++ include/hw/char/ot_uart.h
@@ -2,24 +2,19 @@
  * QEMU OpenTitan UART device
  *
  * Copyright (c) 2020 Western Digital
+ * Copyright (c) 2022-2025 Rivos, Inc.
+ * Copyright (c) 2025-2026 lowRISC contributors.
  *
- * Permission is hereby granted, free of charge, to any person obtaining a copy
- * of this software and associated documentation files (the "Software"), to deal
- * in the Software without restriction, including without limitation the rights
- * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
- * copies of the Software, and to permit persons to whom the Software is
- * furnished to do so, subject to the following conditions:
+ * For details check the documentation here:
+ *    https://opentitan.org/book/hw/ip/uart/doc/
  *
- * The above copyright notice and this permission notice shall be included in
- * all copies or substantial portions of the Software.
+ * Author(s):
+ *  Alistair Francis <alistair.francis@wdc.com>
+ *  Loïc Lefort <loic@rivosinc.com>
+ *  Lex Bailey <lex.bailey@lowrisc.com>
  *
- * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
- * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
- * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL
- * THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
- * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
- * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
- * THE SOFTWARE.
+ * SPDX-License-Identifier: MIT
+ *
  */
 
 #ifndef HW_OT_UART_H
