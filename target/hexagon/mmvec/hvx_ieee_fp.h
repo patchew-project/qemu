d93859d1a0b2431f6dad3ba01708e4253069c2ab
Matheus Tavares Bernardino <matheus.bernardino@oss.qualcomm.com>
2026-04-08 09:37:02 -0700

    target/hexagon: add v73 HVX IEEE bfloat16 insns
    Add HVX IEEE bfloat16 (bf16) instructions:
    
    Arithmetic operations:
    - V6_vadd_sf_bf, V6_vsub_sf_bf: add/sub bf16 widening to sf output
    - V6_vmpy_sf_bf: multiply bf16 widening to sf output
    - V6_vmpy_sf_bf_acc: multiply-accumulate bf16 widening to sf output
    
    Min/Max operations:
    - V6_vmin_bf, V6_vmax_bf: bf16 min/max
    
    Comparison operations:
    - V6_vgtbf: greater-than compare
    - V6_vgtbf_and, V6_vgtbf_or, V6_vgtbf_xor: predicate variants
    
    Conversion operations:
    - V6_vcvt_bf_sf: convert sf to bf16
    
    Signed-off-by: Matheus Tavares Bernardino <matheus.bernardino@oss.qualcomm.com>
    Reviewed-by: Taylor Simpson <ltaylorsimpson@gmail.com>
    Message-Id: <f84d180547f96f809684e858861086dc6dab9ed8.1775665981.git.matheus.bernardino@oss.qualcomm.com>

--- target/hexagon/mmvec/hvx_ieee_fp.h
+++ target/hexagon/mmvec/hvx_ieee_fp.h
@@ -9,8 +9,11 @@
 
 #include "fpu/softfloat.h"
 
+#define FP32_DEF_NAN 0x7FFFFFFF
+
 #define f16_to_f32(A) float16_to_float32((A), true, &env->hvx_fp_status)
 #define f32_to_f16(A) float32_to_float16((A), true, &env->hvx_fp_status)
+#define bf_to_sf(A) bfloat16_to_float32(A, &env->hvx_fp_status)
 
 float32 fp_mult_sf_hf(float16 a1, float16 a2, float_status *fp_status);
 float32 fp_vdmpy(float16 a1, float16 a2, float16 a3, float16 a4,
@@ -28,5 +31,39 @@ int16_t conv_h_hf(float16 a, float_status *fp_status);
 /* IEEE - FP compare instructions */
 uint32_t cmpgt_sf(uint32_t a1, uint32_t a2, float_status *fp_status);
 uint16_t cmpgt_hf(uint16_t a1, uint16_t a2, float_status *fp_status);
+
+/* IEEE BFloat instructions */
+
+#define fp_mult_sf_bf(A, B) \
+    float32_mul(bf_to_sf(A), bf_to_sf(B), &env->hvx_fp_status)
+
+#define fp_add_sf_bf(A, B) \
+    float32_add(bf_to_sf(A), bf_to_sf(B), &env->hvx_fp_status)
+
+#define fp_sub_sf_bf(A, B) \
+    float32_sub(bf_to_sf(A), bf_to_sf(B), &env->hvx_fp_status)
+
+#define fp_mult_sf_bf_acc(f1, f2, f3) \
+    float32_muladd(bf_to_sf(f1), bf_to_sf(f2), f3, 0, &env->hvx_fp_status)
+
+static inline uint16_t sf_to_bf(int32_t A, float_status *fp_status)
+{
+    uint32_t rslt = A;
+    if ((rslt & 0x1FFFF) == 0x08000) {
+        /* do not round up if exactly .5 and even already */
+    } else if ((rslt & 0x8000) == 0x8000) {
+        rslt += 0x8000; /* rounding to nearest number */
+    }
+    rslt = float32_is_any_nan(A) ? FP32_DEF_NAN : rslt;
+    return float32_to_bfloat16(rslt, fp_status);
+}
+
+#define fp_min_bf(A, B) \
+    sf_to_bf(float32_min(bf_to_sf(A), bf_to_sf(B), &env->hvx_fp_status), \
+             &env->hvx_fp_status);
+
+#define fp_max_bf(A, B) \
+    sf_to_bf(float32_max(bf_to_sf(A), bf_to_sf(B), &env->hvx_fp_status), \
+             &env->hvx_fp_status);
 
 #endif
