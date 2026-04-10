bc9c9e4eaf8b1a86602921d35a906ba70c7db4dd
Marc-André Lureau <marcandre.lureau@redhat.com>
2026-04-10 23:18:25 +0400

    ui/vnc-jobs: vnc_has_job_locked() argument cannot be NULL
    The only caller, vnc_jobs_join() cannot take vs == NULL argument, or it
    would later crash.
    
    Reviewed-by: Daniel P. Berrangé <berrange@redhat.com>
    Signed-off-by: Marc-André Lureau <marcandre.lureau@redhat.com>
    Message-Id: <20260410-qemu-vnc-v2-3-231416f76dc3@redhat.com>

--- ui/vnc-jobs.c
+++ ui/vnc-jobs.c
@@ -138,7 +138,7 @@ static bool vnc_has_job_locked(VncState *vs)
     VncJob *job;
 
     QTAILQ_FOREACH(job, &queue->jobs, next) {
-        if (job->vs == vs || !vs) {
+        if (job->vs == vs) {
             return true;
         }
     }
