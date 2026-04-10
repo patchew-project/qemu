fe5704cae2dd5f710d44f04219e8a346847d1e20
Matheus Tavares Bernardino <matheus.bernardino@oss.qualcomm.com>
2026-04-10 10:55:55 -0700

    target/hexagon: add v68 HVX IEEE float min/max insns
    Add HVX IEEE floating-point min/max instructions:
    - vfmin_hf, vfmin_sf: IEEE floating-point minimum
    - vfmax_hf, vfmax_sf: IEEE floating-point maximum
    - vmax_hf, vmax_sf: qfloat IEEE maximum
    - vmin_hf, vmin_sf: qfloat IEEE minimum
    
    The Hexagon qfloat variants are similar to the IEEE-754 ones, but they
    handle NaN slightly differently. See comment on hvx_ieee_fp.h
    
    Reviewed-by: Taylor Simpson <ltaylorsimpson@gmail.com>
    Signed-off-by: Matheus Tavares Bernardino <matheus.bernardino@oss.qualcomm.com>
    Message-Id: <a89b231b2cb4ccbb40a5c1650426139a8c8c43fe.1775843299.git.matheus.bernardino@oss.qualcomm.com>

--- target/hexagon/hex_common.py
+++ target/hexagon/hex_common.py
@@ -216,6 +216,7 @@ def need_env(tag):
             "A_CVI_GATHER" in attribdict[tag] or
             "A_CVI_SCATTER" in attribdict[tag] or
             "A_HVX_IEEE_FP" in attribdict[tag] or
+            "A_HVX_FLT" in attribdict[tag] or
             "A_IMPLICIT_WRITES_USR" in attribdict[tag])
 
 
