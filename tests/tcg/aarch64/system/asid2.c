05d3709a957c11b5302405688616931e8bee4103
Alex Bennée <alex.bennee@linaro.org>
2026-04-17 17:43:23 +0100

    tests/tcg: convert aarch64 asid2 test to use sysreg helpers
    While we are at it we can reduce the number of temporary variables
    needed and fix a format string error.
    
    Signed-off-by: Alex Bennée <alex.bennee@linaro.org>
    Reviewed-by: Richard Henderson <richard.henderson@linaro.org>
    Message-Id: <20260417164328.1009132-4-alex.bennee@linaro.org>

--- tests/tcg/aarch64/system/asid2.c
+++ tests/tcg/aarch64/system/asid2.c
@@ -9,10 +9,11 @@
 
 #include <stdint.h>
 #include <minilib.h>
+#include "sysregs.h"
 
-#define ID_AA64MMFR3_EL1 "S3_0_C0_C7_3"
-#define ID_AA64MMFR4_EL1 "S3_0_C0_C7_4"
-#define TCR2_EL1 "S3_0_C2_C0_3"
+#define ID_AA64MMFR3_EL1 S3_0_C0_C7_3
+#define ID_AA64MMFR4_EL1 S3_0_C0_C7_4
+#define TCR2_EL1 S3_0_C2_C0_3
 
 int main()
 {
@@ -23,7 +24,7 @@ int main()
      * not enabled and read as the written value if A2 is enabled.
      */
 
-    uint64_t out;
+    uint64_t read_tcr2;
     uint64_t idreg3;
     uint64_t idreg4;
     int tcr2_present;
@@ -31,11 +32,8 @@ int main()
 
     /* Mask is FNG1, FNG0, and A2 */
     const uint64_t feature_mask = (1ULL << 18 | 1ULL << 17 | 1ULL << 16);
-    const uint64_t in = feature_mask;
 
-    asm("mrs %[idreg3], " ID_AA64MMFR3_EL1 "\n\t"
-        : [idreg3] "=r" (idreg3));
-
+    idreg3 = read_sysreg(ID_AA64MMFR3_EL1);
     tcr2_present = ((idreg3 & 0xF) != 0);
 
     if (!tcr2_present) {
@@ -43,33 +41,30 @@ int main()
         return 0;
     }
 
-    asm("mrs %[idreg4], " ID_AA64MMFR4_EL1 "\n\t"
-        : [idreg4] "=r" (idreg4));
-
+    idreg4 = read_sysreg(ID_AA64MMFR4_EL1);
     asid2_present = ((idreg4 & 0xF00) != 0);
 
-    asm("msr " TCR2_EL1 ", %[x0]\n\t"
-        "mrs %[x1], " TCR2_EL1 "\n\t"
-        : [x1] "=r" (out)
-        : [x0] "r" (in));
+    /* write the feature mask and read back */
+    write_sysreg(feature_mask, TCR2_EL1);
+    read_tcr2 = read_sysreg(TCR2_EL1);
 
     if (asid2_present) {
-        if ((out & feature_mask) == in) {
+        if ((read_tcr2 & feature_mask) == feature_mask) {
             ml_printf("OK\n");
             return 0;
         } else {
             ml_printf("FAIL: ASID2 present, but read value %lx != "
                       "written value %lx\n",
-                      out & feature_mask, in);
+                      read_tcr2 & feature_mask, feature_mask);
             return 1;
         }
     } else {
-        if (out == 0) {
+        if (read_tcr2 == 0) {
             ml_printf("TCR2_EL1 reads as RES0 as expected\n");
             return 0;
         } else {
-            ml_printf("FAIL: ASID2, missing but read value %lx != 0\n",
-                      out & feature_mask, in);
+            ml_printf("FAIL: ASID2, missing but read value %lx != %lx\n",
+                      read_tcr2 & feature_mask, feature_mask);
             return 1;
         }
     }
