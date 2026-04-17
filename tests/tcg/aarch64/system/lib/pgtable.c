82015d58bb8379fd29acbed735a51a4e674925e2
Alex Bennée <alex.bennee@linaro.org>
2026-04-17 17:43:25 +0100

    tests/tcg: add HW page for aarch64 tests
    If the test cases need to program items like the GIC we need to define
    the region as Device-nGnRE memory. As this is for test cases just map
    the whole first GB block (which is all HW in the -M virt machine).
    
    Signed-off-by: Alex Bennée <alex.bennee@linaro.org>
    Message-Id: <20260417164328.1009132-6-alex.bennee@linaro.org>

--- tests/tcg/aarch64/system/lib/pgtable.c
+++ tests/tcg/aarch64/system/lib/pgtable.c
@@ -17,14 +17,18 @@ uint64_t ttb_l1[512] __attribute__((aligned(4096)));
 uint64_t ttb_l2[512] __attribute__((aligned(4096)));
 
 /*
- * Setup a flat address mapping page-tables. Stage one simply
- * maps RAM to the first Gb. The stage2 tables have two 2mb
- * translation block entries covering a series of adjacent
- * 4k pages.
+ * Setup a flat address mapping page-tables.
+ *
+ * ttb (Level 1):
+ *   - Entry 0 [0 - 1GB]: 1GB Device block (for GIC and other H/W)
+ *   - Entry 1 [1GB - 2GB]: Table entry pointing to ttb_l2 (for RAM)
  */
 void setup_pgtables(void)
 {
-    /* L1 entry points to L2 table */
+    /* L1 entry 0: 1GB Device block mapping at 0x0 */
+    pgt_map_l1_block(ttb_l1, 0, 0, DESC_AF | DESC_ATTRINDX(2));
+
+    /* L1 entry 1: points to L2 table for finer permissions */
     pgt_map_l1_table(ttb_l1, (uintptr_t)_text_start, ttb_l2);
 
     /* L2 entries: 2MB blocks */
@@ -40,8 +44,14 @@ void setup_pgtables(void)
     /* Set TTBR0_EL1 */
     write_sysreg(ttb_l1, ttbr0_el1);
 
-    /* Set MAIR_EL1 */
-    write_sysreg(0xeeULL, mair_el1);
+    /*
+     * Set MAIR_EL1
+     *
+     * Attr0 (0xee): Normal memory, Outer/Inner WB/WA/Read-Alloc
+     * Attr1 (0x00): MTE page
+     * Attr2 (0x04): Device-nGnRE memory (for the first 1GB)
+     */
+    write_sysreg(0x0400eeULL, mair_el1);
 
     /* Set TCR_EL1 */
     uint64_t tcr = TCR_EL1_IPS_40BIT | TCR_EL1_TG0_4KB | TCR_EL1_ORGN0_WBWA | TCR_EL1_IRGN0_WBWA | TCR_EL1_T0SZ(25);
