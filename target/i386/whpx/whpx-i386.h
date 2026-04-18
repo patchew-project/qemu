af1cc1efbfe6247eb2c2331b864bc6e9144486aa
Mohamed Mediouni <mohamed@unpredictable.fr>
2026-04-19 00:33:10 +0200

    whpx: i386: CPU features support for Windows 10
    Signed-off-by: Mohamed Mediouni <mohamed@unpredictable.fr>
    Message-Id: <20260418223320.51330-29-mohamed@unpredictable.fr>

--- target/i386/whpx/whpx-i386.h
+++ target/i386/whpx/whpx-i386.h
@@ -2,3 +2,10 @@
 
 uint32_t whpx_get_supported_cpuid(uint32_t func, uint32_t idx, int reg);
 bool whpx_is_legacy_os(void);
+
+uint32_t whpx_get_supported_cpuid_legacy(uint32_t func, uint32_t idx,
+                                 int reg);
+bool whpx_has_xsave(void);
+bool whpx_has_xsaves(void);
+bool whpx_has_rdtscp(void);
+bool whpx_has_invpcid(void);
