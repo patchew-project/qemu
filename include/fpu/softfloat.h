b05d9e57522ac6e6886b02c3956de27bd89aa094
Richard Henderson <richard.henderson@linaro.org>
2026-04-21 15:13:28 +1000

    fpu: Add scalbn argument to fp8 conversion routines
    All Arm fp8 conversions have a power-of-2 scaling factor.
    
    Signed-off-by: Richard Henderson <richard.henderson@linaro.org>
    Message-Id: <20260421051346.41106-20-richard.henderson@linaro.org>

--- include/fpu/softfloat.h
+++ include/fpu/softfloat.h
@@ -195,13 +195,17 @@ float128 uint128_to_float128(Int128, float_status *status);
 
 float8_e4m3 float4_e2m1_to_float8_e4m3(float4_e2m1, float_status *status);
 
-bfloat16 float8_e4m3_to_bfloat16(float8_e4m3, float_status *status);
-float8_e4m3 bfloat16_to_float8_e4m3(bfloat16, bool sat, float_status *status);
-float8_e4m3 float32_to_float8_e4m3(float32, bool sat, float_status *status);
+bfloat16 float8_e4m3_to_bfloat16(float8_e4m3, int scale, float_status *status);
+float8_e4m3 bfloat16_to_float8_e4m3(bfloat16, int scale,
+                                    bool sat, float_status *status);
+float8_e4m3 float32_to_float8_e4m3(float32, int scale,
+                                   bool sat, float_status *status);
 
-bfloat16 float8_e5m2_to_bfloat16(float8_e5m2, float_status *status);
-float8_e5m2 bfloat16_to_float8_e5m2(bfloat16, bool sat, float_status *status);
-float8_e5m2 float32_to_float8_e5m2(float32, bool sat, float_status *status);
+bfloat16 float8_e5m2_to_bfloat16(float8_e5m2, int scale, float_status *status);
+float8_e5m2 bfloat16_to_float8_e5m2(bfloat16, int scale,
+                                    bool sat, float_status *status);
+float8_e5m2 float32_to_float8_e5m2(float32, int scale,
+                                    bool sat, float_status *status);
 
 /*----------------------------------------------------------------------------
 | Software half-precision conversion routines.
