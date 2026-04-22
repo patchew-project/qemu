f33b7a5de1383f5c1875a0e16d0c08ec436f333d
Stefan Berger <stefanb@linux.ibm.com>
2026-04-22 16:00:17 +0530

    tpm: Extend TPM TIS buffer size to 8192 bytes
    Extend the TIS buffer size to 8192 bytes and store the first 4096 bytes
    using VMSTATE_PARTIAL_BUFFER and the rest using VMSTATE_BUFFER_START_MIDDLE
    when necessary. It is necessary to store the buffer beyond original 4096
    bytes when:
    
     - the user has written more than 4096 bytes to the buffer
     - the TPM 2 response is larger than 4096 bytes
    
    Use the .needed function of the VMStateDescription interface to check
    whether the bytes in the buffer beyond 4096 bytes needed to be saved.
    The .pre_save function is called before the .needed function so that we
    can be sure to have received any response packet from the TPM.
    
    Signed-off-by: Stefan Berger <stefanb@linux.ibm.com>
    [ Arun Menon - Removed WIP tag and TODO for submission ]
    Signed-off-by: Arun Menon <armenon@redhat.com>
    Message-Id: <20260422103018.123608-10-armenon@redhat.com>

--- hw/tpm/tpm_tis_sysbus.c
+++ hw/tpm/tpm_tis_sysbus.c
@@ -48,12 +48,30 @@ static int tpm_tis_pre_save_sysbus(void *opaque)
     return tpm_tis_pre_save(&sbdev->state);
 }
 
+static bool tpm_tis_ext_buffer_migration_needed_sysbus(void *opaque)
+{
+    TPMStateSysBus *sbdev = opaque;
+
+    return tpm_tis_ext_buffer_migration_needed(&sbdev->state);
+}
+
+static const VMStateDescription vmstate_tpm_tis_ext_buffer_sysbus = {
+    .name = "tpm-tis/ext_buffer",
+    .version_id = 0,
+    .needed = tpm_tis_ext_buffer_migration_needed_sysbus,
+    .pre_save  = tpm_tis_pre_save_sysbus,
+    .fields = (const VMStateField[]) {
+        VMSTATE_BUFFER_START_MIDDLE(state.buffer, TPMStateSysBus, 4096),
+        VMSTATE_END_OF_LIST()
+    }
+};
+
 static const VMStateDescription vmstate_tpm_tis_sysbus = {
     .name = "tpm-tis",
     .version_id = 0,
     .pre_save  = tpm_tis_pre_save_sysbus,
     .fields = (const VMStateField[]) {
-        VMSTATE_BUFFER(state.buffer, TPMStateSysBus),
+        VMSTATE_PARTIAL_BUFFER(state.buffer, TPMStateSysBus, 4096),
         VMSTATE_UINT16(state.rw_offset, TPMStateSysBus),
         VMSTATE_UINT8(state.active_locty, TPMStateSysBus),
         VMSTATE_UINT8(state.aborting_locty, TPMStateSysBus),
@@ -63,6 +81,10 @@ static const VMStateDescription vmstate_tpm_tis_sysbus = {
                              0, vmstate_locty, TPMLocality),
 
         VMSTATE_END_OF_LIST()
+    },
+    .subsections = (const VMStateDescription *const[]) {
+        &vmstate_tpm_tis_ext_buffer_sysbus,
+        NULL,
     }
 };
 
