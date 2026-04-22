72d1e438cb531e11ca2a07993ebb3d42f0301326
Mohamed Mediouni <mohamed@unpredictable.fr>
2026-04-22 23:41:55 +0200

    whpx: i386: introduce proper cpuid support
    Unlike the implementation in QEMU 10.2, this one works.
    
    It's not optimal though as it doesn't use the Hyper-V support for this.
    
    Signed-off-by: Mohamed Mediouni <mohamed@unpredictable.fr>
    Message-Id: <20260422214225.2242-8-mohamed@unpredictable.fr>

--- target/i386/whpx/whpx-cpu-legacy.c
+++ target/i386/whpx/whpx-cpu-legacy.c
@@ -4,20 +4,7 @@
  *  Copyright (c) 2003 Fabrice Bellard
  *  Copyright (c) 2017 Google Inc.
  *
- * This program is free software; you can redistribute it and/or
- * modify it under the terms of the GNU Lesser General Public
- * License as published by the Free Software Foundation; either
- * version 2.1 of the License, or (at your option) any later version.
- *
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
- * Lesser General Public License for more details.
- *
- * You should have received a copy of the GNU Lesser General Public
- * License along with this program; if not, see <http://www.gnu.org/licenses/>.
- *
- * cpuid
+ * SPDX-License-Identifier: LGPL-2.1-or-later
  */
 
 #include "qemu/osdep.h"
