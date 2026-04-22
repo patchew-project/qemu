62da75a6134886be6f26946fd83585f1e54a0b18
Mohamed Mediouni <mohamed@unpredictable.fr>
2026-04-22 23:42:09 +0200

    target: i386: HLT type that ignores EFLAGS.IF
    The TLFS says:
    
    > A partition which possesses the AccessGuestIdleMsr privilege may trigger
    > entry into the virtual processor idle sleep state through a read to the
    > hypervisor-defined MSR HV_X64_MSR_GUEST_IDLE. The virtual processor will
    > be woken when an interrupt arrives, regardless of whether the interrupt
    > is enabled on the virtual processor or not.
    
    Meanwhile, Windows 24H2+ calls this MSR anyway without the privilege being set.
    
    Add the infrastructure to support it on the generic QEMU side.
    
    Signed-off-by: Mohamed Mediouni <mohamed@unpredictable.fr>
    Message-Id: <20260422214225.2242-22-mohamed@unpredictable.fr>

--- target/i386/cpu.c
+++ target/i386/cpu.c
@@ -10474,13 +10474,15 @@ int x86_cpu_pending_interrupt(CPUState *cs, int interrupt_request)
                    (((env->hflags2 & HF2_VINTR_MASK) &&
                      (env->hflags2 & HF2_HIF_MASK)) ||
                     (!(env->hflags2 & HF2_VINTR_MASK) &&
-                     (env->eflags & IF_MASK &&
-                      !(env->hflags & HF_INHIBIT_IRQ_MASK))))) {
+                     ((env->eflags & IF_MASK &&
+                      !(env->hflags & HF_INHIBIT_IRQ_MASK))
+                        || env->hflags2 & HF2_HYPERV_HLT_MASK)))) {
             return CPU_INTERRUPT_HARD;
         } else if (env->hflags2 & HF2_VGIF_MASK) {
             if((interrupt_request & CPU_INTERRUPT_VIRQ) &&
-                   (env->eflags & IF_MASK) &&
-                   !(env->hflags & HF_INHIBIT_IRQ_MASK)) {
+                   ((env->eflags & IF_MASK &&
+                      !(env->hflags & HF_INHIBIT_IRQ_MASK))
+                        || env->hflags2 & HF2_HYPERV_HLT_MASK)) {
                         return CPU_INTERRUPT_VIRQ;
             }
         }
