ad602813e49b8069083c1e7469ffc6dcc97a6bfa
Nathan Chen <nathanc@nvidia.com>
2026-04-22 13:43:33 -0700

    hw/arm/smmuv3-accel: Implement "auto" value for "oas"
    Allow accelerated SMMUv3 OAS property to be derived from host IOMMU
    capabilities. Derive host values using IOMMU_GET_HW_INFO, retrieving
    OAS from IDR5.
    
    This keeps the OAS value advertised by the virtual SMMU compatible with
    the capabilities of the host SMMUv3, so that the intermediate physical
    addresses (IPA) consumed by host SMMU for stage-2 translation do not
    exceed the host's max supported IPA size.
    
    Signed-off-by: Nathan Chen <nathanc@nvidia.com>
    Message-Id: <20260422204335.23116-6-nathanc@nvidia.com>

--- hw/arm/smmuv3.c
+++ hw/arm/smmuv3.c
@@ -1975,9 +1975,11 @@ static bool smmu_validate_property(SMMUv3State *s, Error **errp)
     }
 #endif
 
-    if (s->oas != OAS_MODE_44 && s->oas != OAS_MODE_48) {
-        error_setg(errp, "QEMU SMMUv3 model only implements 44 and 48 bit"
-                   "OAS; other OasMode values are not supported");
+    if (s->oas != OAS_MODE_44 && s->oas != OAS_MODE_48 &&
+        s->oas != OAS_MODE_AUTO) {
+        error_setg(errp, "QEMU SMMUv3 model only implements auto, "
+                   "44 bit, or 48 bit OAS. Other OasMode values are "
+                   "not supported.");
         return false;
     }
 
@@ -1991,7 +1993,7 @@ static bool smmu_validate_property(SMMUv3State *s, Error **errp)
             return false;
         }
         if (s->oas > OAS_MODE_44) {
-            error_setg(errp, "OAS must be 44 bits when accel=off");
+            error_setg(errp, "oas must be 44 bits when accel=off");
             return false;
         }
         if (s->ssidsize > SSID_SIZE_MODE_0) {
@@ -2173,9 +2175,8 @@ static void smmuv3_class_init(ObjectClass *klass, const void *data)
         "Valid values are on, off, and auto. Defaults to off. "
         "Please ensure host platform supports ATS before enabling.");
     object_class_property_set_description(klass, "oas",
-        "Specify Output Address Size (for accel=on). Supported values "
-        "are 44 or 48 bits. Defaults to 44 bits. oas=auto is not "
-        "supported.");
+        "Set Output Address Size in bits (for accel=on). "
+        "Valid values are 44, 48, and auto. Defaults to 44 bits.");
     object_class_property_set_description(klass, "ssidsize",
         "Set number of bits used to represent SubstreamIDs (SSIDs). "
         "Valid values are 0-20 and auto. Defaults to 0. "
