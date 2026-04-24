f600c3f4364bbc4a6b744c218bcce3a6fd58508a
Richard Henderson <richard.henderson@linaro.org>
2026-04-24 14:30:13 +1000

    target/arm: Enable FEAT_LUT for -cpu max
    Signed-off-by: Richard Henderson <richard.henderson@linaro.org>
    Message-Id: <20260424043014.46305-40-richard.henderson@linaro.org>

--- target/arm/tcg/cpu64.c
+++ target/arm/tcg/cpu64.c
@@ -1253,6 +1253,7 @@ void aarch64_max_tcg_initfn(Object *obj)
     t = FIELD_DP64(t, ID_AA64ISAR2, BC, 1);       /* FEAT_HBC */
     t = FIELD_DP64(t, ID_AA64ISAR2, WFXT, 2);     /* FEAT_WFxT */
     t = FIELD_DP64(t, ID_AA64ISAR2, CSSC, 1);     /* FEAT_CSSC */
+    t = FIELD_DP64(t, ID_AA64ISAR2, LUT, 1);      /* FEAT_LUT */
     t = FIELD_DP64(t, ID_AA64ISAR2, ATS1A, 1);    /* FEAT_ATS1A */
     SET_IDREG(isar, ID_AA64ISAR2, t);
 
