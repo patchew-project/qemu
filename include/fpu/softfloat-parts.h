38b2bf7fa51ab41420177682f27bb5b3a966ab61
Richard Henderson <richard.henderson@linaro.org>
2026-04-26 23:39:56 +1000

    fpu: Return struct from parts{64,128}_addsub
    At the same time, export.
    
    Signed-off-by: Richard Henderson <richard.henderson@linaro.org>
    Message-Id: <20260426134002.865628-80-richard.henderson@linaro.org>

--- include/fpu/softfloat-parts.h
+++ include/fpu/softfloat-parts.h
@@ -179,6 +179,11 @@ FloatParts128 parts128_pick_nan(const FloatParts128 *, const FloatParts128 *,
  * Operations
  */
 
+FloatParts64 parts64_addsub(const FloatParts64 *a, const FloatParts64 *b,
+                            float_status *s, bool subtract);
+FloatParts128 parts128_addsub(const FloatParts128 *a, const FloatParts128 *b,
+                              float_status *s, bool subtract);
+
 FloatParts64 parts64_div(const FloatParts64 *a, const FloatParts64 *b,
                          float_status *s);
 FloatParts128 parts128_div(const FloatParts128 *a, const FloatParts128 *b,
