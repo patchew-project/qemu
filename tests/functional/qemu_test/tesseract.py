8ca7346d12cce9e5cdd6e2ebaee64036cdc8a2a7
Thomas Huth <thuth@redhat.com>
2026-04-27 11:57:43 +0200

    tests/functional/qemu_test: Silence warnings from pylint in tesseract.py
    Pylint complains:
    
     tesseract.py:1:0: C0114: Missing module docstring (missing-module-docstring)
     tesseract.py:12:0: C0116: Missing function or method docstring (missing-function-docstring)
     tesseract.py:15:11: W1510: 'subprocess.run' used without explicitly defining the value for 'check'. (subprocess-run-check)
     tesseract.py:12:30: W0613: Unused argument 'tesseract_args' (unused-argument)
    
    Thus add the missing bits and remove the unused tesseract_args argument.
    While we're at it, also add a SPDX identifier instead of the weird three
    dots at the beginning of the file, and drop the license boilerplate text.
    
    Reviewed-by: Philippe Mathieu-Daudé <philmd@linaro.org>
    Signed-off-by: Thomas Huth <thuth@redhat.com>
    Message-ID: <20260422071145.244820-1-thuth@redhat.com>
    Message-Id: <20260427095747.1070244-7-thuth@redhat.com>

--- tests/functional/qemu_test/tesseract.py
+++ tests/functional/qemu_test/tesseract.py
@@ -1,19 +1,21 @@
-# ...
+# SPDX-License-Identifier: GPL-2.0-or-later
 #
 # Copyright (c) 2019 Philippe Mathieu-Daudé
-#
-# This work is licensed under the terms of the GNU GPL, version 2 or
-# later. See the COPYING file in the top-level directory.
+'''
+Tesseract is an program for doing Optical Character Recognition (OCR),
+which can be used in the tests to extract text from screenshots.
+'''
 
 import logging
 from subprocess import run
 
 
-def tesseract_ocr(image_path, tesseract_args=''):
+def tesseract_ocr(image_path):
+    ''' Run the tesseract OCR to extract text from a screenshot '''
     console_logger = logging.getLogger('console')
     console_logger.debug(image_path)
     proc = run(['tesseract', image_path, 'stdout'],
-               capture_output=True, encoding='utf8')
+               capture_output=True, encoding='utf8', check=False)
     if proc.returncode:
         return None
     lines = []
