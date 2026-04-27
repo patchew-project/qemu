7b745dd0a987d864c235c6f9864df52c2d71041b
Peter Maydell <peter.maydell@linaro.org>
2026-04-27 13:47:01 +0100

    hw: Move ARM_SYSCTL_GPIO definitions to arm sysctl specific header
    include/hw/arm/primecell.h used to be more expansive, but now the
    only thing it defines is the ARM_SYSCTL_GPIO_* constants for the GPIO
    lines for the arm-sysctl system-control device used on the Realview,
    Versatile and Versatile Express boards.
    
    Replace it with a header file specific to that device.
    
    virt.c and vmapple.c included primecell.h despite not using the
    constants it defined; there we can simply drop the include entirely.
    
    Signed-off-by: Peter Maydell <peter.maydell@linaro.org>
    Reviewed-by: Philippe Mathieu-Daudé <philmd@linaro.org>
    Message-id: 20260416172627.690396-1-peter.maydell@linaro.org
    Message-Id: <20260427124738.966578-28-peter.maydell@linaro.org>

---
+++ include/hw/misc/arm_sysctl.h
@@ -0,0 +1,16 @@
+/*
+ * Status and system control registers for ARM RealView/Versatile boards.
+ *
+ * Copyright (c) Linaro Ltd
+ *
+ * SPDX-License-Identifier: GPL-2.0-or-later
+ */
+
+#ifndef HW_MISC_ARM_SYSCTL_H
+#define HW_MISC_ARM_SYSCTL_H
+
+/* arm_sysctl inbound GPIO lines */
+#define ARM_SYSCTL_GPIO_MMC_WPROT 0
+#define ARM_SYSCTL_GPIO_MMC_CARDIN 1
+
+#endif
