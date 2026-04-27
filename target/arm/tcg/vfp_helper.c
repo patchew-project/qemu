ba54e63d48569243a4b82cd67b7aca3aa5259c6a
Alex Bennée <alex.bennee@linaro.org>
2026-04-27 13:47:29 +0100

    target/arm: migrate check_hcr_el2_trap to use syndrome helper
    It shares the same COPROC_ISS encoding as the other CP traps although
    not all the fields are used.
    
    Reviewed-by: Richard Henderson <richard.henderson@linaro.org>
    Signed-off-by: Alex Bennée <alex.bennee@linaro.org>
    Message-id: 20260422125250.1303100-16-alex.bennee@linaro.org
    Signed-off-by: Peter Maydell <peter.maydell@linaro.org>
    Message-Id: <20260427124738.966578-56-peter.maydell@linaro.org>

--- target/arm/tcg/vfp_helper.c
+++ target/arm/tcg/vfp_helper.c
@@ -1359,10 +1359,7 @@ void HELPER(check_hcr_el2_trap)(CPUARMState *env, uint32_t rt, uint32_t reg)
         g_assert_not_reached();
     }
 
-    syndrome = ((EC_FPIDTRAP << ARM_EL_EC_SHIFT)
-                | ARM_EL_IL
-                | (1 << 24) | (0xe << 20) | (7 << 14)
-                | (reg << 10) | (rt << 5) | 1);
+    syndrome = syn_cp10_rt_trap(1, 0xe, 7, reg, rt, 1);
 
     raise_exception(env, EXCP_HYP_TRAP, syndrome, 2);
 }
