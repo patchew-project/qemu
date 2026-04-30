b5640b0817126a4bd1d78b7809d2cb1e0073260b
Pierrick Bouvier <pierrick.bouvier@oss.qualcomm.com>
2026-04-29 20:56:21 -0700

    target-info: introduce TargetInfo in QOM
    For the single-binary, we want to be able to retrieve at runtime the
    current target among the different ones available.
    A consequence is that we can't rely on existing target_info() definition
    since it will create a conflict once more than one target is available.
    
    To solve this, we add TargetInfo in QOM, with this hierarchy.
    We define one class "target-info-X" per target, that inherits from
    abstract class "target-info". Using concrete vs abstract class ensure we
    can easily filter "target-info-X" from all QOM types.
    Associated TargetInfo is directly set through class initialization,
    without relying on any instance.
    
    For user mode, we simply define target_info() like it was done
    previously. In this patch, we keep the same definition for system-mode
    also, and it will be replaced in next commits.
    
    Signed-off-by: Pierrick Bouvier <pierrick.bouvier@oss.qualcomm.com>
    Message-Id: <20260430035626.3511676-3-pierrick.bouvier@oss.qualcomm.com>

--- include/qemu/target-info-init.h
+++ include/qemu/target-info-init.h
@@ -10,11 +10,60 @@
  */
 
 #ifndef TARGET_INFO_DEF_H
+#define TARGET_INFO_DEF_H
 
+#ifdef CONFIG_USER_ONLY
+
+/*
+ * User mode does not support multiple targets in the same binary, so just
+ * define target_info().
+ */
 #define target_info_init(ti_var)        \
 const TargetInfo *target_info(void)     \
 {                                       \
     return &ti_var;                     \
 }
+
+#else /* CONFIG_USER_ONLY */
+
+#include "qemu/target-info-qom.h"
+#include "qom/object.h"
+
+#define TYPE_TARGET_INFO_TARGET TYPE_TARGET_INFO"-"TARGET_NAME
+
+typedef struct TargetInfoQomTarget {
+    TargetInfoQom parent;
+} TargetInfoQomTarget;
+
+typedef struct TargetInfoQomTargetClass {
+    TargetInfoQomClass parent_class;
+} TargetInfoQomTargetClass;
+
+OBJECT_DECLARE_TYPE(TargetInfoQomTarget, TargetInfoQomTargetClass, TARGET_INFO_TARGET)
+
+#define target_info_init(ti_var)                                            \
+const TargetInfo *target_info(void)                                         \
+{                                                                           \
+    return &ti_var;                                                         \
+}                                                                           \
+                                                                            \
+static void target_info_qom_class_init(ObjectClass *oc, const void * data)  \
+{                                                                           \
+    TargetInfoQomTargetClass *klass = TARGET_INFO_TARGET_CLASS(oc);         \
+    klass->parent_class.target_info = &ti_var;                              \
+}                                                                           \
+                                                                            \
+static const TypeInfo target_info_qom_target_type_info[] = {                \
+{                                                                           \
+    .name = TYPE_TARGET_INFO_TARGET,                                        \
+    .parent = TYPE_TARGET_INFO,                                             \
+    .instance_size = sizeof(TargetInfoQomTarget),                           \
+    .class_size = sizeof(TargetInfoQomTargetClass),                         \
+    .class_init = target_info_qom_class_init,                               \
+    .abstract = false,                                                      \
+} };                                                                        \
+DEFINE_TYPES(target_info_qom_target_type_info)
+
+#endif /* CONFIG_USER_ONLY */
 
 #endif /* TARGET_INFO_DEF_H */
