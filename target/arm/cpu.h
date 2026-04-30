fac3d21ae55351bab8705e59c5c534d48e93e8ef
Alex Bennée <alex.bennee@linaro.org>
2026-04-30 11:44:28 +0100

    target/arm: teach arm_cpu_has_work about halting reasons
    With the advent of WFE and WFI we need to pay closer attention to the
    reason why the vCPU may be sleeping to figure out if we should wake
    it up.
    
    Create env->halt_reason to track this and then re-order the tests so
    we:
    
      - ignore everything is the vCPU is powered off
      - wake up if the event_register is set and we were in a WFE
      - otherwise any IRQ event does wake the vCPU up.
    
    Signed-off-by: Alex Bennée <alex.bennee@linaro.org>
    
    Reviewed-by: Richard Henderson <richard.henderson@linaro.org>
    Message-Id: <20260430104434.1482407-2-alex.bennee@linaro.org>

--- target/arm/cpu.h
+++ target/arm/cpu.h
@@ -257,6 +257,19 @@ typedef enum ARMFPStatusFlavour {
 } ARMFPStatusFlavour;
 #define FPST_COUNT  10
 
+/**
+ * ARMHaltReason - the reason we have entered halt state
+ *
+ * To be able to correctly wake up via arm_cpu_has_work() we need to
+ * track the reason we went to sleep.
+ */
+typedef enum {
+    NOT_HALTED = 0,
+    HALT_PSCI,
+    HALT_WFI,
+    HALT_WFE
+} ARMHaltReason;
+
 typedef struct CPUArchState {
     /* Regs for current mode.  */
     uint32_t regs[16];
@@ -759,6 +772,9 @@ typedef struct CPUArchState {
 
     /* Optional fault info across tlb lookup. */
     ARMMMUFaultInfo *tlb_fi;
+
+    /* Reason the CPU is halted */
+    ARMHaltReason halt_reason;
 
     /*
      * The event register is shared by all ARM profiles (A/R/M),
