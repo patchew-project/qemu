b8f4695c43d1047d32ced23e14b61b292b8a39df
Pierrick Bouvier <pierrick.bouvier@oss.qualcomm.com>
2026-04-30 13:38:41 -0700

    target-info-qom: detect target from QOM
    For now, we expect only one target to be available at runtime. This will
    change with the single-binary and we'll detect which one to use
    dynamically.
    
    Reviewed-by: Marc-André Lureau <marcandre.lureau@redhat.com>
    Signed-off-by: Pierrick Bouvier <pierrick.bouvier@oss.qualcomm.com>
    Message-Id: <20260430203842.29156-8-pierrick.bouvier@oss.qualcomm.com>

--- include/qemu/target-info-qom.h
+++ include/qemu/target-info-qom.h
@@ -25,4 +25,6 @@ typedef struct TargetInfoQomClass {
 
 OBJECT_DECLARE_TYPE(TargetInfoQom, TargetInfoQomClass, TARGET_INFO)
 
+void target_info_qom_set_target(void);
+
 #endif /* QEMU_TARGET_INFO_QOM_H */
