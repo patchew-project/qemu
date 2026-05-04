60f6aed73ce2a3cbf2854282a45fdd810c9ef706
Marc-André Lureau <marcandre.lureau@redhat.com>
2026-05-04 15:35:18 +0400

    system/qtest: add missing qtest_finalize()
    Free owned resources on object finalization.
    
    Fixes: 6ba7ada3559e ("qtest: add a QOM object for qtest")
    Signed-off-by: Marc-André Lureau <marcandre.lureau@redhat.com>
    Reviewed-by: Peter Maydell <peter.maydell@linaro.org>
    Message-Id: <20260504-qom-tests-v2-30-ef7e3dc94f7a@redhat.com>

--- system/qtest.c
+++ system/qtest.c
@@ -1020,10 +1020,20 @@ static void qtest_class_init(ObjectClass *oc, const void *data)
                                   qtest_get_log, qtest_set_log);
 }
 
+static void qtest_finalize(Object *obj)
+{
+    QTest *q = QTEST(obj);
+
+    g_free(q->chr_name);
+    g_free(q->log);
+    object_unref(q->chr);
+}
+
 static const TypeInfo qtest_info = {
     .name = TYPE_QTEST,
     .parent = TYPE_OBJECT,
     .class_init = qtest_class_init,
+    .instance_finalize = qtest_finalize,
     .instance_size = sizeof(QTest),
     .interfaces = (const InterfaceInfo[]) {
         { TYPE_USER_CREATABLE },
