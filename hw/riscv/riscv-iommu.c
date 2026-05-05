d34d14bcfbea12487c69568ec37094d383f1f9f8
CJ Chen <cjchen@igel.co.jp>
2026-05-05 16:26:36 -0400

    hw/riscv: iommu-trap: remove .impl.unaligned = true
    The riscv_iommu_trap_ops MemoryRegionOps specifies that
    unaligned accesses are not valid for this device but that
    it does implement them. This doesn't make much sense, and
    we want to add an assertion that registered MRs don't specify
    this invalid combination of settings.
    
    Drop .impl.unaligned = true, with no behaviour change.
    
    Signed-off-by: CJ Chen <cjchen@igel.co.jp>
    Acked-by: Tomoyuki Hirose <hrstmyk811m@gmail.com>
    Reported-by: Tomoyuki Hirose <hrstmyk811m@gmail.com>
    Reviewed-by: Daniel Henrique Barboza <dbarboza@ventanamicro.com>
    [PMM: reworded commit message]
    Reviewed-by: Peter Maydell <peter.maydell@linaro.org>
    Signed-off-by: Peter Maydell <peter.maydell@linaro.org>
    Reviewed-by: Alistair Francis <alistair.francis@wdc.com>
    Reviewed-by: Philippe Mathieu-Daudé <philmd@linaro.org>
    Link: https://lore.kernel.org/r/20260428093339.2087081-2-peter.maydell@linaro.org
    Signed-off-by: Peter Xu <peterx@redhat.com>
    Message-Id: <20260505202640.1011006-20-peterx@redhat.com>

--- hw/riscv/riscv-iommu.c
+++ hw/riscv/riscv-iommu.c
@@ -2434,7 +2434,6 @@ static const MemoryRegionOps riscv_iommu_trap_ops = {
     .impl = {
         .min_access_size = 4,
         .max_access_size = 8,
-        .unaligned = true,
     },
     .valid = {
         .min_access_size = 4,
