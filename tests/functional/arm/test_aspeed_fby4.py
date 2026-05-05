a4f82638965ef841a1e8dac4ac370f2e97a11444
Alexander Hansen <alexander.hansen@9elements.com>
2026-05-05 17:42:58 +0200

    ast2600: yosemite4 initial support
    Initial patch based on [1] to support yosemite v4 bmc emulation.
    
    The goal of this machine support is to support OpenBMC development.
    
    Reference linux devicetree from openbmc linux is [2].
    
    Status:
    - Enclosure FRU:         showing up
    - Management Board FRU:  showing up
    - Blade Board FRU:       showing up
    - Blade Board sensors:   not implemented
    - Blade Chassis FRU:     showing up
    - Fan Board FRU:         both showing up
    - Fan Board sensors:     supported
    
    Overall the emulation is incomplete but already helpful in development
    and testing.
    
    The focus of this initial support is on the FRU eeproms and fanboard
    sensors.
    
    Tested: booted an OpenBMC image for yosemite4 target.
    
    Tested: functional test (see [5] for the image) passed.
    
    ```
    export QEMU_TEST_QEMU_BINARY=qemu-system-arm
    ./build/run tests/functional/arm/test_aspeed_fby4.py
    TAP version 13
    ok 1 test_aspeed_fby4.YosemiteV4Machine.test_arm_ast2600_yosemitev4_openbmc
    1..1
    ```
    
    References:
    [1] https://github.com/9elements/qemu/commit/32139f913c2bd0ebe4bd26c46765861f6f9f2d49
    [2] arch/arm/boot/dts/aspeed/aspeed-bmc-facebook-yosemite4.dts
    [3] https://github.com/legoater/qemu-aspeed-boot/pull/5
    
    Cc: "Cédric Le Goater" <clg@kaod.org> (maintainer:ASPEED BMCs)
    Cc: Peter Maydell <peter.maydell@linaro.org> (maintainer:ASPEED BMCs)
    Cc: Steven Lee <steven_lee@aspeedtech.com> (reviewer:ASPEED BMCs)
    Cc: Troy Lee <leetroy@gmail.com> (reviewer:ASPEED BMCs)
    Cc: Jamin Lin <jamin_lin@aspeedtech.com> (reviewer:ASPEED BMCs)
    Cc: Kane Chen <kane_chen@aspeedtech.com> (reviewer:ASPEED BMCs)
    Cc: Andrew Jeffery <andrew@codeconstruct.com.au> (reviewer:ASPEED BMCs)
    Cc: Joel Stanley <joel@jms.id.au> (reviewer:ASPEED BMCs)
    Cc: qemu-arm@nongnu.org (open list:ASPEED BMCs)
    Cc: qemu-devel@nongnu.org (open list:All patches CC here)
    Signed-off-by: Alexander Hansen <alexander.hansen@9elements.com>
    Message-Id: <20260505154410.230969-5-alexander.hansen@9elements.com>

---
+++ tests/functional/arm/test_aspeed_fby4.py
@@ -0,0 +1,44 @@
+#!/usr/bin/env python3
+#
+# Functional test that boots the ASPEED machines
+#
+# SPDX-License-Identifier: GPL-2.0-or-later
+
+from qemu_test import Asset
+from aspeed import AspeedTest
+
+
+class YosemiteV4Machine(AspeedTest):
+
+    ASSET_YOSEMITE_V4_FLASH = Asset(
+        'https://github.com/pointbazaar/qemu-aspeed-boot/raw/3ef9a0d8cd2902c2039924ad1f16c8f2345486f3/images/yosemite4-bmc/openbmc-20260505132843/obmc-phosphor-image-yosemite4-20260505132843.static.mtd.xz',
+        'dff6946363b41f952b15cfc3156482b89fcfc1b0ecfc3ec8b3ed496a5f001ef9')
+
+    def do_test_arm_aspeed_openbmc_no_network(self, machine, image, uboot,
+                                   cpu_id, soc):
+
+        self.set_machine(machine)
+        self.vm.set_console()
+        self.vm.add_args('-drive', f'file={image},if=mtd,format=raw',
+                         '-snapshot')
+        self.vm.launch()
+
+        self.wait_for_console_pattern(f'U-Boot {uboot}')
+        self.wait_for_console_pattern('## Loading kernel from FIT Image')
+        self.wait_for_console_pattern('Starting kernel ...')
+        self.wait_for_console_pattern(f'Booting Linux on physical CPU {cpu_id}')
+        self.wait_for_console_pattern(f'ASPEED {soc}')
+        self.wait_for_console_pattern('/init as init process')
+        # yosemite v4 does not emit the hostname log which is
+        # different from the other machines.
+        self.wait_for_console_pattern('yosemite4 login:')
+
+    def test_arm_ast2600_yosemitev4_openbmc(self):
+        image_path = self.uncompress(self.ASSET_YOSEMITE_V4_FLASH)
+
+        self.do_test_arm_aspeed_openbmc_no_network('fby4-bmc', image=image_path,
+                                        uboot='2019.04', cpu_id='0xf00',
+                                        soc='AST2600 rev A3')
+
+if __name__ == '__main__':
+    AspeedTest.main()
