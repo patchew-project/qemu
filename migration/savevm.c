11bf38736553d4630b3b95e703ec67258063982e
Peter Xu <peterx@redhat.com>
2026-05-05 16:26:26 -0400

    migration: Introduce stopcopy_bytes in save_query_pending()
    Allow modules to report data that can only be migrated after VM is stopped.
    
    When this concept is introduced, we will need to account stopcopy size to
    be part of pending_size as before.
    
    However, when there're data only can be migrated in stopcopy phase, it
    means the old "pending_size" may not always be able to reach low enough to
    kickoff an slow version of query sync.
    
    It used to be almost guaranteed to happen as all prior iterative modules
    doesn't have stopcopy only data.  VFIO may change that fact by having some
    data that must be copied during stop phase.
    
    So we need to make sure QEMU will kickoff a synchronized version of query
    pending when all precopy data is migrated.  This might be important to VFIO
    to keep making progress even if the downtime cannot yet be satisfied.
    
    So far, this patch should introduce no functional change, as no module yet
    report stopcopy size.
    
    This paves way for VFIO to properly report its pending data sizes, which
    will start to include stop-only data.
    
    Reviewed-by: Avihai Horon <avihaih@nvidia.com>
    Reviewed-by: Juraj Marcin <jmarcin@redhat.com>
    Link: https://lore.kernel.org/r/20260421202110.306051-8-peterx@redhat.com
    Signed-off-by: Peter Xu <peterx@redhat.com>
    Message-Id: <20260505202640.1011006-10-peterx@redhat.com>

--- migration/savevm.c
+++ migration/savevm.c
@@ -1800,8 +1800,7 @@ void qemu_savevm_query_pending(MigPendingData *pending, bool exact)
 {
     SaveStateEntry *se;
 
-    pending->precopy_bytes = 0;
-    pending->postcopy_bytes = 0;
+    memset(pending, 0, sizeof(*pending));
 
     QTAILQ_FOREACH(se, &savevm_state.handlers, entry) {
         if (!se->ops || !se->ops->save_query_pending) {
@@ -1813,8 +1812,13 @@ void qemu_savevm_query_pending(MigPendingData *pending, bool exact)
         se->ops->save_query_pending(se->opaque, pending, exact);
     }
 
+    pending->total_bytes = pending->precopy_bytes +
+        pending->stopcopy_bytes + pending->postcopy_bytes;
+
     trace_qemu_savevm_query_pending(exact, pending->precopy_bytes,
-                                    pending->postcopy_bytes);
+                                    pending->stopcopy_bytes,
+                                    pending->postcopy_bytes,
+                                    pending->total_bytes);
 }
 
 void qemu_savevm_state_cleanup(void)
