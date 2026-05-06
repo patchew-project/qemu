091d6b90bbb54a3a82a5fc9b2397d649bbb3dd49
Philippe Mathieu-Daudé <philmd@linaro.org>
2026-05-06 15:54:34 +0200

    cpus: Introduce SysemuCPUOps::monitor_defs hook
    Allow targets to register their legacy target_monitor_defs()
    in SysemuCPUOps; check it first in get_monitor_def() otherwise
    fall back to previous per-target helper.
    
    Signed-off-by: Philippe Mathieu-Daudé <philmd@linaro.org>
    Reviewed-by: Richard Henderson <richard.henderson@linaro.org>
    Message-Id: <20260427080738.77138-27-philmd@linaro.org>
    Message-Id: <20260506135524.20617-63-philmd@linaro.org>

--- include/hw/core/sysemu-cpu-ops.h
+++ include/hw/core/sysemu-cpu-ops.h
@@ -94,6 +94,12 @@ typedef struct SysemuCPUOps {
     int (*monitor_get_register)(CPUState *cs, const char *name, int64_t *pval);
 
     /**
+     * @monitor_defs: Array of MonitorDef entries. This field is legacy,
+     *                use @gdb_core_xml_file to dump registers instead.
+     */
+    const MonitorDef *monitor_defs;
+
+    /**
      * @legacy_vmsd: Legacy state for migration.
      *               Do not use in new targets, use #DeviceClass::vmsd instead.
      */
