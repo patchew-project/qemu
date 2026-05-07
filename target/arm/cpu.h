3552a9c38d189d570f0f656e4be90a94cd301af3
Peter Maydell <peter.maydell@linaro.org>
2026-05-07 16:51:23 +0100

    target/arm: GICv5 cpuif: Implement GICv5 PPI active set/clear registers
    In the GICv5 PPI state and control lives in the CPU interface; this
    is different from the GICv3 where this was all in the redistributor.
    
    Implement the access system registers for the PPI active state; this
    is a pair of registers, one of which has "write 1 to clear" behaviour
    and the other of which has "write 1 to set".  In both cases, reads
    return the current state.
    
    We start here by implementing the accessors for the underlying state;
    we don't yet attempt to do anything (e.g.  recalculating the highest
    priority pending PPI) when the state changes.  That will come in
    subsequent commits.
    
    Signed-off-by: Peter Maydell <peter.maydell@linaro.org>
    Reviewed-by: Jonathan Cameron <jonathan.cameron@huawei.com>
    Message-id: 20260327111700.795099-37-peter.maydell@linaro.org
    Message-Id: <20260507155154.1886557-37-peter.maydell@linaro.org>

--- target/arm/cpu.h
+++ target/arm/cpu.h
@@ -256,6 +256,9 @@ typedef enum ARMFPStatusFlavour {
 } ARMFPStatusFlavour;
 #define FPST_COUNT  10
 
+/* Architecturally there are 128 PPIs in a GICv5 */
+#define GICV5_NUM_PPIS 128
+
 typedef struct CPUArchState {
     /* Regs for current mode.  */
     uint32_t regs[16];
@@ -599,6 +602,8 @@ typedef struct CPUArchState {
     struct {
         /* GICv5 CPU interface data */
         uint64_t icc_icsr_el1;
+        /* Most PPI registers have 1 bit per PPI, so 64 PPIs to a register */
+        uint64_t ppi_active[GICV5_NUM_PPIS / 64];
     } gicv5_cpuif;
 
     struct {
