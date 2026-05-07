87a46fb8f19eb1287288ff67411066a1e555d2e8
Warner Losh <imp@bsdimp.com>
2026-05-06 20:21:56 -0600

    bsd-user: Add syscall header generator for FreeBSD
    Generate the syscall numbers from the installed header that has them.
    Ideally, we'd use FreeBSD's lua infra for this, but that requires that
    we have those files installed, and they aren't quite the same across
    supported versions yet, so use this simple, but effective hack. Add to
    meson build, but unused.
    
    Reviewed-by: Philippe Mathieu-Daudé <philmd@linaro.org>
    Reviewed-by: Pierrick Bouvier <pierrick.bouvier@oss.qualcomm.com>
    Signed-off-by: Warner Losh <imp@bsdimp.com>
    Message-Id: <20260507022219.44171-3-imp@bsdimp.com>

---
+++ bsd-user/freebsd/scripts/syscallhdr.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+# Copyright (c) 2026 Warner Losh <imp@bsdimp.com>
+# SPDX-License-Identifier: GPL-2.0-or-later
+
+in="$1"
+out="$2"
+bsd="$3"
+
+awk -v bsd="$3" '{sub("SYS_", "TARGET_" bsd "_NR_", $0); print;}' < $in > $out
