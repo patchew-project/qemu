eb0d4bc59485eccf091574ed304300f979c41545
Marc-André Lureau <marcandre.lureau@redhat.com>
2026-05-12 00:21:06 +0400

    hw/arm/virt: free flash devices and OEM strings on finalization
    Flash devices created in instance_init via qdev_new() hold an extra
    reference that is only dropped on sysbus_realize_and_unref(). When the
    machine is destroyed before realization, the flash objects leak. Also,
    the oem_id and oem_table_id strings from g_strndup() are never freed.
    
    Fixes: 602b458201ff ("acpi: Permit OEM ID and OEM table ID fields to be changed")
    Reviewed-by: Philippe Mathieu-Daudé <philmd@linaro.org>
    Signed-off-by: Marc-André Lureau <marcandre.lureau@redhat.com>
    Message-Id: <20260511202111.3091440-21-marcandre.lureau@redhat.com>

--- hw/arm/virt.c
+++ hw/arm/virt.c
@@ -4321,6 +4321,19 @@ static void virt_instance_init(Object *obj)
     cxl_machine_init(obj, &vms->cxl_devices_state);
 }
 
+static void virt_instance_finalize(Object *obj)
+{
+    VirtMachineState *vms = VIRT_MACHINE(obj);
+
+    for (int i = 0; i < ARRAY_SIZE(vms->flash); i++) {
+        if (vms->flash[i] && !qdev_is_realized(DEVICE(vms->flash[i]))) {
+            object_unref(OBJECT(vms->flash[i]));
+        }
+    }
+    g_free(vms->oem_id);
+    g_free(vms->oem_table_id);
+}
+
 static const TypeInfo virt_machine_info = {
     .name          = TYPE_VIRT_MACHINE,
     .parent        = TYPE_MACHINE,
@@ -4329,6 +4342,7 @@ static const TypeInfo virt_machine_info = {
     .class_size    = sizeof(VirtMachineClass),
     .class_init    = virt_machine_class_init,
     .instance_init = virt_instance_init,
+    .instance_finalize = virt_instance_finalize,
     .interfaces = (const InterfaceInfo[]) {
          { TYPE_HOTPLUG_HANDLER },
          { }
