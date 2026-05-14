09826aa8b6fe44a6f2a806a731549480e8c1a47a
James Hilliard <james.hilliard1@gmail.com>
2026-05-14 11:37:09 -0600

    target/mips: add Octeon MTM2 instruction
    MTM2 loads the high Octeon3 multiplier operand pair from rs/rt into
    MPL[2] and MPL[5].
    
    This supplies the final operand pair consumed by the V3MULU multiplier
    helper.
    
    Signed-off-by: James Hilliard <james.hilliard1@gmail.com>
    Reviewed-by: Richard Henderson <richard.henderson@linaro.org>
    Message-Id: <20260514-mips-octeon-missing-insns-v2-v7-20-226686be4ce1@gmail.com>

--- target/mips/tcg/octeon_translate.c
+++ target/mips/tcg/octeon_translate.c
@@ -296,6 +296,7 @@ TRANS(LWUX, trans_lx, MO_UL);
 TRANS(LDX,  trans_lx, MO_UQ);
 TRANS(MTM0, trans_mtm, 0);
 TRANS(MTM1, trans_mtm, 1);
+TRANS(MTM2, trans_mtm, 2);
 TRANS(MTP0, trans_mtp, 0);
 TRANS(MTP1, trans_mtp, 1);
 TRANS(MTP2, trans_mtp, 2);
