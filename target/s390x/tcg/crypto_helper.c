bd20aefbdee6a6f9e1faa2f3cf11c84a9a5940f0
Harald Freudenberger <freude@linux.ibm.com>
2026-05-26 14:15:39 +0200

    target/s390x: Minimal AES XTS support for cpacf pcc instruction
    Support CPACF pcc subfunctions PCC-Compute-XTS-Parameter-AES-128
    and PCC-Compute-XTS-Parameter-AES-128 but only for the special
    case block sequential number is 0. However, this covers the s390
    AES XTS implementation in the Linux kernel and Libica and thus
    also Opencryptoki clear key via Libica.
    
    Signed-off-by: Harald Freudenberger <freude@linux.ibm.com>
    Tested-by: Holger Dengler <dengler@linux.ibm.com>
    Message-Id: <20260526121550.5296-8-freude@linux.ibm.com>

--- target/s390x/tcg/crypto_helper.c
+++ target/s390x/tcg/crypto_helper.c
@@ -169,6 +169,23 @@ static int cpacf_ppno(CPUS390XState *env, const int mmu_idx, uintptr_t ra,
     return rc;
 }
 
+static int cpacf_pcc(CPUS390XState *env, const int mmu_idx, uintptr_t ra,
+                     uint8_t fc)
+{
+    int rc = 0;
+
+    switch (fc) {
+    case 0x32: /* CPACF_PCC compute XTS param AES-128 */
+    case 0x34: /* CPACF PCC compute XTS param AES-256 */
+            rc = cpacf_aes_pcc(env, mmu_idx, ra, env->regs[1], fc);
+            break;
+    default:
+        g_assert_not_reached();
+    }
+
+    return rc;
+}
+
 uint32_t HELPER(msa)(CPUS390XState *env, uint32_t r1, uint32_t r2, uint32_t r3,
                      uint32_t type)
 {
@@ -227,6 +244,9 @@ uint32_t HELPER(msa)(CPUS390XState *env, uint32_t r1, uint32_t r2, uint32_t r3,
         break;
     case S390_FEAT_TYPE_KMCTR:
         rc = cpacf_kmctr(env, mmu_idx, ra, r1, r2, r3, fc, mod);
+        break;
+    case S390_FEAT_TYPE_PCC:
+        rc = cpacf_pcc(env, mmu_idx, ra, fc);
         break;
     default:
         g_assert_not_reached();
