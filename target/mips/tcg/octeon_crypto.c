c2634bc2c6dc03f824d5e132a5f27d22a6b27bad
James Hilliard <james.hilliard1@gmail.com>
2026-06-08 12:59:27 -0600

    target/mips: add Octeon COP2 crypto helper plumbing
    Add the Octeon COP2 crypto helper source file and build it with the MIPS
    TCG target. This provides the common compilation unit for the COP2 engine
    helpers.
    
    The instruction dispatch itself remains fully decoded by decodetree, and
    operation selectors call per-operation helpers rather than a common
    selector-dispatch helper.
    
    Signed-off-by: James Hilliard <james.hilliard1@gmail.com>
    Reviewed-by: Richard Henderson <richard.henderson@linaro.org>
    Message-Id: <20260608-mips-octeon-missing-insns-v2-v16-2-daef7a0d8b04@gmail.com>

---
+++ target/mips/tcg/octeon_crypto.c
@@ -0,0 +1,16 @@
+/*
+ * SPDX-License-Identifier: GPL-2.0-or-later
+ *
+ * MIPS Octeon crypto emulation helpers.
+ *
+ * Copyright (c) 2026 James Hilliard
+ */
+
+#include "qemu/osdep.h"
+#include "cpu.h"
+#include "internal.h"
+#include "exec/helper-proto.h"
+#include "crypto/aes.h"
+#include "crypto/sm4.h"
+#include "qemu/bitops.h"
+#include "qemu/host-utils.h"
