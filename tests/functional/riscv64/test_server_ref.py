006eee5bedf3fd5477883287e9d17938576c0b37
Daniel Henrique Barboza <daniel.barboza@oss.qualcomm.com>
2026-06-10 18:41:32 -0300

    tests/functional/riscv64: add riscv-server-ref TPM selftest
    Add a TPM specific selftest for the riscv-server-ref board.  The test
    will be skipped if there's no 'swtpm' in the host.
    
    The test has been basically cloned from our Aspeed ast2600 friends.
    Shoutout to monsieur Cédric Le Goater for the code.
    
    Signed-off-by: Daniel Henrique Barboza <daniel.barboza@oss.qualcomm.com>
    Reviewed-by: Alistair Francis <alistair.francis@wdc.com>
    Reviewed-by: Joel Stanley <joel@jms.id.au>
    Message-Id: <20260610214133.1882563-7-daniel.barboza@oss.qualcomm.com>

--- tests/functional/riscv64/test_server_ref.py
+++ tests/functional/riscv64/test_server_ref.py
@@ -8,8 +8,13 @@
 riscv-server-ref board test
 """
 
+import os
+import tempfile
+import subprocess
+
 from qemu_test import QemuSystemTest, Asset
 from qemu_test import wait_for_console_pattern
+from qemu_test import skipIfMissingCommands
 
 class RiscvServerRefTest(QemuSystemTest):
     """
@@ -26,7 +31,7 @@ class RiscvServerRefTest(QemuSystemTest):
          'master/riscv/images/virt64/buildroot/rootfs.ext2'),
         'f00bb88749f945d80675540a1338bd1ccb226574685a5b6c65ab44027d0411a8')
 
-    def test_boot_linux_test(self):
+    def _test_boot_linux_test(self, tpmstate_dir=None):
         self.set_machine('riscv-server-ref')
         kernel_path = self.ASSET_KERNEL.fetch()
         rootfs_path = self.ASSET_ROOTFS.fetch()
@@ -37,6 +42,22 @@ class RiscvServerRefTest(QemuSystemTest):
             f'file={rootfs_path},format=raw,id=hd0,snapshot=on,if=none')
         self.vm.add_args('-device', 'ahci,id=ahci')
         self.vm.add_args('-device', 'ide-hd,drive=hd0,bus=ahci.0')
+
+        if tpmstate_dir is not None:
+            # Note: code taken verbatim from
+            # tests/functional/arm/test_aspeed_ast2600_buildroot_tpm.py
+
+            # We must put the TPM state dir in /tmp/, not the build dir,
+            # because some distros use AppArmor to lock down swtpm and
+            # restrict the set of locations it can access files in.
+            socket = os.path.join(tpmstate_dir, 'swtpm-socket')
+            subprocess.run(['swtpm', 'socket', '-d', '--tpm2',
+                            '--tpmstate', f'dir={tpmstate_dir}',
+                            '--ctrl', f'type=unixio,path={socket}'],
+                           check=True)
+            self.vm.add_args('-chardev', f'socket,id=chrtpm,path={socket}')
+            self.vm.add_args('-tpmdev', 'emulator,id=tpm0,chardev=chrtpm')
+            self.vm.add_args('-device', 'tpm-tis-device,tpmdev=tpm0')
 
         self.vm.set_console()
         self.vm.launch()
@@ -54,6 +75,14 @@ class RiscvServerRefTest(QemuSystemTest):
 
         # Wait for boot to complete - system reaches login prompt
         wait_for_console_pattern(self, 'Run /sbin/init as init process')
+
+    def test_boot_linux_test(self):
+        self._test_boot_linux_test()
+
+    @skipIfMissingCommands('swtpm')
+    def test_boot_linux_test_tpm(self):
+        with tempfile.TemporaryDirectory(prefix="qemu_") as tpmstate_dir:
+            self._test_boot_linux_test(tpmstate_dir)
 
 if __name__ == '__main__':
     QemuSystemTest.main()
