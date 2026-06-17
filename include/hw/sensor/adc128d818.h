ce28acff9b2b6ee442d611448bcf4d556dd6e5f3
Emmanuel Blot <eblot@meta.com>
2026-06-17 12:26:00 +0200

    hw/sensor: adc128d818: add 12-bit 8-channel ADC device
    The ADC128D818 is a TI 12-bit, 8-channel I2C ADC used on several
    OpenBMC platforms for voltage and temperature monitoring.
    
    Implement the device with:
     - four operating modes
     - 12-bit voltage conversion from QOM inputs
     - 9-bit temperature conversion from milli-degree Celsius QOM inputs
     - switchable internal or external voltage reference
     - per-channel high/low limit registers
     - interrupt support
     - software reset
     - one-shot conversion support in shutdown mode
    
    Signed-off-by: Emmanuel Blot <eblot@meta.com>
    Reviewed-by: Alexander Hansen <alexander.hansen@9elements.com>
    Tested-by: Alexander Hansen <alexander.hansen@9elements.com>
    Message-Id: <20260617-i2c-adc128d818-anacapa-v2-1-fd38764f0465@meta.com>

---
+++ include/hw/sensor/adc128d818.h
@@ -0,0 +1,12 @@
+/*
+ * Texas Instruments ADC128D818 12-bit 8-channel ADC with I2C interface
+ *
+ * SPDX-License-Identifier: GPL-2.0-or-later
+ */
+
+#ifndef HW_SENSOR_ADC128D818_H
+#define HW_SENSOR_ADC128D818_H
+
+#define TYPE_ADC128D818 "adc128d818"
+
+#endif
