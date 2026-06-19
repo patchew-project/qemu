517c2ce631e394fdd69af1f4fef3dd7ddd5624fc
Alex Bennée <alex.bennee@linaro.org>
2026-06-19 14:05:31 +0100

    tests/functional: update anacapa-bmc image
    The initial version had the wrong DTB which caused issues with image
    corruption [1]. Update to the latest version.
    
    [1] https://github.com/legoater/qemu-aspeed-boot/pull/7
    
    Suggested-by: Cédric Le Goater <clg@redhat.com>
    Signed-off-by: Alex Bennée <alex.bennee@linaro.org>
    Reviewed-by: Cédric Le Goater <clg@redhat.com>
    Message-Id: <20260619130537.820184-3-alex.bennee@linaro.org>

--- tests/functional/arm/test_aspeed_anacapa.py
+++ tests/functional/arm/test_aspeed_anacapa.py
@@ -11,8 +11,8 @@ from aspeed import FacebookAspeedTest
 class AnacapaMachine(FacebookAspeedTest):
 
     ASSET_ANACAPA_FLASH = Asset(
-        'https://github.com/legoater/qemu-aspeed-boot/raw/3fa3212827b04be4034d43b5adeef57c27d6ab18/images/anacapa-bmc/openbmc-20260512025228/obmc-phosphor-image-anacapa-20260512025228.static.mtd.xz',
-        '2232e241abcfb6d4f6b82cb6c378ce5ce05e364aac6d118785c2b6cc33fe43f3')
+        'https://github.com/legoater/qemu-aspeed-boot/raw/refs/heads/master/images/anacapa-bmc/openbmc-20260616025349/obmc-phosphor-image-anacapa-20260616025349.static.mtd.xz',
+        'de3841fb6ed3085aec6424358ee6efc4b8ee85688361e5aa1987fd1acb7d3fb4')
 
     def test_arm_ast2600_anacapa_openbmc(self):
         image_path = self.uncompress(self.ASSET_ANACAPA_FLASH)
