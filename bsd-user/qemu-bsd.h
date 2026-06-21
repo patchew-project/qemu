bdd3624c9eff2bd6b2d73c6f5b33c7efbb9eddb4
Warner Losh <imp@bsdimp.com>
2026-06-20 22:19:51 -0600

    bsd-user: Remove accidentally duplicated prototypes.
    Now sure how we mismerged these, but we have two sets of
    prototypes. Remove one.
    
    Signed-off-by: Warner Losh <imp@bsdimp.com>
    Acked-by: Richard Henderson <richard.henderson@linaro.org>
    Message-Id: <20260620-misc-2026q2-v3-4-f6ce23ad8dda@bsdimp.com>

--- bsd-user/qemu-bsd.h
+++ bsd-user/qemu-bsd.h
@@ -50,6 +50,32 @@ abi_long target_to_host_msqid_ds(struct msqid_ds *host_md,
 abi_long host_to_target_msqid_ds(abi_ulong target_addr,
         struct msqid_ds *host_md);
 
+/* bsd-socket.c */
+abi_long target_to_host_sockaddr(struct sockaddr *addr, abi_ulong target_addr,
+        socklen_t len);
+abi_long host_to_target_sockaddr(abi_ulong target_addr, struct sockaddr *addr,
+        socklen_t len);
+abi_long target_to_host_ip_mreq(struct ip_mreqn *mreqn, abi_ulong target_addr,
+        socklen_t len);
+
+/* bsd-misc.c */
+abi_long host_to_target_uuid(abi_ulong target_addr, struct uuid *host_uuid);
+
+abi_long target_to_host_semarray(int semid, unsigned short **host_array,
+        abi_ulong target_addr);
+abi_long host_to_target_semarray(int semid, abi_ulong target_addr,
+        unsigned short **host_array);
+
+abi_long target_to_host_semid_ds(struct semid_ds *host_sd,
+        abi_ulong target_addr);
+abi_long host_to_target_semid_ds(abi_ulong target_addr,
+        struct semid_ds *host_sd);
+
+abi_long target_to_host_msqid_ds(struct msqid_ds *host_md,
+        abi_ulong target_addr);
+abi_long host_to_target_msqid_ds(abi_ulong target_addr,
+        struct msqid_ds *host_md);
+
 /* bsd-mem.c */
 void target_to_host_ipc_perm__locked(struct ipc_perm *host_ip,
         struct target_ipc_perm *target_ip);
@@ -59,20 +85,5 @@ abi_long target_to_host_shmid_ds(struct shmid_ds *host_sd,
         abi_ulong target_addr);
 abi_long host_to_target_shmid_ds(abi_ulong target_addr,
         struct shmid_ds *host_sd);
-
-/* bsd-misc.c */
-abi_long host_to_target_uuid(abi_ulong target_addr, struct uuid *host_uuid);
-abi_long target_to_host_semarray(int semid, unsigned short **host_array,
-        abi_ulong target_addr);
-abi_long host_to_target_semarray(int semid, abi_ulong target_addr,
-        unsigned short **host_array);
-abi_long target_to_host_semid_ds(struct semid_ds *host_sd,
-        abi_ulong target_addr);
-abi_long host_to_target_semid_ds(abi_ulong target_addr,
-        struct semid_ds *host_sd);
-abi_long target_to_host_msqid_ds(struct msqid_ds *host_md,
-        abi_ulong target_addr);
-abi_long host_to_target_msqid_ds(abi_ulong target_addr,
-        struct msqid_ds *host_md);
 
 #endif /* QEMU_BSD_H */
