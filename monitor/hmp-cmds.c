32e8f22e561c9a53a849ca8b98e113ee0e912872
Daniel P. Berrangé <berrange@redhat.com>
2026-06-23 10:55:18 +0100

    monitor: replace 'common' with 'parent_obj' in MonitorHMP
    The field name 'parent_obj' is standard practice for QOM structs
    so align the HMP monitor.
    
    Reviewed-by: Marc-André Lureau <marcandre.lureau@redhat.com>
    Reviewed-by: Dr. David Alan Gilbert <dave@treblig.org>
    Signed-off-by: Daniel P. Berrangé <berrange@redhat.com>
    Tested-by: Peter Krempa <pkrempa@redhat.com>
    Message-Id: <20260623095551.2106712-3-berrange@redhat.com>

--- monitor/hmp-cmds.c
+++ monitor/hmp-cmds.c
@@ -287,7 +287,7 @@ void hmp_info_sync_profile(Monitor *mon, const QDict *qdict)
 
 void hmp_info_history(Monitor *mon, const QDict *qdict)
 {
-    MonitorHMP *hmp_mon = container_of(mon, MonitorHMP, common);
+    MonitorHMP *hmp_mon = container_of(mon, MonitorHMP, parent_obj);
     int i;
     const char *str;
 
