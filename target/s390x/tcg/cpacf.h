69f8cf4090e48b552d840aa6937599598e025cb9
Harald Freudenberger <freude@linux.ibm.com>
2026-06-24 10:10:01 +0200

    target/s390x: Support cpacf sha256
    Add a new file cpacf_sha256.c which implements sha256.
    Add support for the sha256 subfuction for CPACF kimd and klmd.
    
    Signed-off-by: Harald Freudenberger <freude@linux.ibm.com>
    Tested-by: Holger Dengler <dengler@linux.ibm.com>
    Reviewed-by: Finn Callies <fcallies@linux.ibm.com>
    Reviewed-by: Ilya Leoshkevich <iii@linux.ibm.com>
    Message-Id: <20260624081029.23815-5-freude@linux.ibm.com>

--- target/s390x/tcg/cpacf.h
+++ target/s390x/tcg/cpacf.h
@@ -11,6 +11,11 @@
 #include "cpu.h"
 #include <stdint.h>
 
+/* from cpacf_sha256.c */
+int cpacf_sha256(CPUS390XState *env, const int mmu_idx, uintptr_t ra,
+                 uint64_t param_addr, uint64_t *message_reg, uint64_t *len_reg,
+                 uint32_t type);
+
 /* from cpacf_sha512.c */
 int cpacf_sha512(CPUS390XState *env, const int mmu_idx, uintptr_t ra,
                  uint64_t param_addr, uint64_t *message_reg, uint64_t *len_reg,
