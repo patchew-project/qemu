ef140f3150c2829a3ae789a5102be1d3570a6ad0
Avihai Horon <avihaih@nvidia.com>
2026-07-06 11:52:09 +0300

    migration: Enable new switchover-ack
    Now that VFIO has implemented new switchover-ack, enable it for new
    machines and keep legacy behavior for old machines.
    
    Reviewed-by: Cédric Le Goater <clg@redhat.com>
    Signed-off-by: Avihai Horon <avihaih@nvidia.com>
    Message-Id: <20260706085211.13905-15-avihaih@nvidia.com>

--- hw/core/machine.c
+++ hw/core/machine.c
@@ -48,6 +48,7 @@ GlobalProperty hw_compat_11_0[] = {
     { TYPE_ARM_SMMUV3, "ril", "on" },
     { TYPE_ARM_SMMUV3, "ssidsize", "0" },
     { TYPE_ARM_SMMUV3, "oas", "44" },
+    { "migration", "switchover-ack-legacy", "on" },
 };
 const size_t hw_compat_11_0_len = G_N_ELEMENTS(hw_compat_11_0);
 
