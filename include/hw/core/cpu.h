ad85b323f68420286a2c13f84ea28a59edd47f84
Philippe Mathieu-Daudé <philmd@oss.qualcomm.com>
2026-07-06 15:45:27 +0200

    cpu: Only check SSTEP_ENABLE flag in cpu_single_stepping()
    Only the SSTEP_ENABLE bitmask means single-step is enabled.
    
    Fixes: 60897d369f1 ("Debugger single step without interrupts")
    Signed-off-by: Philippe Mathieu-Daudé <philmd@oss.qualcomm.com>
    Reviewed-by: Daniel Henrique Barboza <daniel.barboza@oss.qualcomm.com>
    Reviewed-by: Richard Henderson <richard.henderson@linaro.org>
    Message-ID: <20260705215729.62196-33-philmd@oss.qualcomm.com>
    Message-Id: <20260706134527.84808-33-philmd@oss.qualcomm.com>

--- include/hw/core/cpu.h
+++ include/hw/core/cpu.h
@@ -1146,7 +1146,7 @@ void cpu_single_step(CPUState *cpu, unsigned flags);
  */
 static inline bool cpu_single_stepping(const CPUState *cpu)
 {
-    return cpu->singlestep_flags;
+    return cpu->singlestep_flags & SSTEP_ENABLE;
 }
 
 int cpu_breakpoint_insert(CPUState *cpu, vaddr pc, int flags,
