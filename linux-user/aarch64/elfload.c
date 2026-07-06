715116c066a3f8815624701a833bd9f3ec2edd58
Jim MacArthur <jim.macarthur@linaro.org>
2026-07-06 11:37:10 +0100

    linux-user/aarch64/elfload.c: Add FPRCVT
    Reviewed-by: Richard Henderson <richard.henderson@linaro.org>
    Signed-off-by: Jim MacArthur <jim.macarthur@linaro.org>
    Message-id: 20260630-jmac-fprcvt-v3-4-f4840d5e0a7f@linaro.org
    Signed-off-by: Peter Maydell <peter.maydell@linaro.org>
    Message-Id: <20260706103752.320511-8-peter.maydell@linaro.org>

--- linux-user/aarch64/elfload.c
+++ linux-user/aarch64/elfload.c
@@ -174,6 +174,7 @@ abi_ulong get_elf_hwcap(CPUState *cs)
     GET_FEATURE_ID(aa64_f8mm8, ARM_HWCAP_A64_F8MM8);
     GET_FEATURE_ID(aa64_f8mm4, ARM_HWCAP_A64_F8MM4);
     GET_FEATURE_ID(aa64_ssve_aes, ARM_HWCAP_A64_SME_AES);
+    GET_FEATURE_ID(aa64_fprcvt, ARM_HWCAP_A64_FPRCVT);
 
     return hwcaps;
 }
