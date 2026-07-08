e0513dcd6d541d46ac816c014427298ba9054678
Max Chou <max.chou@sifive.com>
2026-07-09 00:18:57 +0800

    target/riscv: rvv: Add vfncvtbf16.sat.f.f.w instruction for Zvfofp8min extension
    The vfncvtbf16.sat.f.f.w instruction converts a vector of 16-bit
    floating-point numbers to a vector of 8-bit floating-point numbers with
    saturation.
    The VTYPE.altfmt field is used to select the format of the 8-bit floating-point
    numbers.
    * altfmt = 0: BF16 to OFP8.e4m3
    * altfmt = 1: BF16 to OFP8.e5m2
    
    Reviewed-by: Chao Liu <chao.liu.zevorn@gmail.com>
    Signed-off-by: Max Chou <max.chou@sifive.com>
    Message-Id: <20260708161859.1124848-8-max.chou@sifive.com>

--- target/riscv/tcg/translate.c
+++ target/riscv/tcg/translate.c
@@ -1213,6 +1213,7 @@ static uint32_t opcode_at(DisasContextBase *dcbase, target_ulong pc)
 #include "insn_trans/trans_privileged.c.inc"
 #include "insn_trans/trans_svinval.c.inc"
 #include "insn_trans/trans_rvbf16.c.inc"
+#include "insn_trans/trans_rvofp8.c.inc"
 #include "decode-xthead.c.inc"
 #include "decode-xmips.c.inc"
 #include "decode-xlrbr.c.inc"
