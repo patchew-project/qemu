b17a92e2c4bd5da05b0dd7bb3d4ee3e109ff8b38
Richard Henderson <richard.henderson@linaro.org>
2026-07-10 13:53:30 -0700

    system: Remove cpu_watchpoint_insert error return path
    Move the validation of the address range to tcg_insert_breakpoint,
    which has to deal with gdbstub inputs.  Leave assertions behind
    in cpu_watchpoint_insert, but no error return path.
    
    Signed-off-by: Richard Henderson <richard.henderson@linaro.org>
    Reviewed-by: Philippe Mathieu-Daudé <philmd@oss.qualcomm.com>
    Message-Id: <20260710205401.836304-21-richard.henderson@linaro.org>

--- accel/tcg/tcg-accel-ops.c
+++ accel/tcg/tcg-accel-ops.c
@@ -143,6 +143,9 @@ static int tcg_insert_gdbstub_breakpoint(CPUState *cs, GdbBreakpointType type,
     case GDB_WATCHPOINT_WRITE:
     case GDB_WATCHPOINT_READ:
     case GDB_WATCHPOINT_ACCESS:
+        if (len == 0 || addr + (len - 1) < addr) {
+            return -EINVAL;
+        }
         CPU_FOREACH(cpu) {
             err = cpu_watchpoint_insert(cpu, addr, len,
                                         xlat_gdb_type(cpu, type), 0, NULL);
