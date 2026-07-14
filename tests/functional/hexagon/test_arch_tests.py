fae44a5c2a30f2dfc9ce4cf17f842b09b9e4eb20
Brian Cain <brian.cain@oss.qualcomm.com>
2026-07-14 08:51:26 -0700

    tests/functional/hexagon: add arch_tests functional test
    Add new test cases from hexagon-arch-tests to exercise exceptions,
    guest mode, interrupt steering, etc.
    
    Signed-off-by: Brian Cain <brian.cain@oss.qualcomm.com>
    Reviewed-by: Pierrick Bouvier <pierrick.bouvier@oss.qualcomm.com>
    Message-Id: <20260714155126.3509544-8-brian.cain@oss.qualcomm.com>

---
+++ tests/functional/hexagon/test_arch_tests.py
@@ -0,0 +1,65 @@
+#!/usr/bin/env python3
+#
+# Copyright (c) Qualcomm Technologies, Inc. and/or its subsidiaries.
+#
+# SPDX-License-Identifier: GPL-2.0-or-later
+
+import os
+from qemu_test import QemuSystemTest, Asset
+from qemu_test.cmd import wait_for_console_pattern
+
+
+class ArchTestsUart(QemuSystemTest):
+    """
+    Hexagon architecture verification tests
+
+    These are bare-metal tests from hexagon-arch-tests that exercise
+    system functionality.
+
+    Tests output results via UART.
+    """
+    timeout = 60
+
+    ASSET_TARBALL = Asset(
+        "https://github.com/qualcomm/qemu-hexagon-testing/releases/"
+        "download/v0.2.5/arch_tests_uart.tar.gz",
+        "edb4f37b877a3a72a0e10920477458a43b40045d34398fee8cb763fefd342f4f",
+    )
+
+    def run_uart_test(self, test_name: str,
+                      machine: str = "virt") -> None:
+        """
+        Run an arch test binary and verify PASS via UART console output.
+        """
+        self.set_machine(machine)
+        self.archive_extract(self.ASSET_TARBALL)
+        target_bin = os.path.join(self.workdir,
+                                  'arch_tests_uart_package',
+                                  'bin', test_name)
+        self.vm.set_console()
+        self.set_vm_arg("-display", "none")
+        self.set_vm_arg("-kernel", target_bin)
+        self.vm.launch()
+        wait_for_console_pattern(self, "PASS")
+
+    def test_exceptions(self) -> None:
+        """Tests exception delivery for trap instructions, privilege
+        violations, and verifies SSR cause codes and ELR values.
+        """
+        self.run_uart_test("test_exceptions")
+
+    def test_guest_mode(self) -> None:
+        """Tests guest mode entry/exit via CCR configuration, verifying
+        GSR fields, GELR, and guest event vector table dispatch.
+        """
+        self.run_uart_test("test_guest_mode")
+
+    def test_int_steering(self) -> None:
+        """Tests interrupt steering via priority-based routing to
+        specific threads using STID priority and iassignw.
+        """
+        self.run_uart_test("test_int_steering")
+
+
+if __name__ == "__main__":
+    QemuSystemTest.main()
