4001d30fa996ec5ae43a4d6c61df6f6a6c4d3ec3
Jamin Lin <jamin_lin@aspeedtech.com>
2026-07-15 03:33:40 +0000

    tests/functional/aarch64/test_aspeed_ast2700: Drop the AST2700 crypto self-test workaround
    The AST2700 boot tests appended 'cryptomgr.notests=1' to the kernel
    command line to skip the Linux crypto manager self-tests.
    
    Now that the HACE crypto engine emulates the crypto command, the
    self-tests pass, so drop the workaround and let them run. This also
    exercises the HACE crypto emulation during the functional boot tests.
    
    Signed-off-by: Jamin Lin <jamin_lin@aspeedtech.com>
    Message-Id: <20260715033311.1648424-18-jamin_lin@aspeedtech.com>

--- tests/functional/aarch64/test_aspeed_ast2700a1.py
+++ tests/functional/aarch64/test_aspeed_ast2700a1.py
@@ -51,10 +51,6 @@ class AST2x00MachineSDK(QemuSystemTest):
         wait_for_console_pattern(self, 'pass')
         wait_for_console_pattern(self, 'Jumping to BL31 (Trusted Firmware-A)')
 
-    def disable_kernel_crypto_selftest(self):
-         exec_command_and_wait_for_pattern(self,
-            'setenv bootargs "${bootargs} cryptomgr.notests=1"', '=>')
-
     def enable_ast2700_pcie2(self):
         exec_command_and_wait_for_pattern(self,
             'cp 100420000 403000000 900000', '=>')
@@ -71,7 +67,6 @@ class AST2x00MachineSDK(QemuSystemTest):
         wait_for_console_pattern(self, 'U-Boot 2023.10')
         wait_for_console_pattern(self, 'Hit any key to stop autoboot')
         exec_command_and_wait_for_pattern(self, '\012', '=>')
-        self.disable_kernel_crypto_selftest()
         if enable_pcie:
             self.enable_ast2700_pcie2()
         else:
