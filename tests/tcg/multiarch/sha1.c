cc17bd82bfb626e2049e7299310645875e6be4ce
Pierrick Bouvier <pierrick.bouvier@oss.qualcomm.com>
2026-07-27 21:41:54 -0700

    tests/tcg/multiarch/sha1.c: fix big endian implementation
    All big endian targets were returning a wrong value due to this.
    
    Tested-by: Alex Bennée <alex.bennee@linaro.org>
    Signed-off-by: Pierrick Bouvier <pierrick.bouvier@oss.qualcomm.com>
    Reviewed-by: Philippe Mathieu-Daudé <philmd@oss.qualcomm.com>
    Tested-by: Aniket Sahu <asahu1x@linux.ibm.com>
    Message-Id: <20260728044248.137626-51-pierrick.bouvier@oss.qualcomm.com>

--- tests/tcg/multiarch/sha1.c
+++ tests/tcg/multiarch/sha1.c
@@ -48,10 +48,10 @@ void SHA1Final(unsigned char digest[20], SHA1_CTX* context);
 
 /* blk0() and blk() perform the initial expand. */
 /* I got the idea of expanding during the round function from SSLeay */
-#if BYTE_ORDER == LITTLE_ENDIAN
+#if __BYTE_ORDER__ == __ORDER_LITTLE_ENDIAN__
 #define blk0(i) (block->l[i] = (rol(block->l[i],24)&0xFF00FF00) \
     |(rol(block->l[i],8)&0x00FF00FF))
-#elif BYTE_ORDER == BIG_ENDIAN
+#elif __BYTE_ORDER__ == __ORDER_BIG_ENDIAN__
 #define blk0(i) block->l[i]
 #else
 #error "Endianness not defined!"
