5d3fb33a3a9475cf9a9e702692602deaa01a443f
Anton Johansson via qemu development <qemu-devel@nongnu.org>
2026-07-30 05:09:47 +0200

    helper-to-tcg: PrepareForOptPass, demangle function names
    Since input LLVM IR might come from languages which mangle function
    names, e.g. C++, take extra care to demangle all function names and save
    them on the side for future lookup.
    
    Signed-off-by: Anton Johansson <anjo@rev.ng>
    Reviewed-by: Alessandro Di Federico <ale@rev.ng>
    Message-Id: <20260730031025.12926-14-anjo@rev.ng>

--- subprojects/helper-to-tcg/src/PrepareForOptPass/PrepareForOptPass.cpp
+++ subprojects/helper-to-tcg/src/PrepareForOptPass/PrepareForOptPass.cpp
@@ -15,15 +15,56 @@
 //  along with this program; if not, see <http://www.gnu.org/licenses/>.
 //
 
-#include "PrepareForOptPass.h"
+#include "PrepareForOptPass.hpp"
 
+#include <llvm/ADT/StringRef.h>
+#include <llvm/ADT/StringSet.h>
+#include <llvm/Demangle/Demangle.h>
 #include <llvm/Support/Debug.h>
 
 #define DEBUG_TYPE "prepare-for-opt"
 
 using namespace llvm;
 
+static void demangleFunctionNames(Module &M) {
+    StringSet DemangledNames;
+    for (Function &F : M) {
+        const std::string DemangledName = llvm::demangle(F.getName().str());
+        if (DemangledName == F.getName()) {
+            // Name not mangled
+            continue;
+        }
+        // The resulting demangled name might look something like
+        //
+        //   namespace::subnamespace::function(...)
+        //
+        // Extract the function name and use this to replace mangled name.  If
+        // we previously encountered the same name, give up and leave the
+        // mangled name.
+        std::string FunctionName;
+        size_t Index = 0;
+        // Remove namespaces
+        Index = DemangledName.find_last_of(':');
+        if (Index != std::string::npos) {
+            FunctionName = DemangledName.substr(Index + 1);
+        }
+        // Remove arguments
+        Index = FunctionName.find_first_of('(');
+        if (Index != std::string::npos) {
+            FunctionName = FunctionName.substr(0, Index);
+        }
+
+        if (DemangledNames.contains(FunctionName)) {
+            continue;
+        }
+        DemangledNames.insert(FunctionName);
+
+        F.setName(FunctionName);
+    }
+}
+
 PreservedAnalyses PrepareForOptPass::run(Module &M,
                                          ModuleAnalysisManager &MAM) {
+    demangleFunctionNames(M);
     return PreservedAnalyses::none();
 }
