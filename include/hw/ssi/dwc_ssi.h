4b193e53185b6b838fc9edb97f6b1fea6b15e648
Kangjie Huang <flamboyant.h.01@gmail.com>
2026-08-09 02:29:52 +0800

    hw/ssi: Add Synopsys DWC SSI standard PIO controller
    Add a reusable SysBus model for the Synopsys DWC SSI controller. The model
    uses the DWC CTRLR0 layout, where TMOD is at bits [11:10]; DW APB SSI
    variants with TMOD at bits [9:8] are not supported.
    
    Implement the Standard SPI register subset, configurable chip-select and
    FIFO resources, the four Standard PIO transfer modes, reset handling,
    chip-select GPIOs, and RAZ/WI handling for unsupported enhanced, DMA, and
    XIP registers.
    
    Add num-cs, fifo-depth, and imr-reset properties. Save only Standard PIO
    state in VMState and reject migration between devices with different
    resource profiles.
    
    The Synopsys databook requires myDesignWare registration and has no stable
    public URL. The K230 TRM is the public register reference for this model:
    
    https://github.com/revyos/external-docs/blob/79b3a79072412ead81427e6755b4d9e6d9ded8d8/K230/en-us/K230_Technical_Reference_Manual_V0.3.1_20241118.pdf
    
    The Linux driver and binding provide software and capability references:
    
    https://github.com/torvalds/linux/blob/f9a2394a23482bfd330911e9c8295b71724feacd/drivers/spi/spi-dw-core.c
    
    https://github.com/torvalds/linux/blob/f9a2394a23482bfd330911e9c8295b71724feacd/drivers/spi/spi-dw.h
    
    https://github.com/torvalds/linux/blob/f9a2394a23482bfd330911e9c8295b71724feacd/Documentation/devicetree/bindings/spi/snps,dw-apb-ssi.yaml
    
    The Intel Arria 10 HPS TRM describes the DW APB SSI family variant and is
    used only for comparison with the DWC layout:
    
    https://www.intel.com/content/www/us/en/docs/programmable/683711/21-2/hard-processor-system-technical-reference.html
    
    Suggested-by: Bin Meng <bmeng.cn@gmail.com>
    Suggested-by: Chao Liu <chao.liu@processmission.com>
    Suggested-by: Anirudh Srinivasan <asrinivasan@oss.tenstorrent.com>
    
    The controller is not instantiated by this patch.
    
    Signed-off-by: Kangjie Huang <flamboyant.h.01@gmail.com>
    Message-Id: <20260808182956.290561-2-flamboyant.h.01@gmail.com>

---
+++ include/hw/ssi/dwc_ssi.h
@@ -0,0 +1,63 @@
+/*
+ * Synopsys DesignWare SSI
+ *
+ * Copyright (c) 2026 Kangjie Huang <flamboyant.h.01@gmail.com>
+ *
+ * SPDX-License-Identifier: GPL-2.0-or-later
+ *
+ * Emulates the DesignWare SSI controller in Standard SPI mode,
+ * covering the PIO/FIFO data path and chip selects.
+ */
+
+#ifndef HW_SSI_DWC_SSI_H
+#define HW_SSI_DWC_SSI_H
+
+#include "hw/core/sysbus.h"
+#include "hw/ssi/ssi.h"
+#include "qemu/fifo32.h"
+#include "qom/object.h"
+
+#define TYPE_DWC_SSI "dwc-ssi"
+OBJECT_DECLARE_SIMPLE_TYPE(DwcSsiState, DWC_SSI)
+
+#define DWC_SSI_MMIO_SIZE 0x1000
+#define DWC_SSI_REGS_SIZE 0x14c
+#define DWC_SSI_NUM_REGS \
+    (DWC_SSI_REGS_SIZE / sizeof(uint32_t))
+
+typedef enum DwcSsiPhase {
+    DWC_SSI_PHASE_IDLE,
+    DWC_SSI_PHASE_STANDARD_TX_ONLY,
+    DWC_SSI_PHASE_RX_ONLY,
+    DWC_SSI_PHASE_EEPROM_COMMAND,
+    DWC_SSI_PHASE_EEPROM_DATA,
+    DWC_SSI_PHASE_STANDARD_TR,
+} DwcSsiPhase;
+
+typedef struct DwcSsiConfig {
+    uint32_t num_cs;
+    uint32_t fifo_depth;
+    uint32_t imr_reset;
+} DwcSsiConfig;
+
+struct DwcSsiState {
+    SysBusDevice parent_obj;
+
+    MemoryRegion mmio;
+    SSIBus *spi;
+
+    qemu_irq *cs_lines;
+
+    Fifo32 tx_fifo;
+    Fifo32 rx_fifo;
+    uint32_t regs[DWC_SSI_NUM_REGS];
+
+    DwcSsiConfig cfg;
+
+    uint32_t phase;
+    uint32_t remaining_frames;
+    uint32_t dummy_frame;
+
+    int active_cs;
+};
+#endif /* HW_SSI_DWC_SSI_H */
