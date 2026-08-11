80e81aed7f8ede029504dfc163c5d1b70af3de22
Jared Rossi <jrossi@linux.ibm.com>
2026-08-11 10:46:23 -0400

    hw/s390x/ipl: Fix incorrect PCI IPL block length constant
    The IplBlockPci struct is 336 bytes in length.
    
    Fix the S390_IPLB_MIN_PCI_LEN to reflect this, and assign a valid blk0_len
    while we are at it.
    
    Signed-off-by: Jared Rossi <jrossi@linux.ibm.com>
    Message-Id: <20260811144629.866641-3-jrossi@linux.ibm.com>

--- hw/s390x/ipl.h
+++ hw/s390x/ipl.h
@@ -107,7 +107,7 @@ QEMU_BUILD_BUG_MSG(offsetof(S390IPLState, iplb) & 3, "alignment of iplb wrong");
 #define S390_IPLB_MIN_PV_LEN 148
 #define S390_IPLB_MIN_CCW_LEN 200
 #define S390_IPLB_MIN_FCP_LEN 384
-#define S390_IPLB_MIN_PCI_LEN 376
+#define S390_IPLB_MIN_PCI_LEN 336
 #define S390_IPLB_MIN_QEMU_SCSI_LEN 200
 
 static inline bool iplb_valid_len(IplParameterBlock *iplb)
