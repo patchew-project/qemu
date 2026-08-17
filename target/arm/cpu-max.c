34e2da6c771e003b6d36019755234d343d8dc04a
Peter Maydell <peter.maydell@linaro.org>
2026-08-17 13:38:34 +0100

    target/arm: Add new feature ARM_FEATURE_NEON_TRAPS
    The traps configurable via CPACR.ASEDIS and HCPTR.TASE that trap only
    Neon instructions are unfortunately IMPDEF about whether they are
    implemented or not, and there is no ID register field that identifies
    whether they are present.  In practice, they are present on every
    implementation I have checked except for the Cortex-A8 (which was the
    first CPU with Neon).
    
    Add a new feature ARM_FEATURE_NEON_TRAPS which we set on every
    ARM_FEATURE_NEON CPU except the Cortex-A8, and make the CPACR.ASEDIS
    and HCPTR.TASE bits RAZ/WI unless the CPU has ARM_FEATURE_NEON and
    ARM_FEATURE_NEON_TRAPS.
    
    Signed-off-by: Peter Maydell <peter.maydell@linaro.org>
    Reviewed-by: Richard Henderson <richard.henderson@linaro.org>
    Message-Id: <20260817123838.1578060-5-peter.maydell@linaro.org>

--- target/arm/cpu-max.c
+++ target/arm/cpu-max.c
@@ -25,6 +25,7 @@ void aarch64_aa32_a57_init(Object *obj, bool aa32_only)
     cpu->dtb_compatible = "arm,cortex-a57";
     set_feature(&cpu->env, ARM_FEATURE_V8);
     set_feature(&cpu->env, ARM_FEATURE_NEON);
+    set_feature(&cpu->env, ARM_FEATURE_NEON_TRAPS);
     set_feature(&cpu->env, ARM_FEATURE_GENERIC_TIMER);
     set_feature(&cpu->env, ARM_FEATURE_BACKCOMPAT_CNTFRQ);
     if (aarch64_enabled) {
