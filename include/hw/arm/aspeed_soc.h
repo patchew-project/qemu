8274bcc0d49ff17608e0186c3dbd86023e23132f
Jamin Lin <jamin_lin@aspeedtech.com>
2026-08-20 08:12:11 +0000

    hw/arm/aspeed_ast2600: Wire up the ACRY model
    Introduce the ASPEED_DEV_ACRY enum slot and add the ACRY model state
    to AspeedSoCState.
    
    Instantiate the new ACRY model on the AST2600 SoC using its
    real-silicon register address and IRQ:
    
    - ACRY register region 0x1e6fa000-0x1e6fafff
    - IRQ 160
    
    Signed-off-by: Jamin Lin <jamin_lin@aspeedtech.com>
    Reviewed-by: Cédric Le Goater <clg@kaod.org>
    Message-Id: <20260820081206.3353463-4-jamin_lin@aspeedtech.com>

--- include/hw/arm/aspeed_soc.h
+++ include/hw/arm/aspeed_soc.h
@@ -25,6 +25,7 @@
 #include "hw/i2c/aspeed_i2c.h"
 #include "hw/i3c/aspeed_i3c.h"
 #include "hw/ssi/aspeed_smc.h"
+#include "hw/misc/aspeed_acry.h"
 #include "hw/misc/aspeed_hace.h"
 #include "hw/misc/aspeed_sbc.h"
 #include "hw/misc/aspeed_sli.h"
@@ -82,6 +83,7 @@ struct AspeedSoCState {
     AspeedSCUState scu;
     AspeedSCUState scuio;
     AspeedHACEState hace;
+    AspeedACRYState acry;
     AspeedXDMAState xdma;
     AspeedADCState adc;
     AspeedSMCState fmc;
@@ -271,6 +273,7 @@ enum {
     ASPEED_DEV_EMMC,
     ASPEED_DEV_KCS,
     ASPEED_DEV_HACE,
+    ASPEED_DEV_ACRY,
     ASPEED_DEV_DPMCU,
     ASPEED_DEV_DP,
     ASPEED_DEV_I3C,
