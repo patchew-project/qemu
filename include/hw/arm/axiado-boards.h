6114ae93458f40d0463c16d8266226991279b875
Kuan-Jui Chiu <kchiu@axiado.com>
2026-08-24 19:11:55 -0700

    hw/arm: Add Axiado Ax3005 EVB
    Add EVK ax3005-evb built with AX3005 SoC
    
    Signed-off-by: Kuan-Jui Chiu <kchiu@axiado.com>
    Message-Id: <20260825021155.2475879-6-kchiu@axiado.com>

--- include/hw/arm/axiado-boards.h
+++ include/hw/arm/axiado-boards.h
@@ -11,6 +11,7 @@
 
 #include "hw/core/boards.h"
 #include "hw/arm/ax3000-soc.h"
+#include "hw/arm/ax3005-soc.h"
 
 #define TYPE_AX3000_MACHINE       MACHINE_TYPE_NAME("ax3000")
 OBJECT_DECLARE_TYPE(Ax3000MachineState, Ax3000MachineClass, AX3000_MACHINE)
@@ -25,4 +26,13 @@ typedef struct Ax3000MachineClass {
     MachineClass parent;
 
 } Ax3000MachineClass;
+
+#define TYPE_AX3005_MACHINE       MACHINE_TYPE_NAME("ax3005-evb")
+OBJECT_DECLARE_SIMPLE_TYPE(Ax3005MachineState, AX3005_MACHINE)
+
+struct Ax3005MachineState {
+    MachineState parent;
+
+    Ax3005SoCState *soc;
+};
 #endif
