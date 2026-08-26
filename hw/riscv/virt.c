4ab363d42951e5df63d56c9980e584ebc83214dd
Yonggang Luo <luoyonggang@gmail.com>
2026-08-27 02:42:19 +0800

    hw/riscv/virt: use unique QOM name
    Use TYPE_RISCV_VIRT_MACHINE so the RISC-V virt QOM type does not
    collide with ARM virt in a combined qemu-system binary. Keep the
    -M / help / migration name as virt via machine_class_set_name().
    
    Signed-off-by: Yonggang Luo <luoyonggang@gmail.com>
    Message-Id: <20260826184229.1145-6-luoyonggang@gmail.com>

--- hw/riscv/virt.c
+++ hw/riscv/virt.c
@@ -1721,6 +1721,7 @@ static void virt_machine_class_init(ObjectClass *oc, const void *data)
 
     TARGET_SPECIFIC_CLASS(oc)->is_available = target_base_riscv;
     mc->desc = "RISC-V VirtIO board";
+    machine_class_set_name(mc, "virt");
     mc->init = virt_machine_init;
     mc->max_cpus = VIRT_CPUS_MAX;
     mc->default_cpu_type = TYPE_RISCV_CPU_BASE;
@@ -1784,7 +1785,7 @@ static void virt_machine_class_init(ObjectClass *oc, const void *data)
 }
 
 static const TypeInfo virt_machine_typeinfo = {
-    .name       = MACHINE_TYPE_NAME("virt"),
+    .name       = TYPE_RISCV_VIRT_MACHINE,
     .parent     = TYPE_MACHINE,
     .class_init = virt_machine_class_init,
     .instance_init = virt_machine_instance_init,
