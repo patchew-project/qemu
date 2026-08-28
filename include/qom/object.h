2558cd29e4f68f42a468d5e0bb33aab9e683c7f5
Mark Cave-Ayland <mark.caveayland@nutanix.com>
2026-08-28 16:01:58 +0100

    qom/object.c: introduce object_class_property_add_alias()
    This is eventually intended to be a replacement for object_property_add_alias()
    which uses an object property instead of a class property.
    
    With the advent of class properties, it is possible that QOM may attempt to
    set or retrieve the value of an unset alias property. Update the existing
    property_get_alias() and property_set_alias() functions to use the null visitor
    if the alias target has not been set, and property_resolve_alias() to return
    NULL for the same case.
    
    This ensures that unset class alias properties accessed e.g. via the monitor do
    not cause QEMU to crash.
    
    Signed-off-by: Mark Cave-Ayland <mark.caveayland@nutanix.com>
    Message-Id: <20260828150313.1883999-3-mark.caveayland@nutanix.com>

--- include/qom/object.h
+++ include/qom/object.h
@@ -2263,6 +2263,11 @@ ObjectProperty *object_class_static_property_add_uint64_ptr(ObjectClass *klass,
                                           const uint64_t *v,
                                           ObjectPropertyFlags flags);
 
+typedef enum {
+    /* private */
+    OBJ_PROP_ALIAS_CLASS = 0x1,
+} ObjectPropertyAliasFlags;
+
 /**
  * object_property_add_alias:
  * @obj: the object to add a property to
@@ -2282,6 +2287,12 @@ ObjectProperty *object_class_static_property_add_uint64_ptr(ObjectClass *klass,
  */
 ObjectProperty *object_property_add_alias(Object *obj, const char *name,
                                Object *target_obj, const char *target_name);
+
+ObjectProperty *
+object_class_property_add_alias(ObjectClass *klass, const char *name,
+                                ptrdiff_t offset,
+                                const char *target_type,
+                                const char *target_name);
 
 /**
  * object_property_add_const_link:
