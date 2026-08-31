9781910e692ff11d8e59231fc7e994b77b8976c5
Marc-André Lureau <marcandre.lureau@redhat.com>
2026-08-31 16:09:26 +0400

    crypto/x509-utils: don't double set errp
    qcrypto_x509_get_ecc_curve() already sets errp on failure.
    
    Fixes: e8317c4c9f68 ("crypto/x509-utils: Add helper functions for DIAG 320 subcode 2")
    Signed-off-by: Marc-André Lureau <marcandre.lureau@redhat.com>
    Reviewed-by: Daniel P. Berrangé <berrange@redhat.com>
    Reviewed-by: Zhuoying Cai <zycai@linux.ibm.com>
    Message-Id: <20260831120926.2809313-1-marcandre.lureau@redhat.com>

--- crypto/x509-utils.c
+++ crypto/x509-utils.c
@@ -325,7 +325,6 @@ int qcrypto_x509_check_ecc_curve_p521(uint8_t *cert, size_t size, Error **errp)
 
     curve_id = qcrypto_x509_get_ecc_curve(cert, size, errp);
     if (curve_id == -1) {
-        error_setg(errp, "Failed to get ECC curve");
         return -1;
     }
 
