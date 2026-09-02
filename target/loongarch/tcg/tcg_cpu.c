fcf47d6342a5256a93890c80f6b256bdc7940117
Philippe Mathieu-Daudé <philmd@oss.qualcomm.com>
2026-09-02 17:20:40 +0200

    targets: Move interrupt handlers to do_interrupt()
    LoongArch, M68K, MicroBlaze, and OpenRISC interrupt handlers
    don't require external BQL protection, so move them from
    do_interrupt_locked() to do_interrupt().
    
    Signed-off-by: Philippe Mathieu-Daudé <philmd@oss.qualcomm.com>
    Message-Id: <20260902152044.31291-10-philmd@oss.qualcomm.com>

--- target/loongarch/tcg/tcg_cpu.c
+++ target/loongarch/tcg/tcg_cpu.c
@@ -329,7 +329,7 @@ const TCGCPUOps loongarch_tcg_ops = {
     .cpu_exec_interrupt = loongarch_cpu_exec_interrupt,
     .cpu_exec_halt = loongarch_cpu_has_work,
     .cpu_exec_reset = cpu_reset,
-    .do_interrupt_locked = loongarch_cpu_do_interrupt,
+    .do_interrupt = loongarch_cpu_do_interrupt,
     .do_transaction_failed = loongarch_cpu_do_transaction_failed,
 #endif
 };
--- target/microblaze/cpu.c
+++ target/microblaze/cpu.c
@@ -450,7 +450,7 @@ static const TCGCPUOps mb_tcg_ops = {
     .cpu_exec_interrupt = mb_cpu_exec_interrupt,
     .cpu_exec_halt = mb_cpu_has_work,
     .cpu_exec_reset = cpu_reset,
-    .do_interrupt_locked = mb_cpu_do_interrupt,
+    .do_interrupt = mb_cpu_do_interrupt,
     .do_transaction_failed = mb_cpu_transaction_failed,
     .do_unaligned_access = mb_cpu_do_unaligned_access,
 #endif /* !CONFIG_USER_ONLY */
