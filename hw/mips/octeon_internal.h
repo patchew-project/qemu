6434e94bfbc3d89c7f2752d76c1f9fc29b134a4f
Kirill A. Korinsky <kirill@korins.ky>
2026-09-03 14:01:57 +0200

    hw/mips: add Octeon CPUs and boot memory
    Create the Octeon CPUs and clock, map the split DRAM layout and
    per-core CVMSEG storage, and provide the EBB7304 boot flash windows.
    Load firmware into the flash reset alias and restore the boot PC during
    machine reset.
    Message-Id: <20260903120510.34876-10-kirill@korins.ky>

--- hw/mips/octeon_internal.h
+++ hw/mips/octeon_internal.h
@@ -10,7 +10,35 @@
 #define HW_MIPS_OCTEON_INTERNAL_H
 
 #include "qemu/typedefs.h"
+#include "system/memory.h"
+#include "target/mips/cpu.h"
 
 #define OCTEON_MAX_CPUS             16
+typedef struct OcteonState OcteonState;
+
+typedef struct OcteonCPUState {
+    OcteonState *board;
+    MIPSCPU *cpu;
+    bool boot_cpu;
+} OcteonCPUState;
+
+struct OcteonState {
+    MachineState *machine;
+    Clock *cpuclk;
+    uint64_t cpu_hz;
+    uint64_t ref_hz;
+    uint64_t io_hz;
+    uint64_t ddr_hz;
+    OcteonCPUState cpu[OCTEON_MAX_CPUS];
+    unsigned int cpu_count;
+    uint64_t firmware_entry;
+
+    MemoryRegion dr0;
+    MemoryRegion dr1;
+    MemoryRegion *flash;
+    MemoryRegion boot_flash;
+    MemoryRegion boot_flash_alias;
+    MemoryRegion cvmseg;
+};
 
 #endif
