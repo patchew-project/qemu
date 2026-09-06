a555ab5676518e01a1410631ab2fad97eb4c94ac
Emmanuel Blot <emmanuel.blot@free.fr>
2026-09-06 19:10:06 +0200

    hw/sensor: tmp105: make device state private to the implementation
    The TMP105State struct, its cast macro and typedef are used only within
    tmp105.c. Move them out of the public header into the source file and
    drop the now-unused includes; the header is reduced to the TYPE_TMP105
    name, with its guard renamed to the path-derived form per current QEMU
    convention.
    
    While here, normalize the source file include order to match the other
    hw/sensor devices.
    
    Signed-off-by: Emmanuel Blot <emmanuel.blot@free.fr>
    Reviewed-by: Cédric Le Goater <clg@redhat.com>
    Link: https://lore.kernel.org/qemu-devel/20260731-sanmiguel-bmc-locator-v2-1-1266926ba769@free.fr
    Signed-off-by: Cédric Le Goater <clg@redhat.com>
    Message-Id: <20260906171021.26568-26-clg@redhat.com>

--- hw/sensor/tmp105.c
+++ hw/sensor/tmp105.c
@@ -19,15 +19,50 @@
  */
 
 #include "qemu/osdep.h"
-#include "hw/i2c/i2c.h"
-#include "hw/core/irq.h"
-#include "migration/vmstate.h"
-#include "hw/sensor/tmp105.h"
+#include "qemu/module.h"
 #include "qapi/error.h"
 #include "qapi/visitor.h"
-#include "qemu/module.h"
+#include "qom/object.h"
+#include "hw/sensor/tmp105.h"
+#include "hw/sensor/tmp105_regs.h"
+#include "hw/core/irq.h"
 #include "hw/core/registerfields.h"
+#include "hw/i2c/i2c.h"
+#include "migration/vmstate.h"
 #include "trace.h"
+
+OBJECT_DECLARE_SIMPLE_TYPE(TMP105State, TMP105)
+
+/**
+ * TMP105State:
+ * @config: Bits 5 and 6 (value 32 and 64) determine the precision of the
+ * temperature. See Table 8 in the data sheet.
+ *
+ * @see_also: http://www.ti.com/lit/gpn/tmp105
+ */
+struct TMP105State {
+    /*< private >*/
+    I2CSlave i2c;
+    /*< public >*/
+
+    uint8_t len;
+    uint8_t buf[2];
+    qemu_irq pin;
+
+    uint8_t pointer;
+    uint8_t config;
+    int16_t temperature;
+    int16_t limit[2];
+    int faults;
+    uint8_t alarm;
+    /*
+     * The TMP105 initially looks for a temperature rising above T_high;
+     * once this is detected, the condition it looks for next is the
+     * temperature falling below T_low. This flag is false when initially
+     * looking for T_high, true when looking for T_low.
+     */
+    bool detect_falling;
+};
 
 FIELD(CONFIG, SHUTDOWN_MODE,        0, 1)
 FIELD(CONFIG, THERMOSTAT_MODE,      1, 1)
