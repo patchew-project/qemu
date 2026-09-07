a93c14dcbe5b76445598590df5c3124c5086d769
Emmanuel Blot via qemu development <qemu-devel@nongnu.org>
2026-09-07 18:42:22 +0200

    hw/sensor: ina230: add current/voltage/power monitor
    Add a model for the Texas Instruments INA230, a 36V, 16-bit I2C
    current-shunt and power monitor.
    
    The device measures a differential shunt voltage and a bus voltage; the
    current and power registers are not measured directly but derived by the
    device from the shunt voltage and the guest-programmed Calibration
    register. The alert is modelled.
    
    Shunt and bus voltages can be injected through the "shunt-voltage" and
    "bus-voltage" QOM properties (in nV and uV respectively).
    
    Note: Conversion is instantaneous: the averaging and conversion-time
    fields are stored but have no timing effect.
    
    Signed-off-by: Emmanuel Blot <emmanuel.blot@free.fr>
    Message-Id: <20260907-bmc-minerva-v1-32-0d83ad610c3b@free.fr>

---
+++ include/hw/sensor/ina230.h
@@ -0,0 +1,14 @@
+/*
+ * Texas Instruments INA230 current/voltage/power monitor with I2C interface
+ *
+ * Copyright (c) 2026 Meta Platforms, Inc. and affiliates.
+ *
+ * SPDX-License-Identifier: GPL-2.0-or-later
+ */
+
+#ifndef HW_SENSOR_INA230_H
+#define HW_SENSOR_INA230_H
+
+#define TYPE_INA230 "ina230"
+
+#endif
