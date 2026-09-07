3c79a558be8596ab8da63c3d0e5e04d4cbfe4ac0
Chen Pei <cp0613@linux.alibaba.com>
2026-09-07 17:37:28 +0800

    hw/riscv/virt: Add CXL support to the RISC-V virt machine
    Enable CXL support on the RISC-V virt machine following the same
    approach used by the ARM virt machine:
    - Add PXB and ACPI_CXL Kconfig selections
    - Add CXLState and PCIBus pointer to RISCVVirtState
    - Register CXL machine properties via cxl_machine_init()
    - Create CXL host register region above the PCIe high MMIO region
    - Call cxl_hook_up_pxb_registers() and cxl_fmws_link_targets() at
      machine_done time
    - Map Fixed Memory Windows above the CXL host register region
    - Add ACPI0017 device in DSDT and build CEDT table in virt-acpi-build.c
    
    Reviewed-by: Junjie Cao <junjie.cao@intel.com>
    Tested-by: Junjie Cao <junjie.cao@intel.com>
    Signed-off-by: Chen Pei <cp0613@linux.alibaba.com>
    Reviewed-by: Chao Liu <chao.liu@processmission.com>
    Reviewed-by: Jonathan Cameron <jonathan.cameron@oss.qualcomm.com>
    Reviewed-by: Michael S. Tsirkin <mst@redhat.com>
    Message-Id: <20260907093735.2753-2-cp0613@linux.alibaba.com>

--- hw/riscv/virt.c
+++ hw/riscv/virt.c
@@ -56,6 +56,8 @@
 #include "hw/pci/pci.h"
 #include "hw/pci-host/gpex.h"
 #include "hw/display/ramfb.h"
+#include "hw/cxl/cxl.h"
+#include "hw/cxl/cxl_host.h"
 #include "hw/acpi/aml-build.h"
 #include "qapi/qapi-visit-common.h"
 #include "hw/virtio/virtio-iommu.h"
@@ -540,6 +542,29 @@ static void create_fdt(RISCVVirtState *s)
     create_fdt_pmu(s);
 }
 
+static void cxl_host_state_init(RISCVVirtState *s)
+{
+    MemoryRegion *sysmem = get_system_memory();
+    MemoryRegion *mr = &s->cxl_devices_state.host_mr;
+    hwaddr base;
+
+    if (!s->cxl_devices_state.is_enabled) {
+        return;
+    }
+
+    base = virt_high_pcie_memmap.base + virt_high_pcie_memmap.size;
+    base = ROUND_UP(base, 64 * KiB);
+
+    memory_region_init(mr, OBJECT(s), "cxl_host_reg", 64 * KiB * 16);
+    memory_region_add_subregion(sysmem, base, mr);
+
+    /* Map the Fixed Memory Windows above the CXL host register region. */
+    base += memory_region_size(mr);
+    base = ROUND_UP(base, 256 * MiB);
+    cxl_fmws_set_memmap(base, UINT64_MAX);
+    cxl_fmws_update_mmio();
+}
+
 static FWCfgState *create_fw_cfg(const MachineState *ms, hwaddr base)
 {
     FWCfgState *fw_cfg;
@@ -624,6 +649,13 @@ static void virt_machine_done(Notifier *notifier, void *data)
     uint64_t kernel_entry = 0;
     BlockBackend *pflash_blk0;
     RISCVBootInfo boot_info;
+
+    cxl_hook_up_pxb_registers(s->pci_bus, &s->cxl_devices_state,
+                              &error_fatal);
+
+    if (s->cxl_devices_state.is_enabled) {
+        cxl_fmws_link_targets(&error_fatal);
+    }
 
     /*
      * An user provided dtb must include everything, including
@@ -716,6 +748,7 @@ static void virt_machine_init(MachineState *machine)
     MemoryRegion *system_memory = get_system_memory();
     MemoryRegion *mask_rom = g_new(MemoryRegion, 1);
     DeviceState *mmio_irqchip, *virtio_irqchip, *pcie_irqchip;
+    DeviceState *gpex_dev;
     int i, base_hartid, hart_count;
     int socket_count = riscv_socket_count(machine);
 
@@ -871,6 +904,8 @@ static void virt_machine_init(MachineState *machine)
             ROUND_UP(virt_high_pcie_memmap.base, virt_high_pcie_memmap.size);
     }
 
+    cxl_host_state_init(s);
+
     /* register system main memory (actual RAM) */
     memory_region_add_subregion(system_memory, s->memmap[VIRT_DRAM].base,
                                 machine->ram);
@@ -898,10 +933,12 @@ static void virt_machine_init(MachineState *machine)
             qdev_get_gpio_in(virtio_irqchip, VIRTIO_IRQ + i));
     }
 
-    riscv_gpex_pcie_init(system_memory, pcie_irqchip,
-                   &s->memmap[VIRT_PCIE_ECAM], &s->memmap[VIRT_PCIE_MMIO],
-                   &virt_high_pcie_memmap, &s->memmap[VIRT_PCIE_PIO],
-                   PCIE_IRQ);
+    gpex_dev = riscv_gpex_pcie_init(system_memory, pcie_irqchip,
+                                    &s->memmap[VIRT_PCIE_ECAM],
+                                    &s->memmap[VIRT_PCIE_MMIO],
+                                    &virt_high_pcie_memmap,
+                                    &s->memmap[VIRT_PCIE_PIO], PCIE_IRQ);
+    s->pci_bus = PCI_HOST_BRIDGE(gpex_dev)->bus;
 
     s->platform_bus_dev = riscv_create_platform_bus(mmio_irqchip,
         &s->memmap[VIRT_PLATFORM_BUS], VIRT_PLATFORM_BUS_IRQ,
@@ -972,6 +1009,8 @@ static void virt_machine_instance_init(Object *obj)
     s->acpi = ON_OFF_AUTO_AUTO;
     s->iommu_sys = ON_OFF_AUTO_AUTO;
     s->num_sources = VIRT_IRQCHIP_NUM_SOURCES;
+
+    cxl_machine_init(obj, &s->cxl_devices_state);
 }
 
 static char *virt_get_aia_guests(Object *obj, Error **errp)
