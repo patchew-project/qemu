5edf9e8d585ec9e8a9702c7d1306f1fefd52af12
Peter Xu <peterx@redhat.com>
2026-09-08 08:30:53 -0500

    kvm: Provide explicit error for kvm_create_guest_memfd()
    So that there will be a verbal string returned when kvm not enabled, or
    kvm not compiled.
    
    Signed-off-by: Peter Xu <peterx@redhat.com>
    Reviewed-by: Xiaoyao Li <xiaoyao.li@intel.com>
    Reviewed-by: Fabiano Rosas <farosas@suse.de>
    Reviewed-by: Michael Roth <michael.roth@amd.com>
    Signed-off-by: Michael Roth <michael.roth@amd.com>
    Reviewed-by: David Hildenbrand <david@kernel.org>
    Message-Id: <20260908133151.836685-4-michael.roth@amd.com>

--- accel/kvm/kvm-all.c
+++ accel/kvm/kvm-all.c
@@ -4758,6 +4758,18 @@ int kvm_create_guest_memfd(uint64_t size, uint64_t flags, Error **errp)
         .flags = flags,
     };
 
+    /*
+     * kvm_create_guest_memfd() can be invoked from generic hostmem code
+     * where KVM might be compiled but not necessarily enabled. Because
+     * of this, check for KVM support explictly before continuing with
+     * any KVM-specific checks.
+     *
+     */
+    if (!kvm_enabled()) {
+        error_setg(errp, "guest-memfd requires KVM accelerator");
+        return -1;
+    }
+
     if (!kvm_guest_memfd_supported) {
         error_setg(errp, "KVM does not support guest_memfd");
         return -1;
