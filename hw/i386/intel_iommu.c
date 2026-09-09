70ac092dce0229250cf98401b0b8f4b6b5378a5b
Zhenzhong Duan <zhenzhong.duan@intel.com>
2026-09-09 15:28:35 +0800

    intel_iommu_accel: Add PRQ injection for passthrough device
    When the guest enables the PRQ in vIOMMU, allocate a FAULTQ object so that
    host-side recoverable fault events can be received and propagated back to
    the guest.
    
    Install an event handler on the FAULTQ fd to read and propagate host
    generated recoverable fault events to the guest.
    
    The handler runs in QEMU's main loop, using a non-blocking fd registered
    via qemu_set_fd_handler().
    
    Signed-off-by: Zhenzhong Duan <zhenzhong.duan@intel.com>
    Tested-by: Xudong Hao <xudong.hao@intel.com>
    Message-Id: <20260909072837.3183154-5-zhenzhong.duan@intel.com>

--- hw/i386/intel_iommu.c
+++ hw/i386/intel_iommu.c
@@ -5383,11 +5383,15 @@ static int vtd_pri_perform_implicit_invalidation(VTDAddressSpace *vtd_as,
     return ret;
 }
 
-/* Page Request Descriptor : 7.4.1.1 */
-static int vtd_pri_request_page(PCIBus *bus, void *opaque, int devfn,
-                                uint32_t pasid, bool priv_req, bool exec_req,
-                                hwaddr addr, bool lpig, uint16_t prgi,
-                                bool is_read, bool is_write)
+/*
+ * Page Request Descriptor : 7.4.1.1
+ *
+ * Because it is facing the emulated device models, so this should be PCI
+ * PASID instead of the pasids used within vIOMMU.
+ */
+int vtd_pri_request_page(PCIBus *bus, void *opaque, int devfn, uint32_t pasid,
+                         bool priv_req, bool exec_req, hwaddr addr, bool lpig,
+                         uint16_t prgi, bool is_read, bool is_write)
 {
     IntelIOMMUState *s = opaque;
     VTDAddressSpace *vtd_as;
