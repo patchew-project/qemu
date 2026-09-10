11aabccdd4be622ab4d07bd518910a64c0eaf559
Xiong Weimin <xiongweimin@kylinos.cn>
2026-09-10 16:13:52 -0400

    hw/virtio: Propagate vhost_virtqueue_mask errors
    vhost_virtqueue_mask() currently reports failures from
    vhost_set_vring_call() but always returns void, so callers cannot
    react.  In particular, vhost_virtqueue_start() has a TODO next to the
    unmask call when !use_guest_notifier_mask.
    
    Make vhost_virtqueue_mask() return the underlying errno, and on failure
    in vhost_virtqueue_start() jump to the existing fail path which unmaps
    the vring and propagates the error to the device start caller.  Other
    call sites keep today's behavior by ignoring the return value.
    
    Signed-off-by: Xiong Weimin <xiongweimin@kylinos.cn>
    Reviewed-by: Michael S. Tsirkin <mst@redhat.com>
    Signed-off-by: Michael S. Tsirkin <mst@redhat.com>
    Message-ID: <20260807024359.2157914-1-xiongweimin@kylinos.cn>
    Message-Id: <22ffc542a1ab7ad898e3efacc0ce16c68ae85c73.1789071043.git.mst@redhat.com>

--- hw/virtio/vhost.c
+++ hw/virtio/vhost.c
@@ -1481,8 +1481,10 @@ int vhost_virtqueue_start(struct vhost_dev *dev,
      * will do it later.
      */
     if (!vdev->use_guest_notifier_mask) {
-        /* TODO: check and handle errors. */
-        vhost_virtqueue_mask(dev, vdev, idx, false);
+        r = vhost_virtqueue_mask(dev, vdev, idx, false);
+        if (r < 0) {
+            goto fail;
+        }
     }
 
     if (k->query_guest_notifiers &&
@@ -1962,7 +1964,7 @@ bool vhost_virtqueue_pending(struct vhost_dev *hdev, int n)
 }
 
 /* Mask/unmask events from this vq. */
-void vhost_virtqueue_mask(struct vhost_dev *hdev, VirtIODevice *vdev, int n,
+int vhost_virtqueue_mask(struct vhost_dev *hdev, VirtIODevice *vdev, int n,
                          bool mask)
 {
     struct VirtQueue *vvq = virtio_get_queue(vdev, n);
@@ -1983,7 +1985,9 @@ void vhost_virtqueue_mask(struct vhost_dev *hdev, VirtIODevice *vdev, int n,
     r = hdev->vhost_ops->vhost_set_vring_call(hdev, &file);
     if (r < 0) {
         error_report("vhost_set_vring_call failed %d", -r);
+        return r;
     }
+    return 0;
 }
 
 bool vhost_config_pending(struct vhost_dev *hdev)
