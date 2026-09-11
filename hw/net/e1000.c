4baba0044a128497795924d72136f5abafa6ad8a
Daniel P. Berrangé <berrange@redhat.com>
2026-09-11 15:36:08 +0100

    hw/net: mark e1000, e1000e, IGB, rtl8139 & sPAPR VLAN as secure
    Historically most NICs are only interesting for non-virtualization
    use cases and have not been written with malicious guests in mind.
    
    As a general rule either virtio-net or xen-net should be used in
    all virtualized guests requiring a security boundary.
    
    There are a handful of exceptions resulting from historical usage
    in the x86 world, to support virtualized guests lacking virtio
    support.
    
    Thus the rtl8139, e1000, e1000e & IGB NICs are declared to provide
    a security boundary.
    
    The PPC sPAPR Virutal LAN device is also marked secure.
    
    Signed-off-by: Daniel P. Berrangé <berrange@redhat.com>
    Message-Id: <20260911143627.2743803-10-berrange@redhat.com>

--- hw/net/e1000.c
+++ hw/net/e1000.c
@@ -1759,6 +1759,7 @@ static void e1000_register_types(void)
         type_info.parent = TYPE_E1000_BASE;
         type_info.class_data = info;
         type_info.class_init = e1000_class_init;
+        type_info.secure = true,
 
         type_register_static(&type_info);
     }
