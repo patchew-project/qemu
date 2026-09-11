patchew-project/qemu

commit 18e899e63dd9d05e567c081023ad7fc5b2c5dc9a

vfio-user: implement VFIO_USER_DMA_MAP/UNMAP
When the vfio-user container gets mapping updates, share them with the
vfio-user by sending a message; this can include the region fd, allowing
the server to directly mmap() the region as needed.

For performance, we only wait for the message responses when we're doing
with a series of updates via the listener_commit() callback.

Originally-by: John Johnson <john.g.johnson@oracle.com>
Signed-off-by: Jagannathan Raman <jag.raman@oracle.com>
Signed-off-by: Elena Ufimtseva <elena.ufimtseva@oracle.com>
Signed-off-by: John Levon <john.levon@nutanix.com>
Reviewed-by: Cédric Le Goater <clg@redhat.com>
Link: https://lore.kernel.org/qemu-devel/20250625193012.2316242-14-john.levon@nutanix.com
Signed-off-by: Cédric Le Goater <clg@redhat.com>

hw/vfio-user/container.c
@@ -12,23 +12,152 @@
 
 #include "hw/vfio-user/container.h"
 #include "hw/vfio-user/device.h"
+#include "hw/vfio-user/trace.h"
 #include "hw/vfio/vfio-cpr.h"
 #include "hw/vfio/vfio-device.h"
 #include "hw/vfio/vfio-listener.h"
 #include "qapi/error.h"
 
+/*
+ * When DMA space is the physical address space, the region add/del listeners
+ * will fire during memory update transactions.  These depend on BQL being held,
+ * so do any resulting map/demap ops async while keeping BQL.
+ */
+static void vfio_user_listener_begin(VFIOContainerBase *bcontainer)
+{
+    VFIOUserContainer *container = container_of(bcontainer, VFIOUserContainer,
+                                                 bcontainer);
+
+    container->proxy->async_ops = true;
+}
+
+static void vfio_user_listener_commit(VFIOContainerBase *bcontainer)
+{
+    VFIOUserContainer *container = container_of(bcontainer, VFIOUserContainer,
+                                            bcontainer);
+
+    /* wait here for any async requests sent during the transaction */
+    container->proxy->async_ops = false;
+    vfio_user_wait_reqs(container->proxy);
+}
+
 static int vfio_user_dma_unmap(const VFIOContainerBase *bcontainer,
                                hwaddr iova, ram_addr_t size,
                                IOMMUTLBEntry *iotlb, bool unmap_all)
 {
-    return -ENOTSUP;
+    VFIOUserContainer *container = container_of(bcontainer, VFIOUserContainer,
+                                            bcontainer);
+    Error *local_err = NULL;
+    int ret = 0;
+
+    VFIOUserDMAUnmap *msgp = g_malloc(sizeof(*msgp));
+
+    vfio_user_request_msg(&msgp->hdr, VFIO_USER_DMA_UNMAP, sizeof(*msgp), 0);
+    msgp->argsz = sizeof(struct vfio_iommu_type1_dma_unmap);
+    msgp->flags = unmap_all ? VFIO_DMA_UNMAP_FLAG_ALL : 0;
+    msgp->iova = iova;
+    msgp->size = size;
+    trace_vfio_user_dma_unmap(msgp->iova, msgp->size, msgp->flags,
+                              container->proxy->async_ops);
+
+    if (container->proxy->async_ops) {
+        if (!vfio_user_send_nowait(container->proxy, &msgp->hdr, NULL,
+                              0, &local_err)) {
+            error_report_err(local_err);
+            ret = -EFAULT;
+        } else {
+            ret = 0;
+        }
+    } else {
+        if (!vfio_user_send_wait(container->proxy, &msgp->hdr, NULL,
+                                 0, &local_err)) {
+                error_report_err(local_err);
+                ret = -EFAULT;
+        }
+
+        if (msgp->hdr.flags & VFIO_USER_ERROR) {
+            ret = -msgp->hdr.error_reply;
+        }
+
+        g_free(msgp);
+    }
+
+    return ret;
 }
 
 static int vfio_user_dma_map(const VFIOContainerBase *bcontainer, hwaddr iova,
                              ram_addr_t size, void *vaddr, bool readonly,
                              MemoryRegion *mrp)
 {
-    return -ENOTSUP;
+    VFIOUserContainer *container = container_of(bcontainer, VFIOUserContainer,
+                                                bcontainer);
+    int fd = memory_region_get_fd(mrp);
+    Error *local_err = NULL;
+    int ret;
+
+    VFIOUserFDs *fds = NULL;
+    VFIOUserDMAMap *msgp = g_malloc0(sizeof(*msgp));
+
+    vfio_user_request_msg(&msgp->hdr, VFIO_USER_DMA_MAP, sizeof(*msgp), 0);
+    msgp->argsz = sizeof(struct vfio_iommu_type1_dma_map);
+    msgp->flags = VFIO_DMA_MAP_FLAG_READ;
+    msgp->offset = 0;
+    msgp->iova = iova;
+    msgp->size = size;
+
+    /*
+     * vaddr enters as a QEMU process address; make it either a file offset
+     * for mapped areas or leave as 0.
+     */
+    if (fd != -1) {
+        msgp->offset = qemu_ram_block_host_offset(mrp->ram_block, vaddr);
+    }
+
+    if (!readonly) {
+        msgp->flags |= VFIO_DMA_MAP_FLAG_WRITE;
+    }
+
+    trace_vfio_user_dma_map(msgp->iova, msgp->size, msgp->offset, msgp->flags,
+                            container->proxy->async_ops);
+
+    /*
+     * The async_ops case sends without blocking. They're later waited for in
+     * vfio_send_wait_reqs.
+     */
+    if (container->proxy->async_ops) {
+        /* can't use auto variable since we don't block */
+        if (fd != -1) {
+            fds = vfio_user_getfds(1);
+            fds->send_fds = 1;
+            fds->fds[0] = fd;
+        }
+
+        if (!vfio_user_send_nowait(container->proxy, &msgp->hdr, fds,
+                              0, &local_err)) {
+            error_report_err(local_err);
+            ret = -EFAULT;
+        } else {
+            ret = 0;
+        }
+    } else {
+        VFIOUserFDs local_fds = { 1, 0, &fd };
+
+        fds = fd != -1 ? &local_fds : NULL;
+
+        if (!vfio_user_send_wait(container->proxy, &msgp->hdr, fds,
+                                 0, &local_err)) {
+                error_report_err(local_err);
+                ret = -EFAULT;
+        }
+
+        if (msgp->hdr.flags & VFIO_USER_ERROR) {
+            ret = -msgp->hdr.error_reply;
+        }
+
+        g_free(msgp);
+    }
+
+    return ret;
 }
 
 static int
@@ -218,6 +347,8 @@ static void vfio_iommu_user_class_init(ObjectClass *klass, const void *data)
     VFIOIOMMUClass *vioc = VFIO_IOMMU_CLASS(klass);
 
     vioc->setup = vfio_user_setup;
+    vioc->listener_begin = vfio_user_listener_begin,
+    vioc->listener_commit = vfio_user_listener_commit,
     vioc->dma_map = vfio_user_dma_map;
     vioc->dma_unmap = vfio_user_dma_unmap;
     vioc->attach_device = vfio_user_device_attach;

hw/vfio-user/proxy.c
@@ -27,7 +27,6 @@ static IOThread *vfio_user_iothread;
 static void vfio_user_shutdown(VFIOUserProxy *proxy);
 static VFIOUserMsg *vfio_user_getmsg(VFIOUserProxy *proxy, VFIOUserHdr *hdr,
                                      VFIOUserFDs *fds);
-static VFIOUserFDs *vfio_user_getfds(int numfds);
 static void vfio_user_recycle(VFIOUserProxy *proxy, VFIOUserMsg *msg);
 
 static void vfio_user_recv(void *opaque);
@@ -132,7 +131,7 @@ static void vfio_user_recycle(VFIOUserProxy *proxy, VFIOUserMsg *msg)
     QTAILQ_INSERT_HEAD(&proxy->free, msg, next);
 }
 
-static VFIOUserFDs *vfio_user_getfds(int numfds)
+VFIOUserFDs *vfio_user_getfds(int numfds)
 {
     VFIOUserFDs *fds = g_malloc0(sizeof(*fds) + (numfds * sizeof(int)));
 
@@ -619,6 +618,43 @@ static bool vfio_user_send_queued(VFIOUserProxy *proxy, VFIOUserMsg *msg,
 }
 
 /*
+ * nowait send - vfio_wait_reqs() can wait for it later
+ *
+ * Returns false if we did not successfully receive a reply message, in which
+ * case @errp will be populated.
+ *
+ * In either case, ownership of @hdr and @fds is taken, and the caller must
+ * *not* free them itself.
+ */
+bool vfio_user_send_nowait(VFIOUserProxy *proxy, VFIOUserHdr *hdr,
+                           VFIOUserFDs *fds, int rsize, Error **errp)
+{
+    VFIOUserMsg *msg;
+
+    QEMU_LOCK_GUARD(&proxy->lock);
+
+    msg = vfio_user_getmsg(proxy, hdr, fds);
+    msg->id = hdr->id;
+    msg->rsize = rsize ? rsize : hdr->size;
+    msg->type = VFIO_MSG_NOWAIT;
+
+    if (hdr->flags & VFIO_USER_NO_REPLY) {
+        error_setg_errno(errp, EINVAL, "%s on NO_REPLY message", __func__);
+        vfio_user_recycle(proxy, msg);
+        return false;
+    }
+
+    if (!vfio_user_send_queued(proxy, msg, errp)) {
+        vfio_user_recycle(proxy, msg);
+        return false;
+    }
+
+    proxy->last_nowait = msg;
+
+    return true;
+}
+
+/*
  * Returns false if we did not successfully receive a reply message, in which
  * case @errp will be populated.
  *
@@ -664,6 +700,50 @@ bool vfio_user_send_wait(VFIOUserProxy *proxy, VFIOUserHdr *hdr,
     qemu_mutex_unlock(&proxy->lock);
 
     return ok;
+}
+
+void vfio_user_wait_reqs(VFIOUserProxy *proxy)
+{
+    VFIOUserMsg *msg;
+
+    /*
+     * Any DMA map/unmap requests sent in the middle
+     * of a memory region transaction were sent nowait.
+     * Wait for them here.
+     */
+    qemu_mutex_lock(&proxy->lock);
+    if (proxy->last_nowait != NULL) {
+        /*
+         * Change type to WAIT to wait for reply
+         */
+        msg = proxy->last_nowait;
+        msg->type = VFIO_MSG_WAIT;
+        proxy->last_nowait = NULL;
+        while (!msg->complete) {
+            if (!qemu_cond_timedwait(&msg->cv, &proxy->lock, wait_time)) {
+                VFIOUserMsgQ *list;
+
+                list = msg->pending ? &proxy->pending : &proxy->outgoing;
+                QTAILQ_REMOVE(list, msg, next);
+                error_printf("vfio_wait_reqs - timed out\n");
+                break;
+            }
+        }
+
+        if (msg->hdr->flags & VFIO_USER_ERROR) {
+            error_printf("vfio_user_wait_reqs - error reply on async ");
+            error_printf("request: command %x error %s\n", msg->hdr->command,
+                         strerror(msg->hdr->error_reply));
+        }
+
+        /*
+         * Change type back to NOWAIT to free
+         */
+        msg->type = VFIO_MSG_NOWAIT;
+        vfio_user_recycle(proxy, msg);
+    }
+
+    qemu_mutex_unlock(&proxy->lock);
 }
 
 static QLIST_HEAD(, VFIOUserProxy) vfio_user_sockets =

hw/vfio-user/proxy.h
@@ -70,6 +70,7 @@ typedef struct VFIOUserProxy {
     QemuCond close_cv;
     AioContext *ctx;
     QEMUBH *req_bh;
+    bool async_ops;
 
     /*
      * above only changed when BQL is held
@@ -99,9 +100,14 @@ void vfio_user_set_handler(VFIODevice *vbasedev,
                            void *reqarg);
 bool vfio_user_validate_version(VFIOUserProxy *proxy, Error **errp);
 
+VFIOUserFDs *vfio_user_getfds(int numfds);
+
 void vfio_user_request_msg(VFIOUserHdr *hdr, uint16_t cmd,
                            uint32_t size, uint32_t flags);
+void vfio_user_wait_reqs(VFIOUserProxy *proxy);
 bool vfio_user_send_wait(VFIOUserProxy *proxy, VFIOUserHdr *hdr,
                          VFIOUserFDs *fds, int rsize, Error **errp);
+bool vfio_user_send_nowait(VFIOUserProxy *proxy, VFIOUserHdr *hdr,
+                           VFIOUserFDs *fds, int rsize, Error **errp);
 
 #endif /* VFIO_USER_PROXY_H */

hw/vfio-user/protocol.h
@@ -113,6 +113,31 @@ typedef struct {
 #define VFIO_USER_DEF_MAX_BITMAP (256 * 1024 * 1024)
 
 /*
+ * VFIO_USER_DMA_MAP
+ * imported from struct vfio_iommu_type1_dma_map
+ */
+typedef struct {
+    VFIOUserHdr hdr;
+    uint32_t argsz;
+    uint32_t flags;
+    uint64_t offset;    /* FD offset */
+    uint64_t iova;
+    uint64_t size;
+} VFIOUserDMAMap;
+
+/*
+ * VFIO_USER_DMA_UNMAP
+ * imported from struct vfio_iommu_type1_dma_unmap
+ */
+typedef struct {
+    VFIOUserHdr hdr;
+    uint32_t argsz;
+    uint32_t flags;
+    uint64_t iova;
+    uint64_t size;
+} VFIOUserDMAUnmap;
+
+/*
  * VFIO_USER_DEVICE_GET_INFO
  * imported from struct vfio_device_info
  */
@@ -174,5 +199,12 @@ typedef struct {
     uint32_t count;
     char data[];
 } VFIOUserRegionRW;
+
+/*imported from struct vfio_bitmap */
+typedef struct {
+    uint64_t pgsize;
+    uint64_t size;
+    char data[];
+} VFIOUserBitmap;
 
 #endif /* VFIO_USER_PROTOCOL_H */

hw/vfio-user/trace-events
@@ -13,3 +13,7 @@ vfio_user_get_region_info(uint32_t index, uint32_t flags, uint64_t size) " index
 vfio_user_region_rw(uint32_t region, uint64_t off, uint32_t count) " region %d offset 0x%"PRIx64" count %d"
 vfio_user_get_irq_info(uint32_t index, uint32_t flags, uint32_t count) " index %d flags 0x%x count %d"
 vfio_user_set_irqs(uint32_t index, uint32_t start, uint32_t count, uint32_t flags) " index %d start %d count %d flags 0x%x"
+
+# container.c
+vfio_user_dma_map(uint64_t iova, uint64_t size, uint64_t off, uint32_t flags, bool async_ops) " iova 0x%"PRIx64" size 0x%"PRIx64" off 0x%"PRIx64" flags 0x%x async_ops %d"
+vfio_user_dma_unmap(uint64_t iova, uint64_t size, uint32_t flags,  bool async_ops) " iova 0x%"PRIx64" size 0x%"PRIx64" flags 0x%x async_ops %d"

commit 770ae44b86b3f1702415e31346b0019d2d527af9

vfio-user: propagate DMA map/unmap request errors
vfio_user_wait_reqs() uses the youngest nowait request as an ordering
fence, but error replies for older requests are logged and recycled. An
error reply or timeout for the youngest request is also only logged. The
memory listener therefore commits even when QEMU and the server may
disagree about DMA mappings.

Preserve the first error from each nowait batch and return it to the
listener commit callback. Propagate failures during listener registration
so device realization fails cleanly. After initialization, report the
error and stop with hw_error(), matching the existing VFIO DMA mapping
failure policy.

The memory listener commit callback cannot roll back a FlatView that has
already advanced. Treat listener teardown the same way: until the proxy is
disconnected, a failed unmap leaves the server's DMA access unconfirmed.
Stopping QEMU avoids continued execution with inconsistent DMA state.

Add unit coverage for an older failed request followed by a successful
youngest fence, a timeout, and an all-success batch.

Fixes: 18e899e63dd9 ("vfio-user: implement VFIO_USER_DMA_MAP/UNMAP")
Signed-off-by: Yuho Choi <oss.patchbox@gmail.com>
Message-Id: <20260910213526.1472467-1-dbgh9129@gmail.com>

MAINTAINERS
@@ -4582,6 +4582,7 @@ F: docs/system/devices/vfio-user.rst
 F: hw/vfio-user/*
 F: subprojects/libvfio-user.wrap
 F: tests/functional/x86_64/test_vfio_user_client.py
+F: tests/unit/test-vfio-user-proxy.c
 
 EBPF
 M: Jason Wang <jasowangio@gmail.com>

hw/vfio-user/container.c
@@ -10,6 +10,7 @@
 #include <sys/ioctl.h>
 #include <linux/vfio.h>
 
+#include "hw/core/hw-error.h"
 #include "hw/vfio-user/container.h"
 #include "hw/vfio-user/device.h"
 #include "hw/vfio-user/trace.h"
@@ -33,10 +34,25 @@ static void vfio_user_listener_begin(VFIOContainer *bcontainer)
 static void vfio_user_listener_commit(VFIOContainer *bcontainer)
 {
     VFIOUserContainer *container = VFIO_IOMMU_USER(bcontainer);
+    Error *local_err = NULL;
 
     /* wait here for any async requests sent during the transaction */
     container->proxy->async_ops = false;
-    vfio_user_wait_reqs(container->proxy);
+    if (vfio_user_wait_reqs(container->proxy, &local_err)) {
+        return;
+    }
+
+    error_prepend(&local_err, "vfio-user DMA mapping transaction failed: ");
+    if (!bcontainer->initialized) {
+        if (!bcontainer->error) {
+            error_propagate(&bcontainer->error, local_err);
+        } else {
+            error_free(local_err);
+        }
+    } else {
+        error_report_err(local_err);
+        hw_error("vfio-user: DMA mapping failed, unable to continue");
+    }
 }
 
 static int vfio_user_dma_unmap(const VFIOContainer *bcontainer,

hw/vfio-user/proxy.c
@@ -52,6 +52,26 @@ static void vfio_user_shutdown(VFIOUserProxy *proxy)
                                    proxy->ctx, NULL, NULL);
 }
 
+static void vfio_user_record_nowait_error(VFIOUserProxy *proxy,
+                                           const Error *err)
+{
+    if (proxy->nowait_error == NULL) {
+        proxy->nowait_error = error_copy(err);
+    }
+}
+
+static void vfio_user_record_nowait_reply_error(VFIOUserProxy *proxy,
+                                                 const VFIOUserMsg *msg)
+{
+    if (proxy->nowait_error == NULL) {
+        int error = msg->hdr->error_reply ?: EIO;
+
+        error_setg_errno(&proxy->nowait_error, error,
+                         "vfio-user command 0x%x failed",
+                         msg->hdr->command);
+    }
+}
+
 /*
  * Same return values as qio_channel_writev_full():
  *
@@ -162,10 +182,14 @@ static void vfio_user_process(VFIOUserProxy *proxy, VFIOUserMsg *msg)
             qemu_cond_signal(&msg->cv);
         } else {
             if (msg->hdr->flags & VFIO_USER_ERROR) {
-                error_printf("vfio_user_process: error reply on async ");
-                error_printf("request command %x error %s\n",
-                             msg->hdr->command,
-                             strerror(msg->hdr->error_reply));
+                if (msg->type == VFIO_MSG_NOWAIT) {
+                    vfio_user_record_nowait_reply_error(proxy, msg);
+                } else {
+                    error_printf("vfio_user_process: error reply on async ");
+                    error_printf("request command %x error %s\n",
+                                 msg->hdr->command,
+                                 strerror(msg->hdr->error_reply));
+                }
             }
             /* youngest nowait msg has been ack'd */
             if (proxy->last_nowait == msg) {
@@ -426,7 +450,15 @@ err:
              */
             vfio_user_set_error(msg->hdr, EINVAL);
             msg->complete = true;
-            qemu_cond_signal(&msg->cv);
+            if (msg->type == VFIO_MSG_NOWAIT) {
+                vfio_user_record_nowait_reply_error(proxy, msg);
+                if (proxy->last_nowait == msg) {
+                    proxy->last_nowait = NULL;
+                }
+                vfio_user_recycle(proxy, msg);
+            } else {
+                qemu_cond_signal(&msg->cv);
+            }
         }
     }
     return -1;
@@ -669,6 +701,7 @@ static bool vfio_user_send_queued(VFIOUserProxy *proxy, VFIOUserMsg *msg,
 bool vfio_user_send_nowait(VFIOUserProxy *proxy, VFIOUserHdr *hdr,
                            VFIOUserFDs *fds, int rsize, Error **errp)
 {
+    Error *local_err = NULL;
     VFIOUserMsg *msg;
 
     QEMU_LOCK_GUARD(&proxy->lock);
@@ -679,12 +712,17 @@ bool vfio_user_send_nowait(VFIOUserProxy *proxy, VFIOUserHdr *hdr,
     msg->type = VFIO_MSG_NOWAIT;
 
     if (hdr->flags & VFIO_USER_NO_REPLY) {
-        error_setg_errno(errp, EINVAL, "%s on NO_REPLY message", __func__);
+        error_setg_errno(&local_err, EINVAL,
+                         "%s on NO_REPLY message", __func__);
+        vfio_user_record_nowait_error(proxy, local_err);
+        error_propagate(errp, local_err);
         vfio_user_recycle(proxy, msg);
         return false;
     }
 
-    if (!vfio_user_send_queued(proxy, msg, errp)) {
+    if (!vfio_user_send_queued(proxy, msg, &local_err)) {
+        vfio_user_record_nowait_error(proxy, local_err);
+        error_propagate(errp, local_err);
         vfio_user_recycle(proxy, msg);
         return false;
     }
@@ -777,21 +815,24 @@ bool vfio_user_send_async(VFIOUserProxy *proxy, VFIOUserHdr *hdr,
     return true;
 }
 
-void vfio_user_wait_reqs(VFIOUserProxy *proxy)
+/*
+ * Wait for completion of the current nowait batch.  DMA map/unmap requests
+ * sent during a memory transaction are nowait requests.  The server processes
+ * commands in receive order, so the youngest reply is a completion fence for
+ * all older requests.  Errors from older replies are saved until the fence is
+ * reached.
+ */
+bool vfio_user_wait_reqs(VFIOUserProxy *proxy, Error **errp)
 {
-    VFIOUserMsg *msg;
+    bool success = true;
 
-    /*
-     * Any DMA map/unmap requests sent in the middle
-     * of a memory region transaction were sent nowait.
-     * Wait for them here.
-     */
     qemu_mutex_lock(&proxy->lock);
     if (proxy->last_nowait != NULL) {
+        VFIOUserMsg *msg = proxy->last_nowait;
+
         /*
          * Change type to WAIT to wait for reply
          */
-        msg = proxy->last_nowait;
         msg->type = VFIO_MSG_WAIT;
         proxy->last_nowait = NULL;
         while (!msg->complete) {
@@ -801,15 +842,16 @@ void vfio_user_wait_reqs(VFIOUserProxy *proxy)
 
                 list = msg->pending ? &proxy->pending : &proxy->outgoing;
                 QTAILQ_REMOVE(list, msg, next);
-                error_printf("vfio_wait_reqs - timed out\n");
+                if (proxy->nowait_error == NULL) {
+                    error_setg_errno(&proxy->nowait_error, ETIMEDOUT,
+                                     "timed out waiting for vfio-user reply");
+                }
                 break;
             }
         }
 
         if (msg->hdr->flags & VFIO_USER_ERROR) {
-            error_printf("vfio_user_wait_reqs - error reply on async ");
-            error_printf("request: command %x error %s\n", msg->hdr->command,
-                         strerror(msg->hdr->error_reply));
+            vfio_user_record_nowait_reply_error(proxy, msg);
         }
 
         /*
@@ -819,7 +861,15 @@ void vfio_user_wait_reqs(VFIOUserProxy *proxy)
         vfio_user_recycle(proxy, msg);
     }
 
+    if (proxy->nowait_error != NULL) {
+        error_propagate(errp, proxy->nowait_error);
+        proxy->nowait_error = NULL;
+        success = false;
+    }
+
     qemu_mutex_unlock(&proxy->lock);
+
+    return success;
 }
 
 /*
@@ -1018,6 +1068,7 @@ void vfio_user_disconnect(VFIOUserProxy *proxy)
 
     /* we now hold the only ref to proxy */
     qemu_mutex_unlock(&proxy->lock);
+    error_free(proxy->nowait_error);
     qemu_cond_destroy(&proxy->close_cv);
     qemu_mutex_destroy(&proxy->lock);
 

hw/vfio-user/proxy.h
@@ -83,6 +83,7 @@ typedef struct VFIOUserProxy {
     VFIOUserMsgQ incoming;
     VFIOUserMsgQ outgoing;
     VFIOUserMsg *last_nowait;
+    Error *nowait_error;
     VFIOUserMsg *part_recv;
     size_t recv_left;
     VFIOUserWRMulti *wr_multi;
@@ -116,7 +117,7 @@ void vfio_user_disable_posted_writes(VFIOUserProxy *proxy);
 
 void vfio_user_request_msg(VFIOUserHdr *hdr, uint16_t cmd,
                            uint32_t size, uint32_t flags);
-void vfio_user_wait_reqs(VFIOUserProxy *proxy);
+bool vfio_user_wait_reqs(VFIOUserProxy *proxy, Error **errp);
 bool vfio_user_send_wait(VFIOUserProxy *proxy, VFIOUserHdr *hdr,
                          VFIOUserFDs *fds, int rsize, Error **errp);
 bool vfio_user_send_nowait(VFIOUserProxy *proxy, VFIOUserHdr *hdr,

tests/unit/meson.build
@@ -148,6 +148,14 @@ if have_system
       genh, qemuutil, qom
     ],
   }
+  if host_os == 'linux'
+    tests += {
+      'test-vfio-user-proxy': [
+        'iothread.c', io,
+        meson.project_source_root() / 'hw/vfio-user/proxy.c',
+      ],
+    }
+  endif
   if config_host_data.get('CONFIG_INOTIFY1')
     tests += {'test-util-filemonitor': []}
   endif

tests/unit/test-vfio-user-proxy.c
@@ -1,0 +1,222 @@
+/*
+ * vfio-user proxy tests
+ *
+ * SPDX-License-Identifier: GPL-2.0-or-later
+ */
+
+#include "qemu/osdep.h"
+#include <glib/gstdio.h>
+#include <sys/socket.h>
+#include <sys/un.h>
+
+#include "hw/vfio/vfio-device.h"
+#include "hw/vfio-user/proxy.h"
+#include "qapi/error.h"
+#include "qemu/cutils.h"
+#include "qemu/main-loop.h"
+#include "qemu/module.h"
+#include "qemu/sockets.h"
+#include "system/iothread.h"
+#include "iothread.h"
+
+typedef enum TestReplyMode {
+    TEST_REPLY_SUCCESS,
+    TEST_REPLY_PRIOR_ERROR,
+    TEST_REPLY_TIMEOUT,
+} TestReplyMode;
+
+typedef struct TestServer {
+    char *tmpdir;
+    char *path;
+    int listen_fd;
+    TestReplyMode reply_mode;
+    GThread *thread;
+} TestServer;
+
+IOThread *iothread_create(const char *id, Error **errp)
+{
+    (void)id;
+    (void)errp;
+    return iothread_new();
+}
+
+void iothread_destroy(IOThread *iothread)
+{
+    iothread_join(iothread);
+}
+
+static void read_all(int fd, void *buf, size_t len)
+{
+    char *p = buf;
+
+    while (len) {
+        ssize_t ret = read(fd, p, len);
+
+        if (ret < 0 && errno == EINTR) {
+            continue;
+        }
+        g_assert_cmpint(ret, >, 0);
+        p += ret;
+        len -= ret;
+    }
+}
+
+static void write_all(int fd, const void *buf, size_t len)
+{
+    const char *p = buf;
+
+    while (len) {
+        ssize_t ret = write(fd, p, len);
+
+        if (ret < 0 && errno == EINTR) {
+            continue;
+        }
+        g_assert_cmpint(ret, >, 0);
+        p += ret;
+        len -= ret;
+    }
+}
+
+static gpointer test_server_thread(gpointer opaque)
+{
+    TestServer *server = opaque;
+    int conn_fd;
+    int i;
+
+    do {
+        conn_fd = accept(server->listen_fd, NULL, NULL);
+    } while (conn_fd < 0 && errno == EINTR);
+    g_assert_cmpint(conn_fd, >=, 0);
+
+    for (i = 0; i < 2; i++) {
+        VFIOUserHdr request;
+        VFIOUserHdr reply;
+
+        read_all(conn_fd, &request, sizeof(request));
+        g_assert_cmpuint(request.size, ==, sizeof(request));
+
+        reply = request;
+        reply.size = sizeof(reply);
+        reply.flags = VFIO_USER_REPLY;
+        reply.error_reply = 0;
+        if (i == 0 && server->reply_mode == TEST_REPLY_PRIOR_ERROR) {
+            reply.flags |= VFIO_USER_ERROR;
+            reply.error_reply = EIO;
+        }
+        if (server->reply_mode != TEST_REPLY_TIMEOUT) {
+            write_all(conn_fd, &reply, sizeof(reply));
+        }
+    }
+
+    while (read(conn_fd, &i, sizeof(i)) < 0 && errno == EINTR) {
+        ;
+    }
+
+    close(conn_fd);
+    return NULL;
+}
+
+static void test_server_start(TestServer *server, TestReplyMode reply_mode)
+{
+    struct sockaddr_un addr = { .sun_family = AF_UNIX };
+
+    server->tmpdir = g_dir_make_tmp("qemu-test-vfio-user.XXXXXX", NULL);
+    g_assert_nonnull(server->tmpdir);
+    server->path = g_build_filename(server->tmpdir, "socket", NULL);
+    g_assert_cmpuint(strlen(server->path), <, sizeof(addr.sun_path));
+    pstrcpy(addr.sun_path, sizeof(addr.sun_path), server->path);
+
+    server->listen_fd = socket(AF_UNIX, SOCK_STREAM, 0);
+    g_assert_cmpint(server->listen_fd, >=, 0);
+    g_assert_no_errno(bind(server->listen_fd, (struct sockaddr *)&addr,
+                           sizeof(addr)));
+    g_assert_no_errno(listen(server->listen_fd, 1));
+
+    server->reply_mode = reply_mode;
+    server->thread = g_thread_new("vfio-user-test-server",
+                                  test_server_thread, server);
+}
+
+static void test_server_stop(TestServer *server)
+{
+    g_thread_join(server->thread);
+    close(server->listen_fd);
+    g_assert_cmpint(g_unlink(server->path), ==, 0);
+    g_assert_cmpint(g_rmdir(server->tmpdir), ==, 0);
+    g_free(server->path);
+    g_free(server->tmpdir);
+}
+
+static void unexpected_request(void *opaque, VFIOUserMsg *msg)
+{
+    (void)opaque;
+    (void)msg;
+    g_assert_not_reached();
+}
+
+static void send_nowait(VFIOUserProxy *proxy)
+{
+    VFIOUserHdr *hdr = g_new0(VFIOUserHdr, 1);
+
+    vfio_user_request_msg(hdr, VFIO_USER_DMA_UNMAP, sizeof(*hdr), 0);
+    g_assert_true(vfio_user_send_nowait(proxy, hdr, NULL, 0, &error_abort));
+}
+
+static void test_nowait_replies(gconstpointer opaque)
+{
+    TestReplyMode reply_mode = GPOINTER_TO_INT(opaque);
+    TestServer server = { 0 };
+    SocketAddress addr = { .type = SOCKET_ADDRESS_TYPE_UNIX };
+    VFIOUserProxy *proxy;
+    VFIODevice vbasedev = { 0 };
+    Error *err = NULL;
+
+    test_server_start(&server, reply_mode);
+    addr.u.q_unix.path = server.path;
+
+    proxy = vfio_user_connect_dev(&addr, &error_abort);
+    proxy->wait_time = reply_mode == TEST_REPLY_TIMEOUT ? 50 : 5000;
+    vbasedev.proxy = proxy;
+    vfio_user_set_handler(&vbasedev, unexpected_request, NULL);
+
+    send_nowait(proxy);
+    send_nowait(proxy);
+
+    if (reply_mode == TEST_REPLY_PRIOR_ERROR) {
+        g_assert_false(vfio_user_wait_reqs(proxy, &err));
+        g_assert_nonnull(err);
+        g_assert_nonnull(strstr(error_get_pretty(err), strerror(EIO)));
+        error_free(err);
+    } else if (reply_mode == TEST_REPLY_TIMEOUT) {
+        g_assert_false(vfio_user_wait_reqs(proxy, &err));
+        g_assert_nonnull(err);
+        g_assert_nonnull(strstr(error_get_pretty(err), "timed out"));
+        error_free(err);
+    } else {
+        g_assert_true(vfio_user_wait_reqs(proxy, &err));
+        g_assert_null(err);
+    }
+
+    vfio_user_disconnect(proxy);
+    test_server_stop(&server);
+}
+
+int main(int argc, char **argv)
+{
+    module_call_init(MODULE_INIT_QOM);
+    qemu_init_main_loop(&error_abort);
+    socket_init();
+    g_test_init(&argc, &argv, NULL);
+
+    g_test_add_data_func("/vfio-user/proxy/nowait/prior-error",
+                         GINT_TO_POINTER(TEST_REPLY_PRIOR_ERROR),
+                         test_nowait_replies);
+    g_test_add_data_func("/vfio-user/proxy/nowait/timeout",
+                         GINT_TO_POINTER(TEST_REPLY_TIMEOUT),
+                         test_nowait_replies);
+    g_test_add_data_func("/vfio-user/proxy/nowait/success",
+                         GINT_TO_POINTER(TEST_REPLY_SUCCESS),
+                         test_nowait_replies);
+
+    return g_test_run();
+}