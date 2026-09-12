1ae36c10df57e4af6357a68cb80b44886def08e1
Richard Henderson <richard.henderson@linaro.org>
2026-09-12 00:30:15 -1000

    tcg: Generic expansion of tcg_gen_subfi_*
    Signed-off-by: Richard Henderson <richard.henderson@linaro.org>
    Message-Id: <20260912103126.598030-12-richard.henderson@linaro.org>

--- tcg/tcg-op.c
+++ tcg/tcg-op.c
@@ -335,6 +335,9 @@ void tcg_gen_plugin_mem_cb(TCGv_i64 addr, unsigned meminfo)
 #define DEF_RRI(NAME) \
     static void glue(gen_,NAME)(TCGType, TCGTemp *, TCGTemp *, int64_t);
 
+#define DEF_RIR(NAME) \
+    static void glue(gen_,NAME)(TCGType, TCGTemp *, int64_t, TCGTemp *);
+
 #include "tcg/tcg-op-def2.h.inc"
 
 #undef DEF_R
@@ -342,6 +345,7 @@ void tcg_gen_plugin_mem_cb(TCGv_i64 addr, unsigned meminfo)
 #undef DEF_RRR
 #undef DEF_RI
 #undef DEF_RRI
+#undef DEF_RIR
 
 /*
  * Generic expansions for templated operations.
@@ -395,6 +399,15 @@ static void gen_subi(TCGType type, TCGTemp *dst, TCGTemp *src1, int64_t src2)
     gen_addi(type, dst, src1, -src2);
 }
 
+static void gen_subfi(TCGType type, TCGTemp *dst, int64_t src1, TCGTemp *src2)
+{
+    if (src1 == 0) {
+        gen_neg(type, dst, src2);
+    } else {
+        gen_sub(type, dst, tcg_constant_internal(type, src1), src2);
+    }
+}
+
 /*
  * Templated operations.
  */
@@ -419,6 +432,10 @@ static void gen_subi(TCGType type, TCGTemp *dst, TCGTemp *src1, int64_t src2)
     void glue(glue(tcg_gen_,NAME),TEXT)(TCGV a, TCGV b, TINT c)         \
     { gen_##NAME(TYPE, TTMP(a), TTMP(b), c); }
 
+#define DEF_RIR(NAME)                                                   \
+    void glue(glue(tcg_gen_,NAME),TEXT)(TCGV a, TINT b, TCGV c)         \
+    { gen_##NAME(TYPE, TTMP(a), b, TTMP(c)); }
+
 #include "tcg/tcg-op-def.h.inc"
 
 #undef DEF_R
@@ -426,17 +443,9 @@ static void gen_subi(TCGType type, TCGTemp *dst, TCGTemp *src1, int64_t src2)
 #undef DEF_RRR
 #undef DEF_RI
 #undef DEF_RRI
+#undef DEF_RIR
 
 /* 32 bit ops */
-
-void tcg_gen_subfi_i32(TCGv_i32 ret, int32_t arg1, TCGv_i32 arg2)
-{
-    if (arg1 == 0) {
-        tcg_gen_neg_i32(ret, arg2);
-    } else {
-        tcg_gen_sub_i32(ret, tcg_constant_i32(arg1), arg2);
-    }
-}
 
 void tcg_gen_and_i32(TCGv_i32 ret, TCGv_i32 arg1, TCGv_i32 arg2)
 {
@@ -1544,15 +1553,6 @@ void tcg_gen_sar_i64(TCGv_i64 ret, TCGv_i64 arg1, TCGv_i64 arg2)
 void tcg_gen_mul_i64(TCGv_i64 ret, TCGv_i64 arg1, TCGv_i64 arg2)
 {
     tcg_gen_op3_i64(INDEX_op_mul, ret, arg1, arg2);
-}
-
-void tcg_gen_subfi_i64(TCGv_i64 ret, int64_t arg1, TCGv_i64 arg2)
-{
-    if (arg1 == 0) {
-        tcg_gen_neg_i64(ret, arg2);
-    } else {
-        tcg_gen_sub_i64(ret, tcg_constant_i64(arg1), arg2);
-    }
 }
 
 void tcg_gen_andi_i64(TCGv_i64 ret, TCGv_i64 arg1, int64_t arg2)
