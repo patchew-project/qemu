b7001754d54ecf3433f852d2eb6e8eb4613975fe
Richard Henderson <richard.henderson@linaro.org>
2026-09-12 16:37:57 -1000

    tcg: Split out gen_extrh_i64_i32
    We'll need this for other generic expansions.
    
    Signed-off-by: Richard Henderson <richard.henderson@linaro.org>
    Message-Id: <20260913023837.741866-47-richard.henderson@linaro.org>

--- tcg/tcg-op.c
+++ tcg/tcg-op.c
@@ -411,6 +411,7 @@ void tcg_gen_plugin_mem_cb(TCGv_i64 addr, unsigned meminfo)
 #undef DEF4
 #undef DEF6
 
+static void gen_extrh_i64_i32(TCGTemp *dst, TCGTemp *src);
 static void gen_extrl_i64_i32(TCGTemp *dst, TCGTemp *src);
 static void gen_extu_i32_i64(TCGTemp *dst, TCGTemp *src);
 
@@ -538,6 +539,11 @@ static void gen_eqv(TCGType type, TCGTemp *dst, TCGTemp *src1, TCGTemp *src2)
         gen_xor(type, dst, src1, src2);
         gen_not(type, dst, dst);
     }
+}
+
+static void gen_extrh_i64_i32(TCGTemp *dst, TCGTemp *src)
+{
+    gen_op_tt(INDEX_op_extrh_i64_i32, TCG_TYPE_I32, dst, src);
 }
 
 static void gen_extrl_i64_i32(TCGTemp *dst, TCGTemp *src)
@@ -2291,8 +2297,7 @@ void tcg_gen_extrl_i64_i32(TCGv_i32 ret, TCGv_i64 arg)
 
 void tcg_gen_extrh_i64_i32(TCGv_i32 ret, TCGv_i64 arg)
 {
-    tcg_gen_op2(INDEX_op_extrh_i64_i32, TCG_TYPE_I32,
-                tcgv_i32_arg(ret), tcgv_i64_arg(arg));
+    gen_extrh_i64_i32(tcgv_i32_temp(ret), tcgv_i64_temp(arg));
 }
 
 void tcg_gen_extu_i32_i64(TCGv_i64 ret, TCGv_i32 arg)
