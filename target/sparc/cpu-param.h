1945526ced16efe60cfa7fbaa332defeeb8ce205
Richard Henderson <richard.henderson@linaro.org>
2026-09-12 16:34:55 -1000

    target/sparc: Enable variable page size for user-only
    Signed-off-by: Richard Henderson <richard.henderson@linaro.org>
    Message-Id: <20260913023455.741626-1-richard.henderson@linaro.org>

--- target/sparc/cpu-param.h
+++ target/sparc/cpu-param.h
@@ -7,15 +7,17 @@
 #ifndef SPARC_CPU_PARAM_H
 #define SPARC_CPU_PARAM_H
 
-#ifdef TARGET_SPARC64
+#ifdef CONFIG_USER_ONLY
+# define TARGET_PAGE_BITS_VARY
+#elif defined(ifdef TARGET_SPARC64)
 # define TARGET_PAGE_BITS 13 /* 8k */
-# ifdef TARGET_ABI32
-#  define TARGET_VIRT_ADDR_SPACE_BITS 32
-# else
-#  define TARGET_VIRT_ADDR_SPACE_BITS 44
-# endif
 #else
 # define TARGET_PAGE_BITS 12 /* 4k */
+#endif
+
+#if defined(TARGET_SPARC64) && !defined(TARGET_ABI32)
+# define TARGET_VIRT_ADDR_SPACE_BITS 44
+#else
 # define TARGET_VIRT_ADDR_SPACE_BITS 32
 #endif
 
