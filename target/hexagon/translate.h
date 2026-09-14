5ab7dddaaa143da949d8b950fbd47301a386818c
Brian Cain <brian.cain@oss.qualcomm.com>
2026-09-14 08:51:33 -0700

    target/hexagon: share HVX contexts among CPUs
    The HVX register file belongs to an extension context, not a hardware thread.
    Model each system-emulation context as a QOM device, link the contexts to
    each vCPU, and use SSR:XA to select the active context.
    
    Keep one embedded context per linux-user CPU.  Carry the selected register-file
    base with HVX operands so translated code can address the selected context
    at run time.
    
    Reviewed-by: Richard Henderson <richard.henderson@linaro.org>
    Signed-off-by: Brian Cain <brian.cain@oss.qualcomm.com>
    Message-Id: <20260914155134.2191912-6-brian.cain@oss.qualcomm.com>

--- target/hexagon/translate.h
+++ target/hexagon/translate.h
@@ -192,9 +192,9 @@ static inline void ctx_log_reg_read_pair(DisasContext *ctx, int rnum)
 }
 
 intptr_t ctx_future_vreg_off(DisasContext *ctx, int regnum,
-                             int num, bool alloc_ok);
+                             int num, bool alloc_ok, TCGv_ptr *base);
 intptr_t ctx_tmp_vreg_off(DisasContext *ctx, int regnum,
-                          int num, bool alloc_ok);
+                          int num, bool alloc_ok, TCGv_ptr *base);
 
 static inline void ctx_start_hvx_insn(DisasContext *ctx)
 {
@@ -328,6 +328,13 @@ extern TCGv_i64 hex_llsc_val_i64;
 extern TCGv hex_vstore_addr[VSTORES_MAX];
 extern TCGv hex_vstore_size[VSTORES_MAX];
 extern TCGv hex_vstore_pending[VSTORES_MAX];
+#ifdef CONFIG_USER_ONLY
+#define hex_hvx_ptr tcg_env
+#define HEX_HVX_OFFSET(member) offsetof(CPUHexagonState, hvx_ctx.member)
+#else
+extern TCGv_ptr hex_hvx_ptr;
+#define HEX_HVX_OFFSET(member) offsetof(HexagonHVXContext, member)
+#endif
 #ifndef CONFIG_USER_ONLY
 extern TCGv_i32 hex_greg[NUM_GREGS];
 extern TCGv_i32 hex_t_sreg[NUM_SREGS];
